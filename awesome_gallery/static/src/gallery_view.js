9dcdbceb50f592501294fd48586549e1b4f7605b
Dardenne Florent (dafl) <dafl@odoo.com>
2022-09-27 16:58:48 +0200

    [Solution] 4.3 Parse the arch

--- awesome_gallery/static/src/gallery_view.js
+++ awesome_gallery/static/src/gallery_view.js
@@ -2,6 +2,7 @@
 
 import { registry } from "@web/core/registry";
 import { GalleryController } from "./gallery_controller";
+import { GalleryArchParser } from "./gallery_arch_parser";
 
 export const galleryView = {
     type: "gallery",
@@ -9,6 +10,18 @@ export const galleryView = {
     icon: "fa fa-picture-o",
     multiRecord: true,
     Controller: GalleryController,
+    ArchParser: GalleryArchParser,
+
+    props(genericProps, view) {
+        const { ArchParser } = view;
+        const { arch } = genericProps;
+        const archInfo = new ArchParser().parse(arch);
+
+        return {
+            ...genericProps,
+            archInfo,
+        };
+    },
 };
 
 registry.category("views").add("gallery", galleryView);
