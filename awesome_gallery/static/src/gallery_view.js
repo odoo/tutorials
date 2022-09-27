0973fa0c56a7b97109430f5e902e35be226ef73c
Dardenne Florent (dafl) <dafl@odoo.com>
2022-09-27 14:57:05 +0200

    [Solution] 2.1 Make a hello world view

---
+++ awesome_gallery/static/src/gallery_view.js
@@ -0,0 +1,12 @@
+import { registry } from "@web/core/registry";
+import { GalleryController } from "./gallery_controller";
+
+export const galleryView = {
+    type: "gallery",
+    display_name: "Gallery",
+    icon: "fa fa-picture-o",
+    multiRecord: true,
+    Controller: GalleryController,
+};
+
+registry.category("views").add("gallery", galleryView);
