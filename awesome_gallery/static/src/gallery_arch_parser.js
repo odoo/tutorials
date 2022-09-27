a58611d6567b7ca89ea076a35e7f14b7dfd5d2c4
Dardenne Florent (dafl) <dafl@odoo.com>
2022-09-27 18:19:23 +0200

    [Solution] 4.4 Load some data

--- awesome_gallery/static/src/gallery_arch_parser.js
+++ awesome_gallery/static/src/gallery_arch_parser.js
@@ -3,8 +3,10 @@
 export class GalleryArchParser {
     parse(xmlDoc) {
         const imageField = xmlDoc.getAttribute("image_field");
+        const limit = xmlDoc.getAttribute("limit") || 80;
         return {
             imageField,
+            limit,
         };
     }
 }
