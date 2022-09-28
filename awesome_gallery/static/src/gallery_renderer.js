d6b22f0860a4010f28bc081994b8b12c9489d07d
Dardenne Florent (dafl) <dafl@odoo.com>
2022-09-28 15:45:14 +0200

    [Solution] 2.8 Display images

--- awesome_gallery/static/src/gallery_renderer.js
+++ awesome_gallery/static/src/gallery_renderer.js
@@ -1,7 +1,9 @@
 import { Component } from "@odoo/owl";
 import { GalleryModel } from "./gallery_model";
+import { GalleryImage } from "./gallery_image";
 
 export class GalleryRenderer extends Component {
+    static components = { GalleryImage };
     static template = "awesome_gallery.GalleryRenderer";
     static props = {
         model: GalleryModel,
