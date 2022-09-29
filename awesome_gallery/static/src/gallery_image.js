ac465adc873d4e04aa4a9c8970c41d708f663874
Dardenne Florent (dafl) <dafl@odoo.com>
2022-09-29 08:13:25 +0200

    [Solution] 4.9 Switch to form view on click

--- awesome_gallery/static/src/gallery_image.js
+++ awesome_gallery/static/src/gallery_image.js
@@ -3,6 +3,7 @@
 import { Component } from "@odoo/owl";
 import { url } from "@web/core/utils/urls";
 import { GalleryModel } from "./gallery_model";
+import { useService } from "@web/core/utils/hooks";
 
 export class GalleryImage extends Component {
     static template = "awesome_gallery.GalleryImage";
@@ -10,6 +11,14 @@ export class GalleryImage extends Component {
         record: Object,
         model: GalleryModel,
     };
+
+    setup() {
+        this.action = useService("action");
+    }
+
+    onImageClick(resId) {
+        this.action.switchView("form", { resId });
+    }
 
     get imageUrl() {
         return url("/web/image", {
