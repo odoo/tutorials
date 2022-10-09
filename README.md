13bd3424979dd3b43a3255b75d368c83c663ed78
Géry Debongnie <ged@odoo.com>
2022-10-09 20:21:18 +0200

    [ADD] initial commit

--- README.md
+++ README.md
@@ -1,10 +1,7 @@
-# Odoo tutorials
+![Odoo Logo](_images/odoo_logo.png)
 
-This repository hosts the code for the bases and solutions of the
-[official Odoo tutorials](https://www.odoo.com/documentation/16.0/developer/howtos.html).
+# Learn the JS Framework
 
-It has 2 branches for each Odoo version: one for the bases and one for
-the solutions. For example, `16.0` and `16.0-solutions`. The first
-contains the code of the modules that serve as base for the tutorials,
-and the second contains the code of the same modules with the complete
-solution.
+This branch contains the commits used as support for my presentation at
+Odoo experience on the topic "Learn the Javascript Framework".
+
---
+++ awesome_dashboard/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Awesome Dashboard",
+
+    'summary': """
+        Companion addon for the Odoo Smartclass 2024 on the JS Framework
+    """,
+
+    'description': """
+        Companion addon for the Odoo Smartclass 2024 on the JS Framework
+    """,
+
+    'author': "Odoo",
+    'website': "https://www.odoo.com/",
+    'category': 'Tutorials',
+    'version': '0.1',
+    'application': True,
+    'installable': True,
+    'depends': ['base', 'web', 'mail', 'crm'],
+
+    'data': [
+        'views/views.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'awesome_dashboard/static/src/**/*',
+        ],
+    },
+    'license': 'AGPL-3'
+}
