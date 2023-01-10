05f6fb8f90c0500e2372bb86a1a2c908cff185a6
Maruan Aguerdouh (magm) <magm@odoo.com>
2023-01-10 13:37:07 +0100

    Everything from chaptre 1 done (11/11)

--- owl_playground/static/src/playground.js
+++ owl_playground/static/src/playground.js
@@ -3,8 +3,9 @@
 import { Component } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { TodoList } from "./todo_list/todo_list";
+import { Card } from "./card/card";
 
 export class Playground extends Component {}
 
-Playground.components = { Counter, TodoList }
+Playground.components = { Counter, TodoList, Card }
 Playground.template = "owl_playground.playground";
