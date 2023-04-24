da8d38ae2fe55f98462037a7cc3a7cede4f40f7d
Antoine Vandevenne (anv) <anv@odoo.com>
2023-04-24 17:26:28 +0200

    [IMP] owl_playground,awesome_tshirt: apply JS guidelines to base code
    Forward-port of fc95927

--- owl_playground/static/src/playground.js
+++ owl_playground/static/src/playground.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import { Component, useState } from "@odoo/owl";
+import { Component } from "@odoo/owl";
 
 export class Playground extends Component {
     static template = "owl_playground.playground";
