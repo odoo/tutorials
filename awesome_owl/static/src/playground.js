32135cbb2563419abe0a3a414cd42a0e94732f2d
Géry Debongnie <ged@odoo.com>
2023-05-10 15:27:57 +0200

    [SOLUTION] 1.4 Using markup to display html

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,8 +1,13 @@
-import { Component } from "@odoo/owl";
+import { Component, markup } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
     static components = { Counter, Card };
+
+    setup() {
+        this.str1 = "<div class='text-primary'>some content</div>";
+        this.str2 = markup("<div class='text-primary'>some content</div>");
+    }
 }
