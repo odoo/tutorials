180bbc2fdcd8cae0859522a93c45f1b4aaad22c5
Géry Debongnie <ged@odoo.com>
2023-05-10 15:41:32 +0200

    [Solution] 1.6 The sum of two Counter

--- awesome_owl/static/src/counter/counter.js
+++ awesome_owl/static/src/counter/counter.js
@@ -4,6 +4,9 @@ import { Component, useState } from "@odoo/owl";
 
 export class Counter extends Component {
     static template = "awesome_owl.Counter";
+    static props = {
+        onChange: { type: Function, optional: true }
+    };
 
     setup() {
         this.state = useState({ value: 1 });
@@ -11,5 +14,8 @@ export class Counter extends Component {
 
     increment() {
         this.state.value = this.state.value + 1;
+        if (this.props.onChange) {
+            this.props.onChange();
+        }
     }
 }
