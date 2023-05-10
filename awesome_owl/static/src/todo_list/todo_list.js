2ce95336d593196863610f05e247a44f64b55880
Géry Debongnie <ged@odoo.com>
2023-05-10 15:50:22 +0200

    [Solution] 1.7 A todo list

---
+++ awesome_owl/static/src/todo_list/todo_list.js
@@ -0,0 +1,14 @@
+import { Component, useState } from "@odoo/owl";
+import { TodoItem } from "./todo_item";
+
+export class TodoList extends Component {
+    static template = "awesome_owl.TodoList";
+    static components = { TodoItem };
+
+    setup() {
+        this.todos = useState([
+            { id: 2, description: "write tutorial", isCompleted: true },
+            { id: 3, description: "buy milk", isCompleted: false },
+        ]);
+    }
+}
