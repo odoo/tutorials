b6b36a9f6e70d0cf7637a2a946f6f34caef9ce0a
Géry Debongnie <ged@odoo.com>
2023-05-10 22:06:54 +0200

    [Solution] 2.7 Real life update

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import { Component, onWillStart } from "@odoo/owl";
+import { Component, onWillStart, useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
 import { useService } from "@web/core/utils/hooks";
@@ -13,13 +13,10 @@ class AwesomeDashboard extends Component {
 
     setup() {
         this.action = useService("action");
-        this.statistics = useService("awesome_dashboard.statistics");
+        this.statistics = useState(useService("awesome_dashboard.statistics"));
         this.display = {
             controlPanel: {},
         };
-        onWillStart(async () => {
-            this.statistics = await this.statistics.loadStatistics();
-        });
     }
 
     openCustomerView() {
