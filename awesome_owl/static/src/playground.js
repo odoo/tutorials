fbc72d4d4e9ca4f65e9ad200d9a96a7b462519de
Géry Debongnie <ged@odoo.com>
2023-05-10 15:41:32 +0200

    [Solution] 1.6 The sum of two Counter

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import { Component, markup } from "@odoo/owl";
+import { Component, markup, useState } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
 
@@ -11,5 +11,10 @@ export class Playground extends Component {
     setup() {
         this.str1 = "<div class='text-primary'>some content</div>";
         this.str2 = markup("<div class='text-primary'>some content</div>");
+        this.sum = useState({ value: 2 });
+    }
+
+    incrementSum() {
+        this.sum.value++;
     }
 }
