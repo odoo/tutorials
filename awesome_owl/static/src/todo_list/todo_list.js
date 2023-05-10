313518da0dfda39f36862804a5ad8bd8ff6e765f
Géry Debongnie <ged@odoo.com>
2023-05-10 16:03:32 +0200

    [Solution] 1.10 Focusing the input

--- awesome_owl/static/src/todo_list/todo_list.js
+++ awesome_owl/static/src/todo_list/todo_list.js
@@ -2,6 +2,7 @@
 
 import { Component, useState } from "@odoo/owl";
 import { TodoItem } from "./todo_item";
+import { useAutofocus } from "../utils";
 
 export class TodoList extends Component {
     static template = "awesome_owl.TodoList";
@@ -10,6 +11,7 @@ export class TodoList extends Component {
     setup() {
         this.nextId = 0;
         this.todos = useState([]);
+        useAutofocus("input")
     }
 
     addTodo(ev) {
