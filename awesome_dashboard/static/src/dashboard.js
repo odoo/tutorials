756d3577d7617d4595b9a1338ffe5185cd27b7b5
Géry Debongnie <ged@odoo.com>
2023-05-10 18:20:56 +0200

    [Solution] 2.4 Call the server, add some statistics

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, onWillStart } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
 import { useService } from "@web/core/utils/hooks";
@@ -12,9 +12,13 @@ class AwesomeDashboard extends Component {
 
     setup() {
         this.action = useService("action");
+        this.rpc = useService("rpc");
         this.display = {
             controlPanel: {},
         };
+        onWillStart(async () => {
+            this.statistics = await this.rpc("/awesome_dashboard/statistics");
+        });
     }
 
     openCustomerView() {
