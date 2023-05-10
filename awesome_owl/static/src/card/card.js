30d6a9bf92101e5182fd51395b825f7675c457a2
Géry Debongnie <ged@odoo.com>
2023-05-10 15:35:45 +0200

    [Solution] 1.5 Props validation

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -4,4 +4,8 @@ import { Component } from "@odoo/owl";
 
 export class Card extends Component {
     static template = "awesome_owl.Card";
+    static props = {
+        title: String,
+        content: String,
+    };
 }
