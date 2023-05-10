7ec5b0fa57e94d05c39f08220a27a809091436ad
Géry Debongnie <ged@odoo.com>
2023-05-10 16:30:19 +0200

    [Solution] 1.13 Generic card with slots

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -6,6 +6,11 @@ export class Card extends Component {
     static template = "awesome_owl.Card";
     static props = {
         title: String,
-        content: String,
+        slots: {
+            type: Object,
+            shape: {
+                default: true
+            },
+        }
     };
 }
