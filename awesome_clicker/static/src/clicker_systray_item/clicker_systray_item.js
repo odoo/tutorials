e42122161465bef3355c9125eea6d92bdf04d500
fdardenne <dafl@odoo.com>
2023-08-16 11:05:19 +0200

    [Solution] 3.1 Create a systray item

---
+++ awesome_clicker/static/src/clicker_systray_item/clicker_systray_item.js
@@ -0,0 +1,23 @@
+/** @odoo-module */
+
+import { registry } from "@web/core/registry";
+import { Component, useState } from "@odoo/owl";
+
+export class ClickerSystray extends Component {
+    static template = "awesome_clicker.ClickerSystray";
+
+    setup() {
+        this.state = useState({ counter: 0 });
+    }
+
+    increment() {
+        this.state.counter++;
+    }
+
+}
+
+export const systrayItem = {
+    Component: ClickerSystray,
+};
+
+registry.category("systray").add("awesome_clicker.ClickerSystray", systrayItem, { sequence: 1000 });
