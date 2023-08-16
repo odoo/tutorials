34325bc622956b987fd7724bf865c37d192bdc0e
fdardenne <dafl@odoo.com>
2023-08-16 11:45:43 +0200

    [SOLUTION] 4.4 Move the state to a service

---
+++ awesome_clicker/static/src/clicker_service.js
@@ -0,0 +1,21 @@
+import { registry } from "@web/core/registry";
+import { reactive } from "@odoo/owl";
+
+const clickerService = {
+    start(env) {
+        const state = reactive({ clicks: 0 });
+
+        function increment(inc) {
+            state.clicks += inc;
+        }
+
+        document.addEventListener("click", () => increment(1), true);
+
+        return {
+            state,
+            increment,
+        };
+    },
+};
+
+registry.category("services").add("awesome_clicker.clicker", clickerService);
--- awesome_clicker/static/src/clicker_systray_item/clicker_systray_item.js
+++ awesome_clicker/static/src/clicker_systray_item/clicker_systray_item.js
@@ -1,4 +1,4 @@
-import { Component, useState, useExternalListener } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
 
@@ -7,13 +7,8 @@ export class ClickerSystray extends Component {
     static props = {};
 
     setup() {
-        this.state = useState({ counter: 0 });
         this.action = useService("action");
-        useExternalListener(document.body, "click", () => this.state.counter++, true);
-    }
-
-    increment() {
-        this.state.counter += 9;
+        this.clickService = useState(useService("awesome_clicker.clicker"));
     }
 
     openClientAction() {
