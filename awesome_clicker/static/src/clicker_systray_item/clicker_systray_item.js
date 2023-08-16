de33471e35d6e807d096412825f3b33d238950bd
fdardenne <dafl@odoo.com>
2023-08-16 11:10:44 +0200

    [Solution] 1.2 Count external clicks

--- awesome_clicker/static/src/clicker_systray_item/clicker_systray_item.js
+++ awesome_clicker/static/src/clicker_systray_item/clicker_systray_item.js
@@ -1,7 +1,7 @@
 /** @odoo-module */
 
 import { registry } from "@web/core/registry";
-import { Component, useState } from "@odoo/owl";
+import { Component, useState, useExternalListener } from "@odoo/owl";
 
 export class ClickerSystray extends Component {
     static template = "awesome_clicker.ClickerSystray";
@@ -9,12 +9,12 @@ export class ClickerSystray extends Component {
 
     setup() {
         this.state = useState({ counter: 0 });
+        useExternalListener(document.body, "click", () => this.state.counter++, true);
     }
 
     increment() {
-        this.state.counter++;
+        this.state.counter += 9;
     }
-
 }
 
 export const systrayItem = {
