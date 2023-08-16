66429b8759bbeaa745f4f6d262192921fbca1c04
fdardenne <dafl@odoo.com>
2023-08-16 11:45:43 +0200

    [Solution] 1.4 Move the state to a service

--- awesome_clicker/static/src/client_action/client_action.js
+++ awesome_clicker/static/src/client_action/client_action.js
@@ -1,9 +1,14 @@
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";
 
 class ClickerClientAction extends Component {
     static template = "awesome_clicker.ClickerClientAction";
     static props = ["*"];
+
+    setup() {
+        this.clickService = useState(useService("awesome_clicker.clicker"));
+    }
 }
 
 registry.category("actions").add("awesome_clicker.client_action", ClickerClientAction);
