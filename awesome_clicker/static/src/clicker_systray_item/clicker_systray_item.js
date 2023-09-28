89465239ff3285a6a44265e6339d681e12e2987d
fdardenne <dafl@odoo.com>
2023-09-28 15:45:27 +0200

    [Solution] 3.6 Humanize the displayed value

--- awesome_clicker/static/src/clicker_systray_item/clicker_systray_item.js
+++ awesome_clicker/static/src/clicker_systray_item/clicker_systray_item.js
@@ -4,9 +4,11 @@ import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
 import { useClicker } from "../clicker_hook";
+import { ClickerValue } from "../clicker_value/clicker_value";
 
 export class ClickerSystray extends Component {
     static template = "awesome_clicker.ClickerSystray";
+    static components = { ClickerValue };
 
     setup() {
         this.action = useService("action");
--- awesome_clicker/static/src/client_action/client_action.js
+++ awesome_clicker/static/src/client_action/client_action.js
@@ -3,9 +3,10 @@
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { useClicker } from "../clicker_hook";
-
+import { ClickerValue } from "../clicker_value/clicker_value";
 class ClickerClientAction extends Component {
     static template = "awesome_clicker.ClickerClientAction";
+    static components = { ClickerValue };
 
     setup() {
         this.clicker = useClicker();
