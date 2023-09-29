71fe587b9aaf82a0fc71088185b398146d5b90e3
fdardenne <dafl@odoo.com>
2023-09-29 10:30:29 +0200

    [Solution] 3.10 Notify when a milestone is reached

--- awesome_clicker/static/src/clicker_service.js
+++ awesome_clicker/static/src/clicker_service.js
@@ -2,13 +2,21 @@ import { registry } from "@web/core/registry";
 import { ClickerModel } from "./clicker_model";
 
 const clickerService = {
-    start(env) {
+    dependencies: ["effect"],
+    start(env, services) {
         const model = new ClickerModel();
 
         document.addEventListener("click", () => model.addClick(), true);
         setInterval(() => {
             model.tick();
         }, 10000);
+        const bus = model.bus;
+        bus.addEventListener("MILESTONE_1k", () => {
+            services.effect.add({
+                message: "Milestone reached! You can now buy clickbots",
+                type: "rainbow_man",
+            });
+        });
 
         return model;
     },
