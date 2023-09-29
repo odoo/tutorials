5319268994729ca801b95c31185dc8494faf1133
fdardenne <dafl@odoo.com>
2023-09-29 14:00:59 +0200

    Solution] 3.11 Add BigBots

--- awesome_clicker/static/src/clicker_model.js
+++ awesome_clicker/static/src/clicker_model.js
@@ -8,29 +8,58 @@ export class ClickerModel extends Reactive {
         super();
         this.clicks = 0;
         this.level = 0;
-        this.clickBots = 0;
         this.bus = new EventBus();
+        this.bots = {
+            clickbot: {
+                price: 1000,
+                level: 1,
+                increment: 10,
+                purchased: 0,
+            },
+            bigbot: {
+                price: 5000,
+                level: 2,
+                increment: 100,
+                purchased: 0,
+            }
+        }
+
 
         document.addEventListener("click", () => this.increment(1), true);
         setInterval(() => {
-            this.clicks += this.clickBots * 10;
+            for (const bot in this.bots) {
+                this.clicks += this.bots[bot].increment * this.bots[bot].purchased;
+            }
         }, 10000);
     }
 
     increment(inc) {
         this.clicks += inc;
-        if (this.level < 1 && this.clicks >= 1000) {
-            this.bus.trigger("MILESTONE_1k");
-            this.level++;
+        if (
+            this.milestones[this.level] &&
+            this.clicks >= this.milestones[this.level].clicks
+        ) {
+            this.bus.trigger("MILESTONE", this.milestones[this.level]);
+            this.level += 1;
         }
     }
 
-    buyClickBot() {
-        const clickBotPrice = 1000;
-        if (this.clicks < clickBotPrice) {
+    buyBot(name) {
+        if (!Object.keys(this.bots).includes(name)) {
+            throw new Error(`Invalid bot name ${name}`);
+        }
+        if (this.clicks < this.bots[name].price) {
             return false;
         }
-        this.clicks -= clickBotPrice;
-        this.clickBots += 1;
+
+        this.clicks -= this.bots[name].price;
+        this.bots[name].purchased += 1;
+    }
+
+    get milestones() {
+        return [
+            { clicks: 1000, unlock: "clickBot" },
+            { clicks: 5000, unlock: "bigBot" },
+        ];
     }
 }
