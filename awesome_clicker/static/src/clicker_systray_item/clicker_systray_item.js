424987842036a8752cd2e3612794dc1e259689cd
fdardenne <dafl@odoo.com>
2023-10-03 13:03:45 +0200

    [Solution] 3.17 Use a dropdown menu for the systray item

--- awesome_clicker/static/src/clicker_systray_item/clicker_systray_item.js
+++ awesome_clicker/static/src/clicker_systray_item/clicker_systray_item.js
@@ -5,10 +5,12 @@ import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
 import { useClicker } from "../clicker_hook";
 import { ClickerValue } from "../clicker_value/clicker_value";
+import { Dropdown } from "@web/core/dropdown/dropdown";
+import { DropdownItem } from "@web/core/dropdown/dropdown_item";
 
 export class ClickerSystray extends Component {
     static template = "awesome_clicker.ClickerSystray";
-    static components = { ClickerValue };
+    static components = { ClickerValue, Dropdown, DropdownItem };
 
     setup() {
         this.action = useService("action");
@@ -22,6 +24,22 @@ export class ClickerSystray extends Component {
             target: "new",
             name: "Clicker Game"
         });
+    }
+
+    get numberTrees() {
+        let sum = 0;
+        for (const tree in this.clicker.trees) {
+            sum += this.clicker.trees[tree].purchased;
+        }
+        return sum;
+    }
+
+    get numberFruits() {
+        let sum = 0;
+        for (const fruit in this.clicker.fruits) {
+            sum += this.clicker.fruits[fruit];
+        }
+        return sum;
     }
 }
 
