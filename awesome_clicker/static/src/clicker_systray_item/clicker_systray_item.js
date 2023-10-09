943522cd12be67f8f00d369b0e1567f7d0cf8d56
fdardenne <dafl@odoo.com>
2023-10-09 15:06:51 +0200

    [Solution] 3.5 Use a custom hook

--- awesome_clicker/static/src/clicker_systray_item/clicker_systray_item.js
+++ awesome_clicker/static/src/clicker_systray_item/clicker_systray_item.js
@@ -1,6 +1,7 @@
-import { Component, useState } from "@odoo/owl";
+import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
+import { useClicker } from "../clicker_hook";
 
 export class ClickerSystray extends Component {
     static template = "awesome_clicker.ClickerSystray";
@@ -8,7 +9,7 @@ export class ClickerSystray extends Component {
 
     setup() {
         this.action = useService("action");
-        this.clickService = useState(useService("awesome_clicker.clicker"));
+        this.clicker = useClicker();
     }
 
     openClientAction() {
