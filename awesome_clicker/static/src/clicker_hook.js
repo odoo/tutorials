f4396f2b150e55ec66e0d86e87ed6942d1fff821
fdardenne <dafl@odoo.com>
2023-10-09 15:06:51 +0200

    [Solution] 1.5 Use a custom hook

---
+++ awesome_clicker/static/src/clicker_hook.js
@@ -0,0 +1,8 @@
+/** @odoo-module */
+
+import { useService } from "@web/core/utils/hooks";
+import { useState } from "@odoo/owl";
+
+export function useClicker() {
+    return useState(useService("awesome_clicker.clicker"));
+}
