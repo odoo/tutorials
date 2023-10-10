7ab2433be6b51882c45727cd542b90f90f4ede47
fdardenne <dafl@odoo.com>
2023-10-10 13:33:42 +0200

    [Solution] 3.9 Refactor to a class model

--- awesome_clicker/static/src/clicker_service.js
+++ awesome_clicker/static/src/clicker_service.js
@@ -1,43 +1,11 @@
 /** @odoo-module */
 
 import { registry } from "@web/core/registry";
-import { reactive } from "@odoo/owl";
+import { ClickerModel } from "./clicker_model";
 
 const clickerService = {
     start(env) {
-        const state = reactive({
-            clicks: 0,
-            level: 0,
-            clickBots: 0,
-        });
-
-        setInterval(() => {
-            state.clicks += state.clickBots * 10;
-        }, 10000);
-
-        function increment(inc) {
-            state.clicks += inc;
-            if (state.level < 1 && state.clicks >= 1000) {
-                state.level++;
-            }
-        }
-
-        function buyClickBot() {
-            const clickBotPrice = 1000;
-            if (state.clicks < clickBotPrice) {
-                return false;
-            }
-            state.clicks -= clickBotPrice;
-            state.clickBots += 1;
-        }
-
-        document.addEventListener("click", () => increment(1), true );
-
-        return {
-            state,
-            increment,
-            buyClickBot,
-        };
+        return new ClickerModel();
     },
 };
 
