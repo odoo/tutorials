f391e35571a33467cf7a22a570a5ea76d36413ea
fdardenne <dafl@odoo.com>
2023-10-16 10:43:45 +0200

    [Solution] 2.13 Upload button on images

--- awesome_gallery/static/src/gallery_image.js
+++ awesome_gallery/static/src/gallery_image.js
@@ -4,12 +4,15 @@ import { Component } from "@odoo/owl";
 import { url } from "@web/core/utils/urls";
 import { GalleryModel } from "./gallery_model";
 import { useService } from "@web/core/utils/hooks";
+import { FileUploader } from "@web/views/fields/file_handler";
 
 export class GalleryImage extends Component {
+    static components = { FileUploader }
     static template = "awesome_gallery.GalleryImage";
     static props = {
         record: Object,
         model: GalleryModel,
+        onImageUpload: Function,
     };
 
     setup() {
@@ -25,6 +28,11 @@ export class GalleryImage extends Component {
             model: this.props.model.resModel,
             id: this.props.record.id,
             field: this.props.model.imageField,
+            unique: this.props.record.write_date,
         });
+    }
+
+    async _onFileUploaded({ data }) {
+        await this.props.onImageUpload(this.props.record.id, data);
     }
 }
