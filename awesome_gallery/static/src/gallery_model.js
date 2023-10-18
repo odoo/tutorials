dba2e6227d1ac0ba64d3e13b98872d93ec465c2c
fdardenne <dafl@odoo.com>
2023-10-18 11:18:26 +0200

    [Solution] 4.14 Advanced tooltip template

--- awesome_gallery/static/src/gallery_model.js
+++ awesome_gallery/static/src/gallery_model.js
@@ -6,53 +6,35 @@ export class GalleryModel {
     constructor(orm, resModel, fields, archInfo) {
         this.orm = orm;
         this.resModel = resModel;
-        const { imageField, limit, tooltipField } = archInfo;
+        const { imageField, limit, fieldsForTooltip } = archInfo;
+        this.fieldsForTooltip = fieldsForTooltip;
         this.imageField = imageField;
         this.fields = fields;
         this.limit = limit;
-        this.tooltipField = tooltipField;
         this.keepLast = new KeepLast();
         this.pager = { offset: 0, limit: limit };
     }
 
     async load(domain) {
+        const specification = {
+            [this.imageField]: {},
+            write_date: {},
+        }
+        for (const field of this.fieldsForTooltip) {
+            specification[field] = {};
+        }
         const { length, records } = await this.keepLast.add(
             this.orm.webSearchRead(this.resModel, domain, {
                 limit: this.pager.limit,
                 offset: this.pager.offset,
-                specification: {
-                    [this.imageField]: {},
-                    ...( this.tooltipField ? {[this.tooltipField]: {}}: {}),
-                    write_date: {}
-                },
+                specification,
                 context: {
                     bin_size: true,
                 }
             })
         );
         this.recordsLength = length;
-
-        if (!this.tooltipField) {
-            this.records = records;
-            return;
-        }
-
-        switch (this.fields[this.tooltipField].type) {
-            case "many2one":
-                this.records = records.map((record) => ({
-                    ...record,
-                    [this.tooltipField]: record[this.tooltipField][1],
-                }));
-                break;
-            case "integer":
-                this.records = records.map((record) => ({
-                    ...record,
-                    [this.tooltipField]: String(record[this.tooltipField]),
-                }));
-                break;
-            default:
-                this.records = records;
-        }
+        this.records = records;
     }
 
     async uploadImage(record_id, image_binary, domain) {
