f95b62e6e74c2bdbefd8ad1dc23414b7ae0864bc
fdardenne <dafl@odoo.com>
2023-10-23 14:39:40 +0200

    [Solution] 3.2: Create a CustomerList component

---
+++ awesome_kanban/static/src/customer_list/customer_list.js
@@ -0,0 +1,12 @@
+/** @odoo-module */
+
+import { Component } from "@odoo/owl";
+
+export class CustomerList extends Component {
+    static template = "awesome_kanban.CustomerList";
+    static props = {
+        selectCustomer: {
+            type: Function,
+        },
+    };
+}
