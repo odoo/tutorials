7dcb3f19cbe44fd5e92f2870914e249036d0fa3e
fdardenne <dafl@odoo.com>
2023-10-30 09:43:05 +0100

    [Solution] 3.5: Only display customers which have an active order

--- awesome_kanban/static/src/customer_list/customer_list.js
+++ awesome_kanban/static/src/customer_list/customer_list.js
@@ -1,5 +1,6 @@
 import { useService } from "@web/core/utils/hooks";
-import { Component, onWillStart } from "@odoo/owl";
+import { Component, onWillStart, useState} from "@odoo/owl";
+import { KeepLast } from "@web/core/utils/concurrency";
 
 export class CustomerList extends Component {
     static template = "awesome_kanban.CustomerList";
@@ -11,8 +12,22 @@ export class CustomerList extends Component {
 
     setup() {
         this.orm = useService("orm");
+        this.partners = useState({ data: [] });
+        this.keepLast = new KeepLast();
+
         onWillStart(async () => {
-            this.partners = await this.orm.searchRead("res.partner", [], ["display_name"]);
+            this.partners.data = await this.loadCustomers([]);
         })
     }
+
+    async onChangeActiveCustomers(ev) {
+        const checked = ev.target.checked;
+        const domain = checked ?  [["opportunity_ids", "!=", false]] : [];
+        this.partners.data = await this.keepLast.add(this.loadCustomers(domain));
+    }
+
+    loadCustomers(domain) {
+        return this.orm.searchRead("res.partner", domain, ["display_name"]);
+    }
+
 }
