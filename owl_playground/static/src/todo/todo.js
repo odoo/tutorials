fca268cc24098e07b867488fb290ef903168d051
ABE-Mark45 <ahmedinmasr@gmail.com>
2023-11-30 17:39:36 +0200

    [ADD] todo: added props validation

--- owl_playground/static/src/todo/todo.js
+++ owl_playground/static/src/todo/todo.js
@@ -4,4 +4,15 @@ import { Component } from "@odoo/owl";
 
 export class Todo extends Component {
     static template = "owl_playground.todo";
+
+    static props = {
+        todo: {
+            type: Object,
+            shape: {
+                id: Number,
+                description: String,
+                done: Boolean
+            }
+        }
+    }
 }
