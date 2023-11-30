6dc026d87fe2fddb295dc5eed2d1f0c8cd052d19
ABE-Mark45 <ahmedinmasr@gmail.com>
2023-11-30 21:26:17 +0200

    [ADD] card: added card component (CH #10, #11)

--- owl_playground/static/src/playground.js
+++ owl_playground/static/src/playground.js
@@ -2,10 +2,11 @@
 
 import { Component } from "@odoo/owl";
 import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
 import { TodoList } from "./todolist/todolist";
 
 export class Playground extends Component {
     static template = "owl_playground.playground";
 
-    static components = { Counter, TodoList };
+    static components = { Counter, TodoList, Card };
 }
