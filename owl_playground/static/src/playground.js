d5d0ff3b124a88b2ddd0d74bb5ea0c0fc0226b07
ABE-Mark45 <ahmedinmasr@gmail.com>
2023-11-30 17:31:23 +0200

    [ADD] todo: added todo component (CH #3)

--- owl_playground/static/src/playground.js
+++ owl_playground/static/src/playground.js
@@ -1,10 +1,18 @@
 /** @odoo-module **/
 
-import { Component, useState } from "@odoo/owl";
+import { Component } from "@odoo/owl";
 import { Counter } from "./counter/counter";
+import { Todo } from "./todo/todo";
 
 export class Playground extends Component {
     static template = "owl_playground.playground";
-    
-    static components = { Counter };
+
+    static components = { Counter, Todo };
+
+    todos = [
+        { id: 1, description: "buy milk 1", done: false },
+        { id: 2, description: "buy milk 2", done: true },
+        { id: 3, description: "buy milk 3", done: false },
+        { id: 4, description: "buy milk 4", done: true },
+    ]
 }
