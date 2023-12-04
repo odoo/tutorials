ce2490781c5f8e1aec029075fafce1e1b4131fbf
ABE-Mark45 <ahmedinmasr@gmail.com>
2023-12-04 09:36:25 +0200

    [ADD] dashboard: add default layout component (CH #2.1)

--- awesome_tshirt/static/src/dashboard.js
+++ awesome_tshirt/static/src/dashboard.js
@@ -1,11 +1,21 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, useSubEnv } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { getDefaultConfig } from "@web/views/view";
+import { Layout } from "@web/search/layout";
 
-class AwesomeDashboard extends Component {}
+class AwesomeDashboard extends Component {
+    setup() {
+        const config = {
+            ...getDefaultConfig(),
+            ...this.env.config,
+        };
+        useSubEnv({ config });
+    }
+}
 
-AwesomeDashboard.components = {};
+AwesomeDashboard.components = { Layout };
 AwesomeDashboard.template = "awesome_tshirt.clientaction";
 
 registry.category("actions").add("awesome_tshirt.dashboard", AwesomeDashboard);
