2d522f6143d9adea2673f4e7b1dd56312d642a39
ABE-Mark45 <ahmedinmasr@gmail.com>
2023-12-04 14:59:13 +0200

    [ADD] dashboard: added order buttons (CH #2.2)

--- awesome_tshirt/static/src/dashboard.js
+++ awesome_tshirt/static/src/dashboard.js
@@ -4,15 +4,51 @@ import { Component, useSubEnv } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { getDefaultConfig } from "@web/views/view";
 import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
+import { Domain } from "@web/core/domain";
+
 
 class AwesomeDashboard extends Component {
+    static defaultFilters = ["('create_date', '>=', (context_today() - datetime.timedelta(days = 7)).strftime('%Y-%m-%d'))"];
     setup() {
         const config = {
             ...getDefaultConfig(),
             ...this.env.config,
         };
         useSubEnv({ config });
+        this.action = useService("action");
     }
+
+    openCustomers() {
+        this.action.doAction({
+            res_model: "res.partner",
+            type: "ir.actions.act_window",
+            views: [[false, "kanban"]],
+            name: this.env._t('Customers'),
+        });
+    }
+
+    openOrders(name, definedFilters = []) {
+        const filters = [...AwesomeDashboard.defaultFilters, ...definedFilters];
+
+        this.action.doAction({
+            res_model: "awesome_tshirt.order",
+            type: "ir.actions.act_window",
+            views: [[false, "list"], [false, "form"]],
+            name: this.env._t(name),
+            domain: new Domain(`[${filters.join()}]`).toList()
+        });
+    }
+
+
+    openNewOrders() {
+        this.openOrders('New Orders');
+    }
+
+    openCancelledOrders() {
+        this.openOrders('Cancelled Orders', ["('state', '=', 'cancelled')"]);
+    }
+
 }
 
 AwesomeDashboard.components = { Layout };
