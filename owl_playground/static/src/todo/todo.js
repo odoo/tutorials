34c4319e9b728388eb4190a7fb637c67d24e7b8f
sbel-odoo <sbel@odoo.com>
2023-12-04 14:55:14 +0100

    [IMP] owl_playground: Props validation

--- owl_playground/static/src/todo/todo.js
+++ owl_playground/static/src/todo/todo.js
@@ -4,4 +4,9 @@ import { Component } from "@odoo/owl";
 
 export class Todo extends Component {
     static template = "owl_playground.Todo";
+    static props = {
+        id: Number,
+        description: String ,
+        done: Boolean
+    };
 }
