3317a7e8b828f95ce98d3105f3f43f7104c1e39e
“OmarAbosamaha” <abom@odoo.com>
2023-12-05 17:30:04 +0200

    Chapter1: Adds counter component

--- owl_playground/static/src/playground.js
+++ owl_playground/static/src/playground.js
@@ -1,7 +1,15 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 
 export class Playground extends Component {
-    static template = "owl_playground.playground";
+  static template = "owl_playground.counter";
+
+  setup() {
+    this.state = useState({ value: 0 });
+  }
+
+  increment() {
+    this.state.value++;
+  }
 }
