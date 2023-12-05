48c61fe3d1d6a40cd9d5953e82acb2bfeeb6a106
jepbarasgarov <asgarovjepbar00@gmail.com>
2023-12-05 10:58:12 +0100

    [IMP] add props validation to the todo component

--- owl_playground/static/src/todo/todo.js
+++ owl_playground/static/src/todo/todo.js
@@ -4,4 +4,9 @@ import { Component, useState } from "@odoo/owl";
 
 export class Todo extends Component {
     static template = "owl_playground.todo";
+    static props = {
+        id: {type: Number},
+        description: {type: String},
+        done: Boolean,
+    }
 }
