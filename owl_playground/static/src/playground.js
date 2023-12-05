5e85a4fc3e1543d8936bf12de3eb0670932069f1
sbel-odoo <sbel@odoo.com>
2023-12-05 15:53:58 +0100

    [IMP] owl_playground: Generic card with slots

--- owl_playground/static/src/playground.js
+++ owl_playground/static/src/playground.js
@@ -3,9 +3,10 @@
 import { Component } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Todolist } from "./todolist/todolist";
+import { Card } from "./card/card";
 
 export class Playground extends Component {
     static template = "owl_playground.playground";
 
-    static components = { Counter, Todolist };
+    static components = { Counter, Todolist, Card };
 }
