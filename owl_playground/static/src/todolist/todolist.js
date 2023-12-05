ebb879947aeaf839642e0f18787c802a4917d652
sbel-odoo <sbel@odoo.com>
2023-12-05 11:42:04 +0100

    [IMP] owl_playground: Focusing the input

--- owl_playground/static/src/todolist/todolist.js
+++ owl_playground/static/src/todolist/todolist.js
@@ -1,15 +1,19 @@
 /** @odoo-module */
 
-import { Component, useState } from "@odoo/owl";
+import { Component, useState, onMounted, useRef } from "@odoo/owl";
 import { Todo } from "../todo/todo";
 
 export class Todolist extends Component {
     static template = "owl_playground.Todolist";
     static components = { Todo };
+    inputRef = useRef("input")
 
     setup(){
         this.currentId = 1;
         this.todolist = useState([]); 
+        onMounted(() => {
+            this.inputRef.el.focus();
+        });
     }
 
     addTodo(ev) {
