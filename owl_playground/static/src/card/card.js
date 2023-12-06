e9c00c43e86e1f74e607f13d0432863c6d7124ba
jepbarasgarov <asgarovjepbar00@gmail.com>
2023-12-06 08:59:08 +0100

    [IMP] add props validation to the card

--- owl_playground/static/src/card/card.js
+++ owl_playground/static/src/card/card.js
@@ -4,4 +4,14 @@ import { Component } from "@odoo/owl";
 
 export class Card extends Component {
     static template = "owl_playground.card";
+
+    static props = {
+        slots: {
+            type: Object,
+            shape: {
+                default: Object,
+                title: { type: Object, optional: true },
+            },
+        },
+    };
 }
