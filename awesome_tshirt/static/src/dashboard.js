e78a835d39d344db584aeaa2a19e55b9b5e96be7
jepbarasgarov <asgarovjepbar00@gmail.com>
2023-12-06 10:07:05 +0100

    [IMP] init the control panel to the awseome-tshirt module

--- awesome_tshirt/static/src/dashboard.js
+++ awesome_tshirt/static/src/dashboard.js
@@ -1,11 +1,31 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, useSubEnv } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
+import { getDefaultConfig } from "@web/views/view";
 
-class AwesomeDashboard extends Component {}
+class AwesomeDashboard extends Component {
+    setup() {
 
-AwesomeDashboard.components = {};
+        // The useSubEnv below can be deleted if you're > 16.0
+        useSubEnv({
+            config: {
+                ...getDefaultConfig(),
+                ...this.env.config,
+            },
+        });
+
+        this.display = {
+            controlPanel: { 
+                "top-right": false, 
+                "bottom-right": false 
+            },
+        };
+    }
+}
+
+AwesomeDashboard.components = { Layout };
 AwesomeDashboard.template = "awesome_tshirt.clientaction";
 
 registry.category("actions").add("awesome_tshirt.dashboard", AwesomeDashboard);
