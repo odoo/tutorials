0e2b1c3122331af18d2229dd0cc3d5e4c30f382d
Beekan-mmha <mmha@odoo.com>
2024-02-19 21:30:30 +0200

    [IMP] Real Estate: Added security permissions for base group user

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,6 +7,9 @@
         'base'
     ],
     'category': 'Tutorials/Real Estate',
+    'data': [
+        'security/ir.model.access.csv',
+    ],
     'installable': True,
     'application': True,
     'auto_install': False
