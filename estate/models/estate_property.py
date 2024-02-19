cac6cbee4d28686f35c66a9985b6ddeefcb85ddb
Beekan-mmha <mmha@odoo.com>
2024-02-19 20:58:08 +0200

    [IMP] Real Estate: Adding new fields to real estate model

---
+++ estate/models/estate_property.py
@@ -0,0 +1,20 @@
+from odoo import fields, models
+
+
+class Property(models.Model):
+    _name = "estate.property"
+    _description = "Real Estate Application"
+
+    name=fields.Char(required=True)
+    description=fields.Text()
+    postcode=fields.Char()
+    date_availability=fields.Date()
+    expected_price=fields.Float(required=True)
+    selling_price=fields.Float()
+    bedrooms=fields.Integer()
+    living_area=fields.Integer()
+    facades=fields.Integer()
+    garage=fields.Boolean()
+    garden=fields.Boolean()
+    garden_area=fields.Integer()
+    garden_orientation=fields.Selection(selection=[("north","North"),("south","South"),("east","East"),("west","West")])
