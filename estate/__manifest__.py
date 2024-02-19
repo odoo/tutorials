350c82a3c74e1e7202b05220c87e5efcc47565fb
Joris Makauskis <jmak@odoo.com>
2024-02-19 16:31:39 +0100

    [ADD] estate: chapter 6
    Created action for estate property model
    Added three level menu for estate property action
    Set estate propety selling price field to read-only
    Set estate property availability and selling price to no copy
    Default values for bedrooms and availabilty fields
    Added state and default fields to property model

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -9,6 +9,8 @@
     ],
     'data': [
         'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ],
     'installable': True,
     'application': True
--- estate/models/estate_properties.py
+++ estate/models/estate_properties.py
@@ -8,10 +8,10 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=fields.Date.add(fields.Date.today(), months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -24,4 +24,17 @@ class EstateProperty(models.Model):
                    ('east', 'East'),
                    ('west', 'West')],
         help="Orientation is used to determine garden orientation"
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        string='state',
+        selection=[('new', 'New'),
+                   ('offer received', 'Offer Received'),
+                   ('offer accepted', 'Offer Accepted'),
+                   ('sold', 'Sold'),
+                   ('canceled', 'Candeled')],
+        required=True,
+        copy=False,
+        default="new",
+        help="State is the current offer status of the property"
     )
