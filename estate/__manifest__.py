616545acf762a4195280913e1bf892c5294c6ed2
Beekan-mmha <mmha@odoo.com>
2024-02-20 00:01:07 +0200

    [IMP] Real Estate: Added three menu levels and an action with modifications to the model fields

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -9,6 +9,9 @@
     'category': 'Tutorials/Real Estate',
     'data': [
         'security/ir.model.access.csv',
+
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
     ],
     'installable': True,
     'application': True,
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,16 +5,28 @@ class Property(models.Model):
     _name = "estate.property"
     _description = "Real Estate Application"
 
-    name=fields.Char(required=True)
-    description=fields.Text()
-    postcode=fields.Char()
-    date_availability=fields.Date()
-    expected_price=fields.Float(required=True)
-    selling_price=fields.Float()
-    bedrooms=fields.Integer()
-    living_area=fields.Integer()
-    facades=fields.Integer()
-    garage=fields.Boolean()
-    garden=fields.Boolean()
-    garden_area=fields.Integer()
-    garden_orientation=fields.Selection(selection=[("north","North"),("south","South"),("east","East"),("west","West")])
+    name=fields.Char(string="Property Name",required=True)
+    description=fields.Text(string="Property Description")
+    postcode=fields.Char(string="Postcode")
+    date_availability=fields.Date(string="Property Availability",copy=False,default=fields.Date.add(fields.Date.today(),days=90))
+    expected_price=fields.Float(string="Expected Price",required=True)
+    selling_price=fields.Float(string="Selling Price",readonly=True,copy=False)
+    bedrooms=fields.Integer(string="Number of Bedrooms",default=2)
+    living_area=fields.Integer(string="Living Area Size")
+    facades=fields.Integer(string="Facades")
+    garage=fields.Boolean(string="Garage")
+    garden=fields.Boolean(string="Garden")
+    garden_area=fields.Integer(string="Garden Area")
+    garden_orientation=fields.Selection(string="Garden Orientation",
+        selection=[("north","North"),
+                   ("south","South"),
+                   ("east","East"),
+                   ("west","West")]
+                   )
+    active=fields.Boolean(string="Active",default=True)
+    state=fields.Selection(copy=False,required=True,default="new",string="State",
+                           selection=[("new","New"),
+                                      ("offer_recieved","Offer Received"),
+                                      ("offer_accepted","Offer Accepted"),
+                                      ("sold","Sold"),
+                                      ("canceled","Canceled")])
