5bf78b613faa1ab6a482662d2e3e75437309f771
berm <berm@odoo.com>
2024-02-19 14:04:17 +0100

    Starting the addons berm-estate

---
+++ estate-berm/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "berm-estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "berm-odoo",
+    'category': 'Sales',
+    'description': "Description text",
+    # data files always loaded at installation
+    'data': [
+        # 'views/mymodule_view.xml',
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+        # 'demo/demo_data.xml',
+    ],
+    'installable': True,
+    'application': True
+}
