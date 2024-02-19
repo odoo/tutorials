c0d9981a2acffcf8031b269c7b0906329a4d3ea3
Joris Makauskis <jmak@odoo.com>
2024-02-19 13:10:56 +0100

    [ADD] Added Real estate property module

---
+++ estate/__manifest__.py
@@ -0,0 +1,21 @@
+{
+<<<<<<< HEAD
+    'name': 'Real Estate',
+    'depends': [
+        'base_setup',
+    ],
+    'application': True,
+}
+=======
+    'name': 'ESTATE',
+    'version': '1.0',
+    'category': 'buildings',
+    'website': 'https://www.odoo.com/page/estate',
+    'depends': [
+        'base',
+        'base_setup',
+    ],
+    'installable': True,
+    'application': True
+}
+>>>>>>> 3f10d08 ([ADD] Added Real estate property module)
---
+++ estate/models/estate_properties.py
@@ -0,0 +1,5 @@
+from odoo import models
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "real estate property"
