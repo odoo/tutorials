2f5d4333197cdaae552e6cc1505985d209beee85
Beekan-mmha <mmha@odoo.com>
2024-02-19 18:43:39 +0200

    [ADD] Real Estate: added real estate module for technical onboarding
    [ADD] Real Estate: added real estate module for technical onboarding

---
+++ estate/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Real Estate',
+    'depends': [
+        'base'
+    ],
+    'category': 'Tutorials/Real Estate',
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
