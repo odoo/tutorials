f0c9df0f46c1bd2558a5f428c646b3ee4423a5f0
berm <berm@odoo.com>
2024-02-19 16:11:16 +0100

    [ADD] Chapter 5 : Set access right.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -8,7 +8,7 @@
     'license': "LGPL-3",
     # data files always loaded at installation
     'data': [
-        # 'views/mymodule_view.xml',
+        'security/ir.model.access.csv',
     ],
     # data files containing optionally loaded demonstration data
     'demo': [
