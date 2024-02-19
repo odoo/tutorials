37a398ddc9ebe950baf72b2713819aea2c1b287e
Hesham <hsal@odoo.com>
2024-02-19 17:28:53 +0200

    [ADD] first commit

---
+++ estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'summary': "Real Estate Advertisement",
+    'description': 'This is the real estate advertisement app which is a part of the odoo technical training',
+    'depends': [
+        'base'
+    ],
+    'application': True
+}
