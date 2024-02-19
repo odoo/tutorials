fb652a843dfe4cb77d8173accde015c84f04850d
“Cameron <cameronnoupoue@gmail.com>
2024-02-19 15:48:19 +0100

    [ADD] estate: chapter 5 (security)

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,7 +1,14 @@
 {
     'name': 'Real Estate',
+    'version': '1.0',
+    'description': """
+    Real Estate Management
+    """,
     'depends': [
         'base_setup',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
     ],
     'application': True,
 }
