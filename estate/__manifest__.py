31c32ca933fdf47feb35165c34c6a26b5e197b89
Beekan-mmha <mmha@odoo.com>
2024-02-20 13:49:00 +0200

    [IMP] Real Estate: Fix comments on pr

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,19 +1,13 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
 {
-    'name': 'Real Estate',
-    'depends': [
-        'base'
+    "name": "Real Estate",
+    "depends": ["base"],
+    "category": "Tutorials/Real Estate",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
     ],
-    'category': 'Tutorials/Real Estate',
-    'data': [
-        'security/ir.model.access.csv',
-
-        'views/estate_property_views.xml',
-        'views/estate_menus.xml'
-    ],
-    'installable': True,
-    'application': True,
-    'auto_install': False
+    "installable": True,
+    "application": True,
+    "auto_install": False,
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,28 +5,42 @@ class Property(models.Model):
     _name = "estate.property"
     _description = "Real Estate Application"
 
-    name=fields.Char(string="Name",required=True)
-    description=fields.Text(string="Description")
-    postcode=fields.Char(string="Postcode")
-    date_availability=fields.Date(string="Availability",copy=False,default=fields.Date.add(fields.Date.today(),days=90))
-    expected_price=fields.Float(string="Expected Price",required=True)
-    selling_price=fields.Float(string="Selling Price",readonly=True,copy=False)
-    bedrooms=fields.Integer(string="Number of Bedrooms",default=2)
-    living_area=fields.Integer(string="Living Area Size")
-    facades=fields.Integer(string="Facades")
-    garage=fields.Boolean(string="Garage")
-    garden=fields.Boolean(string="Garden")
-    garden_area=fields.Integer(string="Garden Area")
-    garden_orientation=fields.Selection(string="Garden Orientation",
-        selection=[("north","North"),
-                   ("south","South"),
-                   ("east","East"),
-                   ("west","West")]
-                   )
-    active=fields.Boolean(string="Active",default=True)
-    state=fields.Selection(copy=False,required=True,default="new",string="State",
-                           selection=[("new","New"),
-                                      ("offer_recieved","Offer Received"),
-                                      ("offer_accepted","Offer Accepted"),
-                                      ("sold","Sold"),
-                                      ("canceled","Canceled")])
+    name = fields.Char(string="Name", required=True)
+    description = fields.Text(string="Description")
+    postcode = fields.Char(string="Postcode")
+    date_availability = fields.Date(
+        string="Availability",
+        copy=False,
+        default=fields.Date.add(fields.Date.today(), months=3),
+    )
+    expected_price = fields.Float(string="Expected Price", required=True)
+    selling_price = fields.Float(string="Selling Price", readonly=True, copy=False)
+    bedrooms = fields.Integer(string="Number of Bedrooms", default=2)
+    living_area = fields.Integer(string="Living Area Size")
+    facades = fields.Integer(string="Facades")
+    garage = fields.Boolean(string="Garage")
+    garden = fields.Boolean(string="Garden")
+    garden_area = fields.Integer(string="Garden Area")
+    garden_orientation = fields.Selection(
+        string="Garden Orientation",
+        selection=[
+            ("north", "North"),
+            ("south", "South"),
+            ("east", "East"),
+            ("west", "West"),
+        ],
+    )
+    active = fields.Boolean(string="Active", default=True)
+    state = fields.Selection(
+        copy=False,
+        required=True,
+        default="new",
+        string="State",
+        selection=[
+            ("new", "New"),
+            ("offer_recieved", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("canceled", "Canceled"),
+        ],
+    )
