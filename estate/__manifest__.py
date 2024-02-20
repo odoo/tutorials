a0f6a3cbccc2b24fbca626a3ec351a7ec6996628
berm <berm@odoo.com>
2024-02-20 09:33:10 +0100

    [ADD] Chapter 6.2 : Create basic menus.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,10 +6,10 @@
     'category': 'Tutorials',
     'description': "Estate management",
     'license': "LGPL-3",
-    # data files always loaded at installation
     'data': [
         'security/ir.model.access.csv',
-        'views/estate_views.xml'
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
     ],
     # data files containing optionally loaded demonstration data
     'demo': [
