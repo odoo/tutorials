87d17a642066cd2de4bd7504c2ae264bdb2f390d
berm <berm@odoo.com>
2024-02-20 10:37:46 +0100

    [ADD] Chapter 6.3 : Set default model values and use reserved fields

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -7,21 +7,34 @@ class Estate(models.Model):
 
     name = fields.Char(required=True)
     description = fields.Text()
+    active = fields.Boolean(default=True)
     postcode = fields.Char()
-    date_availability = fields.Date()
-    expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    date_availability = fields.Date(
+            copy=False,
+            default=fields.Date.add(fields.Date.today(), fields.Date.today(), months=3))
+    expected_price = fields.Float(required=True, readonly=True)
+    selling_price = fields.Float(copy=False, readonly=True)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
-            string='Type',
+            string='Orientation',
             selection=[
                 ('North', 'north'),
                 ('South', 'south'),
                 ('East', 'east'),
                 ('West', 'east')],
             help="Cardinal orientation of the garden.")
+    state = fields.Selection(
+            string='State',
+            required=True,
+            copy=False,
+            default='New',
+            selection=[('New', 'new'),
+                       ('Offer Received', 'offer received'),
+                       ('Offer Accepted', 'Offer Accepted'),
+                       ('Sold', 'sold'),
+                       ('Canceled', 'Canceled')])
