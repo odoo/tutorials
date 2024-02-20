eab9743c6ab44a64248964538ecdfd8e61f35f99
Tanguy Quéguineur <tanguyqueguineur@gmail.com>
2024-02-20 10:37:15 +0100

    [ADD] estate: chapter 7

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,15 +5,15 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Estate Property"
 
-    name = fields.Char(string='Property Name', required=True)
+    name = fields.Char(string='Title', required=True)
     description = fields.Text(string='Property Description')
-    postcode = fields.Char(string='Property Postcode')
-    date_availability = fields.Date(string='Availability', copy=False, default=fields.Date.add(fields.Date.today(),
-                                                                                               months=3))
+    postcode = fields.Char(string='Postcode')
+    date_availability = fields.Date(string='Available From', copy=False,
+                                    default=fields.Date.add(fields.Date.today(), months=3))
     expected_price = fields.Float(string='Expected Price', required=True)
     selling_price = fields.Float(string='Selling Price', readonly=True, copy=False)
     bedrooms = fields.Integer(string='Bedrooms', default=2)
-    living_area = fields.Integer(string='Living Area')
+    living_area = fields.Integer(string='Living Area (sqm)')
     facades = fields.Integer(string='Facades')
     garage = fields.Boolean(string='Garage')
     garden = fields.Boolean(string='Garden')
