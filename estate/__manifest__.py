760802b4f6c883e86e85e21e0b64bf2b08e8e073
Hamza (habo) <habo@odoo.com>
2024-02-20 15:25:50 +0100

    [IMP] Configured views and fields for estate module, fixed formatting

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,6 +5,10 @@
         'base'
     ],
     'data': [
+
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+
         'security/ir.model.access.csv',
     ],
     'application': True
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,28 +1,44 @@
-
 from odoo import models, fields
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real Estate module"
 
+
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False,default=fields.Date.add(fields.Date.today(),months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True,copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
-        string = 'Type',
-        selection=[
+        string = 'Orientation',
+        selection = [
             ('north', 'North'), 
             ('south', 'South'),
             ('east', 'East'),
             ('west', 'West')
             ],
+    )
+
+    active = fields.Boolean(default=True)
+
+    state = fields.Selection(
+        string = 'State',
+        selection = [
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'), 
+            ('offer_accepted', 'Offer Accepted'), 
+            ('sold', 'Sold'), 
+            ('canceled', 'Canceled'),
+            ],
+        required = True,
+        copy = False,
+        default = 'new'
     )
