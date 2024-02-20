cb0cd07a0356cf1a597e6f0da2ecee9bac53a53c
Mohammad Hossam <60348524+mohammadhossam@users.noreply.github.com>
2024-02-20 17:29:03 +0200

    [FIX] estate: rename estate_property model to estate.property

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,9 +1,8 @@
-from email.policy import default
 from odoo import models, fields
 
 
 class EstateProperty(models.Model):
-    _name = "estate_property"
+    _name = "estate.property"
     _description = "Estate Property"
 
     name = fields.Char(required=True, string="Title")
