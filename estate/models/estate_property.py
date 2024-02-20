0d116d74ac03c816ffe8dba85c3c0ed6a0eb01d3
Tanguy Quéguineur <tanguyqueguineur@gmail.com>
2024-02-20 13:37:37 +0100

    [FIX] estate: fixed wrong property name

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -40,4 +40,4 @@ class EstateProperty(models.Model):
     buyer = fields.Many2one('res.partner', string='Buyer', copy=False)
     salesperson = fields.Many2one('res.users', string='Salesperson', default=lambda self: self.env.user)
     tag_ids = fields.Many2many("estate.property.tag")
-    offer_ids = fields.One2many("estate.property.offer", "id", string="Offers")
+    offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
