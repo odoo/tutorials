2a329e63b15d40c83e190559d3c9d9db92564c50
Mohammad Hossam <60348524+mohammadhossam@users.noreply.github.com>
2024-02-20 16:22:24 +0200

    [IMP] estate: add new custom list view, form and search menu (chapter 7)

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -6,15 +6,15 @@ class EstateProperty(models.Model):
     _name = "estate_property"
     _description = "Estate Property"
 
-    name = fields.Char(required=True)
+    name = fields.Char(required=True, string="Title")
     description = fields.Text()
     postcode = fields.Char()
     date_availability = fields.Date(
-        copy=False, default=fields.Date.add(fields.Date.today(), months=3))
+        copy=False, default=fields.Date.add(fields.Date.today(), months=3), string="Available From")
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
+    living_area = fields.Integer(string="Living Area (sqm)")
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
