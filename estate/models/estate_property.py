a364b7755d9f18c1ff3aa18effc3788d067294a1
“Cameron <cameronnoupoue@gmail.com>
2024-02-20 10:35:14 +0100

    [ADD] estate: chapter 7 (basic views)

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -4,21 +4,21 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Estate Property Management"
 
-    name = fields.Char(required=True)
+    name = fields.Char(required=True, string="Title")
     description = fields.Text()
     active = fields.Boolean(default=True)
     postcode = fields.Char()
-    date_availability = fields.Date(copy=False, default=fields.Date.add(fields.Date.today(), months=3))
+    date_availability = fields.Date(copy=False, default=fields.Date.add(fields.Date.today(), months=3), string="Available from")
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
+    living_area = fields.Integer(string="Living Area (sqm)")
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Integer()
+    garden_area = fields.Integer(string="Garden Area (sqm)")
     garden_orientation = fields.Selection(
-        string="Orientation",
+        string="Garden orientation",
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
     )
     state = fields.Selection(
