166efa12b373821abd0383f1ecad9b670b223464
igbe-odoo <igbe@odoo.com>
2024-02-20 10:43:05 +0100

    [ADD] Chapter 6

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,8 +4,11 @@
         'base'
     ],
     'data': [
-        'data/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
     ],
     'installable': True,
-    'application': True
+    'application': True,
+    'license': 'LGPL-3',
 }
--- estate/models/estate_model.py
+++ estate/models/estate_model.py
@@ -4,13 +4,13 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Brand new Model"
 
-    name = fields.Char(required=True)
+    name = fields.Char(required=True, default="Unknown")
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=fields.Date.add(fields.Date.today(), months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -22,4 +22,12 @@ class EstateProperty(models.Model):
         ('E', 'East'),
         ('W', 'West')
     ])
+    active = fields.Boolean(default=False)
+    state = fields.Selection(selection=[
+        ('N', 'New'),
+        ('OR', 'Offer Received'),
+        ('OA', 'Offer Accepted'),
+        ('S', 'Sold'),
+        ('C', 'Canceled')
+    ], required=True, copy=False, default='N')
     
