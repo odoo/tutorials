a1d6c0ad8540a7452c77eb52f38c9f67b7228b5f
Hesham <hsal@odoo.com>
2024-02-20 14:07:19 +0200

    [IMP] estate: chapter 6 views and menus

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,7 +7,9 @@
         'base'
     ],
     'data': [
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ],
     'application': True
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import models, fields
+from datetime import datetime, timedelta
 
 
 class Property(models.Model):
@@ -11,10 +12,12 @@ class Property(models.Model):
                        index=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(
+        default=datetime.today() + timedelta(days=3*30),
+        copy=False)
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -25,3 +28,15 @@ class Property(models.Model):
         ('south', 'South'),
         ('east', 'East'),
         ('west', 'West')])
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        required=True,
+        copy=False,
+        default='new',
+        selection=[
+            ('new', 'New'),
+            ('received', 'Offer Received'),
+            ('accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('canceled', 'Canceled'),
+        ])
