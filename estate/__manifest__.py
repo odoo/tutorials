54c9cfa4e150e949a8961fde07931e1ec061dec5
Tanguy Quéguineur <tanguyqueguineur@gmail.com>
2024-02-20 10:52:16 +0100

    [FIX] estate: review feedback

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,7 +5,6 @@
     ],
     'data': [
         'security/ir.model.access.csv',
-
         'views/estate_property_views.xml',
         'views/estate_menus.xml',
     ],
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -8,8 +8,7 @@ class EstateProperty(models.Model):
     name = fields.Char(string='Title', required=True)
     description = fields.Text(string='Property Description')
     postcode = fields.Char(string='Postcode')
-    date_availability = fields.Date(string='Available From', copy=False,
-                                    default=fields.Date.add(fields.Date.today(), months=3))
+    date_availability = fields.Date(string='Available From', copy=False, default=fields.Date.add(fields.Date.today(), months=3))
     expected_price = fields.Float(string='Expected Price', required=True)
     selling_price = fields.Float(string='Selling Price', readonly=True, copy=False)
     bedrooms = fields.Integer(string='Bedrooms', default=2)
@@ -18,14 +17,22 @@ class EstateProperty(models.Model):
     garage = fields.Boolean(string='Garage')
     garden = fields.Boolean(string='Garden')
     garden_area = fields.Integer(string='Garden Area')
-    garden_orientation = fields.Selection(string='Garden Orientation', selection=[('north', 'North'),
-                                                                                  ('south', 'South'),
-                                                                                  ('east', 'East'),
-                                                                                  ('west', 'West')])
+    garden_orientation = fields.Selection(
+        string='Garden Orientation',
+        selection=[
+            ('north', 'North'),
+            ('south', 'South'),
+            ('east', 'East'),
+            ('west', 'West')
+        ]
+    )
     active = fields.Boolean(string='Active', default=True)
-    state = fields.Selection(string='State', selection=[('new', 'New'),
-                                                        ('offer_received', 'Offer Received'),
-                                                        ('offer_accepted', 'Offer Accepted'),
-                                                        ('sold', 'Sold'),
-                                                        ('cancelled', 'Canceled')],
-                             required=True, default='new', copy=False)
+    state = fields.Selection(string='State', required=True, default='new', copy=False,
+                             selection=[
+                                 ('new', 'New'),
+                                 ('offer_received', 'Offer Received'),
+                                 ('offer_accepted', 'Offer Accepted'),
+                                 ('sold', 'Sold'),
+                                 ('cancelled', 'Canceled')
+                             ]
+                             )
