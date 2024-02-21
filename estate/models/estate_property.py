0ac8df063a4908bb164ddd78f728b3b7681859a9
Hesham <hsal@odoo.com>
2024-02-21 22:05:14 +0200

    [IMP] estate: Added offers to properties

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -50,3 +50,5 @@ class Property(models.Model):
     buyer_id = fields.Many2one("res.partner", copy=False)
 
     property_tag_ids = fields.Many2many("estate.property.tag")
+
+    offer_ids = fields.One2many("estate.property.offer", "property_id")
---
+++ estate/models/estate_property_offer.py
@@ -0,0 +1,16 @@
+from odoo import models, fields
+
+
+class PropertyOffer(models.Model):
+    _name = "estate.property.offer"
+    _description = "Estate property offer"
+
+    price = fields.Float()
+    status = fields.Selection(
+        selection=[
+            ("accepted", "Accepted"),
+            ("refused", "Refused")],
+        copy=False)
+    
+    partner_id = fields.Many2one("res.partner", required=True)
+    property_id = fields.Many2one("estate.property", required=True)
