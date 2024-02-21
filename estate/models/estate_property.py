a86b1a23f07bbf88e9e03b4e4b57020ed5965125
Hamza (habo) <habo@odoo.com>
2024-02-21 12:01:07 +0100

    [IMP] estate: Added salesman and buyer to estate property model

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -43,3 +43,6 @@ class EstateProperty(models.Model):
         copy = False,
         default = 'new'
     )
+
+    salesman_id = fields.Many2one("res.users", string="Salesman", default=lambda self: self.env.user)
+    buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
