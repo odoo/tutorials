64ed34974b099cf3aada513a4826436fda94174a
Hesham <hsal@odoo.com>
2024-02-22 15:59:10 +0200

    [IMP] estate: tested manual and view ordering

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -16,4 +16,5 @@ class PropertyType(models.Model):
 
     property_ids = fields.One2many("estate.property", "property_type_id")
 
-    _order = "name"
+    sequence = fields.Integer()
+    _order = "sequence, name"
