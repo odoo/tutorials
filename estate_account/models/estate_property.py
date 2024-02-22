2f326f01262d83dbd0cef136c1f0edf8304f21b5
Mohammad Hossam <60348524+mohammadhossam@users.noreply.github.com>
2024-02-22 14:53:51 +0200

    [IMP] estate_account: create an empty invoice on marking a property as sold (chapter 14)

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -5,4 +5,10 @@ class EstateProperty(models.Model):
     _inherit = "estate.property"
 
     def action_set_to_sold(self):
+        for property in self:
+            self.env['account.move'].create(
+                {
+                    'partner_id': property.buyer_id.id,
+                    'move_type': 'out_invoice'
+                })
         return super().action_set_to_sold()
