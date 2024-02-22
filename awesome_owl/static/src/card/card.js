db7b47de8bd6a770beadca091e216832bdefb618
Joris Makauskis <jmak@odoo.com>
2024-02-22 13:18:26 +0100

    [IMP] owl 1.13: Changed card component content prop to a slot

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -3,10 +3,10 @@
 import { Component } from "@odoo/owl";
 
 export class Card extends Component {
-    static template = "awesome_owl.card";
+  static template = "awesome_owl.card";
 
-    static props = {
-        title: String,
-        content: String
-    }
+  static props = {
+    title: String,
+    slots: Object
+  };
 }
