9d3b4ecbacad5068a0d7f6b56a7626b0e6a922a5
Joris Makauskis <jmak@odoo.com>
2024-02-22 13:32:00 +0100

    [IMP] owl 2.1: Added layout component skeleton to dashboard

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -2,9 +2,19 @@
 
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
 
 class AwesomeDashboard extends Component {
-    static template = "awesome_dashboard.AwesomeDashboard";
+  static template = "awesome_dashboard.AwesomeDashboard";
+  static components = { Layout };
+
+  setup() {
+    this.display = {
+      controlPanel: {},
+    };
+  }
 }
 
-registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
+registry
+  .category("actions")
+  .add("awesome_dashboard.dashboard", AwesomeDashboard);
