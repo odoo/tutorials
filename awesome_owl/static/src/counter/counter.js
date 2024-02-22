ca9bec6e3384aa89743f819078af84ca3d0559af
Joris Makauskis <jmak@odoo.com>
2024-02-22 11:02:06 +0100

    [ADD] owl 1.6: Add total sum counter
    Created a onChange function to the counter, so that a total sum can be
    tracked

--- awesome_owl/static/src/counter/counter.js
+++ awesome_owl/static/src/counter/counter.js
@@ -4,12 +4,16 @@ import { Component, useState } from "@odoo/owl";
 
 export class Counter extends Component {
     static template = "awesome_owl.counter";
+    static props = {
+        onChange: { type: Function, optional: true }
+    }
 
     setup() {
-        this.state = useState({ value: 0 })
+        this.state = useState({ value: 1 })
     }
 
     increment() {
         this.state.value++;
+        this.props.onChange?.()
     }
 }
--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import { Component, markup } from "@odoo/owl";
+import { Component, markup, useState } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
 
@@ -10,7 +10,14 @@ export class Playground extends Component {
   static components = { Counter, Card };
 
   setup() {
+    this.sum = useState({ value: 2 });
+    this.incrementSum = this.incrementSum.bind(this);
+
     this.content1 = "<p>This is <b>bold</b></p>";
     this.content2 = markup("<p>This is <b>bold</b></p>");
+  }
+
+  incrementSum() {
+    this.sum.value++;
   }
 }
