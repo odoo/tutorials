78cf9d1880aec0089aa617db4a06c76b64b213af
Joris Makauskis <jmak@odoo.com>
2024-02-22 13:52:36 +0100

    [ADD] owl 2.2: Layout buttons to customers and leads views

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -3,6 +3,7 @@
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
 
 class AwesomeDashboard extends Component {
   static template = "awesome_dashboard.AwesomeDashboard";
@@ -12,7 +13,23 @@ class AwesomeDashboard extends Component {
     this.display = {
       controlPanel: {},
     };
+    this.action = useService("action");
   }
+  openCustomers() {
+    this.action.doAction("base.action_partner_form");
+  }
+
+  openLeads() {
+    this.action.doAction({
+        type: "ir.actions.act_window",
+        res_model: "crm.lead",
+        name: "Leads",
+        views: [
+            [false, "list"],
+            [false, "form"],
+        ],
+    });
+}
 }
 
 registry
