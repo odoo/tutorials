22a5aaeb08203abce92032ebcfa27a567cb59bfe
Joris Makauskis <jmak@odoo.com>
2024-02-22 10:36:14 +0100

    [ADD] owl 1.3: Card component
    Created card component, which displays title and contnet, which are
    passed as props.

---
+++ awesome_owl/static/src/card/card.js
@@ -0,0 +1,8 @@
+/** @odoo-module **/
+
+import { Component } from "@odoo/owl";
+
+export class Card extends Component {
+    static template = "awesome_owl.card";
+
+}
--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -2,9 +2,10 @@
 
 import { Component } from "@odoo/owl";
 import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
 
 export class Playground extends Component {
   static template = "awesome_owl.playground";
 
-  static components = { Counter };
+  static components = { Counter, Card };
 }
