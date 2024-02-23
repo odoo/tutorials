806485ac015f29d47f99cfa5e9a2de63d396ffd8
Mohammad Hossam <60348524+mohammadhossam@users.noreply.github.com>
2024-02-23 17:41:25 +0200

    [IMP] awesome_owl: visualize uncomplete todo items using dynamic attributes (chapter 1.8)

--- awesome_owl/static/src/todo/todo_list.js
+++ awesome_owl/static/src/todo/todo_list.js
@@ -8,7 +8,8 @@ export class TodoList extends Component {
 
   setup() {
     this.todos = useState([
-      { id: 3, description: "buy milk", isCompleted: false },
+      { id: 2, description: "write tutorial", isCompleted: false },
+      { id: 3, description: "buy milk", isCompleted: true },
     ]);
   }
 }
