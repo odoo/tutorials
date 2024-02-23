aecaf8844cf26132c1138773be2ddef122a1bf2c
Mohammad Hossam <60348524+mohammadhossam@users.noreply.github.com>
2024-02-23 16:26:54 +0200

    [IMP] awesome_owl: display the sum of the two counters using callback props (chapter 1.6)

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import { Component, markup } from "@odoo/owl";
+import { Component, markup, useState } from "@odoo/owl";
 import { Counter } from "./counter";
 import { Card } from "./card";
 
@@ -8,6 +8,14 @@ export class Playground extends Component {
   static template = "awesome_owl.playground";
 
   static components = { Counter, Card };
+
+  setup() {
+    this.state = useState({ sum: 2 });
+  }
+
+  incrementSum() {
+    this.state.sum++;
+  }
 
   value1 = "<div class='text-primary'>some content</div>";
   value2 = markup("<div class='text-primary'>some content</div>");
