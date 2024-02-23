a728ce7264792905e2c104387ff9a864807d5a42
Tanguy Quéguineur <tanguyqueguineur@gmail.com>
2024-02-23 11:02:29 +0100

    [ADD] Chapter 2 (awesome_dashboard): exercise 1

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -2,9 +2,17 @@
 
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout"
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
+    static components = { Layout }
+
+    setup(){
+        this.display = {
+            controlPanel : {}
+        };
+    }
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
