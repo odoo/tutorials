1342666494d4c9a6e8afaac34a586741e9af71ac
“Cameron <cameronnoupoue@gmail.com>
2024-02-23 10:51:35 +0100

    [ADD] awesome_owl: javascript framework (chap1 1.7)

--- awesome_owl/static/src/counter/counter.js
+++ awesome_owl/static/src/counter/counter.js
@@ -4,10 +4,16 @@ import { Component, useState } from "@odoo/owl";
 
 export class Counter extends Component {
     static template = "awesome_owl.counter";
+    static props = {
+        onChange: { type: Function, optional: true },
+    }
 
     state = useState({ count: 1 });
 
     increment() {
         this.state.count++;
+        if (this.props.onChange) {
+            this.props.onChange();
+        }
     }
 }
--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import { Component, markup } from "@odoo/owl";
+import { Component, markup, useState } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
 
@@ -11,6 +11,11 @@ export class Playground extends Component {
     setup() {
         this.str1 = "<h1>Hello, World!</h1>";
         this.str2 = markup("<h1>Hello, World!</h1>");
+        this.sum = useState( {count: 2});
+    }
+
+    incrementSum() {
+        this.sum.count++;
     }
 }
 
