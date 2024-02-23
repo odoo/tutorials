8100064a1f77ea2840a2478c61a41c12f491f015
Hesham <hsal@odoo.com>
2024-02-23 16:03:55 +0200

    [ADD] estate_account: Added new module

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Estate Accounting',
+    'version': '1.0',
+    'summary': "Real Estate Accounting",
+    'description': 'This is the real estate accounting app which is a part of the odoo technical training',
+    'depends': [
+        'estate',
+        'account'
+    ],
+    'data': []
+}
