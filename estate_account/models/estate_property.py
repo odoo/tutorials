a0d0f00b40ddd9d1d2fdba1d6395ab800085cc53
Hesham <hsal@odoo.com>
2024-02-23 18:37:54 +0200

    [IMP] estate_account: Added invoice and invoice line creation

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,8 +1,27 @@
-from odoo import models
+from odoo import Command, models
 
 
 class Property(models.Model):
     _inherit = "estate.property"
 
     def sell(self):
+        for record in self:
+            values = {
+                'partner_id': record.buyer_id.id,
+                'move_type': 'out_invoice',
+                'invoice_line_ids': [
+                    Command.create({
+                        'name': '6% of the selling price',
+                        'quantity': 1,
+                        'price_unit': 0.06 * record.selling_price,
+                    }),
+                    Command.create({
+                        'name': 'Administrative fees',
+                        'quantity': 1,
+                        'price_unit': 100,
+                    }),
+                ],
+            }
+            self.env['account.move'].create(values)
+
         return super().sell()
