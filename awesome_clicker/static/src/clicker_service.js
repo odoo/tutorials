04d2cf385b5badb0304c30b3a79cf3a0b1d3fc75
Joris Makauskis <jmak@odoo.com>
2024-02-26 16:20:38 +0100

    [IMP] owl 3.8: clickbots
    -Added ability to buy clickbots
    -When clickbot has been purchased, the user automaticaly earns clicks

--- awesome_clicker/static/src/clicker_service.js
+++ awesome_clicker/static/src/clicker_service.js
@@ -6,14 +6,21 @@ import { reactive } from "@odoo/owl";
 const clickerService = {
   dependencies: [],
   start() {
-    const state = reactive({ count: 0 });
+    const state = reactive({ count: 1000, level: 1, bots: 0 });
 
     document.addEventListener("click", () => state.count++, true);
+
+    setInterval(() => (state.count += 10 * state.bots), 10 * 1000);
 
     return {
       state,
       increment(inc) {
         state.count += inc;
+      },
+      buyBot() {
+        if (state.count < 1000) return;
+        state.bots++;
+        state.count -= 1000;
       },
     };
   },
