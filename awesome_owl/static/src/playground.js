b8a692fb9d4f519bbb02cb876cf75fbf3f3f910b
Hesham <hsal@odoo.com>
2024-02-26 08:09:44 +0200

    [IMP] awesome_owl: Used markup and explored t-out vs t-esc

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,10 +1,14 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, markup } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
     static components = { Counter, Card };
+
+    value1 = "<div class='text-primary'>some content</div>";
+    value2 = markup("<div class='text-primary'>some content</div>");
+
 }
