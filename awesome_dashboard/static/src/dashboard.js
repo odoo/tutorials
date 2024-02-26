8a7e4b8e4a7429cb3279bfe2ccd2ba01537b7ee8
Mohammad Hossam <60348524+mohammadhossam@users.noreply.github.com>
2024-02-26 14:06:03 +0200

    [IMP] awesome_dashboard: add customers and leads navigation buttons (chapter 2.2)

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -3,10 +3,31 @@
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
 
 class AwesomeDashboard extends Component {
   static template = "awesome_dashboard.AwesomeDashboard";
   static components = { Layout };
+
+  setup() {
+    this.action = useService("action");
+  }
+
+  openCustomers() {
+    this.action.doAction("base.action_partner_form");
+  }
+
+  openLeads() {
+    this.action.doAction({
+      type: "ir.actions.act_window",
+      name: "Leads",
+      res_model: "crm.lead",
+      views: [
+        [false, "tree"],
+        [false, "form"],
+      ],
+    });
+  }
 }
 
 registry
