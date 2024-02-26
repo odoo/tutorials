5ca5156bcc91a5930b7f9d8e2534f103f52b2194
Hesham <hsal@odoo.com>
2024-02-26 09:13:00 +0200

    [IMP] awesome_owl: added todos

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -3,10 +3,11 @@
 import { Component, markup, useState } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
+import { TodoList } from "./todo/todo_list";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
-    static components = { Counter, Card };
+    static components = { Counter, Card, TodoList };
 
     value1 = "<div class='text-primary'>some content</div>";
     value2 = markup("<div class='text-primary'>some content</div>");
