63c08e812a3d4330d223252d143e15531e408564
Joris Makauskis <jmak@odoo.com>
2024-02-26 13:45:44 +0100

    [IMP] owl 2.12: make dashboard responsive

--- awesome_dashboard/static/src/dashboard/dashboard_item/dashboard_item.js
+++ awesome_dashboard/static/src/dashboard/dashboard_item/dashboard_item.js
@@ -1,20 +1,58 @@
 /** @odoo-module */
 
-import { Component } from "@odoo/owl";
+import { Component, reactive, useState } from "@odoo/owl";
 
 export class DashboardItem extends Component {
-    static template = "awesome_dashboard.dashboardItem"
-    static props = {
-        slots: {
-            type: Object,
-            shape: {
-                default: Object
-            },
-        },
-        size: {
-            type: Number,
-            default: 1,
-            optional: true,
-        },
+  static template = "awesome_dashboard.dashboardItem";
+  static props = {
+    slots: {
+      type: Object,
+      shape: {
+        default: Object,
+      },
+    },
+    size: {
+      type: Number,
+      default: 1,
+      optional: true,
+    },
+  };
+  setup() {
+    this.ui = useState(this.createUI());
+  }
+
+  debounce(func, wait, immediate) {
+    let timeout;
+    return function () {
+      const context = this;
+      const args = arguments;
+      function later() {
+        timeout = null;
+        if (!immediate) {
+          func.apply(context, args);
+        }
+      }
+      const callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) {
+        func.apply(context, args);
+      }
     };
+  }
+
+  createUI() {
+    const getIsMobile = () => window.innerWidth <= 768;
+
+    const ui = reactive({ isMobile: getIsMobile() });
+
+    const updateEnv = this.debounce(() => {
+      const isMobile = getIsMobile();
+      if (ui.isMobile !== isMobile) {
+        ui.isMobile = isMobile;
+      }
+    });
+    window.addEventListener("resize", updateEnv);
+    return ui;
+  }
 }
