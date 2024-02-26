b559d560124409763028e59453b2fc9c8df5e901
Joris Makauskis <jmak@odoo.com>
2024-02-26 14:31:10 +0100

    [IMP] owl 3.2: capture external clicks
    -Added useExternalListener to capture all clicks. Clicks everywehre are worth 1, but the button is worth 10

--- awesome_clicker/static/src/clicker_systray_item/clicker_systray_item.js
+++ awesome_clicker/static/src/clicker_systray_item/clicker_systray_item.js
@@ -1,16 +1,22 @@
 /** @odoo-module **/
 
 import { registry } from "@web/core/registry";
-import { Component, useState } from "@odoo/owl";
+import { Component, useState, useExternalListener } from "@odoo/owl";
 
 export class ClickerSystray extends Component {
   static template = "awesome_clicker.ClickerSystray";
 
   setup() {
     this.state = useState({ count: 0 });
+    useExternalListener(
+      window.document.body,
+      "click",
+      () => this.state.count++,
+      { capture: true }
+    );
   }
 
-  increment = () => this.state.count++;
+  increment = () => (this.state.count += 9);
 }
 
 export const systrayItem = {
