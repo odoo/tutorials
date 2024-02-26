bd623e010f266581f0ca787faa578219a774534e
Mohammad Hossam <60348524+mohammadhossam@users.noreply.github.com>
2024-02-26 14:36:51 +0200

    [IMP] awesome_dashboard: add dashboard item component (chapter 2.3)

---
+++ awesome_dashboard/static/src/dashboard_item/dashboard_item.js
@@ -0,0 +1,13 @@
+/** @odoo-module **/
+
+import { Component } from "@odoo/owl";
+
+export class DashboardItem extends Component {
+  static template = "awesome_dashboard.DashboardItem";
+  static defaultProps = {
+    size: 1,
+  };
+  static props = {
+    size: { type: Number, optional: true },
+  };
+}
