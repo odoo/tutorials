95353725ec42162a0b7019aad9c850b471f15087
Beekan-mmha <mmha@odoo.com>
2024-02-26 16:29:00 +0200

    [IMP] Awesome Owl: add props validation

--- awesome_owl/static/src/card.js
+++ awesome_owl/static/src/card.js
@@ -5,5 +5,7 @@ import { Component , useState } from "@odoo/owl";
 export class Card extends Component {
     static template = "awesome_owl.card";
     
-    
+    static props = {
+        title:String, 
+        content:String};
 }
