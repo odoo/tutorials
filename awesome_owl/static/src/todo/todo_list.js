1af69d93aba088d137b0ec86d2c2e87b4a514151
Hesham <hsal@odoo.com>
2024-02-26 09:37:00 +0200

    [IMP] awesome_owl: dynamic todos

--- awesome_owl/static/src/todo/todo_list.js
+++ awesome_owl/static/src/todo/todo_list.js
@@ -1,22 +1,22 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 import { TodoItem } from "./todo_item";
 
 export class TodoList extends Component {
     static template = "awesome_owl.todo_list";
     static components = { TodoItem };
 
-    todos = [
-        {
-            id: 1,
-            description: "Write tutorial",
-            completed: true,
-        },
-        {
-            id: 2,
-            description: "Buy milk",
-            completed: false,
+    index = 0;
+
+    setup() {
+        this.todos = useState([]);
+    }
+
+    addTodo(ev) {
+        if (ev.keyCode === 13 && ev.target.value != '') {
+            this.todos.push({ id: ++this.index, description: ev.target.value, completed: false, });
+            ev.target.value = '';
         }
-    ];
+    }
 }
