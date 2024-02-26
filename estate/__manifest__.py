729a9e7f38580e0bd75c0613000ea33ad62c0b08
Hamza (habo) <habo@odoo.com>
2024-02-26 18:22:08 +0100

    [IMP] estate: Added properties list view to user form view

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -9,6 +9,7 @@
         'views/estate_property_type_views.xml',
         'views/estate_property_tag_views.xml',
         'views/estate_property_offer_views.xml',
+        'views/users_extended_views.xml',
         'views/estate_menus.xml',
         'security/ir.model.access.csv',
     ],
