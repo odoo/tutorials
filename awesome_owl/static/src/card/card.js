4cf2a86eb6078b8c09368747e396fd33438afcff
Mohammad Hossam <60348524+mohammadhossam@users.noreply.github.com>
2024-02-26 11:05:46 +0200

    [IMP] awesome_owl: add toggle button to show/hide card content (chapter 1.14)

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -1,12 +1,20 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 
 export class Card extends Component {
   static template = "awesome_owl.card";
+
+  setup() {
+    this.state = useState({ open: true });
+  }
 
   static props = {
     title: String,
     slots: { type: Object, optional: true },
   };
+
+  toggleOpen() {
+    this.state.open = !this.state.open;
+  }
 }
