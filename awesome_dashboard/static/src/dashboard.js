17de115a525aadf7501e92766fd07f8a3feaa63b
“Cameron <cameronnoupoue@gmail.com>
2024-02-26 14:41:48 +0100

    [ADD] awesome_dashboard: chap2 : 2.1 - 2.3

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -2,9 +2,28 @@
 
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
+import { DashboardItem } from "./item/item";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
+    static components = { Layout, DashboardItem };
+
+    setup() {
+        this.display = {
+            controlPanel: {},
+        };
+        this.action = useService("action");
+    }
+
+    openCustomerKanbanView() {
+        this.action.doAction("base.action_partner_form");
+    }
+
+    openLeads() {
+        this.action.doAction("crm.crm_lead_all_leads");
+    }
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
