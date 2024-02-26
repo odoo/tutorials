284cb97fc56cf3f45770c218589ca7a14e319356
Beekan-mmha <mmha@odoo.com>
2024-02-26 23:11:13 +0200

    [IMP] Awesome owl: Add todo list completion status

--- awesome_owl/static/src/todo/todo_list.js
+++ awesome_owl/static/src/todo/todo_list.js
@@ -1,16 +1,17 @@
-/** @odoo-module */
+/** @odoo-module **/
 
-import { Component, useState } from "@odoo/owl";
+import { Component , useState } from "@odoo/owl";
 import { TodoItem } from "./todo_item";
 
 export class TodoList extends Component {
     static template = "awesome_owl.todo_list";
     static components = { TodoItem };
-
+    
     setup() {
         this.todos = useState([
-            { id: 2, description: "write tutorial", isCompleted: true },
-            { id: 3, description: "buy milk", isCompleted: false },
+          { id: 3, description: "buy milk", isCompleted: false },
+          { id: 4, description: "testing strike", isCompleted: true }
         ]);
     }
+
 }
