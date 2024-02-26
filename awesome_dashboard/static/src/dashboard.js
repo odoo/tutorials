fc3b4e54a03a798fb1924460ec3c6d3dcc6785a3
Mohammad Hossam <60348524+mohammadhossam@users.noreply.github.com>
2024-02-26 12:14:25 +0200

    [IMP] awesome_dashboard: add a new layout to the app page (chapter 2.1)

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -2,9 +2,13 @@
 
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
 
 class AwesomeDashboard extends Component {
-    static template = "awesome_dashboard.AwesomeDashboard";
+  static template = "awesome_dashboard.AwesomeDashboard";
+  static components = { Layout };
 }
 
-registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
+registry
+  .category("actions")
+  .add("awesome_dashboard.dashboard", AwesomeDashboard);
