7f4c84a71f3cbbc953d505549a24b2e8927549e0
Joris Makauskis <jmak@odoo.com>
2024-02-26 09:46:01 +0100

    [IMP] owl 2.12: Added example of how to implement translation

--- awesome_dashboard/static/src/dashboard/dashboard_items.js
+++ awesome_dashboard/static/src/dashboard/dashboard_items.js
@@ -3,61 +3,62 @@
 import { PieChartCard } from "./dashboard_item/pie_chart/pie_chart_card";
 import { NumberCard } from "./number_card";
 import { registry } from "@web/core/registry";
+import { _t } from "@web/core/l10n/translation";
 
 export const items = [
   {
     id: "average_quantity",
-    description: "Average amount of t-shirt",
+    description: _t("Average amount of t-shirt"),
     Component: NumberCard,
     props: (data) => ({
-      title: "Average amount of t-shirt by order this month",
+      title: _t("Average amount of t-shirt by order this month"),
       value: data.average_quantity,
     }),
   },
   {
     id: "average_time",
-    description: "Average time for an order",
+    description: _t("Average time for an order"),
     Component: NumberCard,
     props: (data) => ({
       title:
-        "Average time for an order to go from 'new' to 'sent' or 'cancelled'",
+        _t("Average time for an order to go from 'new' to 'sent' or 'cancelled'"),
       value: data.average_time,
     }),
   },
   {
     id: "number_new_orders",
-    description: "New orders this month",
+    description: _t("New orders this month"),
     Component: NumberCard,
     props: (data) => ({
-      title: "Number of new orders this month",
+      title: _t("Number of new orders this month"),
       value: data.nb_new_orders,
     }),
   },
   {
     id: "cancelled_orders",
-    description: "Cancelled orders this month",
+    description: _t("Cancelled orders this month"),
     Component: NumberCard,
     props: (data) => ({
-      title: "Number of cancelled orders this month",
+      title: _t("Number of cancelled orders this month"),
       value: data.nb_cancelled_orders,
     }),
   },
   {
     id: "amount_new_orders",
-    description: "amount orders this month",
+    description: _t("amount orders this month"),
     Component: NumberCard,
     props: (data) => ({
-      title: "Total amount of new orders this month",
+      title: _t("Total amount of new orders this month"),
       value: data.total_amount,
     }),
   },
   {
     id: "pie_chart",
-    description: "Shirt orders by size",
+    description: _t("Shirt orders by size"),
     Component: PieChartCard,
     size: 2,
     props: (data) => ({
-      title: "Shirt orders by size",
+      title: _t("Shirt orders by size"),
       values: data.orders_by_size,
     }),
   },
