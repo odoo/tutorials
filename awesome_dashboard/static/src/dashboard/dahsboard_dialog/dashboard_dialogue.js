5edca1fd9ac40445e1f7966b248aa661e97a9ed2
Joris Makauskis <jmak@odoo.com>
2024-02-26 09:35:54 +0100

    [ADD] owl 2.11: added dialogue to select visability of stats

---
+++ awesome_dashboard/static/src/dashboard/dahsboard_dialog/dashboard_dialogue.js
@@ -0,0 +1,38 @@
+/** @odoo-module **/
+
+import { Component, useState } from "@odoo/owl";
+import { Dialog } from "@web/core/dialog/dialog";
+import { CheckBox } from "@web/core/checkbox/checkbox";
+import { browser } from "@web/core/browser/browser";
+
+export class ConfigurationDialog extends Component {
+  static template = "awesome_dashboard.ConfigurationDialog";
+  static components = { Dialog, CheckBox };
+  static props = ["close", "items", "disabledItems", "onUpdateConfiguration"];
+
+  setup() {
+    this.items = useState(
+      this.props.items.map((item) => {
+        return {
+          ...item,
+          enabled: !this.props.disabledItems.includes(item.id),
+        };
+      })
+    );
+  }
+
+  done() {
+    this.props.close();
+  }
+
+  onChange(checked, changedItem) {
+    changedItem.enabled = checked;
+    const newDisabledItems = Object.values(this.items)
+      .filter((item) => !item.enabled)
+      .map((item) => item.id);
+
+    browser.localStorage.setItem("disabledDashboardItems", newDisabledItems);
+
+    this.props.onUpdateConfiguration(newDisabledItems);
+  }
+}
--- awesome_dashboard/static/src/dashboard/dashboard.js
+++ awesome_dashboard/static/src/dashboard/dashboard.js
@@ -5,10 +5,12 @@ import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
 import { useService } from "@web/core/utils/hooks";
 import { DashboardItem } from "./dashboard_item/dashboard_item";
+import { ConfigurationDialog } from "./dahsboard_dialog/dashboard_dialogue";
+import { browser } from "@web/core/browser/browser";
 
 class AwesomeDashboard extends Component {
   static template = "awesome_dashboard.AwesomeDashboard";
-  static components = { Layout, DashboardItem };
+  static components = { Layout, DashboardItem, ConfigurationDialog };
 
   setup() {
     this.display = {
@@ -17,6 +19,12 @@ class AwesomeDashboard extends Component {
     this.action = useService("action");
     this.statistics = useState(useService("awesome_dashboard.statistics"));
     this.items = registry.category("awesome_dashboard_items").getAll();
+    this.dialog = useService("dialog");
+    this.state = useState({
+      disabledItems:
+        browser.localStorage.getItem("disabledDashboardItems")?.split(",") ||
+        [],
+    });
   }
   openCustomers() {
     this.action.doAction("base.action_partner_form");
@@ -32,6 +40,18 @@ class AwesomeDashboard extends Component {
         [false, "form"],
       ],
     });
+  }
+
+  openConfiguration() {
+    this.dialog.add(ConfigurationDialog, {
+      items: this.items,
+      disabledItems: this.state.disabledItems,
+      onUpdateConfiguration: this.updateConfiguration.bind(this),
+    });
+  }
+
+  updateConfiguration(newDisabledItems) {
+    this.state.disabledItems = newDisabledItems;
   }
 }
 
