e88db0be7aa73329e76f257935bd67c8037582e5
Mohammad Hossam <60348524+mohammadhossam@users.noreply.github.com>
2024-02-27 15:19:19 +0200

    [IMP] awesome_dashboard: refresh the dashboard data every 10 minutes (chapter 2.7)

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import { Component, onWillStart } from "@odoo/owl";
+import { Component, onWillStart, useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
 import { useService } from "@web/core/utils/hooks";
@@ -14,10 +14,14 @@ class AwesomeDashboard extends Component {
   async setup() {
     this.action = useService("action");
     this.rpc = useService("rpc");
-    this.statistics = useService("statistics");
-    onWillStart(async () => {
-      this.updateStatistics(await this.statistics());
-    });
+    this.statistics = useState(useService("statistics"));
+    console.log(this.statistics);
+    // onWillStart(async () => {
+    //   this.updateStatistics(await this.statistics());
+    //   this.updateTimer = setInterval(async () => {
+    //     this.updateStatistics(await this.statistics());
+    //   }, 5000);
+    // });
   }
 
   openCustomers() {
@@ -36,15 +40,15 @@ class AwesomeDashboard extends Component {
     });
   }
 
-  updateStatistics(data) {
-    this.newOrders = data.nb_new_orders;
-    this.totalOrders = data.total_amount;
-    this.averageTshirt = data.average_quantity;
-    this.cancelledOrders = data.nb_cancelled_orders;
-    this.averageTime = data.average_time;
-    this.pieChartLabels = Object.keys(data.orders_by_size);
-    this.pieChartData = Object.values(data.orders_by_size);
-  }
+  //   updateStatistics(data) {
+  //     this.state.newOrders = data.nb_new_orders;
+  //     this.state.totalOrders = data.total_amount;
+  //     this.state.averageTshirt = data.average_quantity;
+  //     this.state.cancelledOrders = data.nb_cancelled_orders;
+  //     this.state.averageTime = data.average_time;
+  //     this.state.pieChartLabels = Object.keys(data.orders_by_size);
+  //     this.state.pieChartData = Object.values(data.orders_by_size);
+  //   }
 }
 
 registry
--- awesome_dashboard/static/src/dashboard_item/statistics/statistics_service.js
+++ awesome_dashboard/static/src/dashboard_item/statistics/statistics_service.js
@@ -1,11 +1,21 @@
 /** @odoo-module **/
+import { reactive } from "@odoo/owl";
 import { registry } from "@web/core/registry";
-import { memoize } from "@web/core/utils/functions";
 
 export const statisticsService = {
   dependencies: ["rpc"],
   start(env, { rpc }) {
-    return memoize(() => rpc("/awesome_dashboard/statistics"));
+    let statistics = reactive({});
+
+    async function retrieveStatistics() {
+      let result = await rpc("/awesome_dashboard/statistics");
+      Object.assign(statistics, result);
+    }
+
+    retrieveStatistics();
+    setInterval(retrieveStatistics, 10 * 60 * 1000);
+
+    return statistics;
   },
 };
 
