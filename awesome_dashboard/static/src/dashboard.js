9747fa1eaf454ee8792d0756fd77dc07fad1dda0
Hesham <hsal@odoo.com>
2024-02-27 14:16:51 +0200

    [IMP] awesome_dashboard: Added buttons Customers and leads

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,11 +1,32 @@
 /** @odoo-module **/
 
+import { useService } from "@web/core/utils/hooks";
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
     static components = { Layout };
+
+    setup() {
+        this.action = useService("action");
+    }
+
+    showCustomers() {
+        this.action.doAction("base.action_partner_form");
+    }
+
+    showLeads() {
+        this.action.doAction({
+            type: "ir.actions.act_window",
+            name: "Leads",
+            res_model: "crm.lead",
+            views: [
+                [false, "tree"],
+                [false, "form"],
+            ],
+        });
+    }
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
