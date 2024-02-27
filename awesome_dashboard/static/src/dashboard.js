9ee4d8e70b61f90a5315d2e7f1645d004d78fdfc
Hesham <hsal@odoo.com>
2024-02-27 14:43:07 +0200

    [IMP] awesome_dashboard: Added dashboard_item

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -4,9 +4,10 @@ import { useService } from "@web/core/utils/hooks";
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
+import { DashboardItem } from "./dasboard_item/dashboard_item";
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
-    static components = { Layout };
+    static components = { Layout, DashboardItem };
 
     setup() {
         this.action = useService("action");
