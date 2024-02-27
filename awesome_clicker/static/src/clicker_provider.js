1657c09f7a95c9e1321e20b4fac4a95c47979e51
Joris Makauskis <jmak@odoo.com>
2024-02-27 14:25:53 +0100

    [IMP] owl 3.15: Added clicker commands to command palette

---
+++ awesome_clicker/static/src/clicker_provider.js
@@ -0,0 +1,29 @@
+/** @odoo-module **/
+
+import { registry } from "@web/core/registry";
+
+const commandProviderRegistry = registry.category("command_provider");
+
+commandProviderRegistry.add("clicker", {
+  provide: (env) => {
+    return [
+      {
+        name: "Buy 1 click bot",
+        action() {
+          env.services["awesome_clicker.clickerService"].buyBot("clickbot");
+        },
+      },
+      {
+        name: "Open Clicker Game",
+        action() {
+          env.services.action.doAction({
+            type: "ir.actions.client",
+            tag: "awesome_clicker.client_action",
+            target: "new",
+            name: "Clicker Game",
+          });
+        },
+      },
+    ];
+  },
+});
