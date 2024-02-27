be33c1a0a3e41dbba3eed5f06d461c58af600fa6
Beekan-mmha <mmha@odoo.com>
2024-02-27 19:17:36 +0200

    [IMP] Awesome dashboard: add buttons for people and leads

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -3,17 +3,35 @@
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
     static components = { Layout };
 
     setup() {
+        this.action = useService("action");
         this.display = {
             controlPanel: {},
         };
     }
-    
+
+    openCustomerView() {
+        this.action.doAction("base.action_partner_form")
+    }
+
+    openLeads() {
+        this.action.doAction({
+            type:"ir.actions.act_window",
+            name:"Leads",
+            res_model:"crm.lead",
+            views:[
+                [false,"list"],
+                [false,"form"]
+            ]
+        });
+    }
+
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
