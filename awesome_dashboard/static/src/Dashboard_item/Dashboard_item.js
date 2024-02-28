e98fd12e344f3b5cb4250ce64116aa6a85a5b559
Mohammed Basioni <mohammedalnamaki.com>
2024-02-28 14:47:18 +0200

    [IMP] awesome_owl: add Layout component with dashboard items inside, and navigation buttons.

---
+++ awesome_dashboard/static/src/Dashboard_item/Dashboard_item.js
@@ -0,0 +1,18 @@
+/** @odoo-module **/
+
+import { Component } from "@odoo/owl";
+export class DashboardItem extends Component {
+    static template = "awesome_dashboard.DashboardItem";
+
+    static props = {
+        size: {
+            type: Number,
+            optional: true,
+            default: 1
+        },
+        slots: {
+            type: Object,
+            optional: true
+        }
+    }
+}
--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -2,9 +2,33 @@
 
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
+import { DashboardItem } from "./Dashboard_item/Dashboard_item";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
+    static components = { Layout, DashboardItem }
+
+    setup() {
+        this.display = { controlPanel: {} };
+        this.action = useService("action");
+
+    }
+
+    openCustomers() {
+        this.action.doAction("base.action_partner_form");
+    }
+
+    openLeads() {
+        this.action.doAction({
+            type: 'ir.actions.act_window',
+            name: 'Leads',
+            res_model: 'crm.lead',
+            views: [[false, 'form'], [false, 'list']],
+        });
+    }
+
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
