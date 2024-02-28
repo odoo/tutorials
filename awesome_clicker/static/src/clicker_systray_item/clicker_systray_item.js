c01cb868866e53935073daa4b8dbff0768a09818
Joris Makauskis <jmak@odoo.com>
2024-02-28 13:43:21 +0100

    [IMP] owl 3.17: Change systray item to be a dropdown

--- awesome_clicker/static/src/clicker_systray_item/clicker_systray_item.js
+++ awesome_clicker/static/src/clicker_systray_item/clicker_systray_item.js
@@ -5,10 +5,12 @@ import { Component } from "@odoo/owl";
 import { useService } from "@web/core/utils/hooks";
 import { useClicker } from "../clicker_hook";
 import { ClickerValue } from "../clicker_value/clicker_value";
+import { Dropdown } from "@web/core/dropdown/dropdown";
+import { DropdownItem } from "@web/core/dropdown/dropdown_item";
 
 export class ClickerSystray extends Component {
   static template = "awesome_clicker.ClickerSystray";
-  static components = { ClickerValue };
+  static components = { ClickerValue, Dropdown, DropdownItem };
 
   setup() {
     this.clicker = useClicker();
@@ -24,6 +26,20 @@ export class ClickerSystray extends Component {
     });
 
   increment = () => this.clicker.increment(9);
+
+  get numberTrees() {
+    return Object.keys(this.clicker.trees).reduce(
+      (acc, tree) => acc + this.clicker.trees[tree].purchased,
+      0
+    );
+  }
+
+  get numberFruits() {
+    return Object.keys(this.clicker.fruits).reduce(
+      (acc, fruit) => acc + this.clicker.fruits[fruit],
+      0
+    );
+  }
 }
 
 export const systrayItem = {
