9c9ba8c16ab5c450e9a6dbdbe912458fa84b491a
Joris Makauskis <jmak@odoo.com>
2024-02-28 15:28:29 +0100

    [IMP] owl 3.18: Use notebook component

--- awesome_clicker/static/src/client_action/client_action.js
+++ awesome_clicker/static/src/client_action/client_action.js
@@ -4,10 +4,11 @@ import { registry } from "@web/core/registry";
 import { Component } from "@odoo/owl";
 import { useClicker } from "../clicker_hook";
 import { ClickerValue } from "../clicker_value/clicker_value";
+import { Notebook } from "@web/core/notebook/notebook";
 
 export class ClientAction extends Component {
   static template = "awesome_clicker.client_action";
-  static components = { ClickerValue };
+  static components = { ClickerValue, Notebook };
 
   setup() {
     this.clicker = useClicker();
