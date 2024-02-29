8a46d8adcc3e5b6f71fbcf6dab9e28794373c48c
Beekan-mmha <mmha@odoo.com>
2024-02-29 03:22:44 +0200

    [IMP] Awesome dashboard: give user choice to view which item

--- awesome_dashboard/static/src/dashboard/dashboard.js
+++ awesome_dashboard/static/src/dashboard/dashboard.js
@@ -5,6 +5,9 @@ import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
 import { useService } from "@web/core/utils/hooks";
 import { AwesomeDashboardItem } from "./dashboard_item/dashboard_item"
+import { Dialog } from "@web/core/dialog/dialog";
+import { CheckBox } from "@web/core/checkbox/checkbox";
+import { browser } from "@web/core/browser/browser";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
@@ -16,7 +19,23 @@ class AwesomeDashboard extends Component {
             controlPanel: {},
         };
         this.result_statistics = useState(useService("awesome_dashboard.statistics"));
-        this.items = registry.category("awesome_dashboard").getAll();;
+        this.dialog = useService("dialog");
+        this.items = registry.category("awesome_dashboard").getAll();
+        this.state = useState({
+            disabledItems: browser.localStorage.getItem("uncheckedItems")?.split(",") || []
+        });
+    }
+
+    openConfiguration() {
+        this.dialog.add(ConfigurationDialog, {
+            items: this.items,
+            disabledItems: this.state.disabledItems,
+            onUpdateConfiguration: this.updateConfiguration.bind(this),
+        })
+    }
+
+    updateConfiguration(newDisabledItems) {
+        this.state.disabledItems = newDisabledItems;
     }
 
     openCustomerView() {
@@ -33,6 +52,35 @@ class AwesomeDashboard extends Component {
                 [false,"form"]
             ]
         });
+    }
+
+}
+
+class ConfigurationDialog extends Component {
+    static template = "awesome_dashboard.ConfigurationDialog";
+    static components = { Dialog, CheckBox };
+
+    setup() {
+        this.items = useState(this.props.items.map((item) => {
+            return {
+                ...item,
+                enabled: !this.props.disabledItems.includes(item.id),
+            }
+        }));
+    }
+
+    done() {
+        this.props.close();
+    }
+
+    onChange(checked, item) {
+        item.checked = checked;
+        const newUncheckedItems = Object.values(this.items)
+          .filter((item) => !item.checked)
+          .map((item) => item.id);
+    
+        browser.localStorage.setItem("uncheckedItems", newUncheckedItems);
+        this.props.onUpdateConfiguration(newUncheckedItems);
     }
 
 }
