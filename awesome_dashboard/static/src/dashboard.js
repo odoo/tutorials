b18ac0bada8be2e7fae510d333ce8728368a4ab7
Hamza (habo) <habo@odoo.com>
2024-03-01 13:48:45 +0100

    [ADD] awesome_dashboard: Added dashboard and linked buttons to actions

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -2,9 +2,26 @@
 
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
+
+    setup() {
+        this.action = useService("action");
+        //this.openSettings = this.openSettings.bind(this);
+    }
+    openCustomerList() {
+        console.log('clicked');
+        this.action.doAction("base.action_partner_form");
+    }
+    openLeadList() {
+        console.log('clicked');
+        this.action.doAction("crm.crm_lead_all_leads");
+    }
+
+    static components = { Layout };
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
