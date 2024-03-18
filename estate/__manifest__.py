3192cbb52a7e573d147bd23cd66faa8ad056e18c
muab-odoo <muab@odoo.com>
2024-03-19 01:36:11 +0200

    [ADD] Chapter 3: Add Estate module

---
+++ estate/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'Estate',
+    'version': '1.0',
+    'category': 'Tutorials/Estate',
+    'summary': 'Real Estate Advertisement',
+    'description':
+    """
+    The top area of the form view summarizes important information for the property, such as the name, the property type, the postcode and so on.
+    The first tab contains information describing the property: bedrooms, living area, garage, garden…
+    The second tab lists the offers for the property. We can see here that potential buyers can make offers above or below the expected selling price.
+    It is up to the seller to accept an offer.
+    """,
+    'website': "https://www.odoo.com",
+    'depends': [
+        'base'
+    ],
+    'data': [],
+    'demo': [],
+    'css': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'AGPL-3'
+}
