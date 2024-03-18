29d0fd9da633ad76534d89b9adc76cc73cb9d52e
cacl <cacl@odoo.com>
2024-03-18 11:58:52 +0100

    [ADD] estate: create a empty module to start the tutorial

---
+++ estate/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': "Estate",
+
+    'summary': """
+        A new app to learn the Odoo framework
+    """,
+
+    'description': """
+        A new app to learn the Odoo framework
+    """,
+
+    'author': "Clément Cardot (cacl)",
+
+    'depends': ['base'],
+    'application': True,
+}
