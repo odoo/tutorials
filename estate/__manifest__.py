5f603f5dfaeb20befabd31b4ff45260a5242e08c
mizosoft <moataz.nasser20@gmail.com>
2024-03-19 12:20:27 +0200

    [ADD] estate: add manifest

---
+++ estate/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'RealEstate',
+    'version': '0.1',
+    'summary': 'Tracks RealEstate property and buyer offers',
+    'description': "Tracks RealEstate property and buyer offers",
+    'depends': [
+        'base'
+    ],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
