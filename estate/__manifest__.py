bfcc94ce0247ef39a64cd06cbe60d034fbb84e2e
“Seif <seifgneedy2@gmail.com>
2024-03-19 16:58:30 +0200

    [IMP] estate: Add action, menus, and constrain fields
    Chapter Six

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,6 +5,8 @@
     'application': True,
     'data': [
         'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ],
     'license': 'AGPL-3'
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -8,10 +8,10 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=fields.Date.add(fields.Date.today(), days=90))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -20,3 +20,12 @@ class EstateProperty(models.Model):
     garden_orientation = fields.Selection(
         string='Orientation',
         selection=[('North', 'North'), ('South', 'South'), ('East', 'East'), ('West', 'West')])
+    state = fields.Selection(
+        default='New',
+        selection=[
+            ('New', 'New'),
+            ('Offer Received', 'Offer Received'),
+            ('Offer Accepted', 'Offer Accepted'),
+            ('Sold', 'Sold'),
+            ('Canceled', 'Canceled')])
+    active = fields.Boolean(default=True)
