0feff00dc0c441f580197f5bf4830cb22c868f80
“Seif <seifgneedy2@gmail.com>
2024-03-19 11:29:19 +0200

    [ADD] estate: Add Real Estate App
    Chapter 3

---
+++ estate/__manifest__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Real Estate',
+    'description': "Real Estate Advertisement",
+    'depends': ['base'],
+    'application': True,
+    'license': 'AGPL-3'
+}
