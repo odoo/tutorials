c36cfd1f2f2fb6225dcb9ed9b80f0c8450f05f50
stta-odoo <stta@odoo.com>
2024-03-20 12:11:13 +0800

    Excercise Ch3-Ch7
    1. setup manifest 2. setup access right 3. create estate model 4. create estate view

---
+++ estate/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Real Estate',
+    'application': True,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menu_views.xml',
+    ]
+}
---
+++ estate/models/estate_property.py
@@ -0,0 +1,28 @@
+from odoo import fields, models
+
+class Property(models.Model):
+    _name = "estate.property"
+    _description = "Properties of an estate"
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(string="Available From", copy=False, default=fields.Date.add(fields.Date.today(), months=3))
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer(string="Living Area (sqm)")
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        selection = [("north", "North"),("west", "West"),("east","East"),("south","South")]
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        selection = [("new","New"), ("offer_received","Offer Received"), ("offer_cccepted","Offer Accepted"), ("sold","Sold"), ("canceled","Canceled")],
+        required = True,
+        default = "new",
+        copy = False,
+    )
+
