387244bc97e348dc5e5f7594c879f4e18ca17664
mizosoft <moataz.nasser20@gmail.com>
2024-03-20 11:36:59 +0200

    [ADD] estate: add UI (ch6)

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,7 +5,9 @@
     'description': "Tracks RealEstate property and buyer offers",
     'depends': ['base'],
     'data': [
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
     ],
     'application': True,
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -8,15 +8,16 @@ class EsateProperty(models.Model):
     name = fields.Char('Title', required=True, index=True)
     description = fields.Text('Description')
     postcode = fields.Char('Postal Code')
-    date_availability = fields.Date('Date Availability')
+    date_availability = fields.Date('Available From', copy=False, 
+                                    default=lambda _: fields.Date.add(fields.Date.today(), months=3))
     expected_price = fields.Float('Expected Price', required=True)
-    selling_price = fields.Float('Selling Price')
-    bedrooms = fields.Integer('Bedrooms')
-    living_area = fields.Integer('Living Area')
+    selling_price = fields.Float('Selling Price', readonly=True, copy=False)
+    bedrooms = fields.Integer('Bedrooms', default=2)
+    living_area = fields.Integer('Living Area (sqm)')
     facades = fields.Integer('Facades')
     garage = fields.Boolean('Garage')
     garden = fields.Boolean('Garden')
-    garden_area = fields.Integer('Garden Area')
+    garden_area = fields.Integer('Garden Area (sqm)')
     garden_orientation = fields.Selection([
             ('north', 'North'),
             ('south', 'South'),
@@ -24,3 +25,10 @@ class EsateProperty(models.Model):
             ('west', 'West')
         ],
         'Garden Orientation', required=True)
+    active = fields.Boolean('Active', default=False)
+    staus = fields.Selection([
+        ('new', 'New'),
+        ('offer_received', 'Offer Received'),
+        ('sold', 'Sold'),
+        ('cancelled', 'Cancelled')
+    ], default='new')
