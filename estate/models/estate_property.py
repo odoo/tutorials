6764cf22fa6f5be26c3786cf8dca06f0167ab143
“Seif <seifgneedy2@gmail.com>
2024-03-20 18:01:07 +0200

    [IMP] estate: Set default strings for estate_property fields

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,20 +5,20 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real estate Property Field"
 
-    name = fields.Char(required=True)
-    description = fields.Text()
-    postcode = fields.Char()
-    date_availability = fields.Date(copy=False, default=fields.Date.add(fields.Date.today(), days=90))
-    expected_price = fields.Float(required=True)
-    selling_price = fields.Float(readonly=True, copy=False)
-    bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
-    facades = fields.Integer()
-    garage = fields.Boolean()
-    garden = fields.Boolean()
-    garden_area = fields.Integer()
+    name = fields.Char(required=True, string='Title')
+    description = fields.Text(string='Description')
+    postcode = fields.Char(string='Postcode')
+    date_availability = fields.Date(copy=False, default=fields.Date.add(fields.Date.today(), days=90), string='Available From')
+    expected_price = fields.Float(required=True, string='Expected Price')
+    selling_price = fields.Float(readonly=True, copy=False, string='Selling Price')
+    bedrooms = fields.Integer(default=2, string='Bedrooms')
+    living_area = fields.Integer(string='Living Area (sqm)')
+    facades = fields.Integer(string='Facades')
+    garage = fields.Boolean(string='Garage')
+    garden = fields.Boolean(string='Garden')
+    garden_area = fields.Integer(string='Garden Area (sqm)')
     garden_orientation = fields.Selection(
-        string='Orientation',
+        string='Garden Orientation',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
     state = fields.Selection(
         default='new',
