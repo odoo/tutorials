e4afa73840d934dd04fd4d70cb02d35870d14416
“Seif <seifgneedy2@gmail.com>
2024-03-21 01:15:45 +0200

    [IMP] estate: add computed and onchange to property and offer models
    Chapter Nine

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class PropertyOffer(models.Model):
@@ -9,3 +9,16 @@ class PropertyOffer(models.Model):
     status = fields.Selection(copy=False, string='Status', selection=[('accepted', 'Accepted'), ('refused', 'Refused')])
     partner_id = fields.Many2one('res.partner', string='Partner', required=True)
     property_id = fields.Many2one('estate.property', required=True)
+    validity = fields.Integer(string='Validity (days)', default=7)
+    date_deadline = fields.Date(string='Deadline', compute='_compute_date_deadline', inverse='_inverse_date_deadline')
+
+    @api.depends('validity', 'create_date')
+    def _compute_date_deadline(self):
+        for record in self:
+            create_date = record.create_date or fields.Date.today()
+            record.date_deadline = fields.Date.add(create_date, days=record.validity)
+    
+    def _inverse_date_deadline(self):
+        for record in self:
+            create_date = fields.Date.to_date(record.create_date or fields.Date.today())
+            record.validity = (record.date_deadline - create_date).days
