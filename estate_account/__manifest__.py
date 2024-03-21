c72a89851640eaf217cb2953af07559e68bd0b4f
Solal Vignal <sovi@odoo.com>
2024-03-21 14:26:59 +0100

    [ADD] estate_account: Create the 'estate_account' link module

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Estate X Invoicing: 100.07% Stonks",
+    'version': '1.0',
+    'depends': ['estate', 'account'],
+    'author': "Solal Vignal",
+    'category': 'Productivity',
+    'application': True,
+    'description': """
+    Will make you even richer
+    """,
+    'data': [
+        # 'security/ir.model.access.csv',
+    ],
+}
