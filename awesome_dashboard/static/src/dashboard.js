ef3d9de0fea49743ef0e87d33606f82eecda76ee
cacl <cacl@odoo.com>
2024-03-25 16:51:58 +0100

    [IMP] awesome_dashboard: Part 1 to 5 of the tutorial
    Pratice with Layout, Buttons -> Actions, Dashboard & Dashboard Items, Display Statistics from back end, store data in cache with memoize

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,10 +1,36 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, onWillStart } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
+import { DashboardItem } from "./dashboard_item/dashboard_item";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
+    static components = { Layout, DashboardItem };
+
+    setup() {
+        this.action = useService("action");
+        this.statistics = useService("awesome_dashboard.statistics");
+        onWillStart(async () => {
+            this.statistics = await this.statistics.loadStatistics();
+        });
+    }
+
+    async openCustomers() {
+        this.action.doAction("base.action_partner_form")
+    }
+
+    async openLeads() {
+        this.action.doAction({
+            type: 'ir.actions.act_window',
+            name: 'Leads',
+            target: 'current',
+            res_model: 'crm.lead',
+            views: [[false, 'tree']]
+        });
+    }
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
