1aa4c7ab6d675cd3b354ae34a81b275104c39fc0
muab-odoo <muab@odoo.com>
2024-03-25 20:47:59 +0200

    [IMP] Generic components and tweak to show/hide the content using the callbackHandler

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -1,11 +1,22 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 
 export class Card extends Component {
     static template = "awesome_owl.card";
     static props = {
-        title: {type: String},
-        content: {type: String}
+        title: { type: String },
+        slots: {
+            type: Object,
+            shape: { default: true }
+        }
+    }
+
+    setup() {
+        this.state = useState({ isOpen: true });
+    }
+
+    toggleStatus() {
+        this.state.isOpen = !this.state.isOpen;
     }
 }
