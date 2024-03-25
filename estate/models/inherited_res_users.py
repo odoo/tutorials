071639f15fd8817fc34e8d60816b39fcce809e52
“Seif <seifgneedy2@gmail.com>
2024-03-25 13:30:42 +0200

    [IMP] estate: Add invoicing for sold properties

--- estate/models/inherited_res_users.py
+++ estate/models/inherited_res_users.py
@@ -4,5 +4,5 @@ from odoo import fields, models
 class InheritedResUsers(models.Model):
     _inherit = 'res.users'
 
-    property_ids = fields.One2many('estate.property', 'salesperson_id',
-                    string="Properties", domain=[('state', 'in', ('new', 'offer_received'))])
+    property_ids = fields.One2many('estate.property', 'salesperson_id', string="Properties",
+                                    domain=[('state', 'in', ('new', 'offer_received'))])
---
+++ estate_account/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'Real Estate Accounting',
+    'description': "Real Estate Accounting",
+    'depends': ['base', 'estate', 'account'],
+    'data': [
+    ],
+    'license': 'AGPL-3'
+}
