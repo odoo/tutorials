ade80078c3bbab4f8140bbac18577b1e91c9024e
muab-odoo <muab@odoo.com>
2024-03-25 15:52:11 +0200

    [ADD] Add two counters with sum of them owl props, bind, hooks

---
+++ awesome_owl/static/src/card/card.js
@@ -0,0 +1,11 @@
+/** @odoo-module **/
+
+import { Component } from "@odoo/owl";
+
+export class Card extends Component {
+    static template = "awesome_owl.card";
+    static props = {
+        title: {type: String},
+        content: {type: String}
+    }
+}
