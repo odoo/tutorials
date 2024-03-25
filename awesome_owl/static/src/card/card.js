c1fd60107f1e53bec246823363606405608680a7
axtr <axtr@odoo.com>
2024-03-25 14:48:45 +0100

    [IMP] awesome_owl: upgrade cards

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -1,9 +1,20 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 
 export class Card extends Component {
     static template = "awesome_owl.Card";
 
-    static props = ["title", "content"];
+    static props = {
+        title: {type: String},
+        slots: {type: Object, shape: {default: true}}
+    }
+
+    setup(){
+        this.state = useState({isOpen: true});
+    }
+
+    toggleCard(){
+        this.state.isOpen = !this.state.isOpen;
+    }
 }
