1dabcf3aad1b84da0f0ef7c55681a8974c442ac5
mizosoft <moataz.nasser20@gmail.com>
2024-03-27 18:07:14 +0200

    [IMP] awesome_owl: make Counter a sub-component

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,15 +1,9 @@
 /** @odoo-module **/
 
-import { Component, useState } from "@odoo/owl";
+import { Component } from "@odoo/owl";
+import { Counter } from "./counter/counter";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
-
-    setup() {
-        this.state = useState({value: 0})
-    }
-
-    increment() {
-        this.state.value++
-    }
+    static components = { Counter };
 }
