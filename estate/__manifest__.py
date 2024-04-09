7cf3be3bebab352fca2e1742fab1685e8d6aaf91
listen <1083116757@qq.com>
2024-04-10 01:14:16 +0800

    [ADD] estate: onboarding tutorial, steps 1-6

---
+++ estate/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "estate",
+    'description': """
+    estate
+    """,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+    ],
+    'license': 'LGPL-3',
+}
---
+++ estate/models/estate_property.py
@@ -0,0 +1,32 @@
+from odoo import fields, models
+
+
+class EstateProperty(models.Model):
+    _name = 'estate.property'
+    _description = 'Estate'
+
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(default=fields.Date.add(fields.Date.today(), months=3), copy=False)
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
+    state = fields.Selection(
+        default='new',
+        copy=False,
+        selection=[
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('cancelled', 'Canceled')])
+    active = fields.Boolean(default=True)
+
