21898ca34cd01a965ef931ab38142549b5b56e29
“Mathieu <mathieu.coutant2003@gmail.com>
2024-04-16 15:39:35 +0200

    Real end of chapter 5

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,5 +5,6 @@
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_menus.xml',
+        #'views/estate_views.xml',
     ]
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,5 +1,8 @@
 from odoo import fields, models
+from odoo.tools import date_utils
 
+def date_in_3_months(*args):
+    return date_utils.add(fields.Date.today(), months=3)
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -8,15 +11,22 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(string="Available From", default=date_in_3_months, copy=False)
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
-    living_area = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer(string="Living Area (sqm)")
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        required=True,
+        selection=[('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('canceled', 'Canceled')],
+        default = 'new',
+        copy=False
     )
