f5e3c058a66556fdabfb2e9f5379dfc96115e77a
akha <abdulrahmankhaledg@gmail.com>
2024-04-17 00:34:42 +0200

    [IMP] models: add new fields, attributes and defaults to the estate_property model. End of chapter 5.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -8,13 +8,19 @@ class Property(models.Model):
     name = fields.Char(required = True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy = False, default = fields.Date.add(fields.Date.today(), months = 3))
     expected_price = fields.Float(required = True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly = True, copy = False)
+    bedrooms = fields.Integer(default = 2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
-    garden_orientation = fields.Selection(selection = [('North', 'North'), ('SOUTH', 'SOUTH'), ('EAST', 'EAST'), ('WEST', 'WEST')])
+    garden_orientation = fields.Selection(selection = [('North', 'North'), ('South', 'South'), ('East', 'East'), ('West', 'West')])
+    active = fields.Boolean(default = True)
+    state = fields.Selection(selection = [('New', 'New'), ('Offer Received', 'Offer Received'), ('Offer Accepted', 'Offer Accepted'), ('Sold', 'Sold'), ('Canceled', 'Canceled')],
+                             required = True,
+                             copy = False,
+                             default = 'New')
+    
