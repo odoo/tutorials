1d679544de22ee0859a1616ea1b924d1b40b2a43
“gito-odoo” <giorgio.tocco2013@gmail.com>
2024-04-16 14:51:31 +0200

    [ADD] Chapter 5 functionality

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,7 +4,10 @@
         'base'
     ],
     'data': [
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
     ],
     'application': True
 }
--- estate/models/property.py
+++ estate/models/property.py
@@ -1,21 +1,36 @@
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta
+from datetime import date
 
 class Property(models.Model):
+
+    # A bit ugly having this method on top, but if I move it on the bottom it gives me an error
+    @staticmethod
+    def in_3_months(*args):
+        return date.today() + relativedelta(months=3)
+
     _name = "estate.property"
     _description = "Estate properties"
 
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=in_3_months)
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
-        selection=[('north', 'North'), ('south', 'South'), ('east', 'east'), ('west', 'West')]
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        required=True,
+        default='new',
+        copy=False,
+        selection=[('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('canceled', 'Canceled')]
     )
