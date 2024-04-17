8c252f4572f0214b2f2798e060711dc890bc85d1
loje <jeremylong1307@hotmail.com>
2024-04-17 13:35:36 +0200

    ADD: add views using estate_property_views and estate_menus.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,6 +3,9 @@
     'depends' : ['base'],
     'author': 'loje',
     'data': [
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
     ]
 }
