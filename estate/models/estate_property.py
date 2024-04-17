f310a1a351cf50948b4f8314db807fc358fd868b
Einar Andersen (eian) <eian@odoo.com>
2024-04-17 12:57:55 +0200

    [IMP] Real Estate: Custom list, form and search views. End of chapter 6

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -7,18 +7,18 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real Estate property"
 
-    name = fields.Char(required=True)
+    name = fields.Char("Title", required=True)
     description = fields.Text()
     postcode = fields.Char() 
-    date_availability = fields.Date(default=(date.today() + relativedelta(months=+3)), copy=False) # Default = in 3 months | not copied
+    date_availability = fields.Date("Available From", default=(date.today() + relativedelta(months=+3)), copy=False)
     expected_price = fields.Float(required=True) 
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer() 
+    living_area = fields.Integer("Living Area (sqm)") 
     facades = fields.Integer() 
     garage = fields.Boolean() 
     garden = fields.Boolean()
-    garden_area = fields.Integer() 
+    garden_area = fields.Integer("Garden Area (sqm)") 
     garden_orientation = fields.Selection(
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
     )
