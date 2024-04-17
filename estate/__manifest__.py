1a48e71250fef16fad82e3a4f15d89820bf2329c
loje <jeremylong1307@hotmail.com>
2024-04-17 11:05:15 +0200

    [ADD] LOJE Addition of the "estate" Model, creation of the "estate" model with the "estate_property" where basic fields such as "name" and "expected price" can be found, which are not nullable.

---
+++ estate/__manifest__.py
@@ -0,0 +1,5 @@
+{
+    'name': "estate",
+    'depends' : ['base'],
+    'author': 'loje',
+}
---
+++ estate/models/estate_property.py
@@ -0,0 +1,23 @@
+from odoo import models, fields
+
+class EstateProterty(models.Model):
+    _name = "estate_property"
+    _description = "estate property"
+
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientaton = fields.Selection(
+        string='Type',
+        selection=[('north','North'), ('south','South'),
+                     ('east', 'East'), ('west','West')]
+    )
