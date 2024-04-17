33cdbe10a5b897ff8fd5afe4fd208d2a31643f56
loje <jeremylong1307@hotmail.com>
2024-04-17 15:28:05 +0200

    [IMP] LOJE, standardize my code in the estate_property file.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import fields, models
+
 
 class EstateProterty(models.Model):
     _name = "estate_property"
@@ -7,7 +8,7 @@ class EstateProterty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(default= fields.Date.today(), copy=False)
+    date_availability = fields.Date(default=fields.Date.today(), copy=False)
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(copy=False, readonly=True)
     bedrooms = fields.Integer(default=2)
@@ -17,16 +18,26 @@ class EstateProterty(models.Model):
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientaton = fields.Selection(
-        selection=[('north','North'), ('south','South'),
-                     ('east', 'East'), ('west','West')]
+        selection=[
+            ("north", "North"),
+            ("south", "South"),
+            ("east", "East"),
+            ("west", "West"),
+        ],
+        string="Garden Orientation",
     )
 
     active = fields.Boolean(default=True, active=False)
     state = fields.Selection(
-        default='new',
+        selection=[
+            ("new", "New"),
+            ("offer received", "Offer Received"),
+            ("offer accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("canceled", "Canceled"),
+        ],
+        string="State",
+        default="new",
         copy=False,
         required=True,
-        selection=[('new', 'New'), ('offer received', 'Offer Received'),
-                   ('offer accepted', 'Offer Accepted'), ('sold', 'Sold'),
-                   ('canceled', 'Canceled')]
     )
