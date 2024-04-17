ca842e251dca57ac81b07d5a2600e1bb5320e7ce
akha <abdulrahmankhaledg@gmail.com>
2024-04-17 19:50:11 +0200

    [ADD] views: add list, form and search views of estate_property model. End of chapter 6.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -8,7 +8,8 @@
     'application': True,
     'data': [
         'security/ir.model.access.csv',
+        'views/estate_property_actions.xml',
+        'views/estate_property_menus.xml',
         'views/estate_property_views.xml',
-        'views/estate_menus.xml',
     ]
 }
