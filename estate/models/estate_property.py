c6e3259fbb3e8524f5dc252394d8dae77f345c60
loje <jeremylong1307@hotmail.com>
2024-04-18 10:16:39 +0200

    "[IMP] LOJE, addition of my form for estate property and addition of filters."

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import fields, models  # type: ignore
+from datetime import timedelta
 
 
 class EstateProterty(models.Model):
@@ -6,20 +7,20 @@ class EstateProterty(models.Model):
     _description = "estate property"
 
     name = fields.Char(string="Title", required=True)
-    description = fields.Text()
+    description = fields.Text(string="Description")
     postcode = fields.Char(string="Postcode")
     date_availability = fields.Date(
-        string="Available From", default=fields.Date.today(), copy=False
+        string="Available From", default=fields.Date.today() + timedelta(days=90), copy=False
     )
     expected_price = fields.Float(string="Expected Price", required=True)
     selling_price = fields.Float(string="Selling Price", copy=False, readonly=True)
     bedrooms = fields.Integer(string="Bedrooms", default=2)
-    living_area = fields.Integer()
-    facades = fields.Integer()
-    garage = fields.Boolean()
-    garden = fields.Boolean()
-    garden_area = fields.Integer()
-    garden_orientaton = fields.Selection(
+    living_area = fields.Integer(string="Living Area (sqm)")
+    facades = fields.Integer(string="Facades")
+    garage = fields.Boolean(string="Garage")
+    garden = fields.Boolean(string="Garden")
+    garden_area = fields.Integer(string="Garden area")
+    garden_orientation = fields.Selection(
         selection=[
             ("north", "North"),
             ("south", "South"),
