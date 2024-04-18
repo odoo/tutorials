136b8f416b48823439a4ea38f02b82f5a8634158
Einar Andersen (eian) <eian@odoo.com>
2024-04-18 11:21:36 +0200

    [IMP] Real Estate: Added an offer deadline field that can either be set with a date or days from current date. Chapter 8 - Exercise 3

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,6 @@
-from odoo import fields, models
+from odoo import api, fields, models
+
+from odoo.tools import date_utils
 
 
 class EstatePropertyOffer(models.Model):
@@ -13,3 +15,28 @@ class EstatePropertyOffer(models.Model):
     )
     partner_id = fields.Many2one("res.partner", required=True)
     property_id = fields.Many2one("estate.property", required=True)
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(
+        compute="_compute_deadline",
+        inverse="_inverse_deadline",
+        string="Offer Deadline",
+    )
+
+    @api.depends("validity")
+    def _compute_deadline(self):
+        for record in self:
+            if record.create_date:
+                record.date_deadline = date_utils.add(
+                    record.create_date, days=record.validity
+                )
+            else:
+                record.date_deadline = date_utils.add(
+                    fields.Date.today(), days=record.validity
+                )
+
+    def _inverse_deadline(self):
+        for record in self:
+            if record.create_date:
+                record.validity = (record.date_deadline - fields.Date.today()).days
+            else:
+                record.validity = (record.date_deadline - record.create_date).days
