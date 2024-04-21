8226ff4be954757f465e105e4f811e9ba695b9e9
akha <abdulrahmankhaledg@gmail.com>
2024-04-22 00:10:01 +0200

    [LINT] models: Follow odoo coding guidelines.

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -5,18 +5,20 @@ class EstateProperty(models.Model):
 
     def action_sold(self):
         self.ensure_one()
-        invoice_vals = self._prepare_invoice()
-        invoice_lines_vals = self._prepare_invoice_lines()
-        invoice_vals['invoice_line_ids'] = [Command.create(invoice_line_id) for invoice_line_id in invoice_lines_vals]
-        self.env['account.move'].sudo().with_context(default_move_type='out_invoice').create(invoice_vals)
+        self._create_account_move()
         return super().action_sold()
 
+    def _create_account_move(self):
+        invoice = self._prepare_invoice()
+        invoice_lines = self._prepare_invoice_lines()
+        invoice['invoice_line_ids'] = [Command.create(invoice_line) for invoice_line in invoice_lines]
+        self.env['account.move'].sudo().with_context(default_move_type='out_invoice').create(invoice)
+    
     def _prepare_invoice(self):
-        invoice_vals = {
+        return {
             'move_type': 'out_invoice',
             'partner_id': self.partner_id.id,
         }
-        return invoice_vals
 
     def _prepare_invoice_lines(self):
         """
