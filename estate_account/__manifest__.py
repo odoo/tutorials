6052270914a186a6608db7d763d3316a89a41bff
akha <abdulrahmankhaledg@gmail.com>
2024-04-21 22:34:29 +0200

    [IMP] models: Create estate_account module to link between estate and account modules.
    Create an invoice whenever a real estate property
    is sold. Use estate_account as a linking module
    between estate and account modules.

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "estate_account",
+    'version': '1.0',
+    'depends': [
+        'base',
+        'estate',
+        'account',
+    ],
+    'author': "Author Name",
+    'category': 'Category',
+    'summary': 'Link between estate and account modules',
+    'data': []
+}
---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,35 @@
+from odoo import Command, models
+
+class EstateProperty(models.Model):
+    _inherit = "estate_property"
+
+    def action_sold(self):
+        invoice_vals = self._prepare_invoice()
+        invoice_lines_vals = self._prepare_invoice_lines()
+        invoice_vals['invoice_line_ids'] = [Command.create(invoice_line_id) for invoice_line_id in invoice_lines_vals]
+        move = self.env['account.move'].sudo().with_context(default_move_type='out_invoice').create(invoice_vals)
+        return super().action_sold()
+
+    def _prepare_invoice(self):
+        invoice_vals = {
+            'move_type':'out_invoice',
+            'partner_id':self.partner_id.id,
+        }
+        return invoice_vals
+
+    def _prepare_invoice_lines(self):
+        """
+        Prepare the list of values to create the new invoice lines.
+        """
+        return [
+            {
+                'name': self.name,
+                'quantity': 1,
+                'price_unit': 0.06 * self.selling_price ,
+            },
+            {
+                'name': "Adminstrative Fees",
+                'quantity': 1,
+                'price_unit': 100,
+            },
+        ]
