9157f1a4dec0c33f8a568abdfc2e4646f976af38
akha <abdulrahmankhaledg@gmail.com>
2024-04-22 11:44:14 +0200

    [IMP] static: Solve Exercise 1.4 of odoo web framework

--- awesome_owl/static/src/js/playground.js
+++ awesome_owl/static/src/js/playground.js
@@ -1,10 +1,14 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, markup } from "@odoo/owl";
 import { Counter } from "./counter"
 import { Card } from "./card"
 export class Playground extends Component {
     static template = "awesome_owl.playground";
-
     static components = { Counter, Card }
+
+    setup(){
+        html = "<div> This is a dev </div>";
+        html_markup = markup(this.html);
+    }
 }
