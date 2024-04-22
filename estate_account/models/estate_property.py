067ea757c1e81cea9bbdf949715d27ab08a72015
Einar Andersen (eian) <eian@odoo.com>
2024-04-22 18:38:12 +0200

    [IMP] Real Estate Account: Added product lines to invoices
    Chapter 13 - exercise 4

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import models
+from odoo import models, Command
 
 
 class EstatePropert(models.Model):
@@ -6,6 +6,25 @@ class EstatePropert(models.Model):
 
     def action_set_property_sold(self):
         self.env["account.move"].create(
-            {"partner_id": self.partner_id.id, "move_type": "out_invoice"}
+            {
+                "partner_id": self.partner_id.id,
+                "move_type": "out_invoice",
+                "line_ids": [
+                    Command.create(
+                        {
+                            "name": "Comission",
+                            "quantity": 1,
+                            "price_unit": self.selling_price * 0.06,
+                        }
+                    ),
+                    Command.create(
+                        {
+                            "name": "Administative Fees",
+                            "quantity": 1,
+                            "price_unit": 100.00,
+                        }
+                    ),
+                ],
+            }
         )
         return super().action_set_property_sold()
