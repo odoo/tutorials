1c8169e3ab94e68af4828d02b77f9fe4b73e6ca9
akha <abdulrahmankhaledg@gmail.com>
2024-04-22 19:32:07 +0200

    [REF] static: Update Project Structure

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,8 +1,8 @@
 /** @odoo-module **/
 
 import { Component, markup, useState } from "@odoo/owl";
-import { Counter } from "./counter"
-import { Card } from "./card"
+import { Counter } from "./counter/counter"
+import { Card } from "./card/card"
 export class Playground extends Component {
     static template = "awesome_owl.playground";
     static components = { Counter, Card }
