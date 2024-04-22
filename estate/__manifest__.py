9d69f346d5109f4c5e478957112b9ec4ba11fb27
“Mathieu <mathieu.coutant2003@gmail.com>
2024-04-22 09:29:25 +0200

    [IMP] Training: Add the demo data loading

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,6 +1,5 @@
 {
     'name': "Estate",
-    'application': True,
     'data': [
         'views/inherited_user_views.xml',
         'security/ir.model.access.csv',
@@ -9,5 +8,11 @@
         'views/property_tag_views.xml',
         'views/property_type_views.xml',
         'views/estate_menus.xml',
-    ]
+    ],
+    "demo": [
+        "demo/demo_property_type.xml",
+        "demo/demo_property.xml",
+        "demo/demo_offer.xml",
+    ],
+    'application': True,
 }
