138d32340fdf6d694fdd98c4c502f5508d2d7a71
Einar Andersen (eian) <eian@odoo.com>
2024-04-22 18:26:36 +0200

    [ADD] Real Estate Account: Empty invoices are now created when the sold button is pressed
    Chapter 13 - exercise 3

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,7 +1,11 @@
 from odoo import models
 
+
 class EstatePropert(models.Model):
     _inherit = "estate.property"
 
     def action_set_property_sold(self):
+        self.env["account.move"].create(
+            {"partner_id": self.partner_id.id, "move_type": "out_invoice"}
+        )
         return super().action_set_property_sold()
