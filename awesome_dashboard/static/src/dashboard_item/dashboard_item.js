f732a5a79dde8da7fc5b53fbfc26e2ee31dcec8e
akha <abdulrahmankhaledg@gmail.com>
2024-04-24 00:25:30 +0200

    [IMP] static: Solve Exercise 2.3 of odoo web framework

---
+++ awesome_dashboard/static/src/dashboard_item/dashboard_item.js
@@ -0,0 +1,11 @@
+/** @odoo-module **/
+
+import { Component } from "@odoo/owl";
+export class DashboardItem extends Component {
+    static template = "awesome_dashboard.DashboardItem";
+    static props = {
+        size: {type: Number, default: 1},
+        slots: {type: Object, shape: {type: Object, optional: true}}
+    }
+
+}
