523e7a5d7ae5eb708b9911e540584154e8396d55
akha <abdulrahmankhaledg@gmail.com>
2024-04-23 22:32:25 +0200

    [IMP] static: Solve Exercise 1.14 of odoo web framework

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -1,9 +1,9 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 export class Card extends Component {
     static template = "awesome_owl.card"
-
+    
     static props = {
         title: String,
         slots: {
@@ -12,5 +12,15 @@ export class Card extends Component {
                 default: true, 
             },
         },
+    }
+
+    setup(){
+        this.state = useState({
+            open: true,
+        })
+    }
+
+    toggleCard(){
+        this.state.open = !this.state.open
     }
 }
