1c32cf4a948bcd924546e903196489e3bfc52e4c
akha <abdulrahmankhaledg@gmail.com>
2024-04-24 01:11:37 +0200

    [IMP] static: Solve Exercise 2.4 of odoo web framework

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -4,20 +4,35 @@ import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout"
 import { useService } from "@web/core/utils/hooks"
+import { onWillStart } from "@odoo/owl";
 import { DashboardItem } from "./dashboard_item/dashboard_item"
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
     static components = { Layout, DashboardItem }
 
     setup(){
+        this.statisticsDescription = {
+            'average_quantity':'Average amount of t-shirt by order this month',
+            'average_time':'Average time for an order to get from "new" to "sold" or "canceled"',
+            'nb_cancelled_orders':'Number of canceled orders this month',
+            'nb_new_orders':'Number of new orders this month',
+            'total_amount':'Total number of new orders this month',
+        }
+        
         this.action = useService("action")
+        this.rpc = useService("rpc")
+
+        onWillStart(async () => {
+            this.result = await this.rpc("/awesome_dashboard/statistics", {})
+            console.log(this.result)
+        })
     }
 
     openCustomersKanbanView(){
         this.action.doAction('base.action_partner_form')
     }
     
-    async openCrmLeadsView(activity) {
+    openCrmLeadsView(activity) {
         this.action.doAction({
             type: 'ir.actions.act_window',
             name: 'CRM Leads',
