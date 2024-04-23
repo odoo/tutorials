2b7abe4cbc05982e8f4675ae78c8f2f46c23f65b
akha <abdulrahmankhaledg@gmail.com>
2024-04-23 23:44:59 +0200

    [IMP] static: Solve Exercise 2.2 of odoo web framework

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -3,10 +3,31 @@
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout"
-
+import { useService } from "@web/core/utils/hooks"
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
     static components = { Layout }
+
+    setup(){
+        this.action = useService("action")
+    }
+
+    openCustomersKanbanView(){
+        this.action.doAction('base.action_partner_form')
+    }
+    
+    async openCrmLeadsView(activity) {
+        this.action.doAction({
+            type: 'ir.actions.act_window',
+            name: 'CRM Leads',
+            target: 'current',
+            res_model: 'crm.lead',
+            views: [
+                [false, 'list'],
+                [false, 'form'],
+            ],
+        });
+    }
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
