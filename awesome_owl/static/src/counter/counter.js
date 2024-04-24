e83c3ba5e89bdc3f96b7c77ac26be5782f245ec1
loje <jeremylong1307@hotmail.com>
2024-04-24 09:33:25 +0200

    [ADD 1.2 Add Counter folder in the src with counter xml and counter js file

---
+++ awesome_owl/static/src/counter/counter.js
@@ -0,0 +1,15 @@
+/** @odoo-module **/
+
+import { Component, useState } from "@odoo/owl";
+
+export class Counter extends Component{
+    static template = "awesome_owl.Counter";
+
+    setup(){
+        this.state = useState({value: 0});
+    }
+
+    increment(){
+        this.state.value++;
+    }
+}
