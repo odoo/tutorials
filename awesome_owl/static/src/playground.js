959ef3327d5299866942e876f29b3921552d9568
Einar Andersen (eian) <eian@odoo.com>
2024-04-24 11:23:37 +0200

    [IMP] OWl Chapter 1 - 4: Adding markup to the card

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, markup } from "@odoo/owl";
 import { Counter } from "./counter/counter"
 import { Card } from "./card/card";
 
@@ -8,4 +8,7 @@ import { Card } from "./card/card";
 export class Playground extends Component {
     static template = "awesome_owl.playground";
     static components = { Card, Counter };
+
+    card_1_content = "<em>Card 1 content from the JS file </em>";
+    card_2_content = markup("<em>Card 2 content from the JS file </em>");
 }
