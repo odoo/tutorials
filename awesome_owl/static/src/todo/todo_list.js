df435eacf48dd163e31ba3f6a196eb9c7c8d20c6
Einar Andersen (eian) <eian@odoo.com>
2024-04-24 15:20:10 +0200

    [ADD] OWl Chapter 1 - 10: Focus the todo input by default

--- awesome_owl/static/src/todo/todo_list.js
+++ awesome_owl/static/src/todo/todo_list.js
@@ -1,7 +1,8 @@
 /** @odoo-module **/
 
-import { Component, useState } from "@odoo/owl";
+import { Component, onMounted, useRef, useState } from "@odoo/owl";
 import { TodoItem } from "./todo_item";
+import { useAutofocus } from "../utils";
 
 export class TodoList extends Component {
     static template = "awesome_owl.todo_list";
@@ -10,6 +11,11 @@ export class TodoList extends Component {
     setup() {
         this.todos = useState([]);
         this.todoCounter = useState({value: 1});
+        useAutofocus('todo_input');
+        // this.inputRef = useRef('todo_input');
+        // onMounted(() => {
+        //     this.inputRef.el.focus();
+        // })
     }
 
     addTodo(e){
