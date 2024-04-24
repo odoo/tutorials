cc45df273ec1869bbfe4a352451f244aa4ba9d62
loje <jeremylong1307@hotmail.com>
2024-04-24 10:45:53 +0200

    [IMP] 1.4 Using markup to display html. Update Card to use t-out and import markup in playground

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import { Component, useState } from "@odoo/owl";
+import { Component, markup } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
 
@@ -8,4 +8,9 @@ export class Playground extends Component {
     static template = "awesome_owl.playground";
 
     static components = { Counter, Card };
+
+    setup(){
+        this.test1 = "<div><p>Some text</p></div>";
+        this.test2 = markup("<div><p>Some text</p></div>");
+    }
 }
