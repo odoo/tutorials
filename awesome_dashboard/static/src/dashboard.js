40fae726706cd419e4c38a83b3b97df3a4aa1c5f
Einar Andersen (eian) <eian@odoo.com>
2024-04-25 13:44:37 +0200

    [ADD] OWl Chapter 2 - 1 & 2: Added a layout and buttons to layout

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -2,9 +2,34 @@
 
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks"
+
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
+    static components = { Layout }
+
+    setup() {
+        this.action = useService("action");
+    }
+
+    openCustomers() {
+        this.action.doAction("base.action_partner_form");
+    }
+
+    openLeads() {
+        this.action.doAction({
+            type: 'ir.actions.act_window',
+            name: 'CRM Leads',
+            target: 'current',
+            res_model: 'crm.lead',
+            views: [
+                [false, 'list'],
+                [false, 'form'],
+            ],
+        });
+    }
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
