9cf7f5938e4e34b0626534e03380c9b174ee2a8e
“Mathieu <mathieu.coutant2003@gmail.com>
2024-04-25 13:04:17 +0200

    [IMP] Training: implemented translation

--- awesome_dashboard/static/src/dashboard/dashboard_items.js
+++ awesome_dashboard/static/src/dashboard/dashboard_items.js
@@ -3,14 +3,15 @@
 import { registry } from "@web/core/registry";
 import { NumberCard } from "./number_card";
 import { PieChartCard } from "./pie_chart_card";
+import { _t } from "@web/core/l10n/translation";
 
 const average_quantity = {
     id: "average_quantity",
-    description: "Average amount of t-shirt",
+    description: _t("Average amount of t-shirt"),
     Component: NumberCard,
     size: 1,
     props: (data) => ({
-        title: "Average amount of t-shirt by order this month",
+        title: _t("Average amount of t-shirt by order this month"),
         value: data.average_quantity
     }),
 }
@@ -18,11 +19,11 @@ registry.category("awesome_dashboard").add("average_quantity", average_quantity)
 
 const nb_new_orders = {
     id: "nb_new_orders",
-    description: "Number of new orders this month",
+    description: _t("Number of new orders this month"),
     Component: NumberCard,
     size: 1,
     props: (data) => ({
-        title: "Number of new orders this month",
+        title: _t("Number of new orders this month"),
         value: data.nb_new_orders
     }),
 }
@@ -30,11 +31,11 @@ registry.category("awesome_dashboard").add("nb_new_orders", nb_new_orders);
 
 const total_amount = {
     id: "total_amount",
-    description: "Total amount of new orders this month",
+    description: _t("Total amount of new orders this month"),
     Component: NumberCard,
     size: 1,
     props: (data) => ({
-        title: "Total amount of new orders this month",
+        title: _t("Total amount of new orders this month"),
         value: data.total_amount
     }),
 }
@@ -42,11 +43,11 @@ registry.category("awesome_dashboard").add("total_amount", total_amount);
 
 const nb_cancelled_orders = {
     id: "nb_cancelled_orders",
-    description: "Number of cancelled orders this month",
+    description: _t("Number of cancelled orders this month"),
     Component: NumberCard,
     size: 1,
     props: (data) => ({
-        title: "Number of cancelled orders this month",
+        title: _t("Number of cancelled orders this month"),
         value: data.nb_cancelled_orders
     }),
 }
@@ -54,11 +55,11 @@ registry.category("awesome_dashboard").add("nb_cancelled_orders", nb_cancelled_o
 
 const average_time = {
     id: "average_time",
-    description: "Average time for an order to go from ‘new’ to ‘sent’ or ‘cancelled’",
+    description: _t("Average time for an order to go from ‘new’ to ‘sent’ or ‘cancelled’"),
     Component: NumberCard,
     size: 1,
     props: (data) => ({
-        title: "Average time for an order to go from ‘new’ to ‘sent’ or ‘cancelled’",
+        title: _t("Average time for an order to go from ‘new’ to ‘sent’ or ‘cancelled’"),
         value: data.average_time
     }),
 }
@@ -66,11 +67,11 @@ registry.category("awesome_dashboard").add("average_time", average_time);
 
 const orders_by_size = {
         id: "orders_by_size",
-        description: "Chart of sizes",
+        description: _t("Chart of sizes"),
         Component: PieChartCard,
         size: 1,
         props: (data) => ({
-            title: "Chart of sizes",
+            title: _t("Chart of sizes"),
             value: data.orders_by_size
         }),
     }
