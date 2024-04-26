a1b34ad9e400f2bd23f699b81e97606911bbcc3e
akha <abdulrahmankhaledg@gmail.com>
2024-04-26 17:42:23 +0300

    [IMP] static: Solve Exercise 2.11 of odoo web framework

--- awesome_dashboard/static/src/dashboard/dashboard.js
+++ awesome_dashboard/static/src/dashboard/dashboard.js
@@ -6,15 +6,23 @@ import { Layout } from "@web/search/layout"
 import { useService } from "@web/core/utils/hooks"
 import { useState } from "@odoo/owl";
 import { DashboardItem } from "./dashboard_item/dashboard_item"
-import { items } from "./dashboard_items";
+import { Dialog } from "@web/core/dialog/dialog";
+import { CheckBox } from "@web/core/checkbox/checkbox";
+import { browser } from "@web/core/browser/browser";
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
-    static components = { Layout, DashboardItem }
+    static components = { Layout, DashboardItem, browser}
 
     setup(){
         this.action = useService("action")
+        this.dialog = useService("dialog")
         this.statistics= useState(useService("statisticsService"))
         this.items = registry.category("awesome_dashboard").getAll()
+        this.state = useState({excludedItemsIds: this.retrieveExcludedItemsIds()})
+    }
+
+    retrieveExcludedItemsIds(){
+        return (browser.localStorage.getItem("excludedItemsIds")?.split(",") || [])
     }
 
     openCustomersKanbanView(){
@@ -33,6 +41,48 @@ class AwesomeDashboard extends Component {
             ],
         });
     }
+    
+    openConfigurationDialog(){
+        this.dialog.add(ConfigurationDialog,
+        {
+            items: this.items,
+            excludedItemsIds: this.state.excludedItemsIds,
+            updateConfiguration: this.updateConfiguration.bind(this)
+        })
+    }
+
+    updateConfiguration(excludedItemsIds){
+        this.state.excludedItemsIds = excludedItemsIds
+        browser.localStorage.setItem("excludedItemsIds", this.state.excludedItemsIds)
+    }
+
 }
 
+class ConfigurationDialog extends Component{
+    static template = "awesome_dashboard.ConfigurationDialog"
+    static components = { Dialog, CheckBox}
+    static props = ["close", "items", "excludedItemsIds", "updateConfiguration"]
+    
+    setup(){
+        this.items = this.props.items.map((item) =>{
+            return {
+                ...item,
+                enabled: !this.props.excludedItemsIds.includes(item.id)
+            }
+        })
+    }
+
+    onChange(ev, changedItem){
+        changedItem.enabled = ev 
+        this.excludedItemsIds = Object.values(this.items).filter(item => !item.enabled).map(item => item.id)
+    }
+
+    closeAndUpdateConfiguration(){
+        this.props.close()
+        this.props.updateConfiguration(this.excludedItemsIds)
+    }
+    
+
+    
+}
 registry.category("lazy_components").add("AwesomeDashboard", AwesomeDashboard);
