9d85c8a0eaa224873b52b986ef170d65c746dc08
akha <abdulrahmankhaledg@gmail.com>
2024-04-26 09:10:13 +0300

    [IMP] static: Solve Exercise 2.8 of odoo web framework

--- awesome_dashboard/__manifest__.py
+++ awesome_dashboard/__manifest__.py
@@ -24,6 +24,10 @@
     'assets': {
         'web.assets_backend': [
             'awesome_dashboard/static/src/**/*',
+            ('remove', 'awesome_dashboard/static/src/dashboard/**/*')
+        ],
+        'awesome_dashboard.dashboard': [
+            'awesome_dashboard/static/src/dashboard/**/*'
         ],
     },
     'license': 'AGPL-3'
--- awesome_dashboard/static/src/dashboard/dashboard_item/dashboard_item.js
+++ awesome_dashboard/static/src/dashboard/dashboard_item/dashboard_item.js
@@ -4,7 +4,7 @@ import { Component } from "@odoo/owl";
 export class DashboardItem extends Component {
     static template = "awesome_dashboard.DashboardItem";
     static props = {
-        size: {type: Number, default: 1},
-        slots: {type: Object, shape: {type: Object, optional: true}}
+        size: {type: Number, default: 1, optional: true},
+        slots: {type: Object, shape: {default: Object}}
     }
 }
