33b5e5a6e79d0a9c28cfd5d04a305f191d1dc837
UxioMendez <uxio.mendez@hotmail.com>
2024-04-29 18:40:50 +0200

    atributos

--- estate/models/estate_properties.py
+++ estate/models/estate_properties.py
@@ -1,5 +1,21 @@
 from odoo import fields, models
 
+
 class properties(models.Model):
     _name = "estate_properties"
     _description = "Estate properties"
+
+    name = fields.Char('name')
+    description = fields.Text("description")
+    postcode = fields.Char("Postal code")
+    date_availability = fields.Date("Date availability")
+    expected_price = fields.Float("Expected price")
+    selling_price = fields.Float("Selling price")
+    bedrooms = fields.Integer("Bedrooms")
+    living_area = fields.Integer("Living area")
+    facades = fields.Integer("Facades")
+    garage = fields.Boolean("Garage")
+    garden = fields.Boolean("Garden")
+    garden_area = fields.Integer("Garden area")
+    garden_orientation = fields.Selection(string='Type',
+                                          selection=[("North"), ('South'), ("East"), ("West")])
