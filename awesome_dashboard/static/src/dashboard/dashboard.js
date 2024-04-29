60a0a37f848cdba6bce683b73d883432c27f48e0
Einar Andersen (eian) <eian@odoo.com>
2024-04-29 10:57:16 +0200

    [IMP] OWL Chapter 2 - 11: Togglable dashboard items

--- awesome_dashboard/static/src/dashboard/dashboard.js
+++ awesome_dashboard/static/src/dashboard/dashboard.js
@@ -3,20 +3,31 @@
 import { Component, useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
-import { useService } from "@web/core/utils/hooks"
+import { useService } from "@web/core/utils/hooks";
+import { browser } from "@web/core/browser/browser";
+
+import { CheckBox } from "@web/core/checkbox/checkbox";
+import { Dialog } from "@web/core/dialog/dialog";
 
 import { DashboardItem } from "./dashboard_item/dashboard_item";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
-    static components = { Layout, DashboardItem }
+    static components = { Layout, DashboardItem };
 
     setup() {
         this.action = useService("action");
         this.rpc = useService("rpc");
-        this.statistics = useState(useService('statistics'));
-        this.dashboardItems = registry.category('awesome_dashboard').getAll();
-
+        this.statistics = useState(useService("statistics"));
+        this.dialog = useService("dialog");
+        this.dashboardItems = registry.category("awesome_dashboard").getAll();
+        this.state = useState({
+            visibleItems: browser.localStorage.getItem("visibleItems") || [],
+        });
+        if (!this.state.visibleItems.length) {
+            this.state.visibleItems = this.dashboardItems.map((item) => item.id);
+            browser.localStorage.setItem("visibleItems", this.state.visibleItems);
+        }
     }
 
     openCustomers() {
@@ -25,15 +36,53 @@ class AwesomeDashboard extends Component {
 
     openLeads() {
         this.action.doAction({
-            type: 'ir.actions.act_window',
-            name: 'CRM Leads',
-            target: 'current',
-            res_model: 'crm.lead',
+            type: "ir.actions.act_window",
+            name: "CRM Leads",
+            target: "current",
+            res_model: "crm.lead",
             views: [
-                [false, 'list'],
-                [false, 'form'],
+                [false, "list"],
+                [false, "form"],
             ],
         });
+    }
+
+    openSettings() {
+        this.dialog.add(SettingsDialog, {
+            items: this.dashboardItems,
+            visibleItems: this.state.visibleItems,
+            onUpdateSettings: this.updateSettings.bind(this),
+        });
+    }
+
+    updateSettings(updatedVisibleItems) {
+        this.state.visibleItems = updatedVisibleItems;
+    }
+}
+
+class SettingsDialog extends Component {
+    static template = "awesome_dashboard.SettingsDialog";
+    static components = { Dialog, CheckBox };
+    static props = ["close", "items", "visibleItems", "onUpdateSettings"];
+
+    setup() {
+        this.items = useState(
+            this.props.items.map((item) => ({
+                ...item,
+                visible: this.props.visibleItems.includes(item.id),
+            }))
+        );
+    }
+    closeDialog() {
+        this.props.close();
+    }
+
+    onChange(ev, settingItem) {
+        settingItem.visible = ev;
+        const updatedItems = this.items.filter((item) => item.visible).map((item) => item.id);
+        browser.localStorage.setItem("visibleItems", updatedItems);
+
+        this.props.onUpdateSettings(updatedItems);
     }
 }
 
