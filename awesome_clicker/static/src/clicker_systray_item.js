0b69a8b40768a67a3dad839b7387aa0ebfb3de59
Einar Andersen (eian) <eian@odoo.com>
2024-04-29 14:49:05 +0200

    [IMP] MTWF Chapter 1 - 2: Counting external clicks

--- awesome_clicker/static/src/clicker_systray_item.js
+++ awesome_clicker/static/src/clicker_systray_item.js
@@ -1,18 +1,19 @@
 /** @odoo-module **/
 
 import { registry } from "@web/core/registry";
-import { Component, useState } from "@odoo/owl";
+import { Component, useExternalListener, useState } from "@odoo/owl";
 
 class ClickerSystrayItem extends Component {
-   static template = "awesome_clicker.ClickerSystrayItem" 
+    static template = "awesome_clicker.ClickerSystrayItem"
 
-   setup (){
-    this.state = useState({score: 0})
-   }
+    setup() {
+        this.state = useState({ score: 0 })
+        useExternalListener(document.body, "click", () => this.state.score++, { capture: true });
+    }
 
-   incrementScore(){
-    this.state.score++;
-   }
+    incrementScore() {
+        this.state.score += 9;
+    }
 }
 
 registry.category("systray").add("awesome_clicker.ClickerSystrayItem", {
