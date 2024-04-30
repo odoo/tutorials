af55fef47a5c9115870392ac3614c6fa43eb6a87
UxioMendez <uxio.mendez@hotmail.com>
2024-04-30 12:36:28 +0200

    modificado real state

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,4 +1,7 @@
 {'name': 'Estate',
+ 'category': 'Real Estate',
+ 'summary': 'Cosas de inmobiliaria',
+ 'website': 'https://www.odoo.com/page/estate',
  'depends': 'base',
  'application': True,
  }
