1a4b767f3405b6487a0e6f67f96ca2a37e004179
UxioMendez <uxio.mendez@hotmail.com>
2024-04-30 12:50:21 +0200

    solo nombre

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,7 +1,4 @@
 {'name': 'Estate',
- 'category': 'Real Estate',
- 'summary': 'Cosas de inmobiliaria',
- 'website': 'https://www.odoo.com/page/estate',
  'depends': 'base',
  'application': True,
  }
