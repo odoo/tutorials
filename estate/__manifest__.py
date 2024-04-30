227324df86da97a92b97269add37b90374e84f44
UxioMendez <uxio.mendez@hotmail.com>
2024-04-30 20:17:39 +0200

    menus

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,5 +2,6 @@
  'depends': ['base_setup'],
  'application': True,
  'data':['security/ir.model.access.csv',
-         'views/ir.actions.act_window'],
+         'views/ir.actions.act_window',
+         'views/estate_mode_action',],
  }
