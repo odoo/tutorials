2e78f9d26852d79c957802947518c3868beaa414
UxioMendez <Uxio.Mendez@hotmail.com>
2024-05-06 14:00:56 +0200

    Revisar vistas

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,8 @@
 {'name': 'Real Estate',
  'depends': ['base_setup'],
  'application': True,
- 'Data': ['security/ir.model.access.csv']
+ 'Data': ['security/ir.model.access.csv',
+          'views/estate_property_views.xml',
+          'views/estate_menus.xml',]
 
  }
