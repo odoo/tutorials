7e44ffdddf34f9a91dbf8dd27eedb97dfc5d7a3a
UxioMendez <uxio.mendez@hotmail.com>
2024-05-10 13:50:00 +0200

    Se modifica la validez inversamente

--- models/estate_property_offer.py
+++ models/estate_property_offer.py
@@ -15,16 +15,15 @@ class Offer(models.Model):
                                          ('refused', 'Rechazada')], readonly=True)
     partner_id = fields.Many2one('res.partner', string='Partner', required='True')
     property_id = fields.Many2one('estate.propiedad', string='ID Propiedad', required=True)
-    date_deadline = fields.Date(compute="_compute_date_deadline", string="Fecha límite")
-    creation_date = fields.Date(compute="_compute_create_date")
-    validity = fields.Integer('Validez', default=7)
-
-    @api.depends('creation_date')
-    def _compute_create_date(self):
-        for record in self:
-            record.creation_date = record.create_date
+    validity = fields.Integer(string='Validez (días)', default=7)
+    date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_validity", string="Fecha límite")
 
     @api.depends('date_deadline')
     def _compute_date_deadline(self):
         for record in self:
-            record.date_deadline = record.creation_date + timedelta(days=record.validity)
+            record.date_deadline = record.create_date + timedelta(days=record.validity)
+
+    def _inverse_validity(self):
+        for record in self:
+            days = record.date_deadline - record.create_date.date()
+            record.validity = days.days
