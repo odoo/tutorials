fa5f31431a0318ffc259d17507cf75a6ac82fabb
myah <myah@odoo.com>
2024-05-22 19:21:27 +0300

    [ADD] estate: init estate app (ch2)

---
+++ estate/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Mohamed Yousef",
+    'category': 'Sales',
+    'description': """
+    A Real Estate App to ....
+    """,
+    # data files always loaded at installation
+    # 'data': [
+    #     'views/mymodule_view.xml',
+    # ],
+    # data files containing optionally loaded demonstration data
+    # 'demo': [
+    #     'demo/demo_data.xml',
+    # ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    "license": "LGPL-3"
+}
