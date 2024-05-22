73a2a29cbe550e54752350672c9ba7ce6fedd185
“Victorien <viso@odoo.com>
2024-05-22 16:10:12 +0200

    [IMP] estate: create basic view for the estate.property model as part of the completion of onboarding 101, chapter 5, added views for estate.property model, along with extra fields state and active.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,6 +4,8 @@
     'application': True,
     'depends': ['base'],
     'data': [
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
         'security/ir.model.access.csv',
     ]
 }
--- estate/models/estate_propery.py
+++ estate/models/estate_propery.py
@@ -8,10 +8,10 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date('Available From')
+    date_availability = fields.Date('Available From', copy=False, default=fields.Date.add(fields.Date.today(), months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -19,4 +19,17 @@ class EstateProperty(models.Model):
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        selection=[
+            ('new', 'New'),
+            ('offer_received','Offer Received'),
+            ('offer_acepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('canceled', 'Canceled')
+        ],
+        required=True,
+        copy=False,
+        default='new'
     )
