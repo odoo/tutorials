98f651be62829bb01318ac190448daa4e9e3bfaa
Lucas Thomassin <luth@odoo.com>
2024-05-22 13:18:31 +0200

    [ADD] estate: created UI menu items & enhanced model fields (chap5)

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -12,6 +12,9 @@
     'depends': ['base'],
     'data': [
         'security/ir.model.access.csv',
+
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ],
     'license': 'AGPL-3'
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+from dateutil.utils import today
+
+from odoo.tools.date_utils import add
 
 from odoo import fields, models
 
@@ -6,15 +9,23 @@ from odoo import fields, models
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Estate base property model"
-    _order = "sequence"
 
     name = fields.Char(required=True)
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        selection=[("new", "New"), ("offer_received", "Offer Received"), ("offer_accepted", "Offer Accepted"),
+                   ("sold", "Sold"), ("canceled", "Canceled")],
+        default="new",
+        required=True,
+        copy=False
+    )
+
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=add(today(), months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
