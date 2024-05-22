652fcbabc875b43a81152dfed2eb3f3a76def752
jaeschwa <julien.schwanen99@gmail.com>
2024-05-22 16:01:21 +0200

    [IMP] estate: added model fields and views for menu and action

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,6 +7,8 @@
     "category": "Sales",
     "data": [
         "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
     ],
     "license": "LGPL-3",
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -4,12 +4,18 @@ from odoo import fields, models
 class estate_property(models.Model):
 
     _name = "estate.property"
-    _description = "The estate property model"
-    name = fields.Char(required=True)
+    _description = "The estate property"
+    name = fields.Char("Property name", required=True)
+    description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    active = fields.Boolean(default=True)
+    date_availability = fields.Date(
+        default=fields.Date.add(fields.Date.today(), months=3)
+    )
+
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
+    selling_price = fields.Float(readonly=True)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -24,4 +30,17 @@ class estate_property(models.Model):
             ("east", "East"),
         ],
         help="Type is used to separate Leads and Opportunities",
+    )
+    state = fields.Selection(
+        selection=[
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("canceled", "Canceled"),
+        ],
+        string="State",
+        required=True,
+        copy=False,
+        default="new",
     )
