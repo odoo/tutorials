a43731a75028f152f5b3a763cf013b41aee80b2e
Louis Travaux <lotr@odoo.com>
2024-05-22 11:31:35 +0200

    [ADD] estate: added EstateProperty model

---
+++ estate/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Louis Travaux",
+    'category': 'Category',
+    'description': """
+    Description text
+    """,
+    "installable": True,
+    "application": True, 
+    # # data files always loaded at installation
+    # 'data': [
+    #     'views/mymodule_view.xml',
+    # ],
+    # # data files containing optionally loaded demonstration data
+    # 'demo': [
+    #     'demo/demo_data.xml',
+    # ],
+}
---
+++ estate/models/estate_property.py
@@ -0,0 +1,20 @@
+from odoo import fields, model
+
+class EstateProperty(models.Model):
+    _name = "estate_property"
+    _description = "description"
+
+    name = fields.Char()
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float()
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection()
+
