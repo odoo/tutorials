e1dbd843f999b3493329a9c5935797ae5714e243
jaeschwa <julien.schwanen99@gmail.com>
2024-05-22 13:16:15 +0200

    [FIX] estate : fixed the style and added license key in manifest

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': "estate",
-    'depends': ['base'],
-    'installable': True,
-    'application': True,
-    'version': '1.0',
-    'category': 'Sales',
-        'data': [
-        'security/ir.model.access.csv',]
+    "name": "estate",
+    "depends": ["base"],
+    "installable": True,
+    "application": True,
+    "version": "1.0",
+    "category": "Sales",
+    "data": [
+        "security/ir.model.access.csv",
+    ],
+    "license": "LGPL-3",
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,7 +1,8 @@
 from odoo import fields, models
 
+
 class estate_property(models.Model):
-    
+
     _name = "estate.property"
     _description = "The estate property model"
     name = fields.Char(required=True)
@@ -15,6 +16,12 @@ class estate_property(models.Model):
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
-        string='Type',
-        selection=[('north', 'North'), ('south', 'South'), ('west', 'West'), ('east', 'East')],
-        help="Type is used to separate Leads and Opportunities")
+        string="Type",
+        selection=[
+            ("north", "North"),
+            ("south", "South"),
+            ("west", "West"),
+            ("east", "East"),
+        ],
+        help="Type is used to separate Leads and Opportunities",
+    )
