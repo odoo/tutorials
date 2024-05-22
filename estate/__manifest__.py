e5e6f4b37f8691dc2d036e82ef963010958ae079
Lucas Thomassin <luth@odoo.com>
2024-05-22 11:25:42 +0200

    [ADD] estate: module (APP) creation (chap2)

---
+++ estate/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Real Estate",
+    'summary': """
+        Real Estate app to discover Odoo
+    """,
+    'description': """
+        Real Estate app to discover Odoo
+    """,
+    'application': True,
+    'installable': True,
+    'depends': ['base'],
+}
