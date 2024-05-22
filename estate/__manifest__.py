af6b499b319f0253390b2c3d7d94f4ca9ae2eefd
Louis Travaux <lotr@odoo.com>
2024-05-22 12:54:00 +0200

    [FIX] estate: patched garden orientation selection

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,14 +1,14 @@
 {
-    'name': "Real Estate",
-    'version': '1.0',
-    'depends': ['base'],
-    'author': "Louis Travaux",
-    'category': 'Category',
-    'description': """
+    "name": "Real Estate",
+    "version": "1.0",
+    "depends": ["base"],
+    "author": "Louis Travaux",
+    "category": "Category",
+    "description": """
     Description text
     """,
     "installable": True,
-    "application": True, 
+    "application": True,
     # # data files always loaded at installation
     # 'data': [
     #     'views/mymodule_view.xml',
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,14 +1,15 @@
-from odoo import fields, model
+from odoo import fields, models
+
 
 class EstateProperty(models.Model):
-    _name = "estate_property"
+    _name = "estate.property"
     _description = "description"
 
-    name = fields.Char()
-    description = fields.Text()
-    postcode = fields.Char()
+    name = fields.Char("Property Name", required=True)
+    description = fields.Text("Description")
+    postcode = fields.Char("Postcode")
     date_availability = fields.Date()
-    expected_price = fields.Float()
+    expected_price = fields.Float(required=True)
     selling_price = fields.Float()
     bedrooms = fields.Integer()
     living_area = fields.Integer()
@@ -16,5 +17,8 @@ class EstateProperty(models.Model):
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
-    garden_orientation = fields.Selection()
-
+    garden_orientation = fields.Selection(
+        string="Garden orientation",
+        selection=["North", "South", "East", "West"],
+        help="Garden orientation is used to describe orientation of garden",
+    )
