d8e0dd6b9e5f06cad8b267b945a4bbcc9574683b
“Victorien <viso@odoo.com>
2024-05-23 14:46:30 +0200

    [IMP] estate: add validity and deadline to offers As part of chapter 8 of server framework 101. added deadline and validity to offers model. all changes in commit reflect this new functionality.

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class EstatePropertyOffer(models.Model):
@@ -15,3 +15,21 @@ class EstatePropertyOffer(models.Model):
     )
     partner_id = fields.Many2one('res.partner', string="Partner", required=True)
     property_id = fields.Many2one('estate.property', required=True)
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_date_deadline", string="Deadline")
+
+    @api.depends("create_date", "validity")
+    def _compute_date_deadline(self):
+        for offer in self:
+            tmp_date = fields.Date.today()
+            if offer.create_date:
+                tmp_date = offer.create_date
+            
+            offer.date_deadline = fields.Date.add(tmp_date, days=offer.validity)
+
+    def _inverse_date_deadline(self):
+        for offer in self:
+            start_date = fields.Date.from_string(offer.create_date)
+            end_date = fields.Date.from_string(offer.date_deadline)
+            difference = (end_date - start_date).days
+            offer.validity = difference
