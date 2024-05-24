8d1631a36aa7142cc603145282cbe77f09b11b9f
“Victorien <viso@odoo.com>
2024-05-24 12:47:07 +0200

    [IMP] estate: add list of properties to house type view This commit adds property list to house type view. 1st part of Chapter 11 of server framework 101.

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -6,6 +6,7 @@ class EstatePropertyType(models.Model):
     _description = "Type of real estate property"
 
     name = fields.Char(required=True)
+    property_ids = fields.One2many('estate.property', 'property_type_id')
 
     _sql_constraints = [
         ('unique_name', 'unique(name)', 'Property types must be unique')
