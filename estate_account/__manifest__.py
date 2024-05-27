1c2011c7a281aa6d46031de5ccd74b489b5c9869
Louis Travaux <lotr@odoo.com>
2024-05-27 09:48:12 +0200

    [ADD] estate_account: Interact with other modules

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': "Real Estate Account link",
+    'version': "1.0",
+    'depends': ["estate", "account"],
+    'author': "lotr following Odoo tutos",
+    'category': "Category",
+    'description': """
+    Description text
+    """,
+    'installable': True,
+    'license': "LGPL-3",
+    # data files always loaded at installation
+    'data': [
+        "security/ir.model.access.csv",
+    ],
+    # # data files containing optionally loaded demonstration data
+    # 'demo': [
+    #     'demo/demo_data.xml',
+    # ],
+}
---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,32 @@
+from odoo import Command, models
+
+
+class EstateProperty(models.Model):
+    _inherit = 'estate.property'
+
+    def action_sold(self):
+        move = self.env['account.move'].create(
+            {
+                'partner_id': self.partner_id,
+                'move_type': 'out_invoice',
+                'line_ids': [
+                    Command.create(
+                        {
+                            'name': 'Commission',
+                            'quantity': 1,
+                            'price_unit': 0.06
+                            * self.offer_ids.filtered(lambda o: o.status == 'accepted').price,
+                        }
+                    ),
+                    Command.create(
+                        {
+                            'name': 'Administrative fee',
+                            'quantity': 1,
+                            'price_unit': 100,
+                        }
+                    ),
+                ],
+            }
+        )
+
+        return super().action_sold()
