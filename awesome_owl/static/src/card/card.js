fb0887cb267d51967d8bb28a59c74c13ca4c45d5
“Abdessamad” <abdessamad.zouitenn@gmail.com>
2024-05-27 13:41:11 +0200

    [ADD] awesome_owl: Chapter 1 part 6

---
+++ awesome_owl/static/src/card/card.js
@@ -0,0 +1,11 @@
+/** @odoo-module **/
+
+import { Component } from "@odoo/owl";
+
+export class Card extends Component {
+    static template = "awesome_owl.card";
+    static props = {
+        num: Number,
+        content: String,
+    };
+}
--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,7 +1,17 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
-
+import {Component, markup, useState} from "@odoo/owl"
+import {Counter} from "./counter/counter";
+import {Card} from "./card/card";
 export class Playground extends Component {
     static template = "awesome_owl.playground";
+    static components = {Counter,Card};
+    setup(){
+        this.count=useState({value:0});
+        this.cnt1 = "<b>content1</b>";
+        this.cnt2 = markup("<i>content2</i>");
+    }
+    incre(){
+        this.count.value++;
+    }
 }
