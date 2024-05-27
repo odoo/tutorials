e3e0b7ebd7b20a58ecac102daa33fefa867860f9
“Victorien <viso@odoo.com>
2024-05-27 09:31:14 +0200

    [ADD] estate_account: create empty shell for new module A new module estate_account was created. this will make a connection between the module estate and the invoicing modules. The goal is to automate the creation of invoices from the estate app when a property gets sold.

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'estate_account',
+    'description': """
+    Links the estate and account modules. This will allow auto creation of
+    invoices directly from the estate app.
+    """,
+    'license': 'LGPL-3',
+    'application': False,
+    'depends': ['base',
+    'estate',
+    'account',
+    ],
+    'data': [
+    ]
+}
