5d0b367d6a8728926419870bf181c8e5b3334f3a
“Victorien <viso@odoo.com>
2024-05-28 16:55:53 +0200

    [IMP] awesome_dashboard: complete steps 1 and 2 of js tuto chapter 2

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -2,9 +2,29 @@
 
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
+    static components = { Layout };
+
+    setup() {
+        this.action = useService("action");
+    }
+    showCustomers() {
+        this.action.doAction("base.action_partner_form");
+    }
+
+    showLeads(){
+        this.action.doAction({
+            type: 'ir.actions.act_window',
+            name: 'Leads',
+            res_model: 'crm.lead',
+            views: [[false, 'tree'],[false, 'form']],
+        });
+    }
+
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
