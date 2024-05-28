a0a3978e9c5b9680ed5ea5176ec115c50306c732
seraphin-ndibwami <sndibwami@virunga.org>
2024-05-28 14:10:23 +0200

    setup of estate module

---
+++ estate/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "estate",
+    'depends': ['base'],
+    'author': "sndibwami@virunga.org",
+    'application': True,
+    'installable': True,
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'data': [
+        'security/res_groups.xml',
+    ],
+
+}
