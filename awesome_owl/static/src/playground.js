956e4ac74846a0d2dc686e6f5a56bdb9157f2414
myah <myah@odoo.com>
2024-05-31 15:22:48 +0300

    [IMP] awesome_owl: sum of two Counters (ch1.6)

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import { Component, markup } from "@odoo/owl";
+import { Component, markup, useState } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
 
@@ -10,5 +10,10 @@ export class Playground extends Component {
 
     setup() {
         this.str1 = markup("<div> some content</div>");
+        this.sum = useState({ value: 0})
+    }
+
+    increment(){
+        this.sum.value++;
     }
 }
