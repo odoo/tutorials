74decdb0debf6926e7dbb10fd137f8ffbf8dec5d
Alice Gaudon <agau@odoo.com>
2024-06-03 11:42:29 +0200

    [ADD] awesome_clicker: capture clicks from everywhere
    - clicks on the counter button are worth 10
    - other clicks are worth 1

--- awesome_clicker/static/src/components/clicker_systray_item.js
+++ awesome_clicker/static/src/components/clicker_systray_item.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import { Component, useState } from "@odoo/owl";
+import { Component, useExternalListener, useRef, useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 
 export class ClickerSystrayItem extends Component {
@@ -8,10 +8,19 @@ export class ClickerSystrayItem extends Component {
 
     setup() {
         this.state = useState({ counter: 0 });
+        this.counterButtonRef = useRef('awesome_clicker.counter_button');
+
+        useExternalListener(document.body, 'click', this.onClick, { capture: true });
     }
 
-    increment() {
+    onClick(event) {
+        if (this.counterButtonRef.el.contains(event.target)) return;
+
         this.state.counter++;
+    }
+
+    onButtonClick() {
+        this.state.counter += 10;
     }
 }
 
