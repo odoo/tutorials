bc72f521769b1790ad1fa6ce16677e5cb55ae158
bary <yoannbaron@orange.fr>
2024-06-24 16:17:20 +0200

    awesome clicker part 8 complete

--- awesome_clicker/static/src/clicker_service/clicker_service.js
+++ awesome_clicker/static/src/clicker_service/clicker_service.js
@@ -7,12 +7,17 @@ import { reactive } from "@odoo/owl";
 const clickerService = {
     start(env) {
 
-        const state = reactive({ clicks: 0 });
-        
+        const state = reactive({ clicks: 990, level: 1, clickBots: 0 });
+
         return {
             state,
             increment(inc) {
-               state.clicks += inc
+               state.clicks += inc;
+            },
+            buyClickbot() {
+                state.clickBots++;
+                state.clicks -= 1000;
+                setInterval(() => state.clicks += 10, 10*1000);
             }
          };
     }
