1943869c0ab513f8ecc81d15891e355bbd4a12e1
StartingFrom101 <nguyen.bb.minh@gmail.com>
2024-06-26 10:16:13 +0700

    Chapter 8 tutorial

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from attr import field
+from dateutil.relativedelta import relativedelta
 from odoo import models, fields, tools, api
 
 class PropertyOffer(models.Model):    
@@ -12,15 +12,19 @@ class PropertyOffer(models.Model):
     partner_id = fields.Many2one('res.partner', required=True)
     property_id = fields.Many2one('estate.property', required=True)
     
+    
     # Calculate
     validity = fields.Integer(default=7)
-    date_deadline = fields.Date(compute='_calc_date_deadline', inverse="_calc_inverse_date_deadline")
-    @api.depends("validity")
-    def _calc_date_deadline(self):
-        for offer in self:
-            offer.date_deadline = fields.Date.add(offer.create_date, days=offer.validity)
+    date_deadline = fields.Date(compute='_compute_date_deadline', inverse="_inverse_date_deadline")
     
-    # @api.depends("date_deadline")
-    # def _calc_inverse_date_deadline(self):
-        # for offer in self:
-            # offer.validity = 
+    @api.depends("create_date", "validity")
+    def _compute_date_deadline(self):
+        for offer in self:
+            date = offer.create_date.date() if offer.create_date else fields.Date.today()
+            offer.date_deadline = date + relativedelta(days=offer.validity) 
+
+    def _inverse_date_deadline(self):
+        for offer in self:
+            date = offer.create_date.date() if offer.create_date else fields.Date.today()
+            offer.validity = (offer.date_deadline - date).days
+            
