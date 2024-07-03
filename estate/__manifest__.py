5bbedf8ca962244c36dedca02e433535a4c419f6
Parthkumar P <parp@odoo.com>
2024-07-03 12:03:32 +0530

    [ADD] estate: implementation of new Module
    This commit implements a new module for Real Estate

---
+++ estate/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Odoo",
+    'category': 'Category',
+    'summary': """
+        Real Estate Summary
+    """,
+    'description': """
+        Description text
+    """,
+    # data files always loaded at installation
+    # 'data': [
+    # ],
+    # # data files containing optionally loaded demonstration data
+    # 'demo': [
+    # ],
+    'license': 'AGPL-3',
+    'application': True
+}
