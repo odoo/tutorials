2a079e0a8bcf8bf23397eb84adf1bc525a5d5c61
Nidhi Adhvaryu <niad@odoo.com>
2024-07-03 17:57:42 +0530

    [IMP] estate: Implemention of security functionality to Estate module
    Created ir.model.access.csv file in the security folder with read,write,create and unlink permissions to the group base.group_user

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,10 +1,11 @@
 {
-    'name': "Real Estate",
-    'version': '1.0',
-    'depends': ['base'],
-    'author': "NIAD",
-    'category': 'Category',
-    'description': "",
-    'application': True,
-    'license': 'AGPL-3'
+    "name": "Real Estate",
+    "version": "1.0",
+    "depends": ["base"],
+    "author": "NIAD",
+    "category": "Category",
+    "description": "",
+    "application": True,
+    "license": "AGPL-3",
+    "data": ["security/ir.model.access.csv"],
 }
