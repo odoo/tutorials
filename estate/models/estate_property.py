320327c32e93b6afe355bf9135d09454045a8508
Parthkumar P <parp@odoo.com>
2024-07-03 15:42:24 +0530

    [IMP] estate: implementation of model and fields
    implement estate_property model and related fields in estate module

---
+++ estate/models/estate_property.py
@@ -0,0 +1,28 @@
+
+from odoo import fields, models
+
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Estate Property Model"
+
+    name = fields.Char('Name', required=True)
+    description = fields.Text('Description')
+    postcode = fields.Char('Postcode')
+    date_availability = fields.Date('Availability Date')
+    expected_price = fields.Float('Expected Price', required=True)
+    selling_price = fields.Float('Selling Price')
+    bedrooms = fields.Integer('Bedrooms')
+    living_area = fields.Integer('Leaving Area')
+    facades = fields.Integer('Facade')
+    garage = fields.Boolean('Garage')
+    garden = fields.Boolean('Garden')
+    garden_area = fields.Integer('Garden Area')
+    garden_orientation = fields.Selection(
+        string='Garden Orientation',
+        selection=[
+            ('north', 'North'),
+            ('south', 'South'),
+            ('east', 'East'),
+            ('west', 'West'),
+        ])
