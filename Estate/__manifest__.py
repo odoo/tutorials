a6cde2a3cc278db38537d202b68ae7419b723f10
Vansh Thakkar <vath@odoo.com>
2024-07-03 16:06:47 +0530

    [ADD] Estate : implementation of estate modules
    This is estate modules

---
+++ Estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Vansh",
+    'category': 'estate',
+    'description': """Find Your property Here""",
+    'application': True,
+    'installable': True,
+    'license': "AGPL-3"
+}
