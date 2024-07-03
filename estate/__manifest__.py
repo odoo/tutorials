fbafd217bcea1084862dcf46e8ab0635952e4189
Dhruv Patel <dhru@odoo.com>
2024-07-03 12:03:33 +0530

    [ADD] estate: implementation of new module
    - implementation of new real estate module
    - implementation of fields and models

---
+++ estate/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "real-estate",
+    'version': '1.0',
+    'depends': ["base"],
+    'author': "Dhruv",
+    'category': 'Tutorials/estate',
+    'application': True,
+    'installable': True,
+    'description': """
+    Module for the practice and getting knowledge in the technicality
+    """,
+    'data': [],
+    'demo': [],
+    'license': 'AGPL-3',
+}
---
+++ estate/models/estate_properties_plan.py
@@ -0,0 +1,23 @@
+from odoo import fields, models
+
+
+class propertiesPlan(models.Model):
+    _name = "estate.property"
+    _description = "Real Estate Properties Plan"
+
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        string='Type',
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
+        help="Select the direction to filter the options")
