6e0b52f37904986dab8d96056ab1f1a9e2d96e4d
Nidhi Adhvaryu <niad@odoo.com>
2024-07-03 12:03:33 +0530

    [ADD] estate: Creation of New Module
    This commit implements a new module for real estate.

---
+++ estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "NIAD",
+    'category': 'Category',
+    'description': "",
+    'application': True,
+    'license': 'AGPL-3'
+}
