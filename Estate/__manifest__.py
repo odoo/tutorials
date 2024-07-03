f36eacbd05f31b915782a02e23d8a9d1884f85f9
Sourabh P <sopa@odoo.com>
2024-07-03 12:03:28 +0530

    [ADD] sopa-estate:implementation of new model
    - implementation of new real estate module
    -implementation of fields and models

---
+++ Estate/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Estate",
+
+    'description': """
+        sell and manage your property
+    """,
+
+    'author': "Sourabh",
+    'category': 'Tutorials/Estate',
+    'version': '0.1',
+    'application': True,
+    'installable': True,
+    'depends': ["base"],
+
+    'data': [],
+    'demo': [],
+
+    'license': 'AGPL-3'
+}
