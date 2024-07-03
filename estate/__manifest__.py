13889c63db8e357bfb7d08b1dca22d8307c70e09
Karan <ksku@odoo.com>
2024-07-03 12:03:30 +0530

    [ADD] estate: implementation of new module
    This commit implements a new module for real estate.

---
+++ estate/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Real Estate App",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "KSKU",
+    'category': 'Category',
+    'description': """
+    First odoo app
+    """,
+    # data files always loaded at installation
+    'data': [
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'AGPL-3',
+}
