da4f5c11ffdc96e0e1b97777c9b86c3996e2e2b0
Gaurav Jangid <gaja@odoo.com>
2024-07-03 12:03:34 +0530

    [ADD] estate: Implementation of new module
    - Implementation of new estate module
    - Implementation of fields and records.
    - Implementation of Access rules.

---
+++ estate/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "GAJA",
+    'category': 'Category',
+    'description': """
+    Hell Hoo
+    """,
+    # data files always loaded at installation
+    'data': [
+        'security/ir.model.access.csv'
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+    ],
+    'application': True,
+    'license': 'AGPL-3',
+}
