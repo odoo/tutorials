fd05a0e0ce1ac4f919f341c20292551d518041b7
Ashutosh Y <asya@odoo.com>
2024-07-03 12:03:35 +0530

    [ADD] estate: implementation of new module
    - Implementation of New Module
    - Implementation of fields and Models.

---
+++ estate/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Ashutosh Yadav",
+    'category': 'Real Estate',
+    'description': """
+    Description text
+    """,
+    # data files always loaded at installation
+    'data': [
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+    ],
+    'application': 'true',
+    'license': 'AGPL-3',
+}
