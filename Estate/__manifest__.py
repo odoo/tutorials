0d6c306d7e7f37aa8c8a3712e7d00405db7ef1b2
Vansh Thakkar <vath@odoo.com>
2024-07-04 12:39:33 +0530

    [IMP] estate : Created form and added fields in that form
    creted estate_property_views.xml and estate_property_menus.xml files
    created menuitems and added different fields in estate form

--- Estate/__manifest__.py
+++ Estate/__manifest__.py
@@ -7,6 +7,8 @@
     'description': """Find Your property Here""",
     'data': [
         'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
     ],
     'application': True,
     'installable': True,
--- Estate/models/estate_property.py
+++ Estate/models/estate_property.py
@@ -1,23 +1,32 @@
 from odoo import models, fields
+from datetime import date, timedelta
 
 
 class Testing(models.Model):
-    _name = "estate_property"
+    _name = "estate.property"
     _description = "This is Real Estate"
 
-    name = fields.Char('prop_Name', required=True, translate=True)
+    name = fields.Char('property_Name', required=True)
     description = fields.Text('Description')
     postcode = fields.Char('Postcode')
-    date_availability = fields.Date('Date_avail')
+    date_availability = fields.Date('Date_avail', copy=False, default=lambda self: date.today() + timedelta(days=90))
     expected_price = fields.Float('exp_price', required=True)
-    selling_price = fields.Float('sell_price')
-    bedrooms = fields.Integer('bedrooms')
+    selling_price = fields.Float('sell_price', readonly=True, copy=False)
+    bedrooms = fields.Integer('bedrooms', default=2)
     living_area = fields.Integer('live_area')
     facades = fields.Integer('facades')
     garage = fields.Boolean('garage')
     garden = fields.Boolean('garden')
-    garden_area = fields.Integer('gar_area')
+    garden_area = fields.Integer('garden_area')
     garden_orientation = fields.Selection(
-        string='Type',
+        string='Garden direction',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
     )
+    state = fields.Selection(
+        string='state',
+        selection=[('new', 'New'), ('offer received', 'Offer Received'), ('offer accepted', 'Offer Accepted'), ('sold', 'Sold'), ('canceled', 'Canceled')],
+        required=True,
+        default='new',
+        copy=False
+    )
+    active = fields.Boolean(default=True)
