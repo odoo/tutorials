40749948c3d3704decd4e2d0cbb3c3ab99e29361
krishil modh <krmo@odoo.com>
2024-07-04 12:20:19 +0530

    [IMP] estate: added actions and menus for Real Estate module:
    - Created the estate_property_views.xml & estate_menus.xml files

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,6 +6,8 @@
     'depends': ['base', 'web'],
     'data': [
         'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ],
     'installable': True,
     'application': True,
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta
 
 
 class EstateProperty(models.Model):
@@ -8,16 +9,25 @@ class EstateProperty(models.Model):
     name = fields.Char('Property Name', required=True)
     description = fields.Text('Description')
     postcode = fields.Char('Postcode')
-    date_availability = fields.Date('Availability', readonly=True)
+    date_availability = fields.Date(default=lambda self: fields.Date.today() + relativedelta(months=3), copy=False)
+    selling_price = fields.Float('Selling Price', readonly=True, copy=False)
+    bedrooms = fields.Integer('Bedrooms', default=2)
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        string='State',
+        selection=[('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('canceled', 'Canceled')],
+        required=True,
+        copy=False,
+        default='new'
+    )
     expected_price = fields.Float('Expected Price', required=True)
-    selling_price = fields.Float('Selling Price')
-    bedrooms = fields.Integer('# Rooms')
-    living_area = fields.Integer('# LivingArea')
+    living_area = fields.Integer('LivingArea')
     facades = fields.Integer('Facades')
     garage = fields.Boolean('Garage')
     garden = fields.Boolean('Garden')
     garden_area = fields.Integer('Garden Area')
     garden_orientation = fields.Selection(
         string='Garden Orientation',
-        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West'), ('select', 'Select')],
+        default='select'
     )
