6c33d214ed5a8b659a5b5811ea4782e61ce2ae1c
Ashutosh Y <asya@odoo.com>
2024-07-04 12:58:03 +0530

    [IMP] estate: Implementation of UI - With the help of XML

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -10,10 +10,12 @@
     # data files always loaded at installation
     'data': [
         'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_property.xml'
     ],
     # data files containing optionally loaded demonstration data
     'demo': [
     ],
     'application': 'true',
-    'license': 'AGPL-3',
+    'license': 'AGPL-3'
 }
