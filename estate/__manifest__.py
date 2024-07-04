b7b05c25b62ac48be02b851e3bf21a3af0da769a
Karan <ksku@odoo.com>
2024-07-04 12:25:13 +0530

    [IMP] estate: Menus and actions created with additional fields
    Implementation of 3 menus and actions then created fields such as active , state and add default values in other fields

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -10,6 +10,8 @@
     # data files always loaded at installation
     'data': [
         'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ],
     # data files containing optionally loaded demonstration data
     'demo': [
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,20 +1,25 @@
 from odoo import fields, models
+from datetime import date, timedelta
+
+default_availability_date = (date.today() + timedelta(days=90)).strftime("%Y-%m-%d")
 
 
 class EstateProperty(models.Model):
-    _name = "estate_property"
+    _name = "estate.property"
     _description = "Real Estate properties defined"
 
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=default_availability_date)
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
+    state = fields.Selection(selection=[('new', 'New'), ('offer received', 'Offer Received'), ('offer accepted', 'Offer Accepted'), ('sold and canceled', 'Sold and Canceled')], required=True, copy=False, default='new')
+    active = fields.Boolean(string="Active", default=False)
