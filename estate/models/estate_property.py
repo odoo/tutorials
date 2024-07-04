7836803ab18c78bad3eb4b3341c4a3d872d187b1
Gaurav Jangid <gaja@odoo.com>
2024-07-04 17:22:41 +0530

    [IMP] estate:Implement views creation of list view,form,search view

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -6,18 +6,18 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real_Estate property model"
 
-    name = fields.Char(required=True)
-    description = fields.Text()
-    postcode = fields.Char()
-    date_availability = fields.Date(copy=False, default=fields.Date.today() + relativedelta(months=3))
-    expected_price = fields.Float(required=True)
-    selling_price = fields.Float('selling_price', readonly=True, copy=False)
-    bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
-    facades = fields.Integer()
-    garage = fields.Boolean()
-    garden = fields.Boolean()
-    garden_area = fields.Integer()
+    name = fields.Char('Name', required=True)
+    description = fields.Text('Description')
+    postcode = fields.Char('Postcode')
+    date_availability = fields.Date('Available Data', copy=False, default=fields.Date.today() + relativedelta(months=3))
+    expected_price = fields.Float('Expected Price', required=True)
+    selling_price = fields.Float('Selling price', readonly=True, copy=False)
+    bedrooms = fields.Integer('Bedrooms', default=2)
+    living_area = fields.Integer('Living Area')
+    facades = fields.Integer('Facades')
+    garage = fields.Boolean('Garage')
+    garden = fields.Boolean('Garden')
+    garden_area = fields.Integer('Garden Area')
     garden_orientation = fields.Selection(
         string='Type',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
