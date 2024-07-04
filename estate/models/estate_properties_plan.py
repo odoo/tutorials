ec9893ed5936bfd061fc3a4ca4d72fcee1b8851e
Dhruv Patel <dhru@odoo.com>
2024-07-04 12:00:17 +0530

    [IMP] estate: data_files(XML), actions, menus, fields, attributes and views
    Set p basic form view and make it active so it can be visible in UI.

--- estate/models/estate_properties_plan.py
+++ estate/models/estate_properties_plan.py
@@ -1,4 +1,5 @@
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta
 
 
 class propertiesPlan(models.Model):
@@ -8,16 +9,22 @@ class propertiesPlan(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=fields.Datetime.today() + relativedelta(months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
-        string='Type',
+        string='Garden Orientation',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
         help="Select the direction to filter the options")
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        string='State',
+        selection=[('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('canceled', 'Canceled')],
+        help="Select the state of your property.",
+        default='new')
