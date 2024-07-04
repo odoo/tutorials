8586458041a3d86fdbf4bc3a24e1d779d9e01518
Ashutosh Y <asya@odoo.com>
2024-07-04 18:34:11 +0530

    [IMP] estate: Implementation of UI - Corrected the UI

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -11,7 +11,7 @@
     'data': [
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
-        'views/estate_property.xml'
+        'views/estate_menus.xml'
     ],
     # data files containing optionally loaded demonstration data
     'demo': [
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,21 +1,26 @@
+from datetime import timedelta
 from odoo import fields, models
+
+today = fields.Date.today()
+three_months_later = today + timedelta(days=90).strftime("%Y-%m-%d")
 
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Test Model"
 
-    name = fields.Char(required=True)
-    postcode = fields.Text()
-    date_availability = fields.Date(default='3 months', copy=False)
+    name = fields.Char('Title', required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date('Avaliable From', default=three_months_later, copy=False)
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default='2')
-    living_area = fields.Integer()
+    living_area = fields.Integer('Living Area (sqm)')
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Integer()
-    garden_orientation = fields.Selection(string='Type', selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
-    active = fields.Boolean(default=True)
-    state = fields.Selection(string='Type', selection=[('new', 'New'), ('offer received', 'Offer Received'), ('offer accepted', 'Offer Accepted'), ('sold', 'Sold'), ('canceled', 'Canceled')], copy=False, default='New')
+    garden_area = fields.Integer('Garden Area (sqm)')
+    garden_orientation = fields.Selection(selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
+    state = fields.Selection(string='Status', selection=[('new', 'New'), ('offer received', 'Offer Received'), ('offer accepted', 'Offer Accepted'), ('sold', 'Sold'), ('canceled', 'Canceled')], copy=False, default='new')
+    active = fields.Boolean(string='Active', default=True)
