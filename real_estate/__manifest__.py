0c31e9aa1ba20cc7ba539285262cee9cd5f4ece3
Antoine Vandevenne (anv) <anv@odoo.com>
2024-07-12 13:16:47 +0200

    chap 3 - define actions

--- real_estate/__manifest__.py
+++ real_estate/__manifest__.py
@@ -4,8 +4,9 @@
     'license': 'LGPL-3',
     'depends': ['base'],
     'data': [
+        'actions.xml',
         'ir.model.access.csv',
-        'menus.xml',
+        'menus.xml',  # Depends on `actions.xml`
         'real_estate_property_data.xml',
     ],
     'application': True,
