d33d87e1dfd2a25d1e972c5f5a4b76cecd82800b
Gaurav Jangid <gaja@odoo.com>
2024-07-15 12:27:06 +0530

    [IMP] estate: Implementation of link module and invoice creation.

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Estate Account",
+    'version': '1.0',
+    'depends': ['base', 'estate', 'account'],
+    'author': "GAJA",
+    'category': 'Category',
+    'description': """
+    Hell Hoo
+    """,
+    # data files always loaded at installation
+    'data': [
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+    ],
+    'application': True,
+    'license': 'AGPL-3',
+}
---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,27 @@
+from odoo import models, Command
+
+
+class InheritedModel(models.Model):
+    _inherit = "estate.property"
+
+    def action_set_sold(self):
+        invoice_vals = {
+            'move_type': 'out_invoice',
+            'partner_id': self.user_id.id,
+            'invoice_line_ids': [
+                Command.create({
+                    'name': self.name,
+                    'quantity': 1,
+                    'price_unit': self.selling_price * 0.06
+                    }),
+                Command.create({
+                    'name': 'Administrative Fees',
+                    'quantity': 1,
+                    'price_unit': 100
+                    })
+                ]
+            }
+        self.env['account.move'].create(invoice_vals)
+
+        ans = super().action_set_sold()
+        return ans
