0457ff006bf2dd6beb54589552b560f651a4c02a
Karan <ksku@odoo.com>
2024-07-16 12:21:01 +0530

    [FIX] estate: installation error fixed
    Change in the manifest files inorder to get the data in the correct order to solve the error

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -11,9 +11,9 @@
     'data': [
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
+        'views/estate_property_offer_views.xml',
         'views/estate_property_type_views.xml',
         'views/estate_property_tag_views.xml',
-        'views/estate_property_offer_views.xml',
         'views/res_users_views.xml',
         'views/res_config_settings_views.xml',
         'views/estate_menus.xml',
