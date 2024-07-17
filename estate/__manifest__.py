c804bc245e60999d771ee30cc04977ec8825466e
Nidhi Adhvaryu <niad@odoo.com>
2024-07-17 12:07:58 +0530

    [IMP] estate:"Implementation of define module data"
    Data types, Data declaration, CSV, XML,Data extension, Add X2many fields.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -9,6 +9,8 @@
     "license": "AGPL-3",
     "data": [
         "security/ir.model.access.csv",
+        "data/estate.property.type.csv",
+        "data/master_data.xml",
         "views/res_config_settings_views.xml",
         "views/estate_property_views.xml",
         "views/estate_property_offer_views.xml",
@@ -17,6 +19,7 @@
         "views/estate_menus.xml",
         "views/res_users_views.xml",
     ],
+    "demo": ["demo/demo_data.xml"],
     "images": ["static/description/realestate.png"],
     "installable": True,
     "auto_install": False,
