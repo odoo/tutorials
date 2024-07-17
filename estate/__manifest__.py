6ef5a8aa709e844f6149fef44e372ee1a2d9b86a
Gaurav Jangid <gaja@odoo.com>
2024-07-17 16:42:27 +0530

    [IMP] Estate: Add demo data.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -9,6 +9,7 @@
     """,
     # data files always loaded at installation
     'data': [
+        'data/estate.property.type.csv',
         'security/ir.model.access.csv',
         'views/estate_property_tag_views.xml',
         'views/estate_property_offer_views.xml',
@@ -16,10 +17,11 @@
         'views/estate_property_type_views.xml',
         'views/res_users_views.xml',
         'views/estate_menus.xml',
-        'views/res_config_settings_views.xml'
+        'views/res_config_settings_views.xml',
     ],
     # data files containing optionally loaded demonstration data
     'demo': [
+        'demo/estate_demo_data_views.xml'
     ],
     "images": ["static/description/new.png"],
     'application': True,
--- estate/models/res_config_settings.py
+++ estate/models/res_config_settings.py
@@ -4,4 +4,4 @@ from odoo import fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    selling_property = fields.Boolean(string="can we sold", config_parameter="eatate.sold")
+    selling_property = fields.Boolean(string="can we sold", config_parameter="estate.sold")
