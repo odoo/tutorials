9e83cb6c91b7ab41c08d916a5b2e95620b4232b3
Ashutosh Y <asya@odoo.com>
2024-07-17 17:16:08 +0530

    [IMP] estate: Implementaion of Demo data

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -10,6 +10,7 @@
     # data files always loaded at installation
     'data': [
         'security/ir.model.access.csv',
+        'data/template/estate.property.type.csv',
         'views/res_users.xml',
         'views/estate_property_views.xml',
         'views/estate_property_offer_views.xml',
@@ -20,6 +21,7 @@
     ],
     # data files containing optionally loaded demonstration data
     'demo': [
+        'demo/estate_demo.xml',
     ],
     "image": ["static/descriptions/selfie.png"],
     'installable': True,
