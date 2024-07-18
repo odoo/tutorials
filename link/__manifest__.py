f9b714f7859f0cdf976ce97373047cd429588d1c
lucas <lucasxmb800@gmail.com>
2024-07-18 17:44:56 -0300

    Created models and added files with information

---
+++ link/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Link",
+    'version': "13.0.1.0",
+    'summary': "Link Module",
+    'description': "This is a link module",
+    'author': "Lucas Barcelos Meira",
+    'website': "",
+    'category': 'Uncategorized',
+    'version': '13.0.1',
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
