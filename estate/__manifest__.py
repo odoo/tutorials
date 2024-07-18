52ba1ac2369c4e0831b54ce0776cbc9e6a7e4d3b
“Lulu <lugr@odoo.com>
2024-07-18 14:51:01 +0200

    [ADD] estate: add kanban view

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,6 +6,7 @@
              'views/estate_property_views.xml',
              'views/estate_menus.xml',
              'views/estate_search.xml',
+             'views/estate_kanban.xml',
              'views/user_properties.xml',
              ]
 }
