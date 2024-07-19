fe3a2fde0298633c2e3c160d900ed6de1aee3e98
krishil modh <krmo@odoo.com>
2024-07-19 18:23:22 +0530

    [IMP] estate: Implementation of PDF Reports
    - Implemented Template and Sub-Template and Report Inheritance.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,6 +7,10 @@
     "data": [
         "security/security.xml",
         "security/ir.model.access.csv",
+        "report/estate_subtemplate.xml",
+        "report/estate_property_templates.xml",
+        "report/estate_property_reports.xml",
+        'report/estate_property_invoice.xml',
         "views/res_users_views.xml",
         "views/estate_property_views.xml",
         "views/estate_property_offer_views.xml",
