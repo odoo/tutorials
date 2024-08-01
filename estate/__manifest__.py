4f44de43ba0577e14153b7df256f6a86240f816f
amrendra yadav <amya@odoo.com>
2024-08-01 14:44:41 +0530

    [ADD] estate: create estate.property model and initial views
    - Added `estate.property` model
    - Created initial views for the model
    - Added menus under Advertisement -> Property
    - Covered content up to Chapter 5

---
+++ estate/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name':"estate",
+    'depends':['base_setup'],
+    'description':"Technical practice",
+    'installable': True,
+    'application': True,
+
+     'data':[
+         'security/ir.model.access.csv',
+         'views/estate_property_views.xml',
+         'views/estate_menus.xml'
+             ],
+}
---
+++ estate/models/test_model.py
@@ -0,0 +1,23 @@
+from odoo import models, fields
+from dateutil.relativedelta import relativedelta
+from datetime  import date
+class TestModel(models.Model):
+    _name = "estate.property"
+    _description = "Real Estate Property Data"
+    name = fields.Char(required=True) 
+    description=fields.Char()
+    postcode = fields.Char()
+    date_availability = fields.Date(default = date.today()+relativedelta(months=3))
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float()
+    bedrooms = fields.Integer(default=2)
+    living_area =  fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    active= fields.Boolean(default=False)
+    status = fields.Selection(default='new',selection=[('new','New'),('offer_recived','Offer Recieved')])
+    garden_orientation = fields.Selection(selection=[('north','North'),('south','South'),('east','East'),('west','West')])
+
+
