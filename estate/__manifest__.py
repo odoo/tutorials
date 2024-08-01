629dbab1c202ab6d603fd6f106196655c694aade
akya-odoo <akya@odoo.com>
2024-08-01 11:39:21 +0530

    [ADD] estate: Add new real estate property management module

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,3 +1,16 @@
 {
-    "name": "Real Estate"
+    'name': 'Real Estate',
+    'version': '1.0',
+    'summary': 'Manage real estate properties',
+    'description': 'Module to manage real estate properties',
+    'author': 'Akya',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+        
+    ],
+    'installable': True,
+    'application': True,
 }
---
+++ estate/models/estate_property.py
@@ -0,0 +1,31 @@
+from odoo import models, fields
+
+class EstateProperty(models.Model):
+    _name = 'estate.property'
+    _description = 'Real Estate Property'
+
+    Title = fields.Char(required=True)
+    Description = fields.Text()
+    Postcode = fields.Char()
+    Availability_date = fields.Date(copy=False)
+    Expected_price = fields.Float(required=True,default=0.0)
+    Selling_price = fields.Float(string="Selling Price",readonly=True)
+    Bedrooms = fields.Integer(default=2)
+    Living_area = fields.Integer()
+    Facades = fields.Integer()
+    Garage = fields.Boolean()
+    Garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection([
+        ('north', 'North'),
+        ('south', 'South'),
+        ('east', 'East'),
+        ('west', 'West'),
+    ])
+    state = fields.Selection([
+        ('new', 'New'),
+        ('used', 'Used'),
+        
+    ], string='Status', default='draft')
+    active = fields.Boolean(string='Active', default=True)
+
