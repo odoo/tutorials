39a54d969eff604372a0d548b22adaa2cf09a061
yasp-odoo <yasp@odoo.com>
2024-08-02 17:08:39 +0530

    [IMP] estate: added actions, menus, and custom views for estate_property
    After this commit:
    - Added actions and menus to access estate_property model
    - Created custom list, form, and search views
    - Defined default attributes and fields behavior for estate_property model

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -9,6 +9,8 @@
     'depends': ['base'],
     'data': [
         'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ],
     'license': 'LGPL-3',
     'installable': True,
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import models, fields
+from datetime import timedelta
 
 
 class EstateProperty(models.Model):
@@ -8,10 +9,10 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(default=lambda self: fields.Date.today() + timedelta(days=90), copy=False)
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -23,3 +24,11 @@ class EstateProperty(models.Model):
         ('east', 'East'),
         ('west', 'West')
     ])
+    active = fields.Boolean(default=True)
+    state = fields.Selection([
+        ('new', 'New'),
+        ('offer_received', 'Offer Received'),
+        ('offer_accepted', 'Offer Accepted'),
+        ('sold', 'Sold'),
+        ('canceled', 'Canceled')
+    ], required=True, default='new', copy=False)
