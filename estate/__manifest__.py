87a63a87e15c24812f2bd9671288b6e3a004e364
yasp-odoo <yasp@odoo.com>
2024-08-02 11:50:34 +0530

    [ADD] estate: created estate_property
    After this commit:
    -read the file structure
    -created model given in exercise
    -given the access right

---
+++ estate/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'estate',
+    'version': '1.0',
+    'summary': 'Real Estate Advertisement Module',
+    'description': 'A module for managing real estate advertisements.',
+    'category': 'Sales',
+    'author': 'YASP',
+    'website': 'http://www.estatedemo.com',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+}
