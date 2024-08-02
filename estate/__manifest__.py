73886205a5a75c95734f2e93fff337aec7cad659
smee-odoo <smee@odoo.com>
2024-08-02 18:54:25 +0530

    [IMP] estate: modified estate model
    After this commit:
    -added lisr view
    -added form view
    -added search view

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,7 +6,10 @@
         'base_setup',
     ],
     'data': [
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_property_menu.xml',
+        #'views/estate_test.xml'
     ],
     'demo': [
     ],
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,22 +1,32 @@
 from odoo import  models, fields
+from datetime import date, timedelta
+from dateutil.relativedelta import relativedelta
 
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "test"
 
-    name = fields.Char('Name', required=True)
-    postcode = fields.Char('Postcode')
-    date_availability = fields.Date('Date available')
-    expected_price = fields.Float('Expected Price', required=True)
-    selling_price = fields.Float('Selling Price')
-    bedrooms = fields.Integer('Bedroom')
-    living_area = fields.Integer('Living Area')
-    facades = fields.Integer('Facade')
-    garage = fields.Boolean('Garage')
-    garden = fields.Boolean('Garden')
-    garden_area = fields.Integer('Garden Area')
+    name = fields.Char(string='Name', required=True)
+    postcode = fields.Char(string='Postcode')
+    date_availability = fields.Date(string='Date available', copy=False, default=(date.today()+relativedelta(months=3)))
+    expected_price = fields.Float(string='Expected Price', required=True)
+    selling_price = fields.Float(string='Selling Price', readonly=True, copy=False)
+    bedrooms = fields.Integer(string='Bedroom', default=2)
+    living_area = fields.Integer(string='Living Area')
+    facades = fields.Integer(string='Facade')
+    garage = fields.Boolean(string='Garage')
+    garden = fields.Boolean(string='Garden')
+    garden_area = fields.Integer(string='Garden Area')
     garden_orientation = fields.Selection(
         string='Orientation',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
         help="Direction of the garden")
+    active = fields.Boolean(string='Active', default=True)
+    state = fields.Selection(
+        string='State',
+        selection=[('new', 'New'), ('offer received', 'Offer Received'), ('offer accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')],
+        help='state of the proerty right now',
+        required=True,
+        copy=False,
+        default='new')
