39a6abb45927a18a095ac1c46d07cf0ef9296521
amrendra yadav <amya@odoo.com>
2024-08-02 11:38:09 +0530

    [ADD] estate: created tree ,form, search in estate module
    - Created tree ,form  and search in estate module
    - Created to show the smiling_face when property record data not found on
      default search
    - Covered content up to Chapter 6

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,11 +1,11 @@
 {
-    'name':"estate",
-    'depends':['base_setup'],
-    'description':"Technical practice",
+    'name': "estate",
+    'depends': ['base_setup'],
+    'description': "Technical practice",
     'installable': True,
     'application': True,
 
-     'data':[
+     'data': [
          'security/ir.model.access.csv',
          'views/estate_property_views.xml',
          'views/estate_menus.xml'
