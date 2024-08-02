70209e24a6f6af1350c953bf7c11a190ce4b803d
pkgu-odoo <pkgu@odoo.com>
2024-08-02 17:41:26 +0530

    [IMP] estate: add menu and custom view for estate_property
    - Implemented actions and menus for estate_property model
    - Created custom list, form, and search views
    - Defined default attributes and field behaviors

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,7 +6,11 @@
     'category': 'Sales',
     'author': 'pkgu',
     'depends': ['base'],
-    'data': ['security/ir.model.access.csv'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
+    ],
     'installable': True,
     'application': True,
     'license': 'LGPL-3',
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import fields, models
+from datetime import timedelta
 
 
 class EstateProperty(models.Model):
@@ -8,13 +9,30 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char('Postcode')
-    date_availability = fields.Date('Availabilty Date')
+    date_availability = fields.Date(
+        'Availability Date',
+        default=lambda self: fields.Date.today() + timedelta(days=90),
+        copy=False
+    )
     expected_price = fields.Float('Expected Price', required=True)
-    selling_price = fields.Float('Selling Price')
-    bedrooms = fields.Integer('Bedrooms')
-    living_area = fields.Integer('Livingf Area')
-    facades = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer('Bedrooms', default=2)
+    living_area = fields.Integer('Living Area')
+    facades = fields.Integer('Facades of Property')
     garage = fields.Boolean('Garage')
     garden = fields.Boolean('Garden')
-    garden_area = fields.Integer("Garden Area")
-    garden_orientation = fields.Selection([('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
+    garden_area = fields.Integer('Garden Area')
+    garden_orientation = fields.Selection([
+        ('north', 'North'),
+        ('south', 'South'),
+        ('east', 'East'),
+        ('west', 'West')
+    ])
+    active = fields.Boolean(default=True)
+    state = fields.Selection([
+        ('new', 'New'),
+        ('offer_received', 'Offer Received'),
+        ('offer_accepted', 'Offer Accepted'),
+        ('sold', 'Sold'),
+        ('canceled', 'Canceled')
+    ], required=True, default='new', copy=False)
