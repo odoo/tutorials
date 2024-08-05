0e9f87cacd25a270ed84b350b9244b92188d86d6
prgo-odoo <prgo@odoo.com>
2024-08-05 18:05:08 +0530

    [IMP] estate: added new models to the module
    After this commit:
    -Go through the different relations between the models
    -Added property type and property tag model
    -Implemented different relations through the different models

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -10,6 +10,9 @@
     'data': [
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
+        'views/estate_property_types_views.xml',
+        'views/estate_property_tags_views.xml',
+        'views/estate_property_offers_views.xml',
         'views/estate_menus.xml'
     ],
     'installable': True,
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -38,3 +38,8 @@ class EstateProperty(models.Model):
         copy=False,
         default='new'
     )
+    property_type_id = fields.Many2one('estate.property.type', string='Property Type')
+    buyer_id = fields.Many2one('res.partner', string='Buyer')
+    seller_id = fields.Many2one('res.users', string='Salesman', default=lambda self: self.env.user)
+    tag_ids = fields.Many2many('estate.property.tag', string='Tags')
+    offer_ids = fields.One2many('estate.property.offer', 'property_id', string='Offers')
