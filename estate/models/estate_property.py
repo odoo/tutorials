3928be3fde0ba3ad2bff9935ee3c70bbb5aac108
smee-odoo <smee@odoo.com>
2024-08-13 12:06:53 +0530

    [IMP] estate: added python inheritence
    After this commit
    -Added python inheritece

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -114,3 +114,14 @@ class EstateProperty(models.Model):
             result = float_compare(record.selling_price, (record.expected_price * 0.9), precision_digits=2)
             if result == -1:
                 raise ValidationError("Selling price cannot be lower than 90% of the expected price")
+    
+    @api.ondelete(at_uninstall=False)
+    def _unlink_except_new_cancelled(self):
+        for record in self:
+            if record.state not in ['new', 'cancelled']:
+                raise UserError("Cannot delete properties that are not in new or cancelled state")
+        # return super().unlink()
+
+    # @api.models
+    # def create(self, vals):
+
