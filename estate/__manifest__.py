cac1b22404cd50db1756371adf13a3ca8e2fdcef
Fahad Ashfaq <faas@odoo.com>
2024-08-14 01:12:11 +0400

    chapter 5

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,6 +7,8 @@
     'depends': ['base'],
     'data': [
         'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',  
     ],
     'license': 'LGPL-3',
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import fields, models
+import datetime
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -6,10 +7,10 @@ class EstateProperty(models.Model):
     name = fields.Char('Title', required = True)
     description = fields.Text('Description')
     postcode = fields.Char('Postcode')
-    date_availability = fields.Date('Date availability')
+    date_availability = fields.Date('Date availability', copy=False, default = datetime.date.today()+ datetime.timedelta(weeks=12))
     expected_price = fields.Float('Expected Price',required = True)
-    selling_price = fields.Float('Selling Price')
-    bedrooms = fields.Integer('Bedrooms')
+    selling_price = fields.Float('Selling Price', readonly =True, copy = False)
+    bedrooms = fields.Integer('Bedrooms', default = 2)
     living_area = fields.Integer('Living Area')
     facades = fields.Integer('Facades')
     garage = fields.Boolean('Garage')
@@ -19,3 +20,8 @@ class EstateProperty(models.Model):
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
         help="Direction Facing")
     
+    active = fields.Boolean('Active', default = True)
+
+    state = fields.Selection(string='State',
+        selection=[('new', ' New'), ('offer received ', 'Offer Received'), ('offer accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')],
+        help="Property State")
