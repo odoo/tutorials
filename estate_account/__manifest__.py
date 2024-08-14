34e4874a0720eef53f6dcdbd40f3dd39c6b972fe
prgo-odoo <prgo@odoo.com>
2024-08-14 17:51:24 +0530

    [ADD] estate_account: added new estate_account module
    IN this commit:
    -Added new estate_account module depends on estate and account
    -Inherit the estate_property and override action_sold method
    -Implemented invoicing when a property is sold

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Real Estate Accounting',
+    'version': '1.0',
+    'category': 'Accounting',
+    'description': """
+        Real Estate Accounting
+    """,
+    'depends': ['estate', 'account'],
+    'author': 'prgo',
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+}
---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,29 @@
+from odoo import models, Command
+
+
+class EstateProperty(models.Model):
+    _inherit = 'estate.property'
+
+    def action_sold(self):
+        super().action_sold()
+
+        for record in self:
+            selling_price = record.selling_price or 0.0
+            commission_amount = selling_price * 0.06
+            self.env['account.move'].create({
+                'partner_id': record.buyer_id.id,
+                'move_type': 'out_invoice',
+                'invoice_line_ids': [
+                    Command.create({
+                        'name': ('Commission for selling property %s') % record.name,
+                        'quantity': 1,
+                        'price_unit': commission_amount
+                    }),
+                    Command.create({
+                        'name': ('Administrative Fees'),
+                        'quantity': 1,
+                        'price_unit': 100.0
+                    })
+                ],
+            })
+        return True
