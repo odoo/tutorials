feaba0953fcd9ebf22c84d85c7480adb8f6b9e90
smee-odoo <smee@odoo.com>
2024-08-14 12:01:24 +0530

    [IMP] estate: improved formatting
    After this commit
    -formatiing

--- estate/models/res_users.py
+++ estate/models/res_users.py
@@ -4,6 +4,4 @@ from odoo import models, fields
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    property_ids = fields.One2many('estate.property', 'seller_id', 
-    domain=[('state', 'in', ('new', 'offer received'))])
-    
+    property_ids = fields.One2many('estate.property', 'seller_id', domain=[('state', 'in', ('new', 'offer received'))])
