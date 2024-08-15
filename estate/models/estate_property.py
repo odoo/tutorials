fe9e6c871868b6d7c5d8fdd44edb7cf1d5ea6451
Fahad Ashfaq <faas@odoo.com>
2024-08-15 09:28:27 +0400

    compute field and onchanges

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 import datetime
 
 class EstateProperty(models.Model):
@@ -31,3 +31,27 @@ class EstateProperty(models.Model):
     sales_person_id = fields.Many2one(comodel_name="res.users", string="Sales Person", default=lambda self: self.env.user)
     tag_ids = fields.Many2many(comodel_name="estate.property.tag", string="Tags")
     offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offer")
+    total_area = fields.Float(compute="_compute_total_area")
+    best_price = fields.Float(compute="_compute_best_price")
+    
+    @api.depends("garden_area", "living_area")
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.garden_area + record.living_area
+            
+    
+    @api.depends("offer_ids")
+    def _compute_best_price(self):
+        for record in self:
+            record.best_price = max(record.offer_ids.mapped('price'))
+            
+    
+    @api.onchange('garden')
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = "north"
+        else:
+            self.garden_area = 0
+            self.garden_orientation = ""
+        
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,6 @@
-from odoo import fields, models
+import logging
+from odoo import fields, models, api
+import datetime
 
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
@@ -8,4 +10,26 @@ class EstatePropertyOffer(models.Model):
         selection=[('accepted', 'Accepted'), ('refused', 'Refused')], copy = False)
     partner_id = fields.Many2one(comodel_name="res.partner", string="Partner", required = True)
     property_id = fields.Many2one(comodel_name="estate.property", string="Property", required = True)
+    validity = fields.Integer('Validity', default = 7)
+    date_deadline = fields.Date(string='Deadline', compute='_deadline_compute', inverse = '_inverse_validity')
+
+    @api.depends("validity", "create_date")
+    def _deadline_compute(self):
+        for record in self:
+            if record.create_date:
+                create_date = fields.Date.from_string(record.create_date)
+                record.date_deadline = create_date + datetime.timedelta(days=record.validity)
+            else:
+                record.date_deadline = fields.Date.context_today(record) + datetime.timedelta(days=record.validity)
     
+    
+    @api.onchange('date_deadline')
+    def _inverse_validity(self):
+        for record in self:
+            if record.date_deadline and record.create_date:
+                create_date = fields.Date.from_string(record.create_date)
+                deadline_date = fields.Date.from_string(record.date_deadline)
+                record.validity = (deadline_date - create_date).days
+            else:
+                record.validity = 0
+            
