cf8beb1e47230eafe255674503f1729036fe1213
pach-odoo <pach@odoo.com>
2024-08-20 15:17:49 +0530

    [ADD] estate:  Added master data and demo data
    Define module data and demo data for Estate module
    
    - Added master data for standard Real Estate Property Types (Residential,
      Commercial, Industrial, Land)
    - Created demo data for properties and offers to support testing and development
    - Configured data extension to populate new fields in dependent modules
    - Ensured properties are correctly categorized as Residential in demo data

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -17,5 +17,9 @@
         "views/estate_property_type_views.xml",
         "views/estate_property_tag_views.xml",
         "views/estate_menus.xml",
+        "data/master_data.xml",
+    ],
+    "demo": [
+        "demo/demo_data.xml",
     ],
 }
