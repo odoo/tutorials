e14c47c75d78355c8252fa55fd328bf109b3d64b
st-yes <huntervee0@gmail.com>
2024-08-23 10:53:52 +0200

    - added the estate.property model - set up a 3 level menu - implemented default values and read-only attributes - implemented reserved fields

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,4 +4,9 @@
     'application': True,
     'author': 'Soukaina Tyes',
     'installable': True,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+    ]
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,20 +1,32 @@
 from odoo import models, fields
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 class EstateProperty(models.Model):
     _name = "estate.property"
-    # _description = "estate model"
+    _description = "estate model"
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=lambda self: datetime.today() + relativedelta(months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
-    garden_orientation = fields.Selection()
-    
-    pfffff
+    garden_orientation = fields.Selection([
+        ('north', 'North'),
+        ('south', 'South'),
+        ('east', 'East'),
+        ('west', 'West')])
+    active = fields.Boolean(default=True)
+    state = fields.Selection([
+        ('new', 'New'),
+        ('offer_received', 'Offer Received'),
+        ('offer_accepted', 'Offer Accepted'),
+        ('sold', 'Sold'),
+        ('canceled', 'Canceled')
+    ], default='new', copy=False, string='Status')
