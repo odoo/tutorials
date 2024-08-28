a93fa0da41954998c63c86a6121cdf064f3f51f8
pkgu-odoo <pkgu@odoo.com>
2024-08-28 18:09:00 +0530

    [IMP] estate: add standard Real Estate Property Types and demo data
    - Added standard property types: Residential, Commercial, Industrial, and Land.
    - Created demo data for the estate module.
    - Created demo properties with Property Type set to Residential.
    - Added demo offers using partners defined in the base module.
    - Validated one of the demo data offers using the “Accept Offer” button.
    - Created a new Property with offers directly added inside the One2many field
     linked to the Offers.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -9,12 +9,17 @@
     'data': [
         'security/ir.model.access.csv',
         'wizard/estate_add_offer_wizard_views.xml',
+        'data/property_types_data.xml',
         'views/estate_property_views.xml',
         'views/estate_property_type_view.xml',
         'views/estate_property_tag.xml',
         'views/estate_property_offer.xml',
         'views/res_user.xml',
         'views/estate_menus.xml',
+    ],
+    'demo': [
+        'demo/estate_demo_data.xml',
+        'demo/estate_demo_offers.xml'
     ],
     'installable': True,
     'application': True,
