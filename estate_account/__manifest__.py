2d10d680615db0dc837f7067d99616a5908cb324
pkgu-odoo <pkgu@odoo.com>
2024-08-30 15:27:15 +0530

    [IMP] estate: add property reports in estate module
    - Improved the report by including additional data fields and ensuring the PDF
     output matches the data accurately.
    - Implemented conditional logic to handle cases with no offers, displaying a
     message instead of an empty table.
    - Inherited and extended the property report to add invoice details.

--- estate_account/__manifest__.py
+++ estate_account/__manifest__.py
@@ -2,7 +2,7 @@
     'name': 'Estate Account',
     'version': '1.0',
     'depends': ['estate', 'account'],
-    'data': [],
+    'data': ['report/estate_account_property_report.xml'],
     'installable': True,
     'application': True,
     'auto_install': False,
