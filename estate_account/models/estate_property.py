ae243538397040b967d0979ee76f1dd8e745e552
pkgu-odoo <pkgu@odoo.com>
2024-09-03 10:40:09 +0530

    [ADD] estate:  add properties menu with listing and detail page
    - Implement 'Properties' menu on the website.
    - Display a grid of available  properties that are not sold or canceled.
    - Add property images to the grid view.
    - Create a dedicated, visually appealing property detail page.
    - Ensure the property detail page is mobile-friendly and attractive.

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -11,7 +11,7 @@ class EstateProperty(models.Model):
             'partner_id': self.buyer_id.id,
             'move_type': 'out_invoice',
             'invoice_line_ids': [
-               # 6% of the selling price
+                # 6% of the selling price
                 Command.create({
                     'name': 'Commission (6% of selling price)',
                     'quantity': 1,
@@ -24,6 +24,8 @@ class EstateProperty(models.Model):
                     'price_unit': 100.00,
                 }),
             ],
-            }
-        self.env['account.move'].create(move_values)
+        }
+        self.check_access_rights('write')
+        self.check_access_rule('write')
+        self.env['account.move'].sudo().create(move_values)
         # print(f"Created account.move with ID: {account_move.id} and invoice lines")
