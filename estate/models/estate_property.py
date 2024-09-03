3abb2dfecd646849c8209b1798875024b8fe42a6
yasp-odoo <yasp@odoo.com>
2024-09-03 12:38:11 +0530

    [IMP] estate: implement security groups, access rights, and property access rule
    - Added security groups for Real Estate Agents and Managers.
    - Defined access rights for agents and managers for properties, types, and tags.
    - Implemented a rule to restrict property access for agents based on assigned
      salesperson.
    - Updated estate_account to bypass access rights in action_sold.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -53,6 +53,7 @@ class EstateProperty(models.Model):
     offer_ids = fields.One2many('estate.property.offer', 'property_id', string='Offers')
     total_area = fields.Float(compute='_compute_total_area', store=True)
     best_price = fields.Float(compute='_compute_best_price', store=True)
+    company_id = fields.Many2one('res.company', required=True, default=lambda self: self.env.company.id)
 
     @api.depends('living_area', 'garden_area')
     def _compute_total_area(self):
--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -10,7 +10,9 @@ class EstateProperty(models.Model):
         for record in self:
             selling_price = record.selling_price or 0.0
             commission_amount = selling_price * 0.06
-            self.env['account.move'].create({
+            self.check_access_rights('write')
+            self.check_access_rule('write')
+            self.env["account.move"].sudo().create({
                 'partner_id': record.buyer_id.id,
                 'move_type': 'out_invoice',
                 'invoice_line_ids': [
