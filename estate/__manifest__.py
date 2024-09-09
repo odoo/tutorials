ccea139ae78891e497ebf6247e071b6b18b7aeef
mdmb-odoo <mdmb@odoo.com>
2024-09-09 16:11:09 +0400

    Ch2 Create first initial app

---
+++ estate/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Real Estate",
+
+    'summary': """
+        A new real estate app for onboarding purpose.
+    """,
+
+    'author': "MDMB",
+    'website': "https://www.odoo.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Tutorials/estate',
+    'depends': ['base'],
+    'application': True,
+
+}
