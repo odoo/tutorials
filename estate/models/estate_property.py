c2719ea20c5a452784a8b1e27b31dd95f1534524
Jun Ooi <juoo@odoo.com>
2024-09-10 10:46:49 +0400

    add chap 5 reserved fields and attributes

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -3,14 +3,21 @@ from odoo import fields, models
 class EstateProperty(models.Model):
     _name = "estate_property"
     _description = "Property estates"
-    
+
+    state = fields.Selection(
+        string='Type',
+        selection=[('New', 'New'), ('Offer_Received', 'Offer Received'), ('Offer_Accepted', 'Offer Accepted'), ('Sold', 'Sold'), ('Canceled', 'Canceled')],
+    )
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(
+        copy=False, 
+        default=fields.Date.add(fields.Date.today(), months=3) # default 3 months from now
+    )
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(copy=False, readonly=True)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -20,3 +27,4 @@ class EstateProperty(models.Model):
         string='Type',
         selection=[('North', 'North'), ('East', 'East'), ('South', 'South'), ('West', 'West')],
     )
+    active = fields.Boolean(default=True)
