d915d1657ae603ecc93bb7b0968ca0be4230fd28
Jun Ooi <juoo@odoo.com>
2024-09-10 17:38:46 +0400

    add inverse fn

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 class EstatePropertyType(models.Model):
     _name = "estate.property.offer"
@@ -12,3 +12,18 @@ class EstatePropertyType(models.Model):
     )
     partner_id = fields.Many2one("res.partner", string="Offer by")
     property_id = fields.Many2one("estate.property", string="Property")
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute="_date_deadline", inverse="_inverse_date_deadline")
+
+    # date_deadline = {creation date} + {validity} days
+    @api.depends("create_date", "validity")
+    def _date_deadline(self):
+        for record in self:
+            # record.create_date is None before the record is created
+            startingDate = record.create_date or fields.Date.today()
+
+            record.date_deadline = fields.Date.add(startingDate, days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            record.validity = (record.date_deadline - fields.Date.today()).days
