f2a7b37cdb9e1ce591aea9bc0ad8df7aaec22a9b
MrRose765-cymo <210376@umons.ac.be>
2024-09-16 17:29:28 +0200

    [IMP] estate: Chapter 5: Let register new records on the UI using Action, Menus and Fields.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,6 +6,8 @@
     ],
     'data': [
       'security/ir.model.access.csv',
+      'views/estate_property_views.xml',
+      'views/estate_menus.xml',
     ],
     'installable': True,
     'application': True,
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -8,10 +8,10 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=fields.Date.today)
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -20,4 +20,13 @@ class EstateProperty(models.Model):
     garden_orientation = fields.Selection(
         string='Garden orientation',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        string = "Status",
+        selection=[('new', 'New'), ('offer_received', 'Offer Received'),
+                   ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')],
+        required=True,
+        copy=False,
+        default='new',
     )
