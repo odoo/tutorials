ca55332bc90c381d43781a6253babb221f08c5e4
gteboul <guillaume.teboul@protonmail.com>
2024-09-17 12:07:48 +0200

    [IMP] first exo

---
+++ estate/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Estate",
+
+    'depends': [
+        'base'
+    ],
+
+    'data' : [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml'
+    ],
+    'application': True
+
+
+}
---
+++ estate/models/estate_property.py
@@ -0,0 +1,41 @@
+from odoo import models, fields
+
+
+class Estate(models.Model):
+    _name = "estate.property"
+    _description = "Estate business object"
+
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(copy=False, default=fields.Date.add(fields.Date.today(), months=3))
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        string="Type",
+        selection=[
+            ("north", "North"),
+            ("south", "South"),
+            ("east", "East"),
+            ("west", "West")
+        ]
+    )
+    active = fields.Boolean(default=False)
+    state = fields.Selection(
+        string="State",
+        copy=False,
+        selection=[
+            ("new", "New"),
+            ("offer received", "Offer Received"),
+            ("offer accepted", "Offer accepted"),
+            ("sold", "Sold"),
+            ("canceled", "Canceled")
+        ]
+
+    )
