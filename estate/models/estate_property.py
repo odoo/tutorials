36fbd4062f8343683e478ef089c9d3e0da74ed47
gteboul <guillaume.teboul@protonmail.com>
2024-09-18 14:09:21 +0200

    Odoo technical training chapters 8 and 9

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,5 +1,6 @@
-from odoo import models, fields
-
+from odoo import models, fields, api
+from odoo.exceptions import UserError
+from odoo.tools.translate import _
 
 class Estate(models.Model):
     _name = "estate.property"
@@ -9,6 +10,7 @@ class Estate(models.Model):
     description = fields.Text()
     postcode = fields.Char()
     date_availability = fields.Date(copy=False, default=fields.Date.add(fields.Date.today(), months=3))
+
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
@@ -16,9 +18,10 @@ class Estate(models.Model):
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Integer()
+    garden_area = fields.Integer(default=0)
     garden_orientation = fields.Selection(
         string="Type",
+        default=None,
         selection=[
             ("north", "North"),
             ("south", "South"),
@@ -29,6 +32,7 @@ class Estate(models.Model):
     active = fields.Boolean(default=False)
     state = fields.Selection(
         string="State",
+        default="new",
         copy=False,
         selection=[
             ("new", "New"),
@@ -44,3 +48,49 @@ class Estate(models.Model):
     salesperson_id = fields.Many2one('res.users', default=lambda self: self.env.user)
     estate_property_tag_ids = fields.Many2many(comodel_name="estate.property.tag")
     offer_ids = fields.One2many(comodel_name="estate.property.offer", inverse_name="property_id")
+
+    total_area = fields.Integer(compute="_compute_total_area")
+    best_price = fields.Integer(compute="_compute_best_price")
+
+    @api.depends("garden_area", "living_area")
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.garden_area + record.living_area
+
+    @api.depends("offer_ids.price")
+    def _compute_best_price(self):
+        for record in self:
+            record.best_price = max(record.mapped("offer_ids.price"))
+
+    @api.onchange("garden")
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = "north"
+        else:
+            self.garden_area = 0
+            self.garden_orientation = None
+
+    #TODO: make the status offer received when we received offer but did not accept any
+    # @api.onchange("offer_ids")
+    # def _onchange_offer_ids(self):
+    #     for record in self:
+    #         if record.offer_ids:
+    #             record.state = "offer_received"
+
+
+    def action_sold(self):
+        for record in self:
+            if record.state == "canceled":
+                raise UserError(_("Canceled properties cannot be sold"))
+            else:
+                record.state = "sold"
+        return True
+
+    def action_cancel(self):
+        for record in self:
+            if record.state == "sold":
+                raise UserError(_("A sold property cannot be canceled"))
+            else:
+                record.state = "canceled"
+        return True
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class EstatePropertyOffer(models.Model):
 
@@ -15,3 +15,53 @@ class EstatePropertyOffer(models.Model):
 
     partner_id = fields.Many2one("res.partner", required=True)
     property_id = fields.Many2one("estate.property", required=True)
+
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_date_deadline")
+
+    @api.depends("create_date", "validity")
+    def _compute_date_deadline(self):
+        for record in self:
+            if record.create_date:
+                record.date_deadline = fields.Date.add(record.create_date.date(),days=record.validity)
+            else:
+                record.date_deadline = fields.Date.add(fields.Date.today(), days=record.validity)
+
+    #TODO: validity does not refresh until we refresh the page (it is normal but why ?)
+    def _inverse_date_deadline(self):
+        for record in self:
+            if record.create_date:
+                record.validity = (record.date_deadline - record.create_date.date()).days
+
+    def action_accept(self):
+        for offer in self:
+            offer.status = "accepted"
+            #filtered: return the records in self(input recordset) satisfying param func
+            #return self.browse([rec.id for rec in self if func(rec)])
+            #self = recordset so here it is the offer_ids of property
+            #for each record if func is true then we put its id in a list
+            #browse() fetch a new recordset based on the id of the input list
+            other_offers = offer.property_id.offer_ids.filtered(lambda o: o.id != offer.id)
+
+            #TODO: investigate the write method to update a recordset instead of a for loop
+            #in fields.py def write(self, records, value):
+            for other_offer in other_offers:
+                other_offer.status = "refused"
+
+            #TODO: is there many2one channel the best way to communicate between model ? investigate model communication
+            offer.property_id.buyer_id = offer.partner_id
+            offer.property_id.selling_price = offer.price
+            offer.property_id.state = "offer_accepted"
+        return True
+
+
+    def action_refuse(self):
+        #TODO: investigate what is the naming convention for this kind of action, seen rec, record does it depend on the context
+        for record in self:
+            record.status = "refused"
+            record.property_id.buyer_id = None
+            record.property_id.selling_price = None
+            record.property_id.state = "offer_received"
+        return True
+
+
