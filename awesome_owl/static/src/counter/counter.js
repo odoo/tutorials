109ddf33248e2a8f64fc4d1523e49e18f46663ac
Cyril Moreau (cymo) <210376@umons.ac.be>
2024-09-20 15:39:48 +0200

    [IMP] awesome_owl: Chapter 6. Sum of two counters.

--- awesome_owl/static/src/counter/counter.js
+++ awesome_owl/static/src/counter/counter.js
@@ -4,12 +4,16 @@ import {Component, useState} from "@odoo/owl";
 
 export class Counter extends Component {
     static template = "awesome_owl.Counter";
+    static props = {
+        onChange: {type: Function, optional: true},
+    }
 
     setup() {
-        this.state = useState({value: 0});
+        this.state = useState({value: 1});
     }
 
     increment() {
-        this.state.value = this.state.value + 1;
+        this.state.value++;
+        this.props.onChange?.();
     }
 }
--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import {Component, markup} from "@odoo/owl";
+import {Component, markup, useState} from "@odoo/owl";
 import {Counter} from "./counter/counter";
 import {Card} from "./card/card";
 
@@ -11,5 +11,10 @@ export class Playground extends Component {
     setup() {
         this.value1 = "<div class='text-danger'>Hello</div>";
         this.value2 = markup("<img src='x' onerror='alert(1)'/>");
+        this.sum = useState({value: 2});
+    }
+
+    increment() {
+        this.sum.value++;
     }
 }
