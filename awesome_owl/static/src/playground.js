67d25b8790c0f05479513bf8d6918b8abb3f0d12
Cyril Moreau (cymo) <210376@umons.ac.be>
2024-09-20 14:55:18 +0200

    [IMP] awesome_owl: Chapter 4. Use markup to display html.

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,10 +1,15 @@
 /** @odoo-module **/
 
-import {Component} from "@odoo/owl";
+import {Component, markup} from "@odoo/owl";
 import {Counter} from "./counter/counter";
 import {Card} from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
     static components = {Counter, Card};
+
+    setup() {
+        this.value1 = "<div class='text-danger'>Hello</div>";
+        this.value2 = markup("<img src='x' onerror='alert(1)'/>");
+    }
 }
