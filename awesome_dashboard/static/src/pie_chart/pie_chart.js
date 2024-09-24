d49185b29d652305f817d6c32e7abbca85d33663
Cyril Moreau (cymo) <210376@umons.ac.be>
2024-09-24 12:02:33 +0200

    [IMP] awesome_dashboard: Chapter 6. Add a chart for each size on the dashboard.

---
+++ awesome_dashboard/static/src/pie_chart/pie_chart.js
@@ -0,0 +1,54 @@
+/** @odoo-module **/
+
+import {loadJS} from "@web/core/assets";
+import {Component, onMounted, onWillStart, onWillUnmount, useRef} from "@odoo/owl";
+
+export class PieChart extends Component {
+    static template = "awesome_dashboard.PieChart";
+    static props = {
+        title: String,
+        data: Object,
+    }
+
+
+    setup() {
+        this.canvasRef = useRef("canvas");
+        onWillStart(() => loadJS(["/web/static/lib/Chart/Chart.js"]));
+        onMounted(this.renderChart);
+        onWillUnmount(this.onWillUnmount);
+    }
+
+    onWillUnmount() {
+        if (this.chart) {
+            this.chart.destroy();
+        }
+    }
+
+    renderChart() {
+        if (this.chart) {
+            this.chart.destroy();
+        }
+        const labels = Object.keys(this.props.data); // Get the sizes of t-shirts
+        const data = Object.values(this.props.data); // Get the number of each size
+        this.chart = new Chart(this.canvasRef.el, {
+            type: "pie",
+            data: {
+                labels: labels,
+                datasets: [
+                    {
+                        label: this.props.label,
+                        data: data,
+                        backgroundColor: [
+                            "rgba(255, 99, 132, 0.2)",
+                            "rgba(54, 162, 235, 0.2)",
+                            "rgba(255, 206, 86, 0.2)",
+                            "rgba(75, 192, 192, 0.2)",
+                            "rgba(153, 102, 255, 0.2)",
+                            "rgba(255, 159, 64, 0.2)",
+                        ],
+                    }
+                ]
+            }
+        })
+    }
+}
