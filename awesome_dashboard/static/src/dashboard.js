7c7f80c2f4cd9c77e0037904f3e276217b77792f
Cyril Moreau (cymo) <210376@umons.ac.be>
2024-09-24 10:18:34 +0200

    [IMP] awesome_dashboard: Chapter 2. New buttons to go to custom list & leads.

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -3,15 +3,34 @@
 import {Component} from "@odoo/owl";
 import {registry} from "@web/core/registry";
 import {Layout} from "@web/search/layout";
+import {useService} from "@web/core/utils/hooks";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
     static components = {Layout};
 
     setup() {
+        this.action = useService("action");
         this.display = {
             controlPanel: {}
         }
+    }
+
+    openCustomer() {
+        this.action.doAction("base.action_partner_form");
+    }
+
+    openLeads() {
+        this.action.doAction({
+            type: "ir.actions.act_window",
+            name: "All Leads",
+            res_model: "crm.lead",
+            views: [
+                [false, "list"],
+                [false, "form"]
+            ],
+            target: "current",
+        })
     }
 }
 
