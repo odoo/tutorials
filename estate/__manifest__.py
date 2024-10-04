00efc40dc4731e70620166b945967ce0e7f979d6
rame-odoo <rame@odoo.com>
2024-10-04 18:55:21 +0530

    [ADD] estate: setup environment and create module with XML views.
    - Completed the first four chapters of the Odoo developer tutorials.
    - Set up the development environment.
    - Created the real estate module and model with various fields.
    - Established access rights for the model.
    - Developed initial XML views for the module.
    - Ensured the module is functional and properly registered in the Odoo database.
    - Plan to enhance the user interface and add more features in the upcoming chapters.

---
+++ estate/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "rame_odoo",
+    'category': 'Category',
+    'description': """
+    Description text
+    """,
+    # data files always loaded at installation
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [],
+    'installable': True,
+    'application' : True
+}
---
+++ estate/models/estate_property.py
@@ -0,0 +1,31 @@
+from odoo import fields, models
+from datetime import datetime, timedelta
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Estate Property"
+
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(default=lambda self: datetime.now() + timedelta(days=90))
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection([('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
+    active = fields.Boolean(default=True)
+    state = fields.Selection([
+        ('new', 'New'),
+        ('offer_received', 'Offer Received'),
+        ('offer_accepted', 'Offer Accepted'),
+        ('sold', 'Sold'),
+        ('canceled', 'Canceled')],
+        required=True, default='new')
+
+
+
