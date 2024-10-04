afa58e6b2fd06673abae5a4e1d3f5bcac61e3236
rmun-odoo <rmun@odoo.com>
2024-10-04 18:55:13 +0530

    [ADD] estate: models, security, views setup
    Created the real estate module and model with various fields.
    Established access rights for the model.
    Developed initial XML views for the module.
    Completed the first four chapters of the Odoo developer tutorials.

---
+++ estate/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Rajat",
+    'category': 'Category',
+    'description': """
+    Description text
+    """,
+    # data files always loaded at installation
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
+
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [],
+    'application': True,
+    'installable': True
+}
