84aaaa6737cbea3915b40af99c90e2664a4dbe84
srap-odoo <srap@odoo.com>
2024-10-11 16:04:00 +0530

    [IMP] estate: implemented Computed Fields and Onchange Methods
    Added computed fields to estate.property and estate.property.offer models:
    total_area (sum of living_area and garden_area).
    best_price (maximum offer price).
    date_deadline (validity based on offer's creation date and validity duration).
    Defined inverse methods to update dependent fields.
    Implemented onchange logic for the garden field to set default values for garde
    n area(10) and orientation (North).
    Updated form and list views to reflect the new fields and behaviors.

--- estate/models/estate_property_offers.py
+++ estate/models/estate_property_offers.py
@@ -1,4 +1,5 @@
-from odoo import fields, models # type: ignore
+from odoo import fields, models,api 
+from datetime import timedelta, datetime
 
 class EstatePropertyOffers(models.Model):
 
@@ -11,8 +12,25 @@ class EstatePropertyOffers(models.Model):
         copy=False,
         selection=[('accepted', 'Accepted'), ('refused', 'Refused')],
        )
-    partner_id = fields.Many2one('res.partner', required=True) # type: ignore
+    partner_id = fields.Many2one('res.partner', required=True) 
     property_id = fields.Many2one('estate.property', required=True)
 
+    validity = fields.Integer(default = 7)
+    date_deadline = fields.Date(compute = "_compute_date_deadline", inverse="_inverse_date_deadline", store="True")
 
-
+    # Functions
+    @api.depends('validity')
+    def _compute_date_deadline(self):
+        for record in self:
+            if record.create_date:
+                record.date_deadline = record.create_date + timedelta(record.validity)
+            else:
+                record.date_deadline = datetime.today() + timedelta(7)
+    
+    def _inverse_date_deadline(self):
+        for record in self:
+            if record.create_date and record.date_deadline:
+                record.validity = (record.date_deadline - record.create_date.date()).days
+            else:
+                record.validity = 7
+    
