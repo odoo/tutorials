7566e212ce7332457d52ecbe196dc4c136ed1a0e
chha-odoo <chha@odoo.com>
2024-10-11 16:09:08 +0530

    [IMP] estate: completed chapter 8
    Learned about Computed Fields and Onchanges :
    
    Computed Fields:
    i) computed total area of property by two depending fields living area and
    garden area.
    ii) computed the best offer price for the property using One2many relationship.
    iii) computed date_deadline for the offers using validity period and create date
    . (also used the inverse function to recalculate the validity period after
    changing the date_deadline by user).
    
    Onchanges:
    i) created onchange function for property model. when user check the garden the
    garden_area set to 10 and garden orientation to North automatically and vice
    versa for unchecking the garden.

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from datetime import timedelta, datetime
 
 class EstatePpropertyOffer(models.Model):
     _name = 'estate.property.offer'
@@ -10,7 +11,6 @@ class EstatePpropertyOffer(models.Model):
             ('accepted', 'Accepted'),
             ('refused', 'Refused')
         ],
-        string="status string",
         copy=False,
     )
 
@@ -25,3 +25,18 @@ class EstatePpropertyOffer(models.Model):
         'estate.property',
         required=True
     )
+
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute="_compute_deadDeadline", inverse="_inverse_deadDeadline", store=True)
+
+    @api.depends("validity", "create_date")
+    def _compute_deadDeadline(self):
+        for record in self:
+            if record.create_date:
+                record.date_deadline = record.create_date.date() + timedelta(days=record.validity)
+            else:
+                record.date_deadline = datetime.now() + timedelta(days=record.validity)
+
+    def _inverse_deadDeadline(self):
+        for record in self:
+            record.validity = (record.date_deadline - record.create_date.date()).days
