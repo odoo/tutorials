438a8515c39dc144035d2404dc7eae50ef7ecd78
ayve-odoo <ayve@odoo.com>
2024-10-11 16:08:57 +0530

    [IMP] estate: added compute fields, inverse and buttons with actions
    - Learned about computed fields and the api.depends decorator, which
      defines dependencies for computing fields, making them read-only.
      Some fields should be precalculated so we use compute fields.
    - Implemented inverse functions to allow these fields to be editable and updated
        in both directions.
       Compute fileds are read only, to make them editable and to make changes vice-
       verse we require need of inverse functions
    - Used api.onchange to trigger actions when changing the value or state
        of a variable.
    - Started chapter 9, going through the chapter and working on the exercies

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -9,7 +9,7 @@ class EstateProperty(models.Model):
     description = fields.Text()
     postcode = fields.Char()
     date_availability = fields.Date(default=lambda self: (date.today() + timedelta(days=90)).strftime('%Y-%m-%d'), copy=False)
-    expected_price = fields.Float()
+    expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
@@ -45,19 +45,75 @@ class EstateProperty(models.Model):
         help='Select the current status of the property'
     )
 
-    property_type_id=fields.Many2one("estate.property.type", string="Property Type")
-    seller_id=fields.Many2one("res.users", string="Salesman" ,default=lambda self: self.env.user)
-    buyer_id=fields.Many2one("res.partner", string="Buyer")
-    tag_ids=fields.Many2many("estate.property.tag", string="Property Tags")
-    offer_ids=fields.One2many("estate.property.offer", "property_id", string="Offers")
+    # ONE property can have only ONE property type
+    # But ONE property type can be assigned to MANY properties
+    property_type_id=fields.Many2one(
+        "estate.property.type",
+        string="Property Type"
+    )
+
+    # ONE property can have only ONE buyer and seller
+    # But ONE  buyer/seller can sell/buy MANY properties
+    seller_id=fields.Many2one(
+        "res.users",
+        string="Salesman",
+        default=lambda self: self.env.user
+    )
+
+    buyer_id=fields.Many2one(
+        "res.partner",
+        string="Buyer"
+    )
+
+    # MANY tags can  se assigned to multiple properties and vice versa
+    tag_ids=fields.Many2many(
+        "estate.property.tag",
+        string="Property Tags"
+    )
+
+    # A property can have MANY offers 
+    # But ONE offer can only be applied to ONE property
+    # For Every One2many there should be a Many2one but not vice versa
+    offer_ids=fields.One2many(
+        "estate.property.offer",
+        "property_id",
+        string="Offers"
+    )
 
     @api.depends("living_area","garden_area")
     def _total_area(self):
         for line in self:
             line.total_area = line.living_area + line.garden_area
 
-    @api.depends("best_price")
+    @api.depends("offer_ids.price")
     def _compute_best_price(self):
         for record in self:
-            record.best_price=100
+            record.best_price = max(record.mapped("offer_ids.price"), default=0)
+
+    #Onchange Methods
+    '''In many cases, both computed fields and onchanges may be used to achieve the
+    same result. Always prefer computed fields since they are also triggered outside
+    of the context of a form view. Never ever use an onchange to add business logic 
+    to your model. This is a very bad idea since onchanges are not automatically 
+    triggered when creating a record programmatically; they are only triggered in the form view.'''
+
+    @api.onchange("garden")
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = "north"
+        else:
+            self.garden_area = 0
+            self.garden_orientation = None
+    
+    # Actions for button
+    def sold_button(self):
+        for record in self:
+            record.state = "sold"
+        return True
+    
+    def cancel_button(self):
+        for record in self:
+            record.state = "canceled"
+        return True
         
