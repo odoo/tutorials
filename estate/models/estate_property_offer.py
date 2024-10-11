2d6fe52783a0aa44a5817e5910f6869a04bb38ce
ayve-odoo <ayve@odoo.com>
2024-10-11 16:08:57 +0530

    [IMP] estate: added compute fields, inverse and buttons with actions
    - Learned about computed fields and the api.depends decorator, which
      defines dependencies for computing fields, making them read-only.
      Some fields should be precalculated so we use compute fields.
    - Implemented inverse functions to allow these fields to be editable and updated
        in both directions.
       Compute fileds are read only, to make them editable and to make changes vice-
       verse we require need of inverse functions
    - Used api.onchange to trigger actions when changing the value or state
        of a variable.
    - Started chapter 9, going through the chapter and working on the exercies

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import fields, models  # type: ignore
+from odoo import api, fields, models  # type: ignore
+from datetime import timedelta, date
 
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
@@ -13,7 +14,56 @@ class EstatePropertyOffer(models.Model):
     string='Status',
     copy=False
     )
-    partner_id = fields.Many2one('res.partner',required=True,string="Partner")
-    property_id = fields.Many2one('estate.property',required=True, string="Property ID")
+
+    partner_id = fields.Many2one(
+        'res.partner',
+        required=True,
+        string="Partner"
+    )
+
+    property_id = fields.Many2one(
+        'estate.property',
+        required=True,
+        string="Property ID"
+    )
+
+    validity =  fields.Integer(
+        string="Validity(Days)",
+        default=7
+    )
+    
+    date_deadline=fields.Date(
+        string="Dateline",
+        compute="_compute_deadline",
+        inverse="_inverse_deadline",
+        store=True
+    )
+
+    # Compute Methods
+    # when inverse is used the field becomes editable (not read only)
+    # compute makes it read only
+    @api.depends("validity")
+    def _compute_deadline(self):
+        for record in self:
+            if not record.create_date:
+                record.create_date = date.today()
+            record.date_deadline = record.create_date.date() + timedelta(days=record.validity)
+
+    # used the abs so the app wont crash when getting -ve number
+    def _inverse_deadline(self):
+        for record in self:
+            record.validity=  abs((record.date_deadline - record.create_date.date()).days)
+    
+    #Action Button
+    def action_accept_offer(self):
+        for record in self:
+            record.status = "accepted"
+        return True
+
+    def action_refuse_offer(self):
+        for record in self:
+            record.status = "refused"
+        return True
+
 
     
