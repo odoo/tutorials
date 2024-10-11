740ca38826dfdd9711ace806a2cc2b6ba7b3906d
rmun-odoo <rmun@odoo.com>
2024-10-11 15:55:31 +0530

    [IMP] estate module: till chapter 8
    Added computed and onchange fields.

--- estate/models/real_estate_property_offer_model.py
+++ estate/models/real_estate_property_offer_model.py
@@ -1,5 +1,8 @@
 
-from odoo import fields, models
+from odoo import fields, models, api
+from dateutil.relativedelta import relativedelta
+from datetime import datetime
+
 
 
 class EstateOffer(models.Model):
@@ -13,4 +16,32 @@ class EstateOffer(models.Model):
         copy = False
     )
     partner_id = fields.Many2one('res.partner',string = "Buyer", required=True)
-    property_id = fields.Many2one('estate_property',string="Property", required=True)
+    property_id = fields.Many2one('estate.property',string="Property", required=True)
+    validity = fields.Integer(default = 7)
+    date_deadline = fields.Date(compute = '_compute_deadline', inverse = "_inverse_validity", store=True)
+
+
+    @api.depends('validity', 'create_date')
+    def _compute_deadline(self):
+        for record in self:
+            if record.create_date:
+                # print("Hfedfdfdfi", record.id,record.create_date)
+                # print(record.create_date.date() + relativedelta(days=record.validity))
+                record.date_deadline = record.create_date.date() + relativedelta(days=record.validity)
+            else:
+                record.date_deadline = datetime.now() + relativedelta(days=record.validity)
+        # print(record.create_date, "Hi")
+
+
+
+
+    def _inverse_validity(self):
+        for record in self:
+            record.validity = (record.date_deadline - record.create_date.date()).days
+
+
+    def action_offer(self):
+
+        return True
+
+    
--- estate/models/real_estate_property_type_model.py
+++ estate/models/real_estate_property_type_model.py
@@ -7,7 +7,7 @@ class EstatePropertyType(models.Model):
     _description = "Real estate property type"
 
     name = fields.Char(required=True)
-    # property_ids = fields.One2many('estate_property','property_type_id')
+    # property_ids = fields.One2many('estate.property','property_type_id')
 
 
 
