2f380fa7495c6b86601becd4398e6f0300948592
sahm-odoo <sahm@odoo.com>
2024-10-17 19:05:10 +0530

    [IMP] estate_property: computed Fields And Onchanges
    - Added computed fields for total area and best offer in estate property model.
    - Implemented inverse method for offer validity date computation.
    - Created onchange method to auto-fill garden area and orientation based on
    garden field.

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import models, fields # type: ignore
+from odoo import models, fields, api # type: ignore
+from datetime import timedelta, datetime
 
 class estatePropertyOffer(models.Model):
 
@@ -18,6 +19,36 @@ class estatePropertyOffer(models.Model):
     property_id = fields.Many2one(
         'estate.property',
         string='Property',
-        required=True
+        ondelete='set null'
     )
+    validity = fields.Integer(default=7, string="Validity")
+    date_deadline = fields.Date(compute='_compute_date_deadline', inverse='_compute_validity', string="Date Deadline")
+    sum = fields.Integer(compute='_compute_sum', string="Sum")
+    sum2 = fields.Integer(compute='_compute_sum2', string="Sum2")
+
+    @api.depends('validity')
+    def _compute_date_deadline(self):
+        for record in self:
+            if record.create_date:
+                record.date_deadline = record.create_date + timedelta(record.validity)
+            else:
+                record.date_deadline = datetime.today() + timedelta(record.validity)
+
+    @api.depends('date_deadline') 
+    def _compute_validity(self):
+        for record in self:
+            if record.date_deadline and record.create_date:
+                record.validity = (record.date_deadline - record.create_date.date()).days
+            else:
+                record.validity = (record.date_deadline - datetime.today()).days
+
+    @api.depends('validity')
+    def _compute_sum(self):
+        for record in self:
+            record.sum = 7 + record.validity
+
+    @api.depends('sum')
+    def _compute_sum2(self):
+        for record in self:
+            record.sum2 = 7 + record.sum
     
