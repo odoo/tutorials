b064c2028c49f0c94b2a885d2947e22549bc5cfa
chha-odoo <chha@odoo.com>
2024-10-18 16:10:42 +0530

    [IMP] estate: completed chapter 9 to 11
    Chapter 9
    Learned about Action
    - Added 'Cancel' and 'Sold' buttons with business logic to estate.property
    model.
    - Implemented 'Accept' and 'Refuse' buttons for estate.property.offer model.
    - Set buyer and selling price upon offer acceptance, ensuring only one accepted
    offer per property.
    
    Chapter 10
    Learned about Constraints
    - Added SQL constraints for positive pricing and unique names on property tags
    and types.
    - Implemented Python constraint to ensure selling price is at least 90% of
    expected price.
    
    Chapter 11
    - Added inline list view for property types to display offers.
    - Implemented statusbar widget to show property states.
    - Established default sorting for property and offer models.
    - Introduced manual ordering for property types using a sequence field.
    - Enabled conditional display of buttons based on property state.
    - Applied color decorations to property and offer list views.
    - Made estate.property.offer and estate.property.tag list views editable.
    - Set default filter for 'Available' properties in the action.
    - Improved search functionality for living area filtering.
    - Added stat button to property type form to view related offers.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -9,14 +9,15 @@
     """,
     # data files always loaded at installation
     'data': [
-        'security/ir.model.access.csv',
-        'views/estate_property_views.xml',
+        'views/estate_property_offers_view.xml',
         'views/estate_property_type_view.xml',
         'views/estate_property_tag_view.xml',
+        'views/estate_property_views.xml',
         'views/estate_menus_view.xml',
+        'security/ir.model.access.csv',
     ],
     # data files containing optionally loaded demonstration data
-    'demo': [
-    ],
-    'application': True,  
+    'demo': [],
+    'application': True,
+    'installable': True,
 }
