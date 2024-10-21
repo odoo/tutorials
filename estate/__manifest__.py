640bb1dd019548d52fb9885d0716b327cfe54b4e
malm-odoo <malm@odoo.com>
2024-10-21 17:21:28 +0200

    [ADD] chap5: Added xml files

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,7 +1,12 @@
 {
     'name': 'Real Estate',
-    'summary': 'Create real estate properties and keep track of status',
+    'description': 'Create real estate properties and keep track of status',
     'depends': [
         'base',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ],
 }
---
+++ estate/models/estate_property.py
@@ -0,0 +1,28 @@
+from odoo import models, fields
+from dateutil.relativedelta import relativedelta
+
+class EstateProperty(models.Model):
+    _name = "estate_property"
+    _description = "Create real estate properties and keep track of status"
+
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(default=fields.Datetime.today()+relativedelta(months=3), copy=False)
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        string='Type',
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
+    )
+    active = fields.Boolean('Active', default=True)
+    state = fields.Selection(
+        string='State',
+        selection=[('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', "Cancelled")]
+    )
