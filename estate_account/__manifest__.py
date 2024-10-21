cd6c3f8ab84e444b15321a2c2c01bdea31a914b9
ayve-odoo <ayve@odoo.com>
2024-10-21 18:32:03 +0530

    [IMP] estate: improved module with inheritance invoice linking and Kanban view
    - Implemented custom business logic in CRUD methods:
      - Prevent deletion of properties unless their state is 'New' or 'Canceled'.
      - Change property state to 'Offer Received' upon offer creation.
      - Validate offer prices to ensure no lower offers than existing ones.
    - Added `property_ids` field to `res.users` model for linking properties to
       salespersons .
    - Extended user view to display linked properties in a new notebook page.
    - Created `estate_account` module to integrate real estate and invoicing
        features with dependencies on `estate` and `account` modules.
    - Overrode `action_sold` method in `estate.property` model for proper
       inheritance.
    - Implemented customer invoice creation (`account.move`) upon property sale.
    - Included two invoice lines with amounts as instructed.
    - Developed minimal Kanban view for properties, enhancing it with fields:
       expected price, best price, selling price, and tags.
    - Added conditional displays for best and selling prices based on offer status.
    - Enabled default grouping of properties by type in the Kanban view and disabled
       drag-and-drop functionality.

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "Real Estate Account",
+    'version': '1.0',
+    'depends': ['account', 'estate'],
+    'author': "Ayushmaan",
+    'category': 'Account',
+    'description': """
+    Second Application
+    """,
+    # data files always loaded at installation
+    'data': [],
+    # data files containing optionally loaded demonstration data
+    'demo': [],
+    'installable':True,
+    'application':False
+}
---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,32 @@
+from odoo import Command, models  
+
+class EstateProperty(models.Model):
+    _inherit = "estate.property"
+
+    def sold_button(self):
+        # print("inherited sold action")
+
+        invoice_price = {
+            "name": self.name,
+            "quantity": 1,
+            "price_unit": 0.06*self.selling_price
+        }
+
+        administrative_fees = {
+            "name": "Administrative Fees",
+            "quantity": 1,
+            "price_unit": 100
+        }
+
+        self.env['account.move'].create(
+            {
+                "name": self.name+" Invoice",
+                "partner_id" : self.buyer_id.id,
+                "move_type": "out_invoice",
+                "invoice_line_ids": [
+                    Command.create(invoice_price),
+                    Command.create(administrative_fees)
+                ]
+            }
+        )
+        return super().sold_button()
