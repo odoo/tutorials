ea9a2a0e51b47d085528372073db914896f253f7
srap-odoo <srap@odoo.com>
2024-10-21 13:08:05 +0530

    [IMP] estate: enhanced Module with Inheritance,Linking To Invoice and KanbanView
    Prevented deletion of properties not in valid states by using decorator ondelete
    Set property state to 'Offer Received' upon offer creation and raised an error
    for lower-priced offers
    Added a new field to the user model to display a salesperson's available
    properties in user form view
    Extended the view to include the new field in a new notebook page for better
    organization
    Created a Kanban view for properties displaying essential information
    conditionally
    Grouped properties by type by default to enhance user experience in the Kanban
    view
    Disabled drag-and-drop functionality to maintain the intended order of
    properties

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,15 +1,15 @@
 {
     'name': "estate",
     'version': '1.0',
-    'depends': ['base'],
+    'depends': ['base','mail'],
     'author': "srap",
     'data': [
         'security/ir.model.access.csv',
+        'views/res_users_views.xml',
         'views/estate_property_offers_views.xml',
         'views/estate_property_views.xml',
         'views/estate_property_type_views.xml',
         'views/estate_property_tags_views.xml',
-        'views/estate_consumer.xml',
         'views/estate_menus.xml',
     ],
     'application': True
---
+++ estate_account/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': "estate_account",
+    'version': '1.0',
+    'depends': ['base','account','estate'],
+    'author': "srap",
+    'data': [
+        
+    ],
+    'application': True
+}
