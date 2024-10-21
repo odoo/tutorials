4d5d4f9ade22469a03abb8fc65e83dd16a8beaf7
chha-odoo <chha@odoo.com>
2024-10-21 18:18:11 +0530

    [IMP] estate: auto-generate customer invoice on property sold
    Completed Chapter 13
    - Created a new module to connect the real estate and invoicing.
    - Modified the property sold action to automatically create a customer
    invoice with two line items.

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': "Real Estate Account",
+    'version': '1.0',
+    'depends': ['estate', 'account'],
+    'author': "Harsh Chaudhari",
+    'category': 'Category',
+    'description': """
+    Real Estate description...
+    """,
+    # data files always loaded at installation
+    'data': [
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [],
+    'application': False,
+    'installable': True,
+}
