7a0858e01fbd6bb6b88b251151e51c241270c93b
chha-odoo <chha@odoo.com>
2024-10-21 18:18:11 +0530

    [IMP] estate: auto-generate customer invoice on property sold
    Completed Chapter 13
    - Created a new module to connect the real estate and invoicing.
    - Modified the property sold action to automatically create a customer
    invoice with two line items.

---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,29 @@
+from odoo import Command, models
+
+
+class EstateProperty(models.Model):
+    _inherit = "estate.property"
+
+    def action_property_sold(self):
+        invoice_price = {
+            "name": self.name,
+            "quantity": "1",
+            "price_unit": self.selling_price * 0.06,
+        }
+        administrative_fees = {
+            "name": "administrative fees",
+            "quantity": "1",
+            "price_unit": "100",
+        }
+        self.env["account.move"].create(
+            {
+                "name": self.name + " invoice",  
+                "partner_id": self.buyer_id.id,
+                "move_type": "out_invoice",
+                "invoice_line_ids": [
+                    Command.create(invoice_price),
+                    Command.create(administrative_fees)
+                ]
+            }
+        )
+        return self.action_property_cancel()
