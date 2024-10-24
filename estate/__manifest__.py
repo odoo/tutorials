4e7519ccec579c79446f1bf781687571f6031b37
Odoo <francescocalicchio@hotmail.com>
2024-10-24 15:33:02 +0200

    feat: chapter 12

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -9,6 +9,15 @@
     'depends': [
         'base_setup',
     ],
+    'data': [
+      'data/ir.model.access.csv',
+      'views/estate_property_views.xml',
+      'views/estate_property_tag_views.xml',
+      'views/estate_property_type_views.xml',
+      'views/estate_property_offer_views.xml',
+      'views/res_users_views.xml',
+      'views/estate_menu.xml',
+    ],
     'installable': True,
     'application': True,
     'auto_install': False
