7de8a5f93c58b73a287333e9670436d70ab45668
malm-odoo <malm@odoo.com>
2024-10-25 11:02:04 +0200

    [ADD] estate: Added kanban view

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,8 +6,8 @@
     ],
     'data': [
         'security/ir.model.access.csv',
-        'views/estate_property_views.xml',
         'views/estate_menus.xml',
+        'views/estate_property_views.xml',
         'views/estate_property_types_views.xml',
         'views/estate_property_tag_views.xml',
         'views/estate_property_offer_views.xml',
