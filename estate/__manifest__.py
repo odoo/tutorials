a23543e89f94f97033ba817c45e02fa30279359d
malm-odoo <malm@odoo.com>
2024-10-25 11:24:38 +0200

    [FIX] estate: Fixed views order in manifest

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,11 +6,11 @@
     ],
     'data': [
         'security/ir.model.access.csv',
-        'views/estate_menus.xml',
         'views/estate_property_views.xml',
+        'views/estate_property_offer_views.xml',
         'views/estate_property_types_views.xml',
         'views/estate_property_tag_views.xml',
-        'views/estate_property_offer_views.xml',
+        'views/estate_menus.xml',
         'views/users.xml',
     ],
     'license': "LGPL-3",
