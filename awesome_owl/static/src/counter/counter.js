e749a1de500bd50d918a91eb6dfa2ff73f1701c6
David Van Droogenbroeck (DROD) <drod@odoo.com>
2024-10-28 11:53:01 +0100

    [ADD] awesome_owl: Finish Chapter 6
    [ADD] awesome_owl: Add ugly-ass boxes (Chapter 3)
    
    [IMP] awesome_owl: Finish Chapter 4
    
    I hate JavaScript
    I hate JavaScript
    I hate JavaScript
    I hate JavaScript
    I hate JavaScript
    I hate JavaScript
    I hate JavaScript
    I hate JavaScript
    I hate JavaScript
    I hate JavaScript
    I hate JavaScript
    I hate JavaScript
    I hate JavaScript
    I hate JavaScript
    I hate JavaScript
    I hate JavaScript
    I hate JavaScript
    I hate JavaScript
    I hate JavaScript
    I hate JavaScript
    I hate JavaScript
    I hate JavaScript
    I hate JavaScript
    I hate JavaScript
    I hate JavaScript
    I hate JavaScript
    I hate JavaScript
    
    [IMP] awesome_owl: Finish Chapter 5
    
    [IMP] awesome_owl: Finish Chapter 6

---
+++ awesome_owl/static/src/counter/counter.js
@@ -0,0 +1,21 @@
+/** @odoo-module **/
+
+import { Component, useState, xml } from "@odoo/owl";
+
+export class Counter extends Component {
+    static template = "awesome_owl.counter";
+
+    static props = {
+        onChange: { type: Function }
+    };
+
+    setup() {
+        this.state = useState({ value: 1});
+    }
+    
+    increment() {
+        this.state.value++;
+        if (this.props.onChange)
+            this.props.onChange();
+    }
+}
--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,7 +1,26 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, markup, useState } from "@odoo/owl";
+
+// Custom elements
+import { Card } from "./card/card";
+import { Counter } from "./counter/counter";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
+
+    static components = { Card, Counter };
+
+    setup() {
+        this.card_contents = [
+            { title: "Title 1", content: "<div>Text content 1</div>" },
+            { title: "Title 2", content: markup("<div>Text content 2</div>") }
+        ];
+
+        this.state = useState({ total: 2});
+    }
+
+    incrementSum() {
+        this.state.total++;
+    }
 }
