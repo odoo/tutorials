3d906fb5dd6d527241f15c78074070152b8d8b77
sahm-odoo <sahm@odoo.com>
2024-11-05 14:16:11 +0530

    [IMP] stock_transport: integrate graph and gantt views
    - Added a Graph View to visualize key measures, including volume, weight,
    transfers, and lines.
    - Introduced a customized Gantt View
    - Modified the display name to provide better batch identification.
    - Set default grouping by Dock and Fleet together.
    - Included visual indicators such as decoration-success, decoration-info, and
    decoration-warning based on the batch’s status.

--- stock_transport/models/stock_picking.py
+++ stock_transport/models/stock_picking.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class Picking(models.Model):
@@ -6,7 +6,10 @@ class Picking(models.Model):
 
     volume = fields.Float(compute="_compute_volume")
 
+    @api.depends('move_ids.product_id.volume')
     def _compute_volume(self):
         for record in self:
+            total_volume = 0
             for move_line in record.move_ids:
-                record.volume = record.volume + move_line.quantity * move_line.product_id.volume
+                total_volume = total_volume + move_line.quantity * move_line.product_id.volume
+            record.volume = total_volume
--- stock_transport/models/stock_picking_batch.py
+++ stock_transport/models/stock_picking_batch.py
@@ -4,38 +4,83 @@ from odoo import fields, models, api
 class stockPickingBatch(models.Model):
     _inherit = 'stock.picking.batch'
 
-    weight = fields.Float(compute="_compute_weight", string='Weight')
-    volume = fields.Float(compute="_compute_volume", string='Volume')
+    weight_percentage = fields.Float(compute="_compute_weight_percentage", string='Weight')
+    volume_percentage = fields.Float(compute="_compute_volume_percentage", string='Volume')
+
+    weight = fields.Float(string="Weight", compute="_compute_weight", store=True)
+    volume = fields.Float(string="Volume", compute="_compute_volume", store=True)
+    number_of_transfer = fields.Integer(string="Transfer", compute="_compute_number_of_transfer", store=True)
+    number_of_line = fields.Integer(string="Line", compute="_compute_number_of_line", store=True)
 
     dock_id = fields.Many2one(comodel_name='dock', string="Dock")
     vehicle_id = fields.Many2one(comodel_name='fleet.vehicle', string="Vehicle")
-    category_id = fields.Many2one(comodel_name='fleet.vehicle.model.category', string='Category', compute='_compute_category')
+    category_id = fields.Many2one(comodel_name='fleet.vehicle.model.category', string='Category', compute='_compute_category', store="True")
+
+    def _compute_display_name(self):
+        super()._compute_display_name()
+        for record in self:
+            record.display_name = f"{record.name}: {record.weight} kg, {record.volume} m3"
 
     @api.depends('vehicle_id')
     def _compute_category(self):
         for record in self:
             record.category_id = record.vehicle_id.category_id.id
 
-    def _compute_weight(self):
+    @api.depends('picking_ids', 'picking_ids.move_ids', 'category_id.max_weight', 'picking_ids.move_ids.product_id.weight')
+    def _compute_weight_percentage(self):
         for record in self:
             max_weight = record.category_id.max_weight
+            total_weight = 0
             for picking_line in record.picking_ids:
                 for move_line in picking_line.move_ids:
-                    record.weight = record.weight + move_line.product_id.weight * move_line.quantity
-            total_weight = record.weight
+                    total_weight = total_weight + move_line.product_id.weight * move_line.quantity
             if max_weight == 0:
-                record.weight = 0.0
+                record.weight_percentage = 0.0
             else:
-                record.weight = total_weight / max_weight * 100
+                record.weight_percentage = total_weight / max_weight * 100
 
-    def _compute_volume(self):
+    @api.depends('picking_ids', 'picking_ids.move_ids', 'category_id.max_volume', 'picking_ids.move_ids.product_id.volume')
+    def _compute_volume_percentage(self):
         for record in self:
             max_volume = record.category_id.max_volume
+            total_volume = 0
             for picking_line in record.picking_ids:
                 for move_line in picking_line.move_ids:
-                    record.volume = record.volume + move_line.product_id.volume * move_line.quantity
+                    total_volume = total_volume + move_line.product_id.volume * move_line.quantity
             if max_volume == 0:
-                record.volume = 0.0
+                record.volume_percentage = 0.0
             else:
-                record.volume = record.volume / max_volume * 100
-        
+                record.volume_percentage = total_volume / max_volume * 100
+    
+    @api.depends('picking_ids', 'picking_ids.move_ids', 'picking_ids.move_ids.product_id.weight')
+    def _compute_weight(self):
+        for record in self:
+            total_weight = 0
+            for picking_line in record.picking_ids:
+                for move_line in picking_line.move_ids:
+                    total_weight = total_weight + move_line.product_id.weight * move_line.quantity
+            record.weight = total_weight
+
+    @api.depends('picking_ids', 'picking_ids.move_ids', 'picking_ids.move_ids.product_id.volume')
+    def _compute_volume(self):
+        for record in self:
+            total_volume = 0
+            for picking_line in record.picking_ids:
+                for move_line in picking_line.move_ids:
+                    total_volume = total_volume + move_line.product_id.volume * move_line.quantity
+            record.volume = total_volume
+
+    @api.depends('picking_ids')
+    def _compute_number_of_transfer(self):
+        for record in self:
+            record.number_of_transfer = len(record.picking_ids)
+    
+    @api.depends('picking_ids', 'picking_ids.move_ids')
+    def _compute_number_of_line(self):
+        for record in self:
+            total_lines = 0
+            for picking_line in record.picking_ids:
+                    total_lines = total_lines + len(picking_line.move_ids)
+            record.number_of_line = total_lines
+
+
