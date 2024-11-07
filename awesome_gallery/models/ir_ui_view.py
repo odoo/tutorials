cb7eef680e697bd1fa379081b239367e9bb04085
cove <cover4work@gmail.com>
2024-11-07 15:33:23 +0100

    [FIX] awesome_gallery: fix setup producing a KeyError on gallery
    ===================
    Steps to Reproduce:
    ===================
    
    1. Checkout the origin/18.0 branch for the tutorial repo and install the
     Awesome_Gallery module.
    2. Try to reconnect, you get and error with following traceback:
    `... File "/home/odoo/work/odoo/addons/web/models/ir_ui_view.py", line
    14, in <dictcomp> 'icon': _view_info[type_]['icon'], KeyError: 'gallery'
    `
    3. Try applying the first step of solution 6.1 from the
    origin/18.0-solutions branch. The error persists and you cannot do the
    tutorial.
    
    ==================
    After this commit:
    ==================
    
    The module can be installed without errors, and you can proceed with the
    tutorial without issues.
    Note: The 18.0-solutions branch should be updated with the new v18
    tutorial, which now focuses on the Contact app instead of the
    Awesome_Tshirt app.
    
    task-xxxx

--- awesome_gallery/models/ir_ui_view.py
+++ awesome_gallery/models/ir_ui_view.py
@@ -6,3 +6,8 @@ class View(models.Model):
     _inherit = 'ir.ui.view'
 
     type = fields.Selection(selection_add=[('gallery', "Awesome Gallery")])
+
+    def _get_view_info(self):
+        infos = super()._get_view_info()
+        infos['gallery'] = {'icon': 'fa fa-image'}
+        return infos
