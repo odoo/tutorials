ef407760b543da2aa70af2f78e7f36f13b6e6312
Majed Alhanash (malh) <malh@odoo.com>
2024-11-14 13:05:38 +0400

    [IMP] estate: Added default action for estate property

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -8,6 +8,7 @@
     """,
     'data': [
         'security/ir.model.access.csv',
-        
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ],
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,6 @@
 from odoo import fields, models
+from datetime import date
+from dateutil.relativedelta import relativedelta
 
 class EstatePropertyModel(models.Model):
     _name = "estate.property"
@@ -6,17 +8,18 @@ class EstatePropertyModel(models.Model):
 
 
     title = fields.Char(required=True)
+    active = fields.Boolean(default=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availablility = fields.Date()
+    date_availablility = fields.Date(default=date.today() + relativedelta(months=3), copy=False)
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True)
-    bedrooms = fields.Integer()
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection([('EAST','East'),('WEST','West'), ('SOUTH','South'), ('NORTH','North')])
-    state = fields.Selection([("NEW","New"), ("OFFER_RECEIVED","Offer Received"), ("OFFER_ACCEPTED","Offer Accepted"), ("SOLD","Sold"), ("CANCELLED","Cancelled")], required=True)
+    state = fields.Selection([("NEW","New"), ("OFFER_RECEIVED","Offer Received"), ("OFFER_ACCEPTED","Offer Accepted"), ("SOLD","Sold"), ("CANCELLED","Cancelled")], required=True, copy=False, default="NEW")
     
