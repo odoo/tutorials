e62a678c2ae639abf251b57442c9e94a212c9a94
Majed Alhanash (malh) <malh@odoo.com>
2024-11-14 11:15:19 +0400

    [ADD] estate: Create a new module estate estate is module for handling real estate operations like selling properties

---
+++ estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "malh",
+    'description': """
+    Real estate tutorial application
+    """,
+    
+}
