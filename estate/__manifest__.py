7c96779d988d9f8c2cbcc859672681265a25555b
Abdelrahman Mahmoud (amah) <amah@odoo.com>
2024-11-19 10:20:54 +0100

    [IMP] estate: Chapter 4 - estate.property: security base file

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,6 +4,10 @@
     'depends': ['base'],
     'author': "Abdelrahman Mahmoud (amah)",
     'category': 'Training',
+
+    'data' :[
+        'security/ir.model.access.csv',
+    ],
     'installable': True,
     'application': True,
 }
