f92dd2b3827e4837bc13c0a611b50700281e414c
Abdelrahman Mahmoud (amah) <amah@odoo.com>
2024-11-19 10:12:12 +0100

    [IMP] estate: Chapter 3 - base of property model - estate.property: the basic fields - estate.property: mark the requird fields

---
+++ estate/models/estate_property.py
@@ -0,0 +1,26 @@
+from odoo import models, fields
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Property data model"
+
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        selection=[('n', 'North'), 
+                   ('s', 'South'),
+                   ('e', 'East'),
+                   ('w', 'West'),
+                   ],
+    )
+
