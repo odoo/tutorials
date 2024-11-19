37780a7608727c68ae621093197206d2a1e5b2fe
cemalfarukguney <cemalfarukguney@gmail.com>
2024-11-19 11:23:54 +0100

    Chapter 2 (cgun)

---
+++ estate/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Estate",
+
+    'summary': """
+        Tutorial starting module
+    """,
+
+    'description': """
+        Tutorial starting module
+    """,
+
+    'author': "Cemal Faruk Guney",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Tutorials/Estate',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+    'application': True,
+    'installable': True,
+    'license': 'AGPL-3'
+}
