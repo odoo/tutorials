89f8ee860fd02b00424a65a0dc1ffe536e8ce7cd
Abdelrahman Mahmoud (amah) <amah@odoo.com>
2024-11-19 09:26:33 +0100

    [ADD] estate: chapter 2 add the base for manifest

---
+++ estate/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Abdelrahman Mahmoud (amah)",
+    'category': 'Training',
+    'installable': True,
+    'application': True,
+}
