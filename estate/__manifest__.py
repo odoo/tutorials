68a0e8da3fa9f531aec90de039952839008b2578
Stéphane Vanmeerhaeghe (stva) <stva@odoo.com>
2024-11-19 10:24:59 +0100

    [ADD] estate: new module created
    - New model 'property' created
    - Security: give read, write, create and unlink permissions to the group base.group_user.
    - Basic list, form views added

---
+++ estate/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Estate',
+    'version': '0.1',
+    'depends': ['base'],
+    'summary': 'Estate module',
+    'category': 'Tutorials/Estate',
+    'application': True,
+    'installable': True,
+    'license': 'AGPL-3',
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+    ]
+}
---
+++ estate/models/property.py
@@ -0,0 +1,32 @@
+from dateutil.relativedelta import relativedelta
+from odoo import fields, models
+
+
+def _default_date_availability():
+    return fields.Date.today() + relativedelta(months=3)
+
+
+class EstateProperty(models.Model):
+    _name = 'estate.property'
+    _description = "Estate Property"
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(copy=False,
+                                    default=_default_date_availability,
+                                    string='Available from')
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer(string='Living area (sqm)')
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer(string='Garden area (sqm)')
+    garden_orientation = fields.Selection(
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
+    active: bool = fields.Boolean(default=True)
+    state = fields.Selection(copy=False, default='New', required=True,
+                             selection=[('New', 'New'), ('Offer Received', 'Offer Received'),
+                                        ('Offer Accepted', 'Offer Accepted'), ('Sold', 'Sold'),
+                                        ('Cancelled', 'Cancelled')])
