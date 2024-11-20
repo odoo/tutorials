f2929c972e427c93d5ab237d94959f8c99353f94
nape-odoo <nape@odoo.com>
2024-11-20 11:16:48 +0100

    [IMP] estate: Tutorial chapter 8 - computed and onchange fields for the models

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # licence
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -18,9 +18,10 @@ class EstateProperty(models.Model):
     living_area = fields.Integer('Living area surface')
     facades = fields.Integer('No. of facades')
     garage = fields.Boolean('Has a garage')
-    garden = fields.Boolean('Has a garden')
-    garden_area = fields.Integer('Gargen area surface')
-    garden_orientation = fields.Selection(string='Garden orientation', selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
+    garden = fields.Boolean('Has a garden', default=False)
+    garden_area = fields.Integer('Gargen area surface', default=0)
+    garden_orientation = fields.Selection(string='Garden orientation',
+                                          selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')], default=None)
     # Reserved
     active = fields.Boolean('Active', default=True)
     state = fields.Selection(string='State', selection=[('new', 'New'), ('recieved', 'Offer Received'),
@@ -32,3 +33,27 @@ class EstateProperty(models.Model):
     offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
     buyer_id = fields.Many2one("res.partner", string="Buyer")
     salesperson_id = fields.Many2one("res.users", string="Salesperson") #, default=self.env.user) # <-- apparently this is not how it works
+    # Computed
+    area_total = fields.Float('Total area', compute='_compute_area_total', readonly=True)
+    best_offer = fields.Float('Best offer', compute='_compute_best_offer', readonly=True)
+    
+    def _compute_area_total(self):
+        for record in self:
+            record.area_total = record.living_area + record.garden_area
+            
+    @api.depends("offer_ids.price")
+    def _compute_best_offer(self):
+        for record in self:
+            best = 0
+            for offer in record.offer_ids:
+                if offer.price > best: best = offer.price
+            record.best_offer = best
+            
+    @api.onchange("garden")
+    def _onchange_garden(self):
+        if self.garden == True:
+            self.garden_area = 10
+            self.garden_orientation = 'north'
+        else:
+            self.garden_area = 0
+            self.garden_orientation = None
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -10,8 +10,19 @@ class EstatePropertyOffer(models.Model):
 
     name = fields.Char('Name', required=False, default="- no name -")
     price = fields.Float('Offer price', required=True)
+    validity = fields.Integer('Validity time')
     # Reserved
     state = fields.Selection(string="Status", selection=[('new', 'New'), ('accepted', 'Accepted'), ('refused', 'Refused')], default='new', required=True)
     # Relational
     property_id = fields.Many2one("estate.property", string="Property", required=True)
     partner_id = fields.Many2one("res.partner", string="Buyer", required=True)
+    # Computed
+    date_deadline = fields.Date("Deadline date", compute='_compute_deadline', inverse='_inverse_deadline')
+    
+    def _compute_deadline(self):
+        for record in self:
+            record.date_deadline = fields.Date.add(record.create_date.date(), days=record.validity)
+            
+    def _inverse_deadline(self):
+        for record in self:
+            record.validity = (fields.Date.delta(record.create_date.date(), record.date_deadline)).days
