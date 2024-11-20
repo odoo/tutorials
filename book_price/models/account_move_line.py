299e6a2f6c8dc053acba6814b157f14640540a72
Florian(flg) <flg@odoo.com>
2024-11-20 08:07:03 +0000

    [ADD] book_price: Display 'Book Price' on Sales Order and Invoice Lines
    - Created a new module 'book_price' to display the original pricelist price ('Book Price').
    - Added a computed field to Sales Order Lines and Account Move Lines to fetch the relevant pricelist price based on the selected pricelist, product, and quantity.
    - Updated the Invoice Lines view to display the 'Book Price' field.
    - Updated the Sales Order Lines view to include the 'Book Price' field for price comparison.
    Task ID : 4598912

---
+++ book_price/models/account_move_line.py
@@ -0,0 +1,8 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models
+
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+
+    book_price = fields.Float(string='Book Price', store=True)
