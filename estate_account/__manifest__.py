58e2165a24c0954e2d911b3627b27c96d84e6121
Abdelrahman Mahmoud (amah) <amah@odoo.com>
2024-11-21 15:13:40 +0100

    [IMP] estate_account: improve code style compatibility

--- estate_account/__manifest__.py
+++ estate_account/__manifest__.py
@@ -2,16 +2,7 @@
     'name': "Real Estate Accounting",
     'version': '1.0',
     'depends': ['base', 'estate', 'account'],
-    'author': "Abdelrahman Mahmoud (amah)",
     'category': 'Training',
 
-    'data': [
-
-        # views
-
-        # menu
-
-    ],
-    'installable': True,
     'application': True,
 }
