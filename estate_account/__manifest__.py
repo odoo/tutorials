12efc30d619422e577dbb3abb48ffccc33e01b88
cemalfarukguney <cemalfarukguney@gmail.com>
2024-11-22 10:42:07 +0100

    Chapter 13  (cgun)

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Estate Account",
+    "summary": """
+        Link module for invoicing real estate. 
+    """,
+    "description": """
+        Link module for invoicing real estate. 
+    """,
+    "author": "Cemal Faruk Guney",
+    "category": "Tutorials/Estate",
+    "version": "0.1",
+    "depends": ["base", "estate", "account"],
+    "application": True,
+    "data": [],
+    "license": "AGPL-3",
+}
---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,30 @@
+from odoo import models, Command
+
+
+class EstateProperty(models.Model):
+    _inherit = 'estate.property'
+
+    def action_sold(self):
+        for record in self:
+            invoice_vals = {
+                'partner_id': record.buyer_id.id,
+                'move_type': 'out_invoice',
+                'invoice_line_ids': [
+                    Command.create(
+                        {
+                            'name': record.name,
+                            'quantity': 1,
+                            'price_unit': record.selling_price * 0.06,
+                        }
+                    ),
+                    Command.create(
+                        {
+                            'name': "Administrative Fees",
+                            'quantity': 1,
+                            'price_unit': 100,
+                        }
+                    ),
+                ],
+            }
+            self.env['account.move'].create(invoice_vals)
+            return super().action_sold()
