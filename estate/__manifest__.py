ece13e26e8aaadba24b83780582930c93cbf3d92
Stephane vm (stva) <stva@odoo.com>
2024-11-27 11:29:13 +0100

    [IMP] estate:  add module data

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -9,9 +9,13 @@
     'data': [
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
+        'views/estate_property_offer_views.xml',
         'views/estate_property_type_views.xml',
         'views/estate_property_tag_views.xml',
-        'views/estate_property_offer_views.xml',
         'views/res_users_views.xml',
+        'data/master_data.xml',
+    ],
+    "demo": [
+        "demo/demo_data.xml",
     ]
 }
