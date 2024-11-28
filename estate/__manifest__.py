62cb1bcdd2c2129a5e750b627237e7ef76725187
Dhruvkumar Nagar <dhna@odoo.com>
2024-11-28 18:47:13 +0530

    [ADD] estate:  Added demo data for estate model and some values are added
    Added new model for demo data and added XML file for that views
    Added 3 properties and added 3 offers in property for demonstration.,

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,6 +1,8 @@
 {
     'name': 'estate',
     'version': '1.0',
+    'author' : "DhruvKumar Nagar",
+    "description": "Real estate module for all your property needs!",
     'depends': [
         'base',
     ],
@@ -10,5 +12,8 @@
         'security/ir.model.access.csv',    
         'views/estate_property_views.xml',
         'views/estate_menus.xml',
+    ],
+    'demo':[
+        'demo/estate_property_demo.xml'
     ]
 }
