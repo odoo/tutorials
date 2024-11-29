8f56fcb716627ac4730d0d60c6c186b97fe3c33e
Dhruvkumar Nagar <dhna@odoo.com>
2024-11-29 18:54:53 +0530

    [ADD] estate: added new report pdfs
    I added code for property-related PDF files. All values are in the action
    section for easy download. I also added predefined demo data
    for easy  to understand

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -12,6 +12,12 @@
         'security/ir.model.access.csv',    
         'views/estate_property_views.xml',
         'views/estate_menus.xml',
+        'report/estate_property_offers_templates.xml',
+        'report/estate_property_templates.xml',
+        'report/estate_property_reports.xml',
+        'report/res_users_templates.xml',
+        'report/res_users_reports.xml',
+
     ],
     'demo':[
         'demo/estate_property_demo.xml'
