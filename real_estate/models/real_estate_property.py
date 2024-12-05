795abc85e99a672c9433a29817eca4129ba9f49d
Antoine Vandevenne (anv) <anv@odoo.com>
2024-12-05 16:39:24 +0100

    chap 5 - computed fields

--- real_estate/models/real_estate_property.py
+++ real_estate/models/real_estate_property.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class RealEstateProperty(models.Model):
@@ -36,10 +36,25 @@ class RealEstateProperty(models.Model):
     garden_area = fields.Integer(
         string="Garden Area", help="The garden area excluding the building."
     )
+    total_area = fields.Integer(string="Total Area", compute='_compute_total_area')
     address_id = fields.Many2one(string="Address", comodel_name='res.partner', required=True)
     seller_id = fields.Many2one(string="Seller", comodel_name='res.partner', required=True)
     salesperson_id = fields.Many2one(string="Salesperson", comodel_name='res.users')
     offer_ids = fields.One2many(
         string="Offers", comodel_name='real.estate.offer', inverse_name='property_id'
     )
+    best_offer_amount = fields.Float(string="Best Offer", compute='_compute_best_offer_amount')
     tag_ids = fields.Many2many(string="Tags", comodel_name='real.estate.tag')
+
+    @api.depends('floor_area', 'garden_area')
+    def _compute_total_area(self):
+        for property in self:
+            property.total_area = property.floor_area + property.garden_area
+
+    @api.depends('offer_ids.amount')
+    def _compute_best_offer_amount(self):
+        for property in self:
+            if property.offer_ids:
+                property.best_offer_amount = max(property.offer_ids.mapped('amount'))
+            else:
+                property.best_offer_amount = 0
