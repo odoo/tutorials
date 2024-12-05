3e5bf34dd12de70ce886a3da51e97678c6f8db74
shai-odoo <shai@odoo.com>
2024-12-05 22:34:49 +0530

    Created a model, manifest and init files

---
+++ estate/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'estate',
+
+    'category': 'real estate',
+
+    'summary': 'create a estate property',
+
+    'website': 'https://www.odoo.com',
+
+    'depends': ['base'],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
---
+++ estate/models/estate_property.py
@@ -0,0 +1,7 @@
+from odoo import models, fields
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Real Estate Property"
+
+# more to understand
