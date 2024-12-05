ced8706c0376cb378ea72f11901b89f95f28ed6b
Antoine Vandevenne (anv) <anv@odoo.com>
2024-12-05 16:13:25 +0100

    chap 4 - one2many field

---
+++ real_estate/models/real_estate_offer.py
@@ -0,0 +1,26 @@
+from odoo import fields, models
+
+
+class RealEstateOffer(models.Model):
+    _name = 'real.estate.offer'
+    _description = "Real Estate Offer"
+
+    amount = fields.Float(string="Amount", required=True)
+    buyer_id = fields.Many2one(string="Buyer", comodel_name='res.partner', required=True)
+    date = fields.Date(string="Date", required=True)
+    validity = fields.Integer(
+        string="Validity", help="The number of days before the offer expires.", default=7
+    )
+    state = fields.Selection(
+        string="State",
+        selection=[
+            ('waiting', "Waiting"),
+            ('accepted', "Accepted"),
+            ('refused', "Refused"),
+        ],
+        required=True,
+        default='waiting',
+    )
+    property_id = fields.Many2one(
+        string="Property", comodel_name='real.estate.property', required=True
+    )
