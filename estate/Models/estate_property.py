9a4bd340bacc955e7beb38d1967a4233dab8d438
adip-odoo <adip@odoo.com>
2024-12-06 19:02:21 +0530

    [IMP] estate: define access rights access and add view for properties
    This update enhances the Estate module by implementing access control and
    creating a user interface for managing properties.
    A CSV file defining security access rights has been added to regulate
    permissions for different user roles. Additionally, action and XML files have
    been created to configure and display views for the estate. Property model.
    
    These enhancements ensure that properties are both secure and easily manageable
    through the module's interface, supporting seamless interaction while
    maintaining data integrity.

--- estate/Models/estate_property.py
+++ estate/Models/estate_property.py
@@ -1,24 +1,31 @@
-import datetime
 from odoo import models, fields
 
-class TestModel(models.Model):
-    _name = "estate.property"
-    _description = "Real Estate Property"
-    name = fields.Char(required=True)
+class EstateProperty(models.Model):
+    _name = 'estate.property'
+    _description = 'Real Estate Property'
+    name = fields.Char(required=True, string='Title', trim=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(string='Available From', copy=False, default=fields.Date.add(fields.Date.today(),months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
-    living_area = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False, default=0.0)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer(string='Living Area (sqm)')
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Integer()
+    garden_area = fields.Integer(string='Garden Area (sqm)')
     garden_orientation = fields.Selection(selection=[
             ('north', 'North'),
             ('south', 'South'),
             ('east', 'East'),
             ('west', 'West'),
             ])
+    state = fields.Selection(selection=[
+        ('new', 'New'),
+        ('received', 'Offer Received'),
+        ('accepted', 'Offer Accepted'),
+        ('sold', 'Sold'),
+        ('cancelled', 'Cancelled'),
+    ], default='new')
+    active = fields.Boolean(default=True)
--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,9 +5,18 @@
     'name': 'Real Estate',
     'version': '1.0',
     'author': "Aditi Patel",
-    'category': 'Aditi/Real Estate',
+    'category': 'Real Estate',
     'summary': "Realistic business advertisements",
     'description': "our ultimate real estate spotlight! Effortlessly showcase properties and attract buyers with sleek, engaging ads that sell.",
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+    ],
+    'license': "LGPL-3",
     'application': True,
     'installable': True,
     'auto_install': False
