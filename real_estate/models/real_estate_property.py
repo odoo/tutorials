7f082aa8f614871a14c55ac522d17bd73a06c828
Antoine Vandevenne (anv) <anv@odoo.com>
2024-12-06 14:57:09 +0100

    chap 5 - related fields

--- real_estate/models/real_estate_property.py
+++ real_estate/models/real_estate_property.py
@@ -40,6 +40,7 @@ class RealEstateProperty(models.Model):
     )
     total_area = fields.Integer(string="Total Area", compute='_compute_total_area', store=True)
     address_id = fields.Many2one(string="Address", comodel_name='res.partner', required=True)
+    street = fields.Char(string="Street", related='address_id.street', store=True)
     seller_id = fields.Many2one(string="Seller", comodel_name='res.partner', required=True)
     salesperson_id = fields.Many2one(string="Salesperson", comodel_name='res.users')
     offer_ids = fields.One2many(
