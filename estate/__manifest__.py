7a3b2fecef25a67ecb05938b01841c5091822c06
shka-odoo <shka@odoo.com>
2024-12-06 18:47:11 +0530

    [IMP] estate: add action, menu, and list view for Properties
    - Created the necessary files and folder structure for the module:
      - `__init__.py` for initializing models.
      - `__manifest__.py` for module metadata and dependencies.
    
    - Developed the main model `estate. property` with the following fields:
      - Basic fields: `name`, `description`, `postcode`, `date_availability`,
     `expected_price`, `selling_price`, `bedrooms`, `living_area`, `facades`,
    `garage`,`garden`, `garden_area`, `garden_orientation`.
      - create action view and list view
    
    - Ensured modularity and reusability by testing various menu structures and
    field types.
    
    This commit represents the foundational setup of the Estate module as part of
    the technical training.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,8 +5,14 @@
     'summary': 'use to buy and sell properties ',
     'depends' : ['base'],
     'description': "buy and sell",
+    'data':[
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
+
+    ],
     'application' : True ,
     'installable' : True ,
-    'auto_install' : False,
-
+    'auto_install' : False,  
+    'license': 'LGPL-3',
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import models, fields
+from datetime import timedelta, date
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -7,10 +8,14 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True, string="Title")
     description = fields.Text(string="Description")
     postcode = fields.Char(string="Postcode")
-    date_availability = fields.Date(string="Available From")
+    date_availability = fields.Date(
+        string="Available From",
+        default=date.today() + timedelta(days=90),
+        copy=False
+        )
     expected_price = fields.Float(required=True, string="Expected Price")
-    selling_price = fields.Float(string="Selling Price")
-    bedrooms = fields.Integer(string="Bedrooms")
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(string="Bedrooms", default=2)
     living_area = fields.Integer(string="Living Area (sqm)")
     facades = fields.Integer(string="Number of Facades")
     garage = fields.Boolean(string="Has Garage?")
@@ -24,4 +29,17 @@ class EstateProperty(models.Model):
             ("west", "West"),
         ],
         string="Garden Orientation",
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        selection=[
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled"),
+        ],
+        required=True,
+        default="new",
+        copy=False,
     )
