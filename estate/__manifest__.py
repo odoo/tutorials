6ae2f46a03880548982e974635f9cac4669430e0
kkuj-odoo <kkuj@odoo.com>
2024-12-06 11:40:26 +0530

    [ADD] estate: making real_estate module as training program

---
+++ estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': "Real Estate",
+    'depends': ['base'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': LGPL-3,
+    'data': [
+        "security/ir.model.access.csv",
+    ]
+}     
---
+++ estate/models/estate_property.py
@@ -0,0 +1,22 @@
+from odoo import models, fields
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Estate Property"
+
+    name = fields.Char(required=True, string="Name")
+    description = fields.Text(string="Description")
+    postcode = fields.Char(string="Postcode")
+    date_availability = fields.Date(string="Date Availability")
+    expected_price = fields.Float(required=True, string="Expected Price")
+    selling_price = fields.Float(string="Selling Price")
+    bedrooms = fields.Integer(string="Bedrooms")
+    living_area = fields.Integer(string="Living Area")
+    facades = fields.Integer(string="Facades")
+    garage = fields.Boolean(string="Garage")
+    garden = fields.Boolean(string="Garden")
+    garden_area = fields.Integer(string="Garden Area")
+    garden_orientation = fields.Selection(
+        string='Garden Orientation',
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
+    )
