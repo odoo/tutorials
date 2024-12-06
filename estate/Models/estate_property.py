b52f98358ef7b94a20c48a6b0708681d86857e91
adip-odoo <adip@odoo.com>
2024-12-06 11:40:11 +0530

    [ADD] estate: create a new estate property module
    This estate module will store the real estate property-related information.
    
    It includes fields to capture essential data such as
    the property's name, description, location (postcode), availability date, and
    pricing details, including the expected and selling price.
    
    Additionally, it tracks key features like the number of bedrooms,
    living area size, and the number of facades. The module also provides options to
    indicate if the property includes a garage or garden, with additional details
    such as garden size and orientation (North, South, East, or West).

---
+++ estate/Models/estate_property.py
@@ -0,0 +1,24 @@
+import datetime
+from odoo import models, fields
+
+class TestModel(models.Model):
+    _name = "estate.property"
+    _description = "Real Estate Property"
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(selection=[
+            ('north', 'North'),
+            ('south', 'South'),
+            ('east', 'East'),
+            ('west', 'West'),
+            ])
---
+++ estate/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'author': "Aditi Patel",
+    'category': 'Aditi/Real Estate',
+    'summary': "Realistic business advertisements",
+    'description': "our ultimate real estate spotlight! Effortlessly showcase properties and attract buyers with sleek, engaging ads that sell.",
+    'application': True,
+    'installable': True,
+    'auto_install': False
+}
