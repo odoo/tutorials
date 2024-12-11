56dbb8bce55e9ddfa3dd062c0851fe0a66751cd3
shai-odoo <shai@odoo.com>
2024-12-11 10:13:33 +0530

    updated info

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -16,10 +16,10 @@
     'security/ir.model.access.csv',
     'views/estate_property_views.xml',
     'views/estate_property_type_views.xml',
-    'views/estate_property_tags_views.xml',
+    # 'views/estate_property_tags_views.xml',
     #'views/estate_property_offer_views.xml',
-    'views/estate_menu_type_xml',
-    'views/estate_best_views.xml',
+    # 'views/estate_menu_type_xml',
+    # 'views/estate_best_views.xml',
     'views/estate_menus.xml'
     ],
 
