f798314c3b80c8c423bbcac73c2618602ad09d19
Hammad Arif <arih@odoo.com>
2024-12-12 10:52:19 +0400

    [ADD] estate: add new module app named as estate
    Created new module to add another app named as estate for real estate operations. It contains summary, description, depends, and other values as well. Chapter 2 of the server framework 101

---
+++ estate/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Estate',
+    'summary': """
+        Starting module for "Server framework 101, chapter 2: Build an estate app"
+    """,
+
+    'description': """
+        Starting module for "Server framework 101, chapter 2: Build an estate app"
+    """,
+
+    'author': "Odoo",
+    'website': "https://www.odoo.com/",
+    'category': 'Tutorials/Estate',
+    'version': '0.1',
+    'application': True,
+    'installable': True,
+    'depends': ['base', 'web'],
+
+    'data': []
+}
