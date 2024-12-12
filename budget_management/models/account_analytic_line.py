08d29ab51449b27b28f6798d3ff00477da5f77c7
dhruv <djip@odoo.com>
2024-12-12 17:01:05 +0530

    [IMP] budget_management: added analytic account and updated accordingly
    Added analytic account in wizard which add selected accounts to each budget
    while creating it.
    Updated button for state management of the budget.
    Edited action_on_revise button to post message in chatter when it create
    revised budget for it.
    Updated functions to compute achive amount and achive percentage.

--- budget_management/models/account_analytic_line.py
+++ budget_management/models/account_analytic_line.py
@@ -4,42 +4,79 @@ from odoo.exceptions import ValidationError
 
 class AccountAnalyticLine(models.Model):
     _inherit = "account.analytic.line"
-
-    budget_line_id = fields.Many2one(comodel_name="budget.management.budget.lines")
-
+   
     @api.model_create_multi
     def create(self, vals_list):
         # print("* " * 100)
         # print(vals_list)
         # print("* " * 100)
+        
         for vals in vals_list:
-            budget_line = self.env["budget.management.budget.lines"].browse(
-                vals.get("budget_line_id")
-            )
-            budget = budget_line.budget_id
-            if budget.on_over_budget == "restriction":
-                if sum(budget_line.analytic_line_ids.mapped("amount"))+vals.get("amount") > budget_line.budget_amount:
-                    raise ValidationError(
-                        "You cannot create a budget line because it exceeds the allowed budget!"
-                    )
+            entry_date = vals.get("date")
+            if not entry_date:
+                raise ValidationError("The date field is required to determine the appropriate budget.")
+            
+            budget_line = self.env["budget.management.budget.lines"].search([
+                ("budget_id.date_start", "<=", entry_date),
+                ("budget_id.date_end", ">=", entry_date),
+                ("budget_id.active", "=", True),
+                ("analytic_account_id", "=", vals.get("account_id"))
+            ], limit=1)
+
+            if budget_line:
+                budget = budget_line.budget_id
+
+                analytic_account_lines = self.env["account.analytic.line"].search_read(
+                    [
+                        ("account_id", "=", vals.get("account_id")),
+                        ("date", ">=", budget.date_start),
+                        ("date", "<=", budget.date_end),
+                        ("amount", "<", 0),
+                    ],
+                    fields=["amount"],
+                )
+                # print(list(line.get("amount") for line in analytic_account_lines))
+                achieved = (sum(line.get("amount") for line in analytic_account_lines))
+                # print(budget.on_over_budget, abs(achieved + vals.get("amount")), budget_line.budget_amount)
+
+                if budget.on_over_budget == "restriction":
+                    if abs(achieved + vals.get("amount")) > budget_line.budget_amount:
+                        raise ValidationError(
+                            "You cannot create a budget line because it exceeds the allowed budget!"
+                        )
         return super(AccountAnalyticLine, self).create(vals_list)
 
     def write(self, vals):
-        # print("* " * 100)
-        # print(vals)
-        # print("* " * 100)
-        if "amount" in vals:
+        if "date" in vals or "amount" in vals or "account_id" in vals:
             for record in self:
-                old_amount = record.amount
-                new_amount = vals.get("amount")
-                print(old_amount,new_amount)
-                total_amount = sum(record.budget_line_id.analytic_line_ids.mapped("amount")) + new_amount - old_amount
+                entry_date = vals.get("date", record.date)
 
-                budget_line = record.budget_line_id
-                budget = budget_line.budget_id
-                if budget.on_over_budget == "restriction" and total_amount > budget_line.budget_amount:
-                    raise ValidationError(
-                        "You cannot update this budget line because it exceeds the allowed budget!"
-                    )          
-                                                          
+                budget_line = self.env["budget.management.budget.lines"].search([
+                    ("budget_id.date_start", "<=", entry_date),
+                    ("budget_id.date_end", ">=", entry_date),
+                    ("budget_id.active", "=", True),
+                    ("analytic_account_id", "=", vals.get("account_id", record.account_id.id))
+                ], limit=1)
+
+                if budget_line:
+                    budget = budget_line.budget_id
+
+                    analytic_account_lines = self.env["account.analytic.line"].search_read(
+                        [
+                            ("account_id", "=", vals.get("account_id", record.account_id.id)),
+                            ("date", ">=", budget.date_start),
+                            ("date", "<=", budget.date_end),
+                            ("amount", "<", 0),
+                        ],
+                        fields=["amount"],
+                    )
+                    achieved = (sum(line.get("amount") for line in analytic_account_lines))
+
+                    new_amount = vals.get("amount", record.amount)
+                    if budget.on_over_budget == "restriction":
+                        if abs(achieved - record.amount + new_amount) > budget_line.budget_amount:
+                            raise ValidationError(
+                                "You cannot modify the budget line because it exceeds the allowed budget!"
+                            )
+
         return super(AccountAnalyticLine, self).write(vals)
