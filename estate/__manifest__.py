5cf7328fc2af07cf99b5f4164bde5aca99953b50
chdh-odoo <chdh@odoo.com>
2024-12-16 19:02:08 +0530

    [ADD] estate:  Added new estate app

---
+++ estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'estate',
+    'version': '1.0',
+    'author' : "Dhruv Chauhan",
+    "description": "Real estate module for managing property listings and transactions!",
+    'depends': [
+        'base'
+    ],
+    'installable': True,
+    'application': True,
+    'category': "Real Estate/Brokerage",
+}
