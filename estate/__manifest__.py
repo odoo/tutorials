79671b1eebd2e0d866f99250bc48b079172bd495
abdelrahmanfawzy <abdelrahmanfawzy@aucegypt.edu>
2024-12-16 16:20:08 +0100

    Chapter 1- New Module

---
+++ estate/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'author': "Abdelrhman Fawzy",
+    'category': 'Aditi/Real Estate',
+    'summary': "Realistic business advertisements",
+    'application': True,
+    'installable': True,
+    'auto_install': False
+}
