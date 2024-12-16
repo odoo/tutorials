cb0a5e3e8ce01170e067cb2142fc545b7e2b43e8
Ahmed Abdelaziz <a.taha12322@gmail.com>
2024-12-16 17:13:29 +0100

    Finish Chapter 1: Architecture Overview, Chapter 2: A New Application, and Chapter 3: Models And Basic Fields

---
+++ estate/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': "Real Estate",
+    'author': "Odoo",
+    'website': "https://www.odoo.com/",
+    'version': '0.1',
+    'application': True,
+    'installable': True,
+    'depends': ['base'],
+}
