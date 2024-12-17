da5c20e566efae7c36e0d3f133e3b771727ae110
Hammad Arif <arih@odoo.com>
2024-12-17 10:06:54 +0400

    [IMP] estate: Added computed fields in estate module.
    Added new field in the property model to compute the best offer ever givn to it. Created a method to compute the total area of the property based on garden area and living area.
    
    Created an onchange method as well for the garden checkbox.
    
    Property offer will now have a validity and deadline date which depend on each other.
    
    Completed chpter no 8 of server framework101.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields
+from odoo import models, fields, api
 from datetime import timedelta
 
 class Property(models.Model):
@@ -18,6 +18,8 @@ class Property(models.Model):
 
     expected_price = fields.Float(string='Expected Price', required=True, default=0.0)
     selling_price = fields.Float(string='Selling Price', readonly=True, copy=False)
+    best_price = fields.Float(string='Best Offer', readonly=True, copy=False,
+                              compute="_compute_best_offer")
     
     bedrooms = fields.Integer(string='Number of bedrooms', default=2)
     living_area = fields.Integer()
@@ -32,10 +34,13 @@ class Property(models.Model):
     active = fields.Boolean(default=True)
     
     state = fields.Selection([('new', 'New'), ('offer received', 'Offer Received'),
-                              ('offer accpeted', 'Offer Accepted'), ('cancelled', 'Cancelled')],
+                              ('offer accepted', 'Offer Accepted'), ('cancelled', 'Cancelled')],
                                default="new",
                                required=True,
                                copy=False)
+
+    total_area = fields.Integer(string='Total Area (sqm)', compute="_compute_total_area",
+                                readonly=True, copy=False)
     
     property_type_id = fields.Many2one(
         'estate.property.type',
@@ -66,3 +71,22 @@ class Property(models.Model):
         string="Offers",
         copy=False
     )
+
+    @api.depends("living_area", "garden_area")
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area 
+
+    @api.depends("offer_ids.price")
+    def _compute_best_offer(self):
+        for record in self:
+            record.best_price = max(record.offer_ids.mapped('price'))
+
+    @api.onchange("garden")
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = "north"
+        else:
+            self.garden_area = 0
+            self.garden_orientation = ''
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields
+from odoo import models, fields, api
+from datetime import timedelta, datetime
 
 class PropertyOffer(models.Model):
     _name = "estate.property.offer"
     _description = "Manage different offers from buyers for a specific property."
 
     price = fields.Float(string="Price", required=True)
+
+    validity = fields.Integer(string="Validity (days)", default=7)
+    date_deadline = fields.Date(string="Deadline", compute="_compute_deadline",
+                                inverse="_inverse_deadline")
 
     status = fields.Selection([('accepted', 'Accepted'),
                                ('refused', 'Refused')],
@@ -24,3 +29,14 @@ class PropertyOffer(models.Model):
         string='Property',
         required=True
     )
+
+    @api.depends("validity", "create_date")
+    def _compute_deadline(self):
+        for record in self:
+            create_date = record.create_date if record.create_date else datetime.now()
+            record.date_deadline = create_date + timedelta(days=record.validity)
+
+    def _inverse_deadline(self):
+        for record in self:
+            create_date = record.create_date if record.create_date else datetime.now()
+            record.validity = (record.date_deadline - create_date.date()).days
