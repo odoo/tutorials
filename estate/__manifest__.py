9bda4323e8ba240f0ffa7148dbe5d253380f8e0f
Dylan Provoost <deimort@hotmail.fr>
2024-12-17 13:25:06 +0100

    [IMP] estate: chapter 5

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,7 +1,11 @@
 {
     'name': "Real Estate",
     'depends': ['base'],
+    'application': True,
+    'installable': True,
     'data': [
-        "security/ir.model.access.csv"
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml"
     ]
 }
