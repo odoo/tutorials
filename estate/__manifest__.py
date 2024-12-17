cc2e4711c2ab910121410975e066afd767e2ed47
abdelrahmanfawzy <abdelrahmanfawzy@aucegypt.edu>
2024-12-17 13:06:07 +0100

    [IMP] estate: views  added

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -9,7 +9,9 @@
     'installable': True,
     'auto_install': False,
     'data': [
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menues.xml'
     ]
 
 }
