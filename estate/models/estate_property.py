6a57c6844ce2c73c44c9df2458ae8fe4d0664925
Ahmed Abdelaziz <a.taha12322@gmail.com>
2024-12-17 15:09:49 +0100

    [IMP] estate: use linter to format files

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,5 +1,6 @@
 from odoo import fields, models
 
+
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "this is a model description"
@@ -7,7 +8,10 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(copy=False, default=lambda x:fields.Datetime.add(fields.Datetime.today(), months=3))
+    date_availability = fields.Date(
+        copy=False,
+        default=lambda x: fields.Datetime.add(fields.Datetime.today(), months=3),
+    )
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
@@ -16,6 +20,23 @@ class EstateProperty(models.Model):
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
-    garden_orientation = fields.Selection(string='Type', selection=[('north','North'), ('south', 'South'), ('east', 'East'), ('west','West')])
+    garden_orientation = fields.Selection(
+        string="Type",
+        selection=[
+            ("north", "North"),
+            ("south", "South"),
+            ("east", "East"),
+            ("west", "West"),
+        ],
+    )
     active = fields.Boolean(default=True)
-    state = fields.Selection(string='Type', selection=[('new','New'), ('offer received', 'Offer Received'),('offer accepted','Offer Accepted'),('sold','Sold'),('canceled','Canceled')])
+    state = fields.Selection(
+        string="Type",
+        selection=[
+            ("new", "New"),
+            ("offer received", "Offer Received"),
+            ("offer accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("canceled", "Canceled"),
+        ],
+    )
