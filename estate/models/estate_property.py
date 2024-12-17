b83d00cd39826a6ced2447ddde57592f9bc17dd4
Ahmed Abdelaziz <a.taha12322@gmail.com>
2024-12-17 13:29:17 +0100

    [IMP] estate: Chapter 5: Finally, Some UI To Play With

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,19 +1,21 @@
 from odoo import fields, models
 
 class EstateProperty(models.Model):
-    _name = "estate_property"
+    _name = "estate.property"
     _description = "this is a model description"
 
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=lambda x:fields.Datetime.add(fields.Datetime.today(), months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(string='Type', selection=[('north','North'), ('south', 'South'), ('east', 'East'), ('west','West')])
+    active = fields.Boolean(default=True)
+    state = fields.Selection(string='Type', selection=[('new','New'), ('offer received', 'Offer Received'),('offer accepted','Offer Accepted'),('sold','Sold'),('canceled','Canceled')])
