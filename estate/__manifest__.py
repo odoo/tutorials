4bf898a43e7baab0d272a31ad7d0cb1e3f8ea990
Dylan Provoost <deimort@hotmail.fr>
2024-12-18 12:43:03 +0100

    [IMP] estate: Fixing imports for offers

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,6 +7,7 @@
         "views/estate_property_views.xml",
         "views/estate_property_type_views.xml",
         "views/estate_property_tags_views.xml",
+        "views/estate_property_offer_views.xml",
         "views/estate_menus.xml",
         "security/ir.model.access.csv"
     ]
