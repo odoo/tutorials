7f9248505a13331bac4aa5b7e4e455d5f8cf3d3b
Ahmed Abdelaziz <a.taha12322@gmail.com>
2024-12-19 13:39:27 +0100

    [IMP] estate: Use Computed Fields And Onchanges

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class EstatePropertyOffer(models.Model):
@@ -13,3 +13,22 @@ class EstatePropertyOffer(models.Model):
     )
     partner_id = fields.Many2one("res.partner", required=True)
     property_id = fields.Many2one("estate.property", required=True)
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(
+        compute="_compute_date_deadline", inverse="_inverse_date_deadline"
+    )
+
+    @api.depends("validity")
+    def _compute_date_deadline(self):
+        for record in self:
+            if not record.create_date:
+                record.create_date = fields.Datetime.today()
+            record.date_deadline = fields.Datetime.add(
+                record.create_date, days=record.validity
+            )
+            print("Hi there")
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            print((record.date_deadline - record.create_date.date()).days)
+            record.validity = (record.date_deadline - record.create_date.date()).days
