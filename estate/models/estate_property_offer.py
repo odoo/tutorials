fc1c6e2a00854f8e426e0215434d6118c01e509c
abdelrahmanfawzy <abdelrahmanfawzy@aucegypt.edu>
2024-12-19 11:33:09 +0100

    [IMP] Add computed fields

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,8 +1,13 @@
-from odoo import fields, models
+from datetime import timedelta, date
+from odoo import api, fields, models
+
 
 class EstatePropertyType(models.Model):
     _name = 'estate.property.offer'
     _description = 'Offers for real state properties'
+
+    validity = fields.Integer(string="Validity (Days)",default=7)
+
 
     price = fields.Float()
     status = fields.Selection(
@@ -10,4 +15,22 @@ class EstatePropertyType(models.Model):
     )
     property_id = fields.Many2one(comodel_name='estate.property')
     partner_id = fields.Many2one(comodel_name='res.partner', string='Buyer')
+
+
+    date_deadline = fields.Date(compute='_compute_date_deadline', inverse='_inverse_date_deadline')
+
+    @api.depends('validity')
+    def _compute_date_deadline(self):
+        for record in self:
+            if not record.create_date:
+                record.create_date = fields.Date.today()
+            record.date_deadline = record.create_date + timedelta(days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            if not record.create_date:
+                record.create_date = fields.Date.today()
+            record.validity = (record.date_deadline - record.create_date.date()).days
+
+
 
