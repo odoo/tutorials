956d38072fa5e77cc0e9327174ea3fc2ee1de474
Ahmed Abdelaziz <a.taha12322@gmail.com>
2024-12-26 16:27:09 +0100

    [IMP] awesome_owl: Finish Chapter 1 Sections 1-5

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,7 +1,13 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, markup } from "@odoo/owl";
+import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
 
 export class Playground extends Component {
-    static template = "awesome_owl.playground";
+  static template = "awesome_owl.playground";
+  static components = { Counter, Card };
+  static props = {};
+
+  value = markup("<div class='text-primary'>some text</div>");
 }
