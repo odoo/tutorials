e34aacb0f6dd97b97a9191310dde74bda5ed5ebf
romc-odoo <romc@odoo.com>
2024-12-26 15:44:17 +0100

    [IMP] estate: add demo data

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,10 +7,13 @@
     "data": [
         "security/ir.model.access.csv",
         "views/estate_property_views.xml",
+        "views/estate_property_offer_views.xml",
         "views/estate_property_type_views.xml",
         "views/estate_property_tag_views.xml",
-        "views/estate_property_offer_views.xml",
         "views/res_users_views.xml",
         "views/estate_menus.xml",
+        "data/estate_property_type_data.xml",
+        "data/estate_property_data.xml",
+        "data/estate_property_offer_data.xml",
     ],
 }
