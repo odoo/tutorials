78aac82b3a679ec0187dd43703bf4da278d05e02
ksar-odoo <ksar@odoo.com>
2025-01-06 19:25:09 +0530

    [IMP] estate : Final touch on the old commit

--- estate/models/inherited_res_users.py
+++ estate/models/inherited_res_users.py
@@ -3,4 +3,4 @@ from odoo import fields, models
 # inheriting the res.user and extending the functionality 
 class InheritedResUsers(models.Model):
     _inherit= "res.users"
-    property_ids= fields.One2many(comodel_name="estate.property", inverse_name="salesperson", string="property_ids", domain="['|', ('state', '=', 'new'), ('state', '=', 'offer_received')]" )
+    property_ids= fields.One2many(comodel_name="estate.property", inverse_name="salesperson", string="property_ids", domain=['|', ('state', '=', 'new'), ('state', '=', 'offer_received')] )
--- estate_account/__manifest__.py
+++ estate_account/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'estate_account',
     'installable': True, #Whether a user should be able to install the module from the Web UI or not.
-    'application': True, #Whether the module should be considered as a fully-fledged application (True) or is just a technical module (False) that provides some extra functionality to an existing application module.
+    'application': False, #Whether the module should be considered as a fully-fledged application (True) or is just a technical module (False) that provides some extra functionality to an existing application module.
     'depends': ['account','estate'], # any module necessary for this one to work correctly
     'data': [
         'security/ir.model.access.csv', #security access
