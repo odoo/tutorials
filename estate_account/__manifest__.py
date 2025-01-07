cbc0b0c82ac690fd39951df2341bde6e7e0890da
ksar-odoo <ksar@odoo.com>
2025-01-07 11:13:50 +0530

    [REM] estate: Removing the dead codes from estate_account module
    Updating the code and removing the dead codes from estate_account
    related module's root menu no need to show in the UI.
    task: estate tutorial

--- estate_account/__manifest__.py
+++ estate_account/__manifest__.py
@@ -1,12 +1,9 @@
 {
     'name': 'estate_account',
+    'description': "Module that creates invoice once the estate property is marked as sold",
     'installable': True, #Whether a user should be able to install the module from the Web UI or not.
     'application': False, #Whether the module should be considered as a fully-fledged application (True) or is just a technical module (False) that provides some extra functionality to an existing application module.
     'depends': ['account','estate'], # any module necessary for this one to work correctly
-    'data': [
-        'security/ir.model.access.csv', #security access
-        'views/estate_account_views.xml',
-        'views/estate_account_menus.xml',
-    ],
+    'data': [],
     'license':'LGPL-3'
 }
