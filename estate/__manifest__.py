06c151ea4c704a423543f0f36f23e2f894b3df94
ksar-odoo <ksar@odoo.com>
2025-01-09 17:39:18 +0530

    [IMP] estate: Added report generation for estate property
    This commit is for adding a new functionality to print the estate property
     related report.
    including the salesman based property reports.
    
    task: estate property

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,11 +7,11 @@
         'security/ir.model.access.csv', #security access
         'views/estate_property_type.xml',
         'views/estate_property_tag.xml',
-        'views/estate_property_res_user_view_form.xml',
         'report/estate_property_templates.xml',
         'report/estate_property_reports.xml',
         'views/estate_property_views.xml',#actions defs & views to be shown
         'views/estate_menus.xml', #estate root-menu, first level menu, action menu to be shown/ above because id refers to estate_property_views' id
+        'views/estate_property_res_user_view_form.xml',
     ],
     'demo': [
         'demo/estate.property.type.csv',
--- estate_account/__manifest__.py
+++ estate_account/__manifest__.py
@@ -4,6 +4,8 @@
     'installable': True, #Whether a user should be able to install the module from the Web UI or not.
     'application': False, #Whether the module should be considered as a fully-fledged application (True) or is just a technical module (False) that provides some extra functionality to an existing application module.
     'depends': ['account','estate'], # any module necessary for this one to work correctly
-    'data': [],
+    'data': [
+        'report/estate_account_templates.xml'    
+    ],
     'license':'LGPL-3'
-}
+} # type: ignore
