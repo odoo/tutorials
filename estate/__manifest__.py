22c1ac7299e566dabbcf0ac1320f815cc779b86a
yame-odoo <yame@odoo.com>
2025-01-17 14:01:40 +0530

    [IMP] estate: : refactor code

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,17 +1,8 @@
 # -*- coding: utf-8 -*-
 {
     "name": "estate",
-    "summary": """
-        Starting module for "Discover the JS framework, chapter 1: Owl components"
-    """,
-    "description": """
-        Starting module for "Discover the JS framework, chapter 1: Owl components"
-    """,
     "author": "Odoo",
     "website": "https://www.odoo.com",
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     "category": "Real Estate/Brokerage",
     "version": "0.1",
     # any module necessary for this one to work correctly
