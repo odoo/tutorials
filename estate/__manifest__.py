dace4d86834b32cfae71b46fc7f0f99bb80ed943
Arthur Nanson <artn@odoo.com>
2025-01-20 16:42:49 +0100

    [REF] refactors the commit messages

---
+++ estate/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Real estate module!!",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Arthur Nanson",
+    'category': 'Category',
+    'description': """
+    With this awesome module you can do awesome things like buy a house somehow maybe
+    """,
+    # data files always loaded at installation
+    'data': [
+        'views/mymodule_view.xml',
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+        'demo/demo_data.xml',
+    ],
+}
