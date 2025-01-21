91e2246b79475f785be2ed2eb62e991965d93757
hipr <hipr@odoo.com>
2025-01-21 22:13:31 +0530

    [ADD] sales_permission: permissions for sale user to confirm sale order
    Users without the required permissions will be denied the ability to confirm a
    sale order.
    Once the administrator grants the necessary rights to the user, they will be
    able to confirm the sale order.
    
    Task: 4497335

---
+++ sales_permission/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "Sales Permission",
+    'version': '1.0',
+    'depends': ['base', 'sale_management'],
+    'author': "Hitesh Prajapati",
+    'category': 'Sales/Sales',
+    'license': 'LGPL-3',
+    'description': """
+        Description text
+        """,
+    'application': True,
+    'installable': True,
+     'data':[
+        'views/sales_permission.xml'
+     ]
+}
