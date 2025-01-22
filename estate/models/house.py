adade596c5d40e8e2c0f539f98a00d078934fb51
Hussein Ebrahim(hyhe) <hyhe@odoo.com>
2025-01-22 19:02:14 +0100

    [IMP] estate: modified fields' attributes on the house model
    added the default values for some fields and non copy constraints

--- estate/models/house.py
+++ estate/models/house.py
@@ -3,13 +3,13 @@ from odoo import models, fields
 class house(models.Model):
     _name = 'house'
 
-    name = fields.Char(string='House Name', required=True)
+    name = fields.Char(string='House Name', required=True, default='Unknown')
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=lambda self : self.get_availability_date())
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -19,7 +19,15 @@ class house(models.Model):
                                            ('N', 'North'),
                                            ('W', 'West'),
                                            ('E', 'East')])
+    active = fields.Boolean(default=True)
+    state = fields.Selection(string='Status', selection=[
+        ('New', 'New'),
+        ('Offer Received', 'Offer Received'),
+        ('Offer Accepted', 'Offer Accepted'),
+        ('Sold', 'Sold'),
+        ('Cancelled', 'Cancelled'),
+    ], default='New', required=True)
     
-
-
-
+    def get_availability_date(self):
+        current_date = fields.Date.today()
+        return fields.Date.add(current_date, month=3)
