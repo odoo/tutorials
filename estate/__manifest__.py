455e8d958019c4199fbb93735ba056052eff05b9
plha-odoo <plha@odoo.com>
2025-01-22 14:26:32 +0100

    [ADD] Chapter 5 of the tutorial

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,8 +3,8 @@
         'base',],
     'application': True,
     'data':[
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
     ]
-
-    
   }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import fields, models
+from datetime import date, timedelta
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -7,14 +8,15 @@ class EstateProperty(models.Model):
     name = fields.Char('Property Name',required=True)
     description = fields.Text('The Descritption')
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(default=date.today()+ timedelta(days=90),copy=False)
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(copy=False,readonly=True)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area= fields.Integer()
     garden_orientation = fields.Selection([("North","North"),("South","South"),("East","East"),("West","West")])
-
+    active = fields.Boolean(default=True)
+    state = fields.Selection([("New","New"),("Offer Received","Offer Received"),("Offer Accpeted","Offer Accepted"),("Solde","Solde"),("Cancelled","Cancelled")],copy=False,default="New")
