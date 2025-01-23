686616c98cb7dc88627362eef240381f590d4e1b
plha-odoo <plha@odoo.com>
2025-01-23 13:53:55 +0100

    [ADD] chap 6 of the tutorials

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,7 +4,11 @@
     'application': True,
     'data':[
         'security/ir.model.access.csv',
-        'views/estate_property_views.xml',
-        'views/estate_menus.xml'
+        'views/estate_property_views_action.xml',
+        'views/estate_menus.xml',
+        'views/estate_list_view.xml',
+        'views/estate_form_view.xml',
+        'views/estate_search_view.xml',
+
     ]
   }
