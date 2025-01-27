55ac13c9e895cf33d2367dd03858c3731c7a9afb
Hussein Ebrahim(hyhe) <hyhe@odoo.com>
2025-01-27 21:43:41 +0100

    [IMP] estate: added the validity and deadline for the offer
    - added the validity attr
    - added the deadline attr as computed attr and with inverse changing the validity days of the offer

--- estate/models/offer.py
+++ estate/models/offer.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 class offer(models.Model):
     _name = 'estate.house_offer'
@@ -7,3 +7,16 @@ class offer(models.Model):
     status = fields.Selection(selection=[('Accepted','Accepted'),('Refused','Refused')], copy=False)
     partner_id = fields.Many2one('res.partner', stirng='Partner', required=True)
     property_id = fields.Many2one('house', 'Property applied on', required=True)
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute="_compute_offer_deadline", inverse="_inverse_offer_deadline")
+
+    @api.depends("validity")
+    def _compute_offer_deadline(self):
+        for offer in self:
+            current_date = offer.create_date or fields.Date.today()
+            offer.date_deadline = fields.Date.add(current_date, days=offer.validity)
+
+    def _inverse_offer_deadline(self):
+        for offer in self:
+            current_date = fields.Date.to_date(offer.create_date or fields.Date.today())
+            offer.validity = (offer.date_deadline - current_date).days
