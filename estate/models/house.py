79fb09309f7b787f7a1742c71fc0c239e9fb5937
Hussein Ebrahim(hyhe) <hyhe@odoo.com>
2025-01-28 00:39:17 +0100

    [IMP] estate: added sell/cancel property buttons
    - added buttons for the property model with their corresponding actions
    - changed the buyer of the property to the res.partner table instead of res.user

--- estate/models/house.py
+++ estate/models/house.py
@@ -1,4 +1,5 @@
 from odoo import api, models, fields
+from odoo.exceptions import UserError
 
 class house(models.Model):
     _name = 'house'
@@ -20,20 +21,21 @@ class house(models.Model):
                                            ('W', 'West'),
                                            ('E', 'East')])
     active = fields.Boolean(default=True)
-    state = fields.Selection(string='Status', selection=[
+    state = fields.Selection(string='Status', readonly=True, selection=[
         ('New', 'New'),
         ('Offer Received', 'Offer Received'),
         ('Offer Accepted', 'Offer Accepted'),
         ('Sold', 'Sold'),
         ('Cancelled', 'Cancelled'),
     ], default='New', required=True)
-    buyer_id = fields.Many2one('res.users', string='Buyer')
+    buyer_id = fields.Many2one('res.partner', string='Buyer')
     seller_id = fields.Many2one('res.users', string='Salesperson')
     house_type_id = fields.Many2one('estate.house_type', string='Property Type')
     house_tag_ids = fields.Many2many('estate.house_tag')
     offers_ids = fields.One2many('estate.house_offer', 'property_id')
     total_area = fields.Float(compute='_calculate_total_area')
     best_price = fields.Float(compute='_calculate_best_price')
+    is_offer_accepted = fields.Boolean(default=False)
     
     def get_availability_date(self):
         current_date = fields.Date.today()
@@ -57,3 +59,16 @@ class house(models.Model):
         else:
             self.garden_area = 0
             self.garden_orientation = None
+
+    def sell_property(self):
+        for house in self:
+            if(house.state == 'Cancelled'):
+                raise UserError("Cancelled property can't be sold")
+            house.state = 'Sold'
+    
+    def cancel_property(self):
+        for house in self:
+            if(house.state == 'Sold'):
+                # raise error here
+                raise UserError("Sold porpoerty can't be cancelled")
+            house.state = 'Cancelled'
