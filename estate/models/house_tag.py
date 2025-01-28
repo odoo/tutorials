03061e34be3fed95d25a459f7233416a8207465b
Hussein Ebrahim(hyhe) <hyhe@odoo.com>
2025-01-28 01:46:02 +0100

    [FIX] estate: fixed the unique sql constraint on the house_type/tag models

--- estate/models/house_tag.py
+++ estate/models/house_tag.py
@@ -3,7 +3,7 @@ from odoo import fields, models
 class house_tag(models.Model):
     _name = 'estate.house_tag'
     _sql_constraints = [
-        ('unique_tag_name', 'UNIQUE name', 'tag name value is already exisiting')
+        ('unique_tag_name', 'UNIQUE(name)', 'tag name value is already exisiting')
     ]
 
     name = fields.Char(required=True)
--- estate/models/house_type.py
+++ estate/models/house_type.py
@@ -3,7 +3,7 @@ from odoo import fields, models
 class house_type(models.Model):
     _name = 'estate.house_type'
     _sql_constraints = [
-        ('unique_type_name', 'UNIQUE name', 'type name value is already exisiting')
+        ('unique_type_name', 'UNIQUE(name)', 'type name value is already exisiting')
     ]
 
     name = fields.Char(required=True)
