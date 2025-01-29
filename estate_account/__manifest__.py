ccc40e675ac386f4f29459a6bc18e527613b78da
Arthur Nanson <artn@odoo.com>
2025-01-29 14:16:26 +0100

    [ADD] Add the new estate_account module. This is made to automatically create invoices once the house is sold. Doesn't work for now as the override of the sold_property function doesn't work.

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,17 @@
+
+{
+    'name': "Accounting for real estate",
+    'version': '1.0',
+    'depends': ['estate', 'account'],
+    'author': "Arthur Nanson",
+    'category': 'Category',
+    'description': """
+    Build invoices for your real estate sales
+    """,
+    'application' : True,
+    # data files always loaded at installation
+    #'data' : ['data/estate.property.csv'],
+    #'data' : ['security/ir.model.access.csv',
+
+    #          ],
+}
