aaf95c76c2a468d8bdda056e7231702c7a0fabcf
Hussein Ebrahim(hyhe) <hyhe@odoo.com>
2025-01-29 14:10:23 +0100

    [ADD] estate_account: created the estate_account module app
    - created estate_account module app for handling the invoices when a property is sold
    - added the property model inherited from the house with the overriding of the sell method to create invoices on selling

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Estate Account',
+    'license': 'LGPL-3',
+    'depends': [
+        'base',
+        'estate',
+        'account'
+    ],
+    'application': True
+}
---
+++ estate_account/models/property.py
@@ -0,0 +1,20 @@
+from odoo import Command, models
+
+class Property(models.Model):
+    _inherit = 'house'
+
+    def sell_property(self):
+        print("I am the child behavior")
+        print(self.buyer_id)
+        self.env["account.move"].create({
+            'partner_id': self.buyer_id.id,
+            'move_type': 'out_invoice',
+            'invoice_line_ids': [
+                Command.create({
+                    'name': f'Buying Property {self.name}',
+                    'price_unit': 0.6*self.selling_price,
+                    'quantity': 1
+                })
+            ]
+        })
+        return super().sell_property()
