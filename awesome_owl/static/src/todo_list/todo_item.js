f2d364d45f283e8131d9bed8cab8ab82116cd6a4
Carmine <csantomauro@protonmail.com>
2025-01-31 09:21:49 +0100

    [ADD] Awesome_owl: Owl Components

---
+++ awesome_owl/static/src/todo_list/todo_item.js
@@ -0,0 +1,22 @@
+import { Component } from "@odoo/owl"
+
+export class TodoItem extends Component {
+    static template = "awesome_owl.TodoItem";
+
+    static props = {
+        todo: {
+            type: Object,
+            shape: { id: Number, description: String, isCompleted: Boolean }
+        },
+        toggleState: Function,
+        removeTodo: Function,
+    };
+
+    onChange() {
+        this.props.toggleState(this.props.todo.id);
+    }
+
+    onRemove(){
+        this.props.removeTodo(this.props.todo.id);
+    }
+}
