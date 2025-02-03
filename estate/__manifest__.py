6fec8f7d84811d364a7c4aef583ebc0d0dd5d463
Ashish Nirmal <asni@odoo.com>
2025-02-03 17:04:02 +0530

    [ADD] real_estate: new module real estate for managing property

---
+++ estate/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Real Estate Module",
+    'summary': "This is the real estate module that is used for buying and selling propertise!!",
+    'description': "This is the real estate module that is used for buying and selling propertise!!",
+    'version': '0.1',
+    'application': True,
+    'category': 'Tutorials',
+    'installable': True,
+    'depends': [],
+    'data': [],
+    'assets': {},
+    'license': 'AGPL-3'
+}
