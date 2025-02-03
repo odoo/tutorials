2e2fc58219079f112f11f9062b3e647b5f4bafd9
Kunj Patel(kupa) <kupa@odoo.com>
2025-02-03 17:17:31 +0530

    [ADD] real_estate: Add manifest file for real estate project setup

---
+++ real_estate/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': "Real Estate Module",
+
+    'summary': """
+        Starting module for Real Estate Project"
+    """,
+
+    'description': """
+        Starting module for Real Estate Project Description
+    """,
+
+    'author': "Odoo",
+    'website': "https://www.odoo.com/",
+    'category': 'Tutorials',
+    'version': '0.1',
+    'application': False,
+    'installable': False,
+    'depends': [
+    ],
+
+    'data': [],
+    'assets': {
+    },
+    'license': 'AGPL-3'
+}
