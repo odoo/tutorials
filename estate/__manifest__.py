77f25c286486a6e2fb988b9377b142ce1aa21239
siha-odoo <siha@odoo.com>
2025-02-03 15:24:54 +0530

    [ADD] estate:  Created a new module named Real Estate
    Added Real Estate module with two files init & manifest
    also configured manifest file  to make module visible as App.

---
+++ estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'category': 'tutorials',
+    'depends': ['base'],
+    'summary': 'Chapter 2: Server Framework 101',
+    'description': "",
+    'installable': True,
+    'application': True,
+}
