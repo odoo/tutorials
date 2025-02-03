e6f1f5b8cf4660026d29308caaf8d3cbb2cc58eb
ypat-odoo <ypat@odoo.com>
2025-02-03 18:24:34 +0530

    [ADD] estate: add manifest file for real estate project setup - Added init and manifest for estate module

---
+++ estate/__manifest__.py
@@ -0,0 +1,29 @@
+{
+    'name': "estate app",
+
+    'summary': """
+        estate module"
+    """,
+
+    'description': """
+        estate module"
+    """,
+
+    'author': "Odoo",
+    'website': "https://www.odoo.com/",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Tutorials/estate',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'web'],
+    'application': True,
+    'installable': True,
+    'data': [
+      #  'views/templates.xml',
+    ],
+    'license': 'AGPL-3'
+}
