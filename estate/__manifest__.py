d11f097a7aedb46ca52e15020c0dc03a2be8f48a
mkap-odoo <mkap@odoo.com>
2025-02-03 18:40:39 +0530

    [ADD] real_estate: add manifest file for real estate project setup

---
+++ estate/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "mkap",
+    'category': 'tutorials/estate',
+    'description': """
+    App to list and sell/rent property
+    """,
+    # data files always loaded at installation
+    'data': [
+        'views/mymodule_view.xml',
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+        'demo/demo_data.xml',
+    ],
+}
