c7f4f349b1f57dd397be5b6c382d603ae230954d
drat-odoo <drat@odoo.com>
2025-02-04 17:17:11 +0530

    [ADD] estate: added manifest.py

---
+++ estate/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "drat-odoo",
+    'category': 'Category',
+    'description': """
+    This is base version of estate
+    """,
+    # data files always loaded at installation
+
+    # data files containing optionally loaded demonstration data
+    'installable': True,
+    'license': 'OEEL-1',
+}
