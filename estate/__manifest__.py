566c532d514f25f44313252f81080e97a35f59e8
praj-odoo <praj@odoo.com>
2025-02-04 19:02:24 +0530

    [ADD] estate_module: create estate model and add fields
    Created a new estate model in Odoo to manage estate properties.
    Added relevant fields to the model for handling property details.
    Implemented security rules for the model to control access.

---
+++ estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Estate',
+    'version': '1.0',
+    'category': 'Real Estate',
+    'summary': 'Real Estate Advertisement Management',
+    'author': 'praj',
+    'depends': ['base'],
+    'data': ['security/ir.model.access.csv'],
+    'installable': True,
+    'application': True,
+}
