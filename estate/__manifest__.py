3c802b13cf21d71e9f027fff5613b5921e5679d9
gppa_odoo <gppa@odoo.com>
2025-02-04 18:57:35 +0530

    [IMP] estate: New files added in estate module: Manifest, Init

---
+++ estate/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Real Estate',
+    'depends': [
+        'base'
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ estate/models/real_estate_properties.py
@@ -0,0 +1,6 @@
+from odoo import fields, models
+
+
+class RecurringPlan(models.Model):
+    _name = "estate_property"
+    _description = "Real Estate property table :)"
