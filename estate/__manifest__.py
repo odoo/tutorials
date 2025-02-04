73cadb24b4b00f5a82cb1a047d8b622c136946d5
apri-odoo <apri@odoo.com>
2025-02-04 17:15:39 +0530

    [ADD] estate: added manifest file

---
+++ estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Estate',
+    'version':'1.0',
+    'depends': ['base'],
+    'description': """
+This is a base version
+    """,
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
