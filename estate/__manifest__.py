8ab47e2438a82d5369a15b14916dfa9637a2ade4
rusp-odoo <rusp@odoo.com>
2025-02-04 17:09:57 +0530

    [ADD] estate: added new module
    In this commit:
    - Added new module estate (manifest.py, init.py)

---
+++ estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Estate',
+    'version': '1.0',
+    'depends': ['base'],
+    'author': 'Odoo - Rushil Patel',
+    'category': 'Category',
+    'description': 'Technical training chapter - 2',
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True
+}
