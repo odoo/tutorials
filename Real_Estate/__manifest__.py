444c36c3abe6e8a1461dd9401c4fff2295f02d8c
Prem Patel (prep) <prep@odoo.com>
2025-02-04 14:37:22 +0530

    [ADD] Real_Estate: add Property model and give user access rights

---
+++ Real_Estate/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Real Estate",
+    "summary":"Real Estate Module helps to buy and sell Properties.",
+    "description":"",
+    "application":True,
+    "category": "Tutorials",
+    "installable" : True,
+    "depends": ['base'],
+    "data": ['security/ir.model.access.csv'],
+    "demo": [],
+    "assets" :{},
+    'license': 'AGPL-3',
+}
