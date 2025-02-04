7f1f7dc046fcd26475b88f5bfc91f52e09f067b5
mdra-odoo <mdra@odoo.com>
2025-02-04 17:12:02 +0530

    [ADD] estate: addedd new module in tutorials
    - created new module with __init__ and __manifest__ files

---
+++ estate/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name':'Real Estate',
+    'version':'1.0',
+    'depends':['base'],
+    'author':'odoo',
+    'license':'LGPL-3',
+    'discription':"""This is Real Estate application which makes easy to sold property""",
+    'application':True
+}
