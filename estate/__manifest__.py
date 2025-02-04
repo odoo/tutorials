6fc8272c4e26fc7102178162244df5e352dd8ce5
aych-odoo <aych@odoo.com>
2025-02-04 18:45:53 +0530

    [ADD] estate: Added new module
    Day 1:
      Chapter 2: created directory structure along with manifest file for estate module
      Chapter 3: created estate_properties model

---
+++ estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Real Estate",
+    "author": "Ayush",
+    "website": "https://www.odoo.com/apps/estate",
+    "category": "Tutorials2/RealEstate",
+    "depends": ["base"],
+    "data": ["security/ir.model.access.csv"],
+    "installable": True,
+    "application": True
+}
