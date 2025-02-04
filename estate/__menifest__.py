ad0758a9527bb06dd2a7762c5bdb366169a1ae55
devp-odoo <devp@odoo.com>
2025-02-04 17:24:52 +0530

    [ADD] estate: added real estate module
    Add new module named 'Real Estate'
    - Create init and menifest files
    - Configure menifest file

---
+++ estate/__menifest__.py
@@ -0,0 +1,10 @@
+    {
+    'name': 'Real Estate',
+    'version': '1.0',
+    'summary': 'Real estate module for managing property listings and transactions',
+    'author': 'Dev Patel',
+    'depends': ['base'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
