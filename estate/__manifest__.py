cff986c82beca67ae575eaf1282897aec9c30bc1
Dhruv Godhani <dhgo@odoo.com>
2025-02-04 18:48:30 +0530

    [ADD] estate: create estate module and configure it!
    - create manifest file and add some configurations in it

---
+++ estate/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    # 'depends': ['base'],
+    'author': "Dhruv Godhani",
+    'category': 'Category',
+    'description': """
+    This is the first module
+    """,
+    # data files always loaded at installation
+    'data': [
+        "security/ir.model.access.py"
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+    ],
+    'application': True,
+    'sequence': 1,
+    'license': "LGPL-3"
+}
