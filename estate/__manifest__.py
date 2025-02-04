bd252eaf6f76e1864404144f7c54549893ee7959
siha-odoo <siha@odoo.com>
2025-02-04 16:50:06 +0530

    [IMP] estate:  added actions, menus , fields and attributes.
    -linked actions between menus and views.
    -added three layered action menu(display,task bar,action menu)
    -added new attributes to fields such as read-only, default,reserved fields.
    -implemented 3 months visibility functionality using today() and relativedelta

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -8,6 +8,8 @@
     'installable': True,
     'application': True,
     'data': [
-        'security/ir.model.access.csv',
+        'security/ir.model.access.csv',  #Load security first
+        'views/estate_property_views.xml', #Load model views
+        'views/estate_menus.xml', #Load menus after views
     ],
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,22 +1,31 @@
+from dateutil.relativedelta import relativedelta
 from odoo import fields, models
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real Estate Properties"
 
-    name = fields.Char('name',required = True)
-    description = fields.Text()
-    postcode = fields.Char('postcode')
-    date_availability = fields.Date()
-    expected_price = fields.Float( required = True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
-    living_area = fields.Integer()
-    facades = fields.Integer()
-    garage = fields.Boolean()
-    garden = fields.Boolean()
-    garden_area = fields.Integer()
+    name = fields.Char('Name',required = True)
+    description = fields.Text('Description')
+    postcode = fields.Char('Postcode')
+    date_availability = fields.Date('Available From',copy = False,default = lambda self:fields.Date.add(fields.Date.today()+ relativedelta(3)))
+    expected_price = fields.Float('Expected Price', required = True)
+    selling_price = fields.Float('Selling Price ', readonly = True, copy = False)
+    bedrooms = fields.Integer('Bedrooms', default = 2)
+    living_area = fields.Integer('Living Area(sqm)')
+    facades = fields.Integer('Facades')
+    garage = fields.Boolean('Garage')
+    garden = fields.Boolean('Garden')
+    garden_area = fields.Integer('Garden Area(sqm)')
     garden_orientation = fields.Selection(
-        string = 'garden_orientation',
+        string = 'Garden Orientation',
         selection = [('north','North'),('south','South'),('east','East'),('west','West')]
+    )
+    active = fields.Boolean('Active',default=True)
+    status = fields.Selection(
+        selection = [('new','New'),('offer_received','Offer Received'),('offer_accepted','Offer Accepted'),('sold','Sold'),('cancelled','Cancelled')],
+        string = 'Status',
+        required = True,
+        copy = False,
+        default = 'new'
     )
