c15bab3d98fc044fecfdf58d59684b954ec43f02
rspa-odoo <rspa@odoo.com>
2025-02-04 19:02:56 +0530

    [ADD] estate: add new real estate module
    A new real estate module has been added to the system. This module contains a
    model named estate_property. this model, contains one class called TestModel
    which includes essential  fields such as name and description to store basic
    property details.

---
+++ estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Raj Pavara",
+    'category': 'Property',
+    'description': """
+    Basic Real Estate application
+    """,
+    'installable': True,
+    'application': True,
+}
---
+++ estate/models/estate_property.py
@@ -0,0 +1,22 @@
+from odoo import fields, models
+
+class TestModel(models.Model):
+    _name = "test_model"
+    _description = "Real Estate Property"
+    
+    name = fields.Char(string="Property Name", required=True)
+    description = fields.Text(string="Description")
+    postcode = fields.Char(string="Postcode")
+    date_availability = fields.Date(string="Available From")
+    expected_price = fields.Float(string="Expected Price", required=True)
+    selling_price = fields.Float(string="Selling Price")
+    bedrooms = fields.Integer(string="Number of Bedrooms")
+    living_area = fields.Integer(string="Living Area (sqm)")
+    facades = fields.Integer(string="Number of Facades")
+    garage = fields.Boolean(string="Has Garage?")
+    garden = fields.Boolean(string="Has Garden?")
+    garden_area = fields.Integer(string="Garden Area (sqm)")
+    garden_orientation = fields.Selection(
+        [('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
+        string="Garden Orientation"
+    )
