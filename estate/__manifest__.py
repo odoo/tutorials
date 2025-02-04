6de1c49f270927d719f63bfb0f24d605192c7865
ddba-odoo <ddba@odoo.com>
2025-02-04 18:51:46 +0530

    [ADD] Real_estate: estate_property module created

---
+++ estate/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'estate',
+    'version': '1.8',
+    'summary': 'Track leads and close opportunities',
+
+    'depends': [
+        'base_setup'
+    ],
+
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3', 
+}
