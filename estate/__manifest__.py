1d31b81d9e1b4bbbbc2e533420d09d03cfdfa3aa
Harsh Shah <hash@odoo.com>
2025-02-04 18:25:40 +0530

    [ADD] estate: add estate module

---
+++ estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Real Estate",
+    "version": "1.0",
+    "sequence": 1,
+    "depends": ["base"],
+    "description": """
+    This is the estate application being developed as per Tutorial for Technical Training.
+    """,
+    "installable": True,
+    "application": True,
+}
