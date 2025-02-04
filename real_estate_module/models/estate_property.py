c9c75802480cd75ee3b624f88041b50f823971cf
Devendra <djal@odoo.com>
2025-02-04 15:37:35 +0530

    [ADD] real_estate_module: New module created

---
+++ real_estate_module/models/estate_property.py
@@ -0,0 +1,27 @@
+from odoo import models, fields
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Real Estate Property"
+
+    name = fields.Char(required=True)              # Property name
+    create_uid=fields.Char(required=True)
+    create_date=fields.Char(required=True)
+    postcode=fields.Char()
+    description = fields.Text()                    # Description
+    postcode = fields.Char()                       # Postcode
+    date_availability = fields.Date()              # Available date
+    expected_price = fields.Float(required=True)   # Expected price
+    selling_price = fields.Float()                 # Selling price
+    bedrooms = fields.Integer()                    # Number of bedrooms
+    living_area = fields.Integer()                 # Size of the property
+    facades = fields.Integer()                     # Number of facades
+    garage = fields.Boolean()                      # Does it have a garage?
+    garden = fields.Boolean()                      # Does it have a garden?
+    garden_area = fields.Integer()                 # Garden size
+    garden_orientation = fields.Selection(         # Garden direction
+        [('north', 'North'), 
+         ('south', 'South'), 
+         ('east', 'East'), 
+         ('west', 'West')]
+    )
