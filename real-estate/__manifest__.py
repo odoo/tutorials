606d8b736398fc30b911ca0f5d4cc5585fb7bcdb
nmak-odoo <nmak@odoo.com>
2025-02-04 18:59:47 +0530

    [ADD]  estate : Added new Module
    Day 2 :
    Chapter 2  : Created Folder Structure for Real-estate module
    Chapter 3  : Created estate_properties model with all the basic fields

---
+++ real-estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Real Estate",
+    "author": "nmak",
+    "website": "https://www.odoo.com/apps/estate",
+    "category": "tutorials/real-estate",
+    "depends": ["base"],
+    # "data": ["security/ir.model.access.csv"],
+    "installable": True,
+    "application": True
+}
---
+++ real-estate/models/estate_property.py
@@ -0,0 +1,21 @@
+from odoo import fields, models
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Properties involved in estate"
+
+    name = fields.Char(required = True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float(required = True)
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Boolean()
+    garden_orientation = fields.Selection(
+        selection=[("north" , "North") , ("south", "South"), ("east", "East"), ("west", "West")]
+    )
