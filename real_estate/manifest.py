e014d7df6d2621d7eea1947258005d4842ecef1d
Devendra <djal@odoo.com>
2025-02-04 15:02:33 +0530

    [ADD] real_estate: manifest.py and __init__.py created

---
+++ real_estate/manifest.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Real Estate",
+
+    'summary': """
+        Starting module for Real Estate Tutorial"
+    """,
+
+    'description': """
+        Starting module for Real Estate Tutorial"
+    """,
+
+    'author': "Odoo",
+    'website': "https://www.odoo.com/",
+    'category': '',
+    'version': '0.1',
+    'application': True,
+    'installable': True,
+    'depends': [],
+
+    'data': [],
+    'assets': {
+    },
+    'license': 'AGPL-3'
+}
