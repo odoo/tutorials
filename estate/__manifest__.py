0fcb37d9fb73fb07782490908597e1316447591e
akus06 <akus@odoo.com>
2025-02-04 15:18:30 +0530

    [ADD] estate:  Create Real Estate Management Module.
    Chapter 1: Implemented the multitier architecture followed by Odoo.
    Chapter 2: Created a module named estate, with the following files:
            __manifest__.py: Added basic module information and set application to True.
            __init__.py: Initialized the module.
    Chapter 3: Created a models directory within the estate module, containing:
            estate_property.py: Defined model and fields to create a table for estate properties.
            __init__.py: Initialized the models.

---
+++ estate/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': "estate",
+    'depends': ['base'],
+    'author': "Ayush Kumar Singh",
+    'application': True
+    
+}
+# SHA256:Hmym4hCb2IR8asKknQs4KfejqLJ6MOota2asZe9IQTU akus@odoo.com
