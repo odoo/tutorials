bb102e6d061b7063a34f960efc6e84d730e53634
shmn-odoo <shmn@odoo.com>
2025-02-04 19:22:16 +0530

    [ADD] estate: new module with access rights information
    Created estate module with initial structure
    
    - Added 'Model' folder containing 'estate_property' model.
    - Added 'Security' folder with a  CSV file for access information.

---
+++ estate/models/estate_property.py
@@ -0,0 +1,21 @@
+from odoo import fields, models
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Estate Properties"
+
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float()
+    bedrooms = fields.Integer(index=True)
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        selection = [('north','North'),('south','South'),('east','East'),('west','West')])
+    
