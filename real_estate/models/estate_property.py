c5007ff10ff6b990d5e3148226977aeb5d49104c
Devendra <djal@odoo.com>
2025-02-04 14:52:48 +0530

    [ADD] real_estate: new module added

---
+++ real_estate/models/estate_property.py
@@ -0,0 +1,9 @@
+from odoo import fields,models
+
+
+class EstateProperty(models.Model):
+    _name = "public.estate_property"
+    _description = "Estate Property Table"
+    _order = "sequence"
+
+    
