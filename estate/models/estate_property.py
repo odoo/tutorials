48a95097ebce4a2b2ce5d4229a314645c71458e7
ptpu-odoo <ptpu@odoo.com>
2025-02-04 17:31:52 +0530

    [ADD] estate: for adding xml files to make menus and added form (chap - 5)
    Added menu xml files and created a UI which shows list and form views from where one can create, update and delete data from database. Made selling prices in read only mode and added 3 months from today's date in avalability date section.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,24 +1,36 @@
 from odoo import fields, models
-
+# import for adding months with relativedelta
+from dateutil.relativedelta import relativedelta 
 
 class PropertyPlan(models.Model):
     _name = "estate.property"
     _description = "Estate property tables"
 
-    name = fields.Char(required=True)
-    description = fields.Text()
-    postcode = fields.Char()
-    date_availability = fields.Date()
-    expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
-    living_area = fields.Integer()
-    facades = fields.Integer()
-    garage = fields.Boolean()
-    garden = fields.Boolean()
-    garden_area = fields.Integer()
-    
+    name = fields.Char(string="Name",required=True)
+    description = fields.Text(string="Description")
+    postcode = fields.Char(string="Postcode")
+    date_availability = fields.Date(string="Date Availability",copy=False, default=(fields.Date.today()+relativedelta(months=+3))) #Added 3 months
+    expected_price = fields.Float(string="Expected Price",required=True)
+    selling_price = fields.Float(string="Selling Price",readonly=True)
+    bedrooms = fields.Integer(string="Bedrooms",default=2)
+    living_area = fields.Integer(string="Living Area")
+    facades = fields.Integer(string="Facades")
+    garage = fields.Boolean(string="Garage")
+    garden = fields.Boolean(string="Garden")
+    garden_area = fields.Integer(string="Garden Area")
+    active = fields.Boolean(string="Active",default=True)
+
+    #selection firld for "State"
+    state = fields.Selection(
+        string='Atate',
+        required=True,
+        default="new",
+        copy=False,
+        selection=[('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')],
+        help="Used to decide the state of Garden")
+
+    #selection firld for "Garden Orientation"
     garden_orientation = fields.Selection(
-        string='Type',
+        string='Garden Orientation',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
         help="Used to decide the direction of Garden")
