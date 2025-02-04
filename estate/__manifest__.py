1d5948106bbbf6d55de1e644076867758863f4a2
dvpa-odoo <dvpa@odoo.com>
2025-02-04 18:48:59 +0530

    [ADD] estate: add new module named estate & models and basic fields

---
+++ estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': "estate",
+    'description': "",
+
+    'data' : [
+        'Data/ir.model.access.csv',
+        'Views/estate_property_views.xml',
+        'Views/estate_menus.xml'
+    ]
+}
