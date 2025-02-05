6f531cff6aacd21846b49f97931c050571aaafdd
Jenish Korat <jeka@odoo.com>
2025-02-05 18:34:35 +0530

    [ADD] estate: add menus and changes properties of estate

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,6 +6,8 @@
     "license": "LGPL-3",
     "instalable": True,
     "data" : [
-        'security/ir.model.access.csv'
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+        'security/ir.model.access.csv',
     ]
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,17 +1,23 @@
 from odoo import fields, models
+from datetime import timedelta
 
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Estate Property"
 
-    name = fields.Char(required = True)
+    name = fields.Char(required=True)
     description = fields.Text()
+    active = fields.Boolean(
+        default=True, help="Mark as active if you want the property to be listed."
+    )
     postcode = fields.Char()
-    date_availability = fields.Date()
-    expected_price = fields.Float(required = True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    date_availability = fields.Date(
+        default=fields.Date.today() + timedelta(days=90), copy=False
+    )
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -24,5 +30,18 @@ class EstateProperty(models.Model):
             ("south", "South"),
             ("east", "East"),
             ("west", "West"),
+        ],
+    )
+    state = fields.Selection(
+        String="State",
+        required=True,
+        default="new",
+        copy=False,
+        selection=[
+            ("new", "New"),
+            ("offer received", "Offer Received"),
+            ("offer accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled"),
         ],
     )
