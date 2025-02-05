00aaf966119fd0684b2af3381adb95de5ef540ed
vishw <vpjo@odoo.com>
2025-02-05 15:12:48 +0530

    [ADD] estate: add estate module
    Added __init__.py and __manifest__.py files

---
+++ estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Estate',
+    'description': """
+        It is a Real Estate application made for users for a comfortable purchase
+        and sales of there properties
+        """,
+    'sequence': 1,
+    'depends' : ['base',],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
