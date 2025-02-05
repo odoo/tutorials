240fade89e08cd2356f4c6b2d1eedd0badf217f7
vpat-odoo <vpat@odoo.com>
2025-02-05 14:38:54 +0530

    [IMP] estate: basic UI and views
    Implemented list view and form view. List view is implemented for
    Advertisements->Properties and Form view is implemented for property form. also
    implemented  filters and  group by.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,9 +1,12 @@
 {
     'name': "Real Estate",
-    "application": True,
-    "license": "LGPL-3",
-    # 'depends': [],
-    'installable': True,
-    'data': ['views/estate_property_views.xml',
-    'security/ir.model.access.csv',],
+    'description': """ The Real Estate Advertisement module. """,
+    'license': 'LGPL-3',
+    'application': True,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_property_type_views.xml',
+        'views/estate_menus.xml',
+    ],
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,21 +1,43 @@
 from odoo import fields, models
 
-
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real estate property details"
-    name = fields.Char(string = "Name", required = True)
-    description = fields.Text(string = "Description", required = True)
-    postcode = fields.Char(string = "Postcode")
-    date_availability = fields.Date(string = "Date Availability")
-    expected_price = fields.Float(string = "Expected Price")
-    selling_price = fields.Float(string = "Selling Price")
-    bedrooms = fields.Integer(string = "Bedrooms")
-    living_area = fields.Integer(string = "Living Area")
-    facades = fields.Integer(string = "Facades")
-    garage = fields.Boolean(string = "Garage")
-    garden = fields.Boolean(string = "Garden")
-    garden_area = fields.Integer(string = "Garden Area")
+
+    name = fields.Char(string="Name", required=True)
+    description = fields.Text(string="Description", required=True)
+    postcode = fields.Char(string="Postcode")
+    date_availability = fields.Date(string="Available From", copy=False, default=fields.Date.add(fields.Date.today(), months=3)) 
+    expected_price = fields.Float(string="Expected Price")
+    selling_price = fields.Float(string="Selling Price", readonly=True, copy=False) 
+    bedrooms = fields.Integer(string="Bedrooms", default=2)
+    living_area = fields.Integer(string="Living Area")
+    facades = fields.Integer(string="Facades")
+    garage = fields.Boolean(string="Garage")
+    garden = fields.Boolean(string="Garden")
+    garden_area = fields.Integer(string="Garden Area")
     garden_orientation = fields.Selection(
-        string = "Garden Orientation",
-        selection = [("north", "North"), ("south", "South"), ("east", "East"), ("west", "West")])
+        string="Garden Orientation",
+        selection=[
+            ("north", "North"), 
+            ("south", "South"), 
+            ("east", "East"), 
+            ("west", "West")
+        ]
+    )
+    active = fields.Boolean(string="Active", default=True)
+    state = fields.Selection(
+        string="State",
+        selection=[
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled")
+        ],
+        required=True,
+        copy=False,
+        default="new"
+    )
+    property_type_id = fields.Many2one('estate.property.type', string="Property Type")
+    
