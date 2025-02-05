fbdc1915d18eea543ad424c75cc3f8ad38cd6c17
ssud-odoo <ssud@odoo.com>
2025-02-05 11:16:52 +0530

    [ADD] estate: added new module

---
+++ estate/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Estate",
+    'summary': """
+        Estate Made Easy"
+    """,
+
+    'description': """
+        Estate can be very easy with our 
+    """,
+
+    'author': "Odoo",
+    'website': "https://www.odoo.com"
+}
