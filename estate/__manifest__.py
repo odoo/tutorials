144cfb2bd302cf0d3cabc61191bf9683d26eb1d6
gppa_odoo <gppa@odoo.com>
2025-02-05 15:26:18 +0530

    [IMP] Estate : View added

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,8 +3,14 @@
 
 {
     'name': 'Real Estate',
+    'category':'module_category_real_estate_brokerage',
     'depends': [
         'base'
+    ],
+    "data": [
+         'security/ir.model.access.csv',
+         'views/estate_property_view.xml',
+         'views/estate_menu.xml'
     ],
     'installable': True,
     'application': True,
--- estate/models/real_estate_properties.py
+++ estate/models/real_estate_properties.py
@@ -1,29 +1,39 @@
 from odoo import fields, models
+from datetime import timedelta
+from datetime import datetime
 
 class realEstateProperty(models.Model):
-    _name = "estate_property"
+    _name = "estate.property"
     _description = "Real Estate property table :)"
 
     id = fields.Integer()
     create_uid = fields.Integer()
-    create_date = fields.Datetime('Write Date', readonly=True)
+    create_date = fields.Datetime('Create Date', readonly=True, default=fields.Datetime.now)
     write_uid = fields.Integer()
-    write_date = fields.Datetime('Write Date', readonly=True)
+    write_date = fields.Datetime('Write Date', readonly=True, default=fields.Datetime.now)
     name = fields.Char(required=True)
     description = fields.Text()
-    postcode = fields.Char('postcode')
-    date_availability = fields.Date('date_availability', readonly=True)
-    expected_price = fields.Float('expected_price', required=True)
-    selling_price = fields.Float('selling_price')
-    bedrooms = fields.Integer('bedrooms')
-    living_area = fields.Integer('living_area')
-    facades = fields.Integer('facades')
-    garage = fields.Boolean('garage')
-    garden = fields.Boolean('garden')
-    garden_area = fields.Integer('garden_area')
+    postcode = fields.Char('Postcode')
+    date_availability = fields.Date('Date availability',default = datetime.now() + timedelta(days=90), readonly=True, copy=False)
+    expected_price = fields.Float('Expected price', required=True)
+    selling_price = fields.Float('Selling price', readonly=True, copy=False)
+    bedrooms = fields.Integer('Bedrooms', default=2)
+    living_area = fields.Integer('Living area')
+    facades = fields.Integer('Facades')
+    garage = fields.Boolean('Garage')
+    garden = fields.Boolean('Garden')
+    garden_area = fields.Integer('Garden Area')
     garden_orientation = fields.Selection([
         ('north', 'North'),
         ('south', 'South'),
         ('east', 'East'),
         ('west', 'West')
-    ], string='garden_orientation')
+    ], string='Garden Orientation')
+    active = fields.Boolean('Active', default=True)
+    state = fields.Selection([
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted','Offer Accepted'),
+            ('sold','Sold'),
+            ('cancelled','Cancelled')
+    ], default='new', required=True)
