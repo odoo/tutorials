0c187579f52be518e3c978f913c8ae33d4a091e0
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 <rsbh@odoo.com>
2025-02-05 13:29:52 +0530

    [ADD] estate_property: Create estate_property_views add to manifest

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -8,11 +8,12 @@
     'description': "A module for managing real estate properties",
     'depends':['base'],
     'data': [
-        "security/ir.model.access.xml",
-
+        "security/ir.model.access.csv",
+        # "views/estate_property_views.xml",
+        # "views/estate_menus.xml",
     ],
     'demo': [],
-    'installable': True,
-    'application': True,
-    'auto_install': False,
+    # 'installable': True,
+    # 'application': True,
+    # 'auto_install': False,
 }
