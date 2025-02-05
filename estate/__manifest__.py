3368e6f0b60a42aad54887bb901bebe1411858dc
jsum-odoo <jsum@odoo.com>
2025-02-05 15:54:38 +0530

    [ADD] estate: added new real estate module

---
+++ estate/__manifest__.py
@@ -0,0 +1,12 @@
+
+{
+    "name":"Real Estate",
+    "version":"1.0",
+    "author":"jsum",
+    "depends":["base"],
+    'application': True,
+    'description': """
+   Real Estate Test Description
+    """,
+    "data":["security/ir.model.access.csv","views/estate_property_views.xml","views/estate_menu.xml"]
+}
---
+++ estate/models/estate_property.py
@@ -0,0 +1,22 @@
+from odoo import models,fields
+
+class EstateProperty(models.Model):
+    _name="estate.property"
+    _description="Real Estate Property"
+
+    name = fields.Char(string="Title",required=True)
+    description = fields.Text(string="Property Description")
+    postcode = fields.Char(string="Postcode")
+    date_availability = fields.Date(string="Available From",copy=False,default=fields.datetime.today()+fields.date_utils.relativedelta(months=3))
+    expected_price = fields.Float(string="Expected Price",required=True)
+    selling_price = fields.Float(string="Selling Price",readonly=True,copy=False)
+    bedrooms = fields.Integer(string="Bedrooms",default=2)
+    living_area = fields.Integer(string="Living Area(sqm)")
+    fascades = fields.Integer(string="Fascades")
+    garage = fields.Boolean(string="Garage")
+    garden = fields.Boolean(string="Garden")
+    garden_area = fields.Integer(string="Garden Area")
+    garden_orientation = fields.Selection(string="Garden Orientation",selection=[("north","North"),("south","South"),("east","East"),("west","West")]) # define default value for it in future
+    # Following active fields refer to the visiblity of field in search
+    active = fields.Boolean("Active",default=True)
+    state= fields.Selection([("new","New"),("offer received","Offer Received"),("offer accepted","Offer Accepted"),("sold","Sold"),("cancelled","Cancelled")],"State",required=True,default="new")
