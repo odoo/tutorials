4f2779418facabee6c0161a7f6ad837125287409
akth-odoo <akth@odoo.com>
2025-02-05 11:08:34 +0530

    [IMP] estate:  Added access controls, action, menus and custom views.
    In Chapter 4: Created ir.model.access.csv file in data folder given read, write,
    create and unlink permissions to the group base.group_user. Allowing users in
    that group to perform these actions on the model.
    
    In Chapter 5:Created the estate_property_views.xml file in the  views folder.
    Created an action for the model estate.property. Created the three levels of
    menus  for the estate.property. Set the selling price as read-only
    prevent copying of the availability date and the selling price values.Added the
    appropriate default attributes. Added the active field to the estate.property model.
    Added a state field to the estate.property model.
    
    In Chapter 6: Defined a list view for the estate.property model in
    estate_property_views.xml file. Added a custom form view. Added a custom search
    view, Filter and Group By.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,7 +1,13 @@
 {
     'name': "estate",
     'depends': ['base'],
-    'application': True
+    'application': True,
+    'data': [
+        'data/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
+    ],
+    'license': 'LGPL-3'
     
 }
 
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,23 +1,33 @@
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta
 
 
 class Property_Plan(models.Model):
-    _name = "estate_property"
+    _name = "estate.property"
     _description = "Test Model"
-    name = fields.Char(required=True)
+   
+    name = fields.Char(required=True,string="Title")
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False,default=fields.Date.today()+ relativedelta(months=3),string="Available From")
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
-    living_area = fields.Integer()
+    selling_price = fields.Float(readonly=True,copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer(string="Living Area (sqm)")
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
-        string='Orientation',
+        string='Garden Orientation',
         selection=[("north","North"), ("south","South"),("east","East"),("west","West")],
         help="Type is used for direction"
     )
+    active = fields.Boolean(default=True)
+    state = fields.Selection([
+        ('new', 'New'),
+        ('offer_received', 'Offer Received'),
+        ('offer_accepted', 'Offer Accepted'),
+        ('sold', 'Sold'),
+        ('cancelled', 'Cancelled')
+    ],required=True,copy=False,default='new')
