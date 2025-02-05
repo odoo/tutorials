f35935400e28691e787b82fbfe4e6a16898053df
akus-odoo <akus@odoo.com>
2025-02-05 10:54:20 +0530

    [IMP] estate:  Add more configuration to user interface.
    Chapter 4: Initialize a new file ir.model.access.csv and provide different
    permissions in it.
    Chapter 5: In this chapter, we learn about XML files and why Odoo uses them. We
    then create the estate_property_views.xml file to define actions. After that, we
    create a file called estate_menus.xml to display menus for these actions. We
    define the necessary fields and attributes for our real estate interface, then
    set some limitations on the fields, such as making them read-only. We also add
    default values to some fields and introduce two additional fields: active and
    state, providing values for them.
    Chapter 6: In this chapter, we understand the different types of views (list,
    form, search) in an interface. We first create a list view to display our real
    estate properties. Then, we create a form view for editing or updating existing
    properties, using some HTML tags for customization. Lastly, we create a search
     view to allow users to filter properties based on different criteria. We also
    add filters and groupings to display specific data, such as properties with the
    state set to "new" or "offer received". During this process, we learn about the
     use of domains in Odoo.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,8 +1,13 @@
 {
-    'name': "estate",
+    'name': "Real Estate",
     'depends': ['base'],
     'author': "Ayush Kumar Singh",
-    'application': True
-    
+    'application': True,
+    'data': [
+        'data/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        "views/estate_menus.xml"
+    ],
+    'license': 'LGPL-3'
 }
 # SHA256:Hmym4hCb2IR8asKknQs4KfejqLJ6MOota2asZe9IQTU akus@odoo.com
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,23 +1,27 @@
 from odoo import fields, models
+from datetime import datetime, timedelta
 
-
-class RecurringPlan(models.Model):
+class Property_Plan(models.Model):
     _name = "estate_property"
     _description = "Test Model"
-    name = fields.Char(required=True)
-    description = fields.Text()
-    postcode = fields.Char()
-    date_availability = fields.Date()
-    expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
-    living_area = fields.Integer()
-    facades = fields.Integer()
-    garage = fields.Boolean()
-    garden = fields.Boolean()
-    garden_area = fields.Integer()
+    name = fields.Char(string="Title",required=True)
+    description = fields.Text(string="Description")
+    postcode = fields.Char(string="Postcode")
+    date_availability = fields.Date(string="Date Availability", copy=False, default=lambda self: (datetime.today() + timedelta(days=90)).date())
+    expected_price = fields.Float(string="Expected Price",required=True)
+    selling_price = fields.Float(string="Selling Price",readonly=True,copy=False)
+    bedrooms = fields.Integer(string="Bedrooms",default=2)
+    living_area = fields.Integer(string="Living Area")
+    facades = fields.Integer(string="Facades")
+    garage = fields.Boolean(string="Garage")
+    garden = fields.Boolean(string="Garden")
+    garden_area = fields.Integer(string="Garden Area")
     garden_orientation = fields.Selection(
-        string='Orientation',
+        string='Garden Orientation',
         selection=[("north","North"), ("south","South"),("east","East"),("west","West")],
         help="Type is used for direction"
     )
+    
+    state = fields.Selection(string="State", selection=[('new', 'New'), ('offer received', 'Offer Received'), ('offer accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')], required=True, copy=False)
+    active = fields.Boolean(string="Active", default=True)
+    
