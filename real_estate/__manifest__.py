d4e72a6c3d335b898d4297e6e99e7f7a149bdb89
Kunj Patel(kupa) <kupa@odoo.com>
2025-02-05 18:57:20 +0530

    [ADD] real_estate: create list view, form view, searchable field in search view and also modified model

--- real_estate/__manifest__.py
+++ real_estate/__manifest__.py
@@ -1,27 +1,23 @@
 {
     'name': "Real Estate Module",
 
-    'summary': """
-        Starting module for Real Estate Project"
-    """,
+    'summary': "Starting module for Real Estate Project",
+    'description': "Starting module for Real Estate Project Description",
 
-    'description': """
-        Starting module for Real Estate Project Description
-    """,
-
-    'author': "Odoo",
+    'author': "Odoo SA",
     'website': "https://www.odoo.com/",
     'category': 'Tutorials',
-    'version': '0.1',
+    'version': '1.0',
     'application': True,
     'installable': True,
     'depends': ['base'],
 
     'data': [
-        'security/security_groups.xml',
-        'security/real_estate.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_property_views_list.xml',
+        'views/estate_property_views_form.xml',
+        'views/estate_menu_views.xml'
     ],
-    'assets': {
-    },
     'license': 'AGPL-3'
 }
--- real_estate/models/estate_property.py
+++ real_estate/models/estate_property.py
@@ -1,27 +1,100 @@
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta
 import datetime
 
 class RealEstateProperty(models.Model):
 
-    _name = "estate_property"
+    _name = "estate.property"
     _description = "Real Estate Property"
 
-    name = fields.Char(string='Property Name', required=True, help="Enter the name of the property")
-    description = fields.Text(string='Description', required=True, help="Provide a detailed description of the property")
-    postcode = fields.Char(string='Postcode', required=True, help="Enter the postcode of the property")
-    date_availability = fields.Date(string='Date of Availability', required=True, help="Specify when the property will be available")
-    expected_price = fields.Float(string='Expected Price', required=True, help="Enter the expected price of the property")
-    selling_price = fields.Float(string='Selling Price', required=True, help="Enter the selling price of the property")
 
-    bedrooms = fields.Integer(string='Bedrooms', required=True, help="Enter the number of bedrooms")
-    living_area = fields.Integer(string='Living Area (sq meters)', required=True, help="Enter the living area of the property")
-    facades = fields.Integer(string='Number of Facades', required=True, help="Enter the number of facades the property has")
+    # create_date = fields.Datetime(
+    #     string="Creation Date", 
+    #     default=datetime.datetime.now(), 
+    #     readonly=True
+    # )
+    # create_uid = fields.Many2one(
+    #     'res.users', 
+    #     string='Created By', 
+    #     readonly=True
+    # ) 
+    # write_date = fields.Datetime(
+    #     string='Last Modified', 
+    #     readonly=True
+    # )
+    # write_uid = fields.Many2one(
+    #     'res.users', 
+    #     string='Modified By', 
+    #     readonly=True
+    # )
+
+    name = fields.Char(
+        string='Property Name', 
+        required=True, 
+        help="Enter the name of the property"
+    )
+    description = fields.Text(
+        string='Description', 
+        required=True, 
+        help="Provide a detailed description of the property"
+    )
+    postcode = fields.Char(
+        string='Postcode', 
+        required=True, 
+        help="Enter the postcode of the property"
+    )
+    date_availability = fields.Date(
+        string='Date of Availability', 
+        required=True, 
+        copy=False,
+        default=lambda self: fields.Date.context_today(self) + relativedelta(months=+3),
+        help="Specify when the property will be available"
+    )
+    expected_price = fields.Float(
+        string='Expected Price', 
+        required=True, 
+        help="Enter the expected price of the property"
+    )
+    selling_price = fields.Float(
+        string='Selling Price', 
+        # required=True, 
+        readonly=True,
+        copy=False,
+        help="Enter the selling price of the property"
+    )
+
+    bedrooms = fields.Integer(
+        string='Bedrooms', 
+        required=True, 
+        default=2,
+        help="Enter the number of bedrooms"
+    )
+    living_area = fields.Integer(
+        string='Living Area (sq meters)', 
+        required=True, 
+        help="Enter the living area of the property"
+    )
+    facades = fields.Integer(
+        string='Number of Facades', 
+        required=True, 
+        help="Enter the number of facades the property has"
+    )
     
-    garage = fields.Boolean(string='Has Garage', default=False, help="Check if the property has a garage")
-    garden = fields.Boolean(string='Has Garden', default=False, help="Check if the property has a garden")
+    garage = fields.Boolean(
+        string='Has Garage', 
+        help="Check if the property has a garage"
+    )
+    garden = fields.Boolean(
+        string='Has Garden', 
+        help="Check if the property has a garden"
+    )
     
     
-    garden_area = fields.Integer(string='Garden Area (sq meters)', required=True, help="Enter the area of the garden")
+    garden_area = fields.Integer(
+        string='Garden Area (sq meters)', 
+        required=True, 
+        help="Enter the area of the garden"
+    )
     garden_orientation = fields.Selection(
         [
             ('north', 'North'),
@@ -33,7 +106,24 @@ class RealEstateProperty(models.Model):
         required=True, help="Select the orientation of the garden"
     )
 
-    # create_date = fields.Datetime(string="Creation Date", default=datetime.datetime.now(), readOnly=True)
-    # create_uid = fields.Many2one('res.users', string='Created By', readonly=True) 
-    # write_date = fields.Datetime(string='Last Modified', readonly=True)
-    # write_uid = fields.Many2one('res.users', string='Modified By', readOnly=True)
+    active = fields.Boolean(
+        string='Active',
+        default=False,
+        help="If unchecked, it will archive the property."
+    )
+
+    state = fields.Selection(
+        selection=[
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('cancelled', 'Cancelled'),
+        ],
+        string="State",
+        required=True,
+        default='new',  
+        copy=False,     
+        help="Current state of the property."
+    )
+
