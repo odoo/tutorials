cf86128d3a8b8b3959df0d410369dfcb67a53fc9
mdra-odoo <mdra@odoo.com>
2025-02-05 11:17:04 +0530

    [IMP] estate: cahpter3 chapter4 chapter5_half
    - created estate_property model in models folder from chapter3
    - added csv file in security folder and path added to manifest for access management from chapter4
    - created menuitem and view for Real Estate app chapter5

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,9 +1,16 @@
 {
-    'name':'Real Estate',
-    'version':'1.0',
-    'depends':['base'],
-    'author':'odoo',
-    'license':'LGPL-3',
-    'discription':"""This is Real Estate application which makes easy to sold property""",
-    'application':True
+    'name': 'Real Estate',
+    'version': '1.0',
+    'depends': ['base'],
+    'author': 'odoo',
+    'license': 'LGPL-3',
+    'description':"""
+This is Real Estate application which makes easy to sold property
+    """,
+    'application': True,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+    ]
 }
