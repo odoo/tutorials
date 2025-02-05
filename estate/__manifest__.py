676f5f6a28945292a6596daf454444f539b4c9cd
krge-odoo <krge@odoo.com>
2025-02-05 11:17:35 +0530

    [ADD] estate: create a new estate module

---
+++ estate/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name' : "Real Estate",
+
+    'summary' : "A Real Estate Module that streamlines property listings, transactions, and client management with advanced search and analytics.",
+
+    'description': "The Real Estate Module is a feature-rich solution for managing property listings, buyers, and transactions efficiently. It offers advanced search filters, virtual tours, and interactive maps to enhance the user experience. Built-in lead management and automated workflows help real estate professionals track inquiries and close deals faster. Secure document handling and analytics provide insights for better decision-making. Designed for agents, brokers, and property managers, this module ensures seamless real estate operations.",
+
+    'author': "Krunal Gelot",
+    'website': "https://www.odoo.com",
+
+    'category': 'Tutorials',
+    'version': '0.1',
+
+    'depends': ['base'],
+    'application': True,
+    'installable': True,
+
+
+    'license': 'AGPL-3'
+}
