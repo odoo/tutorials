584da76a48785ecc31d5e21e640ed4941c447cb6
sabj-odoo <sabj@odoo.com>
2025-02-05 10:17:24 +0530

    [ADD] Day 1
    Chapter 1: Architecture Overview
    Odoo follow Multitier architecture which includes present
    tier , logic tier , data tier.
    
    Chapter 2 : A new Application
    A real estate module is created name "estate".

---
+++ estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'category': 'Real Estate',
+    'summary': 'Module to manage real estate advertisements',
+    'description': 'A module to create and manage real estate advertisements.',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
