38f577329dc20531ea17b96e945486f6aad02e85
jmra-odoo <jmra@odoo.com>
2025-02-05 10:22:39 +0530

    [IMP] estate: Access right to group, Menus, List view, Form view and Search view
    In access right provided read, write, update and unlink permission in 4th chapter, implemented menus for properties in 5th chapter, Modification of List, Form and Search view is done with the 6th chapter

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,4 +1,10 @@
 {
     'name': "Real Estate",
     'depends': ['base'],
+    'application': True,
+    'data': [
+        'data/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
+    ]
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,19 +1,41 @@
 from odoo import models, fields
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 class EstateProperty(models.Model):
-    _name = "estate_property"
+    _name = "estate.property"
     _description = "Estate Property"
 
-    name = fields.Char(required=True)
-    description = fields.Text()
-    postcode = fields.Char()
-    date_availability = fields.Date()
-    expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
-    living_area = fields.Integer()
-    facades = fields.Integer()
-    garage = fields.Boolean()
-    garden = fields.Boolean()
-    garden_area = fields.Integer()
-    garden_orientation = fields.Selection(selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]);
+    def calculate_three_months_later():
+        today = datetime.now()
+
+        return today + relativedelta(months=3)
+
+    # Fields of Property
+    name = fields.Char(string="Name", required=True)
+    description = fields.Text(string="Description")
+    postcode = fields.Char(string="Postcode")
+    date_availability = fields.Date(string="Date Availability", default=calculate_three_months_later(), copy=False)
+    expected_price = fields.Float(string="Expected Price", required=True)
+    selling_price = fields.Float(string="Selling Price", readonly=True, copy=False)
+    bedrooms = fields.Integer(string="Bedrooms", default=2)
+    living_area = fields.Integer(string="Living Area")
+    facades = fields.Integer(string="Facades")
+    garage = fields.Boolean(string="Garage")
+    garden = fields.Boolean(string="Garden")
+    garden_area = fields.Integer(string="Garden Area")
+    
+    # Garden Orientation of Property
+    garden_orientation = fields.Selection(
+        string="Garden Orientation", 
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]);
+    
+    active = fields.Boolean(string="Is Active", default=True)
+    
+    # State Selection of Property
+    state = fields.Selection(
+        string="State", 
+        required=True, 
+        copy=False,
+        default="new",
+        selection=[('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')])
