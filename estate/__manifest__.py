43fdf782f85e5c005f6e32c5b2f0e843a90e22a7
duba-odoo <duba@odoo.com>
2025-02-05 18:59:35 +0530

    [ADD] estate: added views and improved UI
    Day 2:
    Chapter 3:Added more fields to existing model
    Chapter 4:Updated Security by giving access
    Chapter 5:Updated UI

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,8 +5,15 @@
     'summary': 'Manage real estate listings and offers',
     'author': 'Anubhav Dubey',
     'website': 'http://localhost:8069',
-    'depends': ['base'],  # Base module is required
-    'application': True,  # Makes it an "App" in Odoo
-    'installable': True,  # Ensures the module can be installed
+    'depends': ['base'],  
+    'data':[
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        #'views/estate_property_views_list.xml',
+        'views/estate_menus.xml',
+        
+    ],
+    'application': True,  
+    'installable': True,  
 }
 
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,10 +1,36 @@
 from odoo import models,fields 
+from datetime import date,timedelta
 
 class EstateProperty(models.Model):
 	_name="estate.property"
-	_desciption = "Real Estate Property"
+	_description = "Real Estate Property"
 
 	name =fields.Char('Property Name',required =True)
+	title=fields.Char('Title')
 	description = fields.Text('Description')
+	postcode=fields.Char('Postcode')
+	available_from=fields.Date('Available From',copy=False,default=date.today()+timedelta(days=90))
 	expected_price=fields.Float('Expected Price',required =True)
-	selling_price = fields.Float('Selling Price')
+	selling_price = fields.Float('Selling Price',readonly=True,copy=False)
+	bedrooms=fields.Integer('Bedrooms',default=2)
+	living_area=fields.Integer('Living Area(sqm)')
+	facades=fields.Integer('Facades')
+	garage=fields.Boolean('Garage')
+	garden=fields.Boolean('Garden')
+	garden_area=fields.Integer('Garden Area')
+	garden_orientation=fields.Selection([
+		('north','North'),
+		('south','South'),
+		('east','East'),
+		('west','West')
+	],string="Garden Orientation",default='north')
+	state=fields.Selection([
+		('new','New'),
+		('offer received','Offer Received'),
+		('offer accepted','Offer Accepted'),
+		('sold','Sold'),
+		('cancelled','Cancelled')
+	],string="Status",required=True,copy=False,default="new")
+	active=fields.Boolean(string="Active",default=True)
+	
+	
