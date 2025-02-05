e02012c301fbe5c9ec036f6943052cca33c03810
nmak-odoo <nmak@odoo.com>
2025-02-05 18:49:52 +0530

    [IMP] real-estate: security , ui and search filters
    Day 2 :
    Chapter 4: Understood Data files(CSV) and gave access-rights
    Chapter 5: Understood Data files(XML) and Implemented actions , menus and fields
    Chapter 6: Understood Basic views and Implemented List , Form and Search filters

--- real-estate/__manifest__.py
+++ real-estate/__manifest__.py
@@ -4,7 +4,14 @@
     "website": "https://www.odoo.com/apps/estate",
     "category": "tutorials/real-estate",
     "depends": ["base"],
-    # "data": ["security/ir.model.access.csv"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
+    ],
     "installable": True,
-    "application": True
+    "application": True,
+    "auto_install": False,
 }
+
+# "data/res.country.state.csv" ,
--- real-estate/models/estate_property.py
+++ real-estate/models/estate_property.py
@@ -1,21 +1,47 @@
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta 
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Properties involved in estate"
 
-    name = fields.Char(required = True)
+    name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
-    expected_price = fields.Float(required = True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    date_availability = fields.Date(
+        "Availability Date",
+        copy=False,
+        default=fields.Date.add(fields.Date.today() , months = 3),
+    )
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Boolean()
+    garden_area = fields.Float()
     garden_orientation = fields.Selection(
-        selection=[("north" , "North") , ("south", "South"), ("east", "East"), ("west", "West")]
+        selection=[
+            ("north", "North"),
+            ("south", "South"),
+            ("east", "East"),
+            ("west", "West"),
+        ]
+    )
+
+    # reserved fields
+    active = fields.Boolean(default=False)
+    state = fields.Selection(
+        [
+            ("new", "New"),
+            ("offer_recieved", "Offer Recieved"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled"),
+        ],
+        string="Invoice Status",
+        default="new",
+        required=True,
+        copy=False,
     )
