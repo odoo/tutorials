59c75af8bf7620ecdf9efaf65ca3bbc825826eb5
pbch-odoo <pbch@odoo.com>
2025-02-05 19:19:14 +0530

    [IMP] estate: Understanding till chapter 5

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -8,7 +8,10 @@
     'depends' : [
         'base'
     ],
-    
+    'data' : [
+        'view/estate_property_views.xml',
+        'data/ir.model.access.csv'
+    ],
     'installable': True,
     'application': True,
    
