bae59a170332f857bf8e8f88d05570bab281f8b8
jsum-odoo <jsum@odoo.com>
2025-02-05 17:14:07 +0530

    [IMP] estate: added license to manifest and formatted the code

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,12 +1,16 @@
-
 {
-    "name":"Real Estate",
-    "version":"1.0",
-    "author":"jsum",
-    "depends":["base"],
-    'application': True,
-    'description': """
+    "name": "Real Estate",
+    "version": "1.0",
+    "author": "jsum",
+    "depends": ["base"],
+    "application": True,
+    "description": """
    Real Estate Test Description
     """,
-    "data":["security/ir.model.access.csv","views/estate_property_views.xml","views/estate_menu.xml"]
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menu.xml",
+    ],
+    "license": "LGPL-3",
 }
