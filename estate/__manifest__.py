96dc4bf8585a1220a0f51f509a10f817e0fd34da
iemu-odoo <iemu@odoo.com>
2025-02-05 14:51:12 +0530

    [IMP] estate: needed to add some new model fields and security
    New fields to the estate.property model were required.
    Security data files were needed to manage groups, access rights of the users.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,25 +1,25 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-
 {
-    'name': 'Real Estate', # display name of the app
-    'summary': 'Allows to buy and sell properties', # a short summary of app's functionality
+    'name': 'Real Estate',
+    'summary': 'Manage real estate properties',
     'description': """
-    Description text
-    """, # functionality of this module in detail
+    This module allows the users to manage their real estate properties.
+    It also enables them to advertise them.
+    """,
+    'category': 'Marketing',
     'version': '1.0',
-    'author': "iemu",
-    'category': '',
-    'depends': [ # a list of all modules this module depends on
+    'depends': [
         'base',
     ],
-    'application': True, # is it an app / is it a module that will be showed in the Apps app
-    'auto_install': False, # If True, this module will automatically be installed if all of its dependencies are installed
-    # 'data': [ # data files always loaded at installation
-        # 'views/mymodule_view.xml',
-    # ],
-    # 'demo': [ # data files containing optionally loaded demonstration data
-        # 'demo/demo_data.xml',
-    # ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,17 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 from odoo import fields, models
 
-
-class EstateProperty(models.Model):
-    _name = "estate.estate.property"
-    _description = "Estate property"
+class Property(models.Model):
+    _name = "estate.property"
+    _description = "Estate property model"
 
     name = fields.Char('Property Name', required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date('Date of Availability')
+    date_availability = fields.Date('Date of Availability',copy=False)
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer('No. of Bedrooms')
+    selling_price = fields.Float(readonly=True,copy=False)
+    bedrooms = fields.Integer('No. of Bedrooms',default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     active = fields.Boolean(default=True)
@@ -20,4 +22,7 @@ class EstateProperty(models.Model):
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
         selection = [('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
-    )
+    ),
+    state = fields.Selection(
+        [("new", "New"), ("offer_received", "Offer Received"), ("offer_accepted", "Offer Accepted"), ("sold", "Sold"), ("cancelled", "Cancelled")],
+        required=True, default="new", copy=False)
