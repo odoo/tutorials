a68e51bc5b58dbb8cb088990a787e1a717f9aa91
apri-odoo <apri@odoo.com>
2025-02-05 19:21:43 +0530

    [ADD] models: added estate models
    -ADD- estate models
    -ADD- __init_.py
    -ADD- estate_property.py
    -ADD- estate security
    -ADD- ir.model.access.csv
    -ADD- link this csv to esate/__manifest__.py

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,10 +2,14 @@
     'name': 'Estate',
     'version':'1.0',
     'depends': ['base'],
-    'description': """
-This is a base version
-    """,
-    'installable': True,
+    'description': """This is a base version of the Estate module for real estate management.""",
+    'license': 'LGPL-3',
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menu.xml',
+    ],
     'application': True,
-    'auto_install': False,
+    'installable':True,
+    # 'auto_install': False,
 }
