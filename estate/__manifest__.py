683468a3016a0b6fca9ff991f3794d5465124639
mkap-odoo <mkap@odoo.com>
2025-02-05 18:52:10 +0530

    [ADD] estate: access rules, views and menus
    In this commit:
    - Created security/ and views/ directories
    - Added real estate application to the root menu
    - Added "advertisements" and "properties" as sub-menus
    - Added action to "properties" sub-menu opening the records of properties
    - Introduced status and active fields in the estate.property model
    - Set the default available_date to 3 months from today's date

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,29 +1,18 @@
 {
     'name': "estate app",
-
     'summary': """
-        estate module"
+        estate app"
     """,
-
     'description': """
-        estate module"
+        A real estate application for selling and renting properties"
     """,
-
     'author': "Odoo",
-    'website': "https://www.odoo.com/",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Tutorials/estate',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base', 'web'],
     'application': True,
-    'installable': True,
-    'data': [
-      #  'views/templates.xml',
+     'data': [
+        'security/ir.model.access.csv',
+        'views/estate_menus.xml',
+        'views/estate_property_views.xml'
     ],
     'license': 'AGPL-3'
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,39 +1,25 @@
 from odoo import fields, models
+from datetime import date , timedelta
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real Estate App"
-    name = fields.Char()
-    description
 
-postcode
-
-date_availability
-
-expected_price
-
-selling_price
-
-bedrooms
-
-living_area
-
-facades
-
-garage
-
-garden
-
-garden_area
-
-garden_orientation
-    # _order = "sequence"
-
-    # name = fields.Char('Plan Name', required=True, translate=True)
-    # number_of_months = fields.Integer('# Months', required=True)
-    # active = fields.Boolean('Active', default=True)
-    # sequence = fields.Integer('Sequence', default=10)
-
-    # _sql_constraints = [
-    #     ('check_number_of_months', 'CHECK(number_of_months >= 0)', 'The number of month can\'t be negative.'),
-    # ]
+    name = fields.Char(required=True)
+    description = fields.Text() 
+    postcode = fields.Char()
+    date_availability = fields.Date(default=lambda self: date.today() + timedelta(days=90))
+    expected_price = fields.Float()
+    selling_price = fields.Float(readonly=True , copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        selection = [('north', 'North'), ('south', 'South'),('east', 'East'),('west', 'West'),]
+    )
+    status = fields.Selection(
+        selection = [('new', 'New'), ('offer_reject', 'Offer Rejected'),('offer_accept', 'Offer Accepted'), ('sold','Sold'),('cancelled', 'Cancelled')], default="new", required=True )
+    active = fields.Boolean(default=True)   
+    
