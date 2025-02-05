e1ba552a38f695d22bf9aa659a0b76b5ff3e2b06
panj-odoo <panj@odoo.com>
2025-02-05 11:16:43 +0530

    [ADD] estate: creating a new module of real estate

---
+++ estate/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name':"Real Estate", # this will be the user friendly name of the module will show as the name of our moudle 
+
+    'summary':"an application which helps every real estate business owner", 
+
+    'description':"This application is designed to streamline operations for real estate business owners, offering a comprehensive platform to manage property listings, client interactions, and transactions efficiently. ",
+
+    'author': "panj",
+    'website': "https://www.odoo.com",
+
+    'category': 'Tutorials/RealEstate',
+
+    'version': '0.1',
+
+    'depends': ['base'],
+    'application': True,
+    'installable': True,
+
+    'license': 'AGPL-3'
+}
+
+
