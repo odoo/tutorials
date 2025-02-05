d2f41543b25280ea9ef4e86bee1ba2347693df01
vpan-odoo <vpan@odoo>
2025-02-05 11:19:19 +0530

    [ADD] estate: initialised the estate module

---
+++ estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "estate",
+    "summary": "A real estate app",
+    "description": "A real estate app developed by Vedant within the four walls of Odoo IN",
+    "author": "Vedant Pandey (vpan)",
+    "website": "https://odoo.com",
+    "category": "Tutorials/Estate",
+    "version": "0.1",
+    "depends": ["base"],
+    "application": True
+
+}
