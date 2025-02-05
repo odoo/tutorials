b9c8bdc5348899a6a47bbc8aabce0764239a5307
Dhruv Godhani <dhgo@odoo.com>
2025-02-05 19:04:32 +0530

    [ADD] estate: add some new fields and add some menuitems
    - create estate.property model

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -9,7 +9,9 @@
     """,
     # data files always loaded at installation
     'data': [
-        "security/ir.model.access.py"
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menu.xml"
     ],
     # data files containing optionally loaded demonstration data
     'demo': [
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta
 
 class estate_property(models.Model):
     _name = "estate.property"
@@ -7,10 +8,10 @@ class estate_property(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(default=fields.Datetime.today() + relativedelta(days=90))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -25,3 +26,4 @@ class estate_property(models.Model):
             ("west", "West"),
         ],
     )
+    active = fields.Boolean()
