da0aa570e7a6827542c711eaa3772000c68fba80
rusp-odoo <rusp@odoo.com>
2025-02-05 17:31:39 +0530

    [ADD] estate: define the estate_property model
    In this commit:
    - Added estate_property model
    - Define the properties of the table

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,9 +3,7 @@
     'version': '1.0',
     'depends': ['base'],
     'author': 'Odoo - Rushil Patel',
-    'category': 'Category',
     'description': 'Technical training chapter - 2',
     'license': 'LGPL-3',
-    'installable': True,
     'application': True
 }
