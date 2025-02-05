275e660b4779c4544b3ba8113da7b83f762f63ed
sabj-odoo <sabj@odoo.com>
2025-02-05 19:15:42 +0530

    [IMP] real_estate: added model, view and security
    Day 2 :
    Chapter 3 :- Add estate_property file  in the models folder in which i added
    various properties like name, description, price, living area.
    
    chapter 4:- Add security to our model.
    
    chapter 5 :- I load our first action and menus through an XML file.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,7 +5,11 @@
     'summary': 'Module to manage real estate advertisements',
     'description': 'A module to create and manage real estate advertisements.',
     'depends': ['base'],
-    'data': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_menus.xml',
+        'views/estate_property_views.xml',
+    ],
     'installable': True,
     'application': True,
     'auto_install': False,
