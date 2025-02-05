18e76ffd0635fd93de40d3db60a9bef90594bdd8
rsbh-odoo <rsbh@odoo.com>
2025-02-05 07:56:08 +0530

    [ADD] estate_module: create estate model and add fields
    - Created a new `estate` model in Odoo to manage estate properties.
    - Added relevant fields to the model for handling property details.
    - Implemented security rules for the model to control access.

---
+++ estate/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "rsbh",
+    'category': 'Real Estate',
+    'license': 'LGPL-3',
+    'description': "A module for managing real estate properties",
+    'depends':['base'],
+    'data': [
+        "security/ir.model.access.xml",
+
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
