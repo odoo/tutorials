6dd07c71c3eedb8209dc71461c333da03fdf0eb1
mdra-odoo <mdra@odoo.com>
2025-02-06 10:33:12 +0530

    [IMP] estate: chapter5 chapter6
    - modified the fields according to requirements(like read only,defaults)
    - created form and list view for displaying data, created filter for identifying
    status as new or offer_receice status, created group for grouping records by
    pincodes.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,16 +1,16 @@
 {
-    'name': 'Real Estate',
-    'version': '1.0',
-    'depends': ['base'],
-    'author': 'odoo',
-    'license': 'LGPL-3',
-    'description':"""
+    "name": "Real Estate",
+    "version": "1.0",
+    "depends": ["base"],
+    "author": "odoo",
+    "license": "LGPL-3",
+    "description": """
 This is Real Estate application which makes easy to sold property
     """,
-    'application': True,
-    'data': [
-        'security/ir.model.access.csv',
-        'views/estate_property_views.xml',
-        'views/estate_menus.xml',
-    ]
+    "application": True,
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_property_menus.xml",
+    ],
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,22 +1,48 @@
-from odoo import models,fields
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+from odoo import models, fields
+
 
 class EstateProperty(models.Model):
-    _name= 'estate.property'
-    _description= 'This is a estate property app'
+    _name = "estate.property"
+    _description = "This is a estate property app"
 
     def set_selection(self):
-        return 'north'
+        
+        return "north"
 
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(
+        default=fields.Date.today() + relativedelta(months=3), copy=False
+    )
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
-    garage = fields.Boolean()
-    garden = fields.Boolean()
+    is_garage = fields.Boolean("Garage")
+    is_garden = fields.Boolean("Garden")
     garden_area = fields.Integer()
-    garden_orientation = fields.Selection(selection=[('north','North'),('south','South'),('east','East'),('west','West')], default=set_selection)
+    garden_orientation = fields.Selection(
+        selection=[
+            ("north", "North"),
+            ("south", "South"),
+            ("east", "East"),
+            ("west", "West"),
+        ],
+        default=set_selection,
+    )
+    active = fields.Boolean(default=True)
+    status = fields.Selection(
+        selection=[
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled"),
+        ],
+        default="new",
+    )
