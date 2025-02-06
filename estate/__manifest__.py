747d455eaf90bd76617cc62cbb5957020d40877b
utma-odoo <utma@odoo.com>
2025-02-06 11:00:10 +0530

    [ADD] estate: add security layer for access right
    After this commit:
    -  Added ir.model.access.csv to give access rights to base.group_user

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,6 +3,10 @@
     'depends': ['base'],
     'author': 'Odoo - Utsav',
     'license': 'LGPL-3',
+    'depends':['base'],
+    'data': [
+        'security/ir.model.access.csv'
+    ],
     'application': False,
     'auto_install': False
 }
