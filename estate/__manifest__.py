f7535c28df659f8c4e71a01471bf838f03b5a339
padi-odoo <padi@odoo.com>
2025-02-06 19:09:50 +0530

    [IMP] estate: Performed changes suggested on previous PR draft, created custome form view & search view, seperated files for menuitems and views, actions.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,11 +1,13 @@
 {
     'name' : 'Real Estate',
+    'version' : '1.0',
     'sequence': 1,
+    'data' : [
+        'security/ir.model.access.csv',
+        
+        'views/estate_property_menu_views.xml',
+        'views/estate_property_views.xml',
+    ],
     'application' : True,
     'license' : 'LGPL-3',
-    'data' : [
-                'security/ir.model.access.csv',
-                'views/estate_property_views.xml',
-                'views/custom_listview.xml'
-            ]
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,32 +1,40 @@
-from datetime import datetime, timedelta, date
-from dateutil.relativedelta import relativedelta
+from datetime import timedelta
 
 from odoo import models, fields
 
+
 class EstateProperty(models.Model):
-
-    
-
     _name = "estate.property"
     _description = "Estate Property Model"
 
-    name = fields.Char(string="Your name", required=True, default="Unknown")
-    active = fields.Boolean(default=False)
-    state = fields.Selection(selection = [('New', 'New'), ('Offer Received', 'Offer Received'), ('Offer Accepted', 'Offer Accepted'), ('Sold', 'Sold'), ('Cancelled', 'Cancelled')], default="New")
-    last_seen = fields.Datetime("Last Seen", default=fields.Datetime.now)
-    description = fields.Text()
-    postcode = fields.Char()
-    date_availability = fields.Date(copy=False, default=fields.Date.today() + timedelta(days=+90))
-    expected_price = fields.Float(required=True)
-    selling_price = fields.Float(readonly=True, copy=False) 
-    bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
-    facades = fields.Integer()
-    garage = fields.Boolean()
-    garden = fields.Boolean()
-    garden_area = fields.Integer()
+    name = fields.Char(string="Name", required=True, default="Unknown")
+    active = fields.Boolean(string="Active", default=True)
+    state = fields.Selection(
+        [('new', 'New'), 
+        ('offer received', 'Offer Received'), 
+        ('offer accepted', 'Offer Accepted'), 
+        ('sold', 'Sold'), 
+        ('cancelled', 'Cancelled')], 
+        default="new",
+        string="State"
+    )
+    last_seen = fields.Datetime(string="Last Seen", default=fields.Datetime.now)
+    description = fields.Text(string="Description")
+    postcode = fields.Char(string="Postcode")
+    date_availability = fields.Date(string="Available from", copy=False, default=fields.Date.today() + timedelta(days=+90))
+    expected_price = fields.Float(string="Expected Price", required=True)
+    selling_price = fields.Float(string="Selling Price", readonly=True, copy=False) 
+    bedrooms = fields.Integer(string="Bedrooms", default=2)
+    living_area = fields.Integer(string="Living Area")
+    facades = fields.Integer(string="Facades")
+    garage = fields.Boolean(string="Garage")
+    garden = fields.Boolean(string="Garden")
+    garden_area = fields.Integer(string="Garden Area")
     garden_orientation = fields.Selection(
-        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
+        [('north', 'North'), 
+        ('south', 'South'), 
+        ('east', 'East'), 
+        ('west', 'West')],
         string="Garden Orientation",
         help="Orientation of the garden"
     )   
