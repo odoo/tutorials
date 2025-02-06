4b4b9fe9270678db03ce154c4e3cb95e05ad423d
mevi-odoo <mevi@odoo.com>
2025-02-06 18:39:57 +0530

    [ADD] estate:  Added UI
    After this commit:
    - Made xml files in views folder, added it to manifest.
    -Added menus
    -Added new attributes, set their default vales , make the copy false.
    -Added states as given.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,5 +4,7 @@
     'application': 'true',
      'data': [
          'security/ir.model.access.csv',
+         'views/estate_property_views.xml',
+         'views/estate_menus.xml',
      ]
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -7,10 +8,10 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False,default=lambda self: fields.Date.today() + relativedelta(months=3),)
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True,copy=False,)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -24,4 +25,18 @@ class EstateProperty(models.Model):
             ('west', 'West')
         ],
         string="Garden Orientation"
-    )
+        )
+    active = fields.Boolean(string="Active", default=False)
+    state = fields.Selection(
+        selection=[
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('cancelled', 'Cancelled')
+        ],
+        string="Status",
+        required=True,
+        copy=False,
+        default='new'
+    )    
