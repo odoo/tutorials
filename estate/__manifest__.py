bf52d425e7bc28cfdf7905104d6af9187fc5c126
path-odoo <path@odoo.com>
2025-02-06 10:52:45 +0530

    [ADD] estate: implemented security
    After this commit:
    - Added security rules and access rights for estate module.
    - Updated necessary security files to ensure proper user permissions.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,6 +3,8 @@
     'version': '1.0',
     'depends': ['base'],
     'license': 'LGPL-3',
-    'application': True
+    'application': True,
+    'data':[
+        'security/ir.model.access.csv'
+    ]
 }
-
