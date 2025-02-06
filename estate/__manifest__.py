766802a6e2f465b8c34ac3049896d6853238195f
makb-odoo <makb@odoo.com>
2025-02-06 18:21:27 +0530

    [ADD] estate: create an estate app
    Created an estate application for Odoo
    Added models with basic fields
    Implemented security rules for models
    Configured menus and basic views (Form and List)
    Introduced application structure and architecture overview

---
+++ estate/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Estate',
+    "description": "This is a real estate listing module.",
+    'version': '1.0',
+    'depends': ['base'],
+    'license': 'LGPL-3',
+    'author' : 'Manthan Akbari',
+    'auto_install': True,
+     'data' : [
+         'security/ir.model.access.csv',
+         'views/estate_property_views.xml',
+         'views/estate_menus.xml',
+         ], 
+     'application': True
+}
---
+++ estate/models/estate_property.py
@@ -0,0 +1,36 @@
+from odoo import models, fields
+from datetime import timedelta
+
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Real Estate Property"
+
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(copy=False,default=fields.Datetime.today() + timedelta(days=90))
+    expected_price = fields.Float()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        [("north", "North"), ("south", "South"), ("east", "East"), ("west", "West")],
+        string="Garden Orientation",
+    )
+    active = fields.Boolean(default=False)
+    state = fields.Selection(
+        selection=[
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled"),
+        ],
+        string="State", required=True, copy=False, default="new",
+    )
+
