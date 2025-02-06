674e3bd5e5c4bbf703920c0f8bac4ed6143efde0
shmn-odoo <shmn@odoo.com>
2025-02-06 19:30:27 +0530

    [ADD] estate: add view directory and refactor model
    - Added a new 'view' directory containing two XML files:
       - Contains menu items.
       - Defines a list view for properties of the model.
    - Updated model:
        - Added default and copy attributes to some fields.
        - Refactored the selection field from a single-line to a multi-line format.
        - Added file headers to all .py files.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,8 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 {
     'name': 'Real Estate',
     'depends': ['base'],
+    'version': '1.0',
+    'summary': 'Real estate internal machinery',
+    'description': """
+This module contains all the common features of Estate property management.
+    """,
     'data': [
         'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ],
+    'installable': True,
     'license': 'LGPL-3',
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,3 +1,5 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 from odoo import fields, models
 
 class EstateProperty(models.Model):
@@ -7,14 +9,31 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date("Available From", default=fields.Date.add(fields.Date.today(), months = 3), copy=False)
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer(index=True)
+    selling_price = fields.Float(copy="False", readonly=True)
+    bedrooms = fields.Integer(index=True, default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
-        selection = [('north','North'),('south','South'),('east','East'),('west','West')])
+        selection=[
+            ('north','North'),
+            ('south','South'),
+            ('east','East'),
+            ('west','West'),
+        ])
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        selection=[
+            ('new','New'),
+            ('offer received','Offer Received'),
+            ('offer accepted','Offer Accepted'),
+            ('sold','Sold'),
+            ('cancelled','Cancelled'),
+        ],
+        copy=False,
+        default='new'
+    )
