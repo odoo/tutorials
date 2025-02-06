077707fe435257391c57ab1c1f8d334963f6520e
unka-odoo <unka@odoo.com>
2025-02-06 18:59:49 +0530

    [IMP] estate:  Added computed fields, added method definition for actions
    Added Compute method with dependencies, Inverse function compute method, and
    added onchanges methods(Chapter-8). Added Object Types in estate_property model
    and estate_property_offer model for changing field values in the form view, and
    Action Types for calling different action by clicking the button in the
    different model view(Chapter-9).

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api, exceptions
 from dateutil.relativedelta import relativedelta
 
 
@@ -11,18 +11,20 @@ class EstateProperty(models.Model):
     property_type = fields.Many2one('estate.property.type', string='Property Type')
     salesman = fields.Many2one('res.users', string='Salesman', default=lambda self: self.env.user)
     buyer = fields.Many2one('res.partner', string='Buyer', copy=False)
-    offer_id = fields.One2many('estate.property.offer', 'property_id')  # we have give the Many2one field defined in the comodel in the One2many field, because One2many relationship is virtual, it only exist when corrensponding Many2one field exist in the comodel.
+    offer_ids = fields.One2many(comodel_name='estate.property.offer', inverse_name='property_id')  # we have give the Many2one field defined in the comodel in the One2many field, because One2many relationship is virtual, it only exist when corrensponding Many2one field exist in the comodel.
     description = fields.Text('Description')
     postcode = fields.Char('Postcode', required=True)
     date_availability = fields.Date('Available From', copy=False, default=fields.Datetime.today() + relativedelta(months=3))
     expected_price = fields.Float('Expected Price', required=True)
-    selling_price = fields.Float('Selleing Price', readonly=True, copy=False)
+    selling_price = fields.Float('Selling Price', readonly=True, copy=False)
+    best_offer = fields.Float('Best Offer', compute="_compute_best_offer")
     bedrooms = fields.Integer('Bedrooms', default="2")
     living_area = fields.Integer('Living Area (sqm)')
     facades = fields.Integer('Facades')
     garage = fields.Boolean('Garage')
     garden = fields.Boolean('Garden')
     garden_area = fields.Integer('Garden Area (sqm)')
+    total_area = fields.Integer('Total Area (sqm)', compute="_compute_total_amount")
     garden_orientation = fields.Selection(
         string='Garden Orientation',
         selection=[('north', 'North'),
@@ -42,3 +44,41 @@ class EstateProperty(models.Model):
         required=True,
         copy=False,
         default="new")
+
+    @api.depends("living_area", "garden_area")
+    def _compute_total_amount(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+    
+    @api.depends("offer_ids.price")
+    def _compute_best_offer(self):
+        for record in self:
+            record.best_offer = max(record.mapped('offer_ids.price'), default=0)
+    
+    @api.onchange('garden')
+    def _onchange_garden(self):
+        if self.garden == True:
+            self.garden_area = 10
+            self.garden_orientation = 'north'
+            # return {'warning': {
+            #     'title': ("Warning"),
+            #     'message': ('This option is not supported for property types other than House, willa, tenaments')}}
+        elif self.garden == False:
+            self.garden_area = None
+            self.garden_orientation = None
+    
+    def action_sold_property(self):
+        for record in self:
+            if record.state == "cancelled":
+                raise exceptions.UserError("Cancelled property can not be sold")
+            else:
+                record.state = "sold"
+        return True
+    
+    def action_cancel_property(self):
+        for record in self:
+            if record.state == "sold":
+                raise exceptions.UserError("Sold property can not be cancel")
+            else:
+                record.state = "cancelled"
+        return True
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import fields, models, api
+from dateutil.relativedelta import relativedelta
 
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
@@ -11,5 +12,31 @@ class EstatePropertyOffer(models.Model):
         ('refused', 'Refused')],
         help="Status of the offer",
         copy=False)
+    validity = fields.Integer('Validity (days)', default='7')
+    date_deadline = fields.Date('Deadline', compute='_compute_date_deadline', inverse='_inverse_date_deadline')
     partner_id = fields.Many2one('res.partner', required=True)
     property_id = fields.Many2one('estate.property', required=True)
+    # base_date = fields.Date() # This is temporory date field for type conversion.
+
+    @api.depends("validity")
+    def _compute_date_deadline(self):
+        for record in self:
+            base_date = record.create_date or fields.Date.today()
+            record.date_deadline = base_date.date() + relativedelta(days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            base_date = record.create_date or fields.Date.today()
+            record.validity = (record.date_deadline - base_date.date()).days
+    
+    def action_accept_offer(self):
+        for record in self:
+            record.status = 'accepted'
+            for offer in record.property_id.offer_ids:
+                if offer.id != record.id:
+                    offer.status = 'refused'
+            record.property_id.selling_price, record.property_id.buyer = record.price, record.partner_id
+                
+    def action_refuse_offer(self):
+        for record in self:
+            record.status = 'refused'
