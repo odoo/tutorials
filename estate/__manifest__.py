2278bd560d656d6cc4632f15bf838552e30d2fe8
vpan-odoo <vpan@odoo>
2025-02-06 17:40:17 +0530

    [IMP] estate: added form view and list view in property tag and type

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -12,7 +12,7 @@
         "views/estate_property_views.xml",
         "views/estate_property_tag_views.xml",
         "views/estate_property_type_views.xml",
-        "views/estate_menus.xml"
+        "views/estate_menus.xml",
     ],
     "application": True,
     "installable": True,
