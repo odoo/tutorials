8fdf6b985d2997f29b1d45e8c8e23d8fe37e2954
bamu <bamu@odoo.com>
2025-02-06 11:38:39 +0530

    [ADD] estate: added list and form views for estate_property  (Chapter - 5)

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,7 +1,11 @@
 {
     "name": "estate",
     "depends": ["base"],
-    "data": ["security/ir.model.access.csv"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
+    ],
     "installable": True,
     "application": True,
     "license": "LGPL-3",
