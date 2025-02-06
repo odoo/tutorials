8c1914a60203049f491b751b881bcb3a32b8e113
pssh-odoo <pssh@odoo.com>
2025-02-06 15:24:09 +0530

    [ADD] estate: create an estate app
    - Created an estate application for odoo
    - Added some basic models and fields
    - Added security for the models
    - Added menu, basic views (Form and List)

---
+++ estate/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name":"estate",
+    "summary": "Create Real Estate leads and close opportunities",
+    "depends": [
+        "base"
+        ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
+             ],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3"
+}
---
+++ estate/models/estate_property.py
@@ -0,0 +1,31 @@
+from odoo import models,fields
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Real Estate Property"
+    name = fields.Char(required = True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(copy = False,
+                                    default= fields.Date().add(fields.Date().today(),days = 90) )
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly = True,
+                                 copy = False)
+    bedrooms = fields.Integer(default = 2)
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+                        selection = [('north','North'),('south','South'),('east','East'),('west','West')]
+                        )
+    active = fields.Boolean("Active",default = True)
+    status = fields.Selection(
+                        selection = [('new','New'),
+                                     ("offer_received","Offer Received"),
+                                     ("offer_accepted","Offer Accepted"),
+                                     ("sold","Sold"),("canceled","Canceled")],
+                        default = 'new',
+                        copy = False
+                        )
