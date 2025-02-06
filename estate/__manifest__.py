bd3c4f9475ce6810a3802d734e234066014400b5
path-odoo <path@odoo.com>
2025-02-06 18:35:55 +0530

    [ADD] estate: implemented UI enhancements
    After this commit:
    - Added a basic UI for property management.
    - Implemented a three-level menu structure for better navigation.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,6 +5,9 @@
     'license': 'LGPL-3',
     'application': True,
     'data':[
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+
+        'views/estate_property_views.xml',
+        'views/estate_property_menus.xml'
     ]
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,16 +1,26 @@
-from odoo import models,fields,api
+from odoo import models,fields,api 
+from dateutil.relativedelta import relativedelta
 
-class estate_property(models.Model):
+class EstateProperty(models.Model):
     _name = 'estate.property'
     _description = "This property is for sell"
 
     def _default_name(self):
         return self.get_value()
     
-    name = fields.Char(string='Name',_default = lambda self:self._default_name(),required=True, tracking=True)
-    description = fields.Text(string='Description', tracking=True)
-    postcode = fields.Char(String='postcode')
-    garden = fields.Boolean(string='Garden')
+    name = fields.Char("Property Name", required=True)
+    active = fields.Boolean(default=True)
+    description = fields.Text("Description")
+    postcode = fields.Char("Postal Code")
+    date_availability = fields.Date("Availability Date", copy=False, default= lambda self: fields.Datetime.now() + relativedelta(months=3))
+    expected_price = fields.Float("Expected Price", required=True)
+    selling_price = fields.Float("Selling Price", readonly=True, copy=False)
+    bedrooms = fields.Integer("Number of bedrooms", default=2)
+    living_area = fields.Integer("Living Area")
+    facades = fields.Integer("Facades")
+    garage = fields.Boolean("Garage")
+    garden = fields.Boolean("Garden")
+    garden_area = fields.Integer("Garden Area")
     garden_orientation = fields.Selection(string='Garden Orientation',
         selection=[
             ('north', 'North'),
@@ -18,4 +28,13 @@ class estate_property(models.Model):
             ('east', 'East'),
             ('west', 'West')
         ])
-    
+    state = fields.Selection(
+        selection = [
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('cancelled', 'Cancelled')
+        ],
+        required=True, copy=False, default="new"
+    )
