d27d8cf21184a464a9e7b4a8ce9689b3e268f62b
ksja-odoo <ksja@odoo.com>
2025-02-06 11:19:50 +0530

    [ADD] estate: added the access properties
    Add Security folder and ir.model.access.csv

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,6 +6,9 @@
         """,
     'sequence': 1,
     'depends' : ['base'],
+    'data' : [
+        'security/ir.model.access.csv'
+        ],
     'installable': True,
     'application': True,
     'license': 'LGPL-3',
