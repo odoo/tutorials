10eb232e88428b1f1cb08e71449e8fa7635be1d3
nmis-odoo <nmis@odoo.com>
2025-02-06 15:26:24 +0530

    [ADD] estate: create an estate app
    - Created an estate application for odoo
    - Added some basic models and fields
    - Added security for the models
    - Added menu, basic views (Form and List)

---
+++ estate/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'estate',
+    'application': True,
+    "depends" : ['base'],
+    "data": ["security/ir.model.access.csv","views/estate_property_views.xml","views/estate_menus.xml"],
+    "license": "LGPL-3",
+    "installable":True
+}
