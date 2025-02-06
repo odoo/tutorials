86a544390a85b4c6932d34b709fa9d85e1a2269c
ddmo <ddmo@odoo.com>
2025-02-06 11:27:58 +0530

    [ADD] estate: attributes and views ch-5

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,9 +1,13 @@
 {
-    'name': 'Real Estate',
-    'depends':['base'],
-    'application': True,
-    'license': 'LGPL-3',
-    'sequence': 1,
-    'description': "This is a real estate app"
+    "name": "Real Estate",
+    "depends": ["base"],
+    "application": True,
+    "data": [
+        "security/ir.model.access.csv",
+        "view/estate_property_views.xml",
+        "view/estate_menus.xml",
+    ],
+    "license": "LGPL-3",
+    "sequence": 1,
+    "description": "This is a real estate app",
 }
-
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,21 +1,41 @@
+from datetime import datetime
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta
 
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Estate Property"
+
+    active = fields.Boolean(string="Active", default=True)
+
     name = fields.Char(required=True)
     description = fields.Text("Description")
     postcode = fields.Text(string="Postcode")
-    date_availability = fields.Date(string="Date Availability")
+    date_availability = fields.Date(
+        string="Date Availability", default=datetime.today() + relativedelta(months=3)
+    )
     expected_price = fields.Float(string="Expected Price", required=True)
     selling_price = fields.Float(string="Selling Price")
-    bedrooms = fields.Integer(string="Number of Bedrooms")
+    bedrooms = fields.Integer(string="Number of Bedrooms", default=2)
     living_area = fields.Integer(string="Living Area (m²)")
     facades = fields.Integer(string="Number of Facades")
     garage = fields.Boolean(string="Has Garage")
     garden = fields.Boolean(string="Has Garden")
     garden_area = fields.Integer(string="Garden Area (m²)")
+    status = fields.Selection(
+        [
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled"),
+        ],
+        string="State",
+        default="new",
+        required=True,
+        copy=False,
+    )
 
     garden_orientation = fields.Selection(
         [
