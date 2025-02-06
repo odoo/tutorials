5e67a933ee6dac3ebe81d3d836291b7f3cf9a738
utma-odoo <utma@odoo.com>
2025-02-06 18:48:12 +0530

    [ADD] estate: add UI components
    After this commit:
    - Added nested menu items to create a list
    - Added new fields and attributes in the estate_property model for the form views

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,8 +5,10 @@
     'license': 'LGPL-3',
     'depends':['base'],
     'data': [
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ],
     'application': False,
-    'auto_install': False
+    'auto_install': False,
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,25 +1,39 @@
 from odoo import models,fields
+from dateutil.relativedelta import relativedelta
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     
-    name = fields.Char("Property Name" , required=True)
+    name = fields.Char("Property Name", required=True)
     description = fields.Text("Description")
     postcode = fields.Char("Postcode")
-    date_availability = fields.Date("Date")
+    date_availability = fields.Date("Date", copy=False, default=fields.Date.today() + relativedelta(months=3))
     expected_price = fields.Float("Expected Price")
-    selling_price = fields.Float("Selling Price")
-    bedrooms = fields.Integer("Bedrooms")
+    selling_price = fields.Float("Selling Price", readonly=True, copy=False)
+    bedrooms = fields.Integer("Bedrooms", default=2)
     living_area = fields.Integer("Living Area")
     facades = fields.Integer("Facades")
     garage = fields.Boolean("Garage")
     garden = fields.Boolean("Garden")
     garden_area = fields.Integer("Garden Area")
+    active = fields.Boolean(active=True)
     garden_orientation = fields.Selection( 
         string="Garden Orientation",
         selection=[
             ('north', 'North'), 
             ('south', 'South'),
             ('east', 'East'),
-            ('west', 'West')
-        ])
+            ('west', 'West'),
+        ]
+    )
+    state = fields.Selection(
+        selection=[
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('cancelled', 'Cancelled'),
+        ],
+        copy=False,
+        default='new',
+    )
