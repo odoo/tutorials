cd4420ca24071f4a71929741c12bb88de626e702
smjo-odoo <smjo@odoo.com>
2025-02-06 18:51:57 +0530

    [ADD] estate: added 3 views in the estate_property_views
    - added the form view, list view, search view
    - search view has filter on "availability"
    - search view has group by options on "postcodes"

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,17 +1,18 @@
 {
     'name': "estate app",
     'summary': "estate module",
-    'description': "estate module for buying and selling properties. Users can buy/sell properties via estate agents",
+    'description': "Estate module for buying and selling properties. Users can buy/sell properties via estate agents",
     'author': "Odoo",
     'category': 'Tutorials/estate',
-    'version': '0.1',
+    'version': '1.0',
     'depends': ['base'],
     'data':[
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_menus.xml',
-        ],
+    ],
     'application': True,
     'installable': True,
     'license': 'AGPL-3'
 }
+
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import fields, models
+from datetime import timedelta,datetime
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -7,16 +8,21 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text() 
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False,default=datetime.now()+timedelta(90))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(copy=False,readonly=True)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
-    garage = fields.Boolean()
-    garden = fields.Boolean()
+    is_garage = fields.Boolean()
+    is_garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
-        selection = [('north', 'North'), ('south', 'South'),('east', 'East'),('west', 'West'),]
+        selection = [('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West'),]
+    )
+    active=fields.Boolean(default=True)
+    state=fields.Selection(
+        selection=[('new','New'),('offer_received','Offer Received'),('offer_accepted','Offer Accepted'),('sold','Sold'),('cancelled','Cancelled')]
+        ,default='new'
     )
     
