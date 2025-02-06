c1b0713a047361a1f8d3be43b7e31c049939bc47
dtda-odoo <dtda@odoo.com>
2025-02-06 18:19:16 +0530

    [ADD] estate: added new folder views

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,7 +4,9 @@
     'depends' : ["base"],
 
     'data' : [
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ],
 
     'installable' : True,
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,16 +1,16 @@
 from odoo import models, fields
 
 class estate_property(models.Model):
-    _name : "estate.property"
-    _description : "estate property"
+    _name = "estate.property"
+    _description = "estate property"
 
-    name= fields.Char("Plan Name", default="MyName",required = True ) # required make property not nullable 
+    name= fields.Char("Plan Name", required = True ) # required make property not nullable 
     description =  fields.Text("Plan desc")
     postcode = fields.Char('postcode')
-    date_availability = fields.Date() 
+    date_availability = fields.Date(copy=False,default=fields.Date.add(fields.Date.today() , months=3))  
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True,copy=False)
+    bedrooms = fields.Integer(default='2')
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -21,5 +21,21 @@ class estate_property(models.Model):
         selection=[('north', 'North'),('east','East'),('west','West'),('south','South')],
         help='this is used to select orientations of garden'
     )
-    
 
+
+    active = fields.Boolean('Active', 
+        default=True
+        )
+
+    state = fields.Selection(
+        required=True,
+        copy=False,
+        default="new",
+        selection=[
+            ('new', 'New'),
+            ('offer received', 'Offer Received'),
+            ('offer accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('cancelled', 'Cancelled')
+        ]
+    )
