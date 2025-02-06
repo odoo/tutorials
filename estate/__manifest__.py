3044ed2f0d321a01949afd6f76e85c0a95a77a89
Dhrumit Parmar <dgpa@odoo.com>
2025-02-06 10:06:52 +0530

    [ADD] esate:  view created by XML and menus

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,7 +4,12 @@
     "depends": ["base"],
     "sequence": 1,
     "license": "LGPL-3",
+    "installable" : True,
     "data": [
         "security/ir.model.access.csv",
+
+        "views/estate_property_views.xml",
+
+        "views/estate_menus.xml",
     ],
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,17 +1,22 @@
 from odoo import fields, models
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real Estate Property"
 
+    def _default_date_availability(self):
+        return datetime.today() + relativedelta(months=3)
+
+    active = fields.Boolean(default=True)
     name = fields.Char(string="Property Name", required=True)
     description = fields.Text(string="Description")
     postcode = fields.Char(string="Postal Code")
-    date_availability = fields.Date(string="Available From")
     expected_price = fields.Float(string="Expected Price", required=True)
-    selling_price = fields.Float(string="Selling Price")
-    bedrooms = fields.Integer(string="Number of Bedrooms")
+    selling_price = fields.Float(string="Selling Price", readonly=True, copy=False)
+    bedrooms = fields.Integer(string="Number of Bedrooms", default=2)
     living_area = fields.Integer(string="Living Area")
     facades = fields.Integer(string="Number of Facades")
     garage = fields.Boolean(string="Garage Available")
@@ -25,4 +30,17 @@ class EstateProperty(models.Model):
             ("east", "East"),
             ("west", "West"),
         ],
+    )
+    state = fields.Selection(
+        string="Status of Document",
+        selection=[
+            ("new", "New"),
+            ("offer Received", "Offer Received"),
+            ("offer Accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled")
+        ],
+    )
+    date_availability = fields.Date(
+        string="Available From", copy=False, default=_default_date_availability
     )
