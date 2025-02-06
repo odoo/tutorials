e9209208640a887c2fd51255d1fd98898a624245
jmra-odoo <jmra@odoo.com>
2025-02-06 11:02:20 +0530

    [IMP] estate: Ch-8, computed fields and onchange
    Chapter-8 was about how to change the value of one field depending on other(s)
    compute attribute defines which method to execute
    inverse attribute makes fields editable and adds 2 side dependency
    onchange can also used to do what compute can but it is not advisable

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from ast import If
+from odoo import api, models, fields
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
@@ -51,3 +52,29 @@ class EstateProperty(models.Model):
 
     # One2many Field
     property_offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
+
+    # Computed Fields
+    total_area = fields.Integer(string="Toatal Area (sqm)", compute="_compute_total_area")
+    best_offer = fields.Float(string="Best Offer", compute="_compute_best_offer")
+
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
+    @api.depends('property_offer_ids')
+    def _compute_best_offer(self):
+        for record in self:
+            if record.property_offer_ids:
+                record.best_offer = max(record.property_offer_ids.mapped('price'), default=0)
+            else:
+                record.best_offer = 0
+
+    @api.onchange('garden')
+    def _onchange_garden(self):
+        if self.garden:    
+            self.garden_area = 10
+            self.garden_orientation = 'north'
+        else:
+            self.garden_area = 0
+            self.garden_orientation = None
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,6 @@
-from odoo import models, fields
+from datetime import timedelta
+from dateutil.relativedelta import relativedelta
+from odoo import api, models, fields
 
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
@@ -9,3 +11,18 @@ class EstatePropertyOffer(models.Model):
     status = fields.Selection(string="Status", copy=False, selection=[('accepted', 'Accepted'), ('refused', 'Refused')])
     partner_id = fields.Many2one("res.partner", required=True)
     property_id = fields.Many2one("estate.property", required=True)
+    validity = fields.Integer(string="Validity (days)", default=7)
+    date_deadline = fields.Date(string="Deadline", compute="_compute_deadline", inverse="_inverse_deadline")
+
+    @api.depends('validity')
+    def _compute_deadline(self):
+        for record in self:
+            record.date_deadline = record.create_date + relativedelta(days=record.validity)
+
+    def _inverse_deadline(self):
+        for record in self:
+            if record.date_deadline:
+                record.validity = (record.date_deadline - fields.Date.today()).days
+            else:
+                record.validity = 7
+    
