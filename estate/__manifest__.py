5b6677a690da3fca452a6c3814f18cef2c10a9d2
duba-odoo <duba@odoo.com>
2025-02-06 12:21:36 +0530

    [ADD] estate: added my estate model
                                                         Day 1:
                                                         Chapter 1:Architecture Overview,Odoo Modules and their structure
                                                         Chapter 2:Created a new application/model .
    
                                                         Day 2:
                                                         Chapter 3:Added more fields to existing model
                                                         Chapter 4:Updated Security by giving access
                                                         Chapter 5:Updated UI

---
+++ estate/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Real Estate Advertisement',
+    'version': '1.0',
+    'category': 'Sales',
+    'summary': 'Manage real estate listings and offers',
+    'author': 'Anubhav Dubey',
+    'website': 'http://localhost:8069',
+    'depends': ['base'],  
+    'data':[
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+        
+    ],
+    'application': True,  
+    'installable': True,  
+}
