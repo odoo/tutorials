7308a3decbb2ae4f36501f3acda83014da3b25d5
rusp-odoo <rusp@odoo.com>
2025-02-06 18:37:07 +0530

    [ADD] estate: add UI components for estate_property
    In this commit:
    - Created  to define actions
    - Added estate_property_menus.xml file to implement three-level menu structure
    - Added new fields and attributes in the estate_property model.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,6 +7,9 @@
     'license': 'LGPL-3',
     'data': [
         'security/ir.model.access.csv',
+
+        'views/estate_property_views.xml',
+        'views/estate_property_menus.xml',
     ],
     'application': True
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,25 +1,38 @@
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta
 
 class EstateProperty(models.Model):
-    _name = 'estate.property'
-    _description = 'Real Estate Property'
+    _name = "estate.property"
+    _description = "Real Estate Property"
 
-    name = fields.Char('Property Name', required=True)
-    description = fields.Text('Description')
-    postcode = fields.Char('Postal Code')
-    date_availability = fields.Date('Availability Date')
-    expected_price = fields.Float('Expected Price', required=True)
-    selling_price = fields.Float('Selling Price')
-    bedrooms = fields.Integer('Number of bedrooms')
-    living_area = fields.Integer('Living Area')
-    facades = fields.Integer('Facades')
-    garage = fields.Boolean('Garage')
-    garden = fields.Boolean('Garden')
-    garden_area = fields.Integer('Garden Area')
+    name = fields.Char("Property Name", required=True)
+    active = fields.Boolean(default=True)
+    description = fields.Text("Description")
+    postcode = fields.Char("Postal Code")
+    date_availability = fields.Date("Availability Date", copy=False, default= lambda self: fields.Datetime.now() + relativedelta(months=3))
+    expected_price = fields.Float("Expected Price", required=True)
+    selling_price = fields.Float("Selling Price", readonly=True, copy=False)
+    bedrooms = fields.Integer("Number of bedrooms", default=2)
+    living_area = fields.Integer("Living Area")
+    facades = fields.Integer("Facades")
+    garage = fields.Boolean("Garage")
+    garden = fields.Boolean("Garden")
+    garden_area = fields.Integer("Garden Area")
     garden_orientation = fields.Selection(
         selection = [
-        ('north','North'),
-        ('south','South'),
-        ('west','West'),
-        ('east','East')]
+            ('north', 'North'),
+            ('south', 'South'),
+            ('west', 'West'),
+            ('east', 'East')
+        ]
+    )
+    state = fields.Selection(
+        selection = [
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('cancelled', 'Cancelled')
+        ],
+        required=True, copy=False, default="new"
     )
