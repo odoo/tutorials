cd36c36f3ebb8c7ba704c1699c04358da2db9259
ksja-odoo <ksja@odoo.com>
2025-02-06 15:20:50 +0530

    [ADD] estate: implement menu structure and attribute in estate_property table
    Added estate_property_views.xml and estate_menu.xml.
    Added new attribute in estate_property table.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,16 +1,17 @@
 from odoo import fields, models
+from datetime import timedelta
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real Estate Property Name"
 
-    name = fields.Char('Property Name', required=True)
-    description = fields.Text('Description')
-    postcode = fields.Char('Postcode')
-    date_availability = fields.Date('Date Availability')
+    name = fields.Char('Property Name', required=True, size=30)
+    description = fields.Text('Description', size=50)
+    postcode = fields.Char('Postcode', size=6)
+    date_availability = fields.Date('Date Availability',default=fields.Date.today() + timedelta(days=90), copy=False)
     expected_price = fields.Float('Expected Price', required=True)
-    selling_price = fields.Float('Selling Price')
-    bedrooms = fields.Integer('Bedrooms')
+    selling_price = fields.Float('Selling Price', readonly=True, copy=False)
+    bedrooms = fields.Integer('Bedrooms', default="2")
     living_area = fields.Integer('Living Area')
     facades = fields.Integer('Facedes')
     garage = fields.Integer('Garage')
@@ -22,5 +23,21 @@ class EstateProperty(models.Model):
             ('south', 'South'),
             ('east', 'East'),
             ('west', 'West')    
+        ]
+    )
+    active = fields.Boolean('Active', 
+        default=True, 
+        help='if you wan to active'
+        )
+    state = fields.Selection(
+        required=True,
+        copy=False,
+        default="new",
+        selection=[
+            ('new', 'New'),
+            ('offer received', 'Offer Received'),
+            ('offer accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('cancelled', 'Cancelled')
         ]
     )
