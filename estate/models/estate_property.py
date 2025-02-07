2f3ac0cb3d48f0465cc7e34af040049d6ef88e5d
mevi-odoo <mevi@odoo.com>
2025-02-07 17:49:10 +0530

    [ADD] estate: basic views
    After this commit:
    -Made list view and added fields like postcode,bedrooms,garden,facades and etc.
    -Designed the form view for the estate.property model .
    -Added search functionality
    -Added filter and group by domain.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -8,9 +8,9 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(copy=False,default=lambda self: fields.Date.today() + relativedelta(months=3),)
+    date_availability = fields.Date(copy=False, default=lambda self: fields.Date.today() + relativedelta(months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float(readonly=True,copy=False,)
+    selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
@@ -18,15 +18,10 @@ class EstateProperty(models.Model):
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
-        selection=[
-            ('north', 'North'), 
-            ('south', 'South'), 
-            ('east', 'East'), 
-            ('west', 'West')
-        ],
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
         string="Garden Orientation"
-        )
-    active = fields.Boolean(string="Active", default=False)
+    )
+    active = fields.Boolean(string="Active", default=True)
     state = fields.Selection(
         selection=[
             ('new', 'New'),
@@ -40,3 +35,39 @@ class EstateProperty(models.Model):
         copy=False,
         default='new'
     )    
+    property_type_id = fields.Many2one("estate.property.type", string="Property Type")
+    buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
+    seller_id = fields.Many2one("res.users", string="Seller", default=lambda self: self.env.user)
+    tag_ids = fields.Many2many("estate.property.tag", string="Tags")
+    offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
+
+# class EstatePropertyType(models.Model):
+#     _name = "estate.property.type"
+#     _description = "Property Type"
+
+#     name = fields.Char(required=True)
+
+# class EstatePropertyTag(models.Model):
+#     _name = "estate.property.tag"
+#     _description = "Real Estate Property Tags"
+#     _order = "name"
+
+#     name = fields.Char(required=True, string="Tag")
+#     #color = fields.Integer(string="Color")
+
+#     _sql_constraints = [
+#         ('unique_tag_name', 'unique(name)', 'Tag name must be unique.')
+#     ]
+# class EstatePropertyOffer(models.Model):
+#     _name = "estate.property.offer"
+#     _description = "Estate Property Offer"
+
+#     price = fields.Float(string="Offer Price", required=True)
+#     status = fields.Selection([
+#         ('accepted', 'Accepted'),
+#         ('refused', 'Refused')
+#     ], string="Status", copy=False)
+
+#     partner_id = fields.Many2one("res.partner", string="Buyer", required=True)
+#     property_id = fields.Many2one("estate.property", string="Property", required=True)
+
---
+++ estate/models/estate_property_offer.py
@@ -0,0 +1,14 @@
+from odoo import fields, models
+
+class EstatePropertyOffer(models.Model):
+    _name = "estate.property.offer"
+    _description = "Estate Property Offer"
+
+    price = fields.Float(string="Offer Price", required=True)
+    status = fields.Selection([
+        ('accepted', 'Accepted'),
+        ('refused', 'Refused')
+    ], string="Status", copy=False)
+
+    partner_id = fields.Many2one("res.partner", string="Buyer", required=True)
+    property_id = fields.Many2one("estate.property", string="Property", required=True)
