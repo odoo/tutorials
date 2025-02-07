1caa8cfe703aeeb3a5d8f5d5d16a18eb062842f8
krge-odoo <krge@odoo.com>
2025-02-07 14:04:00 +0530

    [IMP] estate: implement the search bar and group by option
    - add search bar for specific search with some group by option.
    - also fix the code.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,47 +1,49 @@
-from odoo import models,fields
+from odoo import fields, models
+
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = 'Listing for the properties'
 
-    name = fields.Char(string = "Name", required = True)
-    description = fields.Text(string = 'Description')
-    postcode = fields.Char(string = 'PostCode', index = True)
-    date_availability = fields.Date(
-        string = 'Available Date',
-        default= lambda self:fields.Date.add(fields.Date.today(), months=3),
-        copy = False
+    name = fields.Char(string="Name", required=True)
+    description = fields.Text(string='Description')
+    postcode = fields.Char(string='PostCode', index=True)
+    date_availability=fields.Date(
+        string='Available Date',
+        default=lambda self:fields.Date.add(fields.Date.today(), months=3),
+        copy=False
     )
-    expected_price = fields.Float(string = 'Expected Price', required = True)
-    selling_price = fields.Float(string = 'Selling Price', readonly = True)
-    bedrooms = fields.Integer(string = 'Bedrooms', default = 2)
-    living_area = fields.Integer(string = 'Living Area')
+    expected_price = fields.Float(string='Expected Price', required=True)
+    selling_price = fields.Float(string='Selling Price', readonly=True)
+    bedrooms = fields.Integer(string='Bedrooms', default=2)
+    living_area = fields.Integer(string='Living Area')
     facades = fields.Integer(
-        string = 'Facades',
+        string='Facades',
         help="a facade refers to the front or exterior appearance of a building, usually facing the street."
     )
-    garage = fields.Boolean(string = 'Garage')
-    garden = fields.Boolean(string = 'Garden')
-    garden_area = fields.Integer(string= 'Garden Area')
+    garage = fields.Boolean(string='Garage')
+    garden = fields.Boolean(string='Garden')
+    garden_area = fields.Integer(string='Garden Area')
     garden_orientation = fields.Selection(
-        string = "Gardern Orientation",
-        selection = [
+        string="Gardern Orientation",
+        selection=[
             ('north','North'),
             ('south', 'South'),
             ('west', 'West'),
             ('east', 'East')
         ])
-    active = fields.Boolean(string = 'Active', default = True)
+    active = fields.Boolean(string='Active', default=True)
     state = fields.Selection(
         string="Status",
-        selection =[
+        selection=[
             ('new','New'),
             ('offer_recevied','Offer Received'),
             ('offer_accepted', 'Offer Accepted'),
             ('sold','Sold'),
             ('cancelled','Cancelled')
         ],
-        required = True,
-        copy = False,
-        default = 'new'
+        required=True,
+        copy=False,
+        default='new'
     )
+    estate_property_type_id = fields.Many2one("estate.property.type", string="Property Type")
--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -1,4 +1,5 @@
-from odoo import models,fields
+from odoo import fields, models
+
 
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
