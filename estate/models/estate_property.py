a0ebf2587ff68c095249b0140c9e25317d65f2fa
mear-odoo <mear@odoo.com>
2025-02-07 17:43:01 +0530

    [IMP] estate: added more views and filters to the estate app
    • Added list, form, search view to the estate app
    • Added filters and group by options to the estate app

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -8,7 +8,7 @@ class EstateProperty(models.Model):
     name = fields.Char(required = True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(copy = False,default = lambda self: fields.Datetime.today() + timedelta(days=90))
+    date_availability = fields.Date(copy = False,string="Available Form",default = lambda self: fields.Datetime.today() + timedelta(days=90))
     expected_price = fields.Float(required = True)
     selling_price = fields.Float(readonly = True,copy = False, default = 7000000)
     bedrooms = fields.Integer(default = 2)
@@ -16,9 +16,9 @@ class EstateProperty(models.Model):
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Integer()
+    garden_area = fields.Integer(string="Living Area(sqm)")
     garden_orientation = fields.Selection(
-        string="Type",
+        string="Garden Orientation",
         selection=[
             ("north", "North"),
             ("south", "South"),
@@ -26,15 +26,15 @@ class EstateProperty(models.Model):
             ("west", "West"),
         ],
     )
-    state = fields.Selection(
-        string="State",
-        default = "New",
+    status = fields.Selection(
+        string="Status",
+        default = "new",
         copy = False,
         selection=[
-            ("New", "New"),
-            ("Offer Received", "Offer Received"),
-            ("Offer Accepted", "Offer Accepted"),
-            ("Sold", "Sold"),
-            ("Cancelled", "Cancelled"),
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled"),
         ])
     active = fields.Boolean(default = True)
