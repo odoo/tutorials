38f2e1368d90d672cbf488849c4d3b3ba3ac4857
pbch-odoo <pbch@odoo.com>
2025-02-07 18:49:44 +0530

    [IMP] estate: adding list view and form view

--- estate/models/estate_Property.py
+++ estate/models/estate_Property.py
@@ -1,14 +1,13 @@
-from odoo import models, fields
+from odoo import fields, models
 
 class estate_Property(models.Model):
     _name = "estate_model"  
     _description = "This is the description for esatet properties model:"
 
-    name = fields.Char(required=True)
+    name = fields.Char(required=True, string="Title")
     description = fields.Text()
-    postcode = fields.Char()
-    # date_availability = fields.Date(copy=False, default=fields.Date.add(fields.Date.today, months=3))
-    date_availability = fields.Date(copy=False,default=fields.Date.today)
+    postcode = fields.Integer()
+    date_availability = fields.Date(copy=False,default=fields.Date.today())
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True)
     bedrooms = fields.Integer(default=2)
@@ -18,11 +17,9 @@ class estate_Property(models.Model):
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
-        selection = [('north', 'North'), ('east', 'East'), ('south', 'South'),('west', 'West')]
-    )
+        selection = [('north', 'North'), ('east', 'East'), ('south', 'South'),('west', 'West')])
     active = fields.Boolean(default=True)
-    state = fields.Selection(copy=False,
-        required=True,
-        default='new',
-        selection = [('new', 'New'), ('offerr_received', 'Offer_Received'), ('offer_accepted', 'Offer_accepted'), ('sold', 'Sold')]
-    )
+    state = fields.Selection(copy=False, required=True, default='new',
+        selection = [('new', 'New'), ('offerr_received', 'Offer_Received'), 
+        ('offer_accepted', 'Offer_accepted'), ('sold', 'Sold')])
+    last_seen = fields.Datetime("Last Seen", default=fields.Datetime.now)
