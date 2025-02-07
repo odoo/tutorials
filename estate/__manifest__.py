bca9d05974f075e21d6779e8c42d38b849bb5350
nmis-odoo <nmis@odoo.com>
2025-02-07 17:48:17 +0530

    [IMP] estate:  Create Basic View In Module
    - Created a list view for displaying records
    - Implemented a form view for detailed record entry
    - Added a search view with filters and group-by options
    - Enhanced user experience with structured UI elements

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,7 +2,7 @@
     'name': 'estate',
     'application': True,
     "depends" : ['base'],
-    "data": ["security/ir.model.access.csv","views/estate_property_views.xml","views/estate_menus.xml"],
+    "data": ["security/ir.model.access.csv","views/estate_property_views.xml","views/estate_menus.xml","views/estate_property_forms.xml"],
     "license": "LGPL-3",
     "installable":True
 }
