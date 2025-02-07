12ff0c5621a087968ff39242d384a646b390b2d7
rmbh-odoo <rmbh@odoo.com>
2025-02-07 14:47:15 +0530

    [ADD] estate: added list, form and search view with filter and group
    After this commit:
    - The user can easily see data in the list and form view.
    - Search data based on different record fields.
    - Filter data based on availability.
    - Group data based on postcode.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -2,21 +2,21 @@ from odoo import fields, models
 from dateutil.relativedelta import relativedelta
 
 class EstateProperty(models.Model):
-    _name="estate.property"
-    _description="Estate Property"
+    _name='estate.property'
+    _description='Estate Property'
 
-    name=fields.Char(required=True)
+    name=fields.Char(string='Title', required=True)
     description=fields.Text()
     postcode=fields.Char()
-    date_availablity=fields.Date(copy=False, default=fields.Date.today()+ relativedelta(months=3))
+    date_availablity=fields.Date(string='Available From', copy=False, default=fields.Date.today()+ relativedelta(months=3))
     expected_price=fields.Float(required=True)
     selling_price=fields.Float(readonly=True, copy=False)
     bedrooms=fields.Integer(default=2)
-    living_area=fields.Integer()
+    living_area=fields.Integer(string='Living Area (sqm)')
     facades=fields.Integer()
     garage=fields.Boolean()
     garden=fields.Boolean()
-    garden_area=fields.Integer()
+    garden_area=fields.Integer(string='Garden Area (sqm)')
     garden_orientation=fields.Selection(
         selection=[
             ('north','North'),
@@ -27,12 +27,12 @@ class EstateProperty(models.Model):
     )
     active=fields.Boolean(default=True)
     state=fields.Selection(
+        required=True,
         default='new',
         copy=False,
         selection=[
             ('new','New'),
-            ('offer','Offer'),
-            ('received','Received'),
+            ('offer_received','Offer Received'),
             ('offer_accepted','Offer Accepted'),
             ('sold','Sold'),
             ('cancelled','Cancelled'),
