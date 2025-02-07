6d87544c91c4e86ee9b6102038d4ce28ac69bcf7
pssh-odoo <pssh@odoo.com>
2025-02-07 10:37:56 +0530

    [IMP] estate: added more views and filters to the estate app
    • Added list, form, search view to the estate app
    • Added filters and group by options to the estate app

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,12 +3,12 @@
     "summary": "Create Real Estate leads and close opportunities",
     "depends": [
         "base"
-        ],
+    ],
     "data": [
         "security/ir.model.access.csv",
         "views/estate_property_views.xml",
         "views/estate_menus.xml",
-             ],
+    ],
     "installable": True,
     "application": True,
     "license": "LGPL-3"
