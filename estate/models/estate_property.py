8420695e190c1fb920b57ecc02dc760ba42a7ac9
shmn-odoo <shmn@odoo.com>
2025-02-07 19:02:24 +0530

    [IMP] estate: form view, search view with filter and group
    - Added form view to display property details
    - Implemented search view at the top of the list view
    - Added a filter for available properties
    - Added group by functionality for postcode in the search view

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -6,14 +6,18 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Estate Properties"
 
-    name = fields.Char(required=True)
+    name = fields.Char("Title",required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date("Available From", default=fields.Date.add(fields.Date.today(), months = 3), copy=False)
+    date_availability = fields.Date(
+        string="Available From",
+        default=lambda self:fields.Date.add(fields.Date.today(), months=3),
+        copy=False
+        )
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(copy="False", readonly=True)
     bedrooms = fields.Integer(index=True, default=2)
-    living_area = fields.Integer()
+    living_area = fields.Integer("Living Area (sqm)")
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
