214fb30644c4f4cd8973393d5199847541f2fb8e
vpan-odoo <vpan@odoo>
2025-02-07 15:30:46 +0530

    [IMP] estate: added manytoone and manytomany realtions with views

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -42,3 +42,6 @@ class EstateProperty(models.Model):
         copy=False
     )
     property_type_id = fields.Many2one("estate.property.type", string="Property Type")
+    buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
+    salesperson_id = fields.Many2one("res.users", string="Salesman", default = lambda self: self.env.user)
+    tag_ids = fields.Many2many("estate.property.tag", string="Property Tag")
