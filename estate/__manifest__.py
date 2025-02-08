35c168bd34cc5da7e48b940442bb70ae6cbd6a7c
dtda-odoo <dtda@odoo.com>
2025-02-09 00:42:15 +0530

    [IMP] estate: fix quotation

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,16 +1,16 @@
 {
-    'name':"estate",
-    'version':'1.0',
-    'depends' : ["base"],
+    "name":"estate",
+    "version":"1.0",
+    "depends" : ["base"],
 
-    'data' : [
-        'security/ir.model.access.csv',
-        'views/estate_property_views.xml',
-        'views/estate_menus.xml',
+    "data" : [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
     ],
 
-    'installable' : True,
-    'application' : True, # this makes the applications available even in app filter is on
-    'auto-install' : False,
+    "installable" : True,
+    "application" : True, # this makes the applications available even in app filter is on
+    "auto-install" : False,
 
 }
