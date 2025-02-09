3b08224ebde858df18ae090ec8cd66c8d5115fc8
ssud-odoo <ssud@odoo.com>
2025-02-09 12:47:13 +0530

    [IMP] estate: improved code readability and syntax

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -17,12 +17,13 @@
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_property_tag_views.xml',
-        'views/estate_property_type_views.xml',      
+        'views/estate_property_type_views.xml',
+        'views/estate_property_offer_views.xml',
         'views/estate_menus.xml'
     ],
 
     'installable':True,
     'application':True,
-    'license': "LGPL-3" 
+    'license': "LGPL-3"
     
 }
