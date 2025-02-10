5e43545620b79984091469ac628b4d456b0e08a2
ksja-odoo <ksja@odoo.com>
2025-02-10 13:15:32 +0530

    [ADD] estate: added computed and onchange fields
    Updated estate_property.py and estate_property_offer.py

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,26 +1,27 @@
-from odoo import fields, models
+from odoo import api, fields, models
 from datetime import timedelta
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real Estate Property Name"
 
-    name = fields.Char('Property Name', required=True, size=30)
-    description = fields.Text('Description', size=50)
-    postcode = fields.Char('Postcode', size=6)
+    name = fields.Char('Property Name', required = True, size = 30)
+    description = fields.Text('Description', size = 50)
+    postcode = fields.Char('Postcode', size = 6)
     date_availability = fields.Date('Date Availability',default=fields.Date.today() + timedelta(days=90), copy=False)
-    expected_price = fields.Float('Expected Price', required=True)
+    expected_price = fields.Float('Expected Price', required = True)
     selling_price = fields.Integer('Selling Price', 
         readonly=True, 
         copy=False,
         default=1000000
     )
-    bedrooms = fields.Integer('Bedrooms', default="2")
+    bedrooms = fields.Integer('Bedrooms', default = "2")
     living_area = fields.Integer('Living Area')
     facades = fields.Integer('Facedes')
     garage = fields.Boolean('Garage')
-    garden = fields.Boolean('Garden')
-    garden_area = fields.Integer('Garden Area')
+    garden = fields.Boolean('Garden', default=False)
+    garden_area = fields.Integer('Garden Area(sqm)', default = 0)
+    total_area = fields.Integer('Total Area (sqm)', compute="_compute_total_area", store=True)
     garden_orientation = fields.Selection(
         string ='Type',
         selection=[
@@ -35,10 +36,10 @@ class EstateProperty(models.Model):
         help='if you want to active'
         )
     state = fields.Selection(
-        required=True,
-        copy=False,
-        default="new",
-        selection=[
+        required = True,
+        copy = False,
+        default = "new",
+        selection = [
             ('new', 'New'),
             ('offer received', 'Offer Received'),
             ('offer accepted', 'Offer Accepted'),
@@ -50,3 +51,23 @@ class EstateProperty(models.Model):
     buyer_id = fields.Many2one('res.partner', string='Buyer', copy=True)
     tags_ids = fields.Many2many('estate.property.tag', string='Tags')
     offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
+    best_price = fields.Float(string="Best Offer", compute="_compute_best_price", store=True)
+
+    @api.depends("living_area", "garden_area")
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+    
+    @api.depends("offer_ids.price")
+    def _compute_best_price(self):
+        for record in self:
+            record.best_price = max(record.offer_ids.mapped("price"), default=0.0)
+
+    @api.onchange("garden")
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = 'north'
+        else:
+            self.garden_area = 0
+            self.garden_orientation = False
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import api, fields, models
+
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
     _description = "All the available offer for the property"
@@ -8,11 +9,23 @@ class EstatePropertyOffer(models.Model):
     buyer_id = fields.Many2one('res.partner', required=True)
     status = fields.Selection(
         string="Status",
-        required=True,
-        default="accepted",
         copy=False,
         selection=[
             ("refuse", "Refuse"),
             ("accepted", "Accepted")
         ],
     )
+    validity = fields.Integer(string="Validity(days)", default=7)
+    date_deadline = fields.Date(compute = '_compute_date_deadline', inverse = '_inverse_date_deadline', store = True)
+
+    @api.depends('create_date', 'validity')
+    def _computer_date_deadline(self):
+        for record in self:
+            if record.create_date and record.validity:
+                record.date_deadline = fields.Date.add(record.create_date, days = record.validity)
+            else:
+                record.date_deadline = fields.Date.add(fields.Date.today(), days = record.validity)
+    def _inverse_date_deadline(self):
+        for record in self:
+            if record.date_deadline and record.create_date:
+                record.validity = (record.date_deadline - record.create_date.date()).days
