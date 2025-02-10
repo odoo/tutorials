51039c0b0855646cfda8ccb5ae41a392713bbacd
maap-odoo <maap@odoo.com>
2025-02-10 14:40:49 +0530

    [IMP] estate: added property types, tags, offers, and relations in estate app
    • Created Property Type, Property Tag, and Property Offer models.
    • Added `Many2one`, `Many2many`, and `One2many` relations between models.
    • Updated property views to include types, tags, and offers with widgets.

---
+++ estate/models/estate_property_offer.py
@@ -0,0 +1,15 @@
+from odoo import models, fields
+
+class EstatePropertyOffer(models.Model):
+    _name = "estate.property.offer"
+    _description = "Real Estate Property Offer"
+
+    price = fields.Float(string="Offer Price", required=True)
+
+    status = fields.Selection([
+        ('accepted', "Accepted"),
+        ('refused', "Refused")
+    ], string="Status", copy=False)
+
+    partner_id = fields.Many2one("res.partner", string="Partner", required=True)
+    property_id = fields.Many2one("estate.property", string="Property", required=True)
