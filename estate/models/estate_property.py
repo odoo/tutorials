49e123c926fbcc32e4eba1387f7fedb434dce329
maap-odoo <maap@odoo.com>
2025-02-10 22:23:25 +0530

    [IMP] estate:  Implement action buttons for property and offer management
    • Added action buttons to trigger business logic in the estate module
    • Implemented 'Sold' and 'Cancel' actions for properties with state validation
    • Added 'Accept' and 'Refuse' actions for property offers
    • Ensured 'Accept' action updates buyer and selling price for property

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models, exceptions
 from datetime import timedelta
 
 
@@ -46,7 +46,43 @@ class EstateProperty(models.Model):
         copy=False,
     )
     property_type_id = fields.Many2one("estate.property.type", string="Property Type")
-    buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
+    buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False, readonly=True)
     salesperson_id = fields.Many2one("res.users", string="Salesperson", default=lambda self: self.env.user)
     tag_ids = fields.Many2many("estate.property.tag", string="Property Tag")
     offer_ids = fields.One2many("estate.property.offer", "property_id", string="Property Offer")
+
+    total_area = fields.Float(compute="_compute_total_area", string="Total Area(sqm)")
+
+    @api.depends("living_area", "garden_area")
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+    best_price = fields.Float(compute="_compute_best_price")
+
+    @api.depends("offer_ids.price")
+    def _compute_best_price(self):
+        for record in self:
+            record.best_price = max(record.offer_ids.mapped("price"), default=0.0)
+
+    @api.onchange("garden")
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = "north"
+        else:
+            self.garden_area = 0
+            self.garden_orientation = False
+
+    def action_property_sold(self):
+        for record in self:
+            if record.status == "cancelled":
+                raise exceptions.UserError("A cancelled property cannot be sold!")
+            record.status = 'sold'
+        return True
+
+    def action_property_cancel(self):
+        for record in self:
+            if record.status == 'sold':
+                raise exceptions.UserError("A sold property cannot be cancelled!")
+            record.status = 'cancelled'
+        return True
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import api, models, fields, exceptions
+from datetime import timedelta
 
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
@@ -13,3 +14,37 @@ class EstatePropertyOffer(models.Model):
 
     partner_id = fields.Many2one("res.partner", string="Partner", required=True)
     property_id = fields.Many2one("estate.property", string="Property", required=True)
+
+    create_date = fields.Date(default=fields.Date.today)
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_date_deadline")
+
+    @api.depends("create_date", "validity")
+    def _compute_date_deadline(self):
+        for record in self:
+            if record.create_date:
+                record.date_deadline = record.create_date + timedelta(days=record.validity)
+            else:
+                record.date_deadline = fields.Date.today() + timedelta(days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            if record.date_deadline and record.create_date:
+                delta = record.date_deadline - record.create_date
+                record.validity = delta.days
+
+    def action_accept(self):
+        for record in self:
+            if record.property_id.status == 'sold' or record.status == 'accepted':
+                raise exceptions.UserError("This property is already sold.")
+
+            record.status = 'accepted'
+            record.property_id.status = 'sold'
+            record.property_id.buyer_id = record.partner_id
+            record.property_id.selling_price = record.price
+        return True
+
+    def action_refuse(self):
+        for record in self:
+            record.status = 'refused'
+        return True
