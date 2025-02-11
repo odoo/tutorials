9b44c514fae59c101bf5adf62651c0d9139938a6
amga-odoo <amga@odoo.com>
2025-02-11 10:23:17 +0530

    [IMP] estate: add Kanban view of the properties

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -15,7 +15,7 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date("Availability Date", copy=False, default=fields.Datetime.today() + relativedelta(days=90))
+    date_availability = fields.Date("Availability Date", copy=False, default=lambda self : fields.Date.add(fields.Date.today() , months=3))
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
