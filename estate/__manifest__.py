f248ff02c260c16bd9e1dc2d8164bc220f26f96d
Smit Rathod (smra) <smra@odoo.com>
2025-02-12 15:31:12 +0530

    [ADD] estate: a new application created - created a new module, estate. - added init, manifest files. - added estate_property model and access rights for the model.

---
+++ estate/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "estate",
+    'version': '18.0',
+    'depends': ['base'],
+    'author': "Smit",
+    'category': 'Real Estate',
+    'description': """
+        Buy and sell properties.
+    """,
+    'installable': True,
+    'application': True,
+    'data': [
+        'security/ir.model.access.csv',
+    ]
+}
---
+++ estate/models/estate.property.py
@@ -0,0 +1,25 @@
+from odoo import fields,models
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "See properties"
+
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        string='Type',
+        selection=[('north','North'),
+                ('east','East'),
+                ('west','West'),
+                ('south','South')]
+    )
