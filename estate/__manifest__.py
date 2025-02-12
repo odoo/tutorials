a0218e30868968db227f7eb1fec05685c266fa78
Rahul Jha (jhra) <jhra@odoo.com>
2025-02-12 16:16:48 +0530

    [ADD] estate: Created initial scaffold of a Real Estate module
    +Added the __init__.py and __manifest__.py files

---
+++ estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Real Estate",
+    'version': '18.0',
+    'depends': ['base'],
+    'author': "Rahul Jha (jhra)",
+    'website': "https://www.odoo.com",
+    'category': 'Tutorials/RealEstate',
+    'description': """
+    Test module for selling real estate properties.
+    """,
+    'license': 'AGPL-3'
+}
