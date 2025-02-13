cf9fd2d3dfdcdfe57e2c6cdc3ce9e52ac9def353
Rahul Jha (jhra) <jhra@odoo.com>
2025-02-13 15:13:14 +0530

    [IMP] estate: Added validity and deadline for property offers
    +We can now decide on the termination date of an offer.
    +Best offer price for a property will be displayed.
    +Total area of a property will auto compute based on the entered data.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class EstateProperty(models.Model):
@@ -17,6 +17,11 @@ class EstateProperty(models.Model):
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
+    total_area = fields.Integer(compute='_compute_total_area', store=True)
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
     garden_orientation = fields.Selection(
         string="Garden Orientation",
         selection=[
@@ -25,6 +30,15 @@ class EstateProperty(models.Model):
             ('east', 'East'),
             ('west', 'West')]
     )
+    @api.onchange('garden')
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = 'north'
+        else:
+            self.garden_area = 0
+            self.garden_orientation = False
+    
     state = fields.Selection(
         string="State",
         selection=[
@@ -43,3 +57,8 @@ class EstateProperty(models.Model):
     buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
     tag_ids = fields.Many2many('estate.property.tag', string="Tags")
     offer_ids = fields.One2many('estate.property.offer', 'property_id', string="Offers")
+    best_offer = fields.Float(compute="_compute_best_offer", store=True)
+    @api.depends("offer_ids.price")
+    def _compute_best_offer(self):
+        for record in self:
+            record.best_offer = max(record.offer_ids.mapped("price"), default=0.0)
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
@@ -14,3 +14,18 @@ class EstatePropertyOffer(models.Model):
     )
     partner_id = fields.Many2one('res.partner', string="Partner", required=True)
     property_id = fields.Many2one('estate.property', string="Property", required=True)
+    date_deadline = fields.Date(copy=False, default=fields.Date.today(), compute='_compute_date_deadline', inverse='_inverse_date_deadline', store=True)
+    validity = fields.Integer(default=7, store=True)
+
+    @api.depends('create_date', 'validity')
+    def _compute_date_deadline(self):
+        for record in self:
+            reference_date = record.create_date or fields.Datetime.now()
+            record.date_deadline = fields.Date.add(fields.Date.from_string(reference_date), days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            reference_date = record.create_date or fields.Datetime.now()
+            reference_date = fields.Date.from_string(reference_date)
+            if record.date_deadline:
+                record.validity = (record.date_deadline - reference_date).days
