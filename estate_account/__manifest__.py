7cf6248b444e03a505287e2144263d982d28955b
mear-odoo <mear@odoo.com>
2025-02-13 11:48:30 +0530

    [ADD] estate_account:  Introduce invoicing for sold properties
    • Created  estate_account module, dependent on estate and account
    • Inherited estate.property and overrode action_sold
    • Added invoice creation
    • Created a `Customer Invoice` with `partner_id` from estate property
    • Added invoice lines: 6% of selling price + 100.00 admin fee

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    "name": "Estate Account",
+    "category": "Real Estate",
+    "depends":["estate", "account"],
+    "installable": True,
+    "license": "LGPL-3"
+}
---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,15 @@
+from odoo import fields, models, Command
+
+class EstateProperty(models.Model):
+    _inherit = "estate.property"
+
+    def property_sold(self):
+        self.sudo().env["account.move"].create({
+            "move_type": "out_invoice",
+            "partner_id": self.buyer_id .id,
+            "line_ids": [
+                Command.create({"name": self.name, "quantity": 1, "price_unit": 0.06 * self.selling_price}),
+                Command.create({"name": "Administrative Fees", "quantity": 1, "price_unit": 100.0})
+            ]
+        })
+        return super().property_sold()
