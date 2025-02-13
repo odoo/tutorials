06960b56bd331734af94d95c07d4b463b6924408
dtda-odoo <dtda@odoo.com>
2025-02-13 10:59:47 +0530

    [FIX] estate_account: add conventional loop and remove extra access checks

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -6,20 +6,24 @@ class EstateProperty(models.Model):
 
     def action_set_property_status_sold(self):
 
-        if not self.env["account.move"].check_access_rights("create", False):
-            try:
-                self.check_access_rights("write")
-                self.check_access_rule("write")
-            except AccessError:
-                return self.env["account.move"]
+        # access is already checked in .create() method, checking it again add redundancy 
+
+        # if not self.env["account.move"].check_access_rights("create", False):
+        #     try:
+        #         self.check_access_rights("write")
+        #         self.check_access_rule("write")
+        #     except AccessError:
+        #         return self.env["account.move"]
         
-        self.env["account.move"].create({
-            "partner_id" : self.buyer_id.id,
-            "move_type" : "out_invoice",
-            "invoice_line_ids" : [
-                Command.create({"name" : "6% of Selling Price", "quantity" : 1, "price_unit" : 0.6 * self.selling_price}),
-                Command.create({"name" : "Administration Fee", "quantity" : 1, "price_unit" : 100})
-            ]
-        })
+        for record in self:
+            self.env["account.move"].create({
+                "partner_id" : record.buyer_id.id,
+                "move_type" : "out_invoice",
+                "invoice_line_ids" : [
+                    Command.create({"name" : "6% of Selling Price", "quantity" : 1, "price_unit" : 0.6 * record.selling_price}),
+                    Command.create({"name" : "Administration Fee", "quantity" : 1, "price_unit" : 100})
+                ]
+            })
+
 
         return super().action_set_property_status_sold()
