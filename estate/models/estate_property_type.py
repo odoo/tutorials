2c28c35dfbd2b749907e668be8a85993a1d604de
pmah-odoo <pmah@odoo.com>
2025-02-13 19:05:53 +0530

    [IMP] estate: restrict data access via groups, ACLs & record rules
    - Added "Agent" & "Manager" groups (Manager implies Agent).
    - Set manifest category to Real Estate/Brokerage.
    - ACLs: Managers full; Agents read-only for types/tags; no deletes.
    - Record rule: Agents see only unassigned or their own properties.

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -7,9 +7,7 @@ class EstatePropertyType(models.Model):
     _order = "name"
 
     name = fields.Char(required=True)
-    property_ids = fields.One2many(
-        comodel_name="estate.property", inverse_name="property_type_id", string="Properties"
-    )
+    property_ids = fields.One2many("estate.property", "property_type_id", string="Properties")
     sequence = fields.Integer("Sequence", default=1, help="Used to order property types")
     offer_ids = fields.One2many("estate.property.offer", "property_type_id", string="Offers")
     offer_count = fields.Integer(string="Offer Count", compute="_compute_offer_count")
--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -5,6 +5,7 @@ class EstateProperty(models.Model):
     _inherit = 'estate.property'
 
     def action_estate_property_sold(self):
+        self.env["account.move"].check_access("create")
         res = super().action_estate_property_sold()
         self.env['account.move'].create({
             'partner_id': self.buyer_id.id,
