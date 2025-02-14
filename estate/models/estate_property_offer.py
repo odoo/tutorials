11cdd0dd95e55fb7feb4d39c910228e8bf616660
utma-odoo <utma@odoo.com>
2025-02-14 17:39:55 +0530

    [ADD] estate: add kanban view
    After this commit:
    - Added kanban view for properties
    - Added default grouping by property type in kanban view

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -47,6 +47,7 @@ class EsatePropertyOffer(models.Model):
             record.status = 'accepted'
             record.property_id.selling_price = record.price
             record.property_id.partner_id = record.partner_id
+            record.property_id.state = 'offer_accepted'
             for offer in record.property_id.offer_ids:
                 if offer.id is not record.id:
                     offer.status = 'refused'
@@ -57,6 +58,7 @@ class EsatePropertyOffer(models.Model):
             if(record.property_id.partner_id == record.partner_id):
                 record.property_id.selling_price = 0
                 record.property_id.partner_id = False   
+
     @api.model_create_multi
     def create(self,vals_list):
         for vals in vals_list:
