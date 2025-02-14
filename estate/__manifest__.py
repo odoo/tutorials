cfde7a7bf087d75d0cd539a07cf12391d11ab45b
Smit Rathod (smra) <smra@odoo.com>
2025-02-14 12:23:16 +0530

    [IMP] estate: added res_users_views view.
    - Missed Adding res_users_view and its reference to manifest file so added it now.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -14,6 +14,7 @@
         'views/estate_property_offer_views.xml',
         'views/estate_property_types_views.xml',
         'views/estate_property_tags_views.xml',
+        'views/res_users_views.xml',
         'views/estate_property_views.xml',
         'views/estate_menus.xml',
     ],
