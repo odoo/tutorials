8edcfd759915d1bfc4e318624f48632f3991f236
atpa-odoo <atpa@odoo.com>
2025-02-14 19:19:49 +0530

    [IMP] estate: Restrict access to data and modified security files.
    Added new category for Real Estate/Brokerage  in Users.
    Added access rights in csv file for estate group manager and user(agent).

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,18 +3,19 @@
     'version': '1.0',
     'depends': ['base'],
     'author': 'ATPA',
-    'category': 'Category',
+    'category': 'Real Estate/Brokerage',
     'description': """
 Real Estate module is for training, the module contains property types, offers and tags.
 """,
     'data' : [
+        'security/security.xml',
         'security/ir.model.access.csv',
-        'views/estate_property_menus.xml',
         'views/estate_property_views.xml',
+        'views/estate_property_offer_views.xml',
         'views/estate_property_type_views.xml',
         'views/estate_property_tag_views.xml',
-        'views/estate_property_offer_views.xml',
-        'views/res_users_view.xml'
+        'views/res_users_view.xml',
+        'views/estate_property_menus.xml',
     ],
     'application' : True,
     'license' : 'LGPL-3'
