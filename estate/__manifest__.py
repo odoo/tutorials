ddc617faf9b5ffc08e1cdde5d3c97b790f38f823
Harsh Shah <hash@odoo.com>
2025-02-14 16:04:06 +0530

    [ADD] estate: add module master and demo data
    Added master date for Property Type, and demo data for properties and offers, using various available formats.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -12,8 +12,8 @@
     "license": "LGPL-3",
 
     "data": [
-        'security/ir.model.access.csv',
         'security/estate_security.xml',
+        'security/ir.model.access.csv',
 
         'views/estate_property_views.xml',
         'views/estate_property_offer_views.xml',
@@ -21,5 +21,12 @@
         'views/estate_property_tag_views.xml',
         'views/res_users_views.xml',
         'views/estate_menus.xml',
-    ]
+
+        'data/estate.property.type.csv',
+    ],
+
+    "demo": [
+        'demo/estate_property_demo.xml',
+        'demo/estate_property_offer_demo.xml',
+    ],
 }
