b21819bbd1d92e2ffe81eb6a78f15cbe26c74b81
vpan-odoo <vpan@odoo>
2025-02-14 11:51:10 +0530

    [IMP] estate_account: generating invoices for the sold property

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,8 +1,26 @@
-from odoo import fields, models
+from odoo import fields, models, Command
 
 
 class EstateProperty(models.Model):
     _inherit = "estate.property"
 
     def action_sold_property(self):
-        return super().action_sold_property()
+        res = super().action_sold_property()
+        for record in self:
+            self.env['account.move'].create({
+                'partner_id': self.buyer_id.id,
+                "move_type": "out_invoice",
+                "invoice_line_ids": [
+                    Command.create({
+                        "name": record.name,
+                        "quantity": 1,
+                        "price_unit": record.selling_price * 0.6
+                    }),
+                    Command.create({
+                        "name": record.name,
+                        "quantity": 1,
+                        "price_unit": "100.00"
+                    })
+                ]
+                })
+        return res
