cf05d60cfe80f31f3cb11710ea3ee1ba47492cf9
HichamElmefeddel <elmefeddelhicham@gmail.com>
2025-02-17 14:53:52 +0100

    [ADD] estate: added the real estate module
    In this commit, I initialized the real estate module by setting up the essential
    configuration files, namely __manifest__.py and __init__.py.

---
+++ estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'RealEstate',
+    'version': '1.0.0',
+    'author': 'Hicham',
+    'icon': '/estate/static/real_estate.png',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'category': 'Tools',
+    'summary': 'Managing real estate properties efficiently',
+}
