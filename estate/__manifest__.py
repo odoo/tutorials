404f01eb72ef664ad29c8f0f2a7349c94c251066
Augustin (duau) <duau@odoo.com>
2025-02-17 14:58:19 +0100

    [IMP] added menu and view for property

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,6 +6,8 @@
     ],
     'data': [
         'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menu_views.xml',
     ],
     'installable': True,
     'application': True,
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -3,19 +3,18 @@ from odoo import fields, models
 class Property(models.Model):
     _name = "estate.property"
     _description = "Estate properties"
-
-    name = fields.Char("Property Name", required=True)
+    name = fields.Char("Title", required=True)
     description = fields.Text("Description")
     postcode = fields.Char("Postcode")
-    date_availability = fields.Date("Property Availability")
+    date_availability = fields.Date("Available From", copy=False, default=fields.Date.add(fields.Date.today(), months=3))
     expected_price = fields.Float("Expected Price", required=True)
-    selling_price = fields.Float("Selling Price")
-    bedrooms = fields.Integer("Bedrooms")
-    leaving_area = fields.Integer("Leaving Area")
+    selling_price = fields.Float("Selling Price", readonly=True, copy=False)
+    bedrooms = fields.Integer("Bedrooms", default=2)
+    living_area = fields.Integer("Living Area (sqm)")
     facades = fields.Integer("Facades")
     garage = fields.Boolean("Garage")
     garden = fields.Boolean("Garden")
-    garden_area = fields.Integer("Garden Area")
-    garden_orientation = fields.Selection(("North", "South", "East", "West"), "Garden Orientation")
-
-
+    garden_area = fields.Integer("Garden Area (sqm)")
+    garden_orientation = fields.Selection([("north", "North"), ("south", "South"), ("east", "East"), ("west", "West")], "Garden Orientation")
+    status = fields.Selection([("new", "New"), ("offer_received", "Offer Received"), ("offer_accepted", "Offer Accepted"), ("sold", "Sold"), ("cancelled", "Cancelled")], default="new", string="Status", required=True, copy=False)
+    active = fields.Boolean(default=True)
