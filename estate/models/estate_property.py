379d5e0d52a95151fbbf09bad69e240c7390f7bf
Lune Mercier <lmer@odoo.com>
2025-02-17 13:44:56 +0100

    [IMP] estate: add new model estate.property
    Creation of a new model to store estate properties following the 3rd chapter of the tutorial.

---
+++ estate/models/estate_property.py
@@ -0,0 +1,19 @@
+from odoo import fields, models
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Property of the estate"
+
+    name = fields.Char('Name', required=True)
+    description = fields.Text('Description')
+    postcode = fields.Char('Postcode')
+    date_availability = fields.Date('Availability Date')
+    expected_price = fields.Float('Expected Price', required=True)
+    selling_price = fields.Float('Selling Price')
+    bedrooms = fields.Integer('Bedrooms')
+    living_area = fields.Integer('Living Area')
+    facades = fields.Integer('Facades')
+    garage = fields.Boolean('Garage')
+    garden = fields.Boolean('Garden')
+    garden_area = fields.Integer('Garden Area')
+    garden_orientation = fields.Selection(string='Garden Orientation', selection=[('north', 'North'), ('west', 'West'), ('south', 'South'), ('east', 'East')])
