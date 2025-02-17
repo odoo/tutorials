7350d6b19166bb25643d589213a81c8ac3c16b47
bamu <bamu@odoo.com>
2025-02-17 16:17:55 +0530

    [ADD] estate: implemented PDF reports for properties (Build PDF Reports)

--- estate_account/__manifest__.py
+++ estate_account/__manifest__.py
@@ -2,7 +2,9 @@
     "name": "Estate Account",
     "description": "estate account module",
     "depends": ["estate", "account"],
-    "data": [],
+    "data": [
+        "reports/estate_property_templates.xml",
+    ],
     "installable": True,
     "application": True,
     "license": "LGPL-3",
