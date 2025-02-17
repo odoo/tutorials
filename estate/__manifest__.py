13375d4921d00dab0de7469ee804f4679edfab71
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2025-02-17 15:50:29 +0100

    [ADD] estate: create a basic setup for the module - chapter-2
    apply the instructions by which I declare a new app. add the necessary configurations in manifest to let it show up in the apps list without further filtering.

---
+++ estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Real Estate',
+    'version': '18.0',
+    'description': 'anything',
+    'summary': 'anything again',
+    'data': [],
+    'depends': [
+        'base',
+    ],
+    'application': True,
+    'installable': True,
+}
