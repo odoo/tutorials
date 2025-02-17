3743fab9b90353121706d9b1c9665b94b8ca6ce5
maap-odoo <maap@odoo.com>
2025-02-17 14:59:21 +0530

    [FIX] estate:  Ensure AccessError is raised before creating an invoice
    • Previously, an exception could be created before validating the user's access
      rights, leading to unintended behavior.
    • Now, the `check_access('write')` is executed first, and only if it succeeds
      does the system proceed with creating the invoice.
    • If the user lacks the necessary permissions, an `AccessError` is raised
      immediately, preventing any unintended record creation.

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -2,6 +2,8 @@ from datetime import date
 
 from odoo import Command, models
 
+from odoo.exceptions import AccessError
+
 
 class EstateProperty(models.Model):
     _inherit = "estate.property"
@@ -13,19 +15,17 @@ class EstateProperty(models.Model):
 
     def create_invoice(self):
         try:
-            self.check_access_rights('write')
-            self.check_access_rule('write')
+            self.check_access('write')
+            invoice = self.env['account.move'].sudo().create({
+            'partner_id': self.buyer_id.id,
+            'move_type': 'out_invoice',
+            'name' : f'INV/2025/{self.id}',
+            'invoice_date': date.today(),
+            'invoice_line_ids': [
+                Command.create({'name': "Selling Price", 'quantity': 1, 'price_unit': self.selling_price * 0.06}),
+                Command.create({'name': "Administrative Fees", 'quantity': 1, 'price_unit': 100.00}),
+                ]
+            })
+            return invoice
         except:
-            raise UserError("You do not have the necessary permissions to sell this property.")
-
-        invoice = self.env['account.move'].sudo().create({
-        'partner_id': self.buyer_id.id,
-        'move_type': 'out_invoice',
-        'name' : f'INV/2025/{self.id}',
-        'invoice_date': date.today(),
-        'invoice_line_ids': [
-            Command.create({'name': "Selling Price", 'quantity': 1, 'price_unit': self.selling_price * 0.06}),
-            Command.create({'name': "Administrative Fees", 'quantity': 1, 'price_unit': 100.00}),
-            ]
-        })
-        return invoice
+            raise AccessError("You do not have the necessary permissions to sell this property.")
