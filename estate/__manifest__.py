e1c1ba7e8b1f0c9d664e3ad5dd574d4fae7da6b2
Alessandro Lupo <alessandro.lupo.2311@gmail.com>
2025-02-17 13:55:18 +0100

    [ADD] estate: define basic security rule
    This completes chapter 4.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,22 +1,24 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'Real Estate',
-    'version': '0.1',
-    'category': 'Administration',
-    'sequence': 100,
-    'license': 'LGPL-3',
-    'author': 'alup',
-    'summary': 'Track real estate',
-    'description': "",
-    'website': 'https://www.odoo.com/page/estate',
-    'depends': [
-        'base_setup',
+    "name": "Real Estate",
+    "version": "0.1",
+    "category": "Administration",
+    "sequence": 100,
+    "license": "LGPL-3",
+    "author": "alup",
+    "summary": "Track real estate",
+    "description": "",
+    "website": "https://www.odoo.com/page/estate",
+    "depends": [
+        "base_setup",
     ],
-    'data': [],
-    'demo': [],
-    'css': [],
-    'installable': True,
-    'application': True,
-    'auto_install': False
-}   
+    "data": [
+        "security/ir.model.access.csv",
+    ],
+    "demo": [],
+    "css": [],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
