efee63ad79567eb6ebcf9168c666b07718ca9773
Kevin Gerard <kevin.gerard2906@gmail.com>
2025-02-17 13:43:40 +0100

    [ADD] Chapter2: a New Application
    We are creating this estate this new module since we need this new
    application for training purposes

---
+++ estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "estate",
+    "version": "1.0",
+    "depends": ["base"],
+    "author": "Odoo S.A.",
+    "category": "Category",
+    "description": """
+        This is a module to manage real estate properties
+    """,
+    "application": True,
+    "license": "LGPL-3",
+}
