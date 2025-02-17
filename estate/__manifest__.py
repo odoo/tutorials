e887cebcc592d5fe8887ecb9afe56e514aa91770
Guillaume Jacquet <guja@odoo.com>
2025-02-17 14:56:19 +0100

    [ADD] estate: Odoo recognize the estate module
    added init and manifest files

---
+++ estate/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Author Name",
+    'category': 'Category',
+    'description': """
+    Description text
+    """,
+    # data files always loaded at installation
+    'data': [
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+    ],
+    'application': True
+}
