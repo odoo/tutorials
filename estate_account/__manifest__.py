0b02df56b24a720e2034383962d18574358e7fab
khup-odoo <khup@odoo.com>
2025-02-17 18:54:40 +0530

    [IMP]  estate: add controllers , wizard , tetsts
    - Created a controller to display available properties in website
    - Created a wizard so that same offer can be added to multiple properties
    - Added a test case to check that property with no offer cannot be sold
    - Demo Data for property error fixed

--- estate_account/__manifest__.py
+++ estate_account/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "estate_account",
     "version": "1.0",
     "sequence": 1,
-    "depends": ["base" , "estate" , "account"],
+    "depends": ["estate" , "account"],
     "description": "estate_account module is used to manage real estate properties accounting.",
     "installable": True,
     "application": True, 
