827fba28ef7c303f2c53b9965ecf75497aa259eb
mebh-odoo <mebh@odoo.com>
2025-02-17 18:59:57 +0530

    [IMP] estate: removed errors.

--- estate/models/res_users.py
+++ estate/models/res_users.py
@@ -8,4 +8,4 @@ class ResUsers(models.Model):
     _inherit = 'res.users'
 
     property_ids = fields.One2many(
-        'estate.property', 'salesperson_id', string='Properties', domain="[('state', 'in', ['new', 'offer_received')]]")
+        'estate.property', 'salesperson_id', string='Properties', domain="[('state', 'in', ['new', 'offer_received'])]")
