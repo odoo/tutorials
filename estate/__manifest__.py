15456970754987e64b4c3e8025b0ac8b839adaec
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2025-02-18 18:57:08 +0100

    [IMP] estate: create basic UI, enhance prop model
    create action to add the basic functionality of viewing and adding
    estate properties. implement menus to better visualize and access the
    properties. Add attributes to previously set fields to prevent copying,
    set default values, and prevent modifications. Add more predefined
    reserved fields to enable more functionalities (state, active).
    
    chapter-5

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,7 +3,11 @@
     'version': '1.0',
     'description': 'anything',
     'summary': 'anything again',
-    'data': ['security/ir.model.access.csv'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+    ],
     'depends': [
         'base',
     ],
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -8,10 +8,13 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(
+            copy=False,
+            default=fields.Date.add(fields.Date.today(), months=3)
+    )
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -19,6 +22,25 @@ class EstateProperty(models.Model):
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
             string="Garden Orientation",
-            selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
+            selection=[
+                ('north', 'North'),
+                ('south', 'South'),
+                ('east', 'East'),
+                ('west', 'West')
+            ],
             help='garden orientation is used to choose the orientation of the garden attached to the property'
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+            string="State",
+            selection=[
+                ('new', 'New'),
+                ('offer_received', 'Offer Received'),
+                ('offer_accepted', 'Offer Accepted'),
+                ('sold', 'Sold'),
+                ('cancelled', 'Cancelled'),
+            ],
+            default='new',
+            required=True,
+            copy=False,
     )
