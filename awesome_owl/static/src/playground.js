b0b9990b14db158da8ed6fdd06142f589bc15a46
pmah-odoo <pmah@odoo.com>
2025-02-18 18:40:44 +0530

    [IMP] awesome_owl: add owl components counter and card
    - Update Playground as a reactive counter with useState.
    - Extract counter logic into a reusable Counter sub-component.
    - Create a Card component with title & content props.
    - Use t-out for safe HTML rendering in Card.
    - Add props validation to Card for dev-mode errors.

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,7 +1,12 @@
-/** @odoo-module **/
-
-import { Component } from "@odoo/owl";
+import { Component, markup } from "@odoo/owl";
+import { Counter } from "./components/counter/counter";
+import { Card } from "./components/card/card";
 
 export class Playground extends Component {
-    static template = "awesome_owl.playground";
+  static template = "awesome_owl.playground";
+  static components = { Counter, Card };
+
+  setup() {
+    this.html_markup = markup("<div class='text-primary'>some content</div>");
+  }
 }
