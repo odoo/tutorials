3af347c8623af58c6636be53a3bcdeec6bcd2048
Guillaume Jacquet <guja@odoo.com>
2025-02-18 13:51:05 +0100

    [IMP] estate: (Chapter 5) Default interaction with the UI

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 {
     'name': "Real Estate",
     'version': '1.0',
@@ -10,7 +13,12 @@
     'website': 'https://www.odoo.com/page/estate',
     # data files always loaded at installation
     'data': [
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
+        
+        
     ],
     # data files containing optionally loaded demonstration data
     'demo': [
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,16 +1,18 @@
 from odoo import fields, models
 
 class EstateProperty(models.Model):
-    _name = "estate_property"
+    _name = "estate.property"
     _description = "Estate property"
-
+    
+    
+    active = fields.Boolean(default=True)
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Datetime()
+    date_availability = fields.Datetime(copy=False, default=fields.Datetime.add(fields.Datetime.now(), months=+3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -20,4 +22,11 @@ class EstateProperty(models.Model):
         string='Orientation',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
         help="What is the orientation of the facade of the property"
+    )
+    state = fields.Selection(
+        string='State',
+        selection=[('new', 'New'), ('offer-received', 'Offer received'), ('offer-accepted', 'Offer accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')],
+        required=True,
+        copy=False,
+        default='new'
     )
