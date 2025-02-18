b7831af3b88fb565d28cf40c752e2dd87bb036be
assk-odoo <assk@odoo.com>
2025-02-18 10:28:56 +0530

    [IMP] awesome_owl: upto complete my basic framework
    - how to work hooks and life-cycle of odoo
    - component and template of file

---
+++ awesome_owl/static/src/counter/Counter.js
@@ -0,0 +1,19 @@
+import { Component,useState } from "@odoo/owl";
+
+
+export class Counter extends Component {
+    static template = "awesome_owl.Counter";
+    static props = {
+        onchange: {type: Function, optional: true}
+    };
+    setup(){
+        this.state = useState({value: 1 });
+    
+    }
+    increment(){
+        this.state.value = this.state.value + 1;
+        if(this.props.onchange){
+            this.props.onchange();
+        }
+    }
+}
--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,16 +1,23 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component,useState , markup } from "@odoo/owl";
+import { Counter } from "./counter/Counter";
+import { Card } from "./card/Card";
+import { TodoList } from "./todo_list/todo_list";
 
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
 
-    // setup(){
-    //     this.state = useState({value: 1 });
-    
-    // }
-    // increment(){
-    //     this.state.value = this.state.value + 1;
-    // }
+    static components = { Counter, Card , TodoList};
+    setup(){
+        this.str1 = "<div class='text-primary'> some content</div>"
+        this.str2 = markup("<div class='text-danger'> some content</div>")
+        this.sum = useState({value : 2})
+    }
+
+    incrementSum(){
+        this.sum.value = this.sum.value + 1;
+    }
+
 }
