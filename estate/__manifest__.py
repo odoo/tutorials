48abeb1671f3db2ea3a23744475c05833f96a6f6
Kevin Gerard <kevin.gerard2906@gmail.com>
2025-02-18 11:40:16 +0100

    [IMP] Chapter 5: Finally, Some UI to play with
    The goal is to have a basic UI definition for the user to be able to
    create new properties

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,7 +7,11 @@
     "description": """
         This is a module to manage real estate properties
     """,
-    "data": ["security/ir.model.access.csv"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
+    ],
     "application": True,
     "license": "LGPL-3",
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -8,16 +8,38 @@ class EstatePropertyModel(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(string="Date of availability")
-    expected_price = fields.Float(required=True, string="Expected price")
-    selling_price = fields.Float(string="Selling price")
-    bedrooms = fields.Integer(string="Number of bedrooms")
-    living_area = fields.Integer(string="Living area")
+    date_availability = fields.Date(
+        string="Date of availability",
+        copy=False,
+        default=fields.Date.add(fields.Date.today(), months=3),
+    )
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(string="Number of bedrooms", default=2)
+    living_area = fields.Integer()
     facades = fields.Integer(string="Number of facades")
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Integer(string="Garden area")
+    garden_area = fields.Integer()
     garden_orientation = fields.Selection(
-        [("north", "North"), ("south", "South"), ("east", "East"), ("west", "West")],
+        [
+            ("north", "North"),
+            ("south", "South"),
+            ("east", "East"),
+            ("west", "West"),
+        ],
         string="Garden orientation",
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        [
+            ("new", "New"),
+            ("offer_received", "Offer received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled"),
+        ],
+        required=True,
+        copy=False,
+        default="new",
     )
