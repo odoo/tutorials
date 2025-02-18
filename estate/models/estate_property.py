08c45901e11caf8cab598bc33b44fe7581f03e96
gvar-odoo <codenoko@gmail.com>
2025-02-18 17:27:06 +0100

    [ADD] estate: Adding form view and search bar

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -2,44 +2,44 @@ from odoo import fields, models
 from datetime import timedelta
 
 
-
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Model containing basic info of a property"
 
-    name = fields.Char(required=True, default='Unknown')
-    description = fields.Text('Notes')
+    name = fields.Char(required=True, default="Unknown")
+    description = fields.Text("Notes")
     postcode = fields.Char()
-    date_availability = fields.Date(default=fields.Date.today() + timedelta(days=90), copy=False)
+    date_availability = fields.Date(
+        default=fields.Date.today() + timedelta(days=90), copy=False
+    )
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
-    last_seen = fields.Datetime('Last Seen', default=fields.Datetime.now)
+    last_seen = fields.Datetime("Last Seen", default=fields.Datetime.now)
     garden = fields.Boolean()
     garden_area = fields.Integer()
-    active=fields.Boolean('Active', default=True)
+    active = fields.Boolean("Active", default=True)
     garden_orientation = fields.Selection(
-        string='Garden orientation',
         selection=[
-            ('north', 'North'),
-            ('east', 'East'),
-            ('west', 'West'),
-            ('south', 'South')
+            ("north", "North"),
+            ("east", "East"),
+            ("west", "West"),
+            ("south", "South"),
         ]
     )
     state = fields.Selection(
-        string='State',
+        string="State",
         required=True,
         copy=False,
-        default='new',
+        default="new",
         selection=[
-            ('new', 'New'),
-            ('offer received', 'Offer Received'),
-            ('offer accepted', 'Offer Accepted'),
-            ('sold', 'Sold'),
-            ('cancelled', 'Cancelled')
-        ]
+            ("new", "New"),
+            ("offer received", "Offer Received"),
+            ("offer accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled"),
+        ],
     )
