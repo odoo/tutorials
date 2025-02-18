8fd922d5f56ab6782cc6ba1ebbd8d3b2d4b6150f
HichamElmefeddel <elmefeddelhicham@gmail.com>
2025-02-18 10:51:47 +0100

    [IMP] estate: improved the module by modifying the manifest file and removing the views folder
    I removed the views folder as long as it's not needed for now, I'll try
    to keep the folder structure well-adapted with each part of the tutorials.
    
    I changed some fields in my manifest file because they were not correct, the author field changed
    from my name 'Hicham' to 'Odoo S.A.', the version is '1.0' now rather than '1.0.0' as long as
    this is a small application and it's much convenient for it to have a short version.
    
    Moreover, I added a license field with the value LGPL-3 which is the Odoo license required
    for my module, my module is an open-source one therefore the LGPL-3 is convenient in this case.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,11 +1,12 @@
 {
     'name': 'RealEstate',
-    'version': '1.0.0',
-    'author': 'Hicham',
+    'version': '1.0',
+    'author': 'Odoo S.A.',
     'icon': '/estate/static/real_estate.png',
     'installable': True,
     'application': True,
     'auto_install': False,
     'category': 'Tools',
     'summary': 'Managing real estate properties efficiently',
+    'license': 'LGPL-3',
 }
