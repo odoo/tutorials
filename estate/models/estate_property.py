7849f65b7d9ce3997f1c02f6e5ca857d9970ab9e
HichamElmefeddel <elmefeddelhicham@gmail.com>
2025-02-18 14:34:37 +0100

    [IMP] estate: improved models/constants.py
    added some constants values to the constants.py file, also I changed the name
    of some variables to align with the convention for each model I'll create
    in the future.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -8,15 +8,16 @@ from odoo import models, fields
 from .constants import (
     PROPERTY_ORM_NAME,
     PROPERTY_ORM_DESC,
-    DEFAULT_PROPERTY_GARAGE,
-    DEFAULT_PROPERTY_GARDEN,
-    DEFAULT_PROPERTY_GARDEN_ORIENTATION_SELECTION,
+    PROPERTY_DEFAULT_GARAGE,
+    PROPERTY_DEFAULT_GARDEN,
+    PROPERTY_DEFAULT_GARDEN_ORIENTATION_SELECTION,
     PROPERTY_GARDEN_ORIENTATION_SELECTION,
+    PROPERTY_UI_GARDEN_ORIENTATION_SELECTION,
 )
 
 class Property(models.Model):
-    _name = "estate" 
-    _description = "estate property"
+    _name = PROPERTY_ORM_NAME 
+    _description = PROPERTY_ORM_DESC
 
     postcode = fields.Char(required=True)
     date_availability = fields.Date(required=True)
@@ -25,11 +26,11 @@ class Property(models.Model):
     bedrooms = fields.Integer(required=True) 
     living_area = fields.Integer(required=True)
     facades = fields.Integer(required=True)
-    garage = fields.Boolean(required=False, default=DEFAULT_PROPERTY_GARAGE)
-    garden = fields.Boolean(required=False, default=DEFAULT_PROPERTY_GARDEN)
+    garage = fields.Boolean(required=False, default=PROPERTY_DEFAULT_GARAGE)
+    garden = fields.Boolean(required=False, default=PROPERTY_DEFAULT_GARDEN)
     garden_area = fields.Integer(required=True)
     garden_orientation = fields.Selection(
         selection=PROPERTY_GARDEN_ORIENTATION_SELECTION,
-        string="Garden Orientation",
-        default=DEFAULT_PROPERTY_GARDEN_ORIENTATION_SELECTION,
+        string=PROPERTY_UI_GARDEN_ORIENTATION_SELECTION,
+        default=PROPERTY_DEFAULT_GARDEN_ORIENTATION_SELECTION,
     )
