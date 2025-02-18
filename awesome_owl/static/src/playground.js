7db0a7575103153d3e6b75a7eb5a360bb2ef3bcb
amga-odoo <amga@odoo.com>
2025-02-18 14:41:08 +0530

    [ADD] awesome_owl: add card, counter, todo

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,15 +1,21 @@
 /** @odoo-module **/
 
-import { Component, useState } from "@odoo/owl";
+import { Component, markup, useState } from "@odoo/owl";
+import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
+import { TodoList } from "./todo_list/todo_list";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
-
+    static components = { Counter, Card, TodoList };
+    
     setup(){
-        this.state = useState({ value: 0 });
+        this.html1 = "<div>some content</div>";
+        this.html2 = markup("<div>some content</div>");
+        this.sum = useState({ value: 2 })
     }
 
-    increment(){
-        this.state.value++;
+    incrementSum() {
+        this.sum.value++;
     }
 }
