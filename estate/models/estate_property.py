51128a571f90aa8d6e66cb54dbb59dc8640e5aa5
Guillaume Jacquet <guja@odoo.com>
2025-02-19 15:02:23 +0100

    [IMP] estate: (chapter 8) added computation for the total area, the deadline date, and the garden

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class EstateProperty(models.Model):
@@ -48,3 +48,28 @@ class EstateProperty(models.Model):
     partner_id = fields.Many2one('res.partner', string='Buyer', copy=False) 
     property_tag_ids = fields.Many2many("estate.property.tag", string="Tags")
     offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
+
+    total_area = fields.Integer(compute="_compute_total_area", readonly=True)
+
+    @api.depends("garden_area", "living_area")
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.garden_area + record.living_area
+    
+
+    best_price = fields.Integer(compute="_compute_best_price", readonly=True)
+
+    @api.depends("offer_ids.price")
+    def _compute_best_price(self):
+        for record in self:
+            record.best_price = max(record.offer_ids.mapped('price'))
+
+
+    @api.onchange("garden")
+    def _onchange_garden(self):
+        if self.garden is True:
+            self.garden_area = 10
+            self.garden_orientation = 'north'
+        else:
+            self.garden_area = 0
+            self.garden_orientation = None
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class EstatePropertyOffer(models.Model):
@@ -16,4 +16,17 @@ class EstatePropertyOffer(models.Model):
     partner_id = fields.Many2one('res.partner', string='Buyer', required=True) 
     property_id = fields.Many2one('estate.property', string="Property", required=True)
     
+    validity = fields.Integer(default=7)
+    create_date = fields.Date(default=fields.Date.today(), readonly=True)
+    date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_date_deadline")
 
+    @api.depends("validity", "create_date")
+    def _compute_date_deadline(self):
+        for record in self:
+            record.date_deadline = fields.Date.add(record.create_date, days=+record.validity)
+
+    @api.onchange("date_deadline")
+    def _inverse_date_deadline(self):
+        for record in self:
+            record.validity = (record.date_deadline - record.create_date).days
+            print((record.date_deadline - record.create_date).days)
