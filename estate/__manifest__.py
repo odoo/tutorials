4e34a1453b4b59c39b216747b05927f25cdcb04e
gvar-odoo <codenoko@gmail.com>
2025-02-19 15:44:05 +0100

    [ADD] estate: Adding offers for property

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,10 +5,11 @@
     ],
     "data": [
         "security/ir.model.access.csv",
-        "views/estate_property_views.xml",
         "views/estate_property_type_views.xml",
         "views/estate_property_tags_views.xml",
+        "views/estate_property_offer_views.xml",
         "views/estate_menus_views.xml",
+        "views/estate_property_views.xml",
     ],
     "application": True,
     "installable": True,
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -25,6 +25,7 @@ class EstateProperty(models.Model):
     property_type_id = fields.Many2one("estate.property.type", string="Type")
     tag_ids = fields.Many2many("estate.property.tag")
     buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
+    offer_ids = fields.One2many("estate.property.offer", "property_id")
     salesperson_id = fields.Many2one(
         "res.users", string="Salesman", default=lambda self: self.env.user
     )
