934a64db44cc965ea9a80ed7ed14001944bcf383
lmer-odoo <lmer@odoo.com>
2025-02-19 10:47:41 +0100

    [IMP] estate: Make manifest more complete
    Add license and base_setup dependency

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,9 +1,12 @@
 {
     'name': 'estate',
+    'version': '0.1',
     'application': True,
+    'depends' : ['base_setup'],
     'data': [
         'views/estate_property_views.xml',
         'views/estate_menus.xml',
         'security/ir.model.access.csv',
     ],
+    "license": "AGPL-3",
 }
