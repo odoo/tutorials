37fd4c9c5da99be3f6446a66a67a50fd976c9301
unka-odoo <unka@odoo.com>
2025-02-19 18:58:16 +0530

    [IMP] awesome_owl:  Displayed counter, simple card and added prop validation
    Displayed counter, extracted counter in sub component. Added simple card
    component. Used markup to display html content through t-out directive. Added
    prop validation for card component.

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,7 +1,14 @@
-/** @odoo-module **/
-
-import { Component } from "@odoo/owl";
+import { markup, Component} from "@odoo/owl";
+import { Counter } from "./counter/counter.js";
+import { Card } from "./card/card.js";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
+
+    static components = {Counter, Card};
+
+    setup() {
+        this.normal_content = "<div class='text-primary'>some content</div>"
+        this.html_content = markup("<div class='text-primary'>some content</div>")
+    }
 }
