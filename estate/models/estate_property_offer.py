36bca11c1d304f0167443aded442b495464e777f
Sébastien Blondiau <blse@odoo.com>
2025-02-19 11:02:41 +0100

    [IMP] estate: Add compute and onchange
    Following chapter 8 of server framework 101, qdds the following
    fields to proprety and offer:
    - total area
    - validity
    - deadline
    and also a onchange for garden to set default values to the garden

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import api, models, fields
 
 
 class Offer(models.Model):
@@ -15,3 +15,17 @@ class Offer(models.Model):
     )
     partner_id = fields.Many2one("res.partner", string="Partner", required=True)
     property_id = fields.Many2one("estate.property", string="Property", required=True)
+    validity = fields.Integer(string="Validity (days)")
+    date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_date_deadline")
+
+    @api.depends("validity")
+    def _compute_date_deadline(self):
+        for record in self:
+            record.date_deadline = fields.Date.add(
+                record.create_date or fields.Date.today(),
+                days=record.validity
+            )
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            record.validity = (record.date_deadline - fields.Date.to_date(record.create_date)).days
