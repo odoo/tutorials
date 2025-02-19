37daebbe1bdeb466b66284340318cbaa5635b158
pvag-odoo <pvag@odoo.com>
2025-02-19 16:28:34 +0530

    [IMP] awesome_owl:  Enable HTML rendering in Card component
    • Modified Card component to use t-out instead of t-esc for displaying HTML.
    • Added markup() in Playground to process and render HTML.

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component,markup } from "@odoo/owl";
 import { Counter } from "./Counter/counter";
 import { Card } from "./Card/card";
 
@@ -9,4 +9,8 @@ export class Playground extends Component {
     static template = "awesome_owl.playground";
 
     static components = {Counter,Card}
+
+    value1 = "<div>some text 1</div>";
+    value2 = markup("<div>some text 2</div>");
+
 }
