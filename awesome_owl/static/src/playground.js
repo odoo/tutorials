b2c477ecb0d82188529e0fa5aeb362e56f11f2a5
khaj-odoo <khaj@odoo.com>
2025-02-19 14:47:46 +0530

    [IMP] awesome_owl:  Implemented Counter and Card components
     -  Implemented Counter component with internal state management using useState.
     -  Added increment functionality with support for parent sum updates.
     -  Created reusable Card component to display content.

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,7 +1,16 @@
-/** @odoo-module **/
+import { Component, markup, useState } from "@odoo/owl";
+import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
 
-import { Component } from "@odoo/owl";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
+	static props = [];
+	static components = { Counter, Card };
+	setup() {
+		this.sum = useState({ value: 0 });
+	}
+	incrementSum() {
+		this.sum.value++;
+	}
 }
