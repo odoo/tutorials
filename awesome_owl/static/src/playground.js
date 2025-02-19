1ad07f96281d59333d1166b8fd763e1c2d4607b1
maap-odoo <maap@odoo.com>
2025-02-19 15:30:59 +0530

    [IMP] awesome_owl:  Support HTML content in Card component
    • Updated Card component to use `t-out` instead of `t-esc` for rendering HTML.
    • Introduced markup() in Playground to handle HTML content.

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,10 +1,18 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, markup } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.Playground";
     static components = { Counter, Card };
+
+    setup() {
+        this.cards = [
+            { title: "Safe String", content: "This is a normal string" },
+            { title: "Styled HTML", content: markup("<b style='color: red;'>This is bold and red</b>") },
+            { title: "List Example", content: markup("<ul><li>Item 1</li><li>Item 2</li></ul>") },
+        ];
+    }
 }
