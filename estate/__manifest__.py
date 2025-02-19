e540ab855ca8beaeea715619798b26e9db41efce
mear-odoo <mear@odoo.com>
2025-02-19 10:44:07 +0530

    [IMP] estate: add multiple offer using wizard
    • Added add_offer_wizard to add one offer to multiple properties simultaneously.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,6 +5,7 @@
     "data": [
             "security/security.xml",
             "security/ir.model.access.csv",
+            'views/estate_property_offer_wizard.xml',
             "views/estate_property_views.xml",
             "views/estate_property_offer.xml",
             "views/estate_property_type.xml",
