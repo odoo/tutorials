d041fbe1f766a6cab9dcc8cfbc02233aced48b4a
ssud-odoo <ssud@odoo.com>
2025-02-19 18:41:04 +0530

    [IMP] estate: added date filter for listing property in website

--- estate/controllers/estate_property_controller.py
+++ estate/controllers/estate_property_controller.py
@@ -1,10 +1,12 @@
-from odoo import http, _
+from odoo import http
 from odoo.http import request
+from odoo.addons.website.models.website import pager as website_pager
+from datetime import datetime
 
 class EstatePropertyController(http.Controller):
     
     @http.route(['/properties', '/properties/page/<int:page>'], type='http', auth="public", website=True)
-    def list_properties(self, page=1, **kwargs):
+    def list_properties(self, page=1, date_filter=None, **kwargs):
         Properties = request.env['estate.property']
         
         domain = [('state', 'in', ['new', 'offer_received'])]
@@ -12,13 +14,29 @@ class EstatePropertyController(http.Controller):
         
         per_page = 6
         offset = (page - 1) * per_page
-        
+
+         # Filter properties by create_date if date_filter is provided
+        if date_filter:
+            try:
+                date_obj = datetime.strptime(date_filter, "%Y-%m-%d").date()
+                domain.append(('create_date', '>=', date_obj))
+            except ValueError:
+                pass  # Ignore invalid date formats
+
+        pager = website_pager(
+            url="/properties",
+            total=properties_count,
+            page=page,
+            step=per_page,
+            scope=3
+        )
+
         properties = Properties.search(domain, limit=per_page, offset=offset)
 
         values = {
             'properties': properties,
-            'page': page,
-            'total_pages': (properties_count + per_page - 1) // per_page,
+            'pager': pager,
+            'date_filter': date_filter
         }
 
         return request.render('estate.property_list_template', values)
