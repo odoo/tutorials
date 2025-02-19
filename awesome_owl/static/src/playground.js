982f2cefbc5c1629b19b532b452f4f6ad1db5393
nmis-odoo <nmis@odoo.com>
2025-02-19 15:27:31 +0530

    [IMP] awesome_owl:  Implement Counter component and reusable Card component
    - Created an initial Counter component inside Playground to track state.
    - Extracted Counter logic into a separate component for reusability.
    - Created a reusable Card component with title and content props.
    - Used Markup to display HTML.

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,7 +1,15 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, markup } from "@odoo/owl";
+import { Counter } from "./Counter/counter";
+import { Card } from "./Card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
+    static components = {Counter , Card}
+
+    setup() {
+        this.str1 = "<div class='text-primary'>some content</div>";
+        this.str2 = markup("<div class='text-primary'>some content</div>");
+    }
 }
