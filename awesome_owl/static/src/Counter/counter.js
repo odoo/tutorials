845eb040bb03f8304e25f14f71fa5d4ad0543fcd
pvag-odoo <pvag@odoo.com>
2025-02-20 07:13:39 +0530

    [IMP] awesome_owl:  Implement Props Validation & Counter Sum Feature
    • Added props validation for components to ensure type safety.
    • Implemented sum functionality to calculate the total of two counters.

--- awesome_owl/static/src/Counter/counter.js
+++ awesome_owl/static/src/Counter/counter.js
@@ -4,11 +4,18 @@ import { Component ,useState} from "@odoo/owl";
 export class Counter extends Component {
     static template = "awesome_owl.counter";
 
+    static props = {
+        onChange: { type: Function, optional: true }
+    };
+
     setup() {
-        this.state = useState({ value: 0 });
+        this.state = useState({ value: 1 });
     }
 
     increment() {
         this.state.value++;
+        if (this.props.onChange) {
+            this.props.onChange();
+        }
     }
 }
--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import { Component,markup } from "@odoo/owl";
+import { Component,markup,useState } from "@odoo/owl";
 import { Counter } from "./Counter/counter";
 import { Card } from "./Card/card";
 
@@ -10,6 +10,12 @@ export class Playground extends Component {
 
     static components = {Counter,Card}
 
-    value1 = "<div>some text 1</div>";
-    value2 = markup("<div>some text 2</div>");
+    setup(){
+        this.str1 = "<div class='text-primary'>some content</div>";
+        this.str2 = markup("<div class='text-primary'>some content</div>");
+        this.sum = useState({ value:2 });
+    }
+    incrementSum() {
+        this.sum.value++;
+    }
 }
