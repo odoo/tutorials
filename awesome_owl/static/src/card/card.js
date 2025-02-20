fa73ed32cacc4c8139414a213e65785c40c0ac07
dvsh-odoo <dvsh@odoo.com>
2025-02-20 11:09:54 +0530

    [IMP] awesome_owl:  add card component with custom css
    - Added card component to playground view.
    - Added custom css to give hover animation.

---
+++ awesome_owl/static/src/card/card.js
@@ -0,0 +1,6 @@
+import {Component} from "@odoo/owl";
+
+export class Card extends Component {
+    static template = "awesome_owl.card";
+    static props = { title: { type: String },content: { type: String }};
+}
