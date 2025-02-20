c3bba6238be72ea4525e295f5cc8812cbc39cea0
Romain (roto) <roto@odoo.com>
2025-02-20 16:59:29 +0100

    [IMP] estate: Link Module (Chapter 13 - part 2)

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import models
+from odoo import Command, models
 
 
 class EstateProperty(models.Model):
@@ -7,9 +7,20 @@ class EstateProperty(models.Model):
     def action_set_status_sold(self):
         self.env['account.move'].create(
             {
-                'partner_id': self.partner_id,
+                'partner_id': self.partner_id.id,
                 'move_type': 'out_invoice',
-
+                'invoice_line_ids': [
+                    Command.create({
+                        'name': '6 Percent of selling price',
+                        'quantity': 0.06,
+                        'price_unit': self.selling_price
+                    }),
+                    Command.create({
+                        'name': 'Administrative fees',
+                        'quantity': 1.0,
+                        'price_unit': 100.0,
+                    })
+                ],
             }
         )
         return super().action_set_status_sold()
