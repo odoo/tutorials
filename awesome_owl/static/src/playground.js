f6a7889a5a7291039a1289f7e4ad055861d9cb8e
smjo-odoo <smjo@odoo.com>
2025-02-20 18:42:59 +0530

    [ADD] awesome_owl: implemented Owl components for counter, card, and todo
    - Created a Counter component with reactivity and increment functionality.
    - Used Counter as a reusable sub-component.
    - Built a Card component with props for dynamic title and content display.
    - Implemented markup rendering for HTML content inside the Card component.
    - Developed a Todo List app with dynamic add, toggle, and delete functionality.
    - Enhanced TodoItem component with dynamic attributes and event handling.
    - Integrated lifecycle hooks like onMounted and useRef for autofocus and
      DOM manipulation.
    - Enabled the summation of two Counter values in the Playground component
      via callback props.

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,13 +1,17 @@
-import { Component, useState } from "@odoo/owl";
+import { Component, markup, useState } from "@odoo/owl";
+import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
+import { TodoList } from "./todo_list/todo_list";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
-
-    setup() {
-        this.state = useState({ value: 0 });
+    static components = { Counter, Card, TodoList };
+    setup(){
+        this.val3 = "<div class='text-primary'>card3 content</div>"
+        this.val4 = markup("<div class='text-primary'>card4 content</div>")
+        this.sum = useState({value:2});
     }
-
-    increment() {
-        this.state.value++;
+    incrementSum(){
+        this.sum.value++;
     }
 }
