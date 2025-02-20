02450338912dfc6e5e591e5beb4f85da0dc1e697
Alessandro Lupo <alessandro.lupo.2311@gmail.com>
2025-02-20 09:31:20 +0100

    [IMP] estate: generate invoices (chapter 13)

---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,46 @@
+from odoo import Command, fields, models
+from odoo.exceptions import UserError
+
+
+class Property(models.Model):
+
+    _inherit = "estate.property"
+
+    journal_id = fields.Many2one(
+        "account.journal",
+        string="Invoicing Journal",
+        store=True,
+        domain=[("type", "=", "sale")],
+    )
+
+    def action_set_sold(self):
+        if not self.journal_id:
+            self.journal_id = self.env["account.journal"].search(
+                [
+                    ("type", "=", "sale"),
+                ],
+                limit=1,
+            )
+        if not self.journal_id:
+            raise UserError("Please define an accounting sales journal.")
+
+        move = self.env["account.move"].create(
+            {
+                "partner_id": self.buyer_id.id,
+                "journal_id": self.journal_id.id,
+                "move_type": "out_invoice",
+                "line_ids": [
+                    Command.create(
+                        {
+                            "name": "Commission",
+                            "quantity": 1,
+                            "price_unit": self.selling_price * 0.06,
+                        }
+                    ),
+                    Command.create(
+                        {"name": "Administrative fee", "quantity": 1, "price_unit": 100}
+                    ),
+                ],
+            }
+        )
+        return super().action_set_sold()
