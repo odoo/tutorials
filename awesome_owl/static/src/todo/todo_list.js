c4ff15b1202bc07eb04bd112249941df492ca424
mear-odoo <mear@odoo.com>
2025-02-20 11:39:58 +0530

    [IMP] awesome_owl:  Enable adding new todos dynamically
    • Added an input field with a placeholder for adding new tasks.
    • Implemented addTodo to create a new todo when Enter is pressed.
    • Ensured each new todo has a unique ID.
    • Prevented adding empty todos.

--- awesome_owl/static/src/todo/todo_list.js
+++ awesome_owl/static/src/todo/todo_list.js
@@ -7,9 +7,18 @@ export class TodoList extends Component{
     static components = { TodoItem };
 
     setup() {
-        this.todos = useState([
-            { id: 2, description: "Write Tutorial", isCompleted: true },
-            { id: 3, description: "buy milk", isCompleted: false }
-        ]);
+        this.nextId = 1;
+        this.todos = useState([]);
+    }
+
+    addTask(ev) {
+        if(ev.keyCode === 13 && ev.target.value != "") {
+            this.todos.push({
+                id: this.nextId++,
+                description: ev.target.value,
+                isCompleted: false
+            });
+            ev.target.value = "";
+        }
     }
 }
