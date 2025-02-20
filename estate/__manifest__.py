10e0517f0887f8dfc6fdf858dd10ee5056e4c859
Bryce Burignat <brbu@odoo.com>
2025-02-20 17:58:33 +0100

    [IMP] Estate : added Properties tab in user page

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -13,6 +13,7 @@
         'views/estate_property_type_views.xml',
         'views/estate_property_tag_views.xml',
         'views/estate_menus.xml',
+        'views/res_users_views.xml',
     ],
     'license': 'AGPL-3',
 }
