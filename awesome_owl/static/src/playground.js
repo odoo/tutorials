46156e7ca1b0842e9001fd3e3a313a92a108571d
dvsh-odoo <dvsh@odoo.com>
2025-02-20 15:26:49 +0530

    [IMP] awesome_owl:  add markup to display html

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,10 +1,15 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component , markup} from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card  } from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.Playground";
     static components= { Counter , Card };
+
+    setup(){
+        this.str1 = "<div class='text-primary'>Some Content</div>";
+        this.str2 = markup("<div class='text-primary'>Not some Content</div>");
+    }
 }
