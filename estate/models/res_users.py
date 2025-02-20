6373c48f2a25839b9f4fd6576373835591fc90f8
lmer-odoo <lmer@odoo.com>
2025-02-20 10:49:26 +0100

    [IMP] estate: add tag to see properties from user
    Use inheritance to improve the user data and view with the linked properties
    
    From chapter 12 of tutorial

---
+++ estate/models/res_users.py
@@ -0,0 +1,6 @@
+from odoo import fields, models
+
+class Users(models.Model):
+    _inherit = 'res.users'
+
+    offer_ids = fields.One2many('estate.property', 'user_id', string='Real Estate Properties')
