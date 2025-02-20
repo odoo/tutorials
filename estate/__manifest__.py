725e64ee477977a6f69289db0406ef04ca741da8
lmer-odoo <lmer@odoo.com>
2025-02-20 17:33:32 +0100

    [IMP] estate: Separate views in different file, one per model

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,7 +4,11 @@
     'application': True,
     'depends' : ['base_setup'],
     'data': [
+        'views/res_users_views.xml',
         'views/estate_property_views.xml',
+        'views/estate_property_offer_views.xml',
+        'views/estate_property_tag_views.xml',
+        'views/estate_property_type_views.xml',
         'views/estate_menus.xml',
         'security/ir.model.access.csv',
     ],
