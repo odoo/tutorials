fef6e742aaf6facbb7621ad7a01d4ac548eb8f2d
dijo-odoo <dijo@odoo.com>
2025-02-20 18:58:27 +0530

    [IMP] awesome_owl: added card and counter components used props

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,7 +1,17 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
-
+import { Component , useState ,markup } from "@odoo/owl";
+import { Counter } from "./counter";
+import { Card } from "./card";
 export class Playground extends Component {
     static template = "awesome_owl.playground";
+    static components = { Counter , Card};
+    setup(){
+        this.cardTitle = "My First Card";
+        this.cardContent = "This is <strong>bold</strong> text!";
+        this.safeContent = markup("This is <strong>bold</strong> text!");
+    }
+
 }
+
+
