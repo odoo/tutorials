58427a74acd7750b5a0fa62228884c4fae7f1511
Shiv Bhadaniya (sbha) <sbha@odoo.com>
2025-02-20 18:56:43 +0530

    [IMP] awesome_dashboard:  Set up Dashboard with Navigation and Statistics
    Designed a dashboard layout with quick navigation
    Utilized a service to fetch and display customers and leads

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,10 +1,41 @@
 /** @odoo-module **/
 
+import { _t } from "@web/core/l10n/translation";
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
+import { DashboardItem } from "./dashboard_item/dashboard_item";
 
 class AwesomeDashboard extends Component {
-    static template = "awesome_dashboard.AwesomeDashboard";
+  static template = "awesome_dashboard.AwesomeDashboard";
+  static components = { Layout, DashboardItem };
+
+  setup() {
+    this.display = {
+      controlPanel: {},
+    };
+    this.action = useService("action");
+  }
+
+  showCustomers() {
+    this.action.doAction("base.action_partner_form");
+  }
+
+  showLeads() {
+    this.action.doAction({
+      type: "ir.actions.act_window",
+      name: _t("Leads"),
+      target: "current",
+      res_model: "crm.lead",
+      views: [
+        [false, "list"],
+        [false, "form"],
+      ],
+    });
+  }
 }
 
-registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
+registry
+  .category("actions")
+  .add("awesome_dashboard.dashboard", AwesomeDashboard);
