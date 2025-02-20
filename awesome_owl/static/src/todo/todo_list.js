05c6be6811d56039af3c05c98f79695af530336c
mear-odoo <mear@odoo.com>
2025-02-20 11:06:55 +0530

    [IMP] awesome_owl:  Add dynamic styling for completed todos
    • Applied bootstrap classes dynamically to todo item based on completion status.

--- awesome_owl/static/src/todo/todo_list.js
+++ awesome_owl/static/src/todo/todo_list.js
@@ -8,7 +8,7 @@ export class TodoList extends Component{
 
     setup() {
         this.todos = useState([
-            { id: 2, description: "Write Tutorial", isCompleted: false },
+            { id: 2, description: "Write Tutorial", isCompleted: true },
             { id: 3, description: "buy milk", isCompleted: false }
         ]);
     }
