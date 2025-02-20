32eff3ad3c1ade5302b7e3a348e3fcdd3345ec62
Augustin (duau) <duau@odoo.com>
2025-02-20 10:07:12 +0100

    [IMP] added offers in type and smart button to access them

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -12,7 +12,7 @@ class PropertyOffer(models.Model):
     property_id = fields.Many2one("estate.property", string="Property", required=True)
     validity = fields.Integer(string="Validity (days)", default=7,)
     date_deadline = fields.Date("Deadline", compute="_compute_deadline", inverse="_inverse_deadline")
-    # property_type_id=fields.Many2one(related=property_id.property_type_id, string="Property Type")
+    property_type_id=fields.Many2one('estate.property.type', related="property_id.property_type_id", string="Property Type")
 
     _sql_constraints = [
         ("check_offer_price", "CHECK(price >= 0.0)",
--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -9,15 +9,15 @@ class PropertyType(models.Model):
     name = fields.Char("Type", required=True)
     property_ids = fields.One2many("estate.property", "property_type_id", string="Properties")
     sequence = fields.Integer(default=1)
-    # offer_ids = fields.One2many("estate.property.offer", "property_type_id", string="Offers")
-    # offer_count = fields.Integer(compute="_compute_offer_count")
+    offer_ids = fields.One2many("estate.property.offer", "property_type_id", string="Offers")
+    offer_count = fields.Integer(compute="_compute_offer_count")
 
     _sql_constraints = [
         ("check_unique_type", "UNIQUE(name)",
          "The type name should be unique."),
     ]
 
-    # @api.depends("offer_ids")
-    # def _compute_offer_count(self):
-    #     for type in self:
-    #         type.offer_count = type.offer_ids.count()
+    @api.depends("offer_ids")
+    def _compute_offer_count(self):
+        for type in self:
+            type.offer_count = len(type.offer_ids)
