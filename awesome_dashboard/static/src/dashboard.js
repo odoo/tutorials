7aa560f3f5ac12a360bf773547f7b4dfbed4a5f6
krip-odoo <krip@odoo.com>
2025-02-21 18:49:22 +0530

    [ADD] awesome_dashboard: added a new Layout, notification service

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,10 +1,12 @@
-/** @odoo-module **/
-
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout"
 
-class AwesomeDashboard extends Component {
+export class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
+    static components = { Layout }
+    setup() {
+        this.displayprops = { controlPanel: {} };
+    }
 }
-
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
