dfabc5748ecb5b6eec4dc59f6caf395dd2a823dd
Dhrumit Parmar <dgpa@odoo.com>
2025-02-21 17:08:32 +0530

    [ADD] awesome_dashboard: created awesome_dashboard ( Chapter 2)

---
+++ awesome_dashboard/static/src/dashboard/dashboard_item/dashboardItem.js
@@ -0,0 +1,13 @@
+import { Component, useState } from "@odoo/owl";
+
+export class DashboardItem extends Component {
+  static template = "awesome_dashboard.DashboardItem";
+  static defaultProps = {
+    size: 1,
+  };
+  static props = {
+    slots: { type: Object, optional: true },
+    size: { type: Number, optional: true, default: 1 },
+    item: { type: Object, optional: true },
+  };
+}
---
+++ awesome_dashboard/static/src/dashboard/number_card/number_card.js
@@ -0,0 +1,12 @@
+import { Component } from "@odoo/owl";
+
+export class NumberCard extends Component {
+  static template = "awesome_dashboard.NumberCard";
+  static defaultProps = {
+    size: 1,
+  };
+  static props = {
+    title: { type: String, optional: true },
+    value: { type: Number, optional: true },
+  };
+}
