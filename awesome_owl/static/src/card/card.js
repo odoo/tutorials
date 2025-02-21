e8e48817e7ee5884b0d559ac6b92336bf66aaca2
pcbe-odoo <pcbe@odoo.com>
2025-02-21 19:00:49 +0530

    [REF] awesome_owl: implement foundational Owl components
    - Introduced a Counter application to demonstrate reactive state management.
    - Developed a Card component with prop validation and slot support for flexible
      content display.
    - Created a dynamic Todo list featuring:
      - Addition of new items.
      - State toggling between completed and pending.
      - Item deletion functionality.
    - Enhanced Card component with toggleable content visibility.

---
+++ awesome_owl/static/src/card/card.js
@@ -0,0 +1,23 @@
+import { Component, useState } from "@odoo/owl"
+
+export class Card extends Component {
+    static template = "awesome_owl.Card"
+    static props = {
+        title: String,
+        slots: {
+            type: Object,
+            shape: {
+                default: true
+            },
+        }
+    };
+
+    setup() {
+        this.state = useState({ visible: true });
+    }
+
+    onToggleContent() {
+        console.log("visible => ", this.state.visible)
+        this.state.visible = !this.state.visible;
+    }
+}
