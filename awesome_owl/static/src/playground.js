6265bbb4f0a95c368e289e496e84d1f167a666e6
krge-odoo <krge@odoo.com>
2025-02-21 19:11:37 +0530

    [IMP] awesome_owl: create counter component and reuse it
    - create counter folder and its js and xml file
    - displayed two counter in playground

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,7 +1,8 @@
 /** @odoo-module **/
-
-import { Component } from "@odoo/owl";
+import { Counter } from "./counter/counter";
+import { Component, useState } from "@odoo/owl";
 
 export class Playground extends Component {
-    static template = "awesome_owl.playground";
+    static template = "awesome_owl.playground"
+    static components = {Counter}
 }
