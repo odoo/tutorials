0d9be4d3f6a1c68b0f241cadcd95cef68c42994c
Jenish Korat <jeka@odoo.com>
2025-02-21 18:53:50 +0530

    [IMP] awesome_dashboard: Build a dashboard completed.

--- awesome_dashboard/static/src/dashboard/PieChart/pie_chart.js
+++ awesome_dashboard/static/src/dashboard/PieChart/pie_chart.js
@@ -1,6 +1,5 @@
 import { loadJS } from "@web/core/assets";
-import { getColor } from "@web/core/colors/colors";
-import { Component, onWillStart, useRef, onMounted, onWillUnmount } from "@odoo/owl";
+import { Component, onWillStart, useRef, onMounted, onWillUnmount, useEffect } from "@odoo/owl";
 
 export class PieChart extends Component {
     static template = "awesome_dashboard.PieChart";
@@ -18,12 +17,17 @@ export class PieChart extends Component {
         onWillUnmount(() => {
             this.chart.destroy();
         });
+        useEffect(
+            () => {
+            this.updateChart();
+            },
+            () => [this.props.data]
+        );
     }
 
     renderChart() {
         const labels = Object.keys(this.props.data);
         const data = Object.values(this.props.data);
-        const color = labels.map((_, index) => getColor(index));
         this.chart = new Chart(this.canvasRef.el, {
             type: "pie",
             data: {
@@ -32,10 +36,24 @@ export class PieChart extends Component {
                     {
                         label: this.props.label,
                         data: data,
-                        backgroundColor: color,
+                        backgroundColor: ["red", "blue", "green"],
                     },
                 ],
             },
         });
+    }
+
+    updateChart() {
+        if (!this.chart || !this.props.data) return;
+
+        const labels = Object.keys(this.props.data);
+        const data = Object.values(this.props.data);
+
+
+        this.chart.data.labels = labels;
+        this.chart.data.datasets[0].data = data;
+        this.chart.data.datasets[0].backgroundColor = ["red", "blue", "green"];;
+
+        this.chart.update();
     }
 }
--- awesome_dashboard/static/src/dashboard/dashboard.js
+++ awesome_dashboard/static/src/dashboard/dashboard.js
@@ -6,6 +6,9 @@ import { useService } from "@web/core/utils/hooks";
 import { Layout } from "@web/search/layout";
 import { DashboardItem } from "./DashboardItem/dashboard_item";
 import { PieChart } from "./PieChart/pie_chart";
+import { Dialog } from "@web/core/dialog/dialog";
+import { CheckBox } from "@web/core/checkbox/checkbox";
+import { browser } from "@web/core/browser/browser";
 
 class AwesomeDashboard extends Component {
     static components = { DashboardItem, Layout, PieChart };
@@ -16,6 +19,11 @@ class AwesomeDashboard extends Component {
         this.statisticsService = useService("awesome_dashboard.statistics");
         this.statistics = useState(this.statisticsService.statistics);
         this.items = registry.category("awesome_dashboard").getAll();
+
+        this.dialog = useService("dialog");
+        this.state = useState({
+            disabledItems: browser.localStorage.getItem("disabledDashboardItems")?.split(",") || []
+        });
     }
 
     openCustomers() {
@@ -24,6 +32,51 @@ class AwesomeDashboard extends Component {
 
     openLeads() {
         this.action.doAction("crm.crm_lead_all_leads");
+    }
+
+    openDialog() {
+        this.dialog.add(ConfigurationDialog, {
+            items: this.items,
+            disabledItems: this.state.disabledItems,
+            onUpdateConfiguration: this.updateConfiguration.bind(this),
+        })
+    }
+
+    updateConfiguration(newDisabledItems) {
+        this.state.disabledItems = newDisabledItems;
+    }
+}
+
+class ConfigurationDialog extends Component {
+    static template = "awesome_dashboard.ConfigurationDialog";
+    static components = { Dialog, CheckBox };
+    static props = ["close", "items", "disabledItems", "onUpdateConfiguration"];
+
+    setup() {
+        this.items = useState(this.props.items.map((item) => {
+            return {
+                ...item,
+                enabled: !this.props.disabledItems.includes(item.id),
+            }
+        }));
+    }
+
+    onItemChange(checked, changedItem) {
+        changedItem.enabled = checked; //checked = ev = true or false
+        const newDisabledItems = Object.values(this.items).filter(
+            (item) => !item.enabled         // Keep only disabled items
+        ).map((item) => item.id);          // Extract their IDs
+
+        browser.localStorage.setItem(
+            "disabledDashboardItems",
+            newDisabledItems,
+        );
+
+        this.props.onUpdateConfiguration(newDisabledItems);
+    }
+
+    onApply() {
+        this.props.close();
     }
 }
 
