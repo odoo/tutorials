67c76741c460d0497ca2117a9d0f1f549a5a0f6e
agbr-odoo <agbr@odoo.com>
2025-02-24 19:01:03 +0530

    [ADD] awesome_owl: used card slots, toggling cards and delete list items

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -19,16 +19,42 @@ export class Counter extends Component{
 }
 
 export class Card extends Component{
+    setup(){
+        this.state=useState({open:true});
+    }
+    toggle(){
+        this.state.open=!this.state.open
+        console.log(this.state.open)
+        console.log("TOGGLED")
+
+    }
 static template = "awesome_owl.card"
-static props = ['title','text']
+static props = ['title','slots?']
 }
 
 
 export class TodoItem extends Component{
     static template = "awesome_owl.TodoItem"
-    static props=['todo']
-    
+    static props=['todo', "onChange?","removeTodo?"]
+    setup(){
+      
     }
+    checked(event){
+    if(this.props.onChange){
+        console.log(this.props.todo)
+        this.props.onChange(event.target.id);
+    }
+    }
+    removeTodo(event){
+        console.log("removed function called")
+        console.log(this.props.removeTodo)
+        console.log(event.target)
+       if (this.props.removeTodo){
+        console.log(event.target.id)
+        this.props.removeTodo(event.target.id)
+       }
+    }
+}
 
 export class TodoList extends Component{
     static template = "awesome_owl.TodoList";
@@ -36,7 +62,7 @@ export class TodoList extends Component{
     setup() {
         this.state = useState([]);
         this.inputRef = useRef("input");
-
+        this.removeTodo = this.removeTodo.bind(this)
         onMounted(() => {
             if (this.inputRef.el) {
                 this.inputRef.el.focus();
@@ -52,16 +78,34 @@ export class TodoList extends Component{
             this.state.push({id:this.unique_id,description:event.target.value,isCompleted:false})
             this.unique_id++
             event.target.value=""
+            console.log("Added vals")
 
         }
-    toggleState()
-    {
-        this.state.isCompleted=true;
     
-        }
-        
     }
+    toggleState(todoId){
+        console.log("This is checked!")
+        console.log(todoId)
+        const todo = this.state.find(todo => todo.id == todoId);
+        console.log(todo)
+        if (todo) {
+            todo.isCompleted = !todo.isCompleted
+    }
+
+}
+    removeTodo(todoId){
+        console.log("THIS FUNCTION IS CALLED!!!")
+        const todoIndex = this.state.findIndex(todo => todo.id == todoId);
+        console.log("Todo index:", todoIndex);  // Log the index to verify
     
+        if (todoIndex !== -1) {
+            // Remove the todo from the state array using `splice`
+            this.state.splice(todoIndex, 1);
+            console.log("Todo removed:", this.state);
+        } else {
+            console.error("Todo not found with id:", todoId);
+        }
+    }
     }
     
 
