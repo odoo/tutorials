b75c03bfcce6ae3638f23ec0000ad65c7ce0e03c
Smit Rathod (smra) <smra@odoo.com>
2025-02-24 16:41:53 +0530

    [IMP] awesome_owl: Using markup to display html
    - using t-esc it used to just display the string even if it was html, so replaced with t-out.

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,11 +1,15 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, markup } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
 
+    setup() {
+        this.content_1 = "<div class='text-primary'>some content</div>";
+        this.content_2 = markup("<div class='text-primary'>some content</div>");
+    }
     static components = { Counter, Card };
 }
