a845508ba12a244735ab9fc6907bd88d80336107
pvag-odoo <pvag@odoo.com>
2025-02-24 18:49:20 +0530

    [IMP] awesome_dashboard:  Add navigation buttons to dashboard control panel
    • Add Customers and Leads button linking to actions.
    • Implement action service for navigation.

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,18 +1,33 @@
 /** @odoo-module **/
-
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
-
     static components = { Layout };
 
     setup(){
         this.display={
             controlPanel: {},
         }
+        this.action = useService("action");
+    }
+
+    openCustomerView() {
+        this.action.doAction("base.action_partner_form");
+    }
+
+    openLeadView() {
+        this.action.doAction({
+            type: 'ir.actions.act_window',
+            name: "Lead Management",
+            res_model: 'crm.lead',
+            views: [[false, 'list'],
+                [false, 'form']
+            ],
+        });
     }
 }
 
