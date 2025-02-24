f1a322e1b857077c3d76c5696760fc83e1ac9ad1
bhna-odoo <bhna@odoo.com>
2025-02-24 19:02:21 +0530

    [ADD] awesome_dashboard: added services, layout buttons, dashboard items, rpc

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,10 +1,33 @@
-/** @odoo-module **/
-
-import { Component } from "@odoo/owl";
+import { Component,onWillStart,useState } from "@odoo/owl";
+import { Layout } from "@web/search/layout"
 import { registry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";
+import { DashboardItem } from "./dashboardItem";
+
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
+    static components = {Layout , DashboardItem}
+    setup(){
+    this.action= useService('action')
+    this.statisticsService = useService("awesome_dashboard.statistics");
+    this.stats= {}
+    onWillStart(async () => {
+        this.stats = await this.statisticsService.loadStatistics();
+        console.log(this.statistics)
+    });
+    }
+    openCustomers(){
+        this.action.doAction('base.action_partner_form')
+    }
+    openLeads() {
+        this.action.doAction({
+            type: "ir.actions.act_window",
+            name: ("Leads"),
+            res_model: "crm.lead",
+            target: "current",
+            views: [[false,'list'],[false,'form']]
+        });
+    }
 }
-
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
