21a1327cf0896f62f5235dd4cc12af9c6b20b3ed
Rahul Jha (jhra) <jhra@odoo.com>
2025-02-24 12:35:27 +0530

    [ADD] awesome_owl: Initial commit
    + Added a simple counter

--- awesome_owl/controllers/main.py
+++ awesome_owl/controllers/main.py
@@ -2,7 +2,7 @@ from odoo import http
 from odoo.http import request, route
 
 class OwlPlayground(http.Controller):
-    @http.route(['/awesome_owl'], type='http', auth='public')
+    @http.route(['/awesome-owl'], type='http', auth='public')
     def show_playground(self):
         """
         Renders the owl playground page
--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,7 +1,28 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, markup , useState} from "@odoo/owl";
+
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
+
+    setup() {
+        this.state = useState({
+            value: 0,
+        })
+    }
+
+    increment() {
+        this.state.value++
+    }
+
+    decrement() {
+        this.state.value--
+    }
+
+    reset() {
+        this.state.value = 0
+    }
+    
+
 }
