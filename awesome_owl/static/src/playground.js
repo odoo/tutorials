ac2b6b6412c7b5c384311745aa0bdd5be4f9f07f
bhpr-odoo <bhpr@odoo.com>
2025-02-24 19:09:27 +0530

    [ADD] awesome_owl,  awesome_dashboard:  Implement features of owl framework
    Added  features as per the documentation in awesome_owl and awesome_dashboard,
    including service creation, caching, improved RPC handling, and UI enhancements
    across two chapters.

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,19 +1,22 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
-
+import { Component , markup , useState  } from "@odoo/owl";
+import { Card } from "./card/card";
+import { Counter } from "./counter/counter";
+import { TodoList } from "./todo_list/todo_list";
 export class Playground extends Component {
-    static template = "awesome_owl.playground";
-  
-     static template = xml``
     
-}
-export class Counter extends Component { 
+    static template = "awesome_owl.playground";
 
-    setup() {
-        this.state = useState({ value: 0 });
-    }
-    increment() {
-        this.state.value++;
+    static components = { Card , TodoList , Counter  }
+
+    setup(){
+        this.str1 = "<div class='text-primary'>some content</div>"
+        this.str2 = markup("<div class='text-primary'>some content</div>")
+        this.sum = useState({ value:2 }); 
+    }  
+
+    incrementSum(){
+        this.sum.value++;
     }
 }
