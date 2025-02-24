c4aa7649391091c98e7723ad9a9271358fd29fbb
dvpa-odoo <dvpa@odoo.com>
2025-02-24 19:00:53 +0530

    [ADD] awesome_dashboard: added services, layout buttons, dashboard items, rpc

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,10 +1,40 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, onWillStart, useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
+import { rpc } from '@web/core/network/rpc';
+import { DashboardItem } from "./dashboard_item";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
+    static components = { Layout, DashboardItem };
+
+    setup(){
+        this.action = useService("action");
+        this.stats
+
+        onWillStart(async () => {
+            this.stats = await rpc("/awesome_dashboard/statistics");
+        })
+    }
+
+    openCustomers() {
+        this.action.doAction("base.action_partner_form")
+    }
+
+    openLeads() {
+        this.action.doAction({
+            type: "ir.actions.act_window",
+            name: "Leads",
+            res_model: "crm.lead",
+            target: "current",
+            views: [[false,"list"],[false,"form"]],
+        });
+    }
+
+
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
