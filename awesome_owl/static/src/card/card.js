185e087e8259d835090449d1b34e411f29825398
pssh-odoo <pssh@odoo.com>
2025-02-24 17:47:16 +0530

    [IMP] awesome_owl: added props validation & parent-child communication
    • Added props validation to `Card` and `Counter` to enforce expected prop types.
    • Implemented callback props in `Counter` to notify `Playground`.
    • Updated `Playground` to track and display the sum of multiple `Counter` values.

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -2,4 +2,5 @@ import { Component } from "@odoo/owl"
 
 export class Card extends Component {
     static template = "awesome_owl.Card";
+    static props = { title: { type: String },content: { type: String },};
 }
