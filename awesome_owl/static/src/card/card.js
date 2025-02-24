8c4851bc9ed5951f3830268fba31705b7defc95a
Rahul Jha (jhra) <jhra@odoo.com>
2025-02-24 14:18:51 +0530

    [IMP] awesome_owl: Added props validation in card component

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -4,7 +4,10 @@ import { Component} from "@odoo/owl";
 
 export class Card extends Component {
     static template = "awesome_owl.card";
-    
+    static props = {
+        title: { type: String, optional: true },
+        content: { type: String, optional: true },
+    }
     setup() {
         this.title = this.props.title;
         this.content = this.props.content;
