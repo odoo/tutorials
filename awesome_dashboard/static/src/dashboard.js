c3aabc005c447e1e2de024d703636c3fc4039ff0
khaj-odoo <khaj@odoo.com>
2025-02-24 14:11:02 +0530

    [IMP] awesome_dashboard:  Added navigation buttons for Customers and Leads
    - Added Customers button that triggers the existing customer kanban view.
    - Added Leads button that opens a dynamic action for crm.lead (list and form
      view).

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -3,16 +3,32 @@
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
-
+import { useService } from "@web/core/utils/hooks";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
     static components = { Layout };
 
     setup() {
+        this.action = useService("action");
+
         this.display = {
             controlPanel: {},
         };
+    }
+
+    openCustomers() {
+        this.action.doAction("base.action_partner_form");
+    }
+
+    openLeads() {
+        this.action.doAction({
+            name: "Leads",
+            type: "ir.actions.act_window",
+            res_model: "crm.lead",
+            views: [[false, "list"], [false, "form"]],
+            target: "current",
+        });
     }
 }
 
