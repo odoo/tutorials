75350b11b96c0a0e35fd74ba5923e9e508e0d172
tpma-odoo <tpma@odoo.com>
2025-02-24 19:21:09 +0530

    [IMP] awesome_owl: added hide/show button functionality and generic component

--- awesome_owl/static/src/card.js
+++ awesome_owl/static/src/card.js
@@ -1,13 +1,22 @@
-import { Component, markup } from "@odoo/owl";
+import { Component, markup, useState } from "@odoo/owl";
 
 export class Card extends Component {
     static template = "awesome_owl.card_template";
     static props = {
         title: { type: String },
         content: { type: String },
+        slots: { type: Object, optional: true }
     };
+
+    setup() {
+        this.isOpen = useState({ value: true });
+    }
 
     get safeContent() {
         return markup(this.props.content);
+    }
+
+    toggleContent() {
+        this.isOpen.value = !this.isOpen.value;
     }
 }
