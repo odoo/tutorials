655c35b93e80b4fa83e5db20ad883d59df78385f
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2025-02-24 16:05:41 +0100

    [IMP] awesome_owl: create Counters and Cards
    create a reusable Counter component to be able to create as many separate counters as needed.
    create a Card component and use props to pass values through the parent component Playground. use t-out and markup to show the difference between escaped content and non escaped content.
    
    chapter-1-part-4

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,7 +1,16 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, markup } from '@odoo/owl';
+import { Counter } from './counter/counter';
+import { Card } from './card/card';
 
 export class Playground extends Component {
-    static template = "awesome_owl.playground";
+    static template = 'awesome_owl.playground';
+    static components = { Counter, Card };
+    static props = {};
+
+    setup() {
+        this.escapedValue = "Hello, <b>world</b>!";
+        this.notEscapedValue = markup("Hello, <b>world</b>!");
+    }
 }
