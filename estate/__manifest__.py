a99065e69d69b33fe8f3afc4566eb4cefb5a7b95
akus-odoo <akus@odoo.com>
2025-02-25 17:19:19 +0530

    [IMP] estate:  Add master data and demo data for properties and offers.
    Define module data:
    - Added master data for property types (Residential, Commercial, Industrial,
    Land) in csv format.
    - Created demo properties (Big Villa, Trailer Home) with relevant details in xml
    file.
    - Added demo offers linked to properties using existing partners using the ref
    key.
    - Ensured demo properties are set to Residential property type.
    - Understand eval to set dynamic dates for offers.
    - Included a validated offer using the action_accept function.
    - Created a new property with One2many offers directly defined.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,14 +4,19 @@
     'author': "Ayush Kumar Singh",
     'application': True,
     'data': [
-        'security/ir.model.access.csv',
         'security/security.xml',
+        'security/ir.model.access.csv',
+        'data/estate.property.type.csv',
         'views/estate_property_offer_views.xml',
         'views/estate_property_views.xml',
         'views/estate_property_type_views.xml',
         'views/estate_property_tag_views.xml',
         'views/res_users_views.xml', 
         'views/estate_menus.xml',
+    ],
+    'demo': [
+        'demo/estate_property_demo.xml',
+        'demo/estate_offers_demo.xml',
     ],
     'category': 'Real Estate/Brokerage',
     'license': 'LGPL-3'                                   # default LGPL-3 but if not specified then we get warning in terminal 
