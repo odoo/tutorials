7bbd43193bf770e49e7a7a8195948d804733833e
khaj-odoo <khaj@odoo.com>
2025-02-25 10:15:41 +0530

    [IMP] awesome_dashboard:  Add PieChart component to dashboard
    - Added PieChart component to display t-shirt sales distribution by size
      (S/M/L/XL/XXL) using Chart.js.
    - Implemented lazy loading of Chart.js with loadJs in onWillStart for
      improved performance.

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -5,10 +5,11 @@ import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
 import { DashboardItem } from "./dashboard_item";
 import { useService } from "@web/core/utils/hooks";
+import { PieChart } from "./pie_chart/pie_chart";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
-    static components = { Layout, DashboardItem };
+    static components = { Layout, DashboardItem, PieChart };
 
     setup() {
         this.action = this.env.services.action;
