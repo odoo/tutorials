9d7007521c987da92c33440af6a23b2614bccaec
dtda-odoo <dtda@odoo.com>
2025-02-25 10:31:29 +0530

    [ADD] awesome_owl: add markup to card component

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -5,16 +5,18 @@ export class Card extends Component {
   static props = {
     title: {
       type: String,
+      // default wont work when optional is not set
       default: "My Title",
     },
     content: {
       type: String,
+      optional: true,
       default: "my card content",
     },
   };
 
   /*
-  use of state is not needed here as we are passing static values inside props, this can be used where values are inserted by user
+  use of state is not needed here as we are passing static values inside props, this can be used where values are inserted by user with onchange attribute
   */
 
   // setup() {
--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,10 +1,14 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, markup } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
 
 export class Playground extends Component {
   static template = "awesome_owl.playground";
   static components = { Counter, Card };
+  setup() {
+    this.str1 = "<div class='text-uppercase'>Hello World</div>";
+    this.str2 = markup("<div class='text-uppercase'>Hello World</div>");
+  }
 }
