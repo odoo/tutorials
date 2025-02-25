54c512137d4d1d4e74d9062e33c2cf58ef8b5059
Smit Rathod (smra) <smra@odoo.com>
2025-02-25 14:54:09 +0530

    [IMP] awesome_owl:  Allowing the user to add a todo to the list.
    - No more hardcoded todos, users can add todos.
    - Added the event handler on keyup.
    - Added function addTodos which detects if enter is pressed and pushs the user's todo in todos object.

--- awesome_owl/static/src/todolist/todolist.js
+++ awesome_owl/static/src/todolist/todolist.js
@@ -6,10 +6,18 @@ export class ToDoList extends Component {
     static components = { ToDoItem };
 
     setup() {
-        this.todos = useState([
-            { id: 1, description: "buy milk", isCompleted: true },
-            { id: 2, description: "buy chocolate", isCompleted: true },
-            { id: 3, description: "sell both", isCompleted: false },
-        ])
+        this.ids = 1;
+        this.todos = useState([]);
+    }
+
+    addTodo(ev) {
+        if(ev.keyCode === 13 && ev.target.value != ''){
+            this.todos.push({
+                id: this.ids++,
+                description: ev.target.value,
+                isCompleted: false,
+            });
+            ev.target.value = "";
+        }
     }
 }
