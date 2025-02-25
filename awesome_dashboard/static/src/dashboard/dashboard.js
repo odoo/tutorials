32e0ebe15fc43f1408940c2a350f187d296c5698
nmis-odoo <nmis@odoo.com>
2025-02-25 19:04:54 +0530

    [IMP] awesome_dashboard:  Add settings panel to customize dashboard items
    - Added a settings button with a gear icon in the control panel to open a
    configuration dialog.
    - Implemented a dialog displaying all existing dashboard items with checkboxes.
    - Added an Apply button to capture unchecked item IDs and store them in
    local storage.
    - Modified the `Dashboard` component to filter out items based on stored
    configuration.
    - Enhanced user control over dashboard customization for a more personalized
    experience.

--- awesome_dashboard/static/src/dashboard/dashboard.js
+++ awesome_dashboard/static/src/dashboard/dashboard.js
@@ -4,6 +4,9 @@ import { Layout } from "@web/search/layout";
 import { useService } from "@web/core/utils/hooks";
 import {DashboardItem} from "./dashboard_item/dashboard_item";
 import { PieChart } from "./pie_chart/pie_chart";
+import { Dialog } from "@web/core/dialog/dialog";
+import { CheckBox } from "@web/core/checkbox/checkbox";
+import { browser } from "@web/core/browser/browser";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
@@ -12,10 +15,26 @@ class AwesomeDashboard extends Component {
     setup() {
         this.action = useService("action");
         this.statistics = useState(useService("awesome_dashboard.statistics"));
+        this.dialog = useService("dialog");
         this.display = {
             controlPanel: {},
         };
         this.items = registry.category("awesome_dashboard").getAll();
+        this.state = useState({
+            disabledItems: browser.localStorage.getItem("disabledDashboardItems")?.split(",") || []
+        });
+    }
+
+    updateConfiguration(newDisabledItems) {
+        this.state.disabledItems = newDisabledItems;
+    }
+
+    openConfiguration() {
+        this.dialog.add(ConfigurationDialog, {
+            items: this.items,
+            disabledItems: this.state.disabledItems,
+            onUpdateConfiguration: this.updateConfiguration.bind(this),
+        })
     }
 
     openCustomerView() {
@@ -32,6 +51,40 @@ class AwesomeDashboard extends Component {
                 [false, "form"],
             ],
         });
+    }
+
+}
+
+class ConfigurationDialog extends Component {
+    static template = "awesome_dashboard.ConfigurationDialog";
+    static components = { Dialog, CheckBox };
+    static props = ["close", "items", "disabledItems", "onUpdateConfiguration"];
+
+    setup() {
+        this.items = useState(this.props.items.map((item) => {
+            return {
+                ...item,
+                enabled: !this.props.disabledItems.includes(item.id),
+            }
+        }));
+    }
+
+    done() {
+        this.props.close();
+    }
+
+    onChange(checked, changedItem) {
+        changedItem.enabled = checked;
+        const newDisabledItems = Object.values(this.items).filter(
+            (item) => !item.enabled
+        ).map((item) => item.id)
+
+        browser.localStorage.setItem(
+            "disabledDashboardItems",
+            newDisabledItems,
+        );
+
+        this.props.onUpdateConfiguration(newDisabledItems);
     }
 
 }
