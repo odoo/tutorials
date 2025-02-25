fe7df59d05c61f05689bdaeaacca09842d825cec
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2025-02-25 16:56:09 +0100

    [IMP] awesome_owl: enable dynamic card content
    change the Card component to be able to accept dynamic content as default slot props. add prop validation to the Card.
    
    chapter-1-part-13

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -3,7 +3,13 @@ import { Component } from '@odoo/owl';
 export class Card extends Component {
     static template = 'awesome_owl.Card';
     static props = {
-        "title": String,
-        "content": String,
+        title: {
+            type: String,
+            optional: false,
+        },
+        slots: {
+            type: Object,
+            optional: true,
+        }
     }
 }
