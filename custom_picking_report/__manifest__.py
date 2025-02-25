ecc0d1f04adb2304884c506156380abbbc9ab41f
pdso-odoo <pdso@odoo.com>
2025-02-25 18:50:33 +0530

    [ADD] custom_picking_report: show missing fields on all Picking PDF pages
     - Ensured name, origin, state, and scheduled date appear on every page.
     - Improved report readability.
    
    Task name: Picking operation PDF report

---
+++ custom_picking_report/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Stock Picking Report",
+    'version': "1.0",
+    'depends': ["stock"],
+    'author': "Prathmesh Soni (pdso)",
+    'category': "task/custom_picking_report",
+    'summary': "Enhances Picking Operations PDF report",
+    'description': "An app for enhancing the Picking Operations PDF report.",
+    'data': [
+        "report/custom_report_picking_template.xml",
+    ],
+    'sequence': 2,
+    'installable': True,
+    'license': "LGPL-3",
+}
