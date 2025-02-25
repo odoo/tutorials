c67127d2660f0037bf7a6698271fd817a4e75e09
vishw <vpjo@odoo.com>
2025-02-25 10:53:30 +0530

    [ADD] add_pricelist_price: added book_price field to Sale Order and Invoice line
    This commit adds a new computed field book_price on
    sale.order.line and account.move.line,
    displaying the price based on the applied pricelist.
    The price is computed directly from the sale order’s pricelist for
    sale order lines, and from the linked sale order
    (via sale_line_ids) for invoice lines.
    
    This ensures correct pricing display, avoids
    unnecessary searches,and improves performance.
    If no pricelist is available, the product’s
    lst_price is used as a fallback.
    
    The field is also added to the views of
    both sale order and invoice.
    
    This improves visibility of the applied prices for better transparency
    in the sales and invoicing process.
    
    task-4599399

---
+++ add_pricelist_price/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Add Pricelist Price",
+    "description": """
+        Add Pricelist Price
+    """,
+    "depends": ["sale_management"],
+    "data": [
+        "views/account_move_line_view.xml",
+        "views/sale_order_line_view.xml",
+    ],
+    "installable": True,
+    "license": "LGPL-3",
+}
