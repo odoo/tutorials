e4258ab116f55901d7a5b0650a0630712a742f90
siha-odoo <siha@odoo.com>
2025-02-25 16:21:09 +0530

    [IMP] supplier_portal:  added supplier portal
    -Introduced a new route allowing suppliers to upload invoices
     in PDF and XML format.
    -Added a webpage with a company selection dropdown, displaying companies
     linked to the user.
    -Successfully uploaded documents will generate a new draft vendor bill
     in the backend.

---
+++ supplier_portal/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Supplier Portal",
+    "version": "1.0",
+    "author": "Harsh Siddhapara siha",
+    "depends": ["account", "website"],
+    "data": [
+        "views/supplier_portal_template.xml",
+        "views/supplier_portal_website_menu.xml",
+    ],
+    "installable": True,
+    "license": "LGPL-3",
+}
