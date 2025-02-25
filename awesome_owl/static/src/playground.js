74a41f0df4f4ebe8eccda3da6b4e00587e3136a5
bhna-odoo <bhna@odoo.com>
2025-02-25 18:57:36 +0530

    [IMP] awesome_owl: organized file structures

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,9 +1,9 @@
 /** @odoo-module **/
 
 import { Component , useState , markup} from "@odoo/owl";
-import { Counter } from "./counter";
-import { Card } from "./card"; 
-import { TodoList } from "./TodoList";
+import { Counter } from "./counter/counter";
+import { Card } from "./card/card"; 
+import { TodoList } from "./todo/TodoList";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
