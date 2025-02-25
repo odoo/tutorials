0df641a973f8ca28d92036510f6135b00211f851
nmis-odoo <nmis@odoo.com>
2025-02-25 14:14:19 +0530

    [IMP] awesome_dashboard:  Implementing lazy loading for dashboard
    - Moved all dashboard-related assets to a new /dashboard subfolder for better
      modularity.
    - Created awesome_dashboard.dashboard asset bundle containing all content from
      the /dashboard folder.
    - Modified dashboard.js to register itself with the lazy_components registry
      instead of actions for efficient loading.

--- awesome_dashboard/static/src/dashboard/dashboard.js
+++ awesome_dashboard/static/src/dashboard/dashboard.js
@@ -1,4 +1,4 @@
-import { Component , onWillStart , useState } from "@odoo/owl";
+import { Component , useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
 import { useService } from "@web/core/utils/hooks";
@@ -35,4 +35,4 @@ class AwesomeDashboard extends Component {
 
 }
 
-registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
+registry.category("lazy_components").add("AwesomeDashboard", AwesomeDashboard);
