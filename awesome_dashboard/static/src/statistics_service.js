bbd35da978f6ea9f94870269fce4157ce799ffec
dvsh-odoo <dvsh@odoo.com>
2025-02-26 11:23:32 +0530

    [IMP] awesome_dashboard:  Cache Network Calls with a Custom Service
    - This will ensure caching of statistic data.
    - Statistical data is now reactive across components.

---
+++ awesome_dashboard/static/src/statistics_service.js
@@ -0,0 +1,14 @@
+import { registry } from "@web/core/registry";
+import { rpc } from "@web/core/network/rpc";
+import { memoize } from "@web/core/utils/functions";
+
+const statisticsService = {
+    async: ["loadStatistics"],
+    start() {
+        return {
+            loadStatistics: memoize(() => rpc("/awesome_dashboard/statistics")),
+        };
+    },
+};
+
+registry.category("services").add("awesome_dashboard.statistics", statisticsService);
