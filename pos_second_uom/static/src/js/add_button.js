51ea224fa934689033155074740a9cf9d1516ea7
ptpu-odoo <ptpu@odoo.com>
2025-02-26 10:49:20 +0530

    [IMP] pos:  Added second UOM in POS
    With this Commit
    ================
    
    - Added a Second UoM Field: Introduced a Many2one field in the product.template
      model for selecting a second unit of measure (UoM).
    - Custom Button Implementation: Added an "Add Quantity" button in the
      POS interface.
    - Dialog for Quantity Input: Clicking the button opens a dialog where
      users can enter the quantity in an alternative UoM.
    - Category Constraint: The second UoM must belong to the same category as the
      primary UoM to ensure compatibility.
    - Quantity Adjustment: Upon confirmation, the selected product’s quantity is
      recalculated based on the primary UoM.

--- pos_second_uom/static/src/js/add_button.js
+++ pos_second_uom/static/src/js/add_button.js
@@ -6,38 +6,49 @@ import { Dialog } from "@web/core/dialog/dialog";
 import { Component, onWillStart, useState, useEffect } from "@odoo/owl";
 import { usePos } from "@point_of_sale/app/store/pos_hook";
 
+
 class ConfigurationDialog extends Component {
   static template = "pos_second_uom.ConfigurationDialog";
   static components = { Dialog };
 
   setup() {
     this.orm = this.env.services.orm;
+    
     this.state = useState({
       quantity: 1,
       secUomId: null,
-      factor: null,
+      uomType: null,
+      secFactor: null,
       secUoms: [],
+      secFactors: [],
+      secUomType: null,
+      factor: null,
       factors: [],
-      uomType: null
     });
     this.pos = usePos();
     this.categId = this.props.categId;
     this.uomId = this.props.uomId;
-    this.productId = this.props.productId
+    this.productId = this.props.productId;
 
     onWillStart(async () => {
+      //Find second UOM ID
       this.secondUomId = await this.orm.searchRead(
         "product.template",
-        [
-          ["id", "=", this.productId],
-        ],
-        ["id", "sec_uom_id"]
+        [["id", "=", this.productId]],
+        ["sec_uom_id"]
       );
-      console.log(this.secondUomId[0].sec_uom_id[0])
+      if (!this.secondUomId[0].sec_uom_id) return;
+      //Find second Category ID from product template
+      this.categoryId = await this.orm.searchRead(
+        "uom.uom",
+        [["id", "=", this.secondUomId[0].sec_uom_id[0]]],
+        ["category_id"]
+      );
+      //Find all the UOM id for the same category
       this.state.secUoms = await this.orm.searchRead(
         "uom.uom",
         [
-          ["category_id", "=", this.categId],
+          ["category_id", "=", this.categoryId[0].category_id[0]],
           ["id", "!=", this.uomId],
         ],
         ["id", "name"]
@@ -47,33 +58,48 @@ class ConfigurationDialog extends Component {
       }
     });
   }
-
   updateQuantity(event) {
     this.state.quantity = event.target.value;
   }
-
   updateUom(event) {
     this.state.secUomId = parseInt(event.target.value);
   }
 
   async done() {
+    //Find Second UOM Factor
+    this.state.secFactors = await this.orm.searchRead(
+      "uom.uom",
+      [["id", "=", this.state.secUomId]],
+      ["factor"]
+    );
+    //Find First UOM Factor
     this.state.factors = await this.orm.searchRead(
       "uom.uom",
-      [["id", "=", this.state.secUomId]],
-      ["id", "factor"]
+      [["id", "=", this.uomId]],
+      ["factor"]
     );
-    this.state.uomType = await this.orm.searchRead(
+    //Find Second UOM Type
+    this.state.secUomType = await this.orm.searchRead(
       "uom.uom",
       [["id", "=", this.state.secUomId]],
-      ["id", "uom_type"]
+      ["uom_type"]
     );
-    console.log(this.state.secUomId)
+    //Find First UOM Type
+    this.state.uomType = await this.orm.searchRead(
+      "uom.uom",
+      [["id", "=", this.uomId]],
+      ["uom_type"]
+    );
     this.props.onConfirm(
       this.state.quantity,
-      this.state.secUomId,
+      this.state.uomType[0].uom_type,
+      this.state.secFactors[0].factor,
       this.state.factors[0].factor,
-      this.state.uomType[0].uom_type
+      this.state.secUomType[0].uom_type
     );
+    this.props.close();
+  }
+  cancel(){
     this.props.close();
   }
 }
@@ -84,52 +110,62 @@ patch(ControlButtons.prototype, {
     this.pos = usePos();
     this.orm = this.env.services.orm;
     this.dialog = this.env.services.dialog;
-    this.r = useState({ value: [] });
+    this.length = useState({ value: 0 });
+    this.showCustomButton = useState({value: false})
     useEffect(
       () => {
-        this.loadSecUomId();
+        this.getProductIdsFromOrderlines();
       },
-      () => [this.pos.get_order().get_selected_orderline().id]
+      () => [this.pos.get_order().get_selected_orderline()?.id]
     );
   },
-  getProductIdsFromOrderlines() {
+  async getProductIdsFromOrderlines() {
     const order = this.pos.get_order();
-    const selectedOrder = order.get_selected_orderline().id;
-    const currentOrder = order.lines.filter((id) => id.id == selectedOrder);
-    this.categId = currentOrder[0].product_id.categ_id.id;
-    this.uomId = currentOrder[0].product_id.uom_id.id;
-    this.productId = currentOrder[0].product_id._raw.product_tmpl_id;
-    console.log(currentOrder[0])
-    if (!order) return [];
-    return order.lines.map((line) => line.product_id.id);
-  },
-  async loadSecUomId() {
-    const productIds = this.getProductIdsFromOrderlines();
-    if (productIds.length === 0) {
-      this.r.values = []; // No products in orderline
-      return;
+    if (order.lines.length > 0) {
+      const selectedOrder = order.get_selected_orderline().id;
+      const currentOrder = order.lines.filter((id) => id.id == selectedOrder);
+      this.categId = currentOrder[0].product_id.categ_id.id;
+      this.uomId = currentOrder[0].product_id.uom_id.id;
+      this.productId = currentOrder[0].product_id._raw.product_tmpl_id;
+      const productData = await this.orm.searchRead(
+        "product.template",
+        [["id", "=", this.productId]],
+        ["sec_uom_id"]
+      );
+      if (productData.length > 0 && productData[0].sec_uom_id) {
+        this.showCustomButton.value = true;
+      } else {
+        this.showCustomButton.value = false;
+      }
+      if (!order) return [];
+      return order.lines.map((line) => line.product_id.id);
+      
     }
-    this.r.values = await this.orm.searchRead(
-      "product.template",
-      [["id", "in", productIds]],
-      ["uom_id", "sec_uom_id"]
-    );
   },
   clickCustomButton() {
     this.dialog.add(ConfigurationDialog, {
       categId: this.categId,
       uomId: this.uomId,
       productId: this.productId,
-      onConfirm: (quantity, secUomId, factor, uomType) =>
-        this.handleDialogConfirm(quantity, secUomId, factor, uomType),
+      onConfirm: (quantity, uomType, secFactor, factor, secUomType) =>
+        this.handleDialogConfirm(
+          quantity,
+          uomType,
+          secFactor,
+          factor,
+          secUomType
+        ),
     });
   },
-  handleDialogConfirm(quantity, secUomId, factor, uomType) {
+  handleDialogConfirm(quantity, uomType, secFactor, factor, secUomType) {
     const orderline = this.currentOrder.get_selected_orderline();
-    console.log(factor)
-    if(uomType === "smaller")
-      orderline.set_quantity(quantity / factor);
-    else
-      orderline.set_quantity(quantity * factor);
+    if (secUomType === "bigger") {
+      orderline.set_quantity(quantity * secFactor);
+    } else {
+      if (uomType === "bigger") {
+        orderline.set_quantity(quantity * factor);
+      }
+      if (uomType === "reference") orderline.set_quantity(quantity / secFactor);
+    }
   },
 });
