e60489117a319f27367c7f7ffa7f7f886252fecf
gvar-odoo <codenoko@gmail.com>
2025-02-26 09:00:23 +0100

    [IMP] owl chap 1.4

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,10 +1,11 @@
-import { Component } from "@odoo/owl";
+import { Component, markup } from "@odoo/owl";
 import { Counter } from "./counter";
 import { Card } from "./card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
 
+    html = markup("<div class='text-primary'>some content</div>")
     static components = { Counter, Card }
     static props = {};
 }
