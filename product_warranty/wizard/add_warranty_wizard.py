70baa65b615b09675432961aceb336d66d25037e
vrgo-odoo <vrgo@odoo.com>
2025-02-26 15:46:27 +0530

    [IMP] product_warranty: refactor code
    In this commit
    1. removed creation of warranty in for loop to single create for all lines.
    removed extra variables.
    
    2.removed compute instead using related field

--- product_warranty/wizard/add_warranty_wizard.py
+++ product_warranty/wizard/add_warranty_wizard.py
@@ -14,35 +14,32 @@ class AddWarrantyWizard(models.TransientModel):
     def default_get(self, fields_list):
         res = super().default_get(fields_list)
         sale_order = self.env["sale.order"].browse(self.env.context.get("active_id"))
-        warranty_lines = []
-        for line in sale_order.order_line.filtered(
-            lambda line: line.product_template_id
-            and line.product_template_id.is_warranty
-        ):
-            warranty_lines.append(
+        res.update({
+            'sale_order': sale_order.id,
+            'warranty_lines': [
                 Command.create(
                     {
                         "sale_order_line": line.id,
                         "warranty_config": False,
-                    }
+                    } for line in sale_order.order_line.filtered(lambda line: 
+                        line.product_template_id and line.product_template_id.is_warranty
+                    )
                 )
-            )
-        res["sale_order"] = sale_order.id
-        res["warranty_lines"] = warranty_lines
-        return res 
+            ]
+        })
+        return res
    
     def apply_warranty(self):
-        for line in self.warranty_lines:
-            product = self.env["product.template"].browse(line.product_template.id)
-            if line.warranty_config:
-                self.env["sale.order.line"].create(
-                    {
-                        "order_id": self.sale_order.id,
-                        "name": "Extended Warranty of %d Years - %s"
-                        % (line.warranty_config.years, product.name),
-                        "product_id": self.env.ref('product_warranty.extended_warranty_product_tmpl').id,
-                        "product_uom_qty": line.sale_order_line.product_uom_qty,
-                        "linked_line_id": line.sale_order_line.id,
-                        "price_unit": (line.warranty_config.percentage / 100) * line.sale_order_line.price_unit,
-                    }
-                )
+        extended_warranty_product = self.env.ref("product_warranty.extended_warranty_product_tmpl").id
+        warranty_line_vals = [
+            {
+                "order_id": self.sale_order.id,
+                "product_id": extended_warranty_product,
+                "product_uom_qty": line.sale_order_line.product_uom_qty,
+                "linked_line_id": line.sale_order_line.id,
+                "price_unit": (line.warranty_config.percentage / 100) * line.sale_order_line.price_unit,
+            }
+            for line in self.warranty_lines if line.warranty_config
+        ]
+        if warranty_line_vals:
+            self.env["sale.order.line"].create(warranty_line_vals)
--- product_warranty/wizard/warranty_wizard_line.py
+++ product_warranty/wizard/warranty_wizard_line.py
@@ -11,18 +11,10 @@ class WarrantyWizardLine(models.TransientModel):
     wizard_id = fields.Many2one(comodel_name='add.warranty.wizard', string="Wizard", required=True)
     sale_order_line = fields.Many2one(comodel_name='sale.order.line', string="Order Line", required=True)
     product_template = fields.Many2one(
-        comodel_name='product.template', string="Product", compute='_compute_product_template'
+        string="Product", related='sale_order_line.product_template_id',
     )
     warranty_config = fields.Many2one(comodel_name='product.warranty.config', string="Warranty Config")
     date_end = fields.Date(string="End Date", compute='_compute_date_end')
-
-    @api.depends('sale_order_line')
-    def _compute_product_template(self):
-        for line in self:
-            if line.sale_order_line:
-                line.product_template = line.sale_order_line.product_template_id
-            else:
-                line.product_template = False
 
     @api.depends('warranty_config')
     def _compute_date_end(self):
