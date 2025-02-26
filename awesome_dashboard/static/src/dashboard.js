b74064f3ea6f8219b3624eb9136832578c9cbfa0
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2025-02-26 15:44:58 +0100

    [IMP] awesome_dashboard: create dashboard stats
    create dashboard and dashboard item components. use service and rpc to show customers, leads, and call the server to get stats data. use slots to enable defining the dashboard item dynamically.
    
    chapter-2-part-4

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,10 +1,46 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
-import { registry } from "@web/core/registry";
+import { Component, onWillStart, useState } from '@odoo/owl';
+import { registry } from '@web/core/registry';
+import { Layout } from '@web/search/layout';
+import { useService } from '@web/core/utils/hooks';
+import { DashboardItem } from './dashboard_item/dashboard_item';
+import { rpc } from '@web/core/network/rpc';
 
 class AwesomeDashboard extends Component {
-    static template = "awesome_dashboard.AwesomeDashboard";
+    static template = 'awesome_dashboard.AwesomeDashboard';
+    static components = { Layout, DashboardItem };
+
+    setup() {
+        this.showCustomers = this.showCustomers.bind(this);
+        this.showLeads = this.showLeads.bind(this);
+        this.action = useService("action");
+        this.stats = useState({});
+        onWillStart(async () => {
+            const result = await rpc('/awesome_dashboard/statistics', {});
+            this.stats = { ...result };
+            console.log(this.stats);
+        });
+    }
+
+    showCustomers() {
+        this.action.doAction('base.action_partner_form');
+    }
+
+    showLeads() {
+        this.action.doAction({
+            type: 'ir.actions.act_window',
+            name: 'Leads',
+            target: 'current',
+            res_model: 'crm.lead',
+            views: [
+                [false, 'list'],
+                [false, 'form'],
+            ],
+        });
+    }
 }
 
-registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
+registry
+    .category('actions')
+    .add('awesome_dashboard.dashboard', AwesomeDashboard);
---
+++ awesome_dashboard/static/src/dashboard_item/dashboard_item.js
@@ -0,0 +1,14 @@
+import { Component } from '@odoo/owl';
+
+export class DashboardItem extends Component {
+    static template = "awesome_dashboard.DashboardItem";
+    static props = {
+        size: {
+            type: Number,
+            optional: true,
+        }
+    };
+    static defaultProps = {
+        size: 1,
+    }
+}
