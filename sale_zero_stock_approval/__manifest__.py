3c74d8163b2f5f3c0cef0298090d1f6f1ef9916b
smjo-odoo <smjo@odoo.com>
2025-02-26 18:43:46 +0530

    [FIX] sale: restrict SO confirmation for sale users without admin approval -A sale user was able to confirm a Sales Order (SO) without admin permission.  Since users cannot access SOs of other users/admins, they should also  require admin approval to confirm their own SOs. -Now, the Confirm Order button is only shown if the required approval  field zero_stock_approval is enabled.

--- sale_zero_stock_approval/__manifest__.py
+++ sale_zero_stock_approval/__manifest__.py
@@ -2,11 +2,9 @@
     'name': 'Sale Zero Stock Approval',
     'version': '1.0',
     'summary': 'Allows approval of zero stock sales orders',
-    'author': 'smjo-odoo',
     'depends': ['sale_management', 'stock'],
     'data': ['views/sale_order_view.xml',],
     'installable': True,
-    'application': False,
     'auto_install': True,
     'license': 'LGPL-3'
 }
--- sale_zero_stock_approval/models/sale_order.py
+++ sale_zero_stock_approval/models/sale_order.py
@@ -4,10 +4,9 @@ from odoo.exceptions import UserError
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    zero_stock_approval = fields.Boolean(string="Zero Stock Approval", default=False, help="If True, the sales manager can confirm the sale order even if stock is unavailable.")
+    zero_stock_approval = fields.Boolean(string="Zero Stock Approval", default=False, help="Allow sale users to confirm the SO & avoid stock blockage.")
     is_manager = fields.Boolean(compute="_compute_is_manager")
 
-    @api.depends()
     def _compute_is_manager(self):
         for record in self:
             record.is_manager = self.env.user.has_group('sales_team.group_sale_manager')
@@ -15,6 +14,8 @@ class SaleOrder(models.Model):
     #override the action_confirm method in sale.order model
     def action_confirm(self):
         for order in self:
+            if not order.zero_stock_approval and not order.is_manager:
+                raise UserError("Cannot confirm Sale Order without Admin permission")
             if not order.zero_stock_approval and any(line.product_id.qty_available <= 0 for line in order.order_line):
                 raise UserError("Cannot confirm Sale Order because one or more products have 0 available quantity.")
         return super().action_confirm()
