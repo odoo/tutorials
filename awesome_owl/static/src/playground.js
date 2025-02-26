72baa1fa82b405c969ae450e102b98ddeee371b7
Smit Rathod (smra) <smra@odoo.com>
2025-02-26 11:46:12 +0530

    [IMP] awesome_owl: Generic Card with slots
    - Used the default slot to define the body(content of card).
    - Added props validation for slots.

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -2,9 +2,11 @@
 
 import { Component } from "@odoo/owl";
 import { ToDoList } from "./todolist/todolist";
+import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
 
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
-    static components = { ToDoList };
+    static components = { Card, Counter, ToDoList };
 }
