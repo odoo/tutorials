c61a8b784d1f77974c772486db24ad2a552a174b
Smit Rathod (smra) <smra@odoo.com>
2025-02-26 15:33:33 +0530

    [IMP] awesome_dashboard: Added buttons for quick navigation
    - Added Customers and Leads buttons for quick navigation using action.

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -3,16 +3,30 @@
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
-
+import { useService } from "@web/core/utils/hooks";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
     static components = { Layout }
 
     setup() {
+        this.action = useService("action");
         this.display = {
-            controlPanel : {},
+            controlPanel: {},
         };
+    }
+
+    openCustomers() {
+        this.action.doAction("base.action_partner_form");
+    }   
+
+    openLeads() {
+        this.action.doAction({
+            type: "ir.actions.act_window",
+            name: "Leads",
+            res_model: "crm.lead",
+            views: [[false ,"form"],[false ,"list"]],
+        });
     }
 }
 
