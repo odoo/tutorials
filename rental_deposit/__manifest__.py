316efb28de1169fe5e40a59d4d4c28bc5f3f0a25
ppch-odoo <ppch@odoo.com>
2025-02-26 19:11:01 +0530

    [IMP] rental_deposit: added deposit in rental config settings
    added deposit product in config settings by inheriting res_config_settings model
    added require deposit and deposit amount in product template by inhering
    product_template model
    added inherited view for template as well as config settings
    also inherited view for product template
    
    task - 4605761

---
+++ rental_deposit/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Rental Deposit",
+    'version': '1.0',
+    'depends': ['website_sale_renting'],
+    'author': "ppch",
+    'category': 'Category',
+    'description': """
+    """,'license': "LGPL-3",
+    'data': [
+        'views/res_config_settings_views.xml',
+        'views/product_template_views.xml',
+    ],
+    'installable': True,
+}
