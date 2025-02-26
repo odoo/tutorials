690e75fc837384802f1b1b01f3d2d8e84aa8e226
dvsh-odoo <dvsh@odoo.com>
2025-02-26 16:02:02 +0530

    [IMP] awesome_dashboard:  Used Lazy Loading for Dashboard
    - Moved all dashboard-related files into a /dashboard subfolder for better
      organization.
    - Registered AwesomeDashboard in the lazy_components registry instead of actions
    - Introduced DashboardLoader component using LazyComponent to dynamically load
      the dashboard only when accessed.

--- awesome_dashboard/static/src/dashboard/dashboard.js
+++ awesome_dashboard/static/src/dashboard/dashboard.js
@@ -34,4 +34,4 @@ class AwesomeDashboard extends Component {
     }
 }
 
-registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
+registry.category("lazy_components").add("AwesomeDashboard", AwesomeDashboard);
