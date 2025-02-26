bd6d2d462700ceb2b4e3fd176cd9ad35b2df92c9
bhpr-odoo <bhpr@odoo.com>
2025-02-26 11:42:29 +0530

    [FIX] ticket_limit: fix the bugs
    fix the bugs and improve code

--- ticket_limit/models/ticket_limit.py
+++ ticket_limit/models/ticket_limit.py
@@ -1,11 +1,6 @@
 from odoo import models, fields, api
 
-class EventEvent(models.Model):
-    
-   
-    _inherit = 'event.event.ticket'
+class EventTicket(models.Model):
+    _inherit = "event.event.ticket"
 
-    max_tickets_per_order = fields.Integer(
-        string="Max Tickets per Order",
-        readonly=True,  # Optional: Make it read-only
-    )
+    tickets_per_registration = fields.Integer(string="Tickets per Registration", default=1)
