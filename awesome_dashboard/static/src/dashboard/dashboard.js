7c3ba283f937f64590407561028101c56a216511
lmer-odoo <lmer@odoo.com>
2025-02-26 14:31:09 +0100

    [IMP] awesome_dashboard: add dialog to select which cards to display

--- awesome_dashboard/static/src/dashboard/dashboard.js
+++ awesome_dashboard/static/src/dashboard/dashboard.js
@@ -1,8 +1,9 @@
 /** @odoo-module **/
 
-import { Component, useState } from "@odoo/owl";
+import { Component, useState, useRef, onMounted } from "@odoo/owl";
 
 import { _t } from "@web/core/l10n/translation";
+import { Dialog } from "@web/core/dialog/dialog";
 import { useService } from "@web/core/utils/hooks";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
@@ -12,7 +13,7 @@ import { PieChart } from "./pie_chart"
 
 export class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
-    static components = { Layout, DashboardItem, PieChart }
+    static components = { Dialog, Layout, DashboardItem, PieChart }
 
     setup() {
         this.action = useService("action");
@@ -21,6 +22,13 @@ export class AwesomeDashboard extends Component {
         this.statistics = useState(this.statisticsLoader);
 
         this.items = registry.category("awesome_dashboard").getAll();
+        this.enabledCards = useState({});
+        this.items.forEach((item) => {
+            this.enabledCards[item.id] = true;
+        });
+
+        this.dialogService = useService("dialog");
+        // useSubEnv({dialogData: {}})
     }
 
     openCustomers() {
@@ -34,6 +42,51 @@ export class AwesomeDashboard extends Component {
             res_model: 'account.move',
             views: [[false, 'list'], [false, 'form']]
         });
+    }
+
+    openDialog() {
+        const dialogProps = {
+            items: this.items,
+            enabledItems: this.enabledCards,
+            doneCallback: (values) => {
+                Object.assign(this.enabledCards, values);
+            }
+        };
+        this.dialogService.add(ConfigurationDialog, dialogProps);
+    }
+}
+
+class ConfigurationDialog extends Component {
+    static template = "awesome_dashboard.configuration_dialog"
+    static components = { Dialog };
+    static props = {
+        items: {
+            type: Array,
+            element: Object,
+        },
+        enabledItems: Object,
+        doneCallback: Function,
+        close: Function
+    }
+
+    setup() {
+        this.refs = new Map();
+        this.props.items.forEach(item => {
+            let ref = useRef(item.id)
+            this.refs.set(item.id, ref);
+            if (this.props.enabledItems[item.id]) {
+                onMounted(() => ref.el.checked = true);
+            }
+        });
+    }
+
+    onDone() {
+        let values = {};
+        this.refs.entries().forEach(([id, ref]) => {
+            values[id] = ref.el.checked;
+        });
+        this.props.doneCallback(values);
+        this.props.close();
     }
 }
 
