5b7019fa150a3c6e918cb2bbacaf5cf5277f79f4
Dhrumit Parmar <dgpa@odoo.com>
2025-02-26 13:03:15 +0530

    [FIX] awesome_[dashboard,owl]: suggested bug fix and fix linting

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -6,14 +6,14 @@ import { Card } from "./card/card";
 import { TodoList } from "./todo_list/todolist";
 
 export class Playground extends Component {
-  static template = "awesome_owl.playground";
-  static props = [];
-  static components = { Counter, Card, TodoList };
-  html = markup("<div class='text-primary'>Some Content</div>");
-  setup() {
-    this.sum = useState({ value: 0 });
-  }
-  incrementSum() {
-    this.sum.value++;
-  }
+    static template = "awesome_owl.playground";
+    static props = [];
+    static components = { Counter, Card, TodoList };
+    html = markup("<div class='text-primary'>Some Content</div>");
+    setup() {
+        this.sum = useState({ value: 0 });
+    }
+    incrementSum() {
+        this.sum.value++;
+    }
 }
