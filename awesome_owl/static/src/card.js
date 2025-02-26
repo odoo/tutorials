8ed074c55439a2039901bf252f6c90447a674d22
gvar-odoo <codenoko@gmail.com>
2025-02-26 09:18:56 +0100

    [IMP] owl chap 1.5

--- awesome_owl/static/src/card.js
+++ awesome_owl/static/src/card.js
@@ -3,14 +3,14 @@ import { Component, useState } from "@odoo/owl";
 export class Card extends Component {
     static template = "awesome_owl.card";
     static props = {
-        title: { type: String, optional: true },
-        content: { type: String, optional: true },
+        title: { type: String, optional: false },
+        content: { type: String, optional: false },
         slots: { type: Object, optional: true },
     };
 
     setup() {
         this.state = useState({
-            isExspanded: false,
+            isExpanded: false,
         });
     }
 }
