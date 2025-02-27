14126723a4c807e06d8b62953e069ae09555dbce
tpma-odoo <tpma@odoo.com>
2025-02-27 15:35:07 +0530

    [IMP] appointment_filter: improved appointment filter logic with search function
    Refined appointment filtering logic and optimized domain filtering for better
    performance.
    
    Task-4605840

--- appointment_filter/controllers/controllers.py
+++ appointment_filter/controllers/controllers.py
@@ -1,9 +1,8 @@
 from odoo import http
-from odoo.http import request
 from odoo.addons.website_appointment.controllers.appointment import WebsiteAppointment
 
-class WebsiteAppointmentFiltered(WebsiteAppointment):
 
+class WebsiteAppointmentFiltered(WebsiteAppointment):
     @http.route()
     def appointment_type_index(self, page=1, **kwargs):
         available_domain = self._appointment_website_domain()
@@ -25,12 +24,15 @@ class WebsiteAppointmentFiltered(WebsiteAppointment):
             extra_domain.append(("has_payment_step", "=", False))
 
         final_domain = available_domain + extra_domain
-        appointments = request.env["appointment.type"].search(final_domain)
+        response = super().appointment_type_index(page=page, **kwargs)
 
-        values = self._prepare_appointments_cards_data(
-            appointment_types=appointments,
-            page=page,
-            **kwargs
-        )
-        values['filters'] = kwargs
-        return request.render("website_appointment.appointments_cards_layout", values)
+        appointment_types = response.qcontext.get("appointment_types", [])
+        filtered_appointments = appointment_types.filtered_domain(final_domain)
+
+        response.qcontext["appointment_types"] = filtered_appointments
+        response.qcontext["filters"] = {
+            "appointment_mode": kwargs.get("appointment_mode"),
+            "schedule_based_on": kwargs.get("schedule_based_on"),
+            "has_payment_step": kwargs.get("has_payment_step"),
+        }
+        return response
