6718f86d9b1ceb1b946e7ad96dcbbb278b278642
Smit Patel <pasm@odoo.com>
2025-02-27 10:31:03 +0530

    [ADD] basic folders

---
+++ add_pricelist_price/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Book Price for Sales and Invoice',
+    'version': '1.0',
+    'category': 'Sales',
+    'summary': 'Display the Original Pricelist price (Book Price) on sales orders and invoices',
+    'depends': [],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3'
+}
