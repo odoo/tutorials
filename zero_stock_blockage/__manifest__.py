9099b88ead95747f39ef9bee5784f45eefc3e62d
sabj-odoo <sabj@odoo.com>
2025-02-27 13:50:29 +0530

    [IMP] sale: add 'Zero Stock Approval' boolean field in Sale Order
    - Added a new boolean field Zero Stock Approval in Sale Order.
    - If enabled, then only sale order is confirm.
    - The field is read-only for users in the Sales/User group.
    - Only Sales/Administrator can modify the field.
    
    task-4603888

---
+++ zero_stock_blockage/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "Zero Stock Blockage",
+    "author" : "sabj",
+    "description": "Adds Zero Stock Approval boolean field in Sale Order",
+    "depends": ["sale"],
+    "data": ["views/sale_order_view.xml"],
+    "installable": True,
+    "license": "LGPL-3",
+}
