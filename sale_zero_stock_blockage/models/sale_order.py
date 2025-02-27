7a0c3488ef83b4772a9919b05660750554754920
soza-odoo <soza@odoo.com>
2025-02-27 11:03:43 +0530

    [IMP] sale: resolve changes

--- sale_zero_stock_blockage/models/sale_order.py
+++ sale_zero_stock_blockage/models/sale_order.py
@@ -5,15 +5,13 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     zero_stock_approval = fields.Boolean(string="Zero Stock Approval")
-    group_check = fields.Boolean(compute='_group_check')
+    is_sales_manager = fields.Boolean(compute='_is_sales_manager')
 
-    def _group_check(self):
-        if self.env.user.has_group('sales_team.group_sale_manager'):
-            self.group_check = True
-        else:
-            self.group_check = False
-
+    def _is_sales_manager(self):
+        for sale_order in self:
+            sale_order.is_sales_manager = sale_order.env.user.has_group('sales_team.group_sale_manager')
+        
     def action_confirm(self):
         if not self.zero_stock_approval:
-            raise exceptions.ValidationError("You can not confirm this sales order because zero stock blocklist is not allowed")
+            raise exceptions.ValidationError("You can not confirm this sales order because zero stock blocklist is not enable")
         return super().action_confirm()
