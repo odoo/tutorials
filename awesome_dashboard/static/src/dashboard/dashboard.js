1a429b3770474c9662ac9d64a8b5d7c5eadec882
Rahul Jha (jhra) <jhra@odoo.com>
2025-02-27 08:46:16 +0530

    [IMP] awesome_dashboard: Added ability to toggle item visibility
    + Dashboard items can now be toggled on and off.

--- awesome_dashboard/static/src/dashboard/dashboard.js
+++ awesome_dashboard/static/src/dashboard/dashboard.js
@@ -6,6 +6,9 @@ import {Layout} from "@web/search/layout";
 import {useService} from "@web/core/utils/hooks";
 import { DashboardItem } from "./dashboard_item/dashboard_item";
 import { loadStatistics } from "./statistics";
+import { Dialog } from "@web/core/dialog/dialog";
+import { CheckBox } from "@web/core/checkbox/checkbox";
+import { browser } from "@web/core/browser/browser";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
@@ -16,12 +19,28 @@ class AwesomeDashboard extends Component {
         this.action = useService("action");
         this.orderStats = useState({value: null});
         onWillStart(this.onWillStart);
+        this.dialog = useService( "dialog");
+        this.state = useState({
+            disabledItems: browser.localStorage.getItem("disabledDashboardItems")?.split(",") || []
+        });
     }
     
     async onWillStart() {
         this.orderStats.value = await loadStatistics.start();
         console.log(this.orderStats.value);
         this.items = registry.category("awesome_dashboard").getAll();
+    }
+
+    openConfiguration() {
+        this.dialog.add(ConfigurationDialog, {
+            items: this.items,
+            disabledItems: this.state.disabledItems,
+            onUpdateConfiguration: this.updateConfiguration.bind(this),
+        })
+    }
+
+    updateConfiguration(newDisabledItems) {
+        this.state.disabledItems = newDisabledItems;
     }
 
     openCustomerKanban() {
@@ -38,6 +57,40 @@ class AwesomeDashboard extends Component {
     }
 
     
+}
+
+class ConfigurationDialog extends Component {
+    static template = "awesome_dashboard.ConfigurationDialog";
+    static components = { Dialog, CheckBox };
+    static props = ["close", "items", "disabledItems", "onUpdateConfiguration"];
+
+    setup() {
+        this.items = useState(this.props.items.map((item) => {
+            return {
+                ...item,
+                enabled: !this.props.disabledItems.includes(item.id),
+            }
+        }));
+    }
+
+    done() {
+        this.props.close();
+    }
+
+    onChange(checked, changedItem) {
+        changedItem.enabled = checked;
+        const newDisabledItems = Object.values(this.items).filter(
+            (item) => !item.enabled
+        ).map((item) => item.id)
+
+        browser.localStorage.setItem(
+            "disabledDashboardItems",
+            newDisabledItems,
+        );
+
+        this.props.onUpdateConfiguration(newDisabledItems);
+    }
+
 }
 
 registry.category("lazy_components").add("AwesomeDashboard", AwesomeDashboard);
