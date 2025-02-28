82e7c2d14ba64288c2f2b6b1fa04592549724024
jsum-odoo <jsum@odoo.com>
2025-02-28 10:47:48 +0530

    [IMP] wbsp_prod_desc: enhanced html translation support

--- wbsp_prod_desc/models/product_template.py
+++ wbsp_prod_desc/models/product_template.py
@@ -2,10 +2,11 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import fields, models
+from odoo.tools.translate import html_translate
 
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     wbsp_product_description = fields.Html(
-        string='Ecommerce Extended Description', translate=True)
+        string='Ecommerce Extended Description', translate=html_translate)
