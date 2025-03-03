40352eb169b6d76be0d92559cfa5cfb9c22015a2
Romain (roto) <roto@odoo.com>
2025-03-03 15:50:09 +0100

    [IMP] restrict access data

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,14 +4,16 @@
         'base',
     ],
     'data': [
-        'security/ir.model.access.csv',
-
         'views/res_users_views.xml',
         'views/estate_property_offer_views.xml',
         'views/estate_property_views.xml',
         'views/estate_property_type_views.xml',
         'views/estate_menus.xml',
+
+        'security/security.xml',
+        'security/ir.model.access.csv',
     ],
+    'category': 'Real Estate/Brokerage',
     'application': True,
     'license': 'LGPL-3',
 }
