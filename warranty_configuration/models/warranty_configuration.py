005c5062c89ff0c9d45643e6a9d16357f9e0ddbc
dvpa-odoo <dvpa@odoo.com>
2025-03-03 12:44:02 +0530

    [IMP] warranty_configuration: implement warranty configuration wizard
    - Created a warranty configuration wizard to manage warranty configurations
    - Implemented the necessary fields and computations for warranty configurations
    - access model the `access_warranty_line_wizard`, `access_warranty_wizard`
    - ensured proper linking of warranty configurations to sales order lines

--- warranty_configuration/models/warranty_configuration.py
+++ warranty_configuration/models/warranty_configuration.py
@@ -2,10 +2,12 @@ from odoo import models, fields
 
 
 class WarrantyConfiguration(models.Model):
-    _name = 'warranty.configuration'
-    _description = 'Warranty Configuration'
+    _name = "warranty.configuration"
+    _description = "Warranty Configuration"
 
     name = fields.Char(string="Name", required=True)
-    product_id = fields.Many2one('product.product', string="Product", required=True)
+    product_id = fields.Many2one("product.product", string="Product", required=True)
     percentage = fields.Float(string="Percentage", required=True)
-    warranty_year_id = fields.Many2one('warranty.year', string="Warranty Duration", required=True)
+    warranty_year_id = fields.Many2one(
+        "warranty.year", string="Warranty Duration", required=True
+    )
--- warranty_configuration/wizard/warranty_wizard.py
+++ warranty_configuration/wizard/warranty_wizard.py
@@ -1,37 +1,103 @@
-from odoo import models, fields, api
 from datetime import date, timedelta
 
+from odoo import api, Command, fields, models
 
-class WarrantyWizard(models.TransientModel):
+class WarrantryWizard(models.TransientModel):
     _name = "warranty.wizard"
     _description = "Warranty Wizard"
 
-    product_id = fields.Many2one(
-        "product.product",
-        string="Product",
-        required=True,
-        domain=[("is_warranty_available", "=", True)] 
+    order_id = fields.Many2one("sale.order", string="Sale Order", required=True)
+    warranty_line_ids = fields.One2many(
+        "warranty.line.wizard", "wizard_id", string="Warranty Lines"
     )
-    warranty_year_id = fields.Many2one(
-        "warranty.year", string="Warranty Duration", required=True
-    )
-    warranty_product_id = fields.Many2one(
+
+    @api.model
+    def default_get(self, fields):
+        res = super().default_get(fields)
+        active_id = self.env.context.get("active_id")
+
+        if not active_id:
+            return res
+
+        order = self.env["sale.order"].browse(active_id)
+
+        if not order or not order.exists():
+            return res
+
+        warranty_lines = []
+        for line in order.order_line:
+            if line.product_id.is_warranty_available and not line.product_warranty_id:
+                warranty_lines.append(
+                    {"order_line_id": line.id, "product_id": line.product_id.id}
+                )
+
+        res.update(
+            {
+                "order_id": order.id,
+                "warranty_line_ids": [Command.clear()]
+                + [Command.create(vals) for vals in warranty_lines],
+            }
+        )
+
+        return res
+
+    def action_confirm(self):
+        order_lines = []
+        for line in self.warranty_line_ids:
+            warranty_price = (
+                line.order_line_id.price_unit
+                * line.warranty_configuration_id.percentage
+                / 100
+            )
+            product_line_sequence = line.order_line_id.sequence
+            order_lines.append(
+                {
+                    "order_id": line.order_line_id.order_id.id,
+                    "product_id": line.warranty_configuration_id.product_id.id,
+                    "product_warranty_id": line.order_line_id.product_id.id,  # Original product with warranty link
+                    "name": f"Warranty: {line.warranty_configuration_id.product_id.name}\nFor {line.order_line_id.product_id.name}\nEnd Date: {line.end_date}",
+                    "price_unit": warranty_price,
+                    "product_uom_qty": 1,
+                    "sequence": product_line_sequence + 1,
+                }
+            )
+            
+            if order_lines:
+                self.env["sale.order.line"].create(order_lines)
+            return
+
+
+class WarrantyLineWizard(models.TransientModel):
+    _name = "warranty.line.wizard"
+    _description = "Warranty Line for Wizard"
+
+    wizard_id = fields.Many2one("warranty.wizard", string="Warranty Wizard")
+    product_id = fields.Many2one("product.product", string="Product")
+    order_line_id = fields.Many2one("sale.order.line")
+    warranty_year_id = fields.Many2one("warranty.year", string="Warranty Duration")
+
+    # Linking the warranty configuration that is calculated based on warranty year
+    warranty_configuration_id = fields.Many2one(
         "warranty.configuration",
-        string="Warranty Configuration",
         compute="_compute_warranty_configuration",
+        string="Warranty Configuration",
         store=True,
     )
     end_date = fields.Date(
         string="End Date", compute="_compute_end_date", store=True, readonly=True
     )
+    price_unit = fields.Float(string="Warranty Price")
 
-    @api.depends('warranty_year_id', 'product_id')
+    @api.depends("warranty_year_id")
     def _compute_warranty_configuration(self):
         for record in self:
-            record.warranty_product_id = self.env['warranty.configuration'].search([
-                ('warranty_year_id', '=', record.warranty_year_id.id),
-            ], limit=1)
-
+            warranty = self.env["warranty.configuration"].search(
+                [
+                    ("warranty_year_id", "=", record.warranty_year_id.id),
+                ],
+                limit=1,
+            )
+            record.warranty_configuration_id = warranty
 
     @api.depends("warranty_year_id")
     def _compute_end_date(self):
@@ -40,18 +106,5 @@ class WarrantyWizard(models.TransientModel):
                 record.end_date = date.today() + timedelta(
                     days=record.warranty_year_id.years * 365
                 )
-
- 
-    def apply_warranty(self):
-        active_id = self._context.get('active_id')
-        sale_order = self.env['sale.order'].browse(active_id)
-        
-        if sale_order and self.warranty_product_id:
-            unit_price = (self.product_id.list_price * self.warranty_product_id.percentage) / 100
-            self.env['sale.order.line'].create({
-                'order_id': sale_order.id,
-                'product_id': self.warranty_product_id.id,
-                'product_warranty_id': self.product_id.id,
-                'name': f"{self.warranty_product_id.name}\nEnd Date: {self.end_date}",
-                'price_unit': unit_price,
-            })
+            else:
+                record.end_date = False
