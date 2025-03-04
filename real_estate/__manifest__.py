bae3af79ffa73af230b4fdf01bd513ed053944a1
sris-odoo <sris@odoo.com>
2025-03-04 18:58:37 +0530

    [ADD] real_estate: Odoo Architecture & Module Initialization
    In this commit
    - Understood the core Odoo MVC architecture.
    - Created a Real Estate module with a structured folder hierarchy.
    - Defined the estate.property model with essential fields.
    - Registered the model in the manifest and initialization files.

---
+++ real_estate/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Rishav Shah (sris)",
+    'category': 'Estate',
+    'description': """    START-UP 1    """,
+    'installable':True,
+    'application':True,
+    'auto_install': False,
+    # data files always loaded at installation
+    # 'data': [
+    #     'views/mymodule_view.xml',
+    # ],
+    # # data files containing optionally loaded demonstration data
+    # 'demo': [
+    #     'demo/demo_data.xml',
+    # ],
+}
---
+++ real_estate/models/property.py
@@ -0,0 +1,23 @@
+from odoo import models, fields
+
+class property(models.Model):
+    _name = 'property'  # Database table name
+    _description = 'property'
+
+    name = fields.Char(string="Property Name", required=True)
+    description = fields.Text(string="Description")
+    postcode = fields.Char(string="Postcode")
+    date_availability = fields.Date(string="Available From")
+    expected_price = fields.Float(string="Expected Price", required=True)
+    selling_price = fields.Float(string="Selling Price", readonly=True)
+    bedrooms = fields.Integer(string="Bedrooms")
+    living_area = fields.Integer(string="Living Area (sqm)")
+    facades = fields.Integer(string="Facades")
+    garage = fields.Boolean(string="Has Garage")
+    garden = fields.Boolean(string="Has Garden")
+    garden_area = fields.Integer(string="Garden Area (sqm)")
+
+    garden_orientation = fields.Selection(
+        [('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
+        string="Garden Orientation"
+    )
