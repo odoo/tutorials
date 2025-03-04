e65853249950f0a19d70deedea4bad0614287481
vkas-odoo <vkas@odoo.com>
2025-03-04 18:00:06 +0530

    [ADD] estate: Architecture , models and basic fields
    In this commit we have introduced following changes
     - Created the estate_properties model to manage property data.
     - Added __init__.py and __manifest__.py files to initialize the module and
       define its metadata, dependencies, and basic configuration.
     - Defined a new table estate_properties in the database with fields such as
       name, description, selling_price, expected_price, etc. to store relevant
       property information.

---
+++ estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'sequence': 1,
+    'summary': 'Help users with Real estate.',
+    'depends': [
+        'base_setup'
+    ],
+    'license': 'LGPL-3'
+}
