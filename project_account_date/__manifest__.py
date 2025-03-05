99aaed4e165ad859b179c663ab6a1b8b14d66a25
Harsh Shah <hash@odoo.com>
2025-03-05 12:37:26 +0530

    [ADD] project_account_data: link project date to date in journal items - Created a new module `project_account_data` to manage the linkage between the   project start date and recognition date for journal entries. - Inherited the `project.project` model to add a computed field   `is_reload_required` for detecting changes to journal entries based on the   project start date. - Inherited the `account.move.line` model to introduce reload date for tracking   changes to journal entries. - Enhanced the `account_automatic_entry_wizard` by overriding its `do_action()`   method to incorporate additional logic for recognizing invoices. - Updated the `project.project` view to display a 'Recognise Invoices' button   that triggers the creation of journal entries with the updated project start date.
    Task ID: 4613027

---
+++ project_account_date/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Link Project Date with Account",
+    'version': "1.0",
+    'summary': "Allows the computation of account date based on project planning date",
+    'author': "Harsh Shah (hash)",
+    'depends': ["sale_project", "accountant"],
+    'data': [
+        "views/project_project_view.xml",
+    ],
+    'installable': True,
+    'license': "LGPL-3"
+}
