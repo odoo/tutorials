d00a42d13e2ca158acb64d834fc4709dc71991cf
pssh-odoo <pssh@odoo.com>
2025-03-05 18:50:24 +0530

    [IMP] sell_product_kit:  Prefill quantity and price when reopening the kitwizard
    • Added a functionality that pre fills the product_kit_wizard with the price and
      quantity of the sub products if the sub products are already added in the sale
      order line.

--- sell_product_kit/models/product_kit_wizard.py
+++ sell_product_kit/models/product_kit_wizard.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class ProductKitWizard(models.TransientModel):
@@ -11,35 +10,43 @@ class ProductKitWizard(models.TransientModel):
     product_id = fields.Many2one('product.product', string="Product", readonly=True)
     order_line_id = fields.Many2one('sale.order.line', string="Order Line", readonly=True)
     sub_product_kit = fields.One2many('product.kit.wizard.line', 'wizard_id', string="Sub Products")
-    total_price = fields.Float(string="Total Price", compute="_compute_total_price", store=True)
     sub_total = fields.Float(string='Sub Total', compute="_compute_sub_total_price")
 
     @api.model
     def default_get(self, fields_list):
         res = super(ProductKitWizard, self).default_get(fields_list)
         product_id = self.env.context.get('default_product_id')
-        if product_id and 'sub_product_kit' in fields_list:
+        order_line_id = self.env.context.get('default_order_line_id')
+        if product_id and order_line_id and 'sub_product_kit' in fields_list:
             product = self.env['product.product'].browse(product_id)
-
-            sub_products = product.sub_product_ids
+            sub_product_wizard = self.env['product.kit.wizard'].search([
+                ('order_line_id', '=', order_line_id),
+                ('product_id', '=', product_id)],
+                order='id desc', limit=1)
             sub_product_list = []
-            for sub_product in sub_products:
-                sub_product_list.append((0, 0, {
-                    'product_id': sub_product.id,
-                    'sub_product_name': sub_product.product_tmpl_id.name,
-                    'quantity': 1.0,
-                    'unit_price': sub_product.list_price,
-                    'subtotal': sub_product.list_price,
-                }))
-
+            sub_products = product.sub_product_ids
+            if sub_product_wizard.exists():
+                sub_product_kit = sub_product_wizard.sub_product_kit
+                for sub_product, existing_product in zip(sub_products, sub_product_kit):
+                    sub_product_list.append((0, 0, {
+                        'product_id': sub_product.id,
+                        'sub_product_name': sub_product.product_tmpl_id.name,
+                        'quantity': existing_product.quantity,
+                        'unit_price': existing_product.unit_price,
+                        'subtotal': existing_product.subtotal,
+                    }))
+            else:
+                for sub_product in sub_products:
+                    sub_product_list.append((0, 0, {
+                        'product_id': sub_product.id,
+                        'sub_product_name': sub_product.product_tmpl_id.name,
+                        'quantity': 1.0,
+                        'unit_price': sub_product.list_price,
+                        'subtotal': sub_product.list_price,
+                    }))
             res['sub_product_kit'] = sub_product_list
+            res['order_line_id'] = order_line_id
         return res
-
-    @api.depends('sub_product_kit.subtotal')
-    def _compute_total_price(self):
-        product_price = self.order_line_id.price_unit
-        for wizard in self:
-            wizard.total_price = sum(line.subtotal for line in wizard.sub_product_kit) + product_price
 
     @api.depends('sub_product_kit.subtotal')
     def _compute_sub_total_price(self):
@@ -54,19 +61,40 @@ class ProductKitWizard(models.TransientModel):
             return {'type': 'ir.actions.act_window_close'}
 
         sale_order_id = self.order_line_id.order_id
-        self.order_line_id.price_unit = self.total_price
         sub_product_data = self.env['product.kit.wizard.line'].search([('wizard_id', '=', self.id)], order="id asc")
-        for i,sub_product_id in enumerate(product.sub_product_ids):
+        changes_made = False
+        existing_sub_products = self.env['sale.order.line'].search([
+            ('order_id', '=', sale_order_id.id),
+            ('parent_line_id', '=', self.order_line_id.id),
+            ('is_sub_product', '=', True)
+        ])
+        for i, sub_product_id in enumerate(product.sub_product_ids):
+            wizard_line = sub_product_data[i]
+            existing_sub = existing_sub_products.filtered(lambda l: l.product_id.id == sub_product_id.id)
             vals = {
                 'order_id': sale_order_id.id,
                 'product_id': sub_product_id.id,
                 'name': sub_product_id.product_tmpl_id.name,
-                'product_uom_qty': sub_product_data[i].quantity,
+                'product_uom_qty': wizard_line.quantity,
                 'price_unit': 0.0,
                 'parent_line_id': self.order_line_id.id,
                 'is_sub_product': True,
             }
-            self.env['sale.order.line'].create(vals)
+            if existing_sub:
+                if (existing_sub.product_uom_qty != wizard_line.quantity or
+                    existing_sub.price_unit != wizard_line.unit_price):
+                    existing_sub.write({
+                        'product_uom_qty': vals['product_uom_qty'],
+                        'price_unit': vals['price_unit']
+                    })
+                    changes_made = True
+            else:
+                self.env['sale.order.line'].create(vals)
+                changes_made = True
+
+        if changes_made:
+            main_product_price = product.product_tmpl_id.list_price
+            self.order_line_id.price_unit = main_product_price + self.sub_total
 
         return {'type': 'ir.actions.act_window_close'}
 
