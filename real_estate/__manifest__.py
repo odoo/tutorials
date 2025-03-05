66240de9f4a459fa196abc554fb2deacb508b5c0
mamg-odoo <mamg@odoo.com>
2025-03-05 18:33:44 +0530

    [ADD] estate:  Created estate module for training and implementation.
    Initialized the Estate module with a well-structured foundation:
    
    Organized the module into sub-folders (Models, Views, Security) for better
    maintainability.
    
    Implemented user role management with restricted access ,configuring access
    rights for security, and designing UI views.
    
    Designed form, search, and list views for the property model:
    
    Form view for detailed property management.
    Search view with quick filters and grouping by postcode.
    List view for improved data visualization.

---
+++ real_estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name':"Real Estate",
+    'depends':['base'],
+    'data':[
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+        'views/estate_property_list.xml'
+    ]
+}
