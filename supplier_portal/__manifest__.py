f0f938f9ecbb42966b20c23b65165d73188b4e69
Rahul Jha (jhra) <jhra@odoo.com>
2025-03-05 13:10:38 +0530

    [ADD] supplier_portal: Added a new portal for suppliers
    + Suppliers can create draft invoices
    + They can upload any relevant documents (PDF and XML) as attachments to the invoice

---
+++ supplier_portal/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Supplier Portal',
+    'version': '18.0',
+    'description': 'Supplier Portal',
+    'depends': ['web', 'portal','sale_management'],
+    'author': 'Rahul Jha (jhra)',
+    'website': 'https://www.odoo.com',
+    'category': 'Supplier/Portal',
+    'data': [
+        'views/webclient_templates.xml'
+    ],
+    'license': 'LGPL-3',
+}
