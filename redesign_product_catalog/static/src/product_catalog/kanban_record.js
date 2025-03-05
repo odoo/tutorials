c6eb30a8691410b42a235a24c0ce398e1f2e5f69
pcbe-odoo <pcbe@odoo.com>
2025-03-05 12:32:33 +0530

    [IMP] redesign_product_catalog: modify product catalog click behavior
    In this commit:
    - Adjust the restriction on default behavior when clicking on a product in
      the catalog.
    
    Task - 4609501

--- redesign_product_catalog/static/src/product_catalog/kanban_record.js
+++ redesign_product_catalog/static/src/product_catalog/kanban_record.js
@@ -2,10 +2,6 @@ import { ProductCatalogKanbanRecord } from "@product/product_catalog/kanban_reco
 
 export class ProductCatalogPopupKanbanRecord extends ProductCatalogKanbanRecord {
     onGlobalClick(ev) {
-        if (ev.target.closest(".o_product_catalog_cancel_global_click")) {
-            return;
-        }
-
         // New condition: Prevent the product from being added if clicked on image in mobile
         if (this.env.isSmall && ev.target.closest(".o_product_image")) {
             return;
