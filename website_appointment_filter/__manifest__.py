4b9974e85187839b411e12fd499d75a945d9f05e
sabj-odoo <sabj@odoo.com>
2025-03-05 13:35:21 +0530

    [ADD] website_appointment_filter: add filters for type, schedule, payment step
    - Create a filter for appointment type (Online/Offline).
    - Create a filter for appointment schedule (Users/Resources).
    - Create a filter for payment step (Paid/Free).
    - Update the website appointment view to include filter dropdowns.
    
    task-4617770

---
+++ website_appointment_filter/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Website Appointment Filter",
+    "category": "Services/Appointment",
+    "summary": "Add filters to website appointment view",
+    "description": "Module to add filters on website appointment view.",
+    "version": "1.0",
+    "author": "sabj",
+    "depends": ["website_appointment", "appointment"],
+    "data": ["views/appointment_filter.xml"],
+    "installable": True,
+    "license": "LGPL-3",
+}
