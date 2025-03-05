b457d6221807b5181259367d7a1f9a49386d4c46
kuay-odoo <kuay-odoo>
2025-03-05 18:33:18 +0530

    [ADD] estate:  create module with database, fields, security and views
    This commit introduces the estate module and creation of database.
    We create tables and fields to manage property details,
    such as property name, description, expected price, and selling price.
    It also integrates security access rules, adding an ir.model.access.csv file
    that grants the base.group_user group the appropriate permissions to read, write
    ,create, and delete property records.
    These security configurations ensure that only authorized users can interact
    with estate data, enhancing control over data access and modification within
    the system.
    
    This commit also includes the creation of menus for easy navigation
    of estate records, a reserved field to manage the status of properties,
    and the implementation of list and form views for better representation of
    estate data. The search view has also been configured, allowing users to
    filter on state value be new or offer received and group by using postcode field

---
+++ estate/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "estate",
+    'depends':['base'],
+    'data': ['security/ir.model.access.csv',
+              'views/estate_model_views.xml',
+              'views/estate_menus.xml',
+              'views/list_view.xml',
+              'views/form_view.xml',
+              'views/search_view.xml'
+            ]
+
+}
