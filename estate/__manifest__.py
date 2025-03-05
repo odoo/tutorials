eff4716a23cf288273faee50144be760f3954608
agir-odoo <agir@odoo.com>
2025-03-05 18:32:50 +0530

    [ADD] estate:  Created first E-State Module
    Initialized the estate module with a well-structured base:
    
    - Created sub-folders (Models, Views, Security) to improve readability.
    - Added `__init__.py` and `__manifest__.py` to ensure proper module loading and
    structure.
    - Defined the Property model with essential fields such as name, description,
    price, state, and expected selling price.
    - Implemented user role management to restrict access based on permissions.
    - Added action buttons to manage property state transitions efficiently.
    
    Implemented search, form, and list views for `test.model`:
    - Designed a **form view** for managing property details with an intuitive
    interface.
    - Developed a **search view** with quick search fields and relevant filters for
    better usability.
    - Created a **list view** for improved record visualization .
    - Added **grouping by postcode** in the search view for structured filtering.
    - Replaced state-based filters with alternative field-based filters to ensure
     proper functionality.
    
    These enhancements improve usability, filtering options, and data management,
    laying the foundation for an efficient real estate property management system.

---
+++ estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name' : "Estate",
+    'depends' : ['base'],
+     'data': [
+        'security/ir.model.access.csv',
+        'views/test_models_views.xml',
+        'views/test_menus.xml',
+        'views/test_models_list.xml'
+    ],
+}
