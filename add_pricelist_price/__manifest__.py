d4a73fe6c83f1adeba1d6a10141fe2576a275b11
seva-odoo <seva@odoo.com>
2025-03-06 17:51:16 +0530

    [FIX] Add Pricelist Price : Module to add pricelist price in Sales Order Lines and Account Move Lines
    in this commit:
    
    1.Corrected file format of manifest file
    
    task - 4619729

--- add_pricelist_price/__manifest__.py
+++ add_pricelist_price/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name' : "Add Pricelist Price",
-    'version' : '1.0',
-    'category': 'Sales/Sales',
+    'version' : "1.0",
+    'category': "Sales/Sales",
     'summary' : 'Module to add pricelist price in Sales Order Lines and Account Move Lines',
     'description': """
         This module adds Book price field in invoice line and sales order line to compare 
@@ -12,5 +12,5 @@
         'views/add_pricelist_price_views.xml'
     ],
     'installable' : True,
-    'license': 'AGPL-3',
+    'license': "AGPL-3",
 }
