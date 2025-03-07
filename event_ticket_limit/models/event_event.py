5fc630d335bdb1be4358ac2ccd5dc844bbae8768
seva-odoo <seva@odoo.com>
2025-03-07 11:50:28 +0530

    [IMP] event : Module to Limit the Number of Tickets Per Registration
    in this commit:
    
    1. Added constraint for event_event and event_ticket models to check default_tickets_per_registration and ticket_limit  should be greater then 0.
    
    task - 4499983

--- event_ticket_limit/models/event_event.py
+++ event_ticket_limit/models/event_event.py
@@ -1,11 +1,18 @@
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class Event(models.Model):
     _inherit = "event.event"
 
     default_tickets_per_registration = fields.Integer(
-        string=" Default Tickets per Registration",
+        string="Default Tickets per Registration",
         help='Maximum Number of tickets per registration',
         default=7
     )
+
+    @api.constrains('default_tickets_per_registration')
+    def _check_ticket_limit(self):
+        for event in self:
+            if event.default_tickets_per_registration <= 0 :
+                raise ValidationError("default tickets per registration for event should be greater than 0")
--- event_ticket_limit/models/event_ticket.py
+++ event_ticket_limit/models/event_ticket.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class EventTicket(models.Model):
@@ -6,10 +7,15 @@ class EventTicket(models.Model):
 
     ticket_limit = fields.Integer(
         string="Tickets per Registration",
-        help="Maximum number of tickets per registration",
         default=lambda self: self._get_default_tickets_per_registration()
     )
 
     def _get_default_tickets_per_registration(self):
         """Fetch the default ticket limit from the related event."""
         return self.event_id.default_tickets_per_registration if self.event_id else 7
+
+    @api.constrains('ticket_limit')
+    def _check_ticket_limit(self):
+        for ticket in self:
+            if ticket.ticket_limit <= 0 :
+                raise ValidationError("ticket limit for event should be greater than 0")
