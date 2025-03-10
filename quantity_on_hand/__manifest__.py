a838bab5effc24763ac218ec8ff1a666d18c7fb8
amga-odoo <amga@odoo.com>
2025-03-10 11:05:08 +0530

    [ADD] quantity_on_hand: ui changes of product view
    - added quantity on hand field editable if not multi-locations
    in product general information with update button invisible if not
    multi-locations and visible on hover, link to On Hand report
    - Remove stat button, replace by update besides qty on hand in the form
    - Change the layout of forecast button, red if negative
    - Remove descriptions for goods

---
+++ quantity_on_hand/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "On Hand Quantitiy",
+    "depends": ["stock"],
+    "data": [
+        "views/product_views.xml",
+    ],
+    
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
+}
