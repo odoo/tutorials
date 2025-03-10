96cd8a7d835389f655d5417b840bd02ba0c12a16
dtda-odoo <dtda@odoo.com>
2025-03-10 12:08:21 +0530

    [ADD] global_discount_updater: Add module to update global discount
    Add module which works with global discount in sales order lines
    Add Functionalities
    -  update applied discount when product is added or removed
    from sales order line
     - remove discount lines if no non discount is present in sales order lines

---
+++ global_discount_updater/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "global_discount_updater",
+    "category": "Sales/Sales",
+    "description": "Updates the global discount in sales order lines",
+    "depends": ["sale"],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3",
+    "auto_install": ["sale"]
+}
