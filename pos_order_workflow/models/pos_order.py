8908082abb7485161cc10c0b233c09b89aa24ecc
seva-odoo <seva@odoo.com>
2025-03-10 11:16:52 +0530

    [IMP] point of sale : POS order workflow
    in this commit:
    
    1.Added a send to pick button in POS screen to validate order before payment
    2. Ensured there are no multiple stock moves or orders during loading and payment
    
    task - 4629844

---
+++ pos_order_workflow/models/pos_order.py
@@ -0,0 +1,13 @@
+from odoo import fields, models
+
+
+class PosOrder(models.Model):
+    _inherit = 'pos.order'
+
+    select_shipping_date =  fields.Date(default=fields.Datetime.now)
+
+    def _process_saved_order(self, draft):
+        if draft and  self.select_shipping_date:
+            self._create_order_picking()
+            self._compute_total_cost_in_real_time()
+        return super(PosOrder, self)._process_saved_order(draft)
---
+++ pos_order_workflow/models/stock_picking.py
@@ -0,0 +1,35 @@
+from odoo import api, models
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    @api.model
+    def _create_picking_from_pos_order_lines(self, location_dest_id, lines, picking_type, partner=False):
+        pos_order = lines.mapped("order_id")
+        # Ensure a shipping date is set before proceeding
+        if not pos_order.select_shipping_date:
+            return super()._create_picking_from_pos_order_lines(location_dest_id, lines, picking_type)
+        # Check if an existing picking is already linked to this order and is not done
+        existing_picking = self.search([
+            ('pos_order_id', '=', pos_order.id),
+            ('picking_type_id', '=', picking_type.id),
+            ('state', '!=', 'done')
+        ], limit=1)
+        if existing_picking:
+            try:
+                with self.env.cr.savepoint():
+                    existing_picking._action_done()
+                return existing_picking
+            except Exception as e:
+                print(f"Error while completing picking: {e}")
+                return existing_picking
+        #Check if stock moves already exist for this order
+        existing_moves = self.env['stock.move'].search([
+            ('origin', '=', pos_order.name),
+            ('state', 'not in', ['done', 'cancel'])
+        ])
+        if existing_moves:
+            print(f"Stock moves already exist for POS Order {pos_order.name}, skipping creation.")
+            return existing_picking or self.env['stock.picking']
+        return super()._create_picking_from_pos_order_lines(location_dest_id, lines, picking_type)
