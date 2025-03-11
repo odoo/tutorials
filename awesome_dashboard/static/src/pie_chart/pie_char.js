b8e2137beeb719fd3dfe9033872c28b8f0709a88
ssud-odoo <ssud@odoo.com>
2025-03-11 18:51:33 +0530

    [IMP] awesome_dashboard: add statistics service, and piechart
    - added dashboard component to display content
    - implemented a statistics service to fetch and cache order statistics
    - integrated piechart component to visualize order data (t-shirt sizes)

---
+++ awesome_dashboard/static/src/pie_chart/pie_char.js
@@ -0,0 +1,35 @@
+import { Component, onWillStart, onMounted, useRef, onWillUpdateProps } from "@odoo/owl"
+import { loadJS } from "@web/core/assets";
+
+export class PieChart extends Component{
+    static template = "awesome_dashboard.PieChart"
+
+    setup(){
+        this.canvasRef = useRef("canvasRef")
+        onWillStart(()=> loadJS("/web/static/lib/Chart/Chart.js"))
+        onMounted(()=> this.renderChart())
+        onWillUpdateProps(()=> this.renderChart())
+    }
+
+    renderChart(){
+        if(this.chart){
+            this.chart.destroy()
+        }
+
+        this.chart = new Chart(this.canvasRef.el, {
+            type: "pie",
+            data: {
+                labels: Object.keys(this.props.data),
+                datasets: [
+                    {
+                        data: Object.values(this.props.data)
+                    }
+                ]
+            }
+        });
+    }
+
+    static props = {
+        data: Object
+    }
+}
