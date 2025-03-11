efaf22fd5be68a6066bdb6b55b1ee7ec0c13e3c8
pssh-odoo <pssh@odoo.com>
2025-03-11 13:27:49 +0530

    [ADD] gross_profit_margin:  Compute Gross Profit Margin on product form
    - Added a computed field to display the Gross Profit Margin in percentage.
    - Added the field in the product form view next to Sales Price.
    - Enabled bidirectional update: modifying Gross Profit Margin updates the Sales
      Price accordingly.

---
+++ gross_profit_margin/models/product_product.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    gross_profit_margin = fields.Float(
+        compute='_gross_profit_margin',
+        inverse="_inverse_gross_profit_margin",
+        string='Gross Profit Margin',
+        help='Gross Profit Margin for this product',
+    )
+
+    @api.depends('lst_price','standard_price')
+    def _gross_profit_margin(self):
+        for product in self:
+            if product.lst_price:
+                if product.standard_price != 0:
+                    product.gross_profit_margin = (product.lst_price - product.standard_price) / product.standard_price
+                else:
+                    product.gross_profit_margin = 0.0
+            else:
+                product.gross_profit_margin = 0.0
+
+    def _inverse_gross_profit_margin(self):
+        for product in self:
+            if product.gross_profit_margin:
+                product.lst_price = (product.standard_price*product.gross_profit_margin) + product.standard_price
---
+++ gross_profit_margin/models/product_template.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    gross_profit_margin = fields.Float(
+        compute='_gross_profit_margin',
+        inverse="_inverse_gross_profit_margin",
+        string='Gross Profit Margin',
+        help='Gross Profit Margin for this product',
+    )
+
+    @api.depends('list_price','standard_price')
+    def _gross_profit_margin(self):
+        for product in self:
+            if product.list_price:
+                if product.standard_price != 0:
+                    product.gross_profit_margin = (product.list_price - product.standard_price) / product.standard_price
+                else:
+                    product.gross_profit_margin = 0.0
+            else:
+                product.gross_profit_margin = 0.0
+
+    def _inverse_gross_profit_margin(self):
+        for product in self:
+            if product.gross_profit_margin:
+                product.list_price = (product.standard_price*product.gross_profit_margin) + product.standard_price
