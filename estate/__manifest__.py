d03938462dae22bd52d342afc7159b404c53ec25
Subhadeep <info.subhadeep@gmail.com>
2025-03-17 16:07:53 +0100

    c4

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,7 +4,9 @@
         'base'
     ],
     'data': [
-        'data/ir.model.access.csv'
+        'data/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
     ],
     'application': True
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta
 
 class EstateProperty(models.Model):
     _name = "estate_property"
@@ -6,13 +7,16 @@ class EstateProperty(models.Model):
 
     name = fields.Char(required = True)
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy = False, default = fields.Date.today() + relativedelta(months = 3))
     expected_price = fields.Float(required = True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly = True, copy = False)
+    bedrooms = fields.Integer(default = 2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
-    garden_orientation = fields.Selection([('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
+    garden_orientation = fields.Selection([('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]),
+    active = fields.Boolean(default = True)
+    state = fields.Selection([('new', 'New'), ('received', 'Offer Received'), ('accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')],
+                             required = True, copy = False, default = 'new')
