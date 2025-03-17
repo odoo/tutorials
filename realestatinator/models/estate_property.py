672427119a18d06c3f495d18d97081d5b23d2a29
gato-odoo <gato@odoo.com>
2025-03-17 16:10:04 +0100

    ch.5 - added fields and restrictions to estate_property model

--- realestatinator/models/estate_property.py
+++ realestatinator/models/estate_property.py
@@ -6,17 +6,30 @@ class EstatePropery(models.Model):
 	_order = 'sequence'
 
 	sequence = fields.Integer('Sequence', default=0)
-	name = fields.Char(required=True)
+	name = fields.Char('Name', required=True)
 	description = fields.Text()
 	postcode = fields.Char()
-	date_availability = fields.Date()
+	date_availability = fields.Date('Available date', copy=False, default=fields.Date.add(fields.Date.today(), months=+3))
 	expected_price = fields.Float()
-	selling_price = fields.Float()
-	bedrooms = fields.Integer()
-	living_area = fields.Integer()
+	selling_price = fields.Float('Price', readonly=True, copy=False)
+	bedrooms = fields.Integer('Bedrooms', default=2)
+	living_area = fields.Integer('Living Area')
 	facades = fields.Integer()
-	garage = fields.Boolean()
-	garden = fields.Boolean()
+	garage = fields.Boolean('Garage')
+	garden = fields.Boolean('Garden')
 	garden_area = fields.Integer()
-	garden_orientation = fields.Selection(string='Orientation', selection=[('N', 'N'), ('S', 'S'), ('E', 'E'), ('W', 'W')])
-	
+	garden_orientation = fields.Selection(string='Orientation', selection=[
+		('N', 'North'), 
+		('S', 'South'), 
+		('E', 'East'), 
+		('W', 'West')
+	])
+	active = fields.Boolean('Active', default=False)	
+	state = fields.Selection(string='State', selection=[
+		('new', 'New'),
+		('offer_received', 'Offer Received'),
+		('offer_accepted', 'Offer Accepted'),
+		('sold', 'Sold'),
+		('cancelled', 'Cancelled')
+	], default='new')
+
