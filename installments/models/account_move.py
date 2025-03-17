2891d422a4a2243b71c7537dffacc6d9eca0136a
chdh-odoo <chdh@odoo.com>
2025-03-17 21:46:31 +0530

    [IMP] insallments: added pently logic

--- installments/models/account_move.py
+++ installments/models/account_move.py
@@ -3,4 +3,6 @@ class AccountMove(models.Model):
     _inherit = 'account.move'
 
     sale_order_id=fields.Many2one('sale.order')
+    penalty_invoice_id=fields.Many2one('account.move')
     penalty_applied = fields.Boolean(string="Penalty Applied", default=False)
+
--- installments/models/sale_order.py
+++ installments/models/sale_order.py
@@ -1,5 +1,6 @@
 from odoo import models,fields, Command
 from dateutil.relativedelta import relativedelta
+from datetime import timedelta
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
@@ -10,6 +11,8 @@ class SaleOrder(models.Model):
 
     def cron_recurring_create_installment_invoice(self):
         today = fields.Date.context_today(self)
+
+        #for regular installments
         sale_orders = self.search([('is_emi', '=', True), ('next_installment_date', '=', today)])
 
         # Fetch the installment product
@@ -17,12 +20,15 @@ class SaleOrder(models.Model):
         if not installment_product:
             raise ValueError("Installment product with ID 'product_product_installment' is missing.")
         
+        config = self.env['ir.config_parameter'].sudo()
+        down_payment_percentage = float(config.get_param('installment.down_payment_percentage', default=0.0))
+        annual_rate_percentage = float(config.get_param('installment.annual_rate_percentage', default=0.0))
+        max_duration = float(config.get_param('installment.max_duration', default=12))
+        admin_expenses_percentage = float(config.get_param('installment.admin_expenses_percentage', default=0.0))
+        delay_penalty_process = float(config.get_param('installment.delay_penalty_days', 0.0))
+        delay_penalty_percentage = float(config.get_param('installment.delay_penalty_percentage', default=0.0))
+
         for sale_order in sale_orders:
-            config = self.env['ir.config_parameter'].sudo()
-            down_payment_percentage = float(config.get_param('installment.down_payment_percentage', default=0.0))
-            annual_rate_percentage = float(config.get_param('installment.annual_rate_percentage', default=0.0))
-            max_duration = float(config.get_param('installment.max_duration', default=12))
-            admin_expenses_percentage = float(config.get_param('installment.admin_expenses_percentage', default=0.0))
 
             # Calculate financial values
             down_payment = (down_payment_percentage / 100) * sale_order.amount_total
@@ -44,12 +50,13 @@ class SaleOrder(models.Model):
                 'move_type': 'out_invoice',
                 'partner_id': sale_order.partner_id.id,
                 'invoice_date': today,
-                'invoice_payment_term_id': 'account.account_payment_term_end_following_month',
+                'invoice_date_due': fields.Datetime.now() +relativedelta(month=1)   ,
                 'invoice_line_ids': [
                     Command.create({
                         'product_id': installment_product.id,
                         'quantity': 1,
                         'price_unit': monthly_installment_amount,
+                        'tax_ids': None,
                     })
                 ],
                 'sale_order_id': sale_order.id,
@@ -59,3 +66,49 @@ class SaleOrder(models.Model):
 
             # Update the next installment date
             sale_order.next_installment_date = sale_order.next_installment_date + relativedelta(months=1) if sale_order.next_installment_date else today + relativedelta(months=1)
+
+        #For penalty invoices
+        delay_penalty_date = today - timedelta(days=delay_penalty_process)
+
+        # Fetch the penalty product
+        penalty_product = self.env.ref('installments.product_product_penalty', False)
+        invoices = self.env["account.move"].search([
+            ("move_type", "=", "out_invoice"),
+            ("state", "=", "draft"),
+            ('sale_order_id', "!=", False),
+            ("penalty_applied", "=", False),
+            ("invoice_date", "<=", delay_penalty_date),
+        ])
+        breakpoint()
+        for invoice in invoices:
+            #calculate penalty amount
+            penalty_amount=(delay_penalty_percentage/100)*invoice.amount_total
+            # Create the penalty invoice
+            invoice_vals = {
+                'move_type': 'out_invoice',
+                'partner_id': invoice.partner_id.id,
+                'invoice_date': today,
+                'invoice_date_due': fields.Datetime.now() + relativedelta(month=1),
+                'invoice_line_ids': [
+                    Command.create({
+                        'product_id': penalty_product.id,
+                        'quantity': 1,
+                        'price_unit': penalty_amount,
+                        'tax_ids': None,
+                    })
+                ],
+                'penalty_applied': True,
+                'sale_order_id': invoice.sale_order_id.id,
+            }
+            penalty_invoice = self.env['account.move'].create(invoice_vals)
+
+            invoice.write(
+                        {
+                            'penalty_applied': True,
+                            'penalty_invoice_id':penalty_invoice.id
+                        })
+            
+
+
+            
+
