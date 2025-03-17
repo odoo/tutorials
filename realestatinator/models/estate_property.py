396f45203f7674ddf9af66563306a4d4c82f94a6
gato-odoo <gato@odoo.com>
2025-03-17 17:16:20 +0100

    ch.6 - added search parameters, filters & group by

--- realestatinator/models/estate_property.py
+++ realestatinator/models/estate_property.py
@@ -7,18 +7,18 @@ class EstatePropery(models.Model):
 
 	sequence = fields.Integer('Sequence', default=0)
 	name = fields.Char('Name', required=True)
-	description = fields.Text()
-	postcode = fields.Char()
-	date_availability = fields.Date('Available date', copy=False, default=fields.Date.add(fields.Date.today(), months=+3))
-	expected_price = fields.Float()
-	selling_price = fields.Float('Price', readonly=True, copy=False)
+	description = fields.Text('Postcode')
+	postcode = fields.Char('Postcode')
+	date_availability = fields.Date('Available Date', copy=False, default=fields.Date.add(fields.Date.today(), months=+3))
+	expected_price = fields.Float('Expected Price')
+	selling_price = fields.Float('Selling Price', readonly=True, copy=False)
 	bedrooms = fields.Integer('Bedrooms', default=2)
 	living_area = fields.Integer('Living Area')
-	facades = fields.Integer()
+	facades = fields.Integer('Facades')
 	garage = fields.Boolean('Garage')
 	garden = fields.Boolean('Garden')
-	garden_area = fields.Integer()
-	garden_orientation = fields.Selection(string='Orientation', selection=[
+	garden_area = fields.Integer('Garden Area')
+	garden_orientation = fields.Selection(string='Garden Orientation', selection=[
 		('N', 'North'), 
 		('S', 'South'), 
 		('E', 'East'), 
