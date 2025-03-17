1b70376df913c1fc80912ddb6307e9cebfc5d4d0
gato-odoo <gato@odoo.com>
2025-03-17 14:00:05 +0100

    created estate_property model

--- realestatinator/models/estate_property.py
+++ realestatinator/models/estate_property.py
@@ -6,5 +6,17 @@ class EstatePropery(models.Model):
 	_order = 'sequence'
 
 	sequence = fields.Integer('Sequence', default=0)
-	name = fields.Char('Name', required=True, translate=True)
+	name = fields.Char(required=True)
+	description = fields.Text()
+	postcode = fields.Char()
+	date_availability = fields.Date()
+	expected_price = fields.Float()
+	selling_price = fields.Float()
+	bedrooms = fields.Integer()
+	living_area = fields.Integer()
+	facades = fields.Integer()
+	garage = fields.Boolean()
+	garden = fields.Boolean()
+	garden_area = fields.Integer()
+	garden_orientation = fields.Selection(string='Orientation', selection=[('N', 'N'), ('S', 'S'), ('E', 'E'), ('W', 'W')])
 	
