66515bd444d872ee00d997afd3655a7d1251391d
Odoo <daniele.ye03@gmail.com>
2025-03-17 16:19:53 +0100

    chapter 5 + chapter 6 list & form

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,22 +1,28 @@
 from odoo import fields, models
 
-class TestModel(models.Model):
+class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "estate property descripction"
 
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default = fields.Date.add(fields.Date.today(), months=+3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facedes = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
-        string='Type',
+        string='Garden Orientation',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        string='State',
+        selection = [('new', 'New'), ('offer received', 'Offer Received'), ('offer accepted', 'Offer Accpeted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')],
+        default = 'new'
     )
