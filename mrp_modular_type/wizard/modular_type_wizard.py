5dc051090ab5e7e585e8d52e325e700aeb7fe4dd
vrgo-odoo <vrgo@odoo.com>
2025-03-20 10:26:06 +0530

    [IMP] mrp_modular_type: improve naming conventions in modular wizard lines
    In this commit
    1. improve naming conventions in modular wizard lines. from value to
    modular_quantity and wizard_id to modular_type_wizard_id.

--- mrp_modular_type/wizard/modular_type_wizard.py
+++ mrp_modular_type/wizard/modular_type_wizard.py
@@ -8,7 +8,7 @@ class SaleOrderModularTypeWizard(models.TransientModel):
     sale_order_line_id = fields.Many2one(comodel_name='sale.order.line', required=True)
     modular_value_ids = fields.One2many(
         comodel_name='sale.order.modular.type.wizard.line',
-        inverse_name='wizard_id',
+        inverse_name='modular_type_wizard_id',
         string="Modular Type Values",
     )
 
@@ -22,7 +22,7 @@ class SaleOrderModularTypeWizard(models.TransientModel):
                 'modular_value_ids': [
                     (0, 0, {
                         'modular_type_id': modular_type.id,
-                        'value': modular_type.qty_multiplier
+                        'modular_quantity': modular_type.qty_multiplier
                     })
                     for modular_type in sale_order_line.product_id.product_tmpl_id.modular_type_ids
                 ],
@@ -31,13 +31,13 @@ class SaleOrderModularTypeWizard(models.TransientModel):
 
     def apply_modular_values(self):
         for modular_value_id in self.modular_value_ids:
-            modular_value_id.modular_type_id.write({'qty_multiplier': modular_value_id.value})
+            modular_value_id.modular_type_id.write({'qty_multiplier': modular_value_id.modular_quantity})
         self.env['sale.order.line'].browse(self.env.context.get('active_id')).write({'is_modular_type_set': True})
 
 class SaleOrderModularTypeWizardLine(models.TransientModel):
     _name = 'sale.order.modular.type.wizard.line'
     _description = 'Wizard Modular Type Value'
 
-    wizard_id = fields.Many2one(comodel_name='sale.order.modular.type.wizard', required=True, ondelete='cascade')
-    modular_type_id = fields.Many2one(comodel_name='modular.type', required=True)
-    value = fields.Float(default=1.0)
+    modular_type_wizard_id = fields.Many2one(comodel_name='sale.order.modular.type.wizard', required=True, ondelete='cascade')
+    modular_type_id = fields.Many2one(comodel_name='modular.type', required=True, string="Modular Type")
+    modular_quantity = fields.Float(default=1.0, string="Quantity")
