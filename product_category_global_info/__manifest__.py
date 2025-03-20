97c9970da88911e58da200f3c1d97c67c8c62302
Kunj Patel(kupa) <kupa@odoo.com>
2025-03-20 17:06:49 +0530

    [ADD] product_category_global_info: add required attribute in global info page
    -  Added global info and required attribute options in product category.
    -  Updated the product category form to include these new options.
    - Ensured sale order retrieves details for visible categories.
    - Created a new page in the sale order to display these details clearly.

---
+++ product_category_global_info/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Product Category Global Info",
+    'version': '1.0',
+    'summary': 'Extends product category with global info fields and a new tab grouping attributes by category.',
+    'description': """
+                This module extends the product category form by:
+                - Adding a boolean field "Show on Global Info"
+                - Adding a many2many field to map required attributes
+                It also adds a new tab called "Global Info" that displays category and attribute information grouped by product category.
+                """,
+    'author':'Odoo S.A.',
+    'category': 'Productivity',
+    'depends': ['stock', 'sale_management'],
+    'data': [
+        'views/product_category_global_info.xml',
+        'views/sale_order_view_inherit.xml',
+    ],
+    'license': 'AGPL-3'
+}
