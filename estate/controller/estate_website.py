06d32f2841982bb1126e69908d660e9ccd826f8b
Ashish Nirmal <asni@odoo.com>
2025-03-21 17:06:59 +0530

    [ADD] real_estate: enable users to create property listings from the website. Commit Description:  - added functionality to create property listings from the website.  - Implemented a form for submitting property details, including garden options,  area, and orientation.  - Integrated backend processing to handle property submissions  - Ensured proper validation and error handling for user inputs.

--- estate/controller/estate_website.py
+++ estate/controller/estate_website.py
@@ -1,3 +1,4 @@
+import base64
 from odoo import http
 from odoo.http import request
 
@@ -43,7 +44,12 @@ class EstateWebsite(http.Controller):
 
     @http.route('/aboutus', type='http', auth='public', website=True, methods=['GET'], csrf=False)
     def render_about_us_page(self):
-        Agents = request.env['res.users'].sudo().search([], limit=4)
+        Agents = request.env['res.users'].sudo().search([
+            ('groups_id', 'in', [
+                request.env.ref('estate.estate_group_manager').id,
+                request.env.ref('estate.estate_group_user').id
+            ])
+        ], limit=4)
         return request.render('estate.property_listing_about_us_page', {
             'Agents' : Agents,
         })
@@ -69,7 +75,7 @@ class EstateWebsite(http.Controller):
 
         property = request.env['estate.property'].sudo().browse(property_id)
         if not property.exists():
-            return request.redirect('/error?message="property does not exists"')
+            return request.redirect('/error?message="property not exists"')
 
         request.env['estate.property.offers'].sudo().create({
             'property_id': property.id,
@@ -79,3 +85,66 @@ class EstateWebsite(http.Controller):
         })
 
         return request.redirect('/properties')
+
+    @http.route('/create_new_property', type='http', auth='public', website=True, methods=['GET'])
+    def create_new_property(self, **kwargs):
+        property_tags = request.env['estate.property.tag'].sudo().search([])
+        property_types = request.env['estate.property.type'].sudo().search([])
+        return request.render('estate.property_listing_create_new_property', {
+            'property_types' : property_types,
+            'property_tags' : property_tags,
+        })
+
+    @http.route('/create_property', type='http', auth='public', website=True, methods=['POST'])
+    def create_property(self, **kwargs):
+        image_file = request.httprequest.files.get('image')
+
+        image_data = None
+        if image_file:
+            image_data = base64.b64encode(image_file.read())
+            image_data = image_data.decode('utf-8')
+
+        property_name = kwargs.get("name")
+        property_tag_ids = kwargs.get("property_tag_ids")
+        property_type_id = kwargs.get("property_type_ids")
+        postcode = kwargs.get("postcode")
+        availability_date = kwargs.get("date_availability")
+        expected_price = kwargs.get("expected_price")
+        property_description = kwargs.get("description")
+        bedrooms = kwargs.get("bedrooms")
+        living_area = kwargs.get("living_area")
+        facades = kwargs.get("facades")
+        garage = kwargs.get("garage") == "True"
+        garden = kwargs.get("garden") == "True"
+        garden_orientation = kwargs.get("garden_orientation")
+        garden_area = kwargs.get("garden_area") if garden else 0
+        is_published = kwargs.get("is_published") == 'on'
+
+        try:
+            expected_price = float(expected_price)
+        except ValueError:
+            return request.redirect('/error_page')
+
+        # Create property
+        new_property = request.env['estate.property'].create({
+            'name': property_name,
+            'active': True,
+            'property_type_id': int(property_type_id),
+            'property_tag_ids': int(property_tag_ids) if property_tag_ids else None,
+            'user_id': request.env.user.id,
+            'description': property_description,
+            'postcode': postcode,
+            'date_availability': availability_date,
+            'expected_price': expected_price,
+            'image': image_data,
+            'bedrooms': int(bedrooms) if bedrooms else 0,
+            'living_area': int(living_area) if living_area else 0,
+            'facades': int(facades) if facades else 0,
+            'garage': garage,
+            'garden': garden,
+            'garden_orientation' : garden_orientation,
+            'garden_area': int(garden_area),
+            'is_published': is_published,
+        })
+
+        return request.redirect('/property_success')
