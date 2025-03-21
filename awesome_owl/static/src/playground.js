1b00261ff503d935a80452299dc51dc6c142cd5e
gabriel (gato) <gato@odoo.com>
2025-03-21 10:02:43 +0100

    [ADD] Card: a basic card component
    A basic card component with a title and a description
    
    task-xxxxx

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,9 +1,10 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, markup } from "@odoo/owl";
 import { Counter } from './counter/counter';
+import { Card } from './card/card';
 
 export class Playground extends Component {
-    static template = "awesome_owl.playground";
-    static components = { Counter };
+	static template = "awesome_owl.playground";
+	static components = { Counter, Card };
 }
