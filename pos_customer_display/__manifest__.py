1c93a80ae467bdb86889a897cf594e0ebc6bcaaf
nmak-odoo <nmak@odoo.com>
2025-03-21 15:28:26 +0530

    [ADD] pos_customer_display: show customer name, order details, and refunds
    This module improves the POS Customer Display by providing structured
    information about the order:
    - Displays customer name if available, otherwise shows Guest.
    - Shows order amount and amount per guest for better transparency.
    - Separates refund items from regular order lines.

---
+++ pos_customer_display/__manifest__.py
@@ -0,0 +1,23 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "POS Customer Display",
+    "version": "1.0",
+    "category": "Point of Sale",
+    "description": """
+        Enhances the POS Customer Display by showing customer names, order details, 
+        payment and refund information in a structured format.
+    """,
+    "depends": ["point_of_sale"],
+    "installable": True,
+    "assets": {
+        'point_of_sale.assets_prod': [
+            'pos_customer_display/static/src/pos_order.js',
+        ],
+        'point_of_sale.customer_display_assets': [
+            'pos_customer_display/static/src/customer_display/*',
+        ],
+    },
+    "license": "LGPL-3",
+}
+
