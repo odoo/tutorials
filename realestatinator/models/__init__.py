426b023d3ffe74e3a33ca9a52482777feef88a00
gabriel (gato) <gato@odoo.com>
2025-03-24 10:12:08 +0100

    [ADD] realestatinator: Helps you manage real estate properties.

---
+++ realestatinator/models/__init__.py
@@ -0,0 +1,6 @@
+from . import res_users
+from . import res_partner
+from . import estate_property
+from . import estate_property_type
+from . import estate_property_tags
+from . import estate_property_offer
---
+++ realestatinator/models/estate_property_tags.py
@@ -0,0 +1,11 @@
+from odoo import fields, models
+
+class EstatePropertyTags(models.Model):
+	_name = 'estate.property.tags'
+	_description = 'estate property tag'
+	_order = 'name'
+	_sql_constraints = [
+		('name_unique', 'UNIQUE (name)', 'make sure tag name is unique.')
+	]	
+	name = fields.Char('Name', required=True)
+	color = fields.Integer('Colour')
