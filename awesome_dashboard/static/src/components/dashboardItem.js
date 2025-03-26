b144d4fba5bff2af868e039e2f584de5302fcc41
abpa-odoo <abpa@odoo>
2025-03-26 18:38:34 +0530

    [ADD] awesome_dashboard: add dashboard with dynamic layout and live statistics
    - Implement new dashboard layout using the Layout component with custom styling.
    - Add quick navigation buttons (Customers & Leads) via the action service.
    - Create a generic DashboardItem component with configurable sizing.
    - Integrate RPC calls for fetching business statistics and cache them
      using a custom statistics service.
    - Display a pie chart with lazy-loaded Chart.js to show t-shirt sales .
    - Enhance the statistics service to update data reactively every 10 min.

---
+++ awesome_dashboard/static/src/components/dashboardItem.js
@@ -0,0 +1,13 @@
+import { Component } from "@odoo/owl"
+
+export class DashboardItem extends Component{
+    static template = "awesome_dashboard.DashboardItem"
+    static props = {
+        size : {type: Number, optional:true, default:1},
+        slots: { type: Object, optional: true },
+    }
+    setup(){
+        
+
+    }
+}
--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,35 +1,3 @@
-# # type: ignore
-# from odoo import Command,exceptions,models
-
-
-# class EstateProperty(models.Model):
-#     _inherit = "estate.property"
-
-#     def action_set_sold(self):
-#         print("Estate Account: action_sold method overridden!") # debug for correctly calling this action
-#         if self.buyer_id:
-#                 invoice = self.env['account.move'].sudo().create({
-#                     'partner_id': self.buyer_id.id,
-#                     'move_type': 'out_invoice',
-#                     'journal_id': self.env['account.journal'].search([('type', '=', 'sale')], limit=1).id,
-#                     'invoice_line_ids': [
-#                         Command.create({
-#                             'name': f'Sale of {self.name} (6%)',
-#                             'quantity': 1,
-#                             'price_unit': self.selling_price * 0.06,
-#                         }),
-#                         Command.create({
-#                             'name': 'Administrative Fees',
-#                             'quantity': 1,
-#                             'price_unit': 100.00,
-#                         }),
-#                     ],
-#                 })
-#                 print(f"Invoice Created: {invoice.name}")
-#         else:
-#             raise exceptions.UserError("Buyer not set. Cannot create invoice.")
-#         return super().action_set_sold()
-
 # type: ignore
 from odoo import Command, exceptions, models
 
