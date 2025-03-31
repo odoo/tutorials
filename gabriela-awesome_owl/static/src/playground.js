9feddc5e8b98d0ad20852992ddac3fdcbdb53156
Sanzianu Gabriela <gabriela.sanzianu@mattealeconsulting.com>
2025-03-31 15:32:35 +0300

    Owl components

--- gabriela-awesome_owl/static/src/playground.js
+++ gabriela-awesome_owl/static/src/playground.js
@@ -1,16 +1,21 @@
 /** @odoo-module **/
 
-import { Component, useState } from "@odoo/owl";
+import { Component, markup, useState } from "@odoo/owl";
+import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
+import { TodoList } from "./todo_list/todo_list";
 
 export class Playground extends Component {
     static template = "gabriela-awesome_owl.playground";
+    static components = { Counter , Card, TodoList};
 
     setup() {
-        this.value = useState({value: 1});
+        this.str1 = "<div class='text-primary'>some content</div>";
+        this.str2 = markup("<div class='text-primary'>some content</div");
+        this.sum = useState({ value: 0});
     }
 
-    increment() {
-        this.state.value = this.state.value + 1;
+    incrementSum() {
+        this.sum.value++;
     }
-    
 }
