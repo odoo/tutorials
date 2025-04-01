890f5d12993ef2bd92ba683ad54086022763720c
chga-odoo <chga@odoo.com>
2025-04-01 16:06:50 +0530

    [ADD] zero_stock_blockage: Approval Feature for Orders with Zero Stock
    - Introduced a new option to allow Sales Administrators to confirm orders even
      if there is no stock available.
    - Made the option only editable by Sales Administrators to ensure control over
      when zero-stock orders are approved.
    - Updated the order form so that the approval option is visible only to the
      right people, and it becomes read-only for others.
    
    Task: 4686586

---
+++ zero_stock_blockage/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Zero Stock Blockage',
+    'version': '1.0',
+    'summary': 'Sales Managment Module',
+    'author': 'chirag Gami(chga)',
+    'category': 'Sales',
+    'depends': ['base','sale_management'],
+    'license':'LGPL-3',
+    'data': [
+        # Add your XML/CSV files if any
+        'views/sales_order_views.xml'
+    ],
+    'installable': True,
+    'application': True,
+}
