a4418090f2f1f73a5364c72aeb7270eb8a2e8d31
sris-odoo <sris@odoo.com>
2025-04-07 14:38:52 +0530

    [ADD] sale: Add `Zero Stock Approvalexchttps` field to allow SO confirmation
    In this commit
    -Added a new boolean field in Sale Order.
    -If set to true, it allows sales users to confirm the sale order.
    -Field is read-only for users in the Sales/User group.
    -Editable only by users in the Sales/Administrator group.

---
+++ zero_stock_blockage/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "Zero Stock Blockage",
+    'version': '1.0',
+    'depends': ['base', 'sale_management'],
+    'author': "Rishav Shah",
+    'category': 'sales',
+    'description': """
+    Zero Stock Blockage (Sales Order)
+    """,
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+    'data': [
+        'views/sale_order_views.xml',
+    ],
+}
