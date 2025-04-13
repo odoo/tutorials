afab0ac9838d8c5f0bd1e0d727036b5ddffb2168
alex <kadissa70@gmail.com>
2025-04-13 11:36:31 +0300

    chapter_7 computed_fields, inverse_function

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,6 +1,6 @@
 import datetime
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class EstateProperty(models.Model):
@@ -22,7 +22,7 @@ class EstateProperty(models.Model):
     facades = fields.Integer()
     garage = fields.Boolean(default=False)
     garden = fields.Boolean()
-    garden_area = fields.Integer('Garden Area (sqm)')
+    garden_area = fields.Integer()
     garden_orientation = fields.Selection([
         ('north', 'North'),
         ('south', 'South'),
@@ -45,6 +45,20 @@ class EstateProperty(models.Model):
                                      default=lambda self: self.env.user)
     tags_ids = fields.Many2many('estate.property.tag', string='Tags')
     offer_ids = fields.One2many('estate.property.offer', 'property_id')
+    total_area = fields.Integer(string='Total Area(sqm)',
+                                compute='_compute_total_area')
+    best_price = fields.Float(string='Best Price',
+                              compute='_compute_best_price')
+
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
+    def _compute_best_price(self):
+        for record in self:
+            record.best_price = max(
+                record.offer_ids.mapped('price')) if record.offer_ids else 0.0
 
 
 class EstatePropertyType(models.Model):
@@ -52,6 +66,7 @@ class EstatePropertyType(models.Model):
     _description = 'Estate Property Type'
 
     name = fields.Char(required=True)
+    property_ids = fields.One2many('estate.property', 'property_type_id')
 
 
 class EstatePropertyTag(models.Model):
@@ -72,3 +87,29 @@ class EstatePropertyOffer(models.Model):
         ('accepted', 'Accepted'),
         ('refused', 'Refused'),
     ], copy=False)
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Datetime('Deadline',
+                                    compute='_compute_date_deadline',
+                                    inverse='_inverse_date_deadline',
+                                    store=True)
+
+    @api.depends('validity', 'create_date')
+    def _compute_date_deadline(self):
+        """Вычисляет дату окончания на основе даты создания и срока действия"""
+        for record in self:
+            if record.create_date:
+                print(f'create_date: {record.create_date}')
+                record.date_deadline = record.create_date + datetime.timedelta(
+                    days=record.validity)
+            else:
+                record.date_deadline = fields.Date.today() + datetime.timedelta(
+                    days=record.validity)
+
+    def _inverse_date_deadline(self):
+        """Обратное вычисление - позволяет установить срок действия через изменение даты окончания"""
+        for offer in self:
+            if offer.create_date and offer.date_deadline:
+                create_date = fields.Date.from_string(offer.create_date)
+                deadline_date = fields.Date.from_string(offer.date_deadline)
+                # Вычисляем разницу в днях между датой создания и крайним сроком
+                offer.validity = (deadline_date - create_date).days
