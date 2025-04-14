e89447436a9829240ce9e6888f4bdb7794e2da72
mebh-odoo <mebh@odoo.com>
2025-04-14 19:50:47 +0530

    [IMP] owl_tutorials: added lifecycle methods for the fucntioning of code.

--- owl_tutorials/__manifest__.py
+++ owl_tutorials/__manifest__.py
@@ -1,33 +1,19 @@
 # -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 {
     'name': "owl_tutorials",
-
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-
     'description': """
-Long description of module's purpose
+    odoo owl tutorials practice
     """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
+    'author': "Odoo",
+    'website': "https://www.odoo.com",
+    'license': 'LGPL-3',
+    'version': '1.0',
     'depends': ['base', 'web'],
-
-    # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
     ],
     'assets': {
         'web.assets_backend': [
@@ -37,4 +23,3 @@ Long description of module's purpose
     'installable': True,
     'application': True,
 }
-
--- owl_tutorials/static/src/components/todo_list/todo_list.js
+++ owl_tutorials/static/src/components/todo_list/todo_list.js
@@ -1,14 +1,45 @@
 /** @odoo-module **/
 
-import { Component } from '@odoo/owl';
 import {registry} from '@web/core/registry';
-import {useState} from '@odoo/owl';
+import { Component, useState, onWillStart } from '@odoo/owl';
+import { useService } from '@web/core/utils/hooks';
 
 class OwlTodoList extends Component {
     static template= 'owl.TodoList';
     setup() {
-        this.state = useState({value:45});
-        
+        this.state = useState({
+            task:{name:"", color:"#ff0000", completed:false},
+            taskList:[],
+            isEdit: false,
+            activeId: false
+        });
+        this.orm = useService('orm');
+        this.model = "todo.list";
+
+        onWillStart(async ()=>{
+            await this.getAllTasks();
+        })
+
+    }
+
+    async getAllTasks(){
+        this.state.taskList = await this.orm.searchRead(this.model, [], ['name', 'color', 'completed'])
+    }
+
+    addTask(){
+
+    }
+
+    editTask(){
+
+    }
+
+    async saveTask(){
+        await this.orm.create(this.model, [{
+            name: this.state.task.name,
+            color: this.state.task.color,
+            completed: this.state.task.completed
+        }]);
     }
 }
 registry.category('actions').add('owl.action_todo_list_js', OwlTodoList)
