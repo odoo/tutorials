38bf690e88d1d532bb8c05f27091a78f7a60fdb4
mebh-odoo <mebh@odoo.com>
2025-04-17 15:30:13 +0530

    [IMP] owl_tutorials: added view inheritance for list and kanban view.

--- owl_tutorials/static/src/components/view_inheritance/res_partner_list_view.js
+++ owl_tutorials/static/src/components/view_inheritance/res_partner_list_view.js
@@ -3,17 +3,30 @@
 import { registry } from "@web/core/registry";
 import { listView } from "@web/views/list/list_view";
 import { ListController } from "@web/views/list/list_controller";
+import { useService } from "@web/core/utils/hooks";
 
 class ResPartnerListController extends ListController{
     setup(){
         super.setup()
         console.log("This is res parnter controller")
+        this.action = useService("action")
+    }
+
+    openSalesView(){
+        console.log("opened sales view")
+        this.action.doAction({
+            type: "ir.actions.act_window",
+            name: "Customer Sales",
+            res_model: "sale.order",
+            views: [[false, "list"], [false, "form"]]
+        })
     }
 }
 
 export const ResPartnerListView = {
     ...listView,
-    Controller: ResPartnerListController
+    Controller: ResPartnerListController,
+    buttonTemplate: "owl_tutorials.ResPartnerListView.Buttons"
 }
 
 registry.category("views").add("res_partner_list_view", ResPartnerListView )
