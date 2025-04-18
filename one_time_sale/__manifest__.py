83f9ecdea6adee290474116e1b9499ac13f485d6
ajka-odoo <ajka@odoo.com>
2025-04-18 18:53:14 +0530

    [ADD] one_time_sale: Add 'Accept One-Time' field to product template
    - Introduced a new Boolean field `accept_one_time` on `product.template`
    - Allows subscription products to be sold as one-time purchases
    - Added help text: Define if the subscription product can also be bought as
      a one-time.

--- one_time_sale/__manifest__.py
+++ one_time_sale/__manifest__.py
@@ -1,12 +1,13 @@
 {
     'name': 'one_time_sale',
     'version': '1.0',
-    'depends': ['sale_management','sale_subscription', 'website_sale'],
+    'depends': ['sale_management', 'sale_subscription', 'website_sale'],
     'category': 'Sale',
     'summary': 'Sell products one time',
     'license': 'LGPL-3',
     'data': [
         'views/product_template_views.xml',
+        'views/product_page_templates.xml',
     ],
     'installable': True,
     'auto_install': True,
--- one_time_sale/models/product_template.py
+++ one_time_sale/models/product_template.py
@@ -1,10 +1,24 @@
+# models/product_template.py
 from odoo import models, fields
 
+
+class SaleSubscriptionPlanInherit(models.Model):
+    _inherit = 'sale.subscription.plan'
+
+    # Add the Many2one field for the product template
+    product_template_id = fields.Many2one(
+        'product.template', string="Product Template", required=True, ondelete='cascade',
+        help="The product template to which this subscription plan belongs."
+    )
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     accept_one_time = fields.Boolean(
         string="Accept One-Time",
-        help="Define if the subscription product can also be bought as a one-time.",
+        help="Allow this subscription product to be sold as a one-time purchase."
+    )
+    subscription_plan_ids = fields.One2many(
+        'sale.subscription.plan', 'product_template_id',
+        string="Subscription Plans"
     )
