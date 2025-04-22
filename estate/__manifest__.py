163a287224db6f11ec940fd3e252ea9ced6f521b
mohannedah <mohannedahmed15@gmail.com>
2025-04-22 14:35:33 +0200

    [ADD] estate: adding new module
    Created the estate module with its necassary folder structure, and created the model with all its properties (at that moment).

---
+++ estate/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "estate",
+    "depends": [
+        "base"
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_actions.xml",
+        "views/estate_property_menus.xml"
+    ],
+    "application": True,
+    "installable": True
+}
---
+++ estate/models/estate_property.py
@@ -0,0 +1,22 @@
+from odoo import models, fields
+import datetime
+
+class Estate_Property(models.Model):
+    _name = "estate.property"
+    _description = "Estate Property"
+    name = fields.Char()
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(default=datetime.datetime.now(), copy=False)
+    expected_price = fields.Float()
+    selling_price = fields.Float(readonly=True, copyright=False, copy=False)
+    bedrooms = fields.Integer(default=4)
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    has_garage = fields.Boolean()
+    has_garden = fields.Boolean()
+    active = fields.Boolean(default=True)
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(selection = [("north", "North"), ("south", "South"), ("east", "East"), ("west", "West")])
+    status = fields.Selection(selection=[("new", "New"), ("offer_receieved", "Offer Recieved"), ("sold", "Sold")], readonly=True, default="new")
+
