90709054c4c7d60e926477b7a8832d1047c97a74
zorbmax <orban.maxime@gmail.com>
2025-04-22 14:43:51 +0200

    [IMP] Chapter 5: Finally, Some UI To Play With

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from collections import defaultdict
 from typing_extensions import Required
 from odoo import fields, models
+from dateutil import relativedelta
 
 
 class EstateProperty(models.Model):
@@ -12,10 +14,10 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False,default= fields.Date.today()+relativedelta.relativedelta(months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -25,3 +27,11 @@ class EstateProperty(models.Model):
         string='Orientation',
         selection=[('north', 'North'), ('south', 'South'),
         ('east', 'East'), ('west', 'West')])
+    active = fields.Boolean('Active', default=True)
+    state = fields.Selection(
+        string='State',
+        selection=[('new', 'New'), ('received', 'Offer Received'),
+        ('accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')],
+        default='new',
+        required=True,
+        copy=False)
