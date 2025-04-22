a78cf595dd74af648a99501517668911e3ffefc2
PiaCOS <elco@odoo.com>
2025-04-22 19:39:02 +0200

    [FIX] followed what the runbot was complaining about and a small fix  with the filter

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,17 +1,12 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "Real Estate",
-
-    'application': True,
-    'installable': True,
-    
-    'category': 'Tutorials/RealEstate',
-    'depends': ['base'],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/estate_property_views.xml',
-        'views/estate_menus.xml',
-
-    ]
-    
+    "name": "Real Estate",
+    "application": True,
+    "installable": True,
+    "category": "Tutorials/RealEstate",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
+    ],
 }
