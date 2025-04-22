f518ac9c630e58c65e15b7be31e73303de3d300e
Romain ROSANO (rros) <rros@odoo.com>
2025-04-22 16:21:41 +0200

    [IMP] estate: Chapter 5: Adding menus

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -7,16 +7,16 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(string="Available From")
     expected_price = fields.Float(required=True)
     selling_price = fields.Float()
-    berooms = fields.Integer()
-    living_area = fields.Integer()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer(string='Living Area (sqm)')
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Integer()
+    garden_area = fields.Integer(string="Garden Area (sqm)")
     garden_orientation = fields.Selection(
-        string='Orientation',
+        string='Garden orientation',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
         help="Allows to know where the sun is about the home")
