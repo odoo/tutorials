367306ec489019d8064de208e8039675add22ee8
Sherif Gabr <sherif.gabr@aucegypt.edu>
2025-04-22 17:07:15 +0200

    [IMP] estate: add menu and actions
    New XML for views of menus and actions for estate.property model

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,9 +4,10 @@
     'category': 'Tutorials',
     'depends': ['base'],
     'data': [
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
     ],
-    'installable': True,
     'application': True,
-    'auto_install': False
+    'license': "LGPL-3"
 }
