220f4fc919c09d4f722dfbb7659a9905c3828c25
Romain ROSANO (rros) <rros@odoo.com>
2025-04-22 15:06:15 +0200

    [IMP] estate: Chapter 2 done

---
+++ estate/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': "Estate",
+    'author': "Romain ROSANO (rros)",
+    'version': '0.1',
+    'license': 'LGPL-3',
+    'depends': ['base'],
+    'category': 'Tutorials/AwesomeClicker',
+    'application': True,
+    'installable': True,
+    'description': """
+    This module follows the onboarding training requirements.
+    This module allows to manage home selling. That's means homes can be added, modified and offers made by customers.  
+    """,
+    'data': [
+        'security/ir.model.access.csv',
+        ],
+}
