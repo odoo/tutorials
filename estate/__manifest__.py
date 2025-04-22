63f2346cd7d596dd51c2c5dd73590b376106bb8f
MrMiumo <miumo.contact@gmail.com>
2025-04-22 13:45:04 +0200

    [IMP] estate: Estate page basis - Chapter 5

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,6 +6,9 @@
     'application': True,
     'data': [
         'security/ir.model.access.csv',
+
+        'views/estate_property_views.xml',
+        'views/estate_menu.xml'
     ],
     'depends': [
         'base',
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta
 
 class RecurringPlan(models.Model):
     _name = "estate.property"
@@ -10,10 +11,10 @@ class RecurringPlan(models.Model):
     name = fields.Char('Property Name', required=True)
     description = fields.Text('Description')
     postcode = fields.Char('PostCode')
-    date_availability = fields.Date('Available From')
+    date_availability = fields.Date('Available From', copy=False, default=lambda self: fields.Date.today(self) + relativedelta(months=3)) # in 3 months
     expected_price = fields.Float('Expected Price', required=True)
-    selling_price = fields.Float('Selling Price')
-    bedrooms = fields.Integer('Bedrooms')
+    selling_price = fields.Float('Selling Price', copy=False, readonly=True)
+    bedrooms = fields.Integer('Bedrooms', default=2)
     living_area = fields.Integer('Living Area (sqm)')
     facades = fields.Integer('Facades')
     garage = fields.Boolean('Garage')
@@ -23,6 +24,15 @@ class RecurringPlan(models.Model):
         string='Garden Orientation',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
     )
+    state = fields.Selection(
+        string='State',
+        selection=[('new', 'New'), ('received', 'Offer Received'), ('accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')],
+        required=True,
+        copy=False,
+        default='new'
+        
+    )
+    active = fields.Boolean('Active', default=True)
 
     # _sql_constraints = [
     #     ('check_number_of_months', 'CHECK(number_of_months >= 0)', 'The number of month can\'t be negative.'),
