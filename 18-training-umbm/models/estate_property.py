1555e9f6ce2d7c2146cdf6ef0d1a6e4a16a8faed
Umberto <umbm@odoo.com>
2025-04-22 14:51:21 +0200

    [IMP] estate: add types, tags and offer models and link them to views

--- 18-training-umbm/models/estate_property.py
+++ 18-training-umbm/models/estate_property.py
@@ -18,5 +18,33 @@ class EstateProperty(models.Model):
     garden_area = fields.Integer(string="Garden area")
     garden_orentation = fields.Selection(string="Garden orientation", selection=[('north', 'North'),('south', 'South'), ('ouest', 'West'), ('east', 'East')])
     active = fields.Boolean(default=True)
-    
     states = fields.Selection(string="State", selection=[('new','New'),('offer_received','Offre Received'),('offer_accepted','Offer Accepted'),('sold','Sold'),('cancelled','Cancelled')])
+
+    tags_ids = fields.Many2many("estate_property_tags", string="Tags")
+    salesman_id = fields.Many2one("res.users", string="Salesman", default=lambda self: self.env.user)
+    buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
+    estate_property_type = fields.Many2one("estate_property_type", string="Property Type")
+    offer_ids = fields.One2many(comodel_name="estate_property_offer", inverse_name="property_id", string="Offers")
+
+
+class EstatePropertyType(models.Model):
+    _name = "estate_property_type"
+    _description= "property type"
+
+    name = fields.Char(string = "Name", required=True)
+
+class EstatePropertyTags(models.Model):
+    _name = "estate_property_tags"
+    _description= "property tags"
+
+    name = fields.Char(string = "Name", required=True)
+
+
+class EstatePropertyOffer(models.Model):
+    _name = "estate_property_offer"
+    _description = "property offers"
+
+    price = fields.Float(string="Price")
+    status = fields.Selection(string="Status", selection=[('accepted', 'Accepted'),('refused', 'Refused')])
+    partner_id = fields.Many2one("res.partner", string="Acheteur", required=True)
+    property_id = fields.Many2one("estate_property", string="Property", required=True)
