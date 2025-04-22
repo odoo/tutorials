cba07abdc5df4ce5e7457efe3d2b4526643995c1
Ana Zurabashvili [anzu] <anzu@odoo.com>
2025-04-22 22:22:09 +0200

    [ADD] estate: Chapter 2: real estate module initialization
    - initialization of Real estate module
    
    anzu@odoo.com

---
+++ estate/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Real Estate",
+    'summary': """
+        Real Estate Tutorial Project
+    """,
+    'description': """
+        Real Estate Tutorial Project
+    """,
+    'author': "Odoo",
+    'website': "https://www.odoo.com/",
+    'category': 'Tutorials/Real Estate',
+    'version': '18.0.1.0.0',
+    'application': True,
+    'installable': True,
+    'depends': ['base'],
+    'data': [
+    ],
+    'assets': {
+    },
+    'license': 'AGPL-3'
+}
