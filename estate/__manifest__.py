7f8fe80a487f86b9dc5f40b125b825ead6187d61
PiaCOS <elco@odoo.com>
2025-04-22 11:02:27 +0200

    [ADD] chapter 2

---
+++ estate/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Real Estate",
+
+    'application': True,
+    'installable': True,
+    
+    'category': 'Tutorials/RealEstate',
+    'depends': ['base'],
+    
+}
