71cbb29b8a21898a3d9314d2ceedcd584dc73441
Leo Leclerc <leol@odoo.com>
2025-04-22 16:38:25 +0200

    [IMP] Chapter 6: Basic Views

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,29 +1,22 @@
-# -*- coding: utf-8 -*-
 {
     "name": "Real Estate",
-
-    'summary': """
+    "summary": """
         Starting module for "Discover the JS framework, chapter 1: Owl components"
     """,
-
-    'description': """
+    "description": """
         Starting module for "Discover the JS framework, chapter 1: Owl components"
     """,
-
-    'author': "Odoo",
-    'website': "https://www.odoo.com",
-
-    'category': 'Tutorials/Real Estate',
-    'version': '0.1',
-
-    'application': True,
-    'installable': True,
-
-    'data': [
-        'security/ir.model.access.csv',
-        'views/estate_property_views.xml',
-        'views/estate_menus.xml',
+    "author": "Odoo",
+    "website": "https://www.odoo.com",
+    "license": "LGPL-3",
+    "category": "Tutorials/Real Estate",
+    "version": "0.1",
+    "application": True,
+    "installable": True,
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
     ],
-
-    'depends': ['base']
+    "depends": ["base"],
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-
 from dateutil import relativedelta
+
 from odoo import fields, models
 
 
@@ -8,42 +7,46 @@ class EstateProperty(models.Model):
     _name = "estate_property"
     _description = "Estate property"
 
-    name = fields.Char(string='Name of the Property',required=True)
-    description = fields.Text(string='Description')
-    postcode = fields.Char(string='Postcode')
+    name = fields.Char(string="Name of the Property", required=True)
+    description = fields.Text(string="Description")
+    postcode = fields.Char(string="Postcode")
     date_availability = fields.Date(
-        string='Date of Availability', 
-        copy=False, 
-        default=fields.Date.today() + relativedelta.relativedelta(months=3)
+        string="Date of Availability",
+        copy=False,
+        default=fields.Date.today() + relativedelta.relativedelta(months=3),
     )
-    expected_price = fields.Float(string='Expected Selling Price',required=True)
-    selling_price = fields.Float(string='Selling Price', readonly=True, copy=False)
-    bedrooms = fields.Integer(string='Number of Bedrooms', default=2)
-    living_area = fields.Integer(string='Number of Living Areas')
-    facades = fields.Integer(string='Number of Facades')
-    garage = fields.Boolean(string='Has a garage')
-    garden = fields.Boolean(string='Has a garden')
-    garden_area = fields.Integer(string='Number of garden Areas')
-    garden_orientation =fields.Selection(
-        string='Orientation of the Garden',
+    expected_price = fields.Float(
+        string="Expected Selling Price", required=True
+    )
+    selling_price = fields.Float(
+        string="Selling Price", readonly=True, copy=False
+    )
+    bedrooms = fields.Integer(string="Number of Bedrooms", default=2)
+    living_area = fields.Integer(string="Number of Living Areas")
+    facades = fields.Integer(string="Number of Facades")
+    garage = fields.Boolean(string="Has a garage")
+    garden = fields.Boolean(string="Has a garden")
+    garden_area = fields.Integer(string="Number of garden Areas")
+    garden_orientation = fields.Selection(
+        string="Orientation of the Garden",
         selection=[
-            ('north', 'North'), 
-            ('south', 'South'),
-            ('east', 'East'), 
-            ('west', 'West'),
+            ("north", "North"),
+            ("south", "South"),
+            ("east", "East"),
+            ("west", "West"),
         ],
     )
     active = fields.Boolean(default=True)
     state = fields.Selection(
-        string='State',
+        string="State",
         selection=[
-            ('new', 'New'),
-            ('received', 'Offer Received'),
-            ('accepted', 'Offer Accepted'),
-            ('sold', 'Sold'),
-            ('cancelled', 'Cancelled'),
+            ("new", "New"),
+            ("received", "Offer Received"),
+            ("accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled"),
         ],
         required=True,
         copy=False,
-        default='new',
+        default="new",
     )
