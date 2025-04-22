1c465897cdc6cb6a3b2189c7bccfcedd5ab11d3f
zorbmax <orban.maxime@gmail.com>
2025-04-22 17:05:12 +0200

    [IMP] Chapter 6

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,12 +1,14 @@
-# -*- coding: utf-8 -*-
 {
     "name": "Real Estate",
-    'category': 'Tutorials/Real Estate',
-    'version': '0.1',
-    'application': True,
-    'installable': True,
-    'depends': ['base'],
-    'data': ['security/ir.model.access.csv',
-            'views/estate_property_views.xml',
-            'views/estate_menus.xml']
+    "category": "Tutorials/Real Estate",
+    "version": "0.1",
+    "application": True,
+    "installable": True,
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
+    ],
+    "license": "LGPL-3",
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,37 +1,48 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from collections import defaultdict
-from typing_extensions import Required
-from odoo import fields, models
 from dateutil import relativedelta
+
+from odoo import fields, models
 
 
 class EstateProperty(models.Model):
     _name = "estate_property"
     _description = "Estate property"
 
-    name = fields.Char(required=True)
+    name = fields.Char("Title", required=True)
     description = fields.Text()
-    postcode = fields.Char()
-    date_availability = fields.Date(copy=False,default= fields.Date.today()+relativedelta.relativedelta(months=3))
-    expected_price = fields.Float(required=True)
-    selling_price = fields.Float(readonly=True, copy=False)
-    bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
+    postcode = fields.Char("Postcode")
+    date_availability = fields.Date(
+        "Available From",
+        copy=False,
+        default=fields.Date.today() + relativedelta.relativedelta(months=3),
+    )
+    expected_price = fields.Float("Expected Price", required=True)
+    selling_price = fields.Float("Selling Price", readonly=True, copy=False)
+    bedrooms = fields.Integer("Bedrooms", default=2)
+    living_area = fields.Integer("Living Area (sqm)")
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
-    garden_orientation =fields.Selection(
-        string='Orientation',
-        selection=[('north', 'North'), ('south', 'South'),
-        ('east', 'East'), ('west', 'West')])
-    active = fields.Boolean('Active', default=True)
+    garden_orientation = fields.Selection(
+        string="Orientation",
+        selection=[
+            ("north", "North"),
+            ("south", "South"),
+            ("east", "East"),
+            ("west", "West"),
+        ],
+    )
     state = fields.Selection(
-        string='State',
-        selection=[('new', 'New'), ('received', 'Offer Received'),
-        ('accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')],
-        default='new',
+        string="State",
+        selection=[
+            ("new", "New"),
+            ("received", "Offer Received"),
+            ("accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled"),
+        ],
+        default="new",
         required=True,
-        copy=False)
+        copy=False,
+    )
+    active = fields.Boolean("Active", default=True)
