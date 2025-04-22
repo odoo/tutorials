d13533282f4feeb206c339511ba47ebfc540fa46
Leo Leclerc <leol@odoo.com>
2025-04-22 10:59:25 +0200

    [ADD] Chapter 2: A New Application

---
+++ estate/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Real Estate",
+
+    'summary': """
+        Starting module for "Discover the JS framework, chapter 1: Owl components"
+    """,
+
+    'description': """
+        Starting module for "Discover the JS framework, chapter 1: Owl components"
+    """,
+
+    'author': "Odoo",
+    'website': "https://www.odoo.com",
+
+    'category': 'Tutorials/Real Estate',
+    'version': '0.1',
+
+    'application': True,
+    'installable': True,
+
+    'depends': ['base']
+}
