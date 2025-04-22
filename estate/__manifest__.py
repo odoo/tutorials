d7696ab18ac8f51653a5d6f6827d668e65d8af59
Francois Vasamillet (frva) <frva@odoo.com>
2025-04-22 17:16:01 +0200

    [IMP] estate: End of Chapter 5

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,7 +5,9 @@
         'base'
     ],
     'data':[
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
     ],
     'installable': True,
     'application': True
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,19 +1,22 @@
 from odoo import fields,models
 
 class EstateProperty(models.Model):
-    _name = "estate_property"
+    _name = "estate.property"
     _description = "Properties of an estate"
+    
 
     name = fields.Char('Estate Name',required=True, translate=True)
     description = fields.Text('Description')
     postcode = fields.Char('Postcode')
-    date_availability = fields.Date('Date Availability')
-    expected_price = fields.Float('Expected Price')
-    bedrooms = fields.Integer('Bedrooms')
+    date_availability = fields.Date('Date Availability', copy=False, default=fields.Date.add(fields.Date.today(),months=3))
+    expected_price = fields.Float('Expected Price',required=True)
+    selling_price = fields.Float("Selling Price", readonly=True, copy=False)
+    bedrooms = fields.Integer('Bedrooms',default=2)
     living_area = fields.Integer('Living Area')
     facades = fields.Integer('Facades')
     garage = fields.Boolean('Garage')
     garden = fields.Boolean('Garden')
     garden_area = fields.Integer('Garden Area')
     garden_orientation = fields.Selection(string='Type', selection=[('north', 'North'), ('south', 'South'),('west', 'West'), ('east', 'East')])
-    
+    active = fields.Boolean(default=True)
+    state = fields.Selection(required=True, copy=False, default='New', selection=[('New', 'New'), ('Offer Received', 'Offer Received'),('Offer Accepted', 'Offer Accepted'), ('Sold', 'Sold'), ('Cancelled', 'Cancelled')])
