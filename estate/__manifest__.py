5f6b7c67e183bdc4bd7ba051a369a3eb8363a332
Sherif Gabr <sherif.gabr@aucegypt.edu>
2025-04-22 14:09:42 +0200

    [ADD] estate: initial skeleton for new module for real estate
    New application for real estate purposes.

---
+++ estate/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'category': 'Tutorials',
+    'depends': ['base'],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
