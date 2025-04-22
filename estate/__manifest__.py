190c8905bc91cc3cb3d8223eb5698cb3e0d1aea6
zorbmax <orban.maxime@gmail.com>
2025-04-22 11:02:10 +0200

    [ADD] Chapter 2: A New Application

---
+++ estate/__manifest__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Real Estate",
+    'category': 'Tutorials/Real Estate',
+    'version': '0.1',
+    'application': True,
+    'installable': True,
+    'depends': ['base']
+}
