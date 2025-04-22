0b23021223a947eb5b363399c4f0fe9db0421361
Amr Yasser (amry) <amry@odoo.com>
2025-04-22 15:05:49 +0200

    [ADD] estate: adding new estate module
    Add manifest and dependencies...

---
+++ estate/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    'name': 'estate',
+    'application': True,
+    'depends': [
+        'base',
+    ]
+}
---
+++ estate/models/estate_property.py
@@ -0,0 +1,22 @@
+from odoo import fields,models
+
+
+class TestModel(models.Model):
+    _name = 'estate.property'
+    _description = "A test description"
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        string='Orientation',
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
+    )
