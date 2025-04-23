3c5ec6909a12c6c1fe5ddedc22428492ef2dd1a5
adak-ahmed <adak@odoo.com>
2025-04-23 15:08:08 +0200

    [IMP] realestate: add list,form,and search views to the module

---
+++ realestate/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name':'realestate',
+    'depends':[
+
+        'base'
+    ],
+    'data':[
+        "security/ir.model.access.csv",
+        "views/estate_view.xml"
+        
+       
+    ],
+
+    'application':True,
+    'installable':True,
+
+
+}
+
