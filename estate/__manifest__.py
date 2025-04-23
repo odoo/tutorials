4bd1002edf9f2692d505e852e97cf85bec5bb217
msho-odoo <msho@odoo.com>
2025-04-23 11:57:13 +0200

    [FIX] estate: fix linting and EOF in xml and csv files

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,7 +6,7 @@
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_menu.xml',
-            ],
+    ],
     'installable': True,
     'application': True,
     'auto_install': False,
