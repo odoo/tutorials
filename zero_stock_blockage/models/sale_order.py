942ecb107899f5d98697ccf9bfda41ebbcb10dcd
kame-odoo <kame@odoo.com>
2025-04-23 12:32:47 +0530

    [ADD] zero_stock_blockage: added custom module for zero stock approval
    This commit adds the zero_stock_blockage module, which extends the sale.order
    model to support zero stock approval workflow.
    Introduced a new field to manage zero stock approval on sale orders.
    Applied access control logic to restrict approval actions based on user roles.

---
+++ zero_stock_blockage/models/sale_order.py
@@ -0,0 +1,19 @@
+from odoo import api, models, fields
+from odoo.exceptions import UserError
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    stock_approval = fields.Boolean(string='Zero Stock Approvalexchttps')
+    stock_approval_readonly = fields.Boolean(compute='_compute_stock_approval_readonly')
+
+    @api.depends_context('uid')
+    def _compute_stock_approval_readonly(self):
+        self.stock_approval_readonly = self.env.user.has_group('sales_team.group_sale_manager')
+
+
+    def action_confirm(self):
+        for records in self:
+            if not records.stock_approval:
+                raise UserError("You are not allowed to confirm this order unless 'Zero Stock Approvalexchttps' is enabled.")
+        return super().action_confirm()
