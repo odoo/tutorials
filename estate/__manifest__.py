25cc11818cf830a931445e29c4d9f04c97ec6815
Parth Vyas <pavy@odoo.com>
2025-04-23 14:50:22 +0530

    [ADD] estate: initialize module with manifest file

---
+++ estate/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'estate',
+    'version': '0.0',
+    'category': '',
+    'summary': '',
+    'description': "",
+    'website': '',
+    'depends': [
+        'base',
+    ],
+    'installable': True,
+    'application': True,
+}
