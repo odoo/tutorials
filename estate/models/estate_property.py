8c00a6eb3fe52fc6e9ed834abf859520568150b5
Francois Vasamillet (frva) <frva@odoo.com>
2025-04-23 11:24:08 +0200

    [IMP] estate: End of chapter 6

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,18 +5,18 @@ class EstateProperty(models.Model):
     _description = "Properties of an estate"
     
 
-    name = fields.Char('Estate Name',required=True, translate=True)
+    name = fields.Char('Title',required=True, translate=True)
     description = fields.Text('Description')
     postcode = fields.Char('Postcode')
     date_availability = fields.Date('Date Availability', copy=False, default=fields.Date.add(fields.Date.today(),months=3))
     expected_price = fields.Float('Expected Price',required=True)
     selling_price = fields.Float("Selling Price", readonly=True, copy=False)
     bedrooms = fields.Integer('Bedrooms',default=2)
-    living_area = fields.Integer('Living Area')
+    living_area = fields.Integer('Living Area (sqm)')
     facades = fields.Integer('Facades')
     garage = fields.Boolean('Garage')
     garden = fields.Boolean('Garden')
     garden_area = fields.Integer('Garden Area')
-    garden_orientation = fields.Selection(string='Type', selection=[('north', 'North'), ('south', 'South'),('west', 'West'), ('east', 'East')])
+    garden_orientation = fields.Selection(string='Orientation Type', selection=[('north', 'North'), ('south', 'South'),('west', 'West'), ('east', 'East')])
     active = fields.Boolean(default=True)
     state = fields.Selection(required=True, copy=False, default='New', selection=[('New', 'New'), ('Offer Received', 'Offer Received'),('Offer Accepted', 'Offer Accepted'), ('Sold', 'Sold'), ('Cancelled', 'Cancelled')])
