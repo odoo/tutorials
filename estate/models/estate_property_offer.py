4954cab374e0cfbeaa3353f1d17c179121ca1584
Sherif Gabr <sherif.gabr@aucegypt.edu>
2025-04-23 17:37:16 +0200

    [IMP] estate: add new compute fields in estate property and estate property offer models
    New compute fields added to estate property model. Fields have compute, inverse, and onchange private methods to set appropriate fields.

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import api, fields, models
+from dateutil.relativedelta import relativedelta
 
 
 class PropertyOffer(models.Model):
@@ -15,3 +16,17 @@ class PropertyOffer(models.Model):
     )
     partner_id = fields.Many2one("res.partner", string="Partner", required=True)
     property_id = fields.Many2one("estate.property", string="Property", required=True)
+
+    validity = fields.Integer(string="Validity (days)", default=7)
+    date_deadline = fields.Date(string="Deadline", compute="_compute_date_deadline", inverse="_inverse_date_deadline")
+
+    @api.depends("validity")
+    def _compute_date_deadline(self):
+        for record in self:
+            if record.create_date and record.validity:
+                record.date_deadline = record.create_date + relativedelta(days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            if record.create_date and record.date_deadline:
+                record.validity = (record.date_deadline - record.create_date.date()).days
