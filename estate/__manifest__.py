c33277daabe02f7083563cfb51bd32a6844cce69
odoo <xafr@odoo.com>
2025-04-23 14:37:19 +0200

    [ADD] Chapter 1: Manifest & init

---
+++ estate/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "XAFR's training estate module",
+    'version': '1.0',
+    'summary': 'Training sandbox',
+    'depends': [
+        'base'
+    ],
+    'author': "XAFR",
+    'description': """
+    An application module that aims to serve as an onboarding sandbox.
+    """,
+    'application': "True",
+}
