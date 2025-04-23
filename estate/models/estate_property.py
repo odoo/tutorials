0c863ac7d261cefe0c258ce362cb67f67e2673a9
msho-odoo <msho@odoo.com>
2025-04-23 11:51:51 +0200

    [IMP] estate: add new fields and fileds default values to property form

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta
 
 
 class EstateProperty(models.Model):
@@ -8,10 +9,11 @@ class EstateProperty(models.Model):
     name = fields.Char('name', required=True)
     description = fields.Text('description')
     postcode = fields.Char('postcode')
-    date_availability = fields.Date('availabilty date')
+    date_availability = fields.Date(
+        'availabilty date', copy=False, default=fields.Date.today()+relativedelta(months=3))
     expected_price = fields.Float('expected price', required=True)
-    selling_price = fields.Float('selling price')
-    bedrooms = fields.Integer('bedrooms')
+    selling_price = fields.Float('selling price', readonly=True, copy=False)
+    bedrooms = fields.Integer('bedrooms', default=2)
     living_area = fields.Integer('living area')
     facades = fields.Integer('facades')
     garage = fields.Boolean('garage')
@@ -20,4 +22,12 @@ class EstateProperty(models.Model):
         string='Garden Orientation',
         selection=[('North', 'North'), ('South', 'South'),
                    ('East', 'East'), ('West', 'West')]
+    )
+    active = fields.Boolean('active', default=True)
+    state = fields.Selection(
+        string='State',
+        selection=[('new', 'New'), ('offer_received', 'Offer Received'),
+                   ('offer_accepted', 'Offer Acccepted'), ('sold', 'Sold'),
+                   ('cancelled', 'Cancelled')],
+        default="new"
     )
