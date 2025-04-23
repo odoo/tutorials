29c0511784e8ec46557591ddd5b7f9da3656750e
abeg-odoo <abeg@odoo.com>
2025-04-23 06:17:04 +0200

    [ADD] estate: added a new estate module
    We have added a module as an application with a model a some basic properties like (name, description, expected price ...etc)
    We have added security rules for read, write, create and delete actions on this model
    We have added menu items and UI interaction to access a form view of the records we create and we also added attributes to some
    properties of the model like (copy, default, readonly, required)

---
+++ estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Estate',
+    'depends': ['base'],
+    'application': True,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
+        ]
+}
---
+++ estate/models/estate_property.py
@@ -0,0 +1,45 @@
+from odoo import models, fields
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+def _default_date_availability():
+    return fields.Date.today() + relativedelta(months=3)
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Real Estate Property Information"
+
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(
+        copy=False,
+        default=_default_date_availability()
+    )
+    state = fields.Selection(
+        string='Status',
+        required=True,
+        copy=False,
+        default='new',
+        selection=[
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('cancelled', 'Cancelled')
+        ],
+    )
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        string='Garden Orientation',
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
+        help="Orientation of the garden"
+    )
+    active = fields.Boolean(default=True)
