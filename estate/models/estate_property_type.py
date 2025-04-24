ab764a67ccef1c637b425e1c3edee2beca4d5b4b
Umberto <umbm@odoo.com>
2025-04-24 11:50:56 +0200

    [IMP] estate: make it geen!

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -9,7 +9,7 @@ class EstatePropertyType(models.Model):
     name = fields.Char(string="Name", required=True)
     property_ids = fields.One2many("estate_property", "estate_property_type", string="Properties")
     sequence = fields.Integer('Sequence', default=1, help="Used to order stages. Lower is better.")
-    
+
     offer_ids = fields.One2many(comodel_name='estate_property_offer', inverse_name='property_type_id', string="Related offers")
     offer_count = fields.Integer(compute='_compute_offer_count')
 
