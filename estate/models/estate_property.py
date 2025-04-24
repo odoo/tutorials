fc150fb7ccad4df2b15c478b0c7d7659f4746503
msho-odoo <msho@odoo.com>
2025-04-24 11:56:18 +0200

    [IMP] estate: added accept and refuse offer buttons

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import api, fields, models
+from odoo.exceptions import UserError
 from dateutil.relativedelta import relativedelta
 
 
@@ -7,7 +8,7 @@ class EstateProperty(models.Model):
     _description = "Estate Property discription"
 
     name = fields.Char('name', required=True)
-    description = fields.Text('description', compute="_compute_description")
+    description = fields.Text('description')
     postcode = fields.Char('postcode')
     availability_date = fields.Date(
         'availabilty date', copy=False, default=fields.Date.today() + relativedelta(months=3))
@@ -48,21 +49,11 @@ class EstateProperty(models.Model):
         for record in self:
             record.total_area = record.living_area + record.garden_area
 
-    @api.depends("buyer_id")
-    def _compute_description(self):
-        for record in self:
-            record.description = "Description for buyer %s" % record.buyer_id.name
-
     @api.depends("offer_ids")
     def _compute_best_offer(self):
-        try:
-            for record in self:
-                if len(record.offer_ids) > 0:
-                    record.best_offer = max(record.offer_ids.mapped("price"))
-                else:
-                    record.best_offer = 0.0
-        except Exception as e:
-            print(e)
+        for record in self:
+            record.best_offer = max(record.offer_ids.mapped(
+                "price")) if record.offer_ids else 0.0
 
     @api.onchange("garden")
     def _onchange_garden(self):
@@ -72,3 +63,19 @@ class EstateProperty(models.Model):
         else:
             self.garden_area = 0
             self.garden_orientation = None
+
+    def action_sell(self):
+        for record in self:
+            if record.state == "cancelled":
+                raise UserError("You can't sell a cancelled property")
+            record.state = "sold"
+
+        return True
+
+    def action_cancel(self):
+        for record in self:
+            if record.state == "sold":
+                raise UserError("You can't cancel a sold property")
+            record.state = "cancelled"
+
+        return True
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
 from odoo import api, models, fields
+from odoo.exceptions import UserError
 from dateutil.relativedelta import relativedelta
 
 
@@ -17,18 +18,30 @@ class EstatePropertyOffer(models.Model):
     date_deadline = fields.Date(
         compute="_compute_date_deadline", inverse="_inverse_date_deadline")
 
-    @api.depends("validity")
+    @api.depends("validity", "create_date")
     def _compute_date_deadline(self):
         for record in self:
-            start_date = fields.Date.today()
-            if record.create_date:
-                start_date = record.create_date
+            start_date = record.create_date.date() if record.create_date else fields.Date.today()
             record.date_deadline = start_date + \
                 relativedelta(days=record.validity)
 
     def _inverse_date_deadline(self):
         for record in self:
-            start_date = fields.Date.today()
-            if record.create_date:
-                start_date = record.create_date
-            record.validity = (record.date_deadline - fields.Date.today()).days
+            start_date = record.create_date.date() if record.create_date else fields.Date.today()
+            record.validity = (record.date_deadline - start_date).days
+
+    def action_accept(self):
+        for record in self:
+            if record.property_id.buyer_id:
+                raise UserError("You already accepted an offer")
+            if record.status == "refused":
+                raise UserError("You already refused this offer")
+            record.status = "accepted"
+            record.property_id.selling_price = record.price
+            record.property_id.buyer_id = record.partner_id
+
+    def action_refuse(self):
+        for record in self:
+            if record.status == "accepted":
+                raise UserError("You already accepted this offer")
+            record.status = "refused"
