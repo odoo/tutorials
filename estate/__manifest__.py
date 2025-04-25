df29c68083d53e55a87e1a3b133b0e04837b6e8c
abeg-odoo <abeg@odoo.com>
2025-04-25 14:42:14 +0200

    [FIX] estate: fixed data import order
    Fixed views imports in the data field in the manifest file

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,8 +6,8 @@
     'data': [
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
+        'views/estate_property_offers_views.xml',
         'views/estate_property_type_views.xml',
         'views/estate_property_tags_views.xml',
-        'views/estate_property_offers_views.xml',
         'views/estate_menus.xml']
 }
