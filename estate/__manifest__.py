6675b932d9a28c342f36ff1c7112ff934a9964f4
sben-odoo <sben@odoo.com>
2025-04-25 17:05:28 +0200

    [IMP] add chapter 15

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -12,6 +12,7 @@
         'views/estate_property_views.xml',
         'views/estate_property_type_views.xml',
         'views/res_users_views.xml',
+        'views/estate_property_offer.xml',
         'views/estate_menus.xml',
     ],
     'license': 'LGPL-3',
