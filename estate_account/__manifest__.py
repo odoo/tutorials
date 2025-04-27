88d2ba0f229d44a8ca586ee8bbb95b1a99cf41d9
Romain ROSANO (rros) <rros@odoo.com>
2025-04-27 23:23:52 +0200

    [ADD] estate_account: Chapter 13: Adding module

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Estate Account",
+    'author': "Romain ROSANO (rros)",
+    'version': '0.1',
+    'license': 'LGPL-3',
+    'depends': ['base', 'estate', 'account'],
+    'category': 'Tutorials/Server',
+    'application': True,
+    'installable': True,
+    'description': """
+    This module follows the onboarding training to extend estate and account modules.
+    """,
+    'data': [
+        ],
+}
