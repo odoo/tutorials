b128776eeabc81c61e83b83090d47c2b7774685a
ardo-odoo <ardo@odoo.com>
2025-04-27 19:13:51 +0530

    [ADD] estate: Add the base estate and estate_account modules
    - Add the base estate and estate_account modules to build auctioning upon

---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,32 @@
+from odoo import Command, models
+
+
+class EstateProperty(models.Model):
+    _inherit = 'estate.property'
+
+    def action_sold(self):
+        """Automatically create an invoice for the property sale."""
+        self.check_access('write')
+        if super().action_sold() is True:
+            return (
+                self.env['account.move']
+                .sudo()
+                .create({
+                    'partner_id': self.buyer_id.id,
+                    'move_type': 'out_invoice',
+                    'invoice_line_ids': [
+                        Command.create({
+                            'name': self.name,
+                            'quantity': 1,
+                            'price_unit': self.selling_price * 0.06,
+                        }),
+                        Command.create({
+                            'name': 'Administrative Fees',
+                            'quantity': 1,
+                            'price_unit': 100,
+                        }),
+                    ],
+                })
+            )
+
+        return False
