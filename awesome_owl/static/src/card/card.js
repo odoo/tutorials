4f9e253a26f26f067b6fa481722f6bfa0b5894ba
Francois Vasamillet (frva) <frva@odoo.com>
2025-04-29 16:13:21 +0200

    [IMP] awesome_owl: component, subcomponent and props

---
+++ awesome_owl/static/src/card/card.js
@@ -0,0 +1,5 @@
+import { Component, useState } from "@odoo/owl";
+
+export class Card extends Component {
+    static template = "Card";
+}
--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,7 +1,10 @@
-/** @odoo-module **/
-
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
+import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
+
+    static components = { Counter, Card };
+    state = useState({ title: "string", content: "string"})
 }
