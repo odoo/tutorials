582ad2e12b68af5a32f0769c71c529b4f32cdaf1
Romain ROSANO (rros) <rros@odoo.com>
2025-04-29 16:10:25 +0200

    [IMP] awesome_owl: Chapter 2: sub component

---
+++ awesome_owl/static/src/counter/counter.js
@@ -0,0 +1,13 @@
+/** @odoo-module **/
+
+import { useState, Component } from "@odoo/owl";
+
+export class Counter extends Component {
+    static template = "awesome_owl.counter";
+    
+    state = useState({ value: 0 });
+    
+    increment() {
+        this.state.value++;
+    }
+}
--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,13 +1,11 @@
 /** @odoo-module **/
 
-import { useState, Component } from "@odoo/owl";
+import { Component } from "@odoo/owl";
+
+import { Counter } from "./counter/counter";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
-    
-    state = useState({ value: 0 });
-    
-    increment() {
-        this.state.value++;
-    }
+
+    static components = { Counter };
 }
