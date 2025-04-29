7aecb447db0b5a147bd0e0d7913195e901851e64
Romain ROSANO (rros) <rros@odoo.com>
2025-04-29 13:36:38 +0200

    [IMP] awesome_owl: Chapter 1 (done): Clicker button

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,7 +1,13 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { useState, Component } from "@odoo/owl";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
+    
+    state = useState({ value: 0 });
+    
+    increment() {
+        this.state.value++;
+    }
 }
