459b6a55b7e6eae4ab0c20643e1293b940d2b77c
msho-odoo <msho@odoo.com>
2025-04-30 10:13:18 +0200

    [IMP] awesome_owl: add props validation in card component

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,9 +1,13 @@
 /** @odoo-module **/
 
-import { Component, useState } from "@odoo/owl";
+import { Component, markup } from "@odoo/owl";
 import { Counter } from "./counter";
+import { Card } from "./card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
-    static components = { Counter };
+    static components = { Counter, Card };
+
+    card1Content = markup("<div>Some Text in div Tag</div>");
+    card2Content = "<div>Some Text in div Tag</div>";
 }
