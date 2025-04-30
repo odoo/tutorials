9e33a7ae88377df0de502e5cff2bd83f13176f03
Romain ROSANO (rros) <rros@odoo.com>
2025-04-30 09:35:09 +0200

    [IMP] awesome_owl: Chapter 4 (done): html displayed card

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { markup, Component } from "@odoo/owl";
 
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
@@ -8,4 +8,10 @@ import { Card } from "./card/card";
 export class Playground extends Component {
     static template = "awesome_owl.playground";
     static components = { Counter, Card };
+
+    title_card_2 = "A not very beautiful card :(";
+    content_card_2 = markup("<font color='green'>This one is not so rare...</font>");
+
+    title_card_3 = "Basic card";
+    content_card_3 = "<font color='blue'>This is not a basic card...</font>";
 }
