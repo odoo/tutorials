89d84e78dad69b6476de00b26fabafd5d2301443
Francois Vasamillet (frva) <frva@odoo.com>
2025-04-30 09:58:56 +0200

    [IMP] awesome_owl: markup, props validation and callback prop

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -1,5 +1,6 @@
-import { Component, useState } from "@odoo/owl";
+import { Component } from "@odoo/owl";
 
 export class Card extends Component {
     static template = "Card";
+    static props = ["title", "content"];
 }
--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,10 +1,19 @@
-import { Component, useState } from "@odoo/owl";
+import { Component, useState, markup } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
-
     static components = { Counter, Card };
-    state = useState({ title: "string", content: "string"})
+
+    setup(){
+        this.state = useState({ title: "string", content: "string" });
+        this.html_value_1 = "<div>some text 1</div>";
+        this.html_value_2 = markup("<div>some text 2</div>");
+        this.sum = useState({ value: 3 })
+    }
+
+    incrementSum(){
+        this.sum.value++;
+    }
 }
