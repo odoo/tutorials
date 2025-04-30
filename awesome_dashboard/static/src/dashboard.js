f0b333fb4b31993a85aa7fd4f1c414a913c97c7f
MrMiumo <miumo.contact@gmail.com>
2025-04-30 16:26:50 +0200

    [IMP] awesome_dashboard: Call the server, add some statistics - Chapter 2

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,8 +1,9 @@
-import { Component } from "@odoo/owl";
+import { Component, onWillStart, useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
 import { useService } from "@web/core/utils/hooks";
 import { Card } from "./card/card"
+import { rpc } from "@web/core/network/rpc";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
@@ -11,6 +12,20 @@ class AwesomeDashboard extends Component {
     setup() {
         this.display = { controlPanel: {} };
         this.action = useService("action");
+        this.avg_quantity = useState({ value: 0 });
+        this.avg_time = useState({ value: 0 });
+        this.nb_cancelled_orders = useState({ value: 0 });
+        this.nb_new_orders = useState({ value: 0 });
+        this.total_amount = useState({ value: 0 });
+
+        onWillStart(async () => {
+           const result = await rpc("/awesome_dashboard/statistics");
+           this.avg_quantity.value = result.average_quantity
+           this.avg_time.value = result.average_time;
+           this.nb_cancelled_orders.value = result.nb_cancelled_orders;
+           this.nb_new_orders.value = result.nb_new_orders;
+           this.total_amount.value = result.total_amount;
+        });
     }
 
     openCustomers() {
