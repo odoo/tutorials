d48b0aea95ce2fcdc1eb4c77d143787ffd25093d
Francois Vasamillet (frva) <frva@odoo.com>
2025-05-02 15:26:38 +0200

    [IMP] awesome_dashboard: Add some buttons for quick navigation (2)

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,6 +1,7 @@
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
 
 
 class AwesomeDashboard extends Component {
@@ -9,8 +10,21 @@ class AwesomeDashboard extends Component {
 
     setup(){
         this.myDisplay = {controlPanel: {}};
+        this.action = useService("action");
     }
 
+    openCustomersView(){
+        this.action.doAction("base.action_partner_form");
+    }
+
+    openLeads(){
+        this.action.doAction({
+            type: 'ir.actions.act_window',
+            name: 'All leads',
+            res_model: 'crm.lead',
+            views: [[false, 'list'],[false, 'form']],
+        });
+    }
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
