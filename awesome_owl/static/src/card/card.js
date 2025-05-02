eb27bd5de4e8c971839375a8f67f1fcd7a8f8106
Sherif Gabr <sherif.gabr@aucegypt.edu>
2025-05-02 10:46:55 +0200

    [IMP] awesome_owl: add owl components
    New components that utilize the OWL framework. The components include Counter, Card, and Todo list.

---
+++ awesome_owl/static/src/card/card.js
@@ -0,0 +1,17 @@
+import { Component, useState } from "@odoo/owl";
+
+export class Card extends Component {
+    static template = "awesome_owl.card";
+    static props = {
+        title: String,
+        slots: { type: Object, optional: true },
+    };
+
+    setup() {
+        this.state = useState({ open: true });
+    }
+
+    toggle() {
+        this.state.open = !this.state.open;
+    }
+}
--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,7 +1,21 @@
-/** @odoo-module **/
-
-import { Component } from "@odoo/owl";
+import { markup, Component, useState } from "@odoo/owl";
+import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
+import { TodoList } from "./todo_list/todo_list";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
+    static components = { Counter, Card, TodoList };
+
+    nonescaped_html = "<div>some content</div>";
+    escaped_html = markup("<div>some content</div>");
+
+    setup() {
+        this.incrementSum = this.incrementSum.bind(this);
+        this.state = useState({ sum: 2 });
+    }
+
+    incrementSum() {
+        this.state.sum++;
+    }
 }
