19931855156a25b8f2c40625a1697bfa1a51f295
Romain ROSANO (rros) <rros@odoo.com>
2025-05-05 10:32:12 +0200

    [IMP] awesome_dashboard: Chapter 2 (done): Quick navigation

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -3,10 +3,27 @@
 import { Component } from "@odoo/owl";
 import { Layout } from "@web/search/layout";
 import { registry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
     static components = { Layout };
+
+    setup() {
+        this.action = useService("action");
+    }
+
+    showKanbanCustomersView() {
+        this.action.doAction("base.action_partner_form");
+    }
+
+    showLeads() {
+        this.action.doAction({
+            type: 'ir.actions.act_window',
+            res_model: 'crm.lead',
+            views: [[false, 'list'], [false, 'form']],
+        });
+    }
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
