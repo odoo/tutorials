74d287bc750a6bc725a1cf336ac4e266416b149b
Francois Vasamillet (frva) <frva@odoo.com>
2025-05-07 08:51:55 +0200

    [ADD] estate: Model, fields, security and views

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,18 +1,15 @@
 {
-    'name': 'frva-estate',
+    'name': 'estate',
     'license': 'LGPL-3',
     'depends': [
-        'base'
+        'base',
     ],
     'data': [
+        'models/model_real_estate_property.xml',
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
-        'views/estate_property_tag_views.xml',
-        'views/estate_property_offer_views.xml',
-        'views/estate_property_type_views.xml',
         'views/estate_menus.xml',
-        'views/inherited_users_view.xml'
     ],
-    'installable': True,
-    'application': True
+    "installable": True,
+    "application": True,
 }
