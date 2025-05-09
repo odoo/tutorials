bc02beffe6fc9d613ecf5165c04d98ab0c3589aa
Francois Vasamillet (frva) <frva@odoo.com>
2025-05-09 10:37:36 +0200

    [IMP] estate: Adding simple tour and adding basic demo data

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -9,12 +9,22 @@
         'models/model_real_estate_property_tag.xml',
         'models/model_real_estate_property_offer.xml',
         'models/model_real_estate_property.xml',
+        'models/estate_property_offer_2.xml',
         'security/ir.model.access.csv',
+        'data/estate_tour.xml',
         'views/estate_property_type_views.xml',
         'views/estate_property_tag_views.xml',
         'views/estate_property_offer_views.xml',
         'views/estate_property_views.xml',
         'views/estate_menus.xml',
+    ],
+    "assets": {
+        "web.assets_backend": [
+            "estate/static/src/js/tour.js",
+        ],
+    },
+    "demo": [
+        "demo/x_estate.property.type.csv",
     ],
     "installable": True,
     "application": True,
