c134cd35cbe8563ea621d30f2650e7d8fef3ec30
abhk-odoo <abhk@odoo.com>
2025-05-09 18:57:33 +0530

    [FIX]  estate: solve reference ID issue and change sequence for the menuitem
    Correct external ID reference for 'action_estate_property_offer' in
    estate module
    
    Change menuitem sequence for the all tab

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -9,11 +9,11 @@
     'data': [
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
+        'views/estate_property_offer_views.xml',
         'views/estate_property_type_views.xml',
         'views/estate_property_tag_views.xml',
-        'views/estate_property_offer_views.xml',
         'views/inherited_user_views.xml',
-        'views/estate_property_menu.xml',
+        'views/estate_property_menu.xml'
     ],
     'application': True,
     'installable': True,
