d96fcb5000481eee94f07419d0a672ebf0eb5b0f
kame-odoo <kame@odoo.com>
2025-05-09 10:27:13 +0530

    [ADD] pos_layout: add Configure Receipt to open layout selection wizard
    This commit adds a Configure Receipt button to the POS settings view by
    inheriting res.config.settings. When clicked, it opens a wizard allowing
    users to select the receipt layout from three options: Default, Boxed, or
    Lined , also allows you to change header footer and logo of the same.

---
+++ pos_layout/models/res_config_settings.py
@@ -0,0 +1,29 @@
+from odoo import fields, models
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    pos_disp_type = fields.Selection([
+        ('default', 'Default'),
+        ('lined', 'Lined'),
+        ('boxed', 'Boxed'),
+    ], string="POS Recipt Layout", related='pos_config_id.pos_disp_type')
+
+    def action_open_receipt_preview_wizard(self):
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'POS Receipt Preview',
+            'res_model': 'pos.receipt.preview.wizard',
+            'view_mode': 'form',
+            'target': 'new',
+            'context': {'active_id': self.ids},
+        }
+
+class PosConfig(models.Model):
+    _inherit='pos.config'
+
+    pos_disp_type = fields.Selection([
+        ('default', 'Default'),
+        ('lined', 'Lined'),
+        ('boxed', 'Boxed'),
+    ],string="POS Recipt Layout",store=True)
