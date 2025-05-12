60ae1cd369fbbe6c87c521b1d39a7186765ea803
kame-odoo <kame@odoo.com>
2025-05-12 14:11:59 +0530

    [FIX] pos_layout: remove unused code and correct spacing issues
    Cleaned up the pos_layout module by removing unused variables
    and redundant code in the controller. Also fixed minor spacing and
    indentation inconsistencies to improve code readability and
    maintain coding standards.

--- pos_layout/controllers/res_config_param.py
+++ pos_layout/controllers/res_config_param.py
@@ -5,7 +5,7 @@ from odoo.http import request
 class POSConfigInfo(http.Controller):
 
     @http.route("/pos/get_display_layout", type='json', auth='user')
-    def get_display_layout(self,config_id=None):
+    def get_display_layout(self, config_id=None):
         pos_config = request.env['pos.config'].browse(config_id)
         disp_layout = pos_config.pos_disp_type or 'default'
 
