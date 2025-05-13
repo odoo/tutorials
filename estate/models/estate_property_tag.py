bee651fe9bc79d96ed3c1f7e60068b91d93fcefc
raaa-odoo <raaa@odoo.com>
2025-05-13 11:26:29 +0530

    [IMP] estate: Again apply styling improvements to code
    Refactored the code to enhance readability and maintainability by improving
    indentation, spacing, and overall formatting. No functional or logic changes
    were made—only visual/code style adjustments for clarity.
    
    This improves code quality and aligns with best practices for clean code.

--- estate/models/estate_property_tag.py
+++ estate/models/estate_property_tag.py
@@ -5,11 +5,10 @@ class EstatePropertyTag(models.Model):
     _name = "estate.property.tag"
     _description = "Real Estate Property Tag"
     _order = "name asc"
-    
+
     name = fields.Char(string='Name', required=True)
     color = fields.Integer(string="Color")
     # SQL Constraints
     _sql_constraints = [
         ('unique_tag_name', 'UNIQUE(name)', 'The tag name must be unique.'),
     ]
-    
