31d0f764707fcb2969c9b18a3b5c487a3de517da
Léo Gizard <legi@odoo.com>
2025-05-19 16:06:00 +0200

    [IMP] estate: add a view for the property model

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,11 +1,11 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': 'Real Estate',
-    'category': 'Hidden/Tools',
-    'depends': ['base'],
-    'application': True,
-    'data':[
-        'security/ir.model.access.csv'
-    ]
+    "name": "Real Estate",
+    "category": "Hidden/Tools",
+    "depends": ["base"],
+    "application": True,
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
+    ],
 }
