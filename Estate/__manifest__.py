a40644342bab2878f832e97655701761ff494141
maedekey <maeva.80@hotmail.com>
2025-05-19 14:28:38 +0200

    [ADD] Estate : created real estate app
    Created files for the initialization of the real estate app.

---
+++ Estate/__manifest__.py
@@ -0,0 +1,16 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Real estate',
+    'version': '0.0',
+    'category': 'Estate',
+    'summary': 'Manage real estate',
+    'description': "",
+    'website': 'https://www.odoo.com/page/crm',
+    'depends': [
+        'base',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
