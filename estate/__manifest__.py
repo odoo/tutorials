c0984831aaa555efcff8db0a475a9b4c4e972cb3
oumaima-aarabe <111888559+oumaima-aarabe@users.noreply.github.com>
2025-05-19 16:11:29 +0200

    [ADD] estate CH4 : security checks

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,6 +4,7 @@
     'category': 'Tutorials/Estate',
     'summary': "Real estating module",
 
+    'data': ['security/ir.model.access.csv'],
     'installable': True,
     'depends': ['base'],
     'license': 'AGPL-3'
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,8 +1,8 @@
-# -*- coding: utf-8 -*-
 from odoo import models, fields
+
 
 class EstateProperty(models.Model):
     _name = "estate_property"
     _description = "real estate properties"
-    
+
     name = fields.Char('Property Name')
