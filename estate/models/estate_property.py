124455d56759ad6356b538f5e27d284c1ef636a7
Backendt <matisse.beckandt@proton.me>
2025-05-19 16:55:32 +0200

    [ADD] Added Estate model fields

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -9,16 +9,19 @@ class EstateProperty(models.Model):
     postcode = fields.Char('Postcode', translate=True)
     date_availability = fields.Date('Availability')
     expected_price = fields.Float('Expected price', required=True)
-    selling_price = fields.Float('Selling price', required=True)
-    bedrooms = fields.Integer('# Bedrooms', required=True)
-    living_area = fields.Integer('# Living Areas', required=True)
-    facades = fields.Integer('# Facades', required=True)
+    selling_price = fields.Float('Selling price', readonly=True)
+    bedrooms = fields.Integer('Bedrooms', required=True, default=2)
+    living_area = fields.Integer('Living Areas', required=True)
+    facades = fields.Integer('Facades', required=True)
     garage = fields.Boolean('Has a garage')
     garden = fields.Boolean('Has a garden')
-    garden_area = fields.Integer('#m² of garden area')
+    garden_area = fields.Integer('m² of garden area')
     garden_orientation = fields.Selection(
         string='Orientation',
         selection=[('north', 'North'), ('east', 'East'), ('west', 'West'), ('south', 'South')],
         help="Orientation is used to tell which way the garden is, relative to the house"
     )
+
+    # Reserved Fields
+    active = fields.Boolean()
 
