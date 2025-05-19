7ff8d4851bcaf7a44ce308d51d61f44fd403ec78
oumaima-aarabe <111888559+oumaima-aarabe@users.noreply.github.com>
2025-05-19 13:59:14 +0200

    [ADD] estate: adding real estate module chapter2 of tutorial

---
+++ estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': "Real Estate",
+    'version': '0.1',
+    'category': 'Tutorials/Estate',
+    'summary': "Real estating module",
+
+    'installable': True,
+    'depends': ['base'],
+    'license': 'AGPL-3'
+}
