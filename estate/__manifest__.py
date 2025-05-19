5c40c2382d147dae82d26a0f3742cbcb6a6d3775
abdrahmanrashed <abdelrahman0rashed@gmail.com>
2025-05-19 15:16:34 +0200

    [ADD] created estate module - ch2

---
+++ estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'ESTATE',
+    'description': "aras estate tutorial module",
+    'website': 'https://www.odoo.com/page/estate',
+    'depends': [
+        'base',
+    ],
+    'installable': True,
+    'application': True,
+}
