04713dc392c73c1c9f9703cef06dad120c1b54f3
Mazen <mamr@odoo.com>
2025-05-19 17:49:00 +0200

    [ADD] estate: add an estate module
    This adds an estate module that can be used to manage advertisements and offers for different real estate properties as part of the "Sever framework 101" tutorial

---
+++ estate/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "E-State!",
+
+    'summary': """
+        Manage real estate advertisements
+    """,
+
+    'description': """
+        Manage advertisements and offers for different real estate properties as part of the "Sever framework 101" tutorial
+    """,
+
+    'author': "mamr",
+    'version': '0.1',
+    'application': True,
+    'installable': True,
+    'depends': ['base'],
+
+    'license': 'AGPL-3'
+}
