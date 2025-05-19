439c30dffd1a0cf9d4f7b72b427afff2eca0d2bc
Arthur Mermet <amer@odoo.com>
2025-05-19 14:28:12 +0200

    [ADD] chapter 4

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,9 +1,11 @@
-# -*- coding: utf-8 -*-
 {
     'name': 'Estate',
     'depends': [
         'base'
     ],
     'installable': True,
-    'application': True
+    'application': True,
+    'data': [
+        'security/ir.model.access.csv'
+    ]
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -19,4 +19,3 @@ class EstateProperty(models.Model):
     garden_orientation = fields.Selection(
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
         )
-        
