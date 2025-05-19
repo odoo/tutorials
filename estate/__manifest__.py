fda9b1103c6ed16d2744c8fa92b0483a744f7c3d
jeanschoenlaub <jeanschoen@hotmail.com>
2025-05-19 14:09:07 +0200

    [IMP] estate: Chap 5
    [FIX] checking pre commit hook
    
    [FIX] checking pre commit hook
    
    [IMP] Added basic drop down menu
    
    [FIX] Fixed security access to estate_property model to be able to create new properties
    
    [IMP] Finished chapter 5 adding multiple fields to the estate_property module

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,16 +1,28 @@
 {
     "name": "Real Estate",
+    "description": """
+Assets management
+=================
+Manage assets owned by a company or a person.
+Keeps track of depreciations, and creates corresponding journal entries.
+
+    """,
     "version": "1.8",
-    "category": "Sales",
+    "category": "All",
     "sequence": 15,
     "summary": "Manage your residential opps",
     "depends": [
-        "base",
+        "base_setup",
     ],
-    "data": [],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
+    ],
     "demo": [],
     "installable": True,
     "application": True,
+    "auto_install": True,
     "assets": {},
     "license": "LGPL-3",
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,3 +1,7 @@
+import random
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
 from odoo import fields, models
 
 
@@ -5,15 +9,36 @@ class EstateProperty(models.Model):
     _name = "estate_property"
     _description = "A real estate property"
 
-    name = fields.Char()
-    description = fields.Text()
+    def _three_month_from_now(self):
+        """Return as ORM date compliant value three month from today"""
+
+        return fields.Date.today(self) + relativedelta(months=3)
+
+    name = fields.Char(string="Property Name", required=True)
+    description = fields.Text(string="Estate Description")
+    active = fields.Boolean(default=True)
+
+    state = fields.Selection(
+        selection=[
+            ("new", "New"),
+            ("offer received", "Offer Received"),
+            ("offer accepted", "Offer accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled"),
+        ],
+        default="new",
+        string="State",
+        required=True,
+        copy=False,
+    )
+
     postcode = fields.Char()
 
-    date_availabity = fields.Date()
-    expected_price = fields.Float()
-    selling_price = fields.Float()
+    date_availabity = fields.Date(copy=False, default=_three_month_from_now)
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
 
-    bedrooms = fields.Integer()
+    bedrooms = fields.Integer(default=random.randint(2, 6))
     facades = fields.Integer()
     living_area = fields.Integer()
     garden_area = fields.Integer()
