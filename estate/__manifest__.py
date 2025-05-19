ef2e2055d9f4d2e85dcdb2b260f65431684cc030
Arthur Mermet <amer@odoo.com>
2025-05-19 15:09:22 +0200

    [ADD] chapter 5 first half

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,11 +1,13 @@
 {
-    'name': 'Estate',
+    'name': 'estate',
     'depends': [
         'base'
     ],
     'installable': True,
     'application': True,
     'data': [
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ]
 }
