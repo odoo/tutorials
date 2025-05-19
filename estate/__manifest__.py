88668d3afa8cbf308f7013bda6fbf3e5c36536b9
thha-odoo <thha@odoo.com>
2025-05-19 14:10:07 +0200

    [ADD] Adding estate

---
+++ estate/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'estate',
+    'depends': [
+        'base'
+    ],
+    'application': True
+}
+
