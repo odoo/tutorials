988ddb4ae7c65eaef3ed57a2ad3a055da5f83bad
coco2299 <corentin.heinix@gmail.com>
2025-05-19 15:10:07 +0200

    [IMP] estate : add estate_property model
    I added the estate property model into the database.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,5 +3,11 @@
     'depends': [
         'base'
     ],
+    'data': [
+        'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
+    ],
     'license': 'AGPL-3'
-}
+    }
