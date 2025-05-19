b2ae800cea180009555943ff271b201576d99f73
Léo Gizard <legi@odoo.com>
2025-05-19 13:55:32 +0200

    [ADD] estate: add a new addon called estate, with a new model property representing a real estate property

---
+++ estate/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Real Estate',
+    'category': 'Hidden/Tools',
+    'depends': ['base'],
+    'application': True,
+    'data':[
+        'security/ir.model.access.csv'
+    ]
+}
