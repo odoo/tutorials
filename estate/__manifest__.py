24780a6ea0c310e141cdb12b8d113c2a50f3290d
karygauss03 <karim.omrane@supcom.tn>
2025-05-19 14:00:56 +0200

    [ADD] Estate: Adding the new Real Estate Module's directory. In this PR, I initialized the basic structure of the new Estate module directory (manifest and init files). This work is related to the second chapter of the server framework 101 official documentation by Odoo.

---
+++ estate/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Estate',
+    'version': '1.0',
+    'category': 'Real Estate',
+    'sequence': 15,
+    'summary': 'Track Real Estate',
+    'description': "This app is made for real estate tracking.",
+    'website': 'https://www.odoo.com/page/estate',
+    'depends': [
+        'base'
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3'
+}
