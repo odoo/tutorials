a783a05d2281e62e29defe06037909ec9c497e7f
coco2299 <corentin.heinix@gmail.com>
2025-05-20 11:43:21 +0200

    [IMP] estate : Customising existing views
    I modify the list, form and search view to be better to use.
    
    I made a mistake that merged previous commits.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,9 +5,8 @@
     ],
     'data': [
         'security/ir.model.access.csv',
-        'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_menus.xml'
     ],
     'license': 'AGPL-3'
-    }
+}
