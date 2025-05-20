029d96f82005f26d109a35e4bb345c7bd0e9effc
Corentin Lombard <clom@odoo.com>
2025-05-20 13:35:18 +0200

    [IMP] estate: added some menus and updated fields and defaults value

--- estate/models/estate_properties.py
+++ estate/models/estate_properties.py
@@ -1,3 +1,5 @@
+
+from dateutil.relativedelta import relativedelta
 from odoo import fields, models
 
 
@@ -5,18 +7,32 @@ class Properties(models.Model):
     _name = "estate_property"
     _description = "Properties of the estate model"
 
+    active = fields.Boolean(default=True)
     name = fields.Char("Title", required=True)
     description = fields.Text("description")
     postcode = fields.Integer("Post code")
-    date_availability = fields.Date("Date availability")
+    date_availability = fields.Date(
+        "Date availability",
+        copy=False,
+        default=fields.Date.today() + relativedelta(months=3))
     expected_price = fields.Float("Expected price", required=True)
-    selling_price = fields.Float("Selling price")
-    bedrooms = fields.Integer("# Bedrooms")
+    selling_price = fields.Float("Selling price", readonly=True, copy=False)
+    bedrooms = fields.Integer("# Bedrooms", default=2)
     living_area = fields.Integer("Living area")
     facades = fields.Integer("# facades")
-    garage = fields.Boolean("Garage", default=False)
-    garden = fields.Boolean("Garden", default=False)
+    garage = fields.Boolean("Garage")
+    garden = fields.Boolean("Garden")
     garden_area = fields.Integer("Garden area", default=0)
     garden_orientation = fields.Selection(
         string="Orientation",
         selection=[("north", "North"), ("east", "East"), ("west", "West"), ("south", "South")])
+    state = fields.Selection(
+        selection=[
+            ("new", "New"),
+            ("offer received", "Offer Received"),
+            ("offer accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled")],
+        required=True,
+        copy=False,
+        default="new")
