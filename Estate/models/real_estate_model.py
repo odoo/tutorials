a165ba2360fa2db0300621ac30a9b68ab2b21ca6
maedekey <maeva.80@hotmail.com>
2025-05-20 11:38:57 +0200

    [REL] Estate: Added menus and views for real estate
    Added 3 menus and a list view to see all the attributes of all the real estates. Also created an active field and set a bunch of default values for the fields.
    
    task-None
    Fixes #None
    Closes #None
    opw-None

--- Estate/models/real_estate_model.py
+++ Estate/models/real_estate_model.py
@@ -1,19 +1,22 @@
+from dateutil.relativedelta import relativedelta
 from odoo import fields, models
 
 
 class EstateProperty(models.Model):
     _name = "estate_property"
-    _description = "Estate Propery"
-    name = fields.Char(required=True)
+    _description = "Estate Property"
+    name = fields.Char(required = True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
-    expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    date_availability = fields.Date(copy = False, default=fields.Date.today() + relativedelta(months = 3))
+    expected_price = fields.Float(required = True)
+    selling_price = fields.Float(readonly = True, copy = False)
+    bedrooms = fields.Integer(default = 2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
-    garden_orientation =fields.Selection([('North', 'north'), ('South', 'south'), ('West', 'west'), ('East', 'east')])
+    garden_orientation = fields.Selection([('North', 'north'), ('South', 'south'), ('West', 'west'), ('East', 'east')])
+    state = fields.Selection([('New', 'new'), ('Offer Received', 'offer received'), ('Offer Accepted', 'offer accepted'), ('Sold', 'sold'), ('Cancelled', 'cancelled')], required = True, copy = False, default = 'New')
+    active = fields.Boolean(default = True)
