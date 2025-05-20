62e444343904a515f7b40234284a34d82ff38797
karygauss03 <karim.omrane@supcom.tn>
2025-05-20 10:37:01 +0200

    [IMP] Estate: Adding estate property view and menus. In this commit, I added the estate property view and menus in order to start interacting with our new model using the UI. I also added two new attributes to the estate property model, which are active and state, and I set some attributes to have a default values and to not be copied when a duplication is created. This is related to the 5th chapter of the Server Framework 101 Odoo's course.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,18 +1,26 @@
 from odoo import models, fields
+from dateutil.relativedelta import relativedelta
 
 
 class EstateModel(models.Model):
     _name = "estate_property"
     _description = "Real Estate"
-    _order = "sequence"
 
     name = fields.Char('Name', required=True)
     description = fields.Char('Description')
     post_code = fields.Char('Post Code')
-    date_availability = fields.Date('Available From')
+    state = fields.Selection(
+        string="State",
+        selection=[('new', 'New'), ('offer received', 'Offer Received'), ('offer accepted', 'Offer Accepted'), ('sold', 'Sold'), ('canceled', 'Canceled')],
+        help="Estate current state",
+        required=True,
+        default='new',
+        copy=False
+    )
+    date_availability = fields.Date('Available From', copy=False, default=lambda self: fields.Date.today() + relativedelta(months=3))
     expected_price = fields.Float('Expected Price', required=True)
-    selling_price = fields.Float('Selling Price')
-    bedrooms = fields.Integer('Bedrooms Count')
+    selling_price = fields.Float('Selling Price', readonly=True, copy=False)
+    bedrooms = fields.Integer('Bedrooms Count', default=2)
     living_area = fields.Float('Living Area size')
     facades = fields.Integer('Number of facades')
     garage = fields.Boolean('Has garage')
@@ -23,3 +31,4 @@ class EstateModel(models.Model):
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
         help="Garden orientation selection"
     )
+    active = fields.Boolean('Is Active', default=True)
