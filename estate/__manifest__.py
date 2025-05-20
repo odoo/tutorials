5d116bfa6ba0b8a312bfc8fd2ebd383a90dc6a4c
paay-odoo <paay@odoo.com>
2025-05-20 13:59:01 +0530

    [ADD] estate: Introducing new real estate property management module
    Created a new real estate property management module
    User will be able to use this module for managing workflow related to properties
    Managing properties, creating and updating properties, buying - selling

---
+++ estate/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': "estate",
+    'description': """
+        This module is used to manage the Real estate and properties.
+    """,
+
+    'author': "ayush",
+    'version': '0.1',
+    'application': True,
+    'installable': True,
+    'depends': ['base'],
+    'license': 'LGPL-3',
+    'category': 'Real Estate/Brokerage',
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+}
