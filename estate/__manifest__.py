410b2d38b4aa339aa86544f19665efe4c393e306
Mazen <mamr@odoo.com>
2025-05-20 14:03:05 +0200

    [ADD] estate: add menus to the estate module
    Server framework 101: Chapter 5
    
    This will add menus to the estate module and specific attributes to some of the fields

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -11,6 +11,8 @@
     'installable': True,
     'depends': ['base'],
     'data': [
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
         'security/ir.model.access.csv',
     ],
     'license': 'AGPL-3',
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import fields, models
+from odoo.tools.date_utils import relativedelta
 
 
 class EstateProperty(models.Model):
@@ -8,16 +9,20 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(default=fields.Date.today() + relativedelta(months=3), copy=False)
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
-        string='Garden Orientation',
-        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
+        string='Garden Orientation', selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
     )
+    state = fields.Selection(
+        [('new', 'New'), ('offer-received', 'Offer Received'), ('offer-accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')],
+        required=True, copy=False, default='new'
+    )
+    active = fields.Boolean(default=True)
