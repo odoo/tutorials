de48a277fb456282929ba2a41c8abaad7086e16c
Backendt <matisse.beckandt@proton.me>
2025-05-20 15:35:43 +0200

    [ADD] Added Offers to Estate property

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -35,3 +35,4 @@ class EstateProperty(models.Model):
     buyer_id = fields.Many2one("res.partner", string="Buyer")
     salesperson_id = fields.Many2one("res.users", string="Salesperson", default=lambda self: self.env.user)
     tag_ids = fields.Many2many("estate.property.tag", string="Tags")
+    offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
