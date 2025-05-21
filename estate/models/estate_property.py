52dd22078329669c0745f72a814e5fa8cf93141a
abdrahmanrashed <abdelrahman0rashed@gmail.com>
2025-05-21 17:34:41 +0200

    [IMP]  - Created total area field in estate property model to present the total area of the property, and added a compute method for it. - Created a best price field in estate method to determine the best price in all offers, and a compute method for it. -Created deadline and validity fields for offers to set a maximum time for offer validities, and created compute methods to compute them from each other so only 1 value needs to be inserted. - (ch8 tutorial)

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -30,3 +30,27 @@ class EstateProperty(models.Model):
     sales_person = fields.Many2one("res.users", string="Sales Person", default=lambda self: self.env.user)
     property_tag_ids = fields.Many2many("estate.property.tag")
     offer_ids = fields.One2many("estate.property.offer", "property_id")
+    total_area = fields.Integer(compute="_compute_total_area")
+    best_price = fields.Float(compute="_compute_best_price", string="Best Offer")
+
+    @api.depends("living_area", "garden_area")
+    def _compute_total_area(self):
+        for property in self:
+            property.total_area = property.living_area + property.garden_area
+
+    @api.depends("offer_ids.price")
+    def _compute_best_price(self):
+        self.best_price = 0
+        for id in self.offer_ids:
+            if self.best_price < id.price:
+                self.best_price = id.price
+
+    @api.onchange("garden")
+    def _onchange_partner_id(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = 'north'
+        else:
+            self.garden_area = None
+            self.garden_orientation = None
+
