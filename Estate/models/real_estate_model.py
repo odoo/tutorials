92dbbafd5d357e4c8b33af213f296c6b4ba26fb6
maedekey <maeva.80@hotmail.com>
2025-05-21 13:56:19 +0200

    [REL] Estate: Added buttons to accept and decline an offer Added buttons to only accept one offer and decline the rest. Also added buttons to indicate that the property has been sold or canceled.
    task-None
    Fixes #None
    Closes #None
    opw-None

--- Estate/models/real_estate_model.py
+++ Estate/models/real_estate_model.py
@@ -1,5 +1,6 @@
 from dateutil.relativedelta import relativedelta
-from odoo import fields, models
+from odoo.exceptions import UserError
+from odoo import fields, models, api
 
 
 class EstateProperty(models.Model):
@@ -8,20 +9,65 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(copy=False, default=fields.Date.today() + relativedelta(months = 3))
+    date_availability = fields.Date(copy=False, default=fields.Date.today() + relativedelta(months=3))
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
+
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Integer()
+
     garden_orientation = fields.Selection([('North', 'north'), ('South', 'south'), ('West', 'west'), ('East', 'east')])
-    state = fields.Selection([('New', 'new'), ('Offer Received', 'offer received'), ('Offer Accepted', 'offer accepted'), ('Sold', 'sold'), ('Cancelled', 'cancelled')], required = True, copy = False, default = 'New')
+    state = fields.Selection([
+        ('new', 'New'),
+        ('offer_received', 'Offer Received'),
+        ('offer_accepted', 'Offer Accepted'),
+        ('sold', 'Sold'),
+        ('cancelled', 'Cancelled')], required=True, copy=False, default='new')
     active = fields.Boolean(default=True)
     property_type_id = fields.Many2one('estate.property.type')
-    buyer = fields.Many2one('res.partner')
+    buyer = fields.Many2one('res.partner', readonly=True)
     seller = fields.Many2one('res.users', string='Salesperson', default=lambda self: self.env.user)
     tags = fields.Many2many('estate.property.tag')
-    offer_ids = fields.One2many('estate.property.offer','property_id')
+    offer_ids = fields.One2many('estate.property.offer', 'property_id')
+
+    total_area = fields.Float(compute="_compute_total_area")
+    garden_area = fields.Integer()
+    living_area = fields.Integer()
+
+    best_offer = fields.Integer(compute='_compute_best_offer')
+
+    @api.depends("living_area", "garden_area")
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
+    def _compute_best_offer(self):
+        for record in self:
+            record.best_offer = max(record.offer_ids.mapped("price"))
+
+    @api.onchange("garden")
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 1000
+            self.garden_orientation = 'North'
+        else:
+            self.garden_area = 0
+            self.garden_orientation = None
+
+    def sold(self):
+        for record in self:
+            if record.state != 'cancelled':
+                record.state = "sold"
+            else:
+                raise UserError("Cancelled properties cannot be sold.")
+        return True
+
+    def cancel(self):
+        for record in self:
+            if record.state != 'sold':
+                record.state = "cancelled"
+            else:
+                raise UserError("Sold properties cannot be cancelled.")
+        return True
--- Estate/models/real_estate_offer_model.py
+++ Estate/models/real_estate_offer_model.py
@@ -1,10 +1,40 @@
-from odoo import fields, models
+from dateutil.relativedelta import relativedelta
+from odoo.exceptions import UserError
+from odoo import fields, models, api
 
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
     _description = "Estate Property Offer"
     name = fields.Char(required=True)
     price = fields.Float()
-    status = fields.Selection([('Accepted', 'accepted'), ('Refused', 'refused')], copy=False)
+    status = fields.Selection([('accepted', 'Accepted'), ('refused', 'Refused')], copy=False)
     partner_id = fields.Many2one('res.partner', required=True)
     property_id = fields.Many2one('estate_property', required=True)
+
+    deadline = fields.Date(compute="_compute_deadline", inverse="_inverse_deadline")
+    validity = fields.Integer(default=7)
+
+    @api.depends("validity")
+    def _compute_deadline(self):
+        for record in self:
+            if record.create_date:
+                record.deadline = record.create_date + relativedelta(days=record.validity)
+            else:
+                record.deadline = fields.Date.today() + relativedelta(days=record.validity)
+
+    def _inverse_deadline(self):
+        for record in self:
+            record.validity = (record.deadline - fields.Date.today()).days
+
+    def action_accept(self):
+        for record in self:
+            if record.status != 'accepted':
+                record.status = "accepted"
+                record.property_id.selling_price = record.price
+                record.property_id.buyer = record.partner_id
+            else:
+                raise UserError("You can only accept one offer per property.")
+
+    def action_refuse(self):
+        for record in self:
+            record.status = "refused"
