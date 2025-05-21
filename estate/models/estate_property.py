87454874cffd4da7d9b2856c47f08f985243a85a
Backendt <matisse.beckandt@proton.me>
2025-05-21 09:40:41 +0200

    [ADD] Added Total Area field to Estate property

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -21,6 +21,7 @@ class EstateProperty(models.Model):
         selection=[('north', 'North'), ('east', 'East'), ('west', 'West'), ('south', 'South')],
         help="Orientation is used to tell which way the garden is, relative to the house"
     )
+    total_area = fields.Integer("Total Area", readonly=True, compute="_compute_total_area")
 
     # Reserved Fields
     active = fields.Boolean()
@@ -36,3 +37,9 @@ class EstateProperty(models.Model):
     salesperson_id = fields.Many2one("res.users", string="Salesperson", default=lambda self: self.env.user)
     tag_ids = fields.Many2many("estate.property.tag", string="Tags")
     offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
+
+    @api.depends("living_area", "garden_area")
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
