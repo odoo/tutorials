9d7fc0bc87ee07bc29f03598f65754ddc8613e79
MaximeNoirhomme <noma@odoo.com>
2025-05-21 09:36:33 +0200

    [FIX] give a proper name to the estate property type class and rename other info fields

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -1,7 +1,7 @@
 from odoo import fields, models
 
 
-class EstateProperty(models.Model):
+class EstatePropertyType(models.Model):
     _name = 'estate.property.type'
     _description = 'estate property type module'
 
