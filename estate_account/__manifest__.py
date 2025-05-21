941c88620a351185699254fae2feff3396b69b19
Léo Gizard <legi@odoo.com>
2025-05-21 17:03:51 +0200

    [IMP] Add a link module between estate and accounting
    When selling a house, an invoice is created.

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    "name": "Real Estate Accounting",
+    "category": "Hidden/Tools",
+    "depends": ["base", "account", "estate"],
+    "application": False,
+    "data": [],
+}
---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,16 @@
+from odoo import models, Command
+
+
+class Property(models.Model):
+    _inherit = "estate.property"
+
+    def action_sold(self):
+        for property in self:
+            self.env["account.move"].create(
+                {
+                    "partner_id": property.buyer_id.id,
+                    "move_type": "out_invoice",
+                    "line_ids": [Command.create({"name": "6% of price", "quantity": 1, "price_unit": 0.06 * property.selling_price}), Command.create({"name": "administrative fees", "quantity": 1, "price_unit": 100.00})],
+                }
+            )
+        return super().action_sold()
