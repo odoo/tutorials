190a7c2efd7a9d3ed479da38d6f6f57b8e33d3c4
karygauss03 <karim.omrane@supcom.tn>
2025-05-21 10:24:42 +0200

    [IMP] Estate: Adding computed fields to estate property model. In this commit, I added the following computed fields: - Estate Property: best_price and total_area - Estate Offer: validaty and date_deadline Also, I introduced the onchange method to set default values of the garden area and garden orientation when the garden attribute is set to True. This commit is related to the 8th chapter of Server Framework 101 Odoo's official documentation.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,5 +1,5 @@
-from odoo import models, fields
 from dateutil.relativedelta import relativedelta
+from odoo import api, fields, models
 
 
 class EstateProperty(models.Model):
@@ -20,20 +20,38 @@ class EstateProperty(models.Model):
     date_availability = fields.Date('Available From', copy=False, default=lambda self: fields.Date.today() + relativedelta(months=3))
     expected_price = fields.Float('Expected Price', required=True)
     selling_price = fields.Float('Selling Price', readonly=True, copy=False)
+    best_price = fields.Float('Best Price', compute='_compute_best_price')
     bedrooms = fields.Integer('Bedrooms Count', default=2)
     living_area = fields.Float('Living Area size')
     facades = fields.Integer('Number of facades')
     garage = fields.Boolean('Has garage')
     garden = fields.Boolean('Has garden')
-    garden_area = fields.Integer('Garden area')
+    garden_area = fields.Float('Garden area')
     garden_orientation = fields.Selection(
         string='Garden Orientation',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
         help="Garden orientation selection"
     )
+    total_area = fields.Float('Total Area', compute='_compute_total_area')
     active = fields.Boolean('Is Active', default=True)
     property_type_id = fields.Many2one('estate.property.type', string='Property Type')
     buyer_id = fields.Many2one('res.partner', 'Buyer', copy=False)
     salesperson_id = fields.Many2one('res.users', 'Salesperson', index=True, default=lambda self: self.env.user)
     tag_ids = fields.Many2many('estate.property.tag', string='Tags')
     offer_ids = fields.One2many('estate.property.offer', 'property_id', string='Offers')
+
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
+    @api.depends('offer_ids.price')
+    def _compute_best_price(self):
+        for record in self:
+            prices = record.offer_ids.mapped('price')
+            record.best_price = max(prices) if prices else 0
+
+    @api.onchange('garden')
+    def _onchange_garden_checkbox(self):
+        self.garden_area = 10 if self.garden else 0
+        self.garden_orientation = 'north' if self.garden else None
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from datetime import timedelta
 
 
 class EstatePropertyOffer(models.Model):
@@ -11,5 +12,18 @@ class EstatePropertyOffer(models.Model):
         selection=[('accepted', 'Accepted'), ('refused', 'Refused')],
         copy=False
     )
+    validity = fields.Integer(string="Validity (days)", default=7)
+    date_deadline = fields.Date(string="Deadline", compute="_compute_date_deadline", inverse="_inverse_date_deadline", store=True)
     partner_id = fields.Many2one('res.partner', required=True, string="Partner")
     property_id = fields.Many2one('estate.property', required=True, string="Property")
+
+    @api.depends('create_date', 'validity')
+    def _compute_date_deadline(self):
+        for offer in self:
+            create_date = offer.create_date or fields.Datetime.now()
+            offer.date_deadline = create_date.date() + timedelta(days=offer.validity)
+
+    def _inverse_date_deadline(self):
+        for offer in self:
+            create_date = offer.create_date or fields.Datetime.now()
+            offer.validity = (offer.date_deadline - create_date.date()).days if offer.date_deadline else 0
