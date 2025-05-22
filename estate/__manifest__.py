53dd0876c30a0fe29771f58cbaee34f346a91848
Léo Gizard <legi@odoo.com>
2025-05-22 17:14:49 +0200

    [IMP] estate: add security rules and user groups

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,11 +1,12 @@
 {
     "name": "Real Estate",
-    "category": "Hidden/Tools",
+    "category": "Real Estate/Brokerage",
     "depends": ["base"],
     "application": True,
     "license": "LGPL-3",
     "data": [
         "security/ir.model.access.csv",
+        "security/estate_security.xml",
         "data/estate.property.type.csv",
         "views/estate_property_views.xml",
         "views/estate_property_offer_views.xml",
