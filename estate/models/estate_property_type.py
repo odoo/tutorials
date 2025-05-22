cebd8e415cc25270c1e253cb811cc3db7dd30b34
Backendt <matisse.beckandt@proton.me>
2025-05-22 11:44:31 +0200

    [ADD] estate: Added manual ordering on property types view

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -4,9 +4,10 @@ from odoo import fields, models
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
     _description = "Estate property type"
-    _order = "name"
+    _order = "sequence, name"
 
     name = fields.Char(string='Name', required=True)
+    sequence = fields.Integer(default=1, help="Used to order types. Lower is better.")
 
     # Foreign keys
     property_ids = fields.One2many("estate.property", "property_type_id")
