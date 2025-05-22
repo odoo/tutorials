6c53827aca25c0b4dbd42618ef2777304e6c8c6b
oumaima-aarabe <111888559+oumaima-aarabe@users.noreply.github.com>
2025-05-22 13:11:00 +0200

    [ADD] estate CH6: added fields for the list view and form and better searching engine

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,16 +5,28 @@ class EstateProperty(models.Model):
     _name = "estate_property"
     _description = "real estate properties"
 
-    name = fields.Char(default="Unknown")
+    name = fields.Char('Title', default="Unknown", required=True)
+    description = fields.Char('Descrption')
+    postcode = fields.Char('Postcode', required=True, default='00000')
 
     availability_date = fields.Date('Availability', copy=False)
     selling_price = fields.Float('Selling Price', readonly=True, copy=False)
-    bedrooms = fields.Integer('Bedrooms', default=2)
+    bedrooms = fields.Integer('Bedrooms', default=2, required=True)
     last_seen = fields.Datetime("Last Seen", default=fields.Datetime.now)
     expected_price = fields.Float('Expected Price', required=True)
+    living_area = fields.Float('Living Area (sqm)', required=True, default=20.0)
     facades = fields.Integer('Facades', required=True)
     garage = fields.Boolean('Garage')
     garden = fields.Boolean('Garden')
+    garden_orientation = fields.Selection(
+        string='Garden Orientation',
+        selection=[
+            ('north', 'North'),
+            ('west', 'West'),
+            ('east', 'East'),
+            ('south', 'South'),
+        ],
+    )
     active = fields.Boolean(default=True)
     state = fields.Selection(
         string='Property State',
