d35f9ba9f394f8722dcff38af6b843e980fb193b
Arthur Mermet <amer@odoo.com>
2025-05-22 11:17:28 +0200

    [ADD] estate: chapter 12 done

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -12,6 +12,7 @@
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_menus.xml',
+        'views/estate_res_users.xml',
     ],
     'license': 'LGPL-3',
 }
