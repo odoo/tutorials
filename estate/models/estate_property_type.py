27074ce23d44f70154441c93dee6d065fc6c3b39
oumaima-aarabe <111888559+oumaima-aarabe@users.noreply.github.com>
2025-05-22 15:36:30 +0200

    [FIX] estate: Models names

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -1,7 +1,8 @@
 from odoo import models, fields
 
+
 class EstatePropertyType(models.Model):
-    _name = "estate_property_type"
+    _name = "estate.property.type"
     _description = 'Type for categorizing estate properties'
 
     name = fields.Char('Name', required=True)
