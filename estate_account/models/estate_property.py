5d4f8b627351075affd9c63ba792f7ad2d6b40da
coco2299 <corentin.heinix@gmail.com>
2025-05-22 15:02:24 +0200

    [ADD] estate account : Add a link model + [IMP] estate : add auto invoicing when property sold
    I added a link model between estate and account.
    I inherit and override the sold_property function from estate.property.
    I add some verifications and made an automatic invoice creation.

---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,34 @@
+from odoo import Command, fields, models
+
+
+class EstateProperty(models.Model):
+    _inherit = "estate.property"
+
+    def sold_property(self):
+        super().sold_property()
+        for estateProperty in self:
+            if not estateProperty.buyer_id:
+                #Create invoice with no buyer doesn't make sense 
+                #Skip but could raise an error
+                continue
+            selling_price = estateProperty.best_price if estateProperty.best_price > 0.00 else estateProperty.expected_price
+            accepted_offer = next(filter(lambda x: x.status == "Accepted", estateProperty.offer_ids), None)
+            if accepted_offer and accepted_offer.price < selling_price:
+                selling_price = accepted_offer.price
+            self.env['account.move'].create({
+                'partner_id': estateProperty.buyer_id.id,
+                'move_type': 'out_invoice',
+                'invoice_line_ids': [
+                    Command.create({
+                        'name': '6% of Selling price',
+                        'quantity': 0.06,
+                        'price_unit': selling_price
+                    }),
+                    Command.create({
+                        'name': 'Administrative fees',
+                        'quantity': 1,
+                        'price_unit': 100.00
+                    })
+                ]
+            })
+        return True
