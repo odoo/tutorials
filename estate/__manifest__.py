68edfb5fd7b13d943c73853fb5bf78b564e48d05
jeanschoenlaub <jeanschoen@hotmail.com>
2025-05-22 15:49:49 +0200

    [IMP] estate : Chap 14 & 15 - Added kanban view

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -11,12 +11,13 @@
     "data": [
         "security/ir.model.access.csv",
 
-        "views/estate_menus.xml",
         "views/estate_property_offer_views.xml",
         "views/estate_property_tag_views.xml",
         "views/estate_property_type_views.xml",
         "views/estate_property_views.xml",
         "views/res_users_views.xml",
+
+        "views/estate_menus.xml",
     ],
     "demo": [],
     "installable": True,
