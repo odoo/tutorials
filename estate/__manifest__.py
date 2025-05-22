e8762009c6164a662752d455274848f642fddb53
Léo Gizard <legi@odoo.com>
2025-05-22 15:01:07 +0200

    [IMP] estate: add demo data and business data to the module

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,11 +5,15 @@
     "application": True,
     "data": [
         "security/ir.model.access.csv",
+        "data/estate.property.type.csv",
         "views/estate_property_views.xml",
         "views/estate_property_offer_views.xml",
         "views/estate_property_type_views.xml",
         "views/estate_property_tag_views.xml",
         "views/res_users_views.xml",
         "views/estate_menus.xml",
+    ],
+    "demo": [
+        "demo/demo_data.xml",
     ],
 }
