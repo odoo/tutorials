dd717c8422698c3afee67c29165049de933afe94
oumaima-aarabe <111888559+oumaima-aarabe@users.noreply.github.com>
2025-05-23 11:51:25 +0200

    [ADD] estate CH11: lots of sprinkles

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -1,12 +1,24 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
     _description = 'Type for categorizing estate properties'
+    _order = "sequence, name"
 
     name = fields.Char('Name', required=True)
+    sequence = fields.Integer('Sequence', default=1)
 
     _sql_constraints = [
         ('unique_name', 'UNIQUE(name)', 'A property type name must be unique!'),
     ]
+
+    offer_ids = fields.One2many('estate.property.offer', 'property_type_id')
+    property_ids = fields.One2many('estate.property', 'Property_type_id', 'Properties')
+
+    offer_count = fields.Integer(compute='_compute_offer_count')
+
+    @api.depends('offer_ids')
+    def _compute_offer_count(self):
+        for record in self:
+            record.offer_count = len(record.offer_ids  or [])
