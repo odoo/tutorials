7100e9bb0ed9f850dbf941580876b81674b22b3e
thha-odoo <thha@odoo.com>
2025-05-23 14:42:48 +0200

    [FIX] estate: PR Hot fix chapter 1->10
    IDE files in .gitignore
    inverse attribute not fixed
    Mapped method
    Fixing EstateProperty.action_sold method
    Improving EstateProperty._get_best_offer
    Renaming for following convention
    Fixing EstatePropertyOffer._compute_date_deadline , _compute_date_deadline_inverse and action_accepted methods

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -7,14 +7,14 @@ class EstateProperty(models.Model):
     _description = "Property's properties"
     _order = "id desc"
     _sql_constraints = [
-        ('check_expected_price', 'CHECK (expected_price > 0)', 'The expected price must be greater than 0'),
-        ('check_selling_price', 'CHECK (selling_price >= 0)', 'The selling price must be greater than 0'),
+        ('check_expected_price', 'CHECK (expected_price > 0)', "The expected price must be greater than 0"),
+        ('check_selling_price', 'CHECK (selling_price >= 0)', "The selling price must be greater or equal to 0"),
     ]
 
     name = fields.Char('Property name', required=True, default='Unknown')
     description = fields.Text('Property Description')
     postcode = fields.Char('Postcode')
-    date_availability = fields.Date('Availability', copy=False, default=datetime.now() + timedelta(days=90))
+    date_availability = fields.Date('Availability', copy=False, default=lambda self: datetime.now() + timedelta(days=90))
     expected_price = fields.Float('Expected Price', required=True)
     selling_price = fields.Float('Selling Price', readonly=True, copy=False)
     bedrooms = fields.Integer('Bedrooms', default=2)
@@ -23,20 +23,20 @@ class EstateProperty(models.Model):
     garage = fields.Boolean('Garage')
     garden = fields.Boolean('Garden')
     garden_area = fields.Integer('Garden Area')
-    garden_orientation = fields.Selection([('North', 'North'), ('East', 'East'), ('South', 'South'), ('West', 'West')], default='North')
+    garden_orientation = fields.Selection([('north', 'North'), ('east', 'East'), ('south', 'South'), ('west', 'West')], default='north')
     active = fields.Boolean('Active', default=True)
     state = fields.Selection([
         ('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')
     ], default='new', required=True, copy=False)
 
-    total_area = fields.Float("Total Area", compute="_compute_total_area")
-    best_price = fields.Float("Best Offer", compute="_compute_best_price")
-    is_state_set = fields.Boolean("Is State Set", compute="_compute_is_state_set")
+    total_area = fields.Float('Total Area', compute='_compute_total_area')
+    best_price = fields.Float('Best Offer', compute='_compute_best_price')
+    is_state_set = fields.Boolean('Is State Set', compute='_compute_is_state_set')
 
-    property_type_id = fields.Many2one("estate.property.type", string="Property Type")
+    property_type_id = fields.Many2one('estate.property.type', string="Property Type")
     buyer_id = fields.Many2one("res.partner", string="Buyer")
     salesperson_id = fields.Many2one("res.users", string="Salesman", default=lambda self: self.env.user)
-    tags_ids = fields.Many2many("estate.property.tag", string="Tags")
+    tag_ids = fields.Many2many("estate.property.tag", string="Tags")
     offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
 
     @api.depends("garden_area", "living_area")
@@ -50,20 +50,16 @@ class EstateProperty(models.Model):
             best_offer = record._get_best_offer()
             if best_offer is None:
                 record.best_price = 0
-                return
+                continue
             record.best_price = best_offer.price
 
     def _get_best_offer(self):
         if len(self.offer_ids) == 0:
             return None
-        best_offer = self.offer_ids[0]
-        for offer in self.offer_ids:
-            if offer.price > best_offer.price:
-                best_offer = offer
-        return best_offer
+        return self.offer_ids.sorted(lambda offer: offer.price)[-1]
 
     @api.onchange("garden")
-    def _onchange_partner_id(self):
+    def _onchange_garden(self):
         for record in self:
             if record.garden:
                 record.garden_orientation = 'North'
@@ -73,31 +69,33 @@ class EstateProperty(models.Model):
                 record.garden_area = 0
 
     def action_sold(self):
-        for record in self:
-            if record.state == 'cancelled':
-                raise exceptions.UserError("Can't sell a cancelled offer")
+        valid_records = self.filtered(lambda r: r.state != 'cancelled')
+        if not valid_records:
+            raise exceptions.UserError(self.env._("Can't sell a cancelled offer"))
+        for record in valid_records:
             best_offer = record._get_best_offer()
             if best_offer is None:
-                raise exceptions.UserError("The offers for this property are empty")
-            self.sold(best_offer)
+                raise exceptions.UserError(self.env._("The offers for property % are empty", record.name))
+            record.mark_as_sold(best_offer)
 
-    def sold(self, offer):
+    def mark_as_sold(self, offer):
         offer.status = 'accepted'
+        self.state = 'sold'
         self.buyer_id = offer.partner_id
         self.selling_price = offer.price
 
     def action_cancelled(self):
         for record in self:
             if record.state == 'sold':
-                raise exceptions.UserError("Can't cancel a sold offer")
+                raise exceptions.UserError(self.env._("Can't cancel a sold offer"))
             record.state = 'cancelled'
 
     @api.constrains('selling_price', 'expected_price')
-    def _check_date_end(self):
+    def _check_prices(self):
         for record in self:
-            if not tools.float_is_zero(record.selling_price, 0e-4) and record.selling_price < record.expected_price * 0.9:
+            if not tools.float_is_zero(record.selling_price, 3) and record.selling_price < record.expected_price * 0.9:
                 raise exceptions.ValidationError(
-                    f"The selling price cannot be lower than 90% of the expected price (min ${record.expected_price * 0.9}€)")
+                    self.env._("The selling price cannot be lower than 90% of the expected price (min %€)", record.expected_price * 0.9))
 
     @api.depends('state')
     def _compute_is_state_set(self):
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,22 +1,22 @@
 from datetime import datetime, timedelta
 
-from odoo import api, fields, models
+from odoo import api, fields, models, exceptions
 
 
 class EstatePropertyOffer(models.Model):
     _name = 'estate.property.offer'
     _description = 'An offer'
-    _order = "price desc"
+    _order = 'price desc'
     _sql_constraints = [
         ('check_price', 'CHECK (price > 0)', 'The price must be greater than 0'),
     ]
 
-    price = fields.Float('Price', required=True)
+    price = fields.Float("Price", required=True)
     status = fields.Selection([('accepted', 'Accepted'), ('refused', 'Refused')], copy=False)
-    create_date = fields.Date(string="Creation Date", default=lambda self: datetime.now())
+    offer_date = fields.Date(string="Creation Date", default=lambda self: fields.Date.today())
     validity = fields.Integer("Validity (days)")
 
-    date_deadline = fields.Date("Deadline", readonly=False, compute="_compute_date_deadline")
+    date_deadline = fields.Date("Deadline", readonly=False, compute="_compute_date_deadline", inverse='_compute_date_deadline_inverse')
 
     partner_id = fields.Many2one('res.partner', string="Partner", required=True)
     property_id = fields.Many2one('estate.property', string="Property", required=True)
@@ -25,15 +25,19 @@ class EstatePropertyOffer(models.Model):
     @api.depends("validity")
     def _compute_date_deadline(self):
         for offer in self:
-            offer.date_deadline = offer.create_date + timedelta(days=offer.validity)
+            if offer.offer_date is not None:
+                offer.date_deadline = fields.Date.to_date(offer.offer_date) + timedelta(days=offer.validity)
 
     def _compute_date_deadline_inverse(self):
         for offer in self:
-            offer.validity = (offer.date_deadline - offer.create_date).days
+            offer.validity = (fields.Date.to_date(offer.date_deadline) - fields.Date.to_date(offer.offer_date)).days
 
     def action_accepted(self):
         for offer in self:
-            offer.property_id.sold(self)
+            for prop_offer in offer.property_id.offer_ids:
+                if prop_offer.status == 'accepted':
+                    raise exceptions.UserError("Can't accept an offer when another offer has already been accepted for this property.")
+            offer.property_id.mark_as_sold(self)
             offer.status = 'accepted'
 
     def action_refused(self):
