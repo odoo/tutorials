6c3ab74c3703f9b7a9e1f7bca8e5d07563c80940
Arthur Mermet <amer@odoo.com>
2025-05-23 10:43:39 +0200

    [REV] revised code on mathieu's comments

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -53,11 +53,9 @@ class EstateProperty(models.Model):
     ]
 
     @api.constrains('selling_price', 'expected_price')
-    @api.constrains('selling_price', 'expected_price')
     def _check_selling_price(self):
         for record in self:
             if not float_is_zero(record.selling_price, precision_digits=2):
-                if float_compare(record.selling_price, record.expected_price * 0.9, precision_digits=2) < 0:
                 if float_compare(record.selling_price, record.expected_price * 0.9, precision_digits=2) < 0:
                     raise ValidationError("The selling price cannot be less than 90% of the expected price")
 
@@ -83,7 +81,6 @@ class EstateProperty(models.Model):
         # clear values
         else:
             self.garden_area = 0
-            self.garden_orientation = False
             self.garden_orientation = False
 
     # CRUD methods
