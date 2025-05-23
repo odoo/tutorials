50a34fe21a1da624ce429ecaf4147e35a553aa8b
Backendt <matisse.beckandt@proton.me>
2025-05-23 11:17:04 +0200

    [ADD] awesome_owl: Added props validation on Card component

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -4,9 +4,9 @@ import { Component } from "@odoo/owl";
 
 export class Card extends Component {
     static template = "awesome_owl.card";
-    static defaultProps = {
-        title: "My card",
-        text: "Lorem ipsum",
+    static props = {
+        title: String,
+        text: String
     };
 
 }
