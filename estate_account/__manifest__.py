7123ca6da665bf419e51bdf0eee3fc265cdc929f
Mahdi Alijani (malj) <malj@odoo.com>
2025-05-23 09:56:37 +0200

    [ADD] estate_account: add estate_account module to create invoicing for sold properties

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'estate_account',
+    'version': '1.0',
+    'depends': [
+        'estate',
+        'account'
+    ],
+    'data': [
+    ],
+    'author': "Mahdi (malj)",
+    'license': 'LGPL-3'
+}
