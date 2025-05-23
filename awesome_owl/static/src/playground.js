c7cd317bcfa117e300ffca0a09484956e4efa318
Backendt <matisse.beckandt@proton.me>
2025-05-23 11:01:29 +0200

    [ADD] awesome_owl: Made card fields markupable

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,10 +1,16 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, markup } from "@odoo/owl";
 import { Counter } from "./counter/counter.js";
 import { Card } from "./card/card.js";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
     static components = { Counter, Card };
+
+    raw_card_title = "<u>Hello</u>";
+    raw_card_text = "<u>World</u>";
+
+    card_title = markup(this.raw_card_title);
+    card_text = markup(this.raw_card_text);
 }
