ddb63adf3fa3c7071284331fa74d35ef853dca0d
oumaima-aarabe <111888559+oumaima-aarabe@users.noreply.github.com>
2025-05-26 10:30:12 +0200

    [ADD] estate_property: CH13 INteraction with the invoice module

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Real Estate Account',
+    'version': '1.0',
+    'description': 'Real estate account management',
+    'summary': 'Real estate account management',
+    'data': [],
+    'installable': True,
+    'depends': ['estate', 'account'],
+    'license': 'AGPL-3',
+}
---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,19 @@
+from odoo import models, Command
+
+
+class EstateProperty(models.Model):
+    _inherit = 'estate.property'
+
+    def action_to_sell(self):
+        self.env['account.move'].create(
+            {
+                'move_type': 'out_invoice',
+                'partner_id': self.partner_id.id,
+                'invoice_line_ids': [
+                    Command.create({'name': self.name, 'quantity': 1, 'price_unit': self.best_offer}),
+                    Command.create({'name': 'Taxes', 'quantity': 1, 'price_unit': self.best_offer * 0.06}),
+                    Command.create({'name': 'Adminsitration Fees', 'quantity': 1, 'price_unit': 100}),
+                ],
+            }
+        )
+        return super().action_to_sell()
