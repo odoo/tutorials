1fde2439dd470173edbc18d6225c48604c2b550b
Corentin Lombard <clom@odoo.com>
2025-05-26 13:21:31 +0200

    [IMP] awesome_owl: add some HTML content
    Add some HTML content using t-log and markup

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -4,7 +4,7 @@ import { Component, useState, xml } from "@odoo/owl";
 export class Card extends Component {
     static template = xml`<div class="card d-inline-block m-2">
             <h5 class="card-title"><t t-esc="state.title"/></h5>
-            <p class="card-text"><t t-esc="state.content"/></p>
+            <p class="card-text"><t t-out="state.content"/></p>
         </div>`;
 
     setup() {
--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,12 +1,15 @@
 /** @odoo-module **/
 
-import { Component, useState } from "@odoo/owl";
+import { Component, useState, markup } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
     static components = { Counter, Card };
+
+    someHTML = markup("<div class='text-primary'>some content</div>")
+    someText = "<div class='text-primary'>some content</div>"
 
     setup() {
         this.state = useState({ value:0});
