8e654630fa5aee839a9bba3db969b56846f60ab6
Arthur Mermet <amer@odoo.com>
2025-05-27 15:08:30 +0200

    [ADD] awesome_owl: 1.13 generic card with slots

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -7,6 +7,11 @@ export class Card extends Component {
 
     static props = {
         title: String,
-        content: String
+        slots: {
+            type: Object,
+            shape: {
+                default: true
+            }
+        },
     };
 }
