8d08da0ce23d3650fec562f3572985d6f69fd664
abhk-odoo <abhk@odoo.com>
2025-05-27 11:36:03 +0530

    [ADD] product_warranty: add product warranty management system
    - Implement warranty creation from sales order lines
    - Add warranty wizard for bulk operations
    - Configure warranty duration based on products
    - Extend sales order view with warranty controls
    - Include warranty tracking and validation
    - Add access rights for warranty management

---
+++ product_warranty/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Product Warranty",
+    "version": "1.0",
+    "description": "Product Warranty",
+    "author": "Abhishek Khant (abhk)",
+    "depends": ["sale", "product"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/product_template_views.xml",
+        "views/product_warranty_views.xml",
+        "wizard/product_warranty_wizard_views.xml",
+        "views/sale_order_line_views.xml",
+        "views/product_warranty_menus.xml",
+    ],
+    "auto_install": True,
+    "license": "LGPL-3",
+}
---
+++ product_warranty/wizard/product_warranty_wizard_line.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models
+
+
+class ProductWarrantyWizardLine(models.TransientModel):
+    _name = "product.warranty.wizard.line"
+    _description = "product warranty wizard line"
+
+    warranty_wizard_id = fields.Many2one("product.warranty.wizard")
+    order_line_id = fields.Many2one("sale.order.line", store=True)
+    product_id = fields.Many2one("product.product", store=True)
+    linked_product_name = fields.Char(
+        related="product_id.display_name", string="Product Name"
+    )
+    warranty_product_id = fields.Many2one("product.warranty", string="Year")
+    warranty_end_date = fields.Date(readonly=True, compute="_compute_warranty_end_date")
+
+    @api.depends("warranty_product_id")
+    def _compute_warranty_end_date(self):
+        for record in self:
+            if record.warranty_product_id:
+                record.warranty_end_date = fields.Date.add(
+                    fields.Date.today(), years=record.warranty_product_id.year
+                )
+            else:
+                record.warranty_end_date = False
