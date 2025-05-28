010fb2c1d7da18b480fe1ce684b4b9fa1192a8bc
oumaima-aarabe <111888559+oumaima-aarabe@users.noreply.github.com>
2025-05-28 09:57:09 +0200

    [ADD] awesome_owl: create Card component and integrate into Playground

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,9 +1,8 @@
-/** @odoo-module **/
-
 import { Component } from "@odoo/owl";
 import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
-    static components = { Counter };
+    static components = { Counter, Card };
 }
