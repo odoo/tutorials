36d82b4f51eb4ba7a9d7d8db418772003eb1c3ef
Arthur Mermet <amer@odoo.com>
2025-05-28 11:40:05 +0200

    [ADD] awesome_dashboard 2.4 add some statistics

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,10 +1,11 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, onWillStart } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
 import { useService } from "@web/core/utils/hooks";
 import { DashboardItem } from "./dashboard_item";
+import { rpc } from "@web/core/network/rpc";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
@@ -16,6 +17,10 @@ class AwesomeDashboard extends Component {
         };
 
         this.action = useService("action");
+
+        onWillStart(async () => {
+            this.stats = await rpc("/awesome_dashboard/statistics")
+        });
     }
 
     openCustomers() {
