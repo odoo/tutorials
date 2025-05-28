12264ee0411b6a5daf2f81dc3736209b9ca67fd0
Corentin Lombard <clom@odoo.com>
2025-05-28 17:13:28 +0200

    [IMP] awesome_owl: add toggle buttons on cards

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -2,16 +2,33 @@
 import { Component, useState, xml } from "@odoo/owl";
 
 export class Card extends Component {
-    static template = xml`<div class="card d-inline-block m-2">
-            <h5 class="card-title"><t t-esc="state.title"/></h5>
-            <p class="card-text"><t t-out="state.content"/></p>
-        </div>`;
+    static template = xml`
+<div class="card d-inline-block m-2">
+    <div class="card-header">
+        <button class="btn btn-primary" t-on-click="() => state.state=!state.state">Toggle</button>
+    </div>
+    <div t-if="state.state">
+        <h5 class="card-title"><t t-esc="state.title"/></h5>
+        <p class="card-text">
+            <t t-slot="default"/>
+        </p>
+    </div>
+</div>`;
     static props = {
-        title: { type: String },
-        content: { type: String }
-    }
-    setup() {
-        this.state = useState({ title: this.props.title, content:this.props.content })
+        title: String,
+        slots: {
+            type: Object,
+            shape: {
+                default: true
+            },
+        }
     }
 
+    setup() {
+        this.state = useState({ title: this.props.title, state: true})
+    }
+
+    toggle() {
+
+    }
 }
