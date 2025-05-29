d2659654ac8bf98368261c7943b33612edb5fd78
raaa-odoo <raaa@odoo.com>
2025-05-29 11:27:29 +0530

    [IMP] estate: implement business logic for offers, constraints, state transitions This commit completes the enhancements from chapters 11 to 15 of the Odoo 18 Real Estate module. It introduces a range of improvements:
    - Added state field with custom transitions (`new`, `offer received`,
    `offer accepted`, `sold`, `cancelled`), including buttons and logic for
    Sold/Cancelled.
    - Added computed fields: total_area (living + garden), best_offer, and
    selling_price.
    - Added SQL constraints and Python constraints to ensure offers are above
    a threshold and selling price is logically correct.
    - Used computed and inverse fields to manage garden area visibility based on the
    garden boolean.
    - Refactored UI behavior (e.g., readonly fields) based on the state of the
    property.
    - Automatically assigns partner to offers upon creation.
    - Used Inheritance for inheriting the class and use the features.
    - Created kanban view while using drag and drop.
    
    These changes reinforce business rules and improve usability for real estate
     agents managing listings and offers.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,19 +1,22 @@
 {
     'name': 'Real Estate',
     'version': '1.0',
-    'depends': ['base'],
+    'depends': ['base', 'sale', 'mail'],
     'author': 'Rajeev Aanjana',
     'category': 'Real Estate',
     'description': 'A module for managing real estate properties',
     'data':[
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
+        'views/estate_property_offer_views.xml',
         'views/estate_property_type_views.xml',
         'views/estate_property_tag_views.xml',
-        'views/estate_property_offer_views.xml',
+        'views/res_users_views.xml',
         'views/estate_menus.xml',
     ],
     'license': 'LGPL-3',
     'application': True,
     'installable': True,
 }
+
+
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,13 +1,16 @@
-from odoo import fields, api, models
 from dateutil.relativedelta import relativedelta
-from odoo.exceptions import UserError
-from odoo.exceptions import ValidationError
-from odoo.tools.float_utils import float_compare
+
+from odoo import fields, models,api
+from odoo.exceptions import UserError, ValidationError
+from odoo.tools.float_utils import float_compare, float_is_zero
+
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real Estate Property"
-
+    _order = "id desc"
+    _inherit = ["mail.thread"]
+    
     # Basic Fields
     name = fields.Char(string="Name", required=True)
     description = fields.Text(string="Description")
@@ -26,48 +29,42 @@ class EstateProperty(models.Model):
     
     # Property Details
     bedrooms = fields.Integer(string="Bedrooms", default=2)
-    living_area = fields.Integer(string="Living Area (sqm)")
+    living_area = fields.Float(string="Living Area (sqm)")
     facades = fields.Integer(string="Facades")
     garage = fields.Boolean(string="Garage")
     garden = fields.Boolean(string="Garden")
-    garden_area = fields.Integer(string="Garden Area (sqm)")
+    garden_area = fields.Float(string="Garden Area (sqm)")
+    
+    best_price = fields.Float("Best Offer", compute="_compute_best_price", store=True)
     
     # Selection Fields
-    garden_orientation = fields.Selection(
-        selection=[
-            ('north', 'North'),
-            ('south', 'South'),
-            ('east', 'East'),
-            ('west', 'West'),
-        ],
-        string="Garden Orientation"
-    )
-    
+    garden_orientation = fields.Selection([('north', 'North'),('south', 'South'),('east', 'East'),('west', 'West')], string='Garden Orientation')
     state = fields.Selection(
-        selection=[
-            ('new', 'New'),
-            ('offer_received', 'Offer Received'),
-            ('offer_accepted', 'Offer Accepted'),
-            ('sold', 'Sold'),
-            ('cancelled', 'Cancelled'),
+        [
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("canceled", "Canceled"),
         ],
         string="Status",
         required=True,
         copy=False,
-        default='new'
+        default="new",
     )
+
     
     active = fields.Boolean(string="Active", default=True)
     
     # Many2one, Many2many, One2many Relation to Property Type
-    property_type_id = fields.Many2one("estate.property.type", string="Property Type")
-    
+    property_type_id = fields.Many2one('estate.property.type', string="Property Type", required=True)
     buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
     
     salesperson_id = fields.Many2one(
-    "res.users", 
-    string="Salesperson", 
-    default=lambda self: self.env.user
+        "res.users",
+        string="Sales Person",
+        default=lambda self: self.env.user,
+        required=True,
     )
     
     offer_ids = fields.One2many(
@@ -76,98 +73,123 @@ class EstateProperty(models.Model):
     string="Offers"
     )
     
-    tag_ids = fields.Many2many("estate.property.tag", string="Tags", widget="many2many_tags" )
+    tag_ids = fields.Many2many("estate.property.tag", string="Tags" )
+    total_area = fields.Float(string='Total Area (sqm)',compute='_compute_total_area',store=True,help='Sum of living area and garden area')
     
+                
+    # SQL Constraints
+        
+    _sql_constraints = [
+        (
+            "check_expected_price_positive",
+            "CHECK(expected_price >= 0)",
+            "The expected price must be strictly positive.",
+        ),
+        (
+            "check_selling_price_positive",
+            "CHECK(selling_price >= 0)",
+            "The selling price must be strictly positive.",
+        ),
+        (
+            "check_bedrooms_positive",
+            "CHECK(bedrooms >= 0)",
+            "The number of bedrooms must be zero or positive.",
+        ),
+    ]
     # Computed Fields & Onchange
-    
-    total_area = fields.Float(string="Total Area",compute="_compute_total_area")
     
     @api.depends("living_area", "garden_area")
     def _compute_total_area(self):
         for record in self:
             record.total_area = record.living_area + record.garden_area
     
-    best_price = fields.Float("Best Offer", compute="_compute_best_price", store=True)
     
-    @api.depends("offer_ids.price")
+    
+
+    @api.depends("offer_ids.offer_price")
     def _compute_best_price(self):
         for record in self:
-            if record.offer_ids:
-                record.best_price = max(record.offer_ids.mapped("price"))
-            else:
-                record.best_price = 0.0
+            record.best_price = max(record.offer_ids.mapped("offer_price"), default=0.0)
 
     # Onchange
     
-    @api.onchange("garden")
+    @api.onchange('garden')
     def _onchange_garden(self):
-        if self.garden:
-            self.garden_area = 10
-            self.garden_orientation = "North"
-        else:
-            self.garden_area = 0
-            self.garden_orientation = False
-
-
+        for record in self:
+            if record.garden:
+                record.garden_area = 10
+                record.garden_orientation = 'north'
+            else:
+                record.garden_area = 0
+                record.garden_orientation = ''
+                
     # Add Action Logic of "Cancel" & "Sold"
     
     def action_set_sold(self):
         for record in self:
-            if record.state == "cancelled":
-                raise UserError("Canceled property cannot be sold.")
+            if record.state == "canceled":
+                raise UserError("A cancelled property cannot be sold.")
+
+            if record.state != "offer_accepted":
+                raise UserError(
+                    "You cannot mark a property as sold without accepting an offer."
+                )
+
             record.state = "sold"
-        return {
-            'type': 'ir.actions.client',
-            'tag': 'display_notification',
-            'params': {
-                'title': 'Success',
-                'message': 'The property has been marked as sold.',
-                'sticky': False,
-                'type': 'success',  
-            }
-        }
+
+        return True
+
+        # return {
+        #     'type': 'ir.actions.client',
+        #     'tag': 'display_notification',
+        #     'params': {
+        #         'title': 'Success',
+        #         'message': 'The property has been marked as sold.',
+        #         'sticky': False,
+        #         'type': 'success',  
+        #     }
+        # }
 
     def action_set_canceled(self):
         for record in self:
             if record.state == "sold":
                 raise UserError("Sold property cannot be canceled.")
-            record.state = "cancelled"
-        return {
-            'type': 'ir.actions.client',
-            'tag': 'display_notification',
-            'params': {
-                'title': 'Cancelled',
-                'message': 'The property has been marked as canceled.',
-                'sticky': False,
-                'type': 'warning',  
-            }
-        }
+            record.state = "canceled"
+        # return {
+        #     'type': 'ir.actions.client',
+        #     'tag': 'display_notification',
+        #     'params': {
+        #         'title': 'canceled',
+        #         'message': 'The property has been marked as canceled.',
+        #         'sticky': False,
+        #         'type': 'warning',  
+        #     }
+        # }
         
-        
-        # SQL Constraints
-        
-    _sql_constraints = [
-        (
-            "check_expected_price_positive",
-            "CHECK(expected_price > 0)",
-            "The expected price must be strictly positive.",
-        ),
-        (
-            "check_selling_price_positive",
-            "CHECK(selling_price >= 0)",
-            "The selling price must be positive.",
-        ),
-    ]
 
     @api.constrains('selling_price', 'expected_price')
-    def _check_selling_price_threshold(self):
+    def _check_selling_price(self):
         for record in self:
-            if record.selling_price:
-                if float_compare(
-                    record.selling_price,
-                    record.expected_price * 0.9,
-                    precision_digits=2
-                ) < 0:
-                    raise ValidationError(
-                        "The selling price cannot be lower than 90% of the expected price."
-                    )
+            if float_is_zero(record.selling_price, precision_digits=2):
+                continue
+            min_acceptable_price = 0.9 * record.expected_price
+            if float_compare(record.selling_price, min_acceptable_price, precision_digits=2) < 0:
+                raise ValidationError("The selling price cannot be lower than 90% of the expected price.")
+    
+    @api.ondelete(at_uninstall=False)
+    def _check_state_before_delete(self):
+        for record in self:
+            if record.state not in ('new', 'canceled'):
+                raise UserError("You can only delete properties in 'New' or 'canceled' state.")
+            
+    
+    # def check_offer(self, price):
+    #     curr_offers = []
+    #     for record in self:
+    #         for offer in record.offer_ids:
+    #             curr_offers.append(offer.offer_price)
+    #         if curr_offers:
+    #             if price < min(curr_offers):
+    #                 raise exceptions.UserError("New offer cannot be lower than other offers")
+    #         record.state = 'offer_received'
+            
