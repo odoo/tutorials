73465b9174076043aa9d481e235b1af326a53736
oumaima-aarabe <111888559+oumaima-aarabe@users.noreply.github.com>
2025-05-30 10:00:42 +0200

    [ADD] awesome_owl: update Card component to use slots for dynamic content rendering

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -4,6 +4,9 @@ export class Card extends Component {
     static template = "awesome_owl.Card";
     static props = {
         title: { type: String },
-        content: { type: String, optional: true },
+        slots: {
+            type: Object,
+            shape: { default:true },
+        }
     }
 }
