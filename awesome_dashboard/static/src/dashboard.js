da6d58ef92b1c9e0a8c549b6f39e66102081bb42
Backendt <matisse.beckandt@proton.me>
2025-05-30 09:46:38 +0200

    [ADD] awesome_dashboard: Added Customer and Lead buttons to dashboard

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -3,6 +3,7 @@
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
@@ -12,6 +13,23 @@ class AwesomeDashboard extends Component {
         this.display = {
             controlPanel: {}
         };
+        this.action = useService("action");
+    }
+
+    openCustomerView() {
+        this.action.doAction("base.action_partner_form");
+    }
+
+    openLeadView() {
+        this.action.doAction({
+            type: "ir.actions.act_window",
+            name: "Leads",
+            res_model: "crm.lead",
+            views: [
+                [false, "list"],
+                [false, "form"]
+            ]
+        });
     }
 }
 
