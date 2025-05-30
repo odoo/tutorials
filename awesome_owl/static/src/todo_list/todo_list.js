cf1b7929af50e198c876c70b3032882aac29e658
oumaima-aarabe <111888559+oumaima-aarabe@users.noreply.github.com>
2025-05-30 09:15:35 +0200

    [ADD] awesome_owl: implement autofocus utility and integrate it in TodoList component

--- awesome_owl/static/src/todo_list/todo_list.js
+++ awesome_owl/static/src/todo_list/todo_list.js
@@ -1,5 +1,6 @@
 import {Component, useState} from "@odoo/owl";
 import {TodoItem} from "./todo_item";
+import { useAutofocus } from "../utils";
 
 export class TodoList extends Component {
     static template = "awesome_owl.TodoList";
@@ -7,6 +8,7 @@ export class TodoList extends Component {
 
     setup() {
         this.todos = useState([]);
+        useAutofocus("input")
     }
 
     addTodo(ev) {
---
+++ awesome_owl/static/src/utils.js
@@ -0,0 +1,13 @@
+import {useRef, onMounted} from "@odoo/owl";
+
+export function useAutofocus(input) {
+    const inputRef = useRef(input);
+
+    onMounted(() => {
+        if (inputRef.el) {
+            inputRef.el.focus();
+        }
+    });
+
+    return { inputRef };
+}
