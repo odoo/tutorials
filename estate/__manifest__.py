1596540da220e9a6a391550cccb1cf802a335cf6
Arthur Mermet <amer@odoo.com>
2025-05-30 17:08:58 +0200

    [ADD] estate module: create estate property model and related files

---
+++ estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'estate',
+    'depends': [
+        'base',
+    ],
+    'installable': True,
+    'application': True,
+    'data': [
+        'models/estate_property.xml',
+        'security/ir.model.access.csv',
+    ],
+}
