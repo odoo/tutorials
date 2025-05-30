82ac6adb96b238215528916b1c7c1790a07ce61d
oumaima-aarabe <111888559+oumaima-aarabe@users.noreply.github.com>
2025-05-30 10:19:36 +0200

    [ADD] awesome_owl: enhance Card component with toggle functionality and state management

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -1,4 +1,4 @@
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 
 export class Card extends Component {
     static template = "awesome_owl.Card";
@@ -7,6 +7,16 @@ export class Card extends Component {
         slots: {
             type: Object,
             shape: { default:true },
+        },
+    }
+    
+    setup() {
+        this.state = useState({ isToggled: true });
+    }
+    toggle() {
+        this.state.isToggled = !this.state.isToggled;
+        if (this.props.onChange) {
+            this.props.onChange(this.state.isToggled);
         }
     }
 }
