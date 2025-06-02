8e6c77d905ec9537505d4b41e242f29d259ee874
Arthur Mermet <amer@odoo.com>
2025-06-02 16:58:09 +0200

    [ADD] estate module: added relationnal fields tox_ estate_property and created the x_estate_property_type model

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,8 +4,12 @@
         'base',
     ],
     'data': [
+        'models/estate_property_type.xml',
+        'views/estate_property_type_views.xml',
+
         'models/estate_property.xml',
         'views/estate_property_views.xml',
+
         'views/estate_menus.xml',
         'security/ir.model.access.csv',
 
