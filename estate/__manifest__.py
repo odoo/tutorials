d964a55805bfff355ba0612a6762ffe03885dd1e
Arthur Mermet <amer@odoo.com>
2025-06-02 11:33:24 +0200

    [IMP] estate module: addoed basic list and form view to estate properties + menus

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,7 +5,10 @@
     ],
     'data': [
         'models/estate_property.xml',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
         'security/ir.model.access.csv',
+
     ],
     'license': 'LGPL-3',
 }
