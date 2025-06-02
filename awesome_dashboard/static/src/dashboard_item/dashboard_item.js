5a048a9829a52cd50d77b77eb490c3847735e5c1
oumaima-aarabe <111888559+oumaima-aarabe@users.noreply.github.com>
2025-06-02 10:01:26 +0200

    [ADD] awesome_dashboard: add DashboardItem component and integrate it into AwesomeDashboard layout

---
+++ awesome_dashboard/static/src/dashboard_item/dashboard_item.js
@@ -0,0 +1,16 @@
+import { Component } from "@odoo/owl";
+export class DashboardItem extends Component {
+    static template = "awesome_dashboard.DashboardItem"
+    static props = {
+        slots: {
+            type: Object,
+            optional: true,
+            default: () => ({}),
+        },
+        size: {
+            type: Number,
+            default: 1,
+            optional: true,
+        },
+    };
+}
