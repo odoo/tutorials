cb822ee38be586ef1a774bab6bc06be3f36730b1
Arthur Mermet <amer@odoo.com>
2025-06-03 14:35:13 +0200

    [ADD] estate module: add server action to accept property offers and update related fields

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,15 +7,15 @@
         'models/estate_models.xml',
 
         'models/fields/estate_property_type.xml',
-        'views/estate_property_type_views.xml',
-
         'models/fields/estate_property_tag.xml',
-        'views/estate_property_tag_views.xml',
-
         'models/fields/estate_property_offer.xml',
-        'views/estate_property_offer_views.xml',
-
         'models/fields/estate_property.xml',
+
+        'models/estate_server_actions.xml',
+
+        'views/estate_property_type_views.xml',
+        'views/estate_property_tag_views.xml',
+        'views/estate_property_offer_views.xml',
         'views/estate_property_views.xml',
 
         'views/estate_menus.xml',
