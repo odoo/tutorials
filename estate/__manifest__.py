b219aad27a11be89ab2c4377a3a367585a57c63c
abdrahmanrashed <abdelrahman0rashed@gmail.com>
2025-06-04 13:04:39 +0200

    [IMP] -added some estate property types that are available with estate module installation.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -8,11 +8,14 @@
     'data': [
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
-        'views/estate_property_type_views.xml',
         'views/estate_property_tag_views.xml',
         'views/estate_property_offer_views.xml',
+        'views/estate_property_type_views.xml',
         'views/estate_menus.xml',
-        'views/res_users_views.xml'
+        'views/res_users_views.xml',
+    ],
+    'demo': [
+        'demo/estate.property.type.csv'
     ],
     'installable': True,
     'application': True,
