0076e8672c884836f06144397c9046a3f1ab6b28
arat-odoo <arat@odoo.com>
2025-06-04 19:20:25 +0530

    [ADD] estate: created first module
    Understand the fundamentals of the Odoo framework and its file paths.
    Created the first Estate module and did the configuration file.
    I created the model of the many fields and implemented the user access policies
    and did the basic UI part of the estate module.

---
+++ estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Estate",
+    "version": "1.2",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
+    ],
+    "appication": True,
+    "sequence": 1,
+}
