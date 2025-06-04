2ea4b47fc52d4f926fb0a024186155020f05e3bb
A-Yehia19 <abdullah.yehia19@gmail.com>
2025-06-04 10:05:39 +0200

    [ADD] configuration dialogue

--- awesome_dashboard/static/src/dashboard/dashboard.js
+++ awesome_dashboard/static/src/dashboard/dashboard.js
@@ -1,9 +1,11 @@
 /** @odoo-module **/
 
 import { Component, useState } from "@odoo/owl";
+import { browser } from "@web/core/browser/browser";
 import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
 import { Layout } from "@web/search/layout";
+import { ConfigurationDialog } from "./configuration_dialogue/configuration_dialogue";
 import { DashboardItem } from "./dashboard_item/dashboard_item";
 import { PieChart } from "./pie_chart/pie_chart";
 
@@ -17,6 +19,10 @@ class AwesomeDashboard extends Component {
         this.action = useService("action");
         this.items = registry.category("awesome_dashboard").getAll();
         this.dashboardData = useState(useService("load_statistics"));
+        this.dialog = useService("dialog");
+        this.state = useState({
+            disabledItems: browser.localStorage.getItem("disabledDashboardItems")?.split(",") || [],
+        });
     }
 
     openCustomers() {
@@ -33,6 +39,18 @@ class AwesomeDashboard extends Component {
                 [false, "form"],
             ],
         });
+    }
+
+    openConfiguration() {
+        this.dialog.add(ConfigurationDialog, {
+            items: this.items,
+            disabledItems: this.state.disabledItems,
+            onUpdateConfiguration: this.updateConfiguration.bind(this),
+        })
+    }
+
+    updateConfiguration(newDisabledItems) {
+        this.state.disabledItems = newDisabledItems;
     }
 }
 
