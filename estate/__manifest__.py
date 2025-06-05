60c281cf8ec72c07df0ebd057b95781a30ce6353
abdrahmanrashed <abdelrahman0rashed@gmail.com>
2025-06-05 17:41:45 +0200

    [IMP] -Created estate property type model. -Added list view to estate property type. -Added menu to access estate property type. -Added access rights to estate property type. -Added some related fields to estate property. -Added the new fields to the view.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,9 +7,11 @@
         'base'
     ],
     'data': [
+        'data/estate_property_type.xml',
         'data/estate_property.xml',
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
+        'views/estate_property_type_views.xml',
         'views/estate_menus.xml',
     ],
     'license': 'OEEL-1',
