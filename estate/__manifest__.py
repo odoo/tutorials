6c1dc9abc36be602e3285471bb83b13fdade151f
abdrahmanrashed <abdelrahman0rashed@gmail.com>
2025-06-05 18:07:41 +0200

    [IMP] -created estate property tag model. -created list view for estate property tag. -created menu to access estate property tags. -created a filed for tag ids in estate property. -Added the new field to estate property view.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -8,10 +8,12 @@
     ],
     'data': [
         'data/estate_property_type.xml',
+        'data/estate_property_tag.xml',
         'data/estate_property.xml',
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_property_type_views.xml',
+        'views/estate_property_tag_views.xml',
         'views/estate_menus.xml',
     ],
     'license': 'OEEL-1',
