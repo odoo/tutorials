f37f7e84bce24b2f6e98834c23d3dd8a6b374f22
abdrahmanrashed <abdelrahman0rashed@gmail.com>
2025-06-05 11:08:36 +0200

    [ADD] -Created an importable estate module.

---
+++ estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Estate',
+    'version': '1.0',
+    'category': 'Estate',
+    'summary': 'Tutorial module',
+    'depends': [
+        'base'
+    ],
+    'data': [
+    ],
+    'license': 'OEEL-1',
+}
