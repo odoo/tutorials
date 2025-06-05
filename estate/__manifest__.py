df43be021c1cc8beb05077e973974f80732c67d3
alap-odoo <alap@odoo.com>
2025-06-05 10:47:54 +0530

    [ADD] estate: created New Real Estate module
    Added the new estate Folder. I created the init and manifest files for this.
    Moreover, I have created the models for schema definition, security for the
    assigning access rights, created the views folder, and made the three-level
    menuitems along with the list and form view.

---
+++ estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Real_Estate_Module",
+    "version": "1.0",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
+    ],
+    'sequence':1,
+    'application': True,
+}
+
