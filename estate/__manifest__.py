3855227138404c540d3bc853655c9f571d153be1
abdrahmanrashed <abdelrahman0rashed@gmail.com>
2025-06-06 10:33:49 +0200

    [IMP] -created estate property offer model. -Added access rights to the model. -created list and form view to model. -Added offer ids field to estate property model, and added it to its view.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,13 +7,16 @@
         'base'
     ],
     'data': [
+        'data/estate_models.xml',
         'data/estate_property_type.xml',
         'data/estate_property_tag.xml',
+        'data/estate_property_offer.xml',
         'data/estate_property.xml',
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_property_type_views.xml',
         'views/estate_property_tag_views.xml',
+        'views/estate_property_offer_views.xml',
         'views/estate_menus.xml',
     ],
     'license': 'OEEL-1',
