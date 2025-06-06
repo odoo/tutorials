271bb4f055678fcd9f2a7b1b89be02ab9cd24a56
abdrahmanrashed <abdelrahman0rashed@gmail.com>
2025-06-06 17:15:23 +0200

    [IMP] -Added new API field to estate property model. -Added access rights to public user. -Added read and write record rules to public user. -Added server action to return estate property list.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,7 +4,7 @@
     'category': 'Estate',
     'summary': 'Tutorial module',
     'depends': [
-        'base'
+        'website'
     ],
     'data': [
         'data/estate_models.xml',
@@ -12,12 +12,14 @@
         'data/estate_property_tag.xml',
         'data/estate_property_offer.xml',
         'data/estate_property.xml',
-        'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_property_type_views.xml',
         'views/estate_property_tag_views.xml',
         'views/estate_property_offer_views.xml',
         'views/estate_menus.xml',
+        'controllers/estate_controller.xml',
+        'security/ir.model.access.csv',
+        'security/estate_security.xml',
     ],
     'license': 'OEEL-1',
 }
