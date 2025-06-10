01cbc5ee1a96e9b306732109b5305cc527dd9c0f
alap-odoo <alap@odoo.com>
2025-06-10 17:58:52 +0530

    [IMP] estate_account: added the generate invoice functionality
    inherited the estate.property model and overide the action_sold method to
    generate the invoice using account_move.

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -6,4 +6,31 @@ class EstateProperty(models.Model):
     _description = "Real Estate Property"
 
     def action_sold(self):
+        for property in self:
+            self.env["account.move"].create(
+                {
+                    "partner_id": self.buyer_id.id,
+                    "move_type": "out_invoice",
+                    "invoice_line_ids": [
+                        (
+                            0,
+                            0,
+                            {
+                                "name": "6% Commission",
+                                "quantity": 1,
+                                "price_unit": property.selling_price * 0.06,
+                            },
+                        ),
+                        (
+                            0,
+                            0,
+                            {
+                                "name": "Administrative Fees",
+                                "quantity": 1,
+                                "price_unit": 100.00,
+                            },
+                        ),
+                    ],
+                }
+            )
         return super().action_sold()
