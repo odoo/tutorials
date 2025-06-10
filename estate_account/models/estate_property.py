0c696d1f86797c1a02104887de30bc876dc2b0df
hapt-odoo <hapt@odoo.com>
2025-06-10 18:01:20 +0530

    [IMP] estate_account: generated invoice
    IN the estate_account module, override the action_sold of
    the estate_property model and use the  account_move the generated
    Invoice of the property

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -3,7 +3,29 @@ from odoo import models
 
 class EstateProperty(models.Model):
     _inherit = "estate.property"
+    _description = "Estate Property Model with Account Move"
 
     def action_sold(self):
-        self.env['account.move'].create({})
+        for record in self:
+            record.env['account.move'].create({
+                'move_type': 'out_invoice',
+                'partner_id': record.partner_id.id,
+                'invoice_line_ids': [
+                    (0, 0, {
+                        'name': 'Property Price',
+                        'quantity': 1,
+                        'price_unit': record.selling_price,
+                    }),
+                    (0, 0, {
+                        'name': '6% Commission',
+                        'quantity': 1,
+                        'price_unit': record.selling_price * 0.06,
+                    }),
+                    (0, 0, {
+                        'name': 'Administrative Fees',
+                        'quantity': 1,
+                        'price_unit': 100.00,
+                    }),
+                ],
+            })
         return super().action_sold()
