975a42e871b68c6beba4a702f1bf8a96493ba16e
hapt-odoo <hapt@odoo.com>
2025-06-12 18:26:42 +0530

    [IMP] estate: added data using xml file , Integrated Messaging features
    IN the estate module, added demo data using an XML file and
    added search, added X2many fields in demo.xml.
    In business_trip, added chatter and created subtypes
    for message notifications.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -35,7 +35,7 @@ class EstateProperty(models.Model):
     user_id = fields.Many2one('res.users', string='Salesman', default=lambda self: self.env.user)
     partner_id = fields.Many2one('res.partner', string='Buyer', copy=False)
     tag_ids = fields.Many2many('estate.property.tag', string='Tags')
-    offer_id = fields.One2many('estate.property.offer', 'property_id', string='Offers')
+    offer_ids = fields.One2many('estate.property.offer', 'property_id', string='Offers')
     total_area = fields.Integer(compute='_compute_total_area', store=True)
     best_price = fields.Float(
         string='Best Offer',
@@ -55,10 +55,10 @@ class EstateProperty(models.Model):
         for record in self:
             record.total_area = record.living_area + record.garden_area
 
-    @api.depends('offer_id.price')
+    @api.depends('offer_ids.price')
     def _compute_best_price(self):
         for record in self:
-            best_offer = max(record.offer_id.mapped('price'), default=0)
+            best_offer = max(record.offer_ids.mapped('price'), default=0)
             record.best_price = best_offer
 
     @api.onchange('garden')
