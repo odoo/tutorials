66b1c3afafc05cd4e81b9116b6c3f258081c02f8
Nicolas Renard <nire@odoo.com>
2025-06-17 13:30:40 +0200

    [ADD] initial commit

---
+++ estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Real estate',
+    'description': 'Real estate app',
+    'category': 'Estate',
+    'application': True,
+    # 'version': '18.0',
+    'author': 'Nicolas Renard',
+    'data': [
+    ],
+    'assets': {
+    },
+}
