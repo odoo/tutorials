b9f198c35d73e93bf7ae521384d8396182c8e2ed
Nicolas Renard <nire@odoo.com>
2025-06-18 09:06:15 +0200

    [ADD] chapter 4 and 5

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,6 +6,9 @@
     # 'version': '18.0',
     'author': 'Nicolas Renard',
     'data': [
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+        'security/ir.model.access.csv'
     ],
     'assets': {
     },
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,23 +1,33 @@
 from odoo import fields,models
+from dateutil.relativedelta import relativedelta
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real estate properties"
 
-    name = fields.Char(required=True)
-    description = fields.Text()
-    postcode = fields.Char()
-    date_availability = fields.Date()
-    expected_price = fields.Float()
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
-    living_area = fields.Integer()
-    facades = fields.Integer()
-    garage = fields.Boolean()
-    garden = fields.Boolean()
-    garden_area = fields.Integer()
+    name = fields.Char("Estate name",required=True)
+    description = fields.Text("Description")
+    postcode = fields.Char("Postcode")
+    date_availability = fields.Date("Date availability",default=fields.Date.today() + relativedelta(months=3),copy=False)
+    expected_price = fields.Float("Expected price")
+    selling_price = fields.Float("Selling price",readonly=True,copy=False)
+    bedrooms = fields.Integer("Number of bedrooms",default=2)
+    living_area = fields.Integer("Living area")
+    facades = fields.Integer("Facades")
+    garage = fields.Boolean("Garage")
+    garden = fields.Boolean("Garden")
+    garden_area = fields.Integer("Garden area")
     garden_orientation = fields.Selection(
         string='Garden orientation',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
         help="This Type is used to tell the garden orientation for a property"
+        )
+    active = fields.Boolean("Active",default=True)
+    state = fields.Selection(
+        string='State',
+        selection=[('new', 'New'), ('offer_received', 'Offer received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Canceled')],
+        help="This is the state of the property",
+        default="new",
+        required=True,
+        copy=False
         )
