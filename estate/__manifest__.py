ef18c3f16b887d3a42167c13f79be92635c7fa2c
baje-odoo <baje@odoo.com>
2025-06-18 09:39:08 +0200

    [FIX] estate: pipeline fix

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,14 +2,10 @@
     'name': "estate",
     'version': '0.1',
     'depends': ['base'],
-    'data':[
-<<<<<<< HEAD
+    'data': [
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_menus.xml',
-=======
-        'security/ir.model.access.csv'
->>>>>>> 6f53f08 (Security)
         ],
     'author': "baje",
     'category': 'Uncategorized',
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -2,6 +2,7 @@ from odoo import fields, models
 from dateutil.relativedelta import relativedelta
 
 class EstateProperty(models.Model):
+
     _name = "estate.property"
     _description = "estate properties"
     _order = "id"
@@ -9,7 +10,7 @@ class EstateProperty(models.Model):
     name = fields.Char("Title", required=True)
     description = fields.Text("Description")
     postcode = fields.Char("Postcode")
-    date_availability = fields.Date("Available From", default=(fields.Date.today()+relativedelta(months=3)))
+    date_availability = fields.Date("Available From", default=(fields.Date.today() + relativedelta(months=3)))
     expected_price = fields.Float("Expected Price", required=True)
     selling_price = fields.Float("Selling Price", readonly=True, copy=False)
     bedrooms = fields.Integer("Bedrooms", default=2)
@@ -20,10 +21,10 @@ class EstateProperty(models.Model):
     garden_area = fields.Integer("Garden Area (sqm)")
     garden_orientation = fields.Selection(
         string="Garden Orientation",
-        selection=[('north',"North"),('south',"South"),('east',"East"),('west',"West")]
+        selection=[('north',"North"), ('south',"South"), ('east',"East"), ('west',"West")]
     )
 
-    active= fields.Boolean("Active", default=True)
+    active = fields.Boolean("Active", default=True)
 
     state = fields.Selection(
         selection=[
