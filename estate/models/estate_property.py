db8607d414ebe8bd7e38e5802da2a5c775dba8f2
MostafaTwfiq <mostafa.mohammad.twfiq@gmail.com>
2025-06-18 15:51:00 +0200

    [ADD] estate: finished chapter 6.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import fields, models
+from odoo.tools import date_utils
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -7,18 +8,26 @@ class EstateProperty(models.Model):
     name = fields.Char(string='Estate Name', required=True)
     description = fields.Text(string='Description')
     postcode = fields.Char(string='Postcode')
-    date_available = fields.Date(string='Date Available')
+    date_available = fields.Date(string='Available From Date', copy=False, default=date_utils.add(fields.Date.today(), months=3))
     expected_price = fields.Float(string='Expected Price', required=True)
-    selling_price = fields.Float(string='Selling Price')
-    bedrooms = fields.Integer(string='Bedrooms')
-    living_area = fields.Integer(string='Living Area')
+    selling_price = fields.Float(string='Selling Price', readonly=True, copy=False)
+    bedrooms = fields.Integer(string='Bedrooms', default=2)
+    living_area = fields.Integer(string='Living Area (sqm)')
     facades = fields.Integer(string='Facades')
-    garage = fields.Boolean(string='Garage')
-    garden = fields.Boolean(string='Garden')
-    garden_area = fields.Integer(string='Garden Area')
+    garage = fields.Boolean(string='Has a Garage')
+    garden = fields.Boolean(string='Has a Garden')
+    garden_area = fields.Integer(string='Garden Area (sqm)')
     garden_orientation = fields.Selection(string='Garden Orientation', selection=[
         ('north', "North"),
         ('south', "South"),
         ('east', "East"),
         ('west', "West")
+    ])
+    active = fields.Boolean(string='Active', default=True)
+    state = fields.Selection(string='State', required=True, copy=False, default='new', selection=[
+        ('new', "New"),
+        ('offer received', "Offer Received"),
+        ('offer accepted', "Offer Accepted"),
+        ('sold', "Sold"),
+        ('cancelled', "Cancelled")
     ])
