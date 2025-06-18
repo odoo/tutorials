13630ec8960955db9a53135b86627eb88e00a45a
hapt-odoo <hapt@odoo.com>
2025-06-18 18:26:00 +0530

    [IMP] awesome_owl: added count and card owl-components in template
    In the awesome_owl module, using the owl framework and
    Created a 2-components counter and card, and also created
    function using props to do the sum of both the counter values
    And in the card component used markup with t-out.

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,7 +1,19 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { markup,Component,useState} from "@odoo/owl";
+import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
+    static components = { Counter,Card };
+    setup() {
+        this.state = useState({ sum: 2 });
+    }
+
+    incrementSum(value) {
+        this.state.sum += value;
+    }
+    val1 = "<div class='text-primary'>some text 1</div>";
+    content = markup("<div class='text-primary'>some text 2</div>");
 }
