b398197a49f442b180ca5144c1ec7eacf99b407d
baje-odoo <baje@odoo.com>
2025-06-18 14:17:52 +0200

    [IMP] estate: Add one2many relations

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -47,3 +47,4 @@ class EstateProperty(models.Model):
     user_id = fields.Many2one("res.users", string="Salesman", default=lambda self: self.env.user)
     buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
     tag_ids = fields.Many2many("estate.property.tag", string="Tags")
+    offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
