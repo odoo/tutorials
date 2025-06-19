a1bbac3ee1752631597cc4923c2ffbe49b6c157d
rhri <rhri@odoo.com>
2025-06-19 14:31:34 +0700

    [IMP] estate: create basic views for estate module
    create menu to better navigate the estate module
    create form to interact with estate property model
    Server Framework 101 Chapter 5

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -4,16 +4,35 @@ class Property(models.Model):
     _name = "estate.property"
     _description = "Estate Property"
     
-    name = fields.Char('Property Name', required=True, translate=True)
+    # misc
+    name = fields.Char('Property Name', required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(default=fields.Date.add(fields.Date.today(), months=3), copy=False)
+    
+    # price
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    
+    # rooms
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
-    garden_orientation = fields.Selection([('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
+    garden_orientation = fields.Selection([
+        ('north', 'North'), 
+        ('south', 'South'), 
+        ('east', 'East'), 
+        ('west', 'West')])
+    
+    # reserved
+    active = fields.Boolean(default=True)
+    state = fields.Selection([
+        ('new', 'New'), 
+        ('offer_received', 'Offer Received'), 
+        ('offer_accepted', 'Offer Accepted'), 
+        ('sold', 'Sold'), 
+        ('cancelled', 'Cancelled')],
+        required=True, default='new', copy=False)
