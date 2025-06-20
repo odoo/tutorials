0d4258d29cc628ee530059067fd4505c2625d0c7
alap-odoo <alap@odoo.com>
2025-06-20 18:07:21 +0530

    [IMP] awesome_dashboard: added buttons and statistics data to dashboard module
    Created customer and lead buttons and given their respective click events.
    Moreover, the cards were added to the dashboard component.
    Further, the statistics are added in the dashboard through an RPC call.

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,10 +1,38 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, useState, onWillStart } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
+import { DashboardItem } from "./dashboardItem/dashboarditem";
+import { rpc } from "@web/core/network/rpc";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
+
+    static components = { Layout, DashboardItem };
+
+    setup() {
+        this.action = useService("action");
+        this.state = useState({ statistics: null });
+
+        onWillStart(async () => {
+            this.state.statistics = await rpc("/awesome_dashboard/statistics");
+        });
+    }
+
+    openCustomerView() {
+        this.action.doAction("base.action_partner_form")
+    }
+
+    openLeadsView() {
+        this.action.doAction({
+            type: 'ir.actions.act_window',
+            target: 'current',
+            res_model: 'crm.lead',
+            views: [[false, 'list'], [false, 'form']],
+        })
+    }
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
