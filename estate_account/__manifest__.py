bfdb4ba66da6fc6ea029a78542dad1b1d5657f48
baje-odoo <baje@odoo.com>
2025-06-20 09:50:44 +0200

    [ADD] estate_account: Add link between estate and account

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "estate_account",
+    'version': '0.1',
+    'depends': ['estate', 'account'],
+    'author': "baje",
+    'category': 'Uncategorized',
+    'description': """
+    Link between estate and accounting apps
+    """,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,39 @@
+from odoo import models
+
+
+class EstateProperty(models.Model):
+
+    _inherit = "estate.property"
+
+    def action_sold(self):
+        res = super().action_sold()
+        journal = self.env["account.journal"].search([("type", "=", "sale")], limit=1)
+        for prop in self:
+            self.env["account.move"].create(
+                {
+                    "partner_id": prop.buyer_id.id,
+                    "move_type": "out_invoice",
+                    "journal_id": journal.id,
+                    "invoice_line_ids": [
+                        (
+                            0,
+                            0,
+                            {
+                                "name": prop.name,
+                                "quantity": 1.0,
+                                "price_unit": prop.selling_price * 6.0 / 100.0,
+                            },
+                        ),
+                        (
+                            0,
+                            0,
+                            {
+                                "name": "Administrative fees",
+                                "quantity": 1.0,
+                                "price_unit": 100.0,
+                            },
+                        ),
+                    ],
+                }
+            )
+        return res
