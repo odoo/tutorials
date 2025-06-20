3d8b5a7f7722e8d39659deb30b41c73df14fcd05
arat-odoo <arat@odoo.com>
2025-06-20 18:15:14 +0530

    [IMP] awesome_dashboard: created layout,button,add dashboard item,add statistic
    Created new dashboard layout with add customer and lead buttons,also created new
    component dashboarditem using slots. through rpc function implemented statistic
    for dynamic information.

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,10 +1,42 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, useState, onWillStart } from "@odoo/owl";
 import { registry } from "@web/core/registry";
-
+import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
+import { DashboardItem } from "./dashboardItem/dashboard_item";
+import { rpc } from "@web/core/network/rpc";
 class AwesomeDashboard extends Component {
-    static template = "awesome_dashboard.AwesomeDashboard";
+  static template = "awesome_dashboard.AwesomeDashboard";
+  static components = { Layout, DashboardItem };
+
+  setup() {
+    this.action = useService("action");
+    this.state = useState({ statistic: null });
+
+    onWillStart(async () => {
+      this.state.statistic = await rpc("/awesome_dashboard/statistics");
+    });
+  }
+  openCustomers() {
+    this.action.doAction("base.action_partner_form");
+  }
+
+  async openLeads(activity) {
+    this.action.doAction({
+      type: "ir.actions.act_window",
+      name: "Journal Entry",
+      target: "current",
+      res_id: activity.res_id,
+      res_model: "crm.lead",
+      views: [
+        [false, "list"],
+        [false, "form"],
+      ],
+    });
+  }
 }
 
-registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
+registry
+  .category("actions")
+  .add("awesome_dashboard.dashboard", AwesomeDashboard);
