4ca47accf46e26d52f60da4879cb255e209d551c
hapt-odoo <hapt@odoo.com>
2025-06-20 18:07:48 +0530

    [IMP] awesome_dashboard:  Built a dashboard using quick navigation  and server
    In Awesome Dashboard, I created a custom dashboard using
    Quick navigation action and added dashboard item card
    and using controllers, add statistics using the server.

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,10 +1,38 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, useState, onWillStart} from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
+import { DashboardItem } from "./dashboarditem/dashboarditem";
+import { rpc } from "@web/core/network/rpc";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
+    static components = { Layout, DashboardItem };
+
+    setup() {
+        this.action = useService("action");
+        this.state = useState({ statistics: null });
+
+        onWillStart(async () => {
+        this.state.statistics = await rpc("/awesome_dashboard/statistics");
+        });
+    }
+    openCustomers(){
+        this.action.doAction("base.action_partner_form");
+    }
+    async openLeads(){
+        this.action.doAction({
+            type: "ir.actions.act_window",
+            res_model: "crm.lead",
+            views: [[false, "list"], [false, "form"]],
+            target: "current",
+        });
+    }
+
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
+
+export { AwesomeDashboard };
