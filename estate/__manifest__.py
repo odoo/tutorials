6f10d414a025960e5e6256cfd408e8336590b4d5
dhtr-odoo <dhtr@odoo.com>
2025-06-20 10:52:06 +0530

    [FIX] estate: fix external ID error by reordering XML file loading
    Reordered XML files in  to ensure  is loaded before any views referencing it.\n\nPreviously,  referred to  before it was defined in , causing:\n  External ID not found in the system: estate.estate_menu_settings\n\nThis update prevents that error by ensuring menu definitions are loaded first.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,12 +6,13 @@
     "depends": ["base", "web"],
     "data": [
         "security/ir.model.access.csv",
-        "views/estate_property_tag_views.xml",
-        "views/estate_property_offer_views.xml",
-        "views/estate_property_views.xml",
         "views/estate_menus.xml",
+        "views/estate_property_views.xml",
         "views/estate_property_search.xml",
         "views/estate_property_type_views.xml",
+        "views/estate_property_tag_views.xml",
+        "views/estate_property_offer_views.xml",
     ],
+    "license": "LGPL-3", 
     "application": True,
 }
