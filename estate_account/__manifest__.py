31210bc62418c9409fa2b1a806173ac9ba94e949
dhtr-odoo <dhtr@odoo.com>
2025-06-23 18:54:33 +0530

    [ADD] estate_account: link estate sales to invoices

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Estate Accounting",
+    "version": "1.0",
+    "depends": ["estate", "account"],
+    "category": "Real Estate",
+    "description": "Link module to connect Estate properties with Invoicing.",
+    "data": [],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
