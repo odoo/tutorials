82592bd3e9a84293386f0611c7b2a65e94ed5d0a
baje-odoo <baje@odoo.com>
2025-06-24 13:29:38 +0200

    [IMP] awesome_dashboard: Add navigation and dashboard item

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -3,15 +3,34 @@
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
+import { DashboardItem } from "./dashboard_item";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
-    static components = { Layout };
+    static components = { Layout, DashboardItem };
 
     setup(){
+        this.action = useService("action");
         this.display = {
             controlPanel: {},
         };
+    }
+
+    openCustomerView() {
+        this.action.doAction("base.action_partner_form");
+    }
+
+    openLeadView(){
+        this.action.doAction({
+            type: "ir.actions.act_window",
+            name: "All leads",
+            res_model: "crm.lead",
+            views: [
+                [false, "list"], 
+                [false, "form"]
+            ],
+        });
     }
 }
 
