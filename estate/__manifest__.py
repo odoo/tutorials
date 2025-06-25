11c5887dff06f20348020258fea5f3ff2bad3cf3
rhri <rhri@odoo.com>
2025-06-25 11:13:12 +0700

    [IMP] estate: add master and demo data for init
    add standard estate property type as master data using CSV
    add estate property and offer demo data using XML
    Onboarding Data(Module)

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,12 +6,18 @@
     'data': [
         'security/ir.model.access.csv',
 
+        'views/estate_property_offer_views.xml',
         'views/estate_property_type_views.xml',
         'views/estate_property_tag_views.xml',
         'views/estate_property_views.xml',
-        'views/estate_property_offer_views.xml',
         'views/res_users_views.xml',
         'views/estate_menus.xml',
+
+        'data/estate.property.type.csv',
+    ],
+    'demo': [
+        'demo/estate_property_demo.xml',
+        'demo/estate_property_offer_demo.xml',
     ],
     'application': True,
     'license': 'AGPL-3'
