d5b107f4b1b3e46665bf692911eb21bcfa9ddbbb
alap-odoo <alap@odoo.com>
2025-06-25 19:32:35 +0530

    [ADD] invoice_send_automatic: create automated action for sending mail
    - Added a new field in res.config.settings to store the delay for sending
      invoices.
    - Implemented a cron job that triggers the email sending based on the configured
      delay and invoice status.

---
+++ invoice_send_automatic/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "Automated Action Send Invoice Sales",
+    "version": "1.0",
+    "depends": ["base", "sale_management", "accountant"],
+    "data": ["views/res_config_settings_views.xml", "data/ir_cron.xml"],
+    "sequence": 1,
+    "application": True,
+    "license": "OEEL-1",
+}
