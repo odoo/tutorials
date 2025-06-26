2b9cfb90fac4a3940e782ea7a860dd760a677f42
hapt-odoo <hapt@odoo.com>
2025-06-26 18:44:00 +0530

    [ADD] book_pricelist:  Created a module to add pricelist
    Created a float field book_price and added that into
    the sale. order.line list view

---
+++ book_pricelist/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': "Book Pricelist",
+    'category': '',
+    'version': '0.1',
+    'depends': ['sale'],
+    'sequence': 1,
+    'application': True,
+    'installable': True,
+    'data': [],
+    'license': 'AGPL-3'
+}
---
+++ book_pricelist/models/sale_order_line.py
@@ -0,0 +1,7 @@
+from odoo import fields, models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    book_price = fields.Float()
