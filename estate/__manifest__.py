3f513aed466b52ad5ab1c26fef25097da0f132f1
zadh-odoo <zadh@odoo.com>
2025-07-02 18:59:03 +0530

    [ADD] estate: initial module for real estate properties
    - Created new 'estate' module.
    - Added base model 'estate.property' with fields mentioned in exercise.
    - Set up module structure.
    - Set 'name' and 'expected_price' as required fields.

---
+++ estate/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name':"Real Estate",
+    'summary':"This is real estate module",
+    'category':"Tutorials",
+    'description':"This is real estate module",
+    'author':"Dhruvrajsinh Zala (zadh)",
+    'application':True,
+    'installable':True
+}
