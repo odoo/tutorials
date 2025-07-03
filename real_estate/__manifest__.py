f90b30b220d3bdf800eefce3ca071c2df0f9d2fa
rodh-odoo <rodh@odoo.com>
2025-07-03 14:18:15 +0530

    [ADD] real_estate: one model table is created estate_property
    one model created real estate and display in app. one table created for estate
    property in database with dome database .
    variable of table like name, description, date etc.

---
+++ real_estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'estate',
+    'version': '1.0.0',
+    'author': 'rodh',
+    'category': 'Real Estate',
+    'summary': 'Manage real estate properties and transactions',
+    'depends': [
+        'base',
+    ],
+    'application': True,
+    'installable': True,
+}
