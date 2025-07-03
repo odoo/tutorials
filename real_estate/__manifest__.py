85beafe720b27713e2aed98309983c56304b0cb3
metu-odoo <metu@odoo.com>
2025-07-03 12:18:14 +0530

    [ADD] real_estate: initial module structure and database table
    Created the initial file architecture for the real_estate module,
    including the necessary directories and manifest file.
    
    Implemented the estate.property model using Odoo ORM to
    define the database table structure required for storing property-related data.
    
    This commit sets up the foundation for the module, making it ready for
    further development of features such as property management, views,
    and business logic.

---
+++ real_estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Dream Homes",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Megha Tulsyani",
+#     # data files containing optionally loaded demonstration data
+#     'demo': [
+#         'demo/demo_data.xml',
+#     ],
+    'installable': True,
+    'application': True,
+}
