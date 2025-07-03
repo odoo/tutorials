90c491a934d4ce585ca3e125e6b2db522285b65d
kdes-odoo <kdes@odoo.com>
2025-07-03 10:37:53 +0530

    [ADD] estate: initial module creation
    Created a new 'estate' module as per Chapters 2 and 3. Defined the module
    structure, added the business object 'estate.property' in a Python class, and
    configured the __manifest__.py and __init__.py to make the app visible in the
     apps page.

---
+++ estate/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Estate",
+    'version': '1.0',
+    'license': 'LGPL-3',
+    'depends': ['base'],
+    'author': "Kalpan Desai",
+    'category': 'Estate/sales',
+    'description': """
+        Module specifically designed for real estate business case.
+    """,
+    'installable': True,
+    'application': True,
+    
+}
---
+++ estate/models/estate_property.py
@@ -0,0 +1,23 @@
+from odoo import fields, models
+
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Real Estate Property"
+
+    name = fields.Char('Name', required=True)
+    description = fields.Text('Description')
+    postcode = fields.Char('Postcode')
+    date_availability = fields.Date('Available From')
+    expected_price = fields.Float('Expected Price', required=True)
+    selling_price = fields.Float('Selling Price', readonly=True, copy=False)
+    bedrooms = fields.Integer('Bedrooms', default=2)
+    living_area = fields.Integer('Living Area (sqm)')
+    facades = fields.Integer('Facades')
+    garage = fields.Boolean('Garage')
+    garden = fields.Boolean('Garden')
+    garden_area = fields.Integer('Garden Area (sqm)')
+    garden_orientation = fields.Selection(
+        [('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
+        string='Garden Orientation'
+    )
