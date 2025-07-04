6904337f6dcb88264a6952ce0403bb8884293a15
rodh-odoo <rodh@odoo.com>
2025-07-04 10:59:28 +0530

    [ADD] real_estate: add data through csv and xml in list and form view
    first i add data with csv file in module and after that with xml file code
     i add data that display on frontend in list view and form view both
    also disable selling price so anyone cannot change it

--- real_estate/__manifest__.py
+++ real_estate/__manifest__.py
@@ -9,4 +9,10 @@
     ],
     'application': True,
     'installable': True,
+    'license':'LGPL-3',
+    'data': [
+    'data/ir.model.access.csv',
+    'data/estate_property_views.xml',
+    'data/estate_menus.xml',
+    ],
 }
--- real_estate/models/estate_property.py
+++ real_estate/models/estate_property.py
@@ -1,3 +1,4 @@
+from datetime import date, timedelta
 from odoo import fields, models
 
 
@@ -5,13 +6,13 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real Estate Property"
 
-    name = fields.Char(string="Title", required=True)
+    name = fields.Char(default="Unknown", required=True)
     description = fields.Text(string="Description")
     postcode = fields.Char(string="Postcode")
-    date_availability = fields.Date(string="Available From")
+    availability_date = fields.Date(default=lambda self: date.today() + timedelta(days=90), copy=False)
     expected_price = fields.Float(string="Expected Price",required=True)
-    selling_price = fields.Float(string="Selling Price")
-    bedrooms = fields.Integer(string="Bedrooms")
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer(string="Living Area")
     facades = fields.Integer(string="Facades")
     garage = fields.Boolean(string="Garage")
@@ -25,4 +26,17 @@ class EstateProperty(models.Model):
             ('west', 'West')
         ],
         string="Garden Orientation"
+    )
+    active = fields.Boolean(default=False)
+    state = fields.Selection(
+        selection=[
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('cancelled', 'Cancelled'),
+        ],
+        required=True,
+        copy=False,
+        default='new'
     )
