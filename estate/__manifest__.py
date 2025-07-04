c8c44a504a95defacc975458913e6d06ca138b81
kdes-odoo <kdes@odoo.com>
2025-07-04 08:28:42 +0530

    [ADD] estate:  added access rights, field attributes and custom Views
    Applied the security access to base users group using CSV.
    Started working on UI:
    Create the estate_property_views.xml file in the Views folder
    Created an action for the model estate.property.
    Created the estate_menus.xml
    Created the three levels of menus for the estate.property
    
    Added the appropriate default attributes so that:
    the default number of bedrooms is 2
    the default availability date is in 3 months
    
    Added active and state field.
    
    Defined a custom list view for the estate.property
    Defined a custom form view for the estate.property
    Defined a custom search view for the estate.property

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -10,5 +10,10 @@
     """,
     'installable': True,
     'application': True,
-    
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
+    ]
+
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,14 +1,18 @@
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta
 
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real Estate Property"
 
-    name = fields.Char('Name', required=True)
+    name = fields.Char('Name', required=True, default='Unknown Property')
     description = fields.Text('Description')
     postcode = fields.Char('Postcode')
-    date_availability = fields.Date('Available From')
+    date_availability = fields.Date(
+        "Date Availability",
+        default=lambda self: fields.Date.to_string(fields.Date.context_today(self) + relativedelta(months=3))
+    )
     expected_price = fields.Float('Expected Price', required=True)
     selling_price = fields.Float('Selling Price', readonly=True, copy=False)
     bedrooms = fields.Integer('Bedrooms', default=2)
@@ -20,4 +24,10 @@ class EstateProperty(models.Model):
     garden_orientation = fields.Selection(
         [('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
         string='Garden Orientation'
+    )
+    active = fields.Boolean('Active', default=True)
+    state = fields.Selection(
+        [('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'),
+         ('sold', 'Sold'), ('canceled', 'Canceled')],
+        string='Status', default='new', required=True, copy=False
     )
