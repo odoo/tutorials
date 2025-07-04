3589778b75755ab10c5ea0e3d2904074dde354cd
ksoz <ksoz@odoo.com>
2025-07-04 10:33:20 +0530

    [ADD] estate_property: security files and search, list, filter added
    Added security  files for estate_property module
    Implemented search functionality for estate properties
    Developed listing and filtering features for better property
    management

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,13 +1,18 @@
 {
     'name': 'Estate',
     'version': '1.0',
+    'license': 'LGPL-3',
     'summary': 'Real estate management module',
     'license': 'LGPL-3',
     'description': 'Manage properties, owners, and sales in your real estate agency',
     'category': 'Real Estate',
     'author': 'ksoz',
     'depends': ['base'],
-    'data': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+    ],
     'installable': True,
     'application': True,
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,13 +1,18 @@
-from odoo import fields,models
+from odoo import fields, models
+
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real Estate Property"
 
     name = fields.Char(required=True)
-    description = fields.Text(string="Description")
+    description = fields.Text(string="Title")
     postcode = fields.Char(string="PostCode")
-    date_availability = fields.Date(string="Available From")
+    date_availability = fields.Date(
+        string="Available From",
+        copy=False,
+        default=lambda self: fields.Date.add(fields.Date.today(), months=3),
+    )
     expected_price = fields.Float(required=True, string="Expected Price")
     selling_price = fields.Float(string="Selling Price")
     bedrooms = fields.Integer(string="Bedroom")
@@ -17,11 +22,17 @@ class EstateProperty(models.Model):
     garden = fields.Boolean(string="Garden")
     garden_area = fields.Integer(string="Garden Area")
     garden_orientation = fields.Selection(
-        [
-            ('north', 'North'),
-            ('south', 'South'),
-            ('east', 'East'),
-            ('west', 'West')
-        ],
-        string="Garden Orientation"
+        [("north", "North"), ("south", "South"), ("east", "East"), ("west", "West")],
+        string="Garden Orientation",
     )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        [
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled"),
+        ],string="Status", copy=False, required=True, default="new"
+    )
+
