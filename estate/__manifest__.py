2616bf1c925cf704037cb23ecc05e929164743e5
niyp-odoo <niyp@odoo.com>
2025-07-04 10:06:27 +0530

    [IMP] estate: added unit tests
    This commit adds test cases to validate the key workflows in the estate module,
    including property creation, status transitions, and offer handling.These tests
    ensure better coverage and help prevent regressions during future development.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,5 +2,11 @@
     'name': 'Estate',
     'installable': True,
     'application': True,
-    'auto_install': False
+    'auto_install': False,
+    'data':[
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+    ],
+    'license': 'LGPL-3',
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,5 +1,6 @@
 from odoo import  fields,models
-
+from datetime import date
+from dateutil.relativedelta import relativedelta
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -8,17 +9,24 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_avaiblity = fields.Date()
+    date_avaiblity = fields.Date(copy = False, default = date.today()+ relativedelta(months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly = True, copy = False)
+    bedrooms = fields.Integer(default = 2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
-        string = 'Type',
+        string = 'Direction',
         selection=[('north','North'), ('south','South'), ('east','East'), ('west','West')],
-        help = "Orientation is used to locate garden's direction"
+        help = "This is used to locate garden's direction"
+    )
+    active = fields.Boolean(default = True)
+    state = fields.Selection(
+        selection=[('new','New'), ('offer received','Offer Received'), ('offer acceptedt','Offer Accepted'), ('sold','Sold'), ('cancelled', 'Cancelled')],
+        default = 'new',
+        required = True,
+        copy = False,
     )
