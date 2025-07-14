4e323f195e0aa09aac9f1075da6efde17a4112c5
arkp-odoo <arkp@odoo.com>
2025-07-14 15:59:19 +0530

    [IMP] estate:  Add Kanban view and PDF reports for property and salesperson
    Enhanced the estate module by introducing a Kanban view to improve the visual
    representation of properties and their statuses. Additionally, implemented PDF
    report generation for both individual properties and salespersons, allowing
    users to export key details in a printable format. These features improve user
    experience and support better reporting and communication.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,10 +5,12 @@
     'data': [
         'security/security.xml',
         'security/ir.model.access.csv',
+        "report/estate_property_templates.xml",
+        "report/estate_property_offer_reports.xml",
         'views/estate_property_views.xml',
         'views/estate_property_type_views.xml',
         'views/estate_property_tag_views.xml',
-        'views/inherited_model_view.xml',
+        'views/res_users_view.xml',
         'views/estate_menu.xml',
         'data/estate_property_type.xml',
     ],
