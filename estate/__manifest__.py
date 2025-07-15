f4747d46f56e4b1ac3ef45de956261d07295187e
zadh-odoo <zadh@odoo.com>
2025-07-15 18:31:17 +0530

    [IMP] estate, estate_account: implement and extend PDF property reports
    Added a PDF report for estate properties showing all property offers using QWeb templates and report actions as outlined in the PDF report tutorial. Also inherited and extended this report in estate_account to display invoice information for sold properties. This involved organizing new templates and report actions under each module and updating manifest files accordingly.
    
    This improves reporting capabilities for property offers and ensures that sales-related financial info is integrated into property documents. Enables business users to generate comprehensive property and accounting reports directly from the UI.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,7 +6,7 @@
     'author': "Dhruvrajsinh Zala (zadh)",
     'installable': True,
     'application': True,
-    'data': ['security/security.xml', 'security/ir.model.access.csv', 'views/estate_property_views.xml', 'views/estate_property_offers.xml', 'views/estate_property_type_views.xml', 'views/estate_property_tags.xml', 'views/res_users_views.xml', 'views/estate_menus.xml', 'data/estate.property.types.csv'],
+    'data': ['security/security.xml', 'security/ir.model.access.csv', 'report/estate_property_templates.xml', 'report/estate_user_properties_templates.xml', 'report/estate_user_properties_report.xml', 'report/estate_property_reports.xml', 'views/estate_property_views.xml', 'views/estate_property_offers.xml', 'views/estate_property_type_views.xml', 'views/estate_property_tags.xml', 'views/res_users_views.xml', 'views/estate_menus.xml', 'data/estate.property.types.csv'],
     'demo': ['demo/demo_data.xml'],
     'license': 'AGPL-3'
 }
--- estate_account/__manifest__.py
+++ estate_account/__manifest__.py
@@ -3,5 +3,6 @@
     "depends": ["estate", "account"],
     'category': "Tutorials",
     "installable": True,
-    "license": "AGPL-3"
+    "license": "AGPL-3",
+    'data': ['report/estate_property_report_inherit.xml']
 }
