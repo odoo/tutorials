925c9474104510f5bd44939fadacb0fa4cfad1c5
ksoz <ksoz@odoo.com>
2025-07-16 10:28:45 +0530

    [ADD] estate_property: security and groups for agent and manager
    Added security for agent and manager
    specifying about what they can view and what
    they can edit
    Added report and everything properly
    Update the security csv removed base group user and
    Added separate role for agent and manager

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -51,6 +51,7 @@ class EstateProperty(models.Model):
     offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
     total_area = fields.Integer(compute="_compute_total_area")
     best_offer = fields.Float(compute="_compute_best_offer")
+    company_id = fields.Many2one('res.company', string="Company", default=lambda self: self.env.company)
 
     _sql_constraints = [
         (
