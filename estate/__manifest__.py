ef111107377cfb998b1bab200c38ee3ca85109cc
Eduard Adrian Dinu <eduardadrian.dinu@gmail.com>
2025-07-17 10:22:07 +0200

    Chapter 5 Completed - created views for the form, for the displayed list, added more fields in the model with default values and attributes

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,7 +5,9 @@
     'depends': ['base'],
     'description': "",
     'data': [
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml', 
+        'views/estate_menus.xml'
     ],
     'application': True
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,5 +1,6 @@
 
 from odoo import fields, models
+from datetime import timedelta
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -8,16 +9,18 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(string="Available From", copy=False , default= lambda self: fields.Date.today() + timedelta(90))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
-    living_are = fields.Integer()
+    selling_price = fields.Float(string="Selling Price", readonly=True) 
+    bedrooms = fields.Integer(string="Bedrooms", default=2)
+    living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(selection=[("north", "North"), ("south", "South"), ("east", "East"), ("west", "West")])
+    active = fields.Boolean(string="Active", default=True)
+    state = fields.Selection(selection=[("new", "New"), ("offer received", "Offer Received"), ("offer accepted", "Offer Accepted"), ("sold", "Sold"), ("cancelled", "Cancelled")], required=True, default="new", copy=False, string="Status" )
 
 
 
