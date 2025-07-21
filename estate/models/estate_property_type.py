d2fb17f9503d85518374672c79b43e80d377ccef
Eduard Adrian Dinu <eduardadrian.dinu@gmail.com>
2025-07-21 14:17:56 +0200

    added sql contraint to check uniqueness property type name

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -3,6 +3,7 @@ from odoo import fields, models
 class EstatePropertyType(models.Model): 
     _name = "estate.property.type"
     _description = "Type of a Property"
+    _sql_constraints = [("name_uniq", "UNIQUE(name)", "Property type name must be unique")]
     
     name = fields.Char(required=True)
     property_ids = fields.One2many("estate.property", "property_type_id", string="Properties")
