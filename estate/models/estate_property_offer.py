55cfeef4be3663537d4228c6ef010e94b62aab8e
Eduard Adrian Dinu <eduardadrian.dinu@gmail.com>
2025-07-21 09:04:42 +0200

    added computed field with a compute and inverse function to calculate the deadline or validity date

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import fields, models, api
+from datetime import datetime, timedelta
 
 class EstatePropertyOffer(models.Model): 
     _name = "estate.property.offer"
@@ -8,4 +9,29 @@ class EstatePropertyOffer(models.Model):
     property_id = fields.Many2one("estate.property", string="Property", required=True, help="The property this offer is for")
     partner_id = fields.Many2one("res.partner", string="Partner", required=True, help="The partner making the offer")
     status = fields.Selection([("accepted", "Accepted"), ("refused", "Refused")], string="Status", copy=False)
+    validity = fields.Integer(string="Validity(days)", default=7, help="Number of days this offer is valid")
+    date_deadline = fields.Date(string="Deadline", compute="_compute_date_deadline", inverse="_inverse_date_deadline", help="Date when offer expires")
+    
+    @api.depends('create_date', 'validity')
+    def _compute_date_deadline(self):
+        for record in self:
+            if record.create_date:
+                # if the record existis and it has a create_date
+                record.date_deadline = record.create_date.date() + timedelta(days=record.validity)
+            else:
+                # using today's date if record isn't created yet
+                record.date_deadline = fields.Date.today() + timedelta(days=record.validity)
+    
+    
+    def _inverse_date_deadline(self):
+        for record in self:
+            if record.date_deadline:
+                if record.create_date:
+                    difference = record.date_deadline - record.create_date.date()
+                    record.validity = difference.days
+                else:
+                    #  using today's date again
+                    difference = record.date_deadline - fields.Date.today()
+                    record.validity = difference.days
+                       
     
