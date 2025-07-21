9c0f7d0229ca5b156e3116e1663ef9f8f76c507f
Eduard Adrian Dinu <eduardadrian.dinu@gmail.com>
2025-07-21 14:17:29 +0200

    added sql constraint the check uniqueness of tag name

--- estate/models/estate_property_tag.py
+++ estate/models/estate_property_tag.py
@@ -3,5 +3,6 @@ from odoo import fields, models
 class EstatePropertyTag(models.Model):
     _name = "estate.property.tag"
     _description = "Tags for Properties"
+    _sql_constraints = [("name_uniq", "UNIQUE(name)", "Tag name must be unique")]
     
     name = fields.Char(required=True)
