90651ff73995ab5e50bc2f35b3024b9edbcc4580
Eduard Adrian Dinu <eduardadrian.dinu@gmail.com>
2025-07-22 15:08:55 +0200

    added 2 more views

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,7 +7,9 @@
     'data': [
         'security/ir.model.access.csv',
         'views/estate_property_views.xml', 
-        'views/estate_menus.xml'
+        'views/estate_menus.xml',
+        'views/estate_property_type_views.xml',
+        'views/estate_property_offer_views.xml'
     ],
     'application': True
 }
