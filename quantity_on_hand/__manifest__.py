d3164e9c06376cb5b02cdec8c75d51d54a3fcb68
arkp-odoo <arkp@odoo.com>
2025-07-23 17:58:04 +0530

    [ADD] inventory: Improve stock info and UI in product form
    Before:
    Users could easily get confused or make mistakes when managing stock,
    especially in multi-company environments. Important actions like
    updating quantity on hand were hard to find, and the interface didn’t clearly
    guide safe workflows.
    
    After:
    The interface now better supports new users by reducing confusion and
    make stock workflows safer. Risky actions are hidden if not appropriate, visual
    clarity helps users avoid errors and understand inventory status more easily.
    
    task-4965098

---
+++ quantity_on_hand/__manifest__.py
@@ -0,0 +1,13 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Quantity on hand',
+    'version': '1.0',
+    'depends': ['stock'],
+    'installable': True,
+    'author': 'arkp',
+    'data': [
+        'views/product_template_views.xml',
+    ],
+    'license': 'LGPL-3',
+}
