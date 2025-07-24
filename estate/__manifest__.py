291e43d840dd057ae139d30cfe5e7141adb2b17f
Aaryan (aarp) <aarp@odoo.com>
2025-07-24 11:24:34 +0530

    [ADD] estate: create a real estate app with basic models using Object relational mapping.
    Key changes include:
    
    - Defined core model (estate.property)
    - Created `__init__.py` and `__manifest__.py`
    - Added name and required dependencies of our module in `__manifest__.py`
    - Added field declarations for estate.property
    
    This commit lays the foundation for a functional real estate management system in Odoo.

---
+++ estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': "Real Estate",
+    'description': """Real Estate Application.""",
+    'summary': """Real Estate Application for beginner.""",
+    'depends': ['base'],
+    'author': "Aaryan Parpyani (aarp)",
+    'category': "Tutorials/RealEstate",
+    'version': '1.0',
+    'application': True,
+    'installable': True,
+}
