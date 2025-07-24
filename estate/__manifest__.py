beaf7d151ffd0a67fc6e1d909e9e4835df712403
Jay (jcha) <jay@odoo.com>
2025-07-24 10:38:54 +0530

    Getting Started

---
+++ estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Author Name",
+    'category': 'Category',
+    'description': """
+    Description text
+    """
+}
