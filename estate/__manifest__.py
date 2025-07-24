bac1d855de2de2455cc4b5066742a9a9d277b36d
Aaryan (aarp) <aarp@odoo.com>
2025-07-24 18:49:31 +0530

    [ADD] training: chapter 1-6

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -8,4 +8,9 @@
     'version': '1.0',
     'application': True,
     'installable': True,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_menus_views.xml',
+        'views/estate_property_views.xml',
+    ]
 }
