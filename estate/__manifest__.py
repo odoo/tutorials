a40d0ca813a1155cdaac3e9f7ee55e6d8057ac59
dhbr-odoo <dhbr@odoo.com>
2025-07-24 12:28:26 +0530

    [ADD] estate: initial module structure with models, relationships, and basic views
    Implemented a new custom module as per Odoo’s server framework tutorial. Defined a new model `estate` with different fields in the Python file. Created XML views including form and tree views for the model, and added menu items under a custom top-level menu and submenu. Configured action bindings for the views. Set up security by adding `ir.model.access.csv` with read/write permissions for the model. Installed the module via the UI and verified that the menus, views, and permissions are working correctly as expected.

---
+++ estate/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'author': 'Dhananjay Brahmane - dhbr',
+    'category': 'Services',
+    'summary': 'Invest in properties',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
