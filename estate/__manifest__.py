3e070f0f45f70762634be893ff73e1630582e1b5
jekia2000 <alekjelacamg000@gmail.com>
2025-07-25 17:01:28 +0200

    [IMP] Improved style mistakes further and PR comments

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,19 +1,16 @@
 {
     'name': "estate",
-   'depends': ['base'],
-'application': True,
-'license': 'LGPL-3',
-'data': ['security/ir.model.access.csv',
-'views/estate_property_views.xml',
-'views/estate_property_tag_views.xml',
-'views/estate_property_offer_views.xml',
-'views/estate_property_type_views.xml',
-'views/estate_property_menus.xml',
-'views/inherited_model_view.xml'
-
-
-
-]
+    'depends': ['base'],
+    'application': True,
+    'license': 'LGPL-3',
+    'data': ['security/ir.model.access.csv',
+            'views/estate_property_views.xml',
+            'views/estate_property_tag_views.xml',
+            'views/estate_property_offer_views.xml',
+            'views/estate_property_type_views.xml',
+            'views/estate_property_menus.xml',
+            'views/inherited_model_view.xml'
+            ]
 
 
 }
