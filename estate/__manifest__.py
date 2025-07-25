13be7a52c67d54edfcb5741cfe7dee26fcbba334
jekia2000 <alekjelacamg000@gmail.com>
2025-07-25 16:24:18 +0200

    [IMP] Fixed remaining style mistakes

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,9 +1,9 @@
 {
     'name': "estate",
-   'depends':['base'],
+   'depends': ['base'],
 'application': True,
-'license':'LGPL-3',
-'data':['security/ir.model.access.csv',
+'license': 'LGPL-3',
+'data': ['security/ir.model.access.csv',
 'views/estate_property_views.xml',
 'views/estate_property_tag_views.xml',
 'views/estate_property_offer_views.xml',
