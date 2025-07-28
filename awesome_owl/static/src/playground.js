6ea6524dce5574e0975b4df24397632bac5cb481
Piyush Soni (piso) <piso@odoo.com>
2025-07-28 17:14:42 +0530

    [ADD] awesome_owl: add Counter and Card components in Playground
    - Created `Counter` subcomponent in `Playground`, with support for `onChange` prop to update total sum
    - Added reusable `Card` component with `title` and `content` props, including props validation
    - Used `markup()` function with `t-out` in `Card` to safely render HTML content from props

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,7 +1,22 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, markup, useState } from "@odoo/owl";
+import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
+
 
 export class Playground extends Component {
-    static template = "awesome_owl.playground";
+    static template = "awesome_owl.Playground";
+
+    static components = { Counter, Card }
+    html = '<button class="btn bg-warning">Test</button>'
+    markup_html = markup(this.html)
+
+    setup() {
+        this.state = useState({ sum: 0 });
+    }
+
+    incrementSum() {
+        this.state.sum++;
+    }
 }
