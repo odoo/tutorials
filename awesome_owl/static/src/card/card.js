a8e1dbb6970f5582e4201d07e9d2c79c4b32b893
Mahmoudk3m <mahmoudk3m.mk@gmail.com>
2025-07-28 16:08:02 +0200

    [ADD] awesome_owl: Owl components

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -1,4 +1,4 @@
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 
 export class Card extends Component {
   static template = "awesome_owl.card";
@@ -7,11 +7,19 @@ export class Card extends Component {
     title: "Default Title",
   };
 
+  setup() {
+    this.state = useState({ isOpen: true });
+  }
+
+  toggleContent() {
+    this.state.isOpen = !this.state.isOpen;
+  }
+
   static props = {
     title: {
       type: String,
       optional: true,
     },
-    content: { type: String },
+    "*": true,
   };
 }
--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,19 +1,20 @@
-/** @odoo-module **/
-
 import { Component, markup, useState } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
+import { TodoList } from "./todo/todo-list";
 
 export class Playground extends Component {
   static template = "awesome_owl.playground";
-  static components = { Counter, Card };
+  static components = { Counter, Card, TodoList };
 
   setup() {
     this.regularHtml = "<div class='text-danger'>This will be escaped</div>";
     this.safeHtml = markup(
       "<div class='text-primary'>This will render as HTML</div>"
     );
+
     this.markup = markup;
+
     this.state = useState({ sum: 0 });
   }
 
