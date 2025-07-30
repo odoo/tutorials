e3e1da0eb89cb9179ac9b35f5b215bbffd0283d4
pmad-odoo <pmad@odoo.com>
2025-07-30 12:20:18 +0530

    [ADD] awesome_owl: create modular counter and card components with props/state
    - First, added a local counter with reactive state and an increment method.
    
    - Extracted counter logic into a reusable Counter component with its own template and folder.
    
    - Introduced a Card component accepting title and content props to display structured content.
    
    - Enabled raw HTML rendering using t-out and Owl’s markup() utility.
    
    - Added props validation to Card, enabling dev-time checks for incorrect usage.
    
    - Implemented parent-child communication via a callback (onChange) prop in the Counter, allowing Playground to track and display the sum of counter values.

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,7 +1,22 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, markup, useState } from "@odoo/owl";
+import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
+
+    content = markup("<div class='text-primary'>Full Stack Developer</div>");
+
+    static components = { Counter, Card }
+
+    setup() {
+        this.state = useState({ sum: 2 });
+        this.incrementSum = this.incrementSum.bind(this);
+    }
+
+    incrementSum() {
+        this.state.sum++;
+    }
 }
