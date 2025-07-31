19cab6688dd56b1c6c050d1b312f5b1bb4b4e0ab
pmad-odoo <pmad@odoo.com>
2025-07-31 14:01:20 +0530

    [ADD] awesome_owl: implement TodoList and enhanced Card UI
    Introduced a TodoList feature to demonstrate user interaction, state handling, and dynamic updates.
    
    - Added a list of tasks that can be added, marked as complete, or removed.
    
    - Applied visual feedback for completed tasks.
    
    - Included input focus behavior and streamlined the task-adding process.
    
    - Improved the user experience with confirmation and responsiveness.
    
    Enhanced the Card component for better flexibility and interactivity:
    
    - Replaced fixed content with a more flexible structure.
    
    - Added the ability to show or hide content using a toggle.
    
    - Improved consistency with interface guidelines and added validations.

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -1,10 +1,19 @@
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 
 export class Card extends Component {
     static template = "awesome_owl.card";
 
     static props = {
         title: { type: String },
-        content: { type: String },
+        slots: { type: Object }
     }
+
+    setup() {
+        this.state = useState({ isVisibleContent: true })
+    }
+
+    onToggleContent() {
+        this.state.isVisibleContent = !this.state.isVisibleContent;
+    }
+
 }
--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -3,13 +3,14 @@
 import { Component, markup, useState } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
+import { TodoList } from "./todo_list/todo_list";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
 
     content = markup("<div class='text-primary'>Full Stack Developer</div>");
 
-    static components = { Counter, Card }
+    static components = { Counter, Card, TodoList };
 
     setup() {
         this.state = useState({ sum: 2 });
