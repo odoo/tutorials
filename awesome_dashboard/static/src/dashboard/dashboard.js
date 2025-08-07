f03796e08fb587d4101ff88cb5fc45f3e5527b3b
pmad-odoo <pmad@odoo.com>
2025-08-07 15:58:16 +0530

    [ADD] awesome_dashboard: add dashboard item settings with local storage
    Added a settings button in the control panel that opens a dialog allowing users to customize visible dashboard items.
    
    The dialog lists all items with checkboxes. On applying, checked item IDs are saved to local storage, and the dashboard dynamically filters out those items.
    
    Improves user control and personalization of the dashboard layout.

--- awesome_dashboard/static/src/dashboard/dashboard.js
+++ awesome_dashboard/static/src/dashboard/dashboard.js
@@ -4,17 +4,22 @@ import { Component, onMounted, useState, onWillUnmount } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
 import { useService } from "@web/core/utils/hooks";
+import { browser } from "@web/core/browser/browser";
 import { DashboardItem } from "./dashboard_item/dashboard_item";
+import { settingDialog } from "./setting_dialog/setting_dialog";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
     static components = { Layout, DashboardItem };
 
     setup() {
+        const initial_card_preference = JSON.parse(browser.localStorage.getItem("user_card_preference"));
         this.action = useService("action");
         this.statisticsService = useService("statisticsService");
+        this.dialog = useService("dialog");
         this.state = useState({
-            response: this.statisticsService.response
+            response: this.statisticsService.response,
+            card_preference: initial_card_preference && initial_card_preference.length > 0 ? initial_card_preference : [],
         });
 
         this.intervalId = null;
@@ -44,6 +49,18 @@ class AwesomeDashboard extends Component {
                 [false, "form"]
             ],
             target: "current",
+        });
+    }
+
+    savePreference(selectedPreference, close) {
+        this.state.card_preference.splice(0, this.state.card_preference.length, ...selectedPreference);
+        browser.localStorage.setItem("user_card_preference", JSON.stringify(this.state.card_preference));
+        close();
+    }
+
+    openSettings() {
+        this.dialog.add(settingDialog, {
+            apply: this.savePreference.bind(this)
         });
     }
 }
