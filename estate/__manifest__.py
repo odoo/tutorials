ae1096610cf38c5d54651a188191277f0d271dec
satp-odoo <satp@odoo.com>
2025-08-12 16:30:03 +0530

    [ADD] estate: added security access configuration
    . Allow users to read, create, update, and delete property records.
    . Eliminate warnings related to missing access rules for the property model.
    . Implement default security settings to grant standard users proper data access.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,5 +1,9 @@
 {
     'name': 'Real Estate',
     'application': True,
-    'installable': True
+    'installable': True,
+    'category': 'Real Estate/Brokerage',
+    'data': [
+        'security/ir.model.access.csv'
+    ]
 }
