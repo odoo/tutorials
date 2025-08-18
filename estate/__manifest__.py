25213feb8a768b2d4eabcbb1236c05002da62329
satp-odoo <satp@odoo.com>
2025-08-18 19:10:54 +0530

    [ADD] estate: added menus options and new fields
    . Add root, first-level, and action menus for estate properties.
    . Create list and form views for property records.
    . Make selling price read-only and exclude it and availability date from duplicates.
    . Set default bedrooms to 2 and availability date to 3 months ahead.
    . Add active field (default True) and state field with key property statuses.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,6 +4,8 @@
     'installable': True,
     'category': 'Real Estate/Brokerage',
     'data': [
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
     ]
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import models, fields
+from datetime import timedelta
 
 class EstateModel(models.Model):
     _name = "estate.property"
@@ -7,16 +8,30 @@ class EstateModel(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(
+        copy=False,
+        default= fields.Date.today() + timedelta(days=90)
+        )
+    active= fields.Boolean(default=True)
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
+    state= fields.Selection(
+        string='state',
+        default="New",
+        required=True,
+        copy=False,
+        selection=[('New', 'New'),('Offer Received', 'Offer Received'),('Offer Accepted', 'Offer Accepted'),('Sold', 'Sold'), ('Cancelled', 'Cancelled'),
+        ]
+    )
     garden_orientation = fields.Selection(
-        string='Type',
-        selection=[('north', 'North'),('south', 'South'),('east', 'East'),('west', 'West')],
+        string='Garden Orientation',
+        selection=[('north', 'North'),('south', 'South'),('east', 'East'),('west', 'West'),
+        ],
+        help="Select the direction the garden faces"
     )
