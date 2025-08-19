343fdf9c3e66a1df24c98bde1b9c9f4cdd0eb6a3
tshe-odoo <tshe@odoo.com>
2025-08-19 19:11:56 +0530

    [ADD] estate: property relations:-type, tags, offers, buyer, salesperson

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,6 +7,9 @@
     'data': [
         'security/estate_security.xml',
         'security/ir.model.access.csv',
+        'views/estate_property_offers_views.xml',
+        'views/estate_property_tag_views.xml',
+        'views/estate_property_type_views.xml',
         'views/estate_property_views.xml',
         'views/estate_menus.xml',
     ],
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,21 +1,17 @@
 from odoo import fields, models
-from datetime import timedelta
 
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Estate Property"
 
-    def _default_availability_date(self):
-        today_str = fields.Date.context_today(self) 
-        today_date = fields.Date.to_date(today_str)   
-        future_date = today_date + timedelta(days=30)
-        return future_date.strftime('%Y-%m-%d')   
+  
 
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availaility = fields.Date(copy=False,default=_default_availability_date)
+    date_availaility = fields.Date(default=(fields.Date.add(fields.Date.today(), days=90)), copy=False)
+    
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly = True,copy=False)
     bedrooms = fields.Integer(default=2)
@@ -34,6 +30,11 @@ class EstateProperty(models.Model):
     ('offer_accepted', 'Offer Accepted'),
     ('sold', 'Sold'),
     ('cancelled', 'Cancelled'),],required=True, copy=False, default='new')
+    property_type = fields.Many2one("estate.property.type", string="Property Type")
+    salesman = fields.Many2one("res.users", default = lambda self: self.env.user)
+    buyer = fields.Many2one("res.partner", copy=False)
+    tag_ids = fields.Many2many("estate.property.tag", string="Tags")
+    offers_ids = fields.One2many("estate.property.offers", "property_id")
 
 
 
