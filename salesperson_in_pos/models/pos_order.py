ca13a6816f9dbbbd067926f726dcc41603f8f37b
prbo-odoo <prbo@odoo.com>
2025-08-22 10:27:49 +0530

    [ADD] salesperson_in_pos: adding salesperson button in POS
    - In this PR, the pos.order model was inherited to enable the use of the custom
     app point_of_sale module within the form and list views of POS orders.
    - Also a salesperson button was added to the POS dashboard to allow quick access
     and better user interaction with salesperson-related functionalities.

---
+++ salesperson_in_pos/models/pos_order.py
@@ -0,0 +1,7 @@
+from odoo import models, fields
+
+
+class PosOrder(models.Model):
+    _inherit = "pos.order"
+
+    salesperson_id = fields.Many2one('hr.employee', string='Salesperson', help='Salesperson who created the order')
