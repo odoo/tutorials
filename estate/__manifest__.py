d0a44aa154dae276a79f677bb99f0242ea95adfb
kraj-odoo <kraj@odoo.com>
2025-08-26 10:04:48 +0530

    [ADD] Added Real Estate Module and Access Right in the Module

---
+++ estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'ESTATE',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': True,
+}
