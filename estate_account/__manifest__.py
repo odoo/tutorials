e447e355add54d55e5a823612a9c3fb8fe68589d
kraj-odoo <kraj@odoo.com>
2025-09-04 14:32:12 +0530

    [ADD] estate: Added the estate_account module and kanban view in estate module
    -> Added estate_account module for the invocing of the sold property.
    -> estate_account inherits the estate_property model.
    -> Added the kanban view on the properties page.
    -> shows the best price field in kanban view if the state is recieved
    -> shows the selling price only when the property is sold
    -> drag and drop is set to false

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "Estate Account",
+    'description': 'Real Estate Property Module which will used to help users regarding real estate properties',
+    'version': '1.0',
+    'category': 'Category',
+    'depends': [
+        'base',
+        'estate',
+        'account'
+    ],
+    'data': [],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
