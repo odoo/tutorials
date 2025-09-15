0f0987fb7c94a9f57afd37654eedc656eb4eac59
jepar-odoo <jepar@odoo.com>
2025-09-15 14:54:28 +0200

    Adding required fields

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,12 +5,12 @@ class EstateProperty(models.Model):
     _description = "Real Estate Properties to sell"
     _order = "sequence"
 
-    name = fields.Char()
+    name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
     date_availability = fields.Date()
     date_availability = fields.Date()
-    expected_price = fields.Float()
+    expected_price = fields.Float(required=True)
     selling_price = fields.Float()
     bedrooms = fields.Integer()
     living_area = fields.Integer()
