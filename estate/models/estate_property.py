430c09375656d8209b5fb1ab19670ec54f2db54c
Radu Macocian <rmacocian@yahoo.com>
2025-09-15 15:02:16 +0200

    [IMP] estate: add basic fields on estate model

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,5 +1,31 @@
-from odoo import models
+from odoo import models, fields
 
 
 class EstateProperty(models.Model):
     _name = "estate.property"
+    _description = "Estate Property"
+
+    name = fields.Char("Estate name", required=True, translate=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date("Available From")
+    expected_price = fields.Float()
+    selling_price = fields.Float("Actual Price")
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        selection=[
+            ("north", "North"),
+            ("south", "South"),
+            ("east", "East"),
+            ("west", "West"),
+        ],
+        help="Orientation of the estate property",
+    )
+    property_type = fields.Selection(
+        selection=[("apartment", "Apartment"), ("house", "House")]
+    )
