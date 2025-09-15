f9f924c819a8b45d5d9819699e16e305ea62618f
Tristan Cazier <tristan.gabriel.r.cazier@gmail.com>
2025-09-15 15:32:54 +0200

    [IMP] estate: add chapter 2

---
+++ estate/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Estate',
+    'depends': [
+        'base',
+    ],
+    'installable': True,
+    'application': True,
+}
