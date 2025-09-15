1ca5e462b7ce7ad57a15faaf320bdc5aa1aa7843
nsirjacobs <noam.sirjacobs@gmail.com>
2025-09-15 17:28:15 +0200

    Added views and fields to Estate

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import fields, models
+from datetime import date, timedelta
 
 class estate_property(models.Model):
     _name = "estate.property"
@@ -7,13 +8,15 @@ class estate_property(models.Model):
     name = fields.Char(required = True)
     description =  fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False,string="Available From", default=lambda self: date.today() + timedelta(days=90))
     expected_price = fields.Float(required = True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly = True,copy=False)
+    bedrooms = fields.Integer(default = 2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(selection = [('North', 'North'), ('South', 'South'),('East', 'East'),('West', 'West')])
+    active = fields.Boolean(default = True)
+    state = fields.Selection(required = True, default = 'New', copy = False, selection = [('New','New'),('Offer Received','Offer Received'),('Offer Accepted','Offer Accepted'),('Sold','Sold'),('Cancelled','Cancelled')])
