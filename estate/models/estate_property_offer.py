8170681a5a64cdc31d3abc5535675d5e8738623c
Avery Pinko <avpin@odoo.com>
2025-09-15 16:18:37 +0200

    [FIX] estate: fix offer price field

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -4,7 +4,7 @@ class EstatePropertyOffer(models.Model):
     _name = 'estate.property.offer'
     _description = 'Property Offer'
 
-    price = fields.Monetary()
+    price = fields.Float("Price")
     status = fields.Selection("Status", selection=[('accepted', 'Accepted'), ('refused', 'Refused')], copy=False)
     partner_id = fields.Many2one('res.partner', required=True)
     property_id = fields.Many2one('estate.property', required=True)
