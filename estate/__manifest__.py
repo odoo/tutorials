20ec96902946a4b92b86406daa9ff2c5f6b0b67a
jepar-odoo <jepar@odoo.com>
2025-09-15 11:06:49 +0200

    First commit - 02_newapp.html

---
+++ estate/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Estate',
+    'version': '0.0.1',
+    'category': 'Sales/CRM',
+    'summary': 'Real Estate Module - Training',
+    'website': 'https://www.odoo.com/jobs',
+    'installable': True,
+    'application': False,
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+}
