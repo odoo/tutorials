54e87a3398779127bf7f0c9baf840db9f190f8d8
jepar-odoo <jepar@odoo.com>
2025-09-15 17:18:53 +0200

    Adding active and state fields - chapter 5 - Part 3

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -6,12 +6,24 @@ class EstateProperty(models.Model):
     _description = "Real Estate Properties to sell"
 
     name = fields.Char(required=True)
+    active = fields.Boolean('Active', default=True)
+    state = fields.Selection(
+        selection =[
+            ('new', 'New'),
+            ('offer_received','Offer Received'),
+            ('offer_accepted','Offer Accepted'),
+            ('sold','Sold'),
+            ('canceled', 'Cancelled')],
+        required = True,
+        default = 'new',
+        copy = False,
+    )
     description = fields.Text()
     postcode = fields.Char()
     date_availability = fields.Date(default=fields.Date.today() + relativedelta.relativedelta(months=3))
     expected_price = fields.Float(required=True)
     selling_price = fields.Float()
-    bedrooms = fields.Integer(default=2)
+    bedrooms = fields.Integer('Bedrooms',default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
