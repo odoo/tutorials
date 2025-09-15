adca8fdd286dcf1b50afa63c23e0979168bf5bec
Avery Pinko <avpin@odoo.com>
2025-09-15 15:06:57 +0200

    [ADD] estate: basic list/form/search views

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -7,10 +7,30 @@ class EstateProperty(models.Model):
     _description = 'Property'
     _order = 'name'
 
+
     name = fields.Char("Property", required=True)
     description = fields.Text("Description")
+    
+    active = fields.Boolean(string="Active", default=True, required=True)
+    state = fields.Selection(
+        string="State",
+        selection=[
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled"),
+        ],
+        default="new",
+        required=True,
+        copy=False,
+    )
 
-    date_availability = fields.Date("Date available", default=(lambda _: fields.Date.today() + timedelta(days=90)))
+    date_availability = fields.Date(
+        string="Available From",
+        default=(lambda _: fields.Date.today() + timedelta(days=90)),
+        copy=False,
+    )
 
     # Price fields:
     currency_id = fields.Many2one(
@@ -18,18 +38,18 @@ class EstateProperty(models.Model):
         string="Currency",
     )
     expected_price = fields.Monetary(
-        string="Expected price",
+        string="Expected Price",
         required=True,
     )
     selling_price = fields.Monetary(
-        string="Selling price",
+        string="Selling Price",
     )
 
     # Address fields:
     street = fields.Char("Street")
     street2 = fields.Char("Street (line 2)")
     city = fields.Char("City")
-    postcode = fields.Char("Postal code")
+    postcode = fields.Char("Postcode")
     state_id = fields.Many2one(
         comodel_name="res.country.state",
         string='State',
@@ -42,11 +62,11 @@ class EstateProperty(models.Model):
 
     # Amenity fields:
     bedrooms = fields.Integer(string="Bedrooms", default=2, help="Number of bedrooms")
-    living_area = fields.Integer(string="Living area", help="Habitable area of the property (m^2)")
+    living_area = fields.Integer(string="Living Area (sqm)", help="Habitable area of the property (m^2)")
     facades = fields.Integer(string="Facades", help="Number of facades")
     garage = fields.Boolean(string="Garage")
     garden = fields.Boolean(string="Garden")
-    garden_area = fields.Integer(string="Garden area", help="Size of the garden (m^2)")
+    garden_area = fields.Integer(string="Garden Area (sqm)", help="Size of the garden (m^2)")
     garden_orientation = fields.Selection(
         string='Garden orientation',
         selection=[
