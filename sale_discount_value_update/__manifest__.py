5f467e1e23fe1635efa7c829d54e86260c58e6a2
Sanket Tank(stan) <stan@odoo.com>
2025-09-16 10:39:48 +0530

    [ADD] sale_discount_value_update: Added module for sale_management
    Currently, in the sale_management module, when a global discount line is added
    and changes are made to order lines—such as altering the price, quantity
    Or product the discount line does not update accordingly.
    
    This change introduces a new module, sale_discount_value_update
    which fixes this issue.

---
+++ sale_discount_value_update/__manifest__.py
@@ -0,0 +1,17 @@
+
+{
+    'name': "discount_value_update",
+    'version': '1.0',
+    'depends': [
+        'base',
+        'sale',
+        'sale_management',
+    ],
+    'author': "Sanket Tank",
+    'category': 'Sales/Sales',
+    'description': """
+    This module will fix issue of discount line
+    """,
+    'application': True,
+    'license': 'LGPL-3',
+}
