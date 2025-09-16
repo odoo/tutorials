ba988827ba624209523fec0fe91caf021ce4a8ff
Léopold Cantraine <leopold.cantraine@gmail.com>
2025-09-16 13:40:20 +0200

    [ADD] Chapter 7

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,5 +1,6 @@
-from odoo import models, fields
+from odoo import models, fields  # type: ignore
 from datetime import timedelta
+
 
 class RealEstateProperty(models.Model):
     _name = "estate.property"
@@ -9,22 +10,36 @@ class RealEstateProperty(models.Model):
     description = fields.Text()
     postcode = fields.Char()
     date_availability = fields.Date(
-        copy=False, 
-        default=(lambda x: fields.Date.today() + timedelta(days=90)))
+        copy=False, default=(lambda x: fields.Date.today() + timedelta(days=90))
+    )
     expected_price = fields.Float(required=True, readonly=False)
     selling_price = fields.Float(copy=False)
     bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer('Living Area (sqm)')
+    living_area = fields.Integer("Living Area (sqm)")
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Integer('Garden Area (sqm)')
+    garden_area = fields.Integer("Garden Area (sqm)")
     active = fields.Boolean(default=True)
-    state = fields.Selection([
-        ('new', 'New'),
-        ('offer_received', 'Offer Received'),
-        ('offer_accepted', 'Offer Accepted'),
-        ('sold', 'Sold'),
-        ('canceled', 'Canceled')
-    ], default='new', required=True, copy=False)
-    garden_orientation = fields.Selection([('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
+    state = fields.Selection(
+        [
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("canceled", "Canceled"),
+        ],
+        default="new",
+        required=True,
+        copy=False,
+    )
+    garden_orientation = fields.Selection(
+        [("north", "North"), ("south", "South"), ("east", "East"), ("west", "West")]
+    )
+    property_type_id = fields.Many2one("estate.property.type", string="Property Type")
+    buyer = fields.Many2one("res.partner", string="Buyer", copy=False)
+    salesperson = fields.Many2one(
+        "res.users", string="Salesperson", default=lambda self: self.env.user
+    )
+    tag_ids = fields.Many2many("estate.property.tag", string="Tags")
+    offers_ids = fields.One2many("estate.property.offer", "property_id")
