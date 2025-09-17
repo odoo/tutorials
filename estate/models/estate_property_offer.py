85f5694db4c6cffb08bac8185e81b35ef086cc4d
Avery Pinko <avpin@odoo.com>
2025-09-17 11:11:33 +0200

    [ADD] estate: view offer list for property type

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -15,6 +15,7 @@ class EstatePropertyOffer(models.Model):
     status = fields.Selection(string="Status", selection=[('accepted', "Accepted"), ('refused', "Refused")], copy=False)
     partner_id = fields.Many2one('res.partner', string="Partner", required=True)
     property_id = fields.Many2one('estate.property', string="Property", required=True)
+    property_type_id = fields.Many2one(related='property_id.property_type_id', string="Property Type", stored=True)
     validity = fields.Integer(string="Validity (days)", default=7)
     date_deadline = fields.Date(string="Deadline", compute='_compute_date_deadline', inverse='_inverse_date_deadline')
 
--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 class EstatePropertyType(models.Model):
     _name = 'estate.property.type'
@@ -17,5 +17,16 @@ class EstatePropertyType(models.Model):
         inverse_name='property_type_id',
         string="Properties",
     )
+    offer_ids = fields.One2many(
+        comodel_name='estate.property.offer',
+        inverse_name='property_type_id',
+        string="Offers",
+    )
+    offer_count = fields.Integer(string="Offer Count", compute='_compute_offer_count')
 
     sequence =fields.Integer("Sequence", default=1, help="Used to order property types.")
+
+    @api.depends('offer_ids')
+    def _compute_offer_count(self):
+        for property_type in self:
+            property_type.offer_count = len(property_type.offer_ids)
