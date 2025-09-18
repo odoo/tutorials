c3e0054c1be92c0354132a13004a9545533376f4
Madhav Pancholi <pamd@odoo.com>
2025-09-18 18:04:51 +0530

    [ADD] awesome_owl: awesome_owl module with counter, card, and todo components
    - Implemented Playground component containing:
      - Counter component with increment functionality
      - Card component with subcomponent using slots for flexible content
    - Built TodoList component:
      - Toggle todo completion
      - Remove todo items
      - Integrated TodoItem as a subcomponent
    - Exercises follow the Owl documentation for hands-on learning and practice

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,7 +1,20 @@
-/** @odoo-module **/
-
-import { Component } from "@odoo/owl";
+import { Component, useState, markup } from "@odoo/owl";
+import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
+import { TodoList } from "./todo/todo_list";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
+    static components = { Counter, Card, TodoList };
+
+    setup() {
+        this.state = useState( { value:0 } );
+        this.content1 = "<b>hello content1</b>";
+        this.content2 = markup("<b>hello content2</b>");
+    }
+
+    incrementSum() {
+        this.state.value++;
+    }
+
 }
