3be9674606770e22e82e1d34005f8e8bfc4cbdd4
tshe-odoo <tshe@odoo.com>
2025-09-18 15:46:04 +0530

    [ADD] purchase_global_discount: global discount button for bulk price update
    Added a button to apply global discounts (percentage or amount) to all RFQ lines.
    Popup allows users to input the discount, select percentage/amount, and
    auto-calculate the percentage when the user selects the amount.

---
+++ purchase_global_discount/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Purchase - Global_Discount",
+    "version": "1.0",
+    "description": """
+This Module provides Global Discount on the Purchase Order.
+    """,
+    "category": "Inventory/Purchase",
+    "depends": ["purchase"],
+    "data": [
+        "views/purchase_views.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+    "license": "LGPL-3",
+}
