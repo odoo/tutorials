4d3532fbc2fcf82c3dea9697ec00fd3941bc222e
Tristan Cazier <tristan.gabriel.r.cazier@gmail.com>
2025-09-19 09:27:58 +0200

    [ADD] estate_account: add module manifest

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,13 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Estate Account',
+    'depends': [
+        'base',
+        'estate',
+        'account',
+    ],
+    'version': '19.0.0.0',
+    'installable': True,
+    'application': True,
+}
