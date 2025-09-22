e99d693fda31ad5d7a1e78b22a743f2bc1a3ab46
Florian Malfroid <flmal@odoo.com>
2025-09-22 15:27:54 +0200

    [IMP] awesome_owl: Technical onboarding, chapter 1, step 1 to 5

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,7 +1,12 @@
-/** @odoo-module **/
-
-import { Component } from "@odoo/owl";
+import { Component, markup } from "@odoo/owl";
+import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
+    static components = { Counter, Card };
+    static props = {};
+
+    html_value1 = markup("<a href='#'>some content</a>");
+    html_value2 = "<a href='#'>some content</a>";
 }
