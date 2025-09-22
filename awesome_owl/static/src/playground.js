cdc5670fb791ede7560e7d4b8fed95e468dc553e
Florian Malfroid <flmal@odoo.com>
2025-09-22 16:39:28 +0200

    [IMP] awesome_owl: Discover the web framework step 7: A todo list

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,10 +1,11 @@
 import { Component, markup, useState } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
+import { TodoList } from "./todo_list/todo_list";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
-    static components = { Counter, Card };
+    static components = { Counter, Card, TodoList };
     static props = {};
 
     html_value1 = markup("<a href='#'>some content</a>");
---
+++ awesome_owl/static/src/todo_list/todo_item.js
@@ -0,0 +1,8 @@
+import { Component } from "@odoo/owl";
+
+export class TodoItem extends Component {
+    static template = "awesome_owl.todo_item";
+    static props = {
+        todo: Object
+    };
+}
