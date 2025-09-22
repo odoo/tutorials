7f460d700ce29d1c3574cb26bb2eb4add362b9cb
Avery Pinko <avpin@odoo.com>
2025-09-22 14:54:10 +0200

    copy changes from other branch

--- awesome_owl/__manifest__.py
+++ awesome_owl/__manifest__.py
@@ -16,7 +16,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Tutorials/AwesomeOwl',
+    'category': 'Tutorials',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -31,7 +31,6 @@
             ('include', 'web._assets_helpers'),
             'web/static/src/scss/pre_variables.scss',
             'web/static/lib/bootstrap/scss/_variables.scss',
-            'web/static/lib/bootstrap/scss/_maps.scss',
             ('include', 'web._assets_bootstrap'),
             ('include', 'web._assets_core'),
             'web/static/src/libs/fontawesome/css/font-awesome.css',
