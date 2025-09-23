d547bfebd0757bfc521e8d0a7f1e5480d4e39da8
usseif <usseif97@gmail.com>
2025-09-23 16:33:59 +0200

    chapter1-No.6

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -2,4 +2,10 @@ import { Component, useState } from "@odoo/owl";
 
 export class Card extends Component {
     static template = "awesome_owl.card";
+
+    // Explicit Props
+    static props = {
+        title: String,
+        content: String,
+    };
 }
--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,12 +1,25 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, useState, markup } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
-
     static components = { Counter, Card };
 
+    setup() {
+        this.state = useState({ sum: 2 });
+        this.incrementSum = this.incrementSum.bind(this);  // Bind the method to its instance
+    }
+
+    incrementSum(){
+        this.state.sum++; 
+    }
+
+    cards = [
+        { title: "Card 1", content: markup("<div>some content</div>") },
+        { title: "Card 2", content: "This is the second card." },
+        { title: "Card 3", content: "Another card example." },
+    ];
 }
