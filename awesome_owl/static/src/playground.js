3ef6e224bbfdd54271a166258a3a45ffa7bc403b
Mohamed Ayman <mohmo@odoo.com>
2025-09-23 11:11:11 +0200

    [IMP] playground: update data displayed on cards and try markup

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,10 +1,15 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, markup } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
     static components = { Counter, Card };
+
+    setup() {
+        this.normal_string = "<div class='text-primary'>some content</div>";
+        this.markup_string = markup("<div class='text-primary'>some content</div>");
+    }
 }
