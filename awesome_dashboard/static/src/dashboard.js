ac47f9bf05c4c751fe60a7b90844537a7928627f
Tristan Cazier <tristan.gabriel.r.cazier@gmail.com>
2025-09-23 15:21:02 +0200

    [IMP] awesome_dashboard: add sections 3-4

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,17 +1,24 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, onWillStart } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
 import { useService } from "@web/core/utils/hooks";
 import { _t } from "@web/core/l10n/translation";
+import { DashboardItem } from "./dashboard_item";
+import { rpc } from "@web/core/network/rpc";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
-    static components = { Layout }
+    static components = { Layout, DashboardItem }
 
     setup() {
         this.action = useService("action");
+        onWillStart(async () => {
+            const stats = await rpc("/awesome_dashboard/statistics");
+            this.stats = stats
+            console.log(stats)
+        })
     }
     openPartners() {
         this.action.doAction("base.action_partner_form");
---
+++ awesome_dashboard/static/src/dashboard_item.js
@@ -0,0 +1,14 @@
+import { Component } from "@odoo/owl";
+
+export class DashboardItem extends Component {
+    static template = "awesome_dashboard.dashboard_item"
+    static props = { 
+        size: {type: Number, optional: true},
+        slots: {optional: true}
+    }
+
+    setup() {
+        this.size = this.props.size || 1
+        this.width = `${18 * this.size}rem`
+    }
+}
