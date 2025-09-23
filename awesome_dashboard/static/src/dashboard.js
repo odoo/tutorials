adf412a4dbfdbfde3cb17ba8a7c4834bc87b5dfc
Tristan Cazier <tristan.gabriel.r.cazier@gmail.com>
2025-09-23 13:39:50 +0200

    [IMP] awesome_dashboard: add section 2

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -3,10 +3,31 @@
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
+import { _t } from "@web/core/l10n/translation";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
     static components = { Layout }
+
+    setup() {
+        this.action = useService("action");
+    }
+    openPartners() {
+        this.action.doAction("base.action_partner_form");
+    }
+    async openLeads() {
+        this.action.doAction({
+            type: 'ir.actions.act_window',
+            name: _t('All Leads'),
+            res_model: 'crm.lead',
+            views: [
+                [false, 'list'], 
+                [false, 'form']
+            ],
+        });
+    }
+
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
