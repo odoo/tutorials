b57ecf48a7e574531a46f8bc429f518a22dc22bd
Radu Macocian <rmacocian@yahoo.com>
2025-09-23 13:38:36 +0200

    [IMP] todo: Focusing the input

--- awesome_owl/static/src/todo/todo_list.js
+++ awesome_owl/static/src/todo/todo_list.js
@@ -2,19 +2,21 @@ import { Component, useState } from "@odoo/owl";
 import { TodoItem } from "./todo_item";
 import { Todo } from "./todo";
 import { Card } from "../card/card";
+import { useAutofocus } from "../utils";
 
 export class TodoList extends Component {
     static template = "awesome_owl.todo_list";
     static components = { Card, TodoItem };
 
     ids = 0
-    
+
     setup() {
-        this.state = useState({ todos: [], input: ''})
+        this.state = useState({ todos: [], input: '' })
+        useAutofocus('todo_input')
     }
 
     keyup(event) {
-        if(event.keyCode === 13) {
+        if (event.keyCode === 13) {
             this.state.todos.push(new Todo(this.ids++, event.srcElement.value, false))
         }
     }
