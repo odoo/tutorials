d8c3e5614ebe88ad2874b8eec08efae2505e21b8
Florian Malfroid <flmal@odoo.com>
2025-09-23 11:43:20 +0200

    [IMP] awesome_dashboard: Discover the web framework, Chapter 2, Step 1, A new Layout

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -2,9 +2,18 @@
 
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
+    static components = { Layout };
+    static props = {};
+
+    setup() {
+        this.action = useService("action");
+        this.title = "Awesome Dashboard";
+    }
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
