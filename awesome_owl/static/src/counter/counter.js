aa4125a51fed273d254ae18aa4276705e3a66b13
trcazier <100358304+trcazier@users.noreply.github.com>
2025-09-23 10:16:50 +0200

    [IMP] awesome_owl: replace redundant ternary operator with ||
    Co-authored-by: Cemal Faruk Güney <63407686+cgun-odoo@users.noreply.github.com>

--- awesome_owl/static/src/counter/counter.js
+++ awesome_owl/static/src/counter/counter.js
@@ -8,7 +8,7 @@ export class Counter extends Component {
     }
 
     setup() {
-        this.state = useState({ value: this.props.value? this.props.value : 0 });
+        this.state = useState({ value: this.props.value || 0});
     }
 
     increment() {
