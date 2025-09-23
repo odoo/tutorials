3af4e616f937161da68ca4072b7664e6d8d60f1b
Mohamed Ayman <mohmo@odoo.com>
2025-09-23 17:28:58 +0200

    [IMP] card: allow toggling of cards' content

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -1,9 +1,17 @@
-import { Component } from "@odoo/owl"
+import { Component, useState } from "@odoo/owl"
 
 export class Card extends Component {
     static template = "awesome_owl.card"
     static props = {
         title: String,
         slots: Object,
+    }
+
+    setup() {
+        this.isOpened = useState({ value: true });
+    }
+
+    toggle() {
+        this.isOpened.value = !this.isOpened.value;
     }
 }
