1bf3b4ac94d4f5f767b63ce106039f5e7f0a4a0a
Florian Malfroid <flmal@odoo.com>
2025-09-23 10:49:53 +0200

    [IMP] awesome_owl: Discover the web framework Chapter 1: Owl components

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -1,4 +1,4 @@
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 
 export class Card extends Component {
     static template = "awesome_owl.card";
@@ -6,4 +6,12 @@ export class Card extends Component {
         title: String,
         slots: Object
     };
+
+    setup() {
+        this.state = useState({ open: true });
+    }
+
+    toggle() {
+        this.state.open = !this.state.open;
+    }
 }
