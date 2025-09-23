6da5f5f74ad3de102a0da134a9491dea8474ff68
Mohamed Ayman <mohmo@odoo.com>
2025-09-23 11:44:59 +0200

    [IMP] playground: add a sum and increment it using the counters

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import { Component, markup } from "@odoo/owl";
+import { Component, useState, markup } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
 
@@ -11,5 +11,10 @@ export class Playground extends Component {
     setup() {
         this.normal_string = "<div class='text-primary'>some content</div>";
         this.markup_string = markup("<div class='text-primary'>some content</div>");
+        this.sum = useState({ value: 2 });
+    }
+
+    incrementSum() {
+        this.sum.value++;
     }
 }
