ea02bb016c0ac981faa45964e24a229f00f6847f
Mohamed Ayman <mohmo@odoo.com>
2025-09-23 14:32:05 +0200

    [IMP] todo_list: remove hard coded list and allow user to add todo items

--- awesome_owl/static/src/todo_list/todo_list.js
+++ awesome_owl/static/src/todo_list/todo_list.js
@@ -6,6 +6,15 @@ export class TodoList extends Component {
     static components = { TodoItem };
 
     setup() {
-        this.todos = useState([{ id: 2, description: "write tutorial", isCompleted: true }, { id: 3, description: "buy milk", isCompleted: false }]);
+        this.todos = useState([]);
+        this.todo_idx = useState({ value: 1 });
+    }
+
+    addTodo(ev) {
+        if (ev.keyCode !== 13 || ev.target.value == "") {
+            return;
+        }
+        this.todos.push({ "id": this.todo_idx.value++, "description": ev.target.value, "isCompleted": false });
+        ev.target.value = "";
     }
 }
