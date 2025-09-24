4dad56bbf6b5e504b6af8a4932c77f402c7475c0
usseif <usseif97@gmail.com>
2025-09-24 14:07:55 +0200

    chapter2.3

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,10 +1,34 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
+import { Component } from "@odoo/owl";
+
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
+    static components = { Layout };
+
+    setup() {
+      this.action = useService("action");
+    }   
+
+    openCustomers() {
+      this.action.doAction('base.action_partner_form'); // get the action by its XML ID
+    }
+
+    openLeads(){
+        this.action.doAction({ // define the action inline
+            type: 'ir.actions.act_window',
+            name: 'Leads',
+            target: 'current',
+            res_model: 'crm.lead',
+            views: [
+                [false, 'form'],
+                [false, 'list']],
+        });
+    }
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
