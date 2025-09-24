ef33a663bf9112982a6daa498dcbb3750938b1b6
hamza <hasaa@odoo.com>
2025-09-24 13:55:17 +0200

    [ADD] Section 4

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,14 +1,18 @@
-import { Component } from "@odoo/owl";
+import { Component, onWillStart } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
-import {useService} from "@web/core/utils/hooks";
+import { useService } from "@web/core/utils/hooks";
 import { DashboardItem } from "./dashboard_item";
+import { rpc } from "@web/core/network/rpc";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
     static components = { Layout, DashboardItem }
     setup() {
         this.action = useService("action");
+        onWillStart(async () => {
+            this.result = await rpc("/awesome_dashboard/statistics");
+        });
     }
     openCustomers() {
         this.action.doAction("base.action_partner_form");
