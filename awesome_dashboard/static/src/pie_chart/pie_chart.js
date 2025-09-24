4346a21a83e8ed801b95942ac9111edf71d4310e
Radu Macocian <rmacocian@yahoo.com>
2025-09-24 09:30:52 +0200

    [IMP] awesome_dashboard: display a pie chart

---
+++ awesome_dashboard/static/src/pie_chart/pie_chart.js
@@ -0,0 +1,27 @@
+import { Component, onWillStart, useEffect, useRef } from "@odoo/owl"
+import { loadJS } from "@web/core/assets"
+
+export class PieChart extends Component {
+    static template = "awesome_dashboard.PieChart";
+
+    static props = {'data':{optional: false}, 'size': {type: Number, optional: true}}
+
+    setup() {
+        onWillStart(() => loadJS(["/web/static/lib/Chart/Chart.js"]));
+        this.ctx = useRef('pieChart')
+        this.chart = null
+        useEffect(() => {
+            if(this.chart) {
+                this.chart.destroy()
+            }
+            const dataset = {
+                labels: Object.keys(this.props.data),
+                datasets: [{
+                    label: 'T-Shirt Size Distribution',
+                    data: Object.values(this.props.data)
+                }]
+            }
+            this.chart = new Chart(this.ctx.el, {type: 'pie', data: dataset})
+        })
+    }
+}
