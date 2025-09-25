3dbc265c113fea2d4a45bebd614202d7ed382cf8
hamza <hasaa@odoo.com>
2025-09-25 10:44:33 +0200

    [ADD] Section 8

--- awesome_dashboard/static/src/dashboard/dashboard.js
+++ awesome_dashboard/static/src/dashboard/dashboard.js
@@ -27,5 +27,5 @@ class AwesomeDashboard extends Component {
     }
 }
 
-registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
+registry.category("lazy_components").add("AwesomeDashboard", AwesomeDashboard);
 
