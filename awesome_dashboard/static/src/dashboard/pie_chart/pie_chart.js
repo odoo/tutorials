9a064207e01e8b693c1a0c381f66e3a30b92fdbd
Radu Macocian <rmacocian@yahoo.com>
2025-09-25 14:30:47 +0200

    [IMP] awesome_dashboard: opening orders on chart click

--- awesome_dashboard/static/src/dashboard/pie_chart/pie_chart.js
+++ awesome_dashboard/static/src/dashboard/pie_chart/pie_chart.js
@@ -1,17 +1,20 @@
 import { Component, onWillStart, useRef, onMounted } from "@odoo/owl";
+import { _t } from "@web/core/l10n/translation";
 import { loadJS } from "@web/core/assets";
+import { useService } from "@web/core/utils/hooks";
 
 export class PieChart extends Component {
     static template = "awesome_dashboard.PieChart";
 
     static props = {
-        data: { },
+        data: {},
         size: { type: Number, optional: true },
     };
 
     setup() {
         onWillStart(() => loadJS(["/web/static/lib/Chart/Chart.js"]));
         this.ctx = useRef("pieChart");
+        this.action = useService("action");
         this.chart = null;
         onMounted(() => {
             if (this.chart) {
@@ -25,7 +28,39 @@ export class PieChart extends Component {
                     },
                 ],
             };
-            this.chart = new Chart(this.ctx.el, { type: "pie", data: dataset });
+            this.chart = new Chart(this.ctx.el, {
+                type: "pie",
+                data: dataset,
+                options: {
+                    onClick: (event) => {
+                        const canvasPosition = this.chart.getElementsAtEventForMode(
+                            event,
+                            'nearest',
+                            { intersect: true },
+                            true
+                        );
+                        const variant = Object.keys(this.props.data)[
+                            canvasPosition[0].index
+                        ]
+                        this.action.doAction({
+                            type: "ir.actions.act_window",
+                            name: _t("Sales"),
+                            res_model: "sale.order",
+                            views: [
+                                [false, "list"],
+                                [false, "form"],
+                            ],
+                            domain: [
+                                ['order_line.product_id.name', 'ilike', 'T-Shirt'],
+                                ['order_line.product_id.product_template_attribute_value_ids.name',
+                                    'ilike',
+                                    variant]
+                            ]
+                        });
+                    }
+                }
+            });
+
         });
     }
 }
