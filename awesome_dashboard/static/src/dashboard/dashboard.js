704adb34f054c7d7fc967f5923e5fd84eed9bdda
Mohamed Ayman <mohmo@odoo.com>
2025-09-25 15:05:14 +0200

    [IMP] dashboard: make the dashboard generic

--- awesome_dashboard/static/src/dashboard/dashboard.js
+++ awesome_dashboard/static/src/dashboard/dashboard.js
@@ -5,15 +5,16 @@ import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
 import { useService } from "@web/core/utils/hooks";
 import { DashboardItem } from "./dashboard_item";
-import { PieChart } from "./pie_chart";
+import { items } from "./dashboard_items";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
-    static components = { Layout, DashboardItem, PieChart };
+    static components = { Layout, DashboardItem };
 
     setup() {
         this.action = useService("action");
         this.statistics = useState(useService("statistics_service"));
+        this.items = items;
     }
 
     openCustomers() {
