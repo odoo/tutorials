3fe0f1b213583848d0a9bd8701c3b2181d47f374
Florian Malfroid <flmal@odoo.com>
2025-09-29 09:34:49 +0200

    [IMP] estate: Changed order of data files in manifest

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -12,9 +12,9 @@
         'views/estate_property_views.xml',
         'views/estate_property_type_views.xml',
         'views/estate_property_tag_views.xml',
+        'security/estate_security.xml',
         'views/estate_menus.xml',
         'views/res_users_view.xml',
-        'security/estate_security.xml',
         'security/ir.model.access.csv',
     ]
 }
