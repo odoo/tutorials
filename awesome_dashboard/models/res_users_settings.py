d8b2a2cf0c9808fe2292b8a32a9df883edd0e673
Mohamed Ayman <mohmo@odoo.com>
2025-09-30 10:36:52 +0200

    [IMP] dashboard: remove unnecessary default string, unnecessary array when inheriting

--- awesome_dashboard/models/res_users_settings.py
+++ awesome_dashboard/models/res_users_settings.py
@@ -2,6 +2,6 @@ from odoo import models, fields
 
 
 class ResUsersSettings(models.Model):
-    _inherit = ["res.users.settings"]
+    _inherit = "res.users.settings"
 
-    disabled_dashboard_items = fields.Text("Disabled dashboard items")
+    disabled_dashboard_items = fields.Text()
