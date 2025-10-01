002dfba48e3dddea553b258701bd8514affd89a6
Tristan Cazier <tristan.gabriel.r.cazier@gmail.com>
2025-10-01 14:06:58 +0200

    [FIX] estate: fix runbot warning for string

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -40,7 +40,10 @@ class PropertyOffer(models.Model):
     def _check_selling_price_90_percent(self):
         for record in self:
             if float_compare(record.price, 0.9 * record.property_id.expected_price, precision_digits=2) == -1:
-                raise exceptions.UserError(_(f"The selling price ({record.price}) cannot be lower than 90% of the expected price ({record.property_id.expected_price})"))
+                raise exceptions.UserError(
+                    _("The selling price (%s) cannot be lower than 90%% of the expected price (%s)")
+                    % (record.price, record.property_id.expected_price),
+                )
 
     @api.model_create_multi
     def create(self, vals_list):
