0b054119dba90df934a68c048315bb946fcfab91
Tristan Cazier <tristan.gabriel.r.cazier@gmail.com>
2025-10-02 16:33:13 +0200

    [IMP] x_estate: add offer model, view and add it to property model and form

--- x_estate/__manifest__.py
+++ x_estate/__manifest__.py
@@ -12,12 +12,20 @@
     'installable': True,
     'application': True,
     'data': [
+        # models
+        "models/x_real_estate_property_model.xml",
+        "models/x_real_estate_property_offer_model.xml",
         "models/x_real_estate_property_type.xml",
         "models/x_real_estate_property_tag.xml",
-        "models/x_real_estate_property.xml",
+        # fields
+        "models/x_real_estate_property_offer_fields.xml",
+        "models/x_real_estate_property_fields.xml",
+
         "security/ir.model.access.csv",
+
         "views/x_real_estate_property_type_views.xml",
         "views/x_real_estate_property_tag_views.xml",
+        "views/x_real_estate_property_offer_views.xml",
         "views/x_real_estate_property_views.xml",
         "views/x_real_estate_menus.xml",
     ],
