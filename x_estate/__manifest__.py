a07d26c6cf6b38b2fe4fa86a2de05d55f4218417
Tristan Cazier <tristan.gabriel.r.cazier@gmail.com>
2025-10-02 11:40:57 +0200

    [ADD] x_estate: add new importable module x_estate

---
+++ x_estate/__manifest__.py
@@ -0,0 +1,20 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Estate (import)',
+    'depends': [
+        'base',
+        'base_import_module',
+    ],
+    'version': '19.0.0.0',
+    'author': "Odoo S.A.",
+    'license': "LGPL-3",
+    'installable': True,
+    'application': True,
+    'data': [
+        "models/x_real_estate_property.xml",
+        "security/ir.model.access.csv",
+        "views/x_real_estate_property_views.xml",
+        "views/x_real_estate_menus.xml",
+    ],
+}
