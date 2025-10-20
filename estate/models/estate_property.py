2ebfca3ad7257fa27841a7f551b4849cea3fe26b
hazei-odoo <hazei@odoo.com>
2025-10-20 15:24:21 +0200

    [IMP] estate: estate_property.py

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -4,5 +4,7 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real Estate Property"
 
-    name = fields.char(string="Title", required=true) expected_price = fields.Float(required=True) bedrooms = field.Integer(default=2)
+    name = fields.Char(string="Title", required=True)
+    expected_price = fields.Float(required=True)
+    bedrooms = field.Integer(default=2)
     
