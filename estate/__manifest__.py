500f0fcb26fc62a0ff9641ab34bbce73e68437ea
hazei-odoo <hazei@odoo.com>
2025-10-20 17:18:38 +0200

    [IMP] estate: manifest

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,6 +2,8 @@
     'name': 'Odoo Tutorial Real Estate',
     'category': 'Real Estate',
     'version': '19.0.1.0',
+    'author': 'Hazei',
+    'license': 'LGPL-3',
     'summary': 'Real Estate Management Tutorial',
     'application': True,
     'data': [
