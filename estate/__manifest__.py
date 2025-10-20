c451738feb08e42a02be687ad2c7fc8c05a43078
hazei-odoo <hazei@odoo.com>
2025-10-20 16:39:47 +0200

    [IMP] estate: fix

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name' : 'Odoo Tutorial Real Estate',
-    'category': 'Real Estate',
+    'category':'Real Estate',
     'version': '19.0.1.0',
     'summary' : 'Real Estate Management Tutorial',
     'application': True,
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,10 +1,24 @@
 from odoo import models, fields 
 
+
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real Estate Property"
 
     name = fields.Char(string="Title", required=True)
-    expected_price = fields.Float(required=True)
-    bedrooms = field.Integer(default=2)
-    
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float()
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        string='Selection',
+        selection=[('north', 'North'), ('south', 'South'),
+        ('east', 'East'), ('west', 'West')])
+
