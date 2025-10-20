7076246a69b27308fee3c620de376bd3ad12ee83
matho-odoo <matho@odoo.com>
2025-10-20 13:48:29 +0200

    [ADD] estate: create the app

---
+++ estate/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Estate',
+    'version': '0.1',
+    'application': True,
+    'author': 'matho',
+    'depends': [
+        'base',
+    ],
+}
