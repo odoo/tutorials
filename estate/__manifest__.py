15ad93560bcdc93f946a8b5b8e4ad30f06998247
Hashem Khaled <hashemkhaled@aucegypt.edu>
2025-10-20 15:35:03 +0200

    [IMP] estate: add access control for estate property model

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,6 +2,9 @@
     'name': 'Real Estate',
     'summary': 'Manages real estate properties.',
     'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
     'application': True,
     'installable': True,
 }
