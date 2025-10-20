24e63d0a972b00641e43120464ef84bb697d9e03
Wajih-Wanis <wajihwanis2000@gmail.com>
2025-10-20 13:53:50 +0200

    [ADD] estate: create the app

---
+++ estate/__init__.py
@@ -0,0 +1,4 @@
+from . import models
+
+
+from odoo import api, SUPERUSER_ID
---
+++ estate/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': "Real Estate",
+
+    'summary': """
+        Real Estate Tuto"
+    """,
+
+    'description': """
+        Starting tutorial real estate"
+    """,
+
+    'author': "Odoo",
+    'website': "https://www.odoo.com/",
+    'category': 'Tutorials',
+    'version': '0.1',
+    'application': True,
+    'depends': ['base'],
+
+    'data': [],
+    'assets': {
+
+    },
+    'license': 'AGPL-3'
+}
