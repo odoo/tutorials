7c751ec206fde4282c398e72bfde5740f48bc2b3
Mohamed (moaln) <mohamedkhaled3082@gmail.com>
2025-10-20 13:57:58 +0200

    [IMP] estate: change the module's author name to my name

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,7 +2,7 @@
     'name': 'Real Estate',
     'version': '1.0',
     'depends': ['base'],
-    'author': 'Your Name',
+    'author': 'Mohamed (moaln)',
     'category': 'Real Estate',
     'summary': 'A basic real estate module',
     'description': 'A tutorial module for real estate management',
