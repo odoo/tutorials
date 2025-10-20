812c3a9990497850cb9a61cb210bbb6d1cd7977b
Frantisek Binovsky <frbin@odoo.com>
2025-10-20 13:33:09 +0200

    [IMP] estate: Chapter 3

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,6 +3,8 @@
     'version': '1.0',
     'depends': ['base'],
     'author': "frbin",
+    'category': 'Tutorials',
     'description': "Soon to be the best module ever",
-    'application': True
+    'application': True,
+    'installable': True,
 }
---
+++ estate/models/estate_property.py
@@ -0,0 +1,5 @@
+from odoo import fields, models
+
+class EstateProperty(models.Model):
+    _name = 'estate.estate_property'
+    _description = 'Real Estate Property'
