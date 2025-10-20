0a1b5d447ee793a03a7e118992dd9761316c60c9
Mohamed (moaln) <mohamedkhaled3082@gmail.com>
2025-10-20 13:54:46 +0200

    [ADD] estate: Create the app

---
+++ estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'depends': ['base'],
+    'author': 'Your Name',
+    'category': 'Real Estate',
+    'summary': 'A basic real estate module',
+    'description': 'A tutorial module for real estate management',
+    'application': True,
+}
