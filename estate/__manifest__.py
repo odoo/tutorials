e80c6c5d76cccc1597a3df8ff2c06463dcafb762
Victor Decleire <vdecleire@gmail.com>
2025-10-20 13:48:50 +0200

    [ADD] Estate: create the app

---
+++ estate/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Victor Decleire",
+    'application': True,
+    # 'description': """
+    # Description text
+    # """,
+    # # data files always loaded at installation
+    # 'data': [
+    #     'views/mymodule_view.xml',
+    # ],
+    # # data files containing optionally loaded demonstration data
+    # 'demo': [
+    #     'demo/demo_data.xml',
+    # ],
+}
