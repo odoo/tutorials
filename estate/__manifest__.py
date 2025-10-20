c3c8c66ead3efbd2c55d15d5ff9dd037b1b487a7
Jugurtha <jugurthagaci24@gmail.com>
2025-10-20 15:14:52 +0200

    [ADD] estate: define access rights (chapter 4)

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,5 +3,8 @@
     'version': '1.0',
     'depends': ['base'],
     'author': "GACI Jugurtha (jugac)",
-    'application': True
+    'application': True,
+    'data': [
+        'data/ir.model.access.csv'
+    ]
 }
