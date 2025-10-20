86659ebba4af56d73d26b9ca9893b2c7e1688840
Mohamed (moaln) <mohamedkhaled3082@gmail.com>
2025-10-20 16:00:26 +0200

    [IMP] estate: added access rights

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,10 +1,12 @@
 {
     'name': 'Real Estate',
+    'description': 'A tutorial module for real estate management',
     'version': '1.0',
     'depends': ['base'],
-    'author': 'Mohamed (moaln)',
     'category': 'Real Estate',
     'summary': 'A basic real estate module',
-    'description': 'A tutorial module for real estate management',
+    'data': ['security/ir.model.access.csv'],
     'application': True,
+    'author': 'Mohamed (moaln)',
+    'license': 'LGPL-3',
 }
