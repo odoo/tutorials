77b28abcacbd4d68a7a13813361013ae2d90732e
maler-odoo <maler@odoo.com>
2025-10-20 16:49:00 +0200

    [ADD] estate: chapter 5

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -8,5 +8,8 @@
     'license': 'LGPL-3',
     'data': [
         'security/ir.model.access.csv',
+        
+        'views/estate_menus.xml',
+        'views/estate_property_views.xml',
     ],
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,6 @@
 from odoo import models, fields
+import datetime
+from dateutil.relativedelta import relativedelta
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -7,10 +9,10 @@ class EstateProperty(models.Model):
     name =  fields.Char('Name', required=True)
     description = fields.Text('Description')
     postcode = fields.Char('Post Code')
-    date_availability = fields.Date('Date availability')
+    date_availability = fields.Date('Date availability', copy=False, default=datetime.date.today()+relativedelta(months=+3))
     expected_price = fields.Float('Expected price', required=True)
-    selling_price = fields.Float('Selling price')
-    bedrooms = fields.Integer('Bedrooms')
+    selling_price = fields.Float('Selling price', readonly=True, copy=False)
+    bedrooms = fields.Integer('Bedrooms', default=2)
     living_area = fields.Integer('Living area')
     facades = fields.Integer('Facades')
     garage = fields.Boolean('Garage')
@@ -19,4 +21,9 @@ class EstateProperty(models.Model):
     garden_orientation = fields.Selection(
         string='Orientation',
         selection=[('north','North'), ('east','East'), ('south','South'), ('west','West')],
+    )
+    active = fields.Boolean('Active', default=True)
+    state = fields.Selection(
+        string='state',
+        selection=[('new','New'),('offerreceived','Offer Received'),('offeraccepted','Offer Accepted'),('sold','Sold'),('cancelled','Cancelled')],
     )
