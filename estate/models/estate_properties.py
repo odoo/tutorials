8d89c746c6243ad38fa7d7e16dd914f7aededf25
qucol-odoo <qucol@odoo.com>
2025-10-21 10:04:41 +0200

    [IMP] estate: Chapter 5

--- estate/models/estate_properties.py
+++ estate/models/estate_properties.py
@@ -8,10 +8,10 @@ class PropertyModel(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(default=fields.Date.add(fields.Date.today(), months=3), copy=False)
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -20,9 +20,23 @@ class PropertyModel(models.Model):
     garden_orientation = fields.Selection(
         string='Orientation',
         selection=[
-            ('North', 'north'),
-            ('South', 'south'),
-            ('East', 'east'),
-            ('West', 'west')
+            ('north', 'North'),
+            ('south', 'South'),
+            ('east', 'East'),
+            ('west', 'West')
         ]
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        string="State",
+        selection=[
+            ("new", "New"),
+            ("received", "Offer Received"),
+            ("accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled")
+        ],
+        required=True,
+        copy=False,
+        default="new"
     )
