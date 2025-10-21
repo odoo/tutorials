9d9a3a4808b66bd5fd600da20dec8aeb92f63e66
Hashem Khaled <hashemkhaled@aucegypt.edu>
2025-10-21 14:51:53 +0200

    [FIX] estate: adjust style

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -6,10 +6,14 @@ class PropertyOffer(models.Model):
     _description = "Real Estate Property Offer"
 
     price = fields.Float()
-    status = fields.Selection([
-        ('accepted', 'Accepted'),
-        ('refused', 'Refused'),
-    ], copy=False)
-    partner_id = fields.Many2one("res.partner", string="Partner", required=True)
-    property_id = fields.Many2one("estate.property", string="Property", required=True)
-  
+    status = fields.Selection(
+        [
+            ("accepted", "Accepted"),
+            ("refused", "Refused"),
+        ],
+        copy=False,
+    )
+    partner_id = fields.Many2one(
+        "res.partner", string="Partner", required=True)
+    property_id = fields.Many2one(
+        "estate.property", string="Property", required=True)
