c80104542959731eb67d200d71cdf0af2130a162
qucol-odoo <qucol@odoo.com>
2025-10-21 15:56:00 +0200

    [IMP] estate: Chapter 8

--- estate/models/estate_properties.py
+++ estate/models/estate_properties.py
@@ -1,4 +1,7 @@
-from odoo import fields, models
+from odoo import api, fields, models
+
+DEFAULT_GARDEN_AREA = 10
+DEFAULT_GARDEN_ORIENTATION = "north"
 
 
 class PropertyModel(models.Model):
@@ -10,6 +13,7 @@ class PropertyModel(models.Model):
     postcode = fields.Char()
     date_availability = fields.Date(default=fields.Date.add(fields.Date.today(), months=3), copy=False)
     expected_price = fields.Float(required=True)
+    best_offer = fields.Float(compute="_get_highest_price")
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
@@ -18,13 +22,9 @@ class PropertyModel(models.Model):
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
-        selection=[
-            ('north', 'North'),
-            ('south', 'South'),
-            ('east', 'East'),
-            ('west', 'West')
-        ]
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
     )
+    total_living_area = fields.Integer(compute="_compute_total_area")
     active = fields.Boolean(default=True)
     state = fields.Selection(
         selection=[
@@ -43,3 +43,23 @@ class PropertyModel(models.Model):
     salesperson_id = fields.Many2one("res.users", default=lambda self: self.env.user)
     tag_ids = fields.Many2many("estate.property.tag")
     offer_ids = fields.One2many("estate.property.offer", "property_id")
+
+    @api.depends("living_area", "garden_area")
+    def _compute_total_area(self):
+        for record in self:
+            record.total_living_area = record.living_area + record.garden_area
+
+    @api.depends("offer_ids")
+    def _get_highest_price(self):
+        for record in self:
+            record.best_offer = max(record.offer_ids.mapped("price")) if record.offer_ids else 0
+
+    @api.onchange("garden")
+    def _onchange_garden(self):
+        for record in self:
+            if record.garden:
+                record.garden_area = DEFAULT_GARDEN_AREA
+                record.garden_orientation = DEFAULT_GARDEN_ORIENTATION
+            else:
+                record.garden_area = 0
+                record.garden_orientation = None
--- estate/models/estate_property_offers.py
+++ estate/models/estate_property_offers.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class PropertyOfferModel(models.Model):
@@ -15,3 +15,14 @@ class PropertyOfferModel(models.Model):
     )
     partner_id = fields.Many2one("res.partner", required=True)
     property_id = fields.Many2one("estate.property", required=True)
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute="_compute_deadline", inverse="_inverse_deadline")
+
+    @api.depends("validity")
+    def _compute_deadline(self):
+        for record in self:
+            record.date_deadline = fields.Date.add(record.create_date, days=record.validity) if record.create_date else None
+
+    def _inverse_deadline(self):
+        for record in self:
+            record.validity = (record.date_deadline - fields.Date.to_date(record.create_date)).days if record.date_deadline else record.validity
