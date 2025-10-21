341bbe29e2596bf019f03872022cf875d01b5189
sawer <sawer@odoo.com>
2025-10-21 10:28:36 +0200

    [ADD] Chapter 6 sawer

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -3,45 +3,37 @@ from odoo import models, fields
 
 class EstateProperty(models.Model):
     _name = "estate.property"
-    _description = "Create a new table"
+    _description = "Estate Properties"
 
     # FIELDS DECLARATION #
     name = fields.Char(
-        string='Name', 
-        required=True
+        string='Title', 
+        required=True,
+        default="My new house"
     )
-    description = fields.Text(string='Description')
-    postcode = fields.Char(string='Post code')
+    description = fields.Text()
+    postcode = fields.Char()
     date_availability = fields.Date(
-        string='Availability date',
+        string='Available From',
         default=fields.Date.add(fields.Date.today(), months=3),
         copy=False 
     )
     expected_price = fields.Float(string='Expected price', required=True)
     selling_price = fields.Float(
-        string='Selling price',
         readonly=True,
         copy=False
     )
-    bedrooms = fields.Integer(
-        string='Bedrooms',
-        default=2
-    )
-    living_area = fields.Integer(string='Living area')
-    facades = fields.Integer(string='Facades')
-    garage = fields.Boolean(string='Garage')
-    garden = fields.Boolean(string='Garden')
-    garden_area = fields.Integer(string='Garden area')
-    garden_orientation = fields.Selection( 
-        string='Garden orientation',
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer(string='Living Area (sqm)')
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer(string='Garden Area (sqm)')
+    garden_orientation = fields.Selection(
         selection= [('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
     )
-    active = fields.Boolean(
-        string='Active',
-        default=True
-    )
+    active = fields.Boolean(default=True)
     state = fields.Selection(
-        string='State',
         selection= [('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')],
         default='new'
     )
