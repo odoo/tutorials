405330c882308ca69ff6308dae48521a92f8c143
Mohamed (moaln) <mohamedkhaled3082@gmail.com>
2025-10-21 13:43:27 +0200

    [IMP] estate: created menus, actions and customized model fields

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,7 +5,11 @@
     'depends': ['base'],
     'category': 'Real Estate',
     'summary': 'A basic real estate module',
-    'data': ['security/ir.model.access.csv'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
+    ],
     'application': True,
     'author': 'Mohamed (moaln)',
     'license': 'LGPL-3',
