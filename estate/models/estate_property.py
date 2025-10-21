b9fd3093bf6958887ea9e0a79f8e4e84086a93f9
Victor Decleire <vdecleire@gmail.com>
2025-10-21 13:26:01 +0200

    [IMP] Estate: Create views

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -10,8 +10,8 @@ class EstateProperty(models.Model):
     description = fields.Text()
     postcode = fields.Char()
     date_availability = fields.Date(copy=False, default=lambda x: add(fields.Date.today(), months=3))
-    expected_price = fields.Float()
-    selling_price = fields.Float(required=True, readonly=True, copy=False)
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
@@ -20,4 +20,11 @@ class EstateProperty(models.Model):
     garden_area = fields.Float()
     garden_orientation = fields.Selection(
         selection=[('north', 'North'), ('south', 'South'), ('west', 'West'), ('east', 'East')]
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        selection=[('new', 'New'), ('received', 'Offer Received'), ('accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')],
+        default='new',
+        copy=False,
+        required=True
     )
