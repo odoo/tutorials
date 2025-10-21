b76717705c827226c7725fad30667034b3391fb8
Frantisek Binovsky <frbin@odoo.com>
2025-10-21 11:22:24 +0200

    [IMP] estate: Chapter 6

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -6,23 +6,24 @@ class EstateProperty(models.Model):
     _description = 'Real Estate Property'
 
     name = fields.Char('Property Name', required=True)
-    active = fields.Boolean(default=False)
+    description = fields.Text()
+    active = fields.Boolean(default=True)
     postcode = fields.Char()
-    date_availability = fields.Datetime(copy=False, default=fields.Datetime.add(fields.Datetime.today(), months=3))
+    date_availability = fields.Datetime("Available From", copy=False, default=fields.Datetime.add(fields.Datetime.today(), months=3))
     expected_price = fields.Float()
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
+    living_area = fields.Integer("Living Area (sqm)")
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
         selection=[('north', 'North'), ('east', 'East'), ('south', 'South'), ('west', 'West')],
-        help="Type is used to separate Leads and Opportunities")
+    )
     state = fields.Selection(
         selection=[('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')],
         required=True,
         default='new',
-        copy=False,
+        copy=False
     )
