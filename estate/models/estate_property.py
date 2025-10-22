b69ce9708df115ab45479804d0eb0410c62fa445
Victor Decleire <vdecleire@gmail.com>
2025-10-22 09:02:18 +0200

    [IMP] Estate: Adding computed fields
    delete substract

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -42,3 +42,12 @@ class EstateProperty(models.Model):
                 record.best_price = max(record.offer_ids.mapped('price'))
             else:
                 record.best_price = 0
+
+    @api.onchange('garden')
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = 'north'
+        else:
+            self.garden_area = 0
+            self.garden_orientation = None
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,11 +1,24 @@
-from odoo import fields, models
+from odoo import fields, models, api
+from odoo.tools.date_utils import add
 
 
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
     _description = "Offers for propreties"
 
-    price = fields.Float()
+    price = fields.Float(required=True)
     status = fields.Selection(selection=[('accepted', 'Accepted'), ('refused', 'Refused')], copy=False)
     partner_id = fields.Many2one('res.partner', required=True)
     property_id = fields.Many2one('estate.property', required=True)
+    create_date = fields.Date(default=fields.Date.today(), readonly=True)
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_date_deadline")
+
+    @api.depends('validity', 'create_date')
+    def _compute_date_deadline(self):
+        for record in self:
+            record.date_deadline = add(record.create_date, days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            record.validity = (record.date_deadline - record.create_date).days
