6e4093e57c06618e5fa234d60646bdbf45b95956
odoo-wimar <wimar@odoo.com>
2025-10-22 12:19:08 +0200

    [ADD] estate: Init and manifest
    Create empty __init__.py and __manifest__.py with module name, set as application.

---
+++ estate/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'summary'  "Manage properties and buyers."
+    'depends': [
+        'base'
+    ],
+    'author': "wimar-odoo",
+    'description': """
+    Manage efficiently real estate properties for sale and match them with potential buyers.",
+    """,
+    'application': True,
+}
