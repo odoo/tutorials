92e14f32647f16eef578ca8b24933daf44a3c0bb
yagho-odoo <yagho@odoo.com>
2025-10-22 09:54:03 +0200

    [FIX] estate: added whitespaces to fix styling errors

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -10,9 +10,9 @@ class EstateProperty(models.Model):
     name = fields.Char('name', required=True)
     description = fields.Text('description')
     postcode = fields.Char('postcode')
-    date_availability = fields.Date(copy=False,default=date.today() + relativedelta(months=3))
+    date_availability = fields.Date(copy=False, default=date.today() + relativedelta(months=3))
     expected_price = fields.Float()
-    selling_price = fields.Float(readonly=True,copy=False)
+    selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer('# Bedrooms', default=2)
     living_area = fields.Integer('living area size')
     facades = fields.Integer('# Facades')
@@ -26,13 +26,13 @@ class EstateProperty(models.Model):
     active = fields.Boolean(default=True)
     state = fields.Selection(
         string='Sate',
-        selection=[('New','New'), ('Offer Received','Offer Received'), ('Offer Accepted','Offer Accepted'), ('Sold','Sold'), ('Cancelled','Cancelled')],
+        selection=[('New', 'New'), ('Offer Received', 'Offer Received'), ('Offer Accepted', 'Offer Accepted'), ('Sold', 'Sold'), ('Cancelled', 'Cancelled')],
         required=True,
         copy=False,
         default="New"
     )
     type_id = fields.Many2one("estate.property.type", string="type")
-    buyer_id = fields.Many2one("res.partner", string="buyer",copy=False)
-    salesperson_id = fields.Many2one("res.users", string="salesperson",default=lambda self: self.env.user)
+    buyer_id = fields.Many2one("res.partner", string="buyer", copy=False)
+    salesperson_id = fields.Many2one("res.users", string="salesperson", default=lambda self: self.env.user)
     tag_ids = fields.Many2many("estate.property.tag", string="tag")
     offer_ids = fields.One2many("estate.property.offer", "property_id", string="offer")
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -10,5 +10,5 @@ class EstatePropertyOffer(models.Model):
         string='status',
         selection=[('Accepted', 'Accepted'), ('Refused', 'Refused')],
         copy=False)
-    partner_id = fields.Many2one("res.partner", string="partner",required=True)
-    property_id = fields.Many2one("estate.property",required=True)
+    partner_id = fields.Many2one("res.partner", string="partner", required=True)
+    property_id = fields.Many2one("estate.property", required=True)
