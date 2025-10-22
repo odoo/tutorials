9fd358419b6038a9389ed8e764dc409f5761077c
Mohamed (moaln) <mohamedkhaled3082@gmail.com>
2025-10-22 11:46:39 +0200

    [IMP] estate: added computed fields and onchanges

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import api, models, fields
 from dateutil.relativedelta import relativedelta
 
 
@@ -33,3 +33,25 @@ class EstateProperty(models.Model):
     salesperson_id = fields.Many2one("res.users", default=lambda self: self.env.user)
     estate_property_tag_ids = fields.Many2many("estate.property.tag", string="Propert Tags")
     offer_ids = fields.One2many('estate.property.offer', 'property_id', string="Offers")
+
+    total_area = fields.Float(compute="_compute_total_area")
+    best_price = fields.Float(compute="_compute_best_price")
+
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for property in self:
+            property.total_area = property.living_area + property.garden_area
+
+    @api.depends('offer_ids')
+    def _compute_best_price(self):
+        for property in self:
+            property.best_price = max(property.offer_ids.mapped('price') or [0])
+
+    @api.onchange('garden')
+    def _onchange_garden(self):
+        if(self.garden):
+            self.garden_area = 10
+            self.garden_orientation = "north"
+        else:
+            self.garden_area = False
+            self.garden_orientation = ""
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import api, models, fields
+from dateutil.relativedelta import relativedelta
 
 
 class EstatePropertyOffer(models.Model):
@@ -12,3 +13,15 @@ class EstatePropertyOffer(models.Model):
     )
     partner_id = fields.Many2one('res.partner', string="Partner", required=True)
     property_id = fields.Many2one('estate.property', string="Property", required=True)
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_date_deadline")
+
+    @api.depends("validity", "create_date")
+    def _compute_date_deadline(self):
+        for offer in self:
+            offer.date_deadline = (offer.create_date or fields.Date.today()) + relativedelta(days=+offer.validity)
+
+    def _inverse_date_deadline(self):
+        for offer in self:
+            offer.validity = (offer.date_deadline - (offer.create_date or fields.Date.today()).date()).days
+
