fc5087848fb9912466c96d99b4db93c6a0e52b25
Mohamed (moaln) <mohamedkhaled3082@gmail.com>
2025-10-22 10:11:01 +0200

    [IMP] estate: added Many2one, Many2many and One2many relations

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -28,3 +28,8 @@ class EstateProperty(models.Model):
         copy=False,
         default='new'
     )
+    estate_property_type_id = fields.Many2one("estate.property.type", string="Property Type")
+    buyer_id = fields.Many2one("res.partner", copy=False)
+    salesperson_id = fields.Many2one("res.users", default=lambda self: self.env.user)
+    estate_property_tag_ids = fields.Many2many("estate.property.tag", string="Propert Tags")
+    offer_ids = fields.One2many('estate.property.offer', 'property_id', string="Offers")
