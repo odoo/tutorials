d9e8d79c17f21140c3acf9082e5bf58719de8635
Denis Rygaert <dery@odoo.com>
2025-10-22 15:18:14 +0200

    [IMP] estate: Add notes.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -7,6 +7,7 @@ from odoo.exceptions import UserError
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "property data"
+    notes = fields.Html()
 
     name = fields.Char(required=True)
     description = fields.Text()
