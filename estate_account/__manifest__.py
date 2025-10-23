ba956abcbf2a629bc735ed70032c0e6120dfc702
qucol-odoo <qucol@odoo.com>
2025-10-23 11:10:54 +0200

    [ADD] estate.account: Chapter 13

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Estate Account',
+    'depends': [
+        'base',
+        'account',
+        'estate'
+    ],
+    'data': [],
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3'
+}
