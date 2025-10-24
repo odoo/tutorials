73f89e15660217d9823da822d101877049d3ba99
Wajih-Wanis <wajihwanis2000@gmail.com>
2025-10-24 11:04:57 +0200

    [ADD] estate_account: added interact with other modules

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': "Estate Accounting",
+
+    'summary': """
+        Real Estate Tuto"
+    """,
+
+    'description': """
+        Estate accounting module"
+    """,
+
+    'author': "Odoo",
+    'website': "https://www.odoo.com/",
+    'category': 'Tutorials',
+    'version': '0.1',
+    'application': True,
+    'depends': ['estate','account'],
+
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'assets': {
+
+    },
+    'license': 'AGPL-3'
+}
---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,15 @@
+from odoo import models, fields, Command
+from odoo.exceptions import AccessError
+
+
+class EstateProperty(models.Model):
+    _inherit = 'estate.property'
+
+    def set_property_sold(self):
+        for val in self:
+            self.env['account.move'].create({
+                'partner_id': val.buyer.id, 'move_type':'out_invoice',
+                'invoice_line_ids': [Command.create({'name': val.name, 'quantity': 1, 'price_unit': val.selling_price * 0.06})],
+                'invoice_line_ids': [Command.create({'name': 'administrative_fees', 'quantity':1, 'price_unit': 100.00})]
+                })
+        return super().set_property_sold()
