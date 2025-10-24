e0ef55f96d2ecf0e4afb3f5a7834a73877480902
Jugurtha <jugurthagaci24@gmail.com>
2025-10-24 10:22:10 +0200

    [IMP] fix runbot errors

--- estate/models/__init__.py
+++ estate/models/__init__.py
@@ -3,4 +3,3 @@ from . import estate_type
 from . import estate_tag
 from . import estate_offer
 from . import users
-
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -104,7 +104,7 @@ class EstateProperty(models.Model):
         for record in self:
             if record.state in ("new", "received"):
                 return
-            
+
             if float_compare(record.selling_price, (record.expected_price * 0.9), 2) < 0:
                 raise ValidationError("The selling price cannot be lower than 90%% of the expected price")
 
