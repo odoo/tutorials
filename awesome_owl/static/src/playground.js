04076e2ad8d429a70ca0a0810d5758cc821f00a0
Frantisek Binovsky <frbin@odoo.com>
2025-10-24 14:20:24 +0200

    [ADD] awesome_owl: 1.4 markup

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,10 +1,17 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
-import { Counter } from "./counter/counter";
-import { Card } from "./card/card";
+import {Component, markup, useState} from "@odoo/owl";
+import {Counter} from "./counter/counter";
+import {Card} from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
-    static components = { Counter, Card }
+    static components = {Counter, Card}
+
+    setup() {
+        this.state = useState({
+            value: "Awesome text",
+            htmlValue: markup('<h1 class="font-italic">Hello</h1>')
+        })
+    }
 }
