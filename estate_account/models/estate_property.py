3a9b3fa6065d66aa692b6b654389ed1f989674ae
Wajih-Wanis <wajihwanis2000@gmail.com>
2025-10-24 13:53:05 +0200

    [IMP] estate_account: improve style for estate account

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,5 +1,4 @@
-from odoo import models, fields, Command
-from odoo.exceptions import AccessError
+from odoo import models, Command
 
 
 class EstateProperty(models.Model):
@@ -8,8 +7,8 @@ class EstateProperty(models.Model):
     def set_property_sold(self):
         for val in self:
             self.env['account.move'].create({
-                'partner_id': val.buyer.id, 'move_type':'out_invoice',
-                'invoice_line_ids': [Command.create({'name': val.name, 'quantity': 1, 'price_unit': val.selling_price * 0.06})],
-                'invoice_line_ids': [Command.create({'name': 'administrative_fees', 'quantity':1, 'price_unit': 100.00})]
+                'partner_id': val.buyer.id, 'move_type': 'out_invoice',
+                'invoice_line_ids': [Command.create({'name': val.name, 'quantity': 1, 'price_unit': val.selling_price * 0.06}), 
+                                    Command.create({'name': 'administrative_fees', 'quantity': 1, 'price_unit': 100.00})]
                 })
         return super().set_property_sold()
