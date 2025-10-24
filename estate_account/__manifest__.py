09e1f6eb1cb526b4c0c6f41991dab4e6c488428c
yagho-odoo <yagho@odoo.com>
2025-10-24 09:43:25 +0200

    [IMP] estate_account: added a module to link estate and invoicing modules

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "estate sales link",
+    'version': "0.1",
+    'depends': [
+        'base',
+        'estate',
+        'account'
+        ],
+    'descritpion': "A module to link estate sales with invoicing",
+    'author': "yagho",
+    'application': True,
+    'installable': True,
+    'license': "LGPL-3",
+    'data': []
+}
---
+++ estate_account/models/estate_property_child.py
@@ -0,0 +1,36 @@
+from odoo import models, Command
+from odoo.exceptions import UserError
+
+
+class estatePropertyChild(models.Model):
+    _inherit = 'estate.property'
+
+    def _get_default_journal(self):
+        return self.env['account.journal'].search([
+                *self.env['account.journal']._check_company_domain(self.env.company),
+                ('type', '=', 'sale'),
+            ], limit=1)
+
+    def action_sold(self):
+        journal = self._get_default_journal()
+        for record in self:
+            if record['selling_price'] == 0:
+                raise UserError("An offer must be accepted before setting the property as Sold")
+            self.env['account.move'].create([{
+                'journal_id': journal.id,
+                'move_type': 'out_invoice',
+                'partner_id': record.buyer_id.id,
+                "line_ids": [
+                    Command.create({
+                            "name": record['name'],
+                            "quantity": 1,
+                            "price_unit": record['selling_price'] * 0.06,
+                        }),
+                    Command.create({
+                            "name": "Admin fees",
+                            "quantity": 1,
+                            "price_unit": 100.00,
+                        }),
+                ],
+            }])
+        return super().action_sold()
