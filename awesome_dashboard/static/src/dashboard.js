e286ff6c56e55e6fb37cc7b7cc3fd751b03b56cf
Hashem Khaled <hashemkhaled@aucegypt.edu>
2025-10-28 15:53:16 +0100

    [IMP] awesome_dashboard: add layout with some buttons for quick navigation

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -2,9 +2,35 @@
 
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
 
 class AwesomeDashboard extends Component {
+    static components = { Layout };
     static template = "awesome_dashboard.AwesomeDashboard";
+
+    setup() {
+        this.display = { controlPanel: {} };
+        this.action = useService("action");
+    }
+
+    openCustomers() {
+        this.action.doAction("base.action_partner_form");
+    }
+
+    openLeads() {
+        this.action.doAction({
+            type: "ir.actions.act_window",
+            res_model: "crm.lead",
+            name: "Leads",
+            views: [
+                [false, "list"],
+                [false, "form"],
+            ],
+        });
+    }
 }
 
-registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
+registry
+    .category("actions")
+    .add("awesome_dashboard.dashboard", AwesomeDashboard);
