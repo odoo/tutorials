d636571f3e62387b56175ad9653d7e8897f923a5
utpat-odoo <utpat@odoo.com>
2025-10-28 19:18:13 +0530

    [ADD] estate: create views and security folder
    Create module Real Estate
    add menu list
    set column default, read-only

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,6 +2,9 @@
     'name':'Estate',
     'depends': ['base'],
     'license': 'AGPL-3',
-    'data':['security/ir.model.access.csv']
+    'data':[
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
+        ]
 }
-
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import models, fields
+from dateutil.relativedelta import relativedelta
 
 class EstateProperty(models.Model):
     _name = 'estate_property'
@@ -7,13 +8,16 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=fields.Date.today() + relativedelta(months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation=fields.Selection(string='Garden Orientation',selection=[('north', 'North'), ('south', 'South'), ('east','East'), ('west','West')])
+    active=fields.Boolean(default=False)
+    state=fields.Selection(selection=[('new','New'),('offer_received','Offer Received'),('offer_accepted','Offer Accepted'),('sold','Sold'),('cancelled','Cancelled')])
+    
