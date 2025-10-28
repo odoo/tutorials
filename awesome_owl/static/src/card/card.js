8ac0d11defee4306b980694cc58a95a27ab68fc1
Hashem Khaled <hashemkhaled@aucegypt.edu>
2025-10-28 14:25:29 +0100

    [IMP] awesome_owl:  toggle card content

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -1,4 +1,4 @@
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 
 export class Card extends Component {
     static template = "awesome_owl.card";
@@ -11,4 +11,14 @@ export class Card extends Component {
             },
         },
     };
+
+    setup() {
+        this.state = useState({
+            isOpen: true,
+        });
+    }
+
+    onToggle() {
+        this.state.isOpen = !this.state.isOpen;
+    }
 }
