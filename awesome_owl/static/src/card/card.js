83628ac0f8bc564ff14cecdd94f31cd0a64b0ceb
yagho-odoo <yagho@odoo.com>
2025-10-28 17:20:50 +0100

    [IMP] awesome_owl: added slots to props and a card toggle button

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -1,10 +1,21 @@
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 
 export class Card extends Component {
     static template = "awesome_owl.card";
 
     static props = {
         title: String,
-        content: String,
+        slots: {
+            type: Object,
+            optional: true,
+        },
+        isOpen: Boolean,
+    }
+
+    state = useState({ isOpen: this.props.isOpen });
+
+    onToggleOpen(){
+        this.state.isOpen = !this.state.isOpen
+        console.log(this.state.isOpen)
     }
 }
