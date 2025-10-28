f802755656673198aef9236b4c9392b29bd7d867
Victor Decleire <vdecleire@gmail.com>
2025-10-28 16:33:41 +0100

    [IMP] Awesome_owl: improved cards

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -1,10 +1,19 @@
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 
 export class Card extends Component {
     static template = "awesome_owl.card";
 
     static props = {
         title: {type: String},
-        content: {type: String},
+        slots: {type: Object}
     };
+
+    setup() {
+        this.state = useState({open: true})
+    }
+
+    toggleState() {
+        this.state.open = !this.state.open
+        console.log(this.state.open)
+    }
 }
