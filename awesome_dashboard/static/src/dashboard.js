07aa43b128f02ff1da3350ef182a814b44a415d3
Victor Decleire <vdecleire@gmail.com>
2025-10-29 14:26:23 +0100

    [IMP] Awesome_dashboard: Add cards withs statistics

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,10 +1,40 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, onWillStart } from "@odoo/owl";
+import { useService } from "@web/core/utils/hooks";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
+import { DashboardItem } from "./item/dashboard_item";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
+
+    static components = { Layout, DashboardItem };
+
+    setup() {
+        this.action = useService("action");
+        this.statistics = useService("awesome_dashboard.statistics");
+        onWillStart(async () => {
+            this.statistics = await this.statistics.loadStatistics();
+        })
+    }
+
+    async openCustomers() {
+        this.action.doAction({
+            type: 'ir.actions.act_window',
+            res_model: 'res.partner',
+            views: [[false, 'kanban']],
+        });
+    }
+
+    async openLeads() {
+        this.action.doAction({
+            type: 'ir.actions.act_window',
+            res_model: 'crm.lead',
+            name: 'Leads',
+            views: [[false, 'list'], [false, 'form']],
+        });
+    }
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
