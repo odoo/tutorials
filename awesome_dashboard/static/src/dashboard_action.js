38bfa81e7df18acd2839fe7f9d8ed15892a78e3c
maler-odoo <maler@odoo.com>
2025-10-29 10:29:37 +0100

    [ADD] aweome_dashboard module

---
+++ awesome_dashboard/static/src/dashboard_action.js
@@ -0,0 +1,13 @@
+import { registry } from "@web/core/registry"
+import { Component, xml } from "@odoo/owl"
+import { LazyComponent } from "@web/core/assets"
+
+export class DashboardLazyLoader extends Component {
+    static components = { LazyComponent };
+    static template = xml`
+    <LazyComponent bundle="'awesome_dashboard.dashboard'" Component="'AwesomeDashboard'" props="props"/>
+    `;
+
+}
+
+registry.category("actions").add("awesome_dashboard.dashboard", DashboardLazyLoader)
