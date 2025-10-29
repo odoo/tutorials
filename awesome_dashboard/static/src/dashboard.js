0aa3756bfef26bf80fb7dd89fa5dfeac0330cd39
yagho-odoo <yagho@odoo.com>
2025-10-29 11:01:40 +0100

    [ADD] awesome_dashboard: added quick nav buttons and a dashboard item component

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -2,9 +2,40 @@
 
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
+import { DashboardItem } from "./dashboard_item/dashboard_item";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
+    static components = {
+        Layout,
+        DashboardItem
+    };
+
+    static props = {
+        display : {
+            controlPanel: {}
+        },
+    }
+
+    setup() {
+      this.action = useService("action");
+    }
+
+    openCustomerKanban() {
+        this.action.doAction("base.action_partner_form");
+    }
+
+    async openActivity() {
+        this.action.doAction({
+            type: 'ir.actions.act_window',
+            name: 'Leads',
+            target: 'current',
+            res_model: 'crm.lead',
+            views: [[false, 'list'], [false, 'form']],
+        });
+    }
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
