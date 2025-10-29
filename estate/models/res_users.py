804d83a4517493a9d7c728426441110b941f5093
alan-odoo <alan@odoo.com>
2025-10-29 11:13:38 +0100

    Oh what a trap

--- estate/models/res_users.py
+++ estate/models/res_users.py
@@ -4,4 +4,4 @@ from odoo import fields, models
 class ResUsers(models.Model):
     _inherit = "res.users"
 
-    property_ids = fields.One2many("estate.property", "salesman", domain=['|', ('state', '=', 'New'), ('state', '=', 'Offer Received')])
+    property_ids = fields.One2many("estate.property", "salesman", domain=[('state', 'in', ['New', 'Offer Received'])])
