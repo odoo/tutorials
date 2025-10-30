17de6432be4d98f70b2cbeeff7a9f454193a61b8
pimai-odoo <pimai@odoo.com>
2025-10-30 17:11:14 +0530

    [IMP] estate: added property type,tags,buyer,salesperson & offers in Real Estate
    Added Property Type and Tag models, buyer and salesperson fields,
    and the Property Offer model in the Real Estate module.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,6 +4,12 @@
     'data': [
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
-        'views/estate_menus.xml'
-    ]
+        'views/estate_menus.xml',
+        'views/estate_property_type_views.xml',
+        'views/estate_property_tag_views.xml',
+        'views/estate_property_offer_views.xml'
+    ],
+    'application': True,
+    'installable':True
 }
+
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -2,22 +2,22 @@ from odoo import models, fields
 from dateutil.relativedelta import relativedelta
 
 
-class estateproperty(models.Model):
+class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real Estate Property"
 
     name = fields.Char(required=True)
-    description = fields.Text("Description")
-    postcode = fields.Char("Postcode")
-    date_availability = fields.Date("Availability Date", default=fields.Date.today()+relativedelta(months=3))
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date("Availability Date", default=fields.Date.today() + relativedelta(months=3))
     expected_price = fields.Float("Expected Price", required=True)
     selling_price = fields.Float("Selling Price", readonly=True)
-    bedrooms = fields.Integer("Bedrooms", default=2)
-    living_area = fields.Integer("Living Area (sqm)")
-    facades = fields.Integer("Facades")
-    garage = fields.Boolean("Garage")
-    garden = fields.Boolean("Garden")
-    garden_area = fields.Integer("Garden Area (sqm)")
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer("Living Area(sqm)")
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer("Garden Area(sqm)")
     garden_orientation = fields.Selection(
         selection=[
             ("north", "North"),
@@ -40,6 +40,10 @@ class estateproperty(models.Model):
         copy=False,
         default="new",
     )
-    active = fields.Boolean(string="Active", default=True)
-    
-
+    active = fields.Boolean(default=True)
+    property_type_id = fields.Many2one("estate.property.type", string="Property Type")
+    buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
+    salesperson_id = fields.Many2one(
+        "res.users", string="Salesperson")
+    tag_ids = fields.Many2many("estate.property.tag", string="Tags")
+    offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
