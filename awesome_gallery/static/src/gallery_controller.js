8c0f63b13f9d92ebed395248565f8eebb0f84d0d
Jeanne Delneste <jedel@odoo.com>
2025-10-30 13:04:33 +0100

    [IMP] awesome_gallery: first steps

---
+++ awesome_gallery/static/src/gallery_controller.js
@@ -0,0 +1,41 @@
+import { Component, onWillStart, onWillUpdateProps, useState } from "@odoo/owl";
+import { standardViewProps } from "@web/views/standard_view_props";
+import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
+
+export class GalleryController extends Component {
+    static template = "awesome_gallery.GalleryController";
+    static props = {
+        ...standardViewProps,
+        archInfo: Object,
+    };
+    static components = { Layout };
+
+    setup() {
+        this.orm = useService("orm");
+        this.images = useState({ data: [] });
+        onWillStart(async () => {
+            const { records } = await this.loadImages(this.props.domain);
+            this.images.data = records;
+        });
+
+        onWillUpdateProps(async (nextProps) => {
+            if (JSON.stringify(nextProps.domain) !== JSON.stringify(this.props.domain)) {
+                const { records } = await this.loadImages(nextProps.domain);
+                this.images.data = records;
+            }
+        })
+    }
+
+    loadImages(domain) {
+        return this.orm.webSearchRead(this.props.resModel, domain, {
+            limit: this.props.archInfo.limit,
+            specification: {
+                [this.props.archInfo.imageField]: {},
+            },
+            context: {
+                bin_size: true,
+            }
+        });
+    }
+}
--- awesome_gallery/static/src/gallery_view.js
+++ awesome_gallery/static/src/gallery_view.js
@@ -1,3 +1,27 @@
 /** @odoo-module */
 
-// TODO: Begin here!
+import { registry } from "@web/core/registry"
+import { GalleryController } from "./gallery_controller";
+import { GalleryArchParser } from "./gallery_arch_parser";
+
+export const galleryView = {
+    Type: "gallery",
+    display_name: "Awesome Gallery",
+    icon: "fa fa-picture-o",
+    multiRecord: true,
+    Controller: GalleryController,
+    ArchParser: GalleryArchParser,
+
+    props(genericProps, view) {
+        const { ArchParser } = view;
+        const { arch } = genericProps;
+        const archInfo = new ArchParser().parse(arch);
+
+        return {
+            ...genericProps,
+            archInfo,
+        };
+    },
+};
+
+registry.category("views").add("gallery", galleryView);
