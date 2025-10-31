5d9b7638003c7b5cc0fa8f74d3a22cf908f052ff
hazei-odoo <hazei@odoo.com>
2025-10-31 09:47:24 +0100

    [ADD] Awesome_owl

--- awesome_dashboard/__manifest__.py
+++ awesome_dashboard/__manifest__.py
@@ -24,7 +24,7 @@
     'assets': {
         'web.assets_backend': [
             'awesome_dashboard/static/src/**/*',
-            ('remove','awesome_dashboard/static/src/dashboard/**/*')
+            ('remove',' awesome_dashboard/static/src/dashboard/**/*')
         ],
         'awesome_dashboard.dashboard': [
             'awesome_dashboard/static/src/dashboard/**/*',
