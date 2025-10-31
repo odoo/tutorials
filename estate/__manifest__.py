601b9a6651708e10bb86701b2aa9858508ed258d
qucol-odoo <qucol@odoo.com>
2025-10-31 14:02:40 +0100

    [IMP] estate: Define module data

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,6 +6,7 @@
     'data': [
         'security/ir.model.access.csv',
         'data/ir_config_parameter_data.xml',
+        'data/estate_types.xml',
         'views/estate_property_views.xml',
         'views/estate_property_offer_views.xml',
         'views/estate_property_type_views.xml',
@@ -13,6 +14,10 @@
         'views/estate_res_user_views.xml',
         'views/res_config_settings_views.xml',
         'views/estate_menu_views.xml'
+    ],
+    'demo': [
+        'demo/demo_property_data.xml',
+        'demo/demo_offer_data.xml'
     ],
     'application': True,
     'author': 'Odoo S.A.',
