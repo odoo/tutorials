7cc8a96247278abd259322a8fa8be7b4d995c7fe
Jeanne Delneste <jedel@odoo.com>
2025-11-03 10:32:34 +0100

    [IMP] estate: add demo data

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,6 +3,7 @@
     'depends': ['base'],
     'application': True,
     'author': "Jeanne Delneste",
+    'category': "Real Estate/Brokerage",
     'license': "LGPL-3",
     'data': [
         'views/estate_property_views.xml',
@@ -11,6 +12,13 @@
         'views/estate_property_type_views.xml',
         'views/estate_property_tag_views.xml',
         'views/estate_menus.xml',
-        'security/ir.model.access.csv'
+        'data/estate.property.type.csv',
+        'security/ir.model.access.csv',
+        # 'security/security.xml'
     ],
+    'demo': [
+        'demo/estate_property_demo.xml',
+        'demo/estate_property_offer_demo.xml',
+        'demo/estate_property_type_demo.xml',
+    ]
 }
