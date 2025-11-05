8c2f328ac927f242e70d5cc06e2ee127ac2fac61
hapat-odoo <hapat@odoo.com>
2025-11-05 15:14:49 +0530

    [IMP] estate: Added menu structure and property action
    Introduced a new menu structure for the estate module, including a root menu and submenus for properties. Also added an action for managing estate properties with a corresponding view mode.

--- Estate/__manifest__.py
+++ Estate/__manifest__.py
@@ -1,13 +1,14 @@
 {
-    'name' :'estate',
-    'depends':['base'],
+    'name': 'estate',
+    'depends': ['base'],
     'application': True,
     'installable': True,
-    'author':'estate',
+    'author': 'estate',
     'category': 'Tutorials',
     'license': 'AGPL-3',
     'data': [
-        'security/ir.model.access.csv'
-        'views/estate_property_views.xml'
-    ]
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+    ],
 }
