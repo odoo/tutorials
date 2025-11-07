0001158adbdcd6e632a4b1b01727ab44b6574695
haahi-odoo <haahi@odoo.com>
2025-11-07 17:42:44 +0530

    [IMP] estate: Enhanced Data Models and Functionalities
    -Introduced Property Type and Property Tag models.
    -Added Buyer and Salesperson fields.
    -Implemented the Property Offer model.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -7,34 +7,43 @@ class EstateProperty(models.Model):
     _description = "Real Estate Property"
 
     name = fields.Char(required=True)
-    description = fields.Text("Description")
-    postcode = fields.Char("Postcode", required=True)
-    date_availability = fields.Date("Availability Date", default=fields.Date.today()+relativedelta(months=3))
-    expected_price = fields.Float(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date("Availability Date", default=fields.Date.today() + relativedelta(months=3))
+    expected_price = fields.Float("Expected Price", required=True)
     selling_price = fields.Float("Selling Price", readonly=True)
-    bedrooms = fields.Integer("Bedrooms", default=2)
-    living_area = fields.Integer("living_area(sqm)")
-    facades = fields.Integer("Facades")
-    garage = fields.Boolean("Garage")
-    garden = fields.Boolean("Garden")
-    garden_area = fields.Integer("Garden Area sqm")
-    garden_orientation = fields.Selection([
-        ('north', 'North'),
-        ('south', 'South'),
-        ('east', 'East'),
-        ('west', 'West'),
-    ])
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer("Living Area(sqm)")
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer("Garden Area(sqm)")
+    garden_orientation = fields.Selection(
+        selection=[
+            ("north", "North"),
+            ("south", "South"),
+            ("east", "East"),
+            ("west", "West"),
+        ],
+        string="Garden Orientation",
+    )
     state = fields.Selection(
         [
-            ('new', 'New'),
-            ('offer_received', 'Offer Received'),
-            ('offer_accepted', 'Offer Accepted'),
-            ('sold', 'Sold'),
-            ('canceled', 'Canceled'),
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled"),
         ],
         string="Status",
         required=True,
         copy=False,
-        default='new'
+        default="new",
     )
-    
+    active = fields.Boolean(default=True)
+    property_type_id = fields.Many2one("estate.property.type", string="Property Type")
+    buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
+    salesperson_id = fields.Many2one(
+        "res.users", string="Salesperson")
+    tag_ids = fields.Many2many("estate.property.tag", string="Tags")
+    offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
