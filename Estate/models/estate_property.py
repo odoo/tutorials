c9b39135a8f2583c03658ca728ce730fdaeb306b
hapat-odoo <hapat@odoo.com>
2025-11-10 00:00:39 +0530

    [IMP] estate: Implemented models and views for property offers, tags, and types
    Implemented models and views for property offers
    tags and types

--- Estate/models/estate_property.py
+++ Estate/models/estate_property.py
@@ -37,3 +37,9 @@ class EstateProperty(models.Model):
         copy=False,
         default='new'
     )
+    active = fields.Boolean(default=True)
+    property_type_id = fields.Many2one("estate.property.type", string="Property Type")        
+    buyer_id = fields.Many2one("res.partner", string="Buyer")
+    salesperson_id = fields.Many2one("res.users", string="Salesperson")
+    tag_ids = fields.Many2many("estate.property.tag", string="Tags")
+    offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
