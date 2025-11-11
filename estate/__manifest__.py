d39b68145df84c6f1fc15a04e59148dbd57b301d
danal-odoo <danal@odoo.com>
2025-11-11 13:03:11 +0530

    [ADD] estate: created estate application and important files.
    added estate module.
    created __init__.py and __manifest__.py files.

---
+++ estate/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Darshil Naliyapara",
+    'category': 'Category',
+    'application': True,
+    'description': """
+    Description text
+    """,
+    # data files always loaded at installation
+    'data': [
+        'views/mymodule_view.xml',
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+        'demo/demo_data.xml',
+    ],
+}
