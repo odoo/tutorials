adec7555072065ea3b40fe127d79f2d1d8fc2b0e
pimai-odoo <pimai@odoo.com>
2025-11-11 18:44:17 +0530

    [IMP] estate: added invoice lines for sold properties and Kanban view
    Added two invoice lines during invoice creation for sold properties:
    one for 6% of the selling price and another for administrative fees of 100.00.
    Added a Kanban view to improve property visualization in the Real Estate module.

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import models, Command
 
 
 class EstateProperty(models.Model):
@@ -7,5 +7,22 @@ class EstateProperty(models.Model):
     def action_sold(self):
         self.env['account.move'].create({
             'partner_id': self.buyer_id.id,
-            'move_type': 'out_invoice'})
+            'move_type': 'out_invoice',
+            'invoice_line_ids': [
+                    Command.create(
+                        {
+                            "name": "6% of the selling price",
+                            "quantity": 1,
+                            "price_unit": self.selling_price * 0.06 ,
+                        }
+                    ),
+                    Command.create(
+                        {
+                            "name": "An additional 100 from administrative fees",
+                            "quantity": 1,
+                            "price_unit": 100,
+                        }
+                    )
+                ],
+            })
         return super().action_sold()
