c1b9539dcb552f1c8e73776143110145cfab6131
danal-odoo <danal@odoo.com>
2025-11-11 14:32:31 +0530

    [IMP] estate: created models for estate application.
    created estate property model.
    added all required fields in model.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -10,10 +10,10 @@
     """,
     # data files always loaded at installation
     'data': [
-        'views/mymodule_view.xml',
+        # 'views/mymodule_view.xml',
     ],
     # data files containing optionally loaded demonstration data
     'demo': [
-        'demo/demo_data.xml',
+        # 'demo/demo_data.xml',/
     ],
 }
