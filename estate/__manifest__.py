eb9e8d3fca6322bc75cf4c1b3cec8c94a19793ad
danal-odoo <danal@odoo.com>
2025-11-11 15:00:41 +0530

    [IMP] estate: implimented security for model access.
    added security CSV file in security folder.
    added estate_property model access to user in csv file.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,18 +2,15 @@
     'name': "Real Estate",
     'version': '1.0',
     'depends': ['base'],
-    'author': "Darshil Naliyapara",
+    'author': "danal",
     'category': 'Category',
     'application': True,
+    'license': 'LGPL-3',
     'description': """
     Description text
     """,
-    # data files always loaded at installation
     'data': [
-        # 'views/mymodule_view.xml',
-    ],
-    # data files containing optionally loaded demonstration data
-    'demo': [
-        # 'demo/demo_data.xml',/
+        'views/estate_view.xml',
+        'security/ir.model.access.csv'
     ],
 }
