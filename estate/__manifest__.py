748a2b7c39760ae44f2bf5bd4c62170de1d0cad7
tskv <taskv@odoo.com>
2025-11-18 14:05:20 +0100

    [ADD] estate:taskv

---
+++ estate/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "taskv",
+    'category': 'Category',
+    'description': """
+    Description text
+    """,
+    'data': [
+    ],
+    'demo': [
+    ],
+}
