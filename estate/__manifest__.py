98643d9e31ebe033a58deb227608cd2897f7dd7a
roole-odoo <roole@odoo.com>
2025-11-18 13:23:07 +0100

    [ADD] estate: Init the module

---
+++ estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "My Awesome module",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "roole",
+    'application': True,
+    'installable': True,
+    'category': 'Tutorials',
+    'description': """
+    Description text
+    """,
+}
