537def18fdf7602c9b082acfeffe206db45f8885
youness benbraitit (yoben) <yoben@odoo.com>
2025-11-18 13:39:55 +0100

    [ADD] estate: init commit for estate module

---
+++ estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Real Estate',
+    'description': 'Real Estate !',
+    'version': '1.0',
+    'summary': 'Track Real Estate',
+    'website': 'https://www.odoo.com/app/realestate',
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+    'depends': [
+        'base',
+    ]
+}
