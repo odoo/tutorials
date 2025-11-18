8d8136954477a6787da848897c9e69de105dcb11
roole-odoo <roole@odoo.com>
2025-11-18 17:05:28 +0100

    [IMP] estate : Chapter 5
    - an action have been loaded in the system.
    - Property view created.
    - view Menu added.
    - Extra attributes have been added.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -11,6 +11,8 @@
     """,
     'license': 'GPL-3',
     'data': [
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
     ]
 }
--- estate/models/estate_model.py
+++ estate/models/estate_model.py
@@ -1,3 +1,5 @@
+import datetime
+
 from odoo import fields, models
 
 
@@ -8,10 +10,10 @@ class EstateModel(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False,default=datetime.datetime.now() + datetime.timedelta(days=90))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True,copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -20,3 +22,12 @@ class EstateModel(models.Model):
     garden_orientation = fields.Selection(
         string='Orientation',
         selection=[('north', 'North'), ('south', 'South'), ('west', 'West'), ('east', 'East')])
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        string="Estate status",
+        selection=[('new', 'New'), ('offer received', 'Offer Received'), ('offer accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')],
+        help='This field explain the estate status.',
+        required=True,
+        copy=False,
+        default='new'
+    )
