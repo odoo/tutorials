de4a3154eb070288ea84ace4b508b1eb90423151
haall-odoo <haall@odoo.com>
2025-11-18 17:03:55 +0100

    [IMP] estate: Chapter 5

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,3 +1,5 @@
+import datetime as dt
+
 from odoo import fields, models
 
 
@@ -8,10 +10,10 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=dt.datetime.today() + dt.timedelta(days=90))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -21,4 +23,13 @@ class EstateProperty(models.Model):
         string='Orientation',
         selection=[('north', 'North'), ('west', 'West'), ('south', 'South'), ('east', 'East')],
         help="Choose the appropriate orientation of the garden"
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        string="Estate status",
+        selection=[('new', 'New'), ('offer received', 'Offer Received'), ('offer accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')],
+        help='This field explain the estate status.',
+        required=True,
+        copy=False,
+        default='new'
     )
