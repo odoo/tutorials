3458fe5df828cc6060fc0bc99184f634c626cef3
djhan <djhan@odoo.com>
2025-11-18 14:12:02 +0100

    [ADD]  estate: init

---
+++ estate/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Estate',
+    'version': '1.0',
+    'category': 'Sales/Estate',
+    'sequence': 15,
+    'summary': 'Track all the properties you own',
+    'website': 'https://www.odoo.com/app/estate',
+    'depends': [
+        'base_setup',
+        'sales_team',
+        'mail',
+        'calendar',
+        'resource',
+        'utm',
+        'web_tour',
+        'contacts',
+        'digest',
+        'phone_validation',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+    'assets': {
+    },
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+}
