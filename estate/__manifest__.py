052766fcd76fd2d42bfb067b06df2626a4ac92ce
abshe <abdo.3102003@gmail.com>
2025-11-18 17:32:03 +0100

    [ADD] estate: added views

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,5 +3,11 @@
     'depends': ['base'],
     'application': True,
     'installable': True,
-    'data': ['security/ir.model.access.csv']
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_menus.xml',
+        'views/estate_property_views.xml'
+    ],
+    'license': 'LGPL-3',
+    'author': 'Abdallah'
 }
