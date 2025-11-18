c408da572cd2328531ed38c9f2fa264bca0c89c6
haall-odoo <haall@odoo.com>
2025-11-18 13:36:55 +0100

    [ADD] Estate: init

---
+++ estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Estate',
+    'version': '0.0',
+    'depends': ['base'],
+    'author': 'haall-odoo',
+    'application': True,
+    'installable': True,
+    'category': '',
+    'description': ''
+}
