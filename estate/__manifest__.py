51a3f8830a0a5e03de20bc4591944a0bdd3d17de
ngtpn <ngtpn@odoo.com>
2025-11-18 10:14:17 +0100

    [ADD] estate: Add module

---
+++ estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "ngtpn",
+    'category': 'Category',
+    'description': """
+    Real Estate Advertisement module
+    """,
+}
