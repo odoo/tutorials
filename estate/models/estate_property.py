26010220467d91b7b36e7b9aa28b4e7f896e9665
abshe <abdo.3102003@gmail.com>
2025-11-19 14:41:25 +0100

    [IMP] estate: CH6 added list,form,search views

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,18 +5,41 @@ class Property(models.Model):
     _name = "estate.property"
     _description = "Estate Property Info"
 
-    name = fields.Char(required=True, translate=True)
+    name = fields.Char(required=True, translate=True, string="Title")
     description = fields.Text(translate=True)
     postcode = fields.Char()
-    date_availability = fields.Date(copy=False, default=fields.Datetime.add(fields.Datetime.today(), months=3))
+    date_availability = fields.Date(
+        copy=False,
+        default=fields.Datetime.add(fields.Datetime.today(), months=3),
+        string="Available From",
+    )
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
+    living_area = fields.Integer(string="Living Area(sqm)")
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Integer()
-    garden_orientation = fields.Selection(string='Garden_Orientation', selection=[('north', 'North'), ('east', 'East'), ('west', 'West'), ('south', 'South')])
+    garden_area = fields.Integer(string="Garden Area(sqm)")
+    garden_orientation = fields.Selection(
+        string="Garden Orientation",
+        selection=[
+            ("north", "North"),
+            ("east", "East"),
+            ("west", "West"),
+            ("south", "South"),
+        ],
+    )
     active = fields.Boolean(default=True)
-    status = fields.Selection(required=True, copy=False, default='new', selection=[('new', 'New'), ('offer', 'Offer'), ('recieved', 'Recieved'), ('accepted', 'Accepted'), ('sold', 'Sold')])
+    status = fields.Selection(
+        required=True,
+        copy=False,
+        default="new",
+        selection=[
+            ("new", "New"),
+            ("offer_recieved", "Offer Recieved"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled"),
+        ],
+    )
