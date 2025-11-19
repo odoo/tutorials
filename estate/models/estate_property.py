b87b0a4da91160cc56c23f9ee76dae93733f02d1
tskv <taskv@odoo.com>
2025-11-19 12:53:21 +0100

    [IMP] estate: chapter 7 (relational fields)

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -34,3 +34,10 @@ class EstateProperty(models.Model):
                                     ('sold', 'Sold'),
                                     ('cancelled', 'Cancelled')],
                              default='new', required=True, copy=False)
+    property_type_id = fields.Many2one('estate.property.type', string='Property Type')
+    property_tag_ids = fields.Many2many('estate.property.tag', string='Property Tag')
+    buyer_id = fields.Many2one('res.partner', string='Buyer', copy=False,
+                               domain=[('is_company', '=', False)])
+    salesperson_id = fields.Many2one('res.users', string='Salesperson',
+                                     default=lambda self: self.env.user)
+    offer_ids = fields.Many2many('estate.property.offer', 'property_id', string='Offers')
