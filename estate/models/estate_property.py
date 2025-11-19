afec0e3fda7ee7f452634458df6bb2141f3ea82e
tskv <taskv@odoo.com>
2025-11-19 10:20:40 +0100

    [IMP] estate: chapter 6 (views)

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -8,19 +8,19 @@ class EstateProperty(models.Model):
     _name = 'estate.property'
     _description = 'Estate Property'
 
-    name = fields.Char('Estate Property', required=True, default='Unknown', translate='True')
+    name = fields.Char('Title', required=True, default='Unknown', translate='True')
     active = fields.Boolean('Active', default=True)
     description = fields.Text('Description')
     postcode = fields.Char('Postcode')
-    date_availability = fields.Date('Date Availability', copy=False, default=fields.Datetime.now() + timedelta(days=90))
+    date_availability = fields.Date('Available From', copy=False, default=fields.Datetime.now() + timedelta(days=90))
     expected_price = fields.Float('Expected Price', required=True)
     selling_price = fields.Float('Selling Price', readonly=True, copy=False)
     bedrooms = fields.Integer('Bedrooms', default=2)
-    living_area = fields.Integer('Living Area')
+    living_area = fields.Integer('Living Area (sqm)')
     facades = fields.Integer('Facades')
     garage = fields.Boolean('Garage')
     garden = fields.Boolean('Garden')
-    garden_area = fields.Integer('Garden Area')
+    garden_area = fields.Integer('Garden Area (sqm)')
     garden_orientation = fields.Selection(string='Garden orientation',
                                           selection=[('north', 'North'),
                                                     ('south', 'South'),
