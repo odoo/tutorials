2b746610ca1945d01066da19e370c133f10ec490
djhan <djhan@odoo.com>
2025-11-19 11:37:39 +0100

    [IMP] estate: add UI using default views

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -11,7 +11,9 @@
         'base'
     ],
     'data': [
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
     ],
     'demo': [
     ],
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -7,15 +7,22 @@ class EstateProperty(models.Model):
     name = fields.Char('Property Name', required=True, translate=True)
     description = fields.Text('Property Description', required=True)
     postcode = fields.Char('Property postcode')
-    date_availability = fields.Date('Property availability date')
+    date_availability = fields.Date('Property availability date', copy=False, default=fields.Date.add(fields.Date.today(), months=3))
     expected_price = fields.Float('Property expected price')
-    selling_price = fields.Float('Property selling price')
-    bedrooms = fields.Integer()
+    selling_price = fields.Float('Property selling price', readonly=True)
+    bedrooms = fields.Integer(default=2)
+    active = fields.Boolean(default=True)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
-        string='Type',
+        string='Garden Orientation',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
+    state = fields.Selection(
+        string='State',
+        required=True,
+        default='new',
+        copy=False,
+        selection=[('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')])
