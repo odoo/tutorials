109e9a39e40dcd04d8ec2a391f90f7315beea44a
abshe <abdo.3102003@gmail.com>
2025-11-19 11:03:00 +0100

    [ADD] estate: CH5 added extra fields

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,13 +1,13 @@
 {
     'name': "Real estate",
-    'depends': ['base'],
+    'depends': ['base',],
     'application': True,
     'installable': True,
     'data': [
         'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
         'views/estate_menus.xml',
-        'views/estate_property_views.xml'
     ],
     'license': 'LGPL-3',
-    'author': 'Abdallah'
+    'author': 'Abdallah',
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -3,18 +3,20 @@ from odoo import fields, models
 
 class Property(models.Model):
     _name = "estate.property"
-    _description = "estate property info"
+    _description = "Estate Property Info"
 
     name = fields.Char(required=True, translate=True)
     description = fields.Text(translate=True)
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=fields.Datetime.add(fields.Datetime.today(), months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
-    garden_orientation = fields.Selection(string='garden_orientation', selection=[('North', 'north'), ('east', 'East'), ('West', 'west'), ('South', 'south')])
+    garden_orientation = fields.Selection(string='garden_orientation', selection=[('north', 'North'), ('east', 'East'), ('west', 'West'), ('south', 'South')])
+    active = fields.Boolean(default=True)
+    status = fields.Selection(required=True, copy=False, default='new', selection=[('new', 'New'), ('offer', 'Offer'), ('recieved', 'Recieved'), ('accepted', 'Accepted'), ('sold', 'Sold')])
