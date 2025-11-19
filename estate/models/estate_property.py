ae446452ac1361f760794af8ff7820c8a92902bf
tskv <taskv@odoo.com>
2025-11-19 14:14:48 +0100

    [IMP] estate: chapter 8 (compute and onchange)

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,6 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import fields, models, api
 from datetime import timedelta
 
 
@@ -41,3 +41,27 @@ class EstateProperty(models.Model):
     salesperson_id = fields.Many2one('res.users', string='Salesperson',
                                      default=lambda self: self.env.user)
     offer_ids = fields.Many2many('estate.property.offer', 'property_id', string='Offers')
+    total_area = fields.Integer('Total Area (sqm)', compute='_compute_total_area')
+    best_offer = fields.Float('Best Offer', compute='_compute_best_offer')
+
+    @api.depends('garden_area', 'living_area')
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.garden_area + record.living_area
+
+    @api.depends('offer_ids.price')
+    def _compute_best_offer(self):
+        for record in self:
+            if record.offer_ids:
+                record.best_offer = max(record.offer_ids.mapped('price'))
+            else:
+                record.best_offer = 0
+
+    @api.onchange('garden')
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = 'north'
+        else:
+            self.garden_area = 0
+            self.garden_orientation = ''
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,15 +1,26 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import fields, models, api
+from datetime import date, timedelta
 
 
 class EstatePropertyOffer(models.Model):
     _name = 'estate.property.offer'
     _description = 'Estate Property Offer'
 
-
     price = fields.Float(string='Price')
     status = fields.Selection(string='Status', copy=False,
                             selection=[('accepted', 'Accepted'), ('refused', 'Refused')])
     partner_id = fields.Many2one('res.partner', string='Customer', required=True)
     property_id = fields.Many2one('estate.property', string='Property', required=True)
+    validity = fields.Integer(string='Validity (days)', default=7)
+    date_deadline = fields.Date(string='Deadline', compute='_compute_deadline', inverse='_inverse_deadline')
+
+    @api.depends('validity')
+    def _compute_deadline(self):
+        for record in self:
+            record.date_deadline = (record.create_date if record.create_date else date.today()) + timedelta(days=record.validity)
+
+    def _inverse_deadline(self):
+        for record in self:
+            record.validity = (record.date_deadline - (record.create_date.date() if record.create_date else date.today())).days
