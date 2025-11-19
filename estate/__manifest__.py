e66dd36760a40db0c3f5b66cdb9cb7a8fea1c2f8
haall-odoo <haall@odoo.com>
2025-11-19 13:21:07 +0100

    [FIX] estate: review 1

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,16 +1,17 @@
 {
     'name': 'Estate',
-    'version': '0.0',
+    'version': '1.0',
     'depends': ['base'],
-    'author': 'haall-odoo',
+    'author': 'Odoo S.A.',
     'application': True,
     'installable': True,
     'category': '',
     'description': '',
-    'license': 'GPL-3',
+    'license': 'LGPL-3',
     'data': [
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
+        'views/estate_property_type_views.xml',
         'views/estate_menus.xml'
     ]
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,12 +5,12 @@ from odoo import fields, models
 
 class EstateProperty(models.Model):
     _name = "estate.property"
-    _description = "An awesome estate module"
+    _description = "Estate Property"
 
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(copy=False, default=dt.datetime.today() + dt.timedelta(days=90))
+    date_availability = fields.Date(copy=False, default=fields.Date.add(fields.Date.today(), months=3), string="Available From")
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
@@ -21,15 +21,23 @@ class EstateProperty(models.Model):
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
         string='Orientation',
-        selection=[('north', 'North'), ('west', 'West'), ('south', 'South'), ('east', 'East')],
+        selection=[
+                ('north', 'North'),
+                ('west', 'West'),
+                ('south', 'South'),
+                ('east', 'East')
+            ],
         help="Choose the appropriate orientation of the garden"
     )
     active = fields.Boolean(default=True)
     state = fields.Selection(
         string="Estate status",
-        selection=[('new', 'New'), ('offer received', 'Offer Received'), ('offer accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')],
+        selection=[('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')],
         help='This field explain the estate status.',
         required=True,
         copy=False,
         default='new'
     )
+    property_type_id = fields.Many2one("estate.property.type", string="Type")
+    seller_id = fields.Many2one("res.users", string="Salesman", default=lambda self: self.env.user,  domain="[('type', '=', 'internal')]")
+    buyer_id = fields.Many2one("res.partner", string="Buyer", domain="[('type', '=', 'portal')]")
