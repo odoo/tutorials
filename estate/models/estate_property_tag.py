d76827d9ad91a31798ca73a8444090861517c7c0
haall-odoo <haall@odoo.com>
2025-11-20 14:07:40 +0100

    [FIX] estate: PEP8 correction estate_property_tag.py

--- estate/models/estate_property_tag.py
+++ estate/models/estate_property_tag.py
@@ -4,7 +4,5 @@ from odoo import fields, models
 class EstatePropertyTag(models.Model):
     _name = "estate.property.tag"
     _description = "Estate Property Tag"
-    
-
 
     name = fields.Char(required=True)
