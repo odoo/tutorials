30a5051491a70404f5dc4c6903ff2b324b601806
ngtpn <ngtpn@odoo.com>
2025-11-20 16:19:08 +0100

    [IMP] estate_account: Create account move on sold properties

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,8 +1,24 @@
-from odoo import models
+from odoo import models, Command
 
 
 class EstateProperty(models.Model):
     _inherit = 'estate.property'
 
     def action_mark_as_sold(self):
+        self.env['account.move'].create({
+            'partner_id': self.buyer_id,
+            'move_type': 'out_invoice',
+            'line_ids': [
+                Command.create({
+                    "name": self.name,
+                    "quantity": 1,
+                    "price_unit": 0.06 * self.selling_price,
+                }),
+                Command.create({
+                    "name": "Administrative fees",
+                    "quantity": 1,
+                    "price_unit": 100.00,
+                }),
+            ]
+        })
         return super().action_mark_as_sold()
