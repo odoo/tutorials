6ffae9b03f432a4f3e6dcd349d05f7e00beef49d
ngtpn <ngtpn@odoo.com>
2025-11-20 15:38:06 +0100

    [ADD] estate_account: Init

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Estate Accounting',
+    'license': 'LGPL-3',
+    'version': '1.0',
+    'depends': ['estate', 'account'],
+    'author': 'Odoo S.A.',
+    'category': 'Category',
+    'description': """
+    Real Estate Accounting module
+    """,
+    'application': True,
+    'data': [
+    ],
+}
