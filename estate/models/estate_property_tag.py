cd5b754b92ccb13481cd49d34109c2a1c6e93b80
haall-odoo <haall@odoo.com>
2025-11-20 17:05:10 +0100

    [IMP] estate: chapter 11 color picker part

--- estate/models/estate_property_tag.py
+++ estate/models/estate_property_tag.py
@@ -7,3 +7,4 @@ class EstatePropertyTag(models.Model):
     _order = "name"
 
     name = fields.Char(required=True)
+    color = fields.Integer()
--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -4,7 +4,8 @@ from odoo import fields, models
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
     _description = "Estate Property Types"
-    _order = "name"
+    _order = "sequence, name"
 
     name = fields.Char(required=True)
     property_ids = fields.One2many("estate.property", "property_type_id")
+    sequence = fields.Integer('Sequence', default=1, help="Used to order estate property types.")
