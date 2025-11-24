32d13f95f99afda6ec7c4dcfa754e8a5e8b0bf06
Nguyen (ngtpn) <ngtpn@odoo.com>
2025-11-24 16:34:34 +0100

    [IMP] awesome_owl: Card slot / toggle

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -1,10 +1,19 @@
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 
 class Card extends Component {
     static template = "awesome_owl.card"
     static props = {
         title: {type: String},
         content: {type: String},
+        slots: {type: Object, optional: true}
+    }
+
+    setup() {
+        this.state = useState({ open: true });
+    }
+
+    toggleOpen() {
+        this.state.open = !this.state.open;
     }
 }
 
