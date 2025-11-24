527af1fbeadc8df21741f727f5f4ab38e2249177
tskv <taskv@odoo.com>
2025-11-24 10:10:25 +0100

    [ADD] awsome_owl: task 3 (card)

---
+++ awesome_owl/static/src/card/card.js
@@ -0,0 +1,11 @@
+import { useState, Component } from "@odoo/owl";
+
+export class Card extends Component {
+    static template = "awesome_owl.Card";
+
+    static props = {
+        title : {type: String},
+        content : {type: String}
+    }
+
+}
--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,9 +1,10 @@
 import { Component, useState } from "@odoo/owl";
 import { Counter } from "./counter/counter"
+import { Card } from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.Playground";
-    static components = {Counter};
+    static components = { Counter, Card };
 
     setup() {
     }
