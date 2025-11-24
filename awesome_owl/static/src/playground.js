fceec125d2892e49892f123e6329128bac8afacf
tskv <taskv@odoo.com>
2025-11-24 10:40:48 +0100

    [IMP] awsome_owl: task 4 (markup)

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,10 +1,13 @@
-import { Component, useState } from "@odoo/owl";
+import { Component, useState, markup } from "@odoo/owl";
 import { Counter } from "./counter/counter"
 import { Card } from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.Playground";
     static components = { Counter, Card };
+
+    value1 = "<div class='text-primary'>test</div>";
+    value2 = markup("<a href='odoo.com'>test2</a>");
 
     setup() {
     }
