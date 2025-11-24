23f8834238d10f736e0f2bd708452aa2310ce150
abshe <abdo.3102003@gmail.com>
2025-11-24 15:45:56 +0100

    [IMP] estate: added account/invoicing

--- estate/models/res_user.py
+++ estate/models/res_user.py
@@ -4,4 +4,8 @@ from odoo import fields, models
 class ResUsers(models.Model):
     _inherit = "res.users"
 
-    property_ids = fields.One2many("estate.property", "salesperson_id", domain=[('state', 'in', ('new', 'offer_received'))])
+    property_ids = fields.One2many(
+        "estate.property",
+        "salesperson_id",
+        domain=[("state", "in", ("new", "offer_received"))],
+    )
---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,32 @@
+from odoo import models, Command
+
+
+class EstateProperty(models.Model):
+    _inherit = "estate.property"
+
+    def button_sell_property(self):
+        invoice_name = "INV/ESTATE/" + self.name
+        invoice_name2 = "INV/ESTATE/" + self.name + "/Administrative Fees"
+        self.env["account.move"].create(
+            {
+                "partner_id": self.buyer_id,
+                "move_type": "out_invoice",
+                "line_ids": [
+                    Command.create(
+                        {
+                            "name": invoice_name,
+                            "quantity": 1,
+                            "price_unit": 0.06 * self.selling_price,
+                        },
+                    ),
+                    Command.create(
+                        {
+                            "name": invoice_name2,
+                            "quantity": 1,
+                            "price_unit": 100.00,
+                        },
+                    ),
+                ]
+            }
+        )
+        return super().button_sell_property()
