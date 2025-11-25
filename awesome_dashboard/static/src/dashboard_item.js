647c006f7b3cb10f58e489bb0b3d1327fcd25c13
tskv <taskv@odoo.com>
2025-11-25 13:20:34 +0100

    [ADD] awesome_dashboard: tasks 1-3

---
+++ awesome_dashboard/static/src/dashboard_item.js
@@ -0,0 +1,18 @@
+import { Component } from "@odoo/owl";
+
+export class DashboardItem extends Component {
+    static template = "awesome_dashboard.DashboardItem";
+    static props = {
+        slots : {
+            type: Object,
+            optional: true
+        },
+        size : {
+            type: Number,
+            optional: true,
+        },
+    };
+    static defaultProps = {
+        size: 1,
+  };
+}
