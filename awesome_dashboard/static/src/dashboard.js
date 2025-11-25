cac97e2abd0a97937f9b1c33d88808a92d91373c
Nguyen (ngtpn) <ngtpn@odoo.com>
2025-11-25 10:50:21 +0100

    [IMP] awesome_dashboard: Add Layout / navigation buttons

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,8 +1,31 @@
 import { Component } from "@odoo/owl";
+import { useService } from "@web/core/utils/hooks";
+import { Layout } from "@web/search/layout";
 import { registry } from "@web/core/registry";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
+    static components = { Layout };
+
+    setup() {
+        this.action = useService("action");
+    }
+
+    openLeads() {
+        this.action.doAction({
+            type: "ir.actions.act_window",
+            name: "Leads",
+            res_model: "crm.lead",
+            views: [
+                [false, 'list'],
+                [false, 'form'],
+            ],
+        });
+    }
+
+    openCustomers() {
+        this.action.doAction("base.action_partner_form");
+    }
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
