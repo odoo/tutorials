9db9c2715ebb94ed834b7b0c500e5fe881c405a9
Nguyen (ngtpn) <ngtpn@odoo.com>
2025-11-25 11:19:48 +0100

    [IMP] awesome_dashboard: Dashboard item

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,11 +1,12 @@
 import { Component } from "@odoo/owl";
+import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
 import { Layout } from "@web/search/layout";
-import { registry } from "@web/core/registry";
+import { DashboardItem } from "./dashboard_item/dashboard_item";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
-    static components = { Layout };
+    static components = { Layout, DashboardItem };
 
     setup() {
         this.action = useService("action");
---
+++ awesome_dashboard/static/src/dashboard_item/dashboard_item.js
@@ -0,0 +1,8 @@
+import { Component } from "@odoo/owl";
+
+export class DashboardItem extends Component {
+    static template = "awesome_dashboard.DashboardItem";
+    static props = {
+        size: { type: Number, default: 1 },
+    }
+}
