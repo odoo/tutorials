8750017c705db62be91ace20dac2ccb89e10d684
Nguyen (ngtpn) <ngtpn@odoo.com>
2025-11-25 15:22:01 +0100

    [IMP] awesome_dashboard: Add pie chart

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -3,10 +3,11 @@ import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
 import { Layout } from "@web/search/layout";
 import { DashboardItem } from "./dashboard_item/dashboard_item";
+import { PieChart } from './pie_chart/pie_chart';
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
-    static components = { Layout, DashboardItem };
+    static components = { Layout, DashboardItem, PieChart };
 
     setup() {
         this.action = useService("action");
---
+++ awesome_dashboard/static/src/pie_chart/pie_chart.js
@@ -0,0 +1,61 @@
+import {
+    Component,
+    onWillStart,
+    onWillUnmount,
+    useRef,
+    useEffect,
+} from "@odoo/owl";
+import { loadJS } from "@web/core/assets";
+
+export class PieChart extends Component {
+    static template = "awesome_dashboard.PieChart";
+    static props = { data: { optional: true } }
+
+    setup() {
+        this.rootRef = useRef("root");
+        this.canvasRef = useRef("canvas");
+        this.containerRef = useRef("container");
+
+        this.chart = null;
+        this.tooltip = null;
+        this.legendTooltip = null;
+
+        onWillStart(async () => {
+            await loadJS("/web/static/lib/Chart/Chart.js")
+        });
+
+        useEffect(() => {
+            this.renderChart();
+        });
+
+        onWillUnmount(this.onWillUnmount);
+    }
+
+    onWillUnmount() {
+        if (this.chart) {
+            this.chart.destroy();
+        }
+    }
+
+    renderChart() {
+        if (this.chart) {
+            this.chart.destroy();
+        }
+
+        const labels = Object.keys(this.props.data);
+        const values = Object.values(this.props.data);
+
+        const config = {
+            type: 'pie',
+            data: {
+                labels: labels,
+                datasets: [{
+                  label: "Orders by size",
+                  data: values,
+                }],
+            },
+        }
+
+        this.chart = new Chart(this.canvasRef.el, config);
+    }
+}
