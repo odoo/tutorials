b97bbe11e2d4efe44d9a781fe5e5dbfe8e0f89de
shsak-odoo <shsak@odoo.com>
2025-12-03 14:58:48 +0530

    [ADD] estate:  added init file and manifest file to make it a module
    Created basic module structure
    
    Chapter 2

---
+++ estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Shivam Saksham(shsak)",
+    'category': 'Sales',
+    'description': """
+    An Real Estate App to buy, sell, and rent properties.
+    """,
+    'application': True,
+    'license': 'LGPL-3'
+}
