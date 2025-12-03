dbeb0a21993a904cda00484fdff80003b225c513
Ravij Parikh <snrav@odoo.com>
2025-12-03 15:34:46 +0530

    [IMP] estate: add initial estate application
    Created the necessary __init__.py and __manifest__.py files
    to initialize the estate application.

---
+++ estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'category': 'Real Estate',
+    'depends': ['base'],
+    'author': 'snrav-odoo',
+    'license': 'LGPL-3',
+    'description': 'Real estate purchase & sales',
+    'application': True,
+}
+
+
