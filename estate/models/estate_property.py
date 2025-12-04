5a8d5d3478fa570fab518e64a521b7776f01d907
shsak-odoo <shsak@odoo.com>
2025-12-04 17:10:44 +0530

    [IMP] estate:  Added menus,  actions, and views for the UI
    Added menus to show the application on the odoo root menu
    Action to open the view
    List and form view to Create, Edit, and Show Estate Property
    
    Chapter 5

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -8,16 +8,24 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(default=fields.Date.add(fields.Date.today(), month=3), copy=False)
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
-        string='Orientation',
+        string='Garden Orientation',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
     )
+    state = fields.Selection(
+        string='Status',
+        selection=[('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')],
+        default='new',
+        required=True,
+        copy=False
+    )
+    active = fields.Boolean(default=True)
