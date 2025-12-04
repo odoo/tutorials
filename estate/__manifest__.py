0ce5e44f700f5e517451d8606cc7317fee1f9d08
haman-odoo <haman@odoo.com>
2025-12-04 11:10:33 +0530

    [ADD] estate: Created base module structure and manifest
    Created the initial structure for the Estate module:
    
    - Added __init__.py file to initialize the module.
    - Added __manifest__.py file with basic module metadata like:
      - name
      - version
      - depends
      - data
      - description

---
+++ estate/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'license': 'LGPL-3',
+    'summary': 'Real Estate advertisement tutorial module',
+    'depends': ['base'],
+    'author': "Harsh Maniya",
+    'category': 'Sales/Real Estate',
+    'installable': True,
+    'auto_install': False,
+    'description': """
+    Description text
+    """,
+}
