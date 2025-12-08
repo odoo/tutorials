c260137275148c320a0893c5dd711290ee133db5
haman-odoo <haman@odoo.com>
2025-12-08 11:25:13 +0530

    [IMP] estate: Added custom list, form and search views
    chapter 6 : Created list, form, and search views for the estate.property model.
    Added menu items and actions to open the property views in the Real Estate module.
    Created new model estate.property.type and added its list & form views, menu, and access rights.

--- estate/models/__init__.py
+++ estate/models/__init__.py
@@ -1,1 +1,2 @@
 from . import estate_property  # noqa: F401
+from . import estate_property_type
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -50,4 +50,8 @@ class EstateProperty(models.Model):
     required=True,
     copy=False,
     default='new',
-)
+    )
+    property_type_id = fields.Many2one(
+        "estate.property.type",
+        string="Property Type",
+    )
