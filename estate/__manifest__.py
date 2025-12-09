458d9300e7f0c8ece3dcfa2faca66b2e3ad9451b
hatiw-odoo <hatiw@odoo.com>
2025-12-09 10:34:40 +0530

    [ADD]  estate:  Architecture, app, models, security
    Chapter 2 –
    Created the base structure for a new custom Odoo application.
    Chapter 3 –
    Implemented primary models with essential fields and basic logic.
    Chapter 4 –
    Added ACLs and basic security rules for controlled data access.

---
+++ estate/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Harsh Tiwari",
+    'category': 'Category',
+    'application': True,
+    'description': """
+    Description text
+    """,
+    'license': 'LGPL-3',
+    'data': [
+        'views/estate_menus.xml',
+        'views/estate_property_views.xml',
+        'security/ir.model.access.csv'
+    ]
+}
