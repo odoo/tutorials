bac1ca07edc3291ed91fbb1c511aeec060d651b9
Vincent Janssen <jvince.2112@outlook.com>
2025-12-10 12:40:41 +0100

    vijan-Server101-2

---
+++ estate/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name':"estate",
+    'description':"test",
+    'depends': [
+        'base_setup'
+    ],
+    'category': "Tutorials",
+    'installable': True,
+    'application': True
+
+}
