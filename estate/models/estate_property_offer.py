b048a7d3a4ad8eb7a96c5253bf6f3e220771bde6
haman-odoo <haman@odoo.com>
2025-12-10 12:30:06 +0530

    [IMP] estate: Compute total area, best price and add onchange for garden fields
    Chapter 8 : Added compute, inverse, and onchange methods for dynamic field updates.
    Implemented automatic offer price, total area, and deadline calculations.
    Added Tag and Offer models with validity and date deadline logic.

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,9 +1,11 @@
-from odoo import models, fields
+from dateutil.relativedelta import relativedelta
+from odoo import models, fields, api
 
 
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
     _description = "Real Estate Property Offer"
+
     price = fields.Float(string="Price")
     status = fields.Selection(
         [
@@ -20,3 +22,22 @@ class EstatePropertyOffer(models.Model):
         "estate.property",
         required=True,
     )
+    validity = fields.Integer(
+        default=7,
+    )
+    date_deadline = fields.Date(
+        compute="_compute_date_deadline",
+        inverse="_inverse_date_deadline",
+        store=True,
+    )
+
+    @api.depends("validity")
+    def _compute_date_deadline(self):
+        for rec in self:
+            create = rec.create_date or fields.Date.today()
+            rec.date_deadline = (create + relativedelta(days=rec.validity))
+
+    def _inverse_date_deadline(self):
+        for rec in self:
+            create = rec.create_date or fields.Date.today()
+            rec.validity = (rec.date_deadline - fields.Date.today(create)).days
