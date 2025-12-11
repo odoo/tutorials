7f9f93d2c89d8f3469dbe824830a788b16f8d17b
Vincent Janssen <jvince.2112@outlook.com>
2025-12-11 10:34:40 +0100

    vijan-Server101-6

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -10,7 +10,7 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True,default="Unkown")
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(copy=False,default= fields.Datetime.today() + relativedelta(months=3))
+    date_availability = fields.Date("Available From",copy=False,default= fields.Datetime.today() + relativedelta(months=3))
     last_seen= fields.Date("Last Seen", default=fields.Datetime.now)
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True,copy=False)
