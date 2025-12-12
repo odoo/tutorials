ab0ad67699669355ca1a317f094ad658ee0cf3de
danal-odoo <danal@odoo.com>
2025-12-12 13:32:57 +0530

    [ADD] mrp_bom_overview_forecast: merge status and availability
    Currently, the BoM Overview report displays 'Status' and 'Availability'
    as two separate columns/indicators. This leads to a cluttered interface
    and occupies unnecessary horizontal space on the report lines.
    
    This module merges these two indicators into a single column to
    streamline the display.
    
    The following logic is implemented:
    1. For the parent product: Displays the production status if all
       components are available, will always green. If not ready,
       it falls back to showing the availability date/state.
    2. For components: Always displays the availability state.
    
    Technical details:
    - Template `mrp.BomOverviewLine`: Removed the availability_display column
      and consolidated the badge display logic in the XML.
    - JS `BomOverviewLine`: Created `ColorClass`  getter to dynamically assign
      colors (success, warning, danger, dark) based on the availability state.
    - Template `mrp.BomOverviewTable`: Removed Availability column and margins
      using `xpath` for a cleaner forecast layout.
    
    Task-5403914

---
+++ mrp_bom_overview_forecast/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "BOM overview",
+    "version": "1.0",
+    "depends": ["mrp", "purchase"],
+    "author": "danal",
+    "category": "Category",
+    "license": "LGPL-3",
+    "assets": {
+        "web.assets_backend": [
+            "mrp_bom_overview_forecast/static/src/**/*",
+        ],
+    },
+}
