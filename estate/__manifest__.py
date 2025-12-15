d56b90acc7932d2cdf1838ef024db85d22f053a9
Faruk Avcı <55799734+Faruk372742@users.noreply.github.com>
2025-12-15 14:12:50 +0100

    [ADD] estate: initialize the estate app
    Creating the manifest file.

---
+++ estate/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "estate",
+
+    'author': "Faruk",
+    'website': "",
+    'category': 'Tutorials',
+    'version': '0.1',
+    'application': True,
+    'installable': True,
+    'application': True, 
+    'depends': ['base'],
+
+    'data': [],
+    'assets': {},
+}
