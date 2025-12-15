7dc2f6eba5b3233f1733b9945eb4cbc791497385
Steven François (stfra) <stfra@odoo.com>
2025-12-15 17:25:25 +0100

    [FIX] estate: Fix required field needed in expected price and a typo

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -8,7 +8,7 @@ class EstateProperty(models.Model):
     description = fields.Text('Description')
     postcode = fields.Char('Poscode', default='1000')
     date_availability = fields.Date('Date availability')
-    expected_price = fields.Float('Expected price')
+    expected_price = fields.Float('Expected price', required=True)
     selling_price = fields.Float('Selling price')
     bedrooms = fields.Integer('Bedrooms')
     living_area = fields.Integer('Living Area')
@@ -16,4 +16,9 @@ class EstateProperty(models.Model):
     garage = fields.Boolean('Garage')
     garden = fields.Boolean('Garden')
     garden_area = fields.Integer('Garden Area')
-    garden_orientation = fields.Selection('Gordden orientation', selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
+    garden_orientation = fields.Selection(selection=[
+        ('north', 'North'),
+        ('south', 'South'),
+        ('east', 'East'),
+        ('west', 'West')
+    ], string='Garden orientation')
