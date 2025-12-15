604a7d89d5a1fc143df53959ec1e22b88dc04629
lkerroum <leker@odoo.com>
2025-12-15 17:16:44 +0100

    [IMP] estate: added new record action and menu

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,6 +1,10 @@
-{'name': 'Real Estate',
-'depends': ['base'],
-'application': True,
-'author': 'leker',
-'license': 'LGPL-3',
+{
+    'name': 'Real Estate',
+    'depends': ['base'],
+    'data': ['security/ir.model.access.csv',
+            'views/estate_property_views.xml',
+            'views/estate_menus.xml'],
+    'application': True,
+    'author': 'leker',
+    'license': 'LGPL-3',
 }
