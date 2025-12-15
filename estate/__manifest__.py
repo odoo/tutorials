f5d86c51da42711c4a094a03f0b42debfc298562
Oussema Heni <oussemaheny@gmail.com>
2025-12-15 16:42:59 +0100

    [ADD] real_estate: add estate property model and its props

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,13 +1,6 @@
-# -*- coding: utf-8 -*-
 {
     'name': "Real Estate",
-
-    'summary': """
-        Starting module for "Real Estate"
-    """,
-
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base',],
+    'application': True,
+    'installable': True,
+    'depends': ['base']
 }
---
+++ estate/models/estate_property.py
@@ -0,0 +1,28 @@
+from odoo import fields, models
+
+
+class EstateProperty(models.Model):
+    _name = "public.estate_property"
+    _description = "Estate property"
+
+    name = fields.Char('Property Name', required=True)
+    description = fields.Text('Property Description')
+    postcode = fields.Char('Postcode')
+    date_availability = fields.Date('Availability Date')
+    expected_price = fields.Float('Expected Price', required=True)
+    selling_price = fields.Float('Selling Price')
+    bedrooms = fields.Integer('# Bedrooms')
+    living_area = fields.Integer('# Living Area')
+    facades = fields.Integer('# Facades')
+    garage = fields.Boolean('Has Garage')
+    garden = fields.Boolean('Has Garden')
+    garden_area = fields.Integer('Garden area')
+    garden_orientation = fields.Selection(
+        string='type',
+        selection=[
+        ('North', 'North Garden Orientation'),
+        ('South', 'South Garden Orientation'),
+        ('East', 'East Garden Orientation'),
+        ('West', 'West Garden Orientation')
+    ], string='North Garden Orientation', default='North')
+    
