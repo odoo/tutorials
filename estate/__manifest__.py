7e844b7bf638408fd5a32370211d598c062033b1
Oussema Heni <oussemaheny@gmail.com>
2025-12-15 14:49:41 +0100

    [ADD] real_estate: init real estate addon

---
+++ estate/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Real Estate",
+
+    'summary': """
+        Starting module for "Real Estate"
+    """,
+
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base',],
+}
