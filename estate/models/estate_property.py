f693dde22f6d0c6450c5e7d2df3b8068f6196ef7
lkerroum <leker@odoo.com>
2025-12-15 16:09:25 +0100

    [IMP] estate: real estate properties model definition

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,5 +1,25 @@
-from odoo import models
+from odoo import fields, models
 
 
 class Property(models.Model):
     _name = "estate.property"
+    _description = "Real Estate property"
+
+    name = fields.Char('Name', required=True)
+    description = fields.Text('Description')
+    postcode = fields.Float('Postcode')
+    date_availability = fields.Date('Date availability')
+    expected_price = fields.Float('Expected price', required=True)
+    selling_price = fields.Float('Selling price')
+    bedrooms = fields.Integer('Bedrooms')
+    living_area = fields.Integer('Living area')
+    facades = fields.Integer('Facades')
+    garage = fields.Boolean('Garage')
+    garden = fields.Boolean('Garden')
+    garden_area = fields.Integer('Garden area')
+    garden_orientation = fields.Selection([
+        ('north', 'North'),
+        ('south', 'South'),
+        ('east', 'East'),
+        ('west', 'West'),
+    ], string='Garden Orientation')
