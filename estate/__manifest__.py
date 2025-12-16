709dd423e7de92dd621893f5761e7735b7f05f6d
ibrahim (ibmah) <ibmah@odoo.com>
2025-12-16 13:30:50 +0100

    [IMP] estate: Implemet menus and new model customizations
    Chapter 5

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -11,5 +11,7 @@
     "installable": True,
     "data": [
         "data/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
     ],
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+from dateutil.relativedelta import relativedelta
 from odoo import fields, models
 
 
@@ -7,11 +10,14 @@ class EstateProperty(models.Model):
 
     name = fields.Char(required=True)
     description = fields.Text()
+    active = fields.Boolean(default=True)
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(
+        copy=False, default=datetime.now() + relativedelta(month=3)
+    )
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -25,4 +31,17 @@ class EstateProperty(models.Model):
             ("west", "West"),
         ],
         help="Indicates the direction the garden is facing with respect to the house",
+    )
+    state = fields.Selection(
+        [
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled"),
+        ],
+        required=True,
+        copy=False,
+        default="new",
+        help="State of the proprty listing lifecycle",
     )
