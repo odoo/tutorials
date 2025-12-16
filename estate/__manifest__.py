923489a5b801a94d94ff56e4e8cf7ebaed7b7a46
Faruk Avcı <faavc@odoo.com>
2025-12-16 13:21:17 +0100

    [ADD] estate: adding menus and changing fields - Chapter 5
    [LINT] estate: linting all python files with ruff

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,16 +1,17 @@
 {
-    'name': "estate",
-
-    'website': "",
-    'category': 'Tutorials',
-    'version': '0.1',
-    'application': True,
-    'installable': True,
-    'depends': ['base'],
-    'data': [
-        'security/ir.model.access.csv'
+    "name": "estate",
+    "website": "",
+    "category": "Tutorials",
+    "version": "0.1",
+    "application": True,
+    "installable": True,
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
     ],
-    'assets': {},
-    'author': 'Odoo S.A.',
-    'license': 'LGPL-3',
+    "assets": {},
+    "author": "Odoo S.A.",
+    "license": "LGPL-3",
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,6 @@
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta
+from datetime import datetime
 
 
 class EstateProperty(models.Model):
@@ -7,14 +9,36 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(
+        copy=False, default=datetime.now() + relativedelta(months=3)
+    )
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
-    garden_orientation = fields.Selection(string="Orientation", selection=[("North", "North"), ("South", "South"), ("East", "East"), ("West", "West")])
-    
+    garden_orientation = fields.Selection(
+        string="Orientation",
+        selection=[
+            ("North", "North"),
+            ("South", "South"),
+            ("East", "East"),
+            ("West", "West"),
+        ],
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        selection=[
+            ("New", "New"),
+            ("Offer Received", "Offer Received"),
+            ("Offer Accepted", "Offer Accepted"),
+            ("Sold", "Sold"),
+            ("Cancelled", "Cancelled"),
+        ],
+        default="New",
+        required=True,
+        copy=False,
+    )
