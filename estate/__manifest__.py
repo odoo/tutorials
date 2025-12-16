6591727a7e57fb7e8c51cef089f13be4ab213f89
SurryaT10 <surrya.thagamuthu@gmail.com>
2025-12-16 15:17:30 +0100

    [FIX] Added missed imports for views and security in manifest file

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -20,6 +20,11 @@
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'web'],
+    'data': [
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+        'security/ir.model.access.csv',
+    ],
     'application': True,
     'installable': True,
     'license': 'AGPL-3'
