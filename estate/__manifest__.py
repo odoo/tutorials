f2bcc72f7597d05a943d6154b5c2350deb159ac2
mobar <mobar@odoo.com>
2025-12-16 14:47:56 +0100

    [ADD] Add Chapter 4

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,7 +1,12 @@
 {
     'name': 'Estate',
+    'author': 'mobar',
+    'license': 'LGPL-3',
     'depends': [
         'base',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
     ],
     'installable': True,
     'application': True,
