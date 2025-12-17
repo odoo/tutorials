2e143d9f74d44127dd7ca1b77bf2ccbead5c1532
SurryaT10 <surrya.thagamuthu@gmail.com>
2025-12-17 11:25:28 +0100

    [ADD] Chapter 6: Build Views
    Added estate_property_views.xml which contains list and form views
    
    Added a separate estate_property_search_view.xml for search with filters for active and availability as well as group by based on postcode.
    
    Also addressed code review comments.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,26 +1,15 @@
 {
     'name': "Estate",
 
-    'summary': """
-        Starting module for "Estate"
-    """,
-
-    'description': """
-        Starting module for "Estate"
-    """,
-
     'author': "Odoo",
     'website': "https://www.odoo.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Tutorials',
     'version': '0.1',
 
-    # any module necessary for this one to work correctly
     'depends': ['base', 'web'],
     'data': [
+        'views/estate_property_search_view.xml',
         'views/estate_property_views.xml',
         'views/estate_menus.xml',
         'security/ir.model.access.csv',
--- estate/models/estate.py
+++ estate/models/estate.py
@@ -4,19 +4,25 @@ class Estate(models.Model):
     _name = 'estate.property'
     _description = 'Estate Property'
 
-    name = fields.Char('Property Name', required=True)
-    description = fields.Text('Description')
-    postcode = fields.Char('Postcode')
-    date_availability = fields.Date('Availability')
-    expected_price = fields.Float('Expected Price', required=True)
-    selling_price = fields.Float('Selling Price')
-    bedrooms = fields.Integer('# Bedrooms')
-    living_area = fields.Integer('Living Area')
-    facades = fields.Integer('Facades')
-    garage = fields.Boolean('Garage', default=True)
-    garden = fields.Boolean('Garden', default=True)
-    garden_area = fields.Integer('Garden Area')
+    name = fields.Char("Property Name", required=True)
+    description = fields.Text("Description")
+    postcode = fields.Char("Postcode")
+    date_availability = fields.Date("Available From", default=fields.Date.add(fields.Date.today(), months=3))
+    expected_price = fields.Float("Expected Price", required=True)
+    selling_price = fields.Float("Selling Price", readonly=True, copy=False)
+    bedrooms = fields.Integer("Bedrooms", default=2)
+    living_area = fields.Integer("Living Area (sqm)")
+    facades = fields.Integer("Facades")
+    garage = fields.Boolean("Garage", default=True)
+    garden = fields.Boolean("Garden", default=True)
+    garden_area = fields.Integer("Garden Area (sqm)")
     garden_orientation = fields.Selection(
-        string='Type',
-        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
+        string="Garden Orientation",
+        selection=[('north', "North"), ('south', "South"), ('east', "East"), ('west', "West")],
         required=True)
+    status = fields.Selection(
+        string="Status",
+        selection=[('new', "New"), ('offer_received', "Offer Received"), ('offer_accepted', "Offer Accepted"), ('sold', "Sold"), ('canceled', "Canceled")],
+        default='new',
+        required=True)
+    active = fields.Boolean("Active", default=True)
