a2ae3a1638819bf5a92c5016e8d98c7d9fb03aa1
ibrahim (ibmah) <ibmah@odoo.com>
2025-12-17 13:41:01 +0100

    [IMP] estate: Add relations between real estate property models
    Chapter 7
    
    This adds tags to properties, lists offers made (whether refued or
    accepted) along with the salesperson and buyer in "Other info" tab

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -12,6 +12,9 @@
     "data": [
         "data/ir.model.access.csv",
         "views/estate_property_views.xml",
+        "views/estate_property_type.xml",
+        "views/estate_property_tag.xml",
+        "views/estate_property_offer.xml",
         "views/estate_menus.xml",
     ],
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,7 +5,7 @@ from odoo import fields, models
 
 
 class EstateProperty(models.Model):
-    _name = "estate_property"
+    _name = "estate.property"
     _description = "Estate Property data model"
 
     name = fields.Char(required=True)
@@ -45,3 +45,10 @@ class EstateProperty(models.Model):
         default="new",
         help="State of the proprty listing lifecycle",
     )
+    property_type_id = fields.Many2one("estate.property.type", string="Property Type")
+    buyer_id = fields.Many2one("res.partner", string="Buyer")
+    salesperson_id = fields.Many2one(
+        "res.users", string="Salesman", default=lambda self: self.env.user
+    )
+    tag_ids = fields.Many2many("estate.property.tag", string="Property Tags")
+    offer_ids = fields.One2many("estate.property.offer", "property_id")
