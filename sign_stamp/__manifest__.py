bb6c97f5e8091ced004109ce40152b9ba5428659
utpat-odoo <utpat@odoo.com>
2025-12-18 21:46:05 +0530

    [ADD] sign_stamp: add stamp field in sign app
    Purpose:
    The Stamp field allows users to apply an official company stamp during the
    document signing process.
    
    Technical changes:
    add new stamp field in sign.item.type
    Configured a multi-line placeholder to display structured stamp information
    (Company, Address, City, Country, VAT Number)

---
+++ sign_stamp/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'author': 'Odoo S.A.',
+    'name': 'Sign Stamp',
+    'description': """
+    add stamp field in sign app
+    """,
+    'data': [
+        'data/sign_data.xml',
+    ],
+    'depends': ['sign'],
+    'license': 'LGPL-3',
+    'application': True,
+    'installable': True
+}
