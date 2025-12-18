580267f20a2e047832473d58c1a37a8ab9239333
Oussema Heni <oussemaheny@gmail.com>
2025-12-18 17:27:22 +0100

    [IMP] real_estate: add property type and tags models

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -27,6 +27,7 @@ class EstateProperty(models.Model):
         ('east', 'East'),
         ('west', 'West')
     ])
+    
     state = fields.Selection(
         [
             ('new', 'New'),
@@ -39,4 +40,26 @@ class EstateProperty(models.Model):
         copy=False,
         default='new',
     )
+
+    buyer_id = fields.Many2one(
+        "res.partner",
+        string="Buyer",
+        copy=False
+    )
+
+    salesperson_id = fields.Many2one(
+        "res.users",
+        string="Salesperson",
+        default=lambda self: self.env.user
+    )
+
+    property_type_id = fields.Many2one(
+        "estate.property.type",
+        string="Property Type"
+    )
+
     
+    tag_ids = fields.Many2many(
+    "estate.property.tag",
+    string="Tags"
+    )
