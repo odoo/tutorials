e5124588c58a27a91c812f66b8fb9ac30a192bc7
SurryaT10 <surrya.thagamuthu@gmail.com>
2025-12-18 13:37:57 +0100

    [IMP] Added computed fields - total_area and best_offer
    total_area field is based on the size of both garden_area and living_area
    
    best_offer field is based on the max price among the list of offers
    
    Also added validity and deadline for the offers as a computed and inverse function impacting each other.

--- estate/models/estate.py
+++ estate/models/estate.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class Estate(models.Model):
@@ -32,3 +32,24 @@ class Estate(models.Model):
     salesperson_id = fields.Many2one('res.users', string="Salesperson", default=lambda self: self.env.user)
     tag_ids = fields.Many2many('estate.property.tag', string="Tags")
     offer_ids = fields.One2many('estate.property.offer', 'property_id', string="Offers")
+    total_area = fields.Integer('Total Area (sqm)', compute='_compute_total_area')
+    best_price = fields.Float("Best Offer", compute='_compute_best_price')
+
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
+    @api.depends('offer_ids.price')
+    def _compute_best_price(self):
+        for record in self:
+            record.best_price = max(record.offer_ids.mapped('price'))
+
+    @api.onchange("garden")
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = 'north'
+        else:
+            self.garden_area = 0
+            self.garden_orientation = False
--- estate/models/offer.py
+++ estate/models/offer.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class Offer(models.Model):
@@ -12,3 +12,16 @@ class Offer(models.Model):
         copy=False)
     partner_id = fields.Many2one('res.partner', string="Partner", required=True)
     property_id = fields.Many2one('estate.property', string="Property", required=True)
+    validity = fields.Integer('Validity (days)', default=7)
+    date_deadline = fields.Date("Deadline", compute='_compute_date_deadline', inverse="_inverse_date_deadline")
+
+    @api.depends('validity', 'create_date')
+    def _compute_date_deadline(self):
+        for record in self:
+            create_date = record.create_date if record.create_date else fields.Date.today()
+            record.date_deadline = fields.Date.add(create_date, days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            create_date = record.create_date if record.create_date else fields.Date.today()
+            record.validity = (record.date_deadline - create_date.date()).days
