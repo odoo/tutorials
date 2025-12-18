339b8ebe1b9173894affc00a1058b7763aca02fe
Faruk Avcı <faavc@odoo.com>
2025-12-18 17:39:35 +0100

    [REF] estate: resolving onboarder's comments
    ref: maximizing method and name of the computation method

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -12,7 +12,7 @@ class EstateProperty(models.Model):
     description = fields.Text()
     postcode = fields.Char()
     date_availability = fields.Date(
-        copy=False, default=datetime.now() + relativedelta(months=3)
+        copy=False, default=lambda self: datetime.now() + relativedelta(months=3)
     )
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
@@ -53,7 +53,7 @@ class EstateProperty(models.Model):
     buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
     tag_ids = fields.Many2many("estate_property_tag")
     offer_ids = fields.One2many("estate_property_offer", "property_id", string="Offers")
-    total_area = fields.Integer("Total Area (sqm)", compute="_compute_area")
+    total_area = fields.Integer("Total Area (sqm)", compute="_compute_total_area")
     best_price = fields.Float("Best Price", compute="_compute_best_price")
 
     _check_expected_price = models.Constraint(
@@ -64,18 +64,14 @@ class EstateProperty(models.Model):
     )
 
     @api.depends("living_area", "garden_area")
-    def _compute_area(self):
+    def _compute_total_area(self):
         for record in self:
             record.total_area = record.living_area + record.garden_area
 
     @api.depends("offer_ids.price")
     def _compute_best_price(self):
         for record in self:
-            record.best_price = (
-                0
-                if len(record.offer_ids) == 0
-                else max(offer.price for offer in record.offer_ids)
-            )
+            record.best_price = max(record.offer_ids.mapped("price") or [0])
 
     @api.onchange("garden")
     def _onchange_garden(self):
