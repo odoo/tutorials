f9086da598ebb5fa139430e51daec73be018fb9f
Robin (robal) <robal@odoo.com>
2025-12-22 09:08:00 +0100

    [IMP] awesome_owl: add toggle to hide/minimize card contents

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -1,4 +1,4 @@
-import { Component } from "@odoo/owl";
+import { useState, Component } from "@odoo/owl";
 
 export class Card extends Component {
     static template = "awesome_owl.card";
@@ -6,4 +6,12 @@ export class Card extends Component {
         title: String,
         slots: {type: Object, optional: true},
     };
+
+    setup() {
+        this.state = useState({ visible: true });
+    }
+
+    toggleVisible() {
+        this.state.visible = !this.state.visible;
+    }
 }
