cc92213481198d874666b5355d9811cfc85b1b3e
haman-odoo <haman@odoo.com>
2025-12-22 10:36:48 +0530

    [IMP] awesome_owl:  Added Counter and Card component, props and callbacks
    Added Counter and extract it into sub component
    Added Card component with markup to display html values
    Added callback() function, props and incrementSum method in Playground

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,5 +1,18 @@
-import { Component } from "@odoo/owl";
+import { Component, markup, useState } from "@odoo/owl";
+import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
+    static components = { Counter, Card };
+
+    setup() {
+        this.state = useState({ sum: 2 });
+        this.htmlContent = markup("<h1>Markup Text</h1>");
+        this.normalContent = "<h1>some content</h1>";
+    }
+    incrementSum() {
+        this.state.sum++;
+    }
 }
+
