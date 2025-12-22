6c404f481ace26f2bed62864bf7f9d5624eadbdf
Robin (robal) <robal@odoo.com>
2025-12-22 10:34:48 +0100

    [IMP] awesome_dashboard: add action buttons
    - Add an action button that redirects to a kanban view of customers
    - Add an action button that redirects to the lead model

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,10 +1,28 @@
-import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";
 import { Layout } from "@web/search/layout";
+import { Component } from "@odoo/owl";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
     static components = { Layout };
+
+    setup() {
+        this.action = useService("action");
+    }
+
+    actionCustomers() {
+        this.action.doAction("base.action_partner_form", { viewType: "kanban"});
+    }
+
+    actionLeads() {
+        this.action.doAction({
+            type: "ir.actions.act_window",
+            name: "All leads",
+            res_model: "crm.lead",
+            views: [[false, "list"], [false, "form"]],
+        });
+    }
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
