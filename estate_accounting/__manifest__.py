9e6806cc98f1130fe6a38bba47af135b81c6e3d4
Omar Khalil (omkha) <omarkhalil.m17@gmail.com>
2025-12-22 17:27:40 +0100

    [ADD] Added Module: Estate Account Description: 1. Added a new module to create invoices for new completed sales in estate module

---
+++ estate_accounting/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Realestate accounting",
+    "description": "Real Estate Accounting System",
+    "category": "Tutorials",
+    "version": "1.0",
+    "application": True,
+    "depends": ["estate", "account"],
+    "author": "Odoo S.A.",
+    "license": "LGPL-3",
+}
