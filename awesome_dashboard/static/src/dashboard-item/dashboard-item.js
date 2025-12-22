22ad38d72730c27b40b4a0d491d51f1945b071b1
Robin (robal) <robal@odoo.com>
2025-12-22 14:56:10 +0100

    [REF] awesome_dashboard: refactor components (part 1)

--- awesome_dashboard/static/src/dashboard-item/dashboard-item.js
+++ awesome_dashboard/static/src/dashboard-item/dashboard-item.js
@@ -4,6 +4,7 @@ export class DashboardItem extends Component {
     static template = "awesome_dashboard.dashboard-item";
     static props = {
         size: { type: Number, optional: true },
+        title: String,
         slots: { type: Object, optional: true },
     };
     static defaultProps = {
