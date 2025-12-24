ac522b12596918f2161dfd3fb8f9c6f5dc9d9f2e
Robin (robal) <robal@odoo.com>
2025-12-24 08:51:36 +0100

    [FIX] estate_account: optimize invoice creation
    - Move the call to "create" outside the loop

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -7,8 +7,9 @@ class EstateProperty(models.Model):
     def action_set_sold(self) -> bool:
         percent_description = "6 percent of selling price"
         admin_fee_description = "Administrative fees"
-        # Note: does not work if fiscal localization is not set
+        account_moves = []
         for record in self:
+            # TODO test buyer_id not being set
             account_move = {
                 "partner_id": record.buyer_id.id,
                 "move_type": "out_invoice",
@@ -17,5 +18,8 @@ class EstateProperty(models.Model):
                     Command.create({"name": admin_fee_description, "quantity": 1, "price_unit": 100.0}),
                 ],
             }
-            self.env["account.move"].create(account_move)
+            account_moves.append(account_move)
+
+        # Note: account move creation does not work if fiscal localization is not set
+        self.env["account.move"].create(account_moves)
         return super().action_set_sold()
