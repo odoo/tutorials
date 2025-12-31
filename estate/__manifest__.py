a6ca23655a2346c95b3e021733544a3c7d15a8bf
japat-odoo <japat@odoo.com>
2025-12-31 15:06:54 +0530

    [ADD] estate: created the model and Initalized it with the postgre db
    - The Estate Module, which is needed as of tutorials
    - Required fields were added, including the selection
    - CHAPTER 1,2,3

---
+++ estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': "Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "japat",
+    'category': 'Category',
+    'description': """
+    Hello , Real Estate for everyone
+    """,
+}
