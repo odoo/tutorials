63f07fb6a2bf27d78856ccec7bec7b3ea367246e
isgos-odoo <isgos@odoo.com>
2025-12-31 18:22:51 +0530

    [ADD] real_estate: create base module structure
    Implemented Chapters(1 & 2) Architecture Overview and New Application.
    
    Changes:
    - Created module directory `real_estate`.
    - Defined `__manifest__.py` with metadata (name, depends, license).
    - Added empty `__init__.py` to initialize the Python package.

---
+++ real_estate/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'real_estate',
+    'version': '19.0.1.0',
+    'catogary': 'sales',
+    'summary': 'Manage real estate properties',
+    'depends': [
+        'base'
+    ],
+    'installable': True,
+    'application': True,
+    'author': 'Odoo S.A',
+    'license': 'LGPL-3',
+}
