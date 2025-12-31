1db64650784218a297b6228f4925f4b1551c44c6
shtha-odoo <shtha@odoo.com>
2025-12-31 10:33:46 +0530

    [ADD] estate: module created estate (ch-1,2) done

---
+++ estate/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Real Estate",
+    'version': '1.0.0',
+    'depends': ['base'],
+    'author': "john addams",
+    'category': 'Tutorials',
+    'description': "ytyut",
+    'application': True,
+    'installable': True,
+    '
+    # data files always loaded at installation
+    'data': [
+        'views/mymodule_view.xml',
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+        'demo/demo_data.xml',
+    ],
+}
