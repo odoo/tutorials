3f42781c4889f1e3e99324eca5d16e10539b6755
mekav-odoo <mekav@odoo.com>
2025-12-31 10:28:00 +0530

    [ADD] estate: create module real-estate. chapter 1,2

---
+++ estate/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "estate",
+
+    'description': """
+        A real-estate application"
+    """,
+
+    'author': "meet kavathiya",
+    'website': "https://www.odoo.com/",
+    'category': 'Real-estate',
+    'version': '0.1',
+    'application': True,
+    'installable': True,
+    'depends': ['base', 'web'],
+
+    'data': [],
+    'assets': {
+
+    },
+    'license': 'AGPL-3'
+}
