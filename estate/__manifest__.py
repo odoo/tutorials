71296e371360e60a5b9ff97d95b730106a6e7cc0
jakan-odoo <jakan@odoo.com>
2025-12-31 15:47:16 +0530

    [ADD] estate: initialize module structure and model
    - Real Estate module created
    - Basic structure and required fields added
    - CHAPTER 1, 2, and 3

---
+++ estate/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'category': 'Real Estate',
+    'description':'Manage real estate properties',
+    'depends': ['base'],
+    'application': True,
+    'installable': True,
+}
---
+++ estate/models/estate_property.py
@@ -0,0 +1,23 @@
+from odoo import fields, models
+
+class EstateProperty(models.Model):
+    _name = 'estate.property'   
+    _description = 'Real Estate Property'
+
+    # Each field becomes a column in PostgreSQL table
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        string='Direction',
+        selection=[('north', 'North'),('south', 'South'),('east', 'East'),('west', 'West')],
+    )
