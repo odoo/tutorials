838d9b8cfd48ef15838ce9c5f1631aae96fd1d0e
mibav-odoo <mibav@odoo.com>
2025-12-31 16:35:45 +0530

    [ADD] estate:Initialize new module
    -Define manifest file
    -Define new model and security file
    
    Chapter 1: Architecture Overview
    Chapter 2: A New Application
    Chapter 3: Models And Basic Fields
    Chapter 4: Security - A Brief Introduction

---
+++ estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Estate',
+    'version': '0.1',
+    'summary': 'This is the Real Estate Advertisement module',
+    'description': 'This is the Real Estate Advertisement module.',
+    'data': [
+        'security/ir.model.access.csv'
+    ],
+    'author': 'Milan Bavishi',
+    'license': 'LGPL-3'
+}
