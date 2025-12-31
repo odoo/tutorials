ac7ad928d6193fe00923a1c71abcd1f99ae408bb
Abhi Bhingradiya <abbhi@odoo.com>
2025-12-31 16:31:46 +0530

    [ADD] estate: created estate Property model and manifest file
    - Adds property model with necessary fields.
    - chapter 2,3

---
+++ estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Estate',
+    'version': '1.0',
+    'author': 'Abhi Bhingradiya(abbhi)',
+    'description': 'Real state app',
+    'license': 'LGPL-3',
+    'application': True,
+    'data': [
+        'security/ir.model.access.csv',
+    ]
+}
