987e6c26aae16304bd630a51162d8bcf9fa624ab
radet-odoo <radet@odoo.com>
2025-12-31 16:11:34 +0530

    [ADD] estate: initial module setup and ORM configuration
    Add estate module
    Configure custom module structure in Odoo 19
    Introduce addons structure and ORM models
    Define database tables using Odoo ORM
    CHAPTER 1,2,3

---
+++ estate/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': "Real Estate",
+    'category': 'Real Estate',
+    'version': '1.0',
+    'summary': 'Manage real estate properties',
+    'depends': ['base'],
+    'application': True,
+}
