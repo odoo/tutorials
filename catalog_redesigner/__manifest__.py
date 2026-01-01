b47cf920a522aaec07adf23bc98fe6ed0a1d4a23
shsak-odoo <shsak@odoo.com>
2026-01-01 11:09:13 +0530

    [ADD] catalog_redesigner:  create the catalog re-designer module
    - add the manifest
    - add the inherited model
    
    task-5452394

---
+++ catalog_redesigner/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Product Catalog Redesigner',
+    'version': '1.0',
+    'depends': ['product'],
+    'author': 'Shivam Saksham(shsak)',
+    'category': 'Sales',
+    'description': """
+    An module to redesign the catalog in small screens.
+    """,
+    'application': True,
+    'license': 'LGPL-3',
+}
---
+++ catalog_redesigner/models/product_product.py
@@ -0,0 +1,5 @@
+from odoo import models
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
