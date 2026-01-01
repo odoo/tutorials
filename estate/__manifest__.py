ce13e139b9d79fce0921a49a84157ac3ed40b9b2
mibav-odoo <mibav@odoo.com>
2026-01-01 17:57:32 +0530

    [ADD] estate:add basic menus, form and list views
    - Created estate property form and list view
    - Created search bar fields
    
    Chapter 5: Finally, Some UI To Play With
    Chapter 6: Basic Views

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,7 +4,9 @@
     'summary': 'This is the Real Estate Advertisement module',
     'description': 'This is the Real Estate Advertisement module.',
     'data': [
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
     ],
     'author': 'Milan Bavishi',
     'license': 'LGPL-3'
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,17 +1,19 @@
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta
 
 
 class EstateProperty(models.Model):
     _name = "estate_property"
     _description = "This is the table of real estate property data"
 
-    name = fields.Char(required=True)
+    name = fields.Char(required=True, default="Unknown")
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False,
+                                    default=lambda self: fields.Date.today() + relativedelta(month=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -23,3 +25,15 @@ class EstateProperty(models.Model):
                    ('south', 'South'),
                    ('east', 'East'),
                    ('west', 'West')])
+    state = fields.Selection(
+        string='State',
+        default='new',
+        required=True,
+        copy=False,
+        selection=[('new', 'New'),
+                   ('offer received', 'Offer Received'),
+                   ('offer accepted', 'Offer accepted'),
+                   ('sold', 'Sold'),
+                   ('cancelled', 'Cancelled')]
+    )
+    active = fields.Boolean()
