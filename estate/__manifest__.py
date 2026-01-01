26ff004eec974c91fc9fcc27bba64e3493f2990e
japat-odoo <japat@odoo.com>
2026-01-01 18:15:00 +0530

    [IMP] estate: done ui major search, list, form
    -Added the list and form view for proper readability with the group feature
    -Added the search functionality, i.e, available and Postcode
    -Added the security read, write, and create rules  to solve the warning message

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,4 +7,10 @@
     'description': """
     Hello , Real Estate for everyone
     """,
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
+    ],
+    "application": True,
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import models, fields
+from datetime import date, timedelta
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -7,17 +8,19 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False,default=lambda self: fields.Date.today() + timedelta(days=90))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True,copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
-        string='Direction',
-        selection=[('east','East'),('west','West'),('north','North'),('south','South')],
+        string='Direction', 
+        selection=[('east', 'East'), ('west', 'West'), ('north', 'North'), ('south', 'South')],
     )
-
+    active = fields.Boolean(default=True)
+    state = fields.Selection(string="state", selection=[('new','New'),('offer_received','Offer Received'),('offer_accepted','Offer Accepted'),('sold_and_cancelled','Sold and Cancelled')])
+    
