b90bee4ad3130b6471aba306a2b90ff77c7480b5
javek-odoo <javek@odoo.com>
2026-01-01 12:55:02 +0530

    [IMP] estate: created views, menuitem
    modified manifest file.
    added action view in estate property.
    added more fields to model
    Chapter 5: Finally, Some UI To Play With

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -9,6 +9,8 @@
     'application': True,
     'license': 'LGPL-3',
     'data': [
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
     ]
 }
