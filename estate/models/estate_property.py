363b4bf70ac1a370eb623a9b6aa670e9ca3d8f1e
mekav-odoo <mekav@odoo.com>
2026-01-01 19:09:49 +0530

    [IMP] estate: complete ch-5 and start ch-6
    complete field, attribute , view in ch-5 and list and form in ch-6

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import fields , models
+from odoo import fields, models
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -7,15 +7,22 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=fields.Date.add(fields.Date.today(), months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
-    garden_orientation = fields.Selection( 
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        required=True,
+        default='New',  
+        copy=False, 
+        selection=[('New', 'new'), ('Offer Received', 'offer received'), ('Offer Accepted', 'offer accepted'), ('Sold', 'sold'), ('Cancelled', 'cancelled')])
+    garden_orientation = fields.Selection(
         string='Type',
-        selection=[('North','north'),('South','south'),('East','east'),('West','west')])
+        selection=[('North', 'north'), ('South', 'south'), ('East', 'east'), ('West', 'west')])
+
