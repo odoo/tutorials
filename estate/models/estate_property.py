40526e2344e40c5eb7a68e53ace9ce0ec8435dba
pkhu-odoo <pkhu@odoo.com>
2026-01-01 11:08:51 +0530

    [ADD] estate: completed Chapters 1–5 of Odoo tutorial Demo
    - Covered Odoo architecture concepts
    - Initialized a new application
    - Implemented core models and fields
    - Added basic security configuration
    - Created simple UI views to interact with data

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,5 +1,5 @@
-from odoo import models,fields
-from datetime import date, timedelta
+from odoo import models, fields
+from datetime import timedelta
 
 class Property(models.Model):
     _name = "estate.property"
@@ -8,16 +8,32 @@ class Property(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(copy=False,default=lambda self: fields.Date.today() + timedelta(days=90))
-    expected_price =  fields.Float(required=True)
-    selling_price = fields.Float(readonly=True,copy=False)
+    date_availability = fields.Date(copy=False, default=lambda self: fields.Date.today() + timedelta(days=90))
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
-    garden_orientation = fields.Selection(selection=[('north','North'),('west','West'),('east','East'),('south','South')])
+    garden_orientation = fields.Selection(
+        selection=[
+            ('north', 'North'),
+            ('west', 'West'),
+            ('east', 'East'),
+            ('south', 'South')
+            ])
     active = fields.Boolean(default=False)
-    state = fields.Selection(default='New',selection=[('New','New'),('Offer Received','Offer Received'),
-    ('Offer Accepted','Offer Accepted'),('Sold','Sold'),('Cancelled','Cancelled')],copy=False,required=True)
+    state = fields.Selection(
+        selection=[
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('cancelled', 'Cancelled')
+        ],
+        default='new',
+        copy=False,
+        required=True
+    )
