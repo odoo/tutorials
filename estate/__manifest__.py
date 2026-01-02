833d4f5e32427af5786e38e9cac4239461b14bb7
japat-odoo <japat@odoo.com>
2026-01-02 17:21:11 +0530

    [IMP] estate: add property offer model and relations
    - Created the 'estate.property.offer' model with fields
    - Add a One2many relationship 'offer_ids' to the 'estate.property' model.
    - Implement list and form views for the offer model.
    - Update the property form view to display offers in a dedicated tab.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -10,7 +10,10 @@
     "data": [
         "security/ir.model.access.csv",
         "views/estate_property_views.xml",
+        "views/estate_property_type_views.xml",
         "views/estate_menus.xml",
+        "views/estate_property_tag_views.xml",
+        "views/estate_property_offer.xml",
     ],
     "license": "LGPL-3",
     "application": True,
