46d3a73abf005d58aa0dc1b7e0b1fdb04ce617b5
mekot-odoo <mekot@odoo.com>
2026-01-02 10:11:29 +0530

    [IMP] estate: add view of list,form and search
    In this commit i add the customize list view in this module for state property ,
    also customize the form view as given instruction and in search ad filter and
    group by feature.
    
    chapter:-5,6

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,22 +1,16 @@
 {
-    'name': "Real estate",
-    'version': '1.0.0',
-    'depends': ['base','web'],
-    'author': "Mehul Kotak",
-   'category': 'Tutorials',
-    'description': "This Real Estate app in which the ad for any propertiues can add and also buyer can see and give thier price for that property",
-    'application': True,
-    'installable': True,
-
-    'licence': 'LGPL-3',
-
-    'data':[
-        
-        'security/ir.model.access.csv',
-        
-        
-        
-        'views/estate_property_views.xml',
-        'views/estate_menus.xml']
-
+    "name": "Real estate",
+    "version": "1.0.0",
+    "depends": ["base", "web"],
+    "author": "Mehul Kotak",
+    "category": "Tutorials",
+    "description": "This Real Estate app in which the ad for any propertiues can add and also buyer can see and give thier price for that property",
+    "application": True,
+    "installable": True,
+    "license": "LGPL-3",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
+    ],
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,37 +1,40 @@
-from odoo import models,fields
+from odoo import models, fields
+
 
 class EstateProperty(models.Model):
     _name = "estate_property"
-    _description="Storing Properties of Real Estate" 
+    _description = "Storing Properties of Real Estate"
 
-    name=fields.Char(string='property_name',required=True)
-    description=fields.Text(string='property_description')
-    pastcode=fields.Char(string='property_pastcode')
-    date_availability =fields.Date(string='property_date_availability')
-    expected_price=fields.Float(string='property_expected_price',required=True)
-    selling_price=fields.Float(string='property_selling_price')
-    bedrooms = fields.Integer(string="Bedrooms")
+    name = fields.Char(string="property_name", required=True)
+    description = fields.Text(string="property_description")
+    postcode = fields.Char("property_postcode")
+    date_availability = fields.Date(
+        string="property_date_availability",
+        default=fields.Date.add(fields.Date.today(), months=3),
+        copy=False,
+    )
+    expected_price = fields.Float(string="property_expected_price", required=True)
+    selling_price = fields.Float(
+        string="property_selling_price", readonly=True, copy=False
+    )
+    bedrooms = fields.Integer(string="Bedrooms", default=2)
     living_area = fields.Integer(string="Living Area (sqm)")
     facades = fields.Integer(string="Facades")
     garage = fields.Boolean(string="Garage")
     garden = fields.Boolean(string="Garden")
     garden_area = fields.Integer(string="Garden Area (sqm)")
-    garden_orientation=fields.Selection([
-        ('North','north'),
-        ('South','south'),
-        ('East','east'),
-        ('West','west')
-    ])
-
-
-
-    
-
-
-
-
-
-
-    
-
-    
+    garden_orientation = fields.Selection(
+        [("North", "north"), ("South", "south"), ("East", "east"), ("West", "west")]
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        [
+            ("New", "new"),
+            ("Offer Received", "Offer Received"),
+            ("Offer Accepted", "Offer Accepted"),
+            ("Sold", "sold"),
+            ("Cancelled", "cancelled"),
+        ],
+        default="New",
+        copy=False,
+    )
