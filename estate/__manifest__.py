33bbbad249697f2d7136dc21ada9e738e846ce8b
shtha-odoo <shtha@odoo.com>
2026-01-02 12:12:45 +0530

    [IMP] estate: chapter-5,6 (UI+VIEWS) Completed

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,17 +1,18 @@
 {
-    'name': "Real Estate",
-    'version': '1.0.0',
-    'depends': ['base'],
-    'author': "john addams",
-    'category': 'Tutorials',
-    'description': "ytyut",
-    'application': True,
-    'installable': True,
-    'license': 'LGPL-3',
-
-    'data': [
-        'Security/ir.model.access.csv'
-    ]
+    "name": "Real Estate",
+    "version": "1.0.0",
+    "depends": ["base","web"],
+    "author": "john addams",
+    "category": "Tutorials",
+    "description": "ytyut",
+    "application": True,
+    "installable": True,
+    "license": "LGPL-3",
+    "data": [
+        "Security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
+    ],
     # # data files containing optionally loaded demonstration data
     # 'demo': [
     #     'demo/demo_data.xml',
--- estate/models/home_plan.py
+++ estate/models/home_plan.py
@@ -1,25 +1,44 @@
 from odoo import fields, models
 
+
 class homePlan(models.Model):
     _name = "home.plan"
     _description = "this is home plan"
-    
-    name = fields.Char('Plan Name', required=True)
-    description = fields.Char('Description')
-    postcode = fields.Char('Post code', required=True)
-    date_availability = fields.Datetime('available till', readonly=True)
-    number_of_months = fields.Integer('# Months', required=True)
-    expected_price = fields.Float('Price', required=True)
-    Garage = fields.Boolean('Active', default=True)
 
-    orientation_direction = fields.Selection(
-        [('North','north'),
-        ('East','east'),
-        ('West','west'),
-        ('South','south')
-        ]
+    name = fields.Char("Plan Name", required=True, default="Unknown")
+    description = fields.Char("Description")
+    postcode = fields.Char("Post code", required=True)
+    date_availability = fields.Datetime(
+        "Available till",
+        copy=False,
+        default=fields.Date.add(fields.Date.today(), months=3),
+    )
+    last_seen = fields.Datetime("Last Seen", default=fields.Datetime.now)
+    expected_price = fields.Float("Expected Price")
+    selling_price = fields.Float("Selling price", copy=False, readonly=True)
+    bedrooms = fields.Integer("bedrooms", default="2")
+    living_area = fields.Integer("Living area")
+    facades = fields.Integer("facades")
+    Garage = fields.Boolean("Garage", default=True)
+    Garden = fields.Boolean("Garden")
+    Garden_area = fields.Integer("Garden area")
+    active = fields.Boolean("Active", default=True)
+
+    State = fields.Selection(
+        [
+            ("New", "new"),
+            ("Offer Received", "offer received"),
+            ("Offer Accepted", "offer accepted"),
+            ("Sold", "sold"),
+            ("Cancelled", "cancelled"),
+        ],
+        default="New",
+        copy=False,
     )
 
+    Garden_orientation_direction = fields.Selection(
+        [("North", "north"), ("East", "east"), ("West", "west"), ("South", "south")]
+    )
 
     # _sql_constraints = [
     #     ('check_number_of_months', 'CHECK(number_of_months >= 0)', 'The number of month can\'t be negative.'),
