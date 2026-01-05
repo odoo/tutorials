399ea739c62af242a1e53358e9b97c4414ba98c9
krpat-odoo <krpat@odoo.com>
2026-01-05 17:19:36 +0530

    [ADD] estate: Perform Many2One and Many2Many relationship
    Added Many2one relationship for estate property types and
    Many2many relationship for estate property tags.
    
    Implement list and form views for property types and tags

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -35,7 +35,7 @@ class EstateProperty(models.Model):
     )
     active = fields.Boolean(default=True)
     state = fields.Selection(
-         [
+        [
             ('new', 'New'),
             ('offer_received', 'Offer Received'),
             ('offer_accepted', 'Offer Accepted'),
@@ -45,4 +45,22 @@ class EstateProperty(models.Model):
         required=True,
         copy=False,
         default='new',
+    )
+    property_type_id = fields.Many2one(
+        'estate.property.type',
+        string='Property Type'
+    )
+    buyer_id = fields.Many2one(
+        'res.partner',
+        string='Buyer',
+        copy=False,
+    )
+    user_id = fields.Many2one(
+        'res.users',
+        string='Salesman',
+        default=lambda self: self.env.user
+    )
+    tag_ids = fields.Many2many(
+        'estate.property.tag',
+        string='Tags',
     )
---
+++ estate/models/estate_property_type.py
@@ -0,0 +1,10 @@
+from odoo import models, fields
+
+
+class EstatePropertyType(models.Model):
+    _name = 'estate.property.type'
+    _description = 'Estate Property Type'
+
+    name = fields.Char(
+        required=True
+    )
