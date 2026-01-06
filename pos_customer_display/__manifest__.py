80e71c9b54f40c29f9f4131fe24d30a0883869bf
Ravij Parikh <snrav@odoo.com>
2026-01-06 23:15:48 +0530

    [IMP] pos_customer_display: add customer display template
    Added a new customer display XML template to show additional order
    information on the secondary screen. The template displays the customer
    name, amount per guest, and prepares structure for showing refund lines
    separately.
    
    Impact:
    - Improves visibility of key order details for customers.
    - Enhances clarity on the customer-facing display.

--- pos_customer_display/__manifest__.py
+++ pos_customer_display/__manifest__.py
@@ -2,8 +2,16 @@
     "name": "POS Customer Display",
     "version": "1.0",
     "author": "Ravij Parikh (snrav)",
-    "Description": "Display customer name, amount/guest and refund lines in POS",
+    "description": "Display customer name, amount/guest and refund lines in POS",
     "depends": ["point_of_sale"],
     "application": True,
-    "license": "LGPL-3"
+    "license": "LGPL-3",
+    "assets": {
+        "point_of_sale.assets": [
+            "pos_customer_display/static/src/pos_order.js",
+        ],
+        "point_of_sale.customer_display_assets": [
+            "pos_customer_display/static/src/customer_display/*",
+        ],
+    }
 }
