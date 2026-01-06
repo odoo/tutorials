37413c2e7b3b8335b95d0faafa9aebc478404b5d
japat-odoo <japat@odoo.com>
2026-01-06 11:24:26 +0530

    [IMP] estate: add onchange and fix parseError
    - Add an onchange method for the garden field
    - Fix ParseError by ensuring actions are defined before menuitems
    - chapter 8

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -66,3 +66,12 @@ class Property(models.Model):
                 record.best_price = max(prices)
             else:
                 record.best_price = 0.0
+
+    @api.onchange("garden")
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = "north"
+        else:
+            self.garden_area = 0
+            self.garden_orientation = False
