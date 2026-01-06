6bb07352466359eb12d84171291220755e8d4ed2
krpat-odoo <krpat@odoo.com>
2026-01-06 16:05:07 +0530

    [ADD] estate:  Chp7 - One2many relation and property offer model
    Implemented the One2many relation
    
    Created the Estate Property Offer Model

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -64,3 +64,8 @@ class EstateProperty(models.Model):
         'estate.property.tag',
         string='Tags',
     )
+    offer_ids = fields.One2many(
+        'estate.property.offer',
+        'property_id',
+        string='Offers',
+    )
