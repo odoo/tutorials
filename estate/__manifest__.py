c5bae2e5ce8fad8ba6ad96697d66a81344f33145
kukor-odoo <kuko@odoo.com>
2026-01-07 12:26:31 +0530

    [FIX] estate: fixed the single and double quotes
    - Replaced inconsistent use of single and double quotes across Python files.
    - Fixed quote usage in the manifest file to align with coding standards.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,15 +1,15 @@
 {
-    "name": "Real Estate",
-    "author": "Kunj Koradiya",
-    "description": "This is the description",
-    "license": "LGPL-3",
-    "depends": [],
-    "data": [
-        "security/ir.model.access.csv",
-        "views/estate_property_views.xml",
-        "views/estate_type_views.xml",
-        "views/estate_property_tags.xml",
-        "views/estate_property_offers.xml",
-        "views/estate_menus.xml",
+    'name': "Real Estate",
+    'author': "Kunj Koradiya",
+    'description': "This is the description",
+    'license': "LGPL-3",
+    'depends': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_type_views.xml',
+        'views/estate_property_tags.xml',
+        'views/estate_property_offers.xml',
+        'views/estate_menus.xml',
     ],
 }
