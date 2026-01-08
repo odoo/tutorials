d36512b67809fb6eb5c73310bba063fa3cd8d845
Ravij Parikh <snrav@odoo.com>
2026-01-08 22:59:04 +0530

    [ADD] pos_improvements: add manifest file Create the module manifest to declare metadata, dependencies, and assets required for the POS improvement  module.

---
+++ pos_improvements/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "POS - Improvements",
+    "version": "1.0",
+    "description": "Enable direct payment from the ticket screen in POS",
+    "author": "Ravij Parikh (snrav)",
+    "application": True,
+    "depends": ["point_of_sale"],
+    "license": "LGPL-3"
+}
