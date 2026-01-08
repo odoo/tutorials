f338d5f780a22bb4e2f2f480c5564f1c3f14bc4d
Robin Lejeune (role) <role@odoo.com>
2026-01-08 09:53:00 +0100

    Laying ground - Website Framework

--- awesome_website/__manifest__.py
+++ awesome_website/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 {
     'name': "Awesome Website",
 
@@ -14,16 +13,17 @@
     'website': "https://www.odoo.com",
 
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # Check https://github.com/odoo/odoo/blob/19.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Tutorials',
     'version': '0.2',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'web'],
+    'depends': ['website'],
     'application': True,
     'installable': True,
     'data': [
+        'data/images.xml',
     ],
     'assets': {
     },
