3a423ce162d11d169a0e73d948ef6464c2fb119c
Ravij Parikh <snrav@odoo.com>
2026-01-10 00:30:43 +0530

    [IMP] pos_improvements: allow direct payment from TicketScreen
    Before:
    Cashiers were required to:
    1. Use "Load Order" to open a ticket
    2. Then press "Payment" to navigate to the PaymentScreen
    This resulted in an extra step during checkout.
    
    After:
    - Added a new "Payment" button next to "Load Order" on the TicketScreen
    - Clicking it loads the selected ticket and opens the PaymentScreen immediately
    
    Impact:
    Speeds up the cashier checkout process by reducing the number of actions needed to reach payment.
    
    Task: [5476673]

--- pos_improvements/__manifest__.py
+++ pos_improvements/__manifest__.py
@@ -5,5 +5,11 @@
     "author": "Ravij Parikh (snrav)",
     "application": True,
     "depends": ["point_of_sale"],
-    "license": "LGPL-3"
+    "license": "LGPL-3",
+     "assets": {
+        "point_of_sale.assets_prod": [
+            "pos_improvements/static/src/pos_ticket_screen/js/pos_ticket_screen.js",
+            "pos_improvements/static/src/pos_ticket_screen/xml/pos_ticket_screen.xml"
+        ],
+    }
 }
