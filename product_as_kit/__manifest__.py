ae57fc807d4c5285162102a5eec23a4c9d9ef95d
shsak-odoo <shsak@odoo.com>
2026-01-09 18:18:57 +0530

    [ADD] product_as_kit:  create new module to add new type to the product
    - inherits product.template model to add isKit and sub_products fields
    - inherits product template form view to show kit fields in ui

---
+++ product_as_kit/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Product as Kit",
+    "version": "1.0",
+    "depends": ["product", "sale_management"],
+    "author": "Shivam Saksham(shsak)",
+    "category": "Sales",
+    "description": """
+    An module to let the product be sold as kit.
+    """,
+    "license": "LGPL-3",
+    "data": [
+        "views/product_template_views.xml",
+    ],
+}
