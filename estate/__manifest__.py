2bfaa511c62b2f3f859be53073a4fd9e09de0a69
mekav-odoo <mekav@odoo.com>
2026-01-13 19:02:15 +0530

    [IMP] estate: complete ch-12 Inheritance
    done implementation of  model inheritance & view inheritance to display the list
     of properties linked to a salesperson directly in the Settings / Users form
     view and implemented  ondelete() and create() functions.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -15,6 +15,7 @@
         "views/estate_property_type_views.xml",
         "views/estate_property_maintenance_request_views.xml",
         "views/estate_property_views.xml",
+        "views/res_users_view.xml",
         "views/estate_menus.xml",
     ],
     'assets': {},
