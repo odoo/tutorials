abd9e6b4943c438405a604021c89e54f05304dd8
Amr Elkhatieb <amahm@odoo.com>
2026-01-19 17:20:13 +0100

    Add estate property model

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,6 +3,8 @@
     'version': '1.9',
     'category': 'Real Estate',
     'summary' : 'Manage your real estate properties',
+    'author': 'Odoo',
+    'license': 'LGPL-3',
     'depends': ['base'],
     'installable': True,
     'application': True,
---
+++ estate/models/estate_property.py
@@ -0,0 +1,33 @@
+from odoo import models, fields
+
+class EstateProperty(models.Model):
+    _name = 'estate.property'
+    _description = 'Estate Property'
+    name = fields.Char(string='Name', required=True)
+
+    property_type = fields.Selection([
+        ('apartment', 'Apartment'),
+        ('house', 'House'),
+        ('villa', 'Villa'),
+    ], string='Property Type')
+    postcode = fields.Char(string='Postcode')
+    available_from = fields.Date(string='Available From')
+
+    expected_price = fields.Float(string='Expected Price')
+    selling_price = fields.Float(string='Selling Price')
+    best_offer = fields.Float(string='Best Offer')
+
+    description = fields.Text(string='Description')
+    bedrooms = fields.Integer(string='Bedrooms')
+    living_area = fields.Integer(string='Living Area (sqm)')
+    facades = fields.Integer(string='Facades')
+    garage = fields.Boolean(string='Garage')
+    garden = fields.Boolean(string='Garden')
+    garden_area = fields.Integer(string='Garden Area (sqm)')
+    garden_orientation = fields.Selection([
+        ('north', 'North'),
+        ('south', 'South'),
+        ('east', 'East'),
+        ('west', 'West'),
+    ], string='Garden Orientation')
+    total_area = fields.Integer(string='Total Area (sqm)')
