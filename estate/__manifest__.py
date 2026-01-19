078cbd168c3c5f2b7ba7cad96b336eb233febefb
mohamedakhalil18 <mohamedakhalil18@gmail.com>
2026-01-19 15:40:26 +0100

    [ADD] Estate: Added estate property module declaration and models Added a new module to the odoo ERP suite to enable users to manage the real estate listings and properties through the odoo software.

---
+++ estate/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Estate Advirtisements',
+    'version':'1.0',
+    'summary':'Track advertisements added for real estate and allow selling them',
+    'website':'https://www.odoo.com/app/estate',
+    'depends':[
+        'base'
+        ],
+    'application':True,
+    'installable':True,
+    'author':'moali',
+    'license':'LGPL-3'
+}
---
+++ estate/models/estate_property.py
@@ -0,0 +1,27 @@
+from odoo import fields, models
+
+class EstateProperty(models.Model):
+    _name = 'estate_property'
+    _description = 'Estate Property'
+
+    name = fields.Char(string = "Property Name", required = True)
+    description = fields.Char(string = 'Property Description')
+    postcode = fields.Char(string = 'Postal Code')
+    date_availability = fields.Date(string = 'Available From')
+    expected_price = fields.Float(string = 'Expected Selling Price', required = True)
+    selling_price = fields.Float(string = 'Actual Selling Price')
+    bedrooms = fields.Integer(string = 'Number of Bedrooms')
+    living_area = fields.Float(string = 'Living Area (sqm)')
+    facades = fields.Integer(string = 'Number of Facades')
+    garage = fields.Boolean(string = 'Garage')
+    garden = fields.Boolean(string = 'Garden')
+    garden_area = fields.Float(string = 'Garden Area (sqm)')
+    garden_orientation = fields.Selection(
+        string = 'Garden Orientation',
+        selection = [
+            ('north', 'North'),
+            ('south', 'South'),
+            ('east', 'East'),
+            ('west', 'West')
+        ]
+    )
