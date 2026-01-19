7d946f02cf0ef2bbdbd379ea27fd5fc19421d315
Dilya Anvarbekova <dianv@odoo.com>
2026-01-19 17:37:13 +0100

    [IMP] estate: add UI interaction by creating the form and adding menus - created menus to see the fields of the model

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,6 +4,8 @@
     'depends': ['base'],
     'data': [
         'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ],
     'application': True,
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -7,14 +7,22 @@ class EstateProperty(models.Model):
     name = fields.Char(string='Property Name', required=True)
     description = fields.Text(string='Description')
     postcode = fields.Char(string='Postcode')
-    date_availability = fields.Date(string='Available Date')
+    date_availability = fields.Date(string='Available Date', copy=False, default=(fields.Date.add(fields.Date.today(), months=3)))
     expected_price = fields.Float(string='Expected Price', required=True)
-    selling_price = fields.Float(string='Selling Price')
-    bedrooms = fields.Integer(string='Number of Bedrooms')
+    selling_price = fields.Float(string='Selling Price', readonly=True, copy=False)
+    bedrooms = fields.Integer(string='Number of Bedrooms', default=2)
     living_area = fields.Integer(string='Living Area')
     facades = fields.Integer(string='Number of Facades')
     garage = fields.Boolean(string='Garage')
     garden = fields.Boolean(string='Garden')
     garden_area = fields.Integer(string='Garden Area')
-    garden_orientation = fields.Selection(('north', 'south', 'east', 'west'), string='Garden Orientation')
-    
+    garden_orientation = fields.Selection([('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')], string='Garden Orientation')
+    active = fields.Boolean(string='Active', default=True)
+    state = fields.Selection([
+        ('new', 'New'),
+        ('offer_received', 'Offer Received'),
+        ('offer_accepted', 'Offer Accepted'),
+        ('sold', 'Sold'),
+        ('canceled', 'Canceled')
+    ], string='Status', default='new', required=True, copy=False
+    )
