52cf939b2439531c6a388e196c6c05af488ae596
Yoann Baron <yoannbaron@orange.fr>
2026-01-19 17:14:08 +0100

    [IMP] estate: create views for estate models
    Added list and form views, as well as search filters for estate property
    model.
    
    This corresponds to chapter 6 of the technical onboarding exercise.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -7,18 +7,18 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real estate property model"
 
-    name = fields.Char(required=True)
+    name = fields.Char(required=True, string="Title")
     description= fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(copy=False, default=datetime.date.today() + relativedelta(months=3))
+    date_availability = fields.Date(copy=False, default=datetime.date.today() + relativedelta(months=3), string="Available From")
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
+    living_area = fields.Integer(string="Living Area (sqm)")
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Integer()
+    garden_area = fields.Integer(string="Garden Area (sqm)")
     garden_orientation = fields.Selection(
         selection=[
             ('north', 'North'),
@@ -34,5 +34,6 @@ class EstateProperty(models.Model):
             ("offer_received", "Offer Received"),
             ("offer_accepted", "Offer Accepted"),
             ("sold", "Sold"),
-            ("cancelled", "Cancelled")],
+            ("cancelled", "Cancelled")
+        ],
     )
