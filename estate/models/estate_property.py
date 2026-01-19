a172ae5c436a0cb90a453f4411fab51d539ef607
Thomas des Touches <thdes@odoo.com>
2026-01-19 16:45:57 +0100

    [IMP] Add fields to EstateProperty

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,19 @@
-from odoo import models
+from odoo import fields, models
 
 class EstateProperty(models.Model):
-    _name = "estate_property"
+    _name = 'estate_property'
+    _description = 'Estate Property'
+    
+    name = fields.Char('Name', required = True)
+    description = fields.Text('Description')
+    postcode = fields.Char('Post Code')
+    date_availability = fields.Date('Availability Date')
+    expected_price = fields.Float('Expected Price', required = True)
+    selling_price = fields.Float('Selling Price')
+    bedrooms = fields.Integer('# Bedrooms')
+    living_area = fields.Integer('Living Area')
+    facades = fields.Integer('# Facades')
+    garage = fields.Boolean('Garage')
+    garden = fields.Boolean('Garden')
+    garden_area = fields.Integer('Garden area')
+    garden_orientation = fields.Selection('Garden Orientation')
