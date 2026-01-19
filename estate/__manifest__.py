2aae33ed2dd5be993bd8e75cea12ed9a9f1634d9
Amr Elkhatieb <amahm@odoo.com>
2026-01-19 16:08:37 +0100

    adding real estate module

---
+++ estate/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Estate',
+    'version': '1.9',
+    'category': 'Real Estate',
+    'summary' : 'Manage your real estate properties',
+    'depends': ['base'],
+    'installable': True,
+    'application': True,
+}
