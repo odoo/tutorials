a54dcce093e615c632721a7612b2739e2e9e4fa5
Thomas des Touches <thdes@odoo.com>
2026-01-19 15:40:51 +0100

    [ADD] Define an empty "Estate" module
    This module will be the basis of the Estate App

---
+++ estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Estate',
+    'category': 'Sales/CRM',
+    'description': 'Advertise your real estate',
+    'author': '[THDES] Thomas des Touches',
+    'depends': [
+        'base_setup',       
+    ],
+    'application': True, 
+}
