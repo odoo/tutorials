b6e4d0697bef2c121f3cdcab2785daf4b18d81ab
anden-odoo <anden@odoo.com>
2026-01-19 17:45:06 +0100

    [IMP] estate: add informations field
    This adds fields to store information related to
    the property (name, description, price,
    living area…)

--- estate/models/property.py
+++ estate/models/property.py
@@ -1,5 +1,19 @@
-from odoo import models
+from odoo import fields, models
 
 class EstateProperty(models.Model):
     _name = "estate_property"
     _description = "estate property"
+
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(string="Date availability")
+    expected_price = fields.Float(string="Expected price", required=True)
+    selling_price = fields.Float(string="Selling price")
+    bedrooms = fields.Integer()
+    living_area = fields.Integer(string="Living area")
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer(string="Garden area")
+    garden_orientation = fields.Selection(string="Garden orientation", selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
