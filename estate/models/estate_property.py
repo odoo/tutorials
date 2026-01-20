7925fa1682f6e4a83fde0d54d0dc9124767f1ab4
Thomas des Touches <thdes@odoo.com>
2026-01-20 13:31:29 +0100

    [IMP] Add active and state fields to EstateProperty

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -8,14 +8,17 @@ class EstateProperty(models.Model):
     name = fields.Char('Name', required=True)
     description = fields.Text('Description')
     postcode = fields.Char('Post Code')
+
     date_availability = fields.Date('Availability Date', copy=False,
         default=fields.Date.add(fields.Date.today(), months=3))
     expected_price = fields.Float('Expected Price', required=True)
     selling_price = fields.Float('Selling Price', readonly=True, copy=False)
+
     bedrooms = fields.Integer('# Bedrooms', default=2)
     living_area = fields.Integer('Living Area')
     facades = fields.Integer('# Facades')
     garage = fields.Boolean('Garage')
+
     garden = fields.Boolean('Garden')
     garden_area = fields.Integer('Garden area')
     garden_orientation = fields.Selection(
@@ -24,3 +27,13 @@ class EstateProperty(models.Model):
             ('north', 'North'), ('south', 'South'),
             ('east', 'East'), ('west', 'West')
         ])
+
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        selection=[
+            ('new','New'), ('offer_received','Offer Received'),
+            ('offer_accepted','Offer Accepted'),
+            ('sold','Sold'), ('cancelled','Cancelled'), ],
+        default='new',
+        required=True
+    )
