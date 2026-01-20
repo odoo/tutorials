b75db3bf87c265cc6e01a81d6a7fff822f8c2621
Ester Andreetto <eesteerina@gmail.com>
2026-01-20 09:31:23 +0100

    [ADD] some elements added

---
+++ estate/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Estate",
+    "version": "1.0",
+    "summary": "Real Estate Property Management",
+    "description": """
+        Tutorial module for managing real estate properties.
+    """,
+    "author": "Your Name",
+    "category": "Tutorial",
+    "depends": ["base"],
+    "data": [],
+    "installable": True,
+    "application": True,
+}
