5fedcdf74c868da97a4919bd8c773174f15bcf9b
mohamedakhalil18 <mohamedakhalil18@gmail.com>
2026-01-20 11:19:26 +0100

    [FIX] Estate: Added linting and formatting Added the linting and formatting required through ruff in order to follow the coding guidelines and conventions set at Odoo to maintain a proper and clean codebase

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,13 +1,11 @@
 {
-    'name': 'Estate Advirtisements',
-    'version':'1.0',
-    'summary':'Track advertisements added for real estate and allow selling them',
-    'website':'https://www.odoo.com/app/estate',
-    'depends':[
-        'base'
-        ],
-    'application':True,
-    'installable':True,
-    'author':'moali',
-    'license':'LGPL-3'
+    "name": "Estate Advirtisements",
+    "version": "1.0",
+    "summary": "Track advertisements added for real estate and allow selling them",
+    "website": "https://www.odoo.com/app/estate",
+    "depends": ["base"],
+    "application": True,
+    "installable": True,
+    "author": "moali",
+    "license": "LGPL-3",
 }
