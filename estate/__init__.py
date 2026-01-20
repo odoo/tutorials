06130c719d2f70f9483820f5f6554f1a9945ea28
mohamedakhalil18 <mohamedakhalil18@gmail.com>
2026-01-20 15:31:07 +0100

    [IMP] Estate: Added action and menus to enable user to add properties Improved the functionalities available in the system to allow for user input through the basic form view to enable data entry of the real estate listings

--- estate/__init__.py
+++ estate/__init__.py
@@ -1,2 +1,3 @@
 from . import models
 from . import security
+from . import views
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -2,16 +2,22 @@ from odoo import fields, models
 
 
 class EstateProperty(models.Model):
-    _name = "estate_property"
+    _name = "estate.property"
     _description = "Estate Property"
 
     name = fields.Char(string="Property Name", required=True)
     description = fields.Char(string="Property Description")
     postcode = fields.Char(string="Postal Code")
-    date_availability = fields.Date(string="Available From")
+    date_availability = fields.Date(
+        string="Available From",
+        default=fields.Date.add(value=fields.Date.today(), months=3),
+        copy=False,
+    )
     expected_price = fields.Float(string="Expected Selling Price", required=True)
-    selling_price = fields.Float(string="Actual Selling Price")
-    bedrooms = fields.Integer(string="Number of Bedrooms")
+    selling_price = fields.Float(
+        string="Actual Selling Price", readonly=True, copy=False
+    )
+    bedrooms = fields.Integer(string="Number of Bedrooms", default=2)
     living_area = fields.Float(string="Living Area (sqm)")
     facades = fields.Integer(string="Number of Facades")
     garage = fields.Boolean(string="Garage")
@@ -25,4 +31,17 @@ class EstateProperty(models.Model):
             ("east", "East"),
             ("west", "West"),
         ],
+    )
+    active = fields.Boolean(string="Active", default=True)
+    status = fields.Selection(
+        string="Status",
+        selection=[
+            ("new", "New"),
+            ("offer received", "Offer Received"),
+            ("offer accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled"),
+        ],
+        default="new",
+        copy=False,
     )
