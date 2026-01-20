e59b4ce2292ff90b1c5162152a1bacb5374017f9
Hazem Ibrahim <waheedfcai@gmail.com>
2026-01-20 15:54:50 +0100

    [ADD] estate: add real estate module
    This module provides managing real estate advertisements.
    
    It introduces the \`estate.property\` model with fields for:
    - Property descriptions (name, description, postcode, etc.)
    - Pricing (expected and selling price)
    - Physical characteristics (bedrooms, living area, facades)
    - Garden information (area and orientation)

---
+++ estate/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Real Estate',
+    'category': 'tutorials',
+    'summary': 'Real estate module to sell/rent houses and offices',
+    'description': """
+    immoweb alias xd
+    """,
+    'website': 'https://www.odoo.com/app/real_estate',
+    'depends': [
+        'base_setup',
+        'base'
+    ],
+    'installable': True,
+    'application': True,
+    'author': 'ibrha',
+}
