b93b8d1ce45ca683fa57397870d8bed6d1c32868
MAGREMENT <zavan@odoo.com>
2026-01-20 10:58:59 +0100

    [ADD] estate : Added Chapter 6 features
    Created the list, form & search views

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -9,7 +9,7 @@ class EstateProperty(models.Model) :
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(copy=False, default=fields.Date.today() + relativedelta(months=3))
+    date_availability = fields.Date("Available From", copy=False, default=fields.Date.today() + relativedelta(months=3))
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
