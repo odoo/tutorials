58c8f38682d1995ed748243d31d1c5c1cc8d9ae5
Gendi (HOELG) <hossameljendy@gmail.com>
2026-01-20 15:19:37 +0100

    [IMP] estate: add access for estate_property
    Adding read access right for base.group_user

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,6 +3,9 @@
     "depends": [
         "base",
     ],
+    "data": [
+        "security/ir.model.access.csv",
+    ],
     "application": True,
     "installable": True,
     "author": "Gendi (HOELG)",
