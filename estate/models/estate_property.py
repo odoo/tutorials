2edd4dd22311c64de1a4eb86ca429294e743e153
MAGREMENT <zavan@odoo.com>
2026-01-20 09:40:21 +0100

    [ADD] estate : Added chapter 5 features
    Made the basic menus, added the Properties action and edited the properties

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,6 @@
 from odoo import fields, models
+from datetime import date
+from dateutil.relativedelta import relativedelta
 
 class EstateProperty(models.Model) :
     _name = 'estate.property'
@@ -7,13 +9,17 @@ class EstateProperty(models.Model) :
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=fields.Date.today() + relativedelta(months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        selection=[('new', 'New'), ('offer-received', 'Offer Received'), ('offer-accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')],
+        copy=False, default="new", required=True)
