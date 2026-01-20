8065ba31641370c7414cba43f79099c2f4820c77
TudorCalinCS <panzaru.tudor@gmail.com>
2026-01-20 13:13:00 +0100

    [FIX] estate: Update estate/__manifest__.py
    Co-authored-by: plha-odoo <plha@odoo.com>

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,7 +7,7 @@
     'depends': [
         'base'
     ],
-    'data':[
+    'data': [
         'data/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_menu.xml'
