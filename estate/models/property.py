b330f0eb5bb4cc3c08a580912dcb83152b7700cf
anden-odoo <anden@odoo.com>
2026-01-20 11:32:04 +0100

    [IMP] estate: change attributes of some fields
    This changes attributes of selling price to read-only
    and prevents the copy of availability date and selling
    price fields.

--- estate/models/property.py
+++ estate/models/property.py
@@ -5,17 +5,16 @@ class EstateProperty(models.Model):
     _name = 'estate_property'
     _description = 'estate property'
 
-    name = fields.Char(required=True)
+    name = fields.Char(string='Title', required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(string='Date availability')
+    date_availability = fields.Date(string='Date availability', copy=False)
     expected_price = fields.Float(string='Expected price', required=True)
-    selling_price = fields.Float(string='Selling price')
+    selling_price = fields.Float(string='Selling price', readonly=True, copy=False)
     bedrooms = fields.Integer()
-    living_area = fields.Integer(string='Living area')
+    living_area = fields.Integer(string='Living area (sqm)')
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Integer(string='Garden area')
+    garden_area = fields.Integer(string='Garden area (sqm)')
     garden_orientation = fields.Selection(string='Garden orientation', selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
-    
