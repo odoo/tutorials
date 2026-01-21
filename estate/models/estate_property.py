40493c225a7fc770e97db75fa609344fe1d0d196
Hazem Ibrahim <waheedfcai@gmail.com>
2026-01-21 13:40:59 +0100

    [IMP] estate: add views, menus and property fields
    adds the initial UI for the estate module:
    - Add action and menus in XML files.
    - Add new fields and modify the existing fields.
    - Update manifest to include the new XML files.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,3 +1,4 @@
+from dateutil.relativedelta import relativedelta
 from odoo import models, fields
 
 class Property(models.Model):
@@ -7,17 +8,32 @@ class Property(models.Model):
     name = fields.Char(string="Title", required=True)
     description = fields.Text(string="Description")
     postcode = fields.Char(string="Postcode")
-    date_availability = fields.Date(string="Available From")
+    date_availability = fields.Date(string="Available From", default=fields.Date.today() + relativedelta(months=3), copy=False)
     expected_price = fields.Float(string="Expected Price", required=True)
-    selling_price = fields.Float(string="Selling Price")
-    bedrooms = fields.Integer(string="Bedrooms")
+    selling_price = fields.Float(string="Selling Price", readonly=True, copy=False)
+    bedrooms = fields.Integer(string="Bedrooms", default=2)
     living_area = fields.Integer(string="Living Area")
     facades = fields.Integer(string="Facades")
     garage = fields.Boolean(string="Garage")
     garden = fields.Boolean(string="Garden")
     garden_area = fields.Integer(string="Garden Area")
-
+    active = fields.Boolean(string="Active", default=True)
     garden_orientation = fields.Selection(
         string='Garden Orientation',
-        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
+        selection=[
+            ('north', 'North'), 
+            ('south', 'South'),
+            ('east', 'East'),
+            ('west', 'West')
+        ]
+    )
+    state = fields.Selection(
+        string='Status',
+        selection=[
+            ('new', 'New'), 
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'), 
+            ('cancelled', 'Cancelled')
+        ]
     )
