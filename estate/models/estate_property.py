a6fb90447c0f342ebc4debad096a66b17509a290
Gendi (HOELG) <hossameljendy@gmail.com>
2026-01-21 17:27:42 +0100

    [IMP] estate: implement basic views
    Adding basic (form, list, search) views for the propety model along with some UI customizations

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -7,21 +7,22 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real state property"
 
-    name = fields.Char(required=True)
+    name = fields.Char(required=True, string="Title")
     description = fields.Text()
     postcode = fields.Char()
     date_availability = fields.Date(
         copy=False,
         default=fields.Date.today() + relativedelta(months=3),
+        string="Available From",
     )
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
+    living_area = fields.Integer(string="Living Area (sqm)")
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Integer()
+    garden_area = fields.Integer(string="Garden Area (sqm)")
     garden_orientation = fields.Selection(
         selection=[
             ("north", "North"),
