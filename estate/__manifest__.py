476383d437cb485a4eca59aaf94d25670d74e90c
anden-odoo <anden@odoo.com>
2026-01-21 09:39:10 +0100

    [IMP] estate: add salesperson and buyer as field
    This adds add salesperson and buyer as field of the
    property module. This also adds salesperson and buyer
    in the property form.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,9 +1,10 @@
 {
     'name': "Real estate",
     'depends': ['base'],
-    'category': 'Tutorials',
+    'category': 'Real Estate/Brokerage',
     'application': True,
     'data': [
+        # 'security/security.xml',
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_property_type_views.xml',
--- estate/models/property.py
+++ estate/models/property.py
@@ -22,4 +22,6 @@ class EstateProperty(models.Model):
     state = fields.Selection(selection=[('new', 'New'), ('offer received', 'Offer Received'), ('offer accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')], required=True, copy=False, default='new')
     
     property_type_id = fields.Many2one('estate_property_type', string='Property Type')
-    
+
+    salesperson = fields.Many2one('res.users', string='Salesman', default=lambda self: self.env.user)
+    buyer = fields.Many2one('res.partner', copy=False)
