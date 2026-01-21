46850b4ea6743626f133168ea90b839550ead5b5
Dilya Anvarbekova <dianv@odoo.com>
2026-01-21 10:01:27 +0100

    [IMP] estate: add property types, tags, and offers
    Chapter 7 of the Server 101 tutorial on model relationships.
    - Added a model for property types and added buyers and salesmen to the estate property model to showcase many2one relationships
    - Added tags to showcase many2many relationships
    - Added a model for offers to showcase one2many relationships
    - Created list and form views for the models and added fields to the property model

---
+++ estate/models/estate_property_offer.py
@@ -0,0 +1,11 @@
+from odoo import models, fields
+
+
+class EstatePropertyOffer(models.Model):
+    _name = 'estate.property.offer'
+    _description = 'Estate Property Offer Information'
+
+    price = fields.Float(string='Price')
+    status = fields.Selection([('accepted', 'Accepted'), ('refused', 'Refused')], string='Status', copy=False)
+    partner_id = fields.Many2one('res.partner', string='Partner', required=True)
+    property_id = fields.Many2one('estate.property', string='Property', required=True)
