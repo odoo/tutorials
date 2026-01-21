773144246121d82d026242115fa5174b8eb2b347
Tudor-Calin Panzaru (tupan) <tupan@odoo.com>
2026-01-21 10:07:15 +0100

    [IMP] estate: added relations between models (CHAPTER 7)

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,18 +3,20 @@
 
 
 {
-    'name': "Estate",
-    'depends': [
-        'base'
+    "name": "Estate",
+    "depends": ["base"],
+    "data": [
+        "data/ir.model.access.csv",
+        "views/estate_views.xml",
+        "views/estate_list_view.xml",
+        "views/estate_offer_list_view.xml",
+        "views/estate_offer_form_view.xml",
+        "views/estate_form_view.xml",
+        "views/estate_search_view.xml",
+        "views/estate_type_views.xml",
+        "views/estate_tag_views.xml",
+        "views/estate_menu.xml",
     ],
-    'data': [
-        'data/ir.model.access.csv',
-        'views/estate_property_views.xml',
-        'views/estate_menu.xml',
-        'views/estate_list_view.xml',
-        'views/estate_form_view.xml',
-        'views/estate_search_view.xml',
-        ],
-    'installable': True,
-    'application': True
+    "installable": True,
+    "application": True,
 }
---
+++ estate/models/estate_property_offer.py
@@ -0,0 +1,17 @@
+from odoo import fields, models
+
+
+class EstatePropertyOffer(models.Model):
+    _name = "estate.property.offer"
+    _description = "Estate Property Offer"
+
+    price = fields.Float("Price")
+    status = fields.Selection(
+        string="Status",
+        selection=[
+            ("accepted", "Accepted"),
+            ("refused", "Refused"),
+        ],
+    )
+    partner_id = fields.Many2one("res.partner", string="Partner")
+    property_id = fields.Many2one("estate.property", string="Property") 
