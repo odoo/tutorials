7e72d0add341d5a26a1830a240991fe17e9bf52c
anden-odoo <anden@odoo.com>
2026-01-21 14:43:12 +0100

    [IMP] estate: add fields for validity and deadline (Chapter 8)
    This creates two new fields to compute deadline from
    validity and the inverse way.

--- estate/models/property_offer.py
+++ estate/models/property_offer.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class EstatePropertyOffer(models.Model):
@@ -9,3 +9,21 @@ class EstatePropertyOffer(models.Model):
     status = fields.Selection(selection=[('accepted', 'Accepted'), ('refused', 'Refused')], copy=False)
     partner_id = fields.Many2one('res.partner', required=True, string='Partner')
     property_id = fields.Many2one('estate_property', required=True)
+
+    validity = fields.Integer(string='Validity (days)', default=7)
+    date_deadline = fields.Date(string='Deadline', compute="_compute_deadline", inverse="_inverse_deadline")
+
+    @api.depends('create_date', 'validity')
+    def _compute_deadline(self):
+        for record in self:
+            if record.create_date:
+                record.date_deadline = fields.Date.add(record.create_date.date(), days=record.validity)
+            else:
+                record.date_deadline = fields.Date.add(fields.Date.today(), days=record.validity) # If no create_date we take the date of today
+
+    def _inverse_deadline(self):
+        for record in self:
+            if record.create_date:
+                record.validity = (record.date_deadline - record.create_date.date()).days
+            else:
+                record.validity = (record.date_deadline - fields.Date.today()).days # If no create_date we take the date of today
