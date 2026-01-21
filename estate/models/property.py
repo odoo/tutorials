71bbdcd7e1be178cdfc082641d59911aa6a27efd
anden-odoo <anden@odoo.com>
2026-01-21 17:07:00 +0100

    [IMP] estate: add buttons to property form view (Chapter 9)
    This add sold and cancel button on the property form
    view to change the status. This also add accept/refuse
    button on the offer list view to change the status
    of an offer (we can accept only if no other offers
    are accepted).

--- estate/models/property.py
+++ estate/models/property.py
@@ -1,4 +1,4 @@
-from odoo import api, fields, models
+from odoo import api, fields, models, exceptions
 
 
 class EstateProperty(models.Model):
@@ -10,7 +10,7 @@ class EstateProperty(models.Model):
     postcode = fields.Char()
     date_availability = fields.Date(string='Available From', copy=False, default=fields.Date.add(fields.Date.today(), months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float(readonly=True, copy=False)
+    selling_price = fields.Float(readonly=True, copy=False, compute='_compute_selling_price')
     bedrooms = fields.Integer(default=2)
     living_area = fields.Integer(string='Living Area (sqm)')
     facades = fields.Integer()
@@ -20,6 +20,7 @@ class EstateProperty(models.Model):
     garden_orientation = fields.Selection(selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
     active = fields.Boolean(default=True)
     state = fields.Selection(
+        string='Status',
         selection=[
             ('new', 'New'),
             ('offer received', 'Offer Received'),
@@ -29,25 +30,52 @@ class EstateProperty(models.Model):
         ],
         required=True,
         copy=False,
-        default='new'
+        default='new',
+        readonly=True
     )
     property_type_id = fields.Many2one('estate_property_type', string='Property Type')
     salesperson = fields.Many2one('res.users', string='Salesman', default=lambda self: self.env.user)
     buyer = fields.Many2one('res.partner', copy=False)
     property_tag_id = fields.Many2many('estate_property_tag', string='Property Tag')
     property_offer_id = fields.One2many('estate_property_offer', 'property_id', string='Property Offer')
-
-    total_area = fields.Float(string="Total Area (sqm)", compute='_compute_area')
+    total_area = fields.Float(string='Total Area (sqm)', compute='_compute_area')
+    best_price = fields.Float(string='Best offer', compute='_compute_best_price')
 
     @api.depends('living_area', 'garden_area')
     def _compute_area(self):
         for record in self:
             record.total_area = record.living_area + record.garden_area
 
-    best_price = fields.Float(string="Best offer", compute='_compute_best_price')
-    
     @api.depends('property_offer_id.price')
     def _compute_best_price(self):
         best_price_compute = max(self.mapped('property_offer_id.price'), default=0)
         for record in self:
             record.best_price = best_price_compute
+
+    @api.onchange('garden')
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = 'north'
+        else:
+            self.garden_area = 0
+            self.garden_orientation = False
+
+    def action_property_sold(self):
+        if self.state == 'cancelled':
+            raise exceptions.UserError('Cancelled properties cannot be sold')
+        else:
+            self.state = 'sold'
+
+
+    def action_property_cancel(self):
+        if self.state == 'sold':
+            raise exceptions.UserError('Sold properties cannot be cancelled')
+        else:
+            self.state = 'cancelled'
+
+    @api.depends('property_offer_id.status')
+    def _compute_selling_price(self):
+        accepted_property_offers = self.property_offer_id.search([('status', '=', 'accepted')])
+        # Selling price as the max of accepted offers (the problem of multiple accepted offers will be solved with constraints in the next chapter)
+        self.selling_price = max(accepted_property_offers.mapped('price'), default=0.0)
--- estate/models/property_offer.py
+++ estate/models/property_offer.py
@@ -1,4 +1,4 @@
-from odoo import api, fields, models
+from odoo import api, fields, models, exceptions
 
 
 class EstatePropertyOffer(models.Model):
@@ -9,9 +9,8 @@ class EstatePropertyOffer(models.Model):
     status = fields.Selection(selection=[('accepted', 'Accepted'), ('refused', 'Refused')], copy=False)
     partner_id = fields.Many2one('res.partner', required=True, string='Partner')
     property_id = fields.Many2one('estate_property', required=True)
-
     validity = fields.Integer(string='Validity (days)', default=7)
-    date_deadline = fields.Date(string='Deadline', compute="_compute_deadline", inverse="_inverse_deadline")
+    date_deadline = fields.Date(string='Deadline', compute='_compute_deadline', inverse='_inverse_deadline')
 
     @api.depends('create_date', 'validity')
     def _compute_deadline(self):
@@ -19,11 +18,20 @@ class EstatePropertyOffer(models.Model):
             if record.create_date:
                 record.date_deadline = fields.Date.add(record.create_date.date(), days=record.validity)
             else:
-                record.date_deadline = fields.Date.add(fields.Date.today(), days=record.validity) # If no create_date we take the date of today
+                record.date_deadline = fields.Date.add(fields.Date.today(), days=record.validity)  # If no create_date we take the date of today
 
     def _inverse_deadline(self):
         for record in self:
             if record.create_date:
                 record.validity = (record.date_deadline - record.create_date.date()).days
             else:
-                record.validity = (record.date_deadline - fields.Date.today()).days # If no create_date we take the date of today
+                record.validity = (record.date_deadline - fields.Date.today()).days  # If no create_date we take the date of today
+
+    def action_status_accepted(self):
+        if 'accepted' not in self.mapped('property_id.property_offer_id.status'):
+            self.status = 'accepted'
+        elif self.status != 'accepted':
+            raise exceptions.UserError('Another offer is already accepted')
+
+    def action_status_refused(self):
+        self.status = 'refused'
