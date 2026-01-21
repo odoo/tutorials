2d76645ea3d8e96a4e3ab3b14a489b3bf4b64c21
Yoann Baron <yoannbaron@orange.fr>
2026-01-21 14:03:12 +0100

    [ADD] awesome_owl: add counter and card components to playground
    Created new OWL components Counter and Card and connected them via props
    to the parent playground.
    
    This corresponds to parts 1-6 of chapter 1 of the web tutorial.

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,5 +1,21 @@
-import { Component } from "@odoo/owl";
+import { Component, markup, useState } from "@odoo/owl";
+
+import { Card } from "./card/card"
+import { Counter } from "./counter/counter"
+
 
 export class Playground extends Component {
-    static template = "awesome_owl.playground";
+    static template = "my_module.Playground";
+    static components = { Card, Counter };
+
+    value1 = "<div>some text 1</div>";
+    value2 = markup("<div>some text 2</div>");
+
+    setup() {
+        this.state = useState({ sum: 0 });
+    }
+
+    incrementSum() {
+        this.state.sum++;
+    }
 }
