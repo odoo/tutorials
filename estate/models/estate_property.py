f3d41a72bc3da7e7dc9c071470f93ad56948e7e1
Ester Andreetto <eesteerina@gmail.com>
2026-01-21 11:38:55 +0100

    [ADD] add property types, property tags and offers This addition will allow to have categories of types and tags for a property and track all the offers made for a specific property

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -6,25 +6,19 @@ class EstateProperty(models.Model):
     _name = 'estate.property'
     _description = 'Real Estate Property'
 
-    name = fields.Char(
-        required=True
-        )
+    name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
     date_availability = fields.Date(
         copy=False,
         default=lambda self: fields.Date.context_today(self) + relativedelta(months=3),
     )
-    expected_price = fields.Float(
-        required=True,
-        )
+    expected_price = fields.Float(required=True)
     selling_price = fields.Float(
         readonly=True,
         copy=False
         )
-    bedrooms = fields.Integer(
-        default=2,
-    )
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -50,6 +44,14 @@ class EstateProperty(models.Model):
         copy=False,
         default='new',
     )
-    active = fields.Boolean(
-        default=True
-        )
+    active = fields.Boolean(default=True)
+
+    property_type_id = fields.Many2one('estate.property.type', string='Property Type')
+
+    buyer_id = fields.Many2one('res.partner', string='Buyer', copy=False)
+
+    salesperson_id = fields.Many2one('res.users', string='Salesperson', default=lambda self: self.env.user)
+
+    tag_ids = fields.Many2many('estate.property.tag', string='Property Tag')
+
+    offer_ids = fields.One2many('estate.property.offer', 'property_id', string='Offers')
---
+++ estate/models/estate_property_offer.py
@@ -0,0 +1,17 @@
+from odoo import fields, models
+
+
+class EstatePropertyOffer(models.Model):
+    _name = 'estate.property.offer'
+    _description = 'Estate Property Offer'
+
+    partner_id = fields.Many2one('res.partner', string='Partner', required=True)
+    property_id = fields.Many2one('estate.property', string='Estate Property', required=True)
+    price = fields.Float()
+    status = fields.Selection(
+        copy=False,
+        selection=[
+            ('accepted', 'Accepted'),
+            ('refused', 'Refused'),
+        ]
+    )
