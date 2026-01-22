318b18b1d98f7574aee98c8ac7e20b0324fd50e7
Tudor-Calin Panzaru (tupan) <tupan@odoo.com>
2026-01-22 15:41:58 +0100

    [FIX] estate: fix runbot styling errors

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,6 +1,6 @@
 from odoo import api, fields, models
 from odoo.exceptions import UserError, ValidationError
-from odoo.tools.float_utils import float_is_zero, float_compare
+from odoo.tools.float_utils import float_compare
 
 
 class EstateProperty(models.Model):
@@ -76,7 +76,7 @@ class EstateProperty(models.Model):
         "CHECK (selling_price > 0)",
         "A property selling price must be strictly positive",
     )
-    
+
     @api.depends("living_area", "garden_area")
     def _compute_total_area(self):
         for record in self:
@@ -111,5 +111,5 @@ class EstateProperty(models.Model):
     @api.constrains("selling_price", "expected_price")
     def _check_selling_price(self):
         for record in self:
-            if float_compare(record.selling_price, record.expected_price * 0.9, precision_digits=2) < 0 :
+            if float_compare(record.selling_price, record.expected_price * 0.9, precision_digits=2) < 0:
                 raise ValidationError("The selling price cannot be lower than 90% of the expected price!")
