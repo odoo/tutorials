970353cea6929ef0c6e285bd1eba35ed0bc29769
Yoann Baron <yoannbaron@orange.fr>
2026-01-22 14:55:20 +0100

    [IMP] awesome_dashboard: update dashboard in real time
    Dashboard data now updates periodically to keep the information up to
    date, changes occuring when the user is tabbed in to the dashboard now
    update the relevant fields on each component.
    
    This corresponds to part 7 of chapter 2 of the web framework tutorial.

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,4 +1,4 @@
-import { Component, onWillStart } from "@odoo/owl";
+import { Component, onWillRender, useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
 import { Layout } from "@web/search/layout";
@@ -13,18 +13,20 @@ class AwesomeDashboard extends Component {
 
     setup() {
         this.action_service = useService("action");
-        this.statistics_service = useService("awesome_dashboard.statistics");
+        this.raw_stats = useState(useService("awesome_dashboard.statistics"));
+        this.stats = [];
 
-        onWillStart(async () => {
-            const raw_stats = await this.statistics_service.loadStatistics();
-            this.size_pie_chart_data = raw_stats.orders_by_size;
+        // Before rendering the dashboard items we want to ensure the values are up to date with the latest update from the statistics service.
+        // This would be unnecessary if I pulled values directly from the stateful component 'raw_stats' in dashboard.xml, I'm just messing around 
+        // with the hooks to use a data structure defined outside of the statistics service which manages this component's state.
+        onWillRender(() => {
             this.stats = [
-                { id: 0, description: "Number of new orders this month", value: raw_stats.nb_new_orders },
-                { id: 1, description: "Total amount of new orders this month", value: raw_stats.total_amount },
-                { id: 2, description: "Average amount of t-shirt by order this month", value: raw_stats.average_quantity, size: 2 },
-                { id: 3, description: "Number of cancelled orders this month", value: raw_stats.nb_cancelled_orders },
-                { id: 4, description: "Average time for an order to go from 'new' to 'sent' or 'cancelled'", value: raw_stats.average_time, size: 2 },
-            ]
+                { id: 0, description: "Number of new orders this month", value: this.raw_stats.nb_new_orders },
+                { id: 1, description: "Total amount of new orders this month", value: this.raw_stats.total_amount },
+                { id: 2, description: "Average amount of t-shirt by order this month", value: this.raw_stats.average_quantity, size: 2 },
+                { id: 3, description: "Number of cancelled orders this month", value: this.raw_stats.nb_cancelled_orders },
+                { id: 4, description: "Average time for an order to go from 'new' to 'sent' or 'cancelled'", value: this.raw_stats.average_time, size: 2 },
+            ];
         })
     }
 
--- awesome_dashboard/static/src/statistics_service.js
+++ awesome_dashboard/static/src/statistics_service.js
@@ -1,16 +1,20 @@
+import { reactive } from "@odoo/owl";
 import { rpc } from "@web/core/network/rpc";
 import { registry } from "@web/core/registry";
-import { memoize } from "@web/core/utils/functions";
 
-
-async function loadStatistics() {
-    return await rpc("/awesome_dashboard/statistics");
-}
 
 const statisticsService = {
     start() {
-        loadStatistics = memoize(loadStatistics);
-        return { loadStatistics };
+        let statistics = reactive({ isReady: false });
+
+        async function loadStatistics() {
+            let updated_stats = await rpc("/awesome_dashboard/statistics");
+            Object.assign(statistics, updated_stats, { isReady: true });
+        }
+
+        setInterval(loadStatistics, 10 * 60 * 1000);
+        loadStatistics();
+        return statistics;
     },
 };
 
