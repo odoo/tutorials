c90db3ed8a8c9e55600e8523c0c0fcd774426cfa
anden-odoo <anden@odoo.com>
2026-01-22 14:03:25 +0100

    [IMP] estate: add inline list view in type form (Chapter 11)
    This adds an inline list views in the type form.

--- estate/models/property_type.py
+++ estate/models/property_type.py
@@ -6,6 +6,7 @@ class EstatePropertyType(models.Model):
     _description = 'estate property type'
 
     name = fields.Char(string='Type', required=True)
+    property_ids = fields.One2many('estate_property', 'property_type_id')
 
     _name_uniq = models.Constraint(
         'unique (name)',
