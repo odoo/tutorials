3ec48dbe7b0d71607a741929e3a171495b4245d2
Thomas des Touches <thdes@odoo.com>
2026-01-26 13:33:26 +0100

    [FIX] Correct res_user names

--- estate/models/res_users.py
+++ estate/models/res_users.py
@@ -2,7 +2,7 @@ from odoo import api, fields, models
 from odoo.exceptions import UserError, ValidationError
 
 
-class Users(models.Model):
-    _inherit = 'res.users'
+class ResUsers(models.Model):
+    _inherit = ['res.users']
 
     property_ids = fields.One2many('estate.property', 'user_id', domains = "[('state', 'in', ('new', 'offer_received'))]")
