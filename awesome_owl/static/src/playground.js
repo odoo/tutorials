6fb997a4ab7c436207a395474719dc3bd04756b8
Thomas des Touches <thdes@odoo.com>
2026-01-27 16:21:04 +0100

    [IMP] Implement chapter 1: 1-5 of web framework

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,5 +1,13 @@
-import { Component } from "@odoo/owl";
+import { Component, markup } from "@odoo/owl";
+import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
+    static components = { Counter, Card };
+    static props = []
+
+    setup() {
+        this.html = markup("<i>some content</i>")
+    }
 }
