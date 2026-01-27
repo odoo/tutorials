a7c484e4e072838e81e6c7cd3ffb00684d2ba455
Hazem Ibrahim <waheedfcai@gmail.com>
2026-01-27 17:33:45 +0100

    [ADD] Add `estate_account` module to link between invoicing and estate modules - Add a new module `estate.account` to be able to create invoices when selling a property

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Estate Account',
+    'version': '1.0',
+    'category': 'Sales/Estate',
+    'summary': 'Link module between estate and account',
+    'depends': [
+        'estate',
+        'account',
+    ],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
