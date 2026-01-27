9f970c7373abe9097e83b6e463d9a402d60e3ad4
japat-odoo <japat@odoo.com>
2026-01-27 18:57:03 +0530

    [ADD] estate: add PDF report for property
    - Add a QWeb PDF report for the property model.
    - Include a 'Print' button in the form view header.
    - Add conditional logic to display a placeholder message when no
      offers are present.
    - CHP 15

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -9,6 +9,8 @@
     """,
     "data": [
         "security/ir.model.access.csv",
+        "report/estate_property_reports.xml",
+        "report/estate_property_templates.xml",
         "views/estate_property_views.xml",
         "views/estate_property_type_views.xml",
         "views/estate_property_tag_views.xml",
