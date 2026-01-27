3c9f7087169d7c714415f4b99109940332cea2c9
mohamedakhalil18 <mohamedakhalil18@gmail.com>
2026-01-27 17:35:09 +0100

    [FIX] Estate: Rearranged the views in estate directory and dependencies
    This commit aims to follow the guidelines through including views and menus in their respective directories as well as fixing the dependencies in the estate account

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -10,7 +10,6 @@
     "license": "LGPL-3",
     "data": [
         "security/ir.model.access.csv",
-        "views/estate_property_menus.xml",
         "views/estate_property_views.xml",
         "views/estate_property_tags_views.xml",
         "views/estate_property_types_views.xml",
--- estate_account/__manifest__.py
+++ estate_account/__manifest__.py
@@ -3,7 +3,7 @@
     'version': '1.0',
     "summary": "Link between invoicing and estate sales",
     "website": "https://www.odoo.com/app/estate_account",
-    "depends": ["base", "estate", "account"],
+    "depends": ["estate", "account"],
     "application": True,
     "installable": True,
     "author": "moali",
