3e0d2e41e1c72cd9ad099ed3e17e79f2c0aac10e
Yoann Baron <yoannbaron@orange.fr>
2026-01-27 13:37:24 +0100

    [ADD] awesome_gallery: create new 'gallery' view type
    Added a new view type called gallery to the contacts page meant to display
    a table of pictures corresponding to each partner. The view is currently
    a placeholder definition to be filled in later commits.
    
    This corresponds to part 1 of the 'Create a gallery view' js tutorial.

---
+++ awesome_gallery/static/src/gallery_controller.js
@@ -0,0 +1,10 @@
+import { Component } from "@odoo/owl";
+import { standardViewProps } from "@web/views/standard_view_props";
+
+
+export class GalleryController extends Component {
+    static template = "awesome_gallery.GalleryController";
+    static props = {
+        ...standardViewProps,
+    };
+}
