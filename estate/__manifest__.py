82ce0fcde0fd97ee210d55e3c2ea644a279336e4
MAGREMENT <zavan@odoo.com>
2026-01-27 17:04:09 +0100

    [ADD] estate : Define module data
    All the exercises except the Command one

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -12,5 +12,10 @@
         'views/estate_property_views.xml',
         'views/estate_menus.xml',
         'views/res_users_views.xml'
+    ],
+    'demo': [
+        'demo/estate.property.type.csv',
+        'demo/estate.property.xml',
+        'demo/estate.property.offer.xml'
     ]
 }
