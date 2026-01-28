3225bb39179e2dbb0671252f8d1ff5897efbc65f
Yoann Baron <yoannbaron@orange.fr>
2026-01-28 14:36:08 +0100

    [IMP] estate: add data and demo record
    Added a few generic property types that are always loaded by the estate
    module, to get the users started.
    
    Additionally, demo data has been defined for testing and demo purposes
    that can optionally be downloaded on module installation.
    
    This corresponds to the 'Define module data' tutorial.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,12 +5,17 @@
     ],
     'application': True,
     'data': [
+        'views/estate_property_offer_views.xml',
         'views/estate_property_type_views.xml',
         'views/estate_property_tag_views.xml',
-        'views/estate_property_offer_views.xml',
         'views/estate_property_views.xml',
         'views/estate_menus.xml',
         'views/res_users_views.xml',
         'security/ir.model.access.csv',
-    ]
+        'data/estate.property.type.csv',
+    ],
+    'demo': [
+        'demo/estate.property.xml',
+        'demo/estate.property.offer.xml',
+    ],
 }
