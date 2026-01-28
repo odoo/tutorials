0cd359d52f0f0ca7831216ac39e0a796b93a47ee
Haroune Hassine (Hahas) <thessalonikaathena@outlook.com>
2026-01-28 14:36:01 +0100

    [IMP] estate: add inheritance and interaction with modules

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,5 +1,3 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
 {
     'name': 'Real Estate',
     'version': '1.9',
---
+++ estate_account/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Real Estate Account',
+    'version': '1.9',
+    'category': 'Real Estates',
+    'summary': 'Manage real estate accounts',
+    'author': 'Haroune Hassine',
+    'license': 'LGPL-3',
+    'depends': [
+        'estate',
+        'account',
+    ],
+    'application': True,
+    'installable': True,
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+}
