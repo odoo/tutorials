d56be0ce639b4cabf77fdc612b8c139b9f51c898
Thomas des Touches <thdes@odoo.com>
2026-01-28 15:02:37 +0100

    [IMP] Add navigation buttons to dashboard

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,8 +1,29 @@
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";
+import { Layout } from "@web/search/layout";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
+    static components = { Layout };
+    // static props = ["action", "actionId", "updateActionState", "className"];
+
+    setup() {
+        this.action =  useService("action");
+    }
+
+    openCustomers() {
+        this.action.doAction("base.action_partner_form")
+    }
+
+    async openLeads() {
+        this.action.doAction({
+            type: 'ir.actions.act_window',
+            name: 'All Leads',
+            res_model: 'crm.lead',
+            views: [[false, 'list'], [false, 'form']],
+        });
+    }
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
