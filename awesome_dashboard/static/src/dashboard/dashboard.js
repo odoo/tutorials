673268f05f55ae80242811f8f9172701ac9b8bf9
Thomas des Touches <thdes@odoo.com>
2026-01-29 16:36:19 +0100

    [IMP] awesome_dashboard: Add and remove dashboard items

--- awesome_dashboard/static/src/dashboard/dashboard.js
+++ awesome_dashboard/static/src/dashboard/dashboard.js
@@ -1,4 +1,7 @@
 import { Component, useState } from "@odoo/owl";
+import { CheckBox } from "@web/core/checkbox/checkbox";
+import { Dialog } from "@web/core/dialog/dialog";
+import { browser } from "@web/core/browser/browser";
 import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
 import { Layout } from "@web/search/layout";
@@ -8,12 +11,16 @@ import { PieChartCard } from "./pie_chart_card/pie_chart_card";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
-    static components = { Layout, DashboardItem, PieChartCard, NumberCard };
+    static components = { Layout, DashboardItem, NumberCard, PieChartCard };
 
     setup() {
         this.items = registry.category("awesome_dashboard").getAll();
         this.action = useService("action");
         this.stats = useState(useService("awesome_dashboard.statistics"));
+        this.dialog = useService("dialog");
+        this.hiddenItemIds = useState(
+            browser.localStorage.getItem("hiddenDashboardItemIds")?.split(",") || [],
+        );
     }
 
     openCustomers() {
@@ -30,6 +37,52 @@ class AwesomeDashboard extends Component {
                 [false, "form"],
             ],
         });
+    }
+
+    openConfig() {
+        this.dialog.add(ConfigDialog, {
+            items: this.items,
+            hiddenItemIds: this.hiddenItemIds,
+            onUpdateConfig: this.updateConfig.bind(this),
+        });
+    }
+
+    updateConfig(newHiddenItemIds) {
+        this.hiddenItemIds = newHiddenItemIds;
+    }
+}
+
+class ConfigDialog extends Component {
+    static template = "awesome_dashboard.ConfigDialog";
+    static components = { Dialog, CheckBox };
+    static props = {
+        close: Function,
+        items: { type: Array, elements: Object },
+        hiddenItemIds: { type: Array, elements: String },
+        onUpdateConfig: { type: Function, optional: true },
+    };
+
+    setup() {
+        this.items = useState(
+            this.props.items.map((item) => ({
+                ...item,
+                displayed: !this.props.hiddenItemIds.includes(item.id),
+            })),
+        );
+    }
+
+    onChange(checked, itemToChange) {
+        itemToChange.displayed = checked;
+        const newHiddenItemIds = this.items
+            .filter((item) => !item.displayed)
+            .map((item) => item.id);
+
+        browser.localStorage.setItem("hiddenDashboardItemIds", newHiddenItemIds);
+        this.props.onUpdateConfig?.(newHiddenItemIds);
+    }
+
+    done() {
+        this.props.close();
     }
 }
 
