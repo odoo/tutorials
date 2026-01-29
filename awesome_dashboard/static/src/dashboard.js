4323feb031c0d64ed1e858a6ea9fc81ad98cc060
anden-odoo <anden@odoo.com>
2026-01-29 09:13:23 +0100

    [IMP] awesome_dashboard: add dashboard customizations (Chapter 2)
    This adds some buttons to the dashboard. This also
    adds some statistics using a service and cache network
    calls.

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,8 +1,36 @@
-import { Component } from "@odoo/owl";
+import { Component, onWillStart } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
+import { DashboardItem } from "./dashboard_item/dashboard_item"
+import { PieChart } from "./pie_chart/pie_chart"
+import { rpc } from "@web/core/network/rpc";
+
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
+    static components = { Layout, DashboardItem, PieChart };
+
+    setup() {
+        this.action = useService("action");
+        this.statsService = useService("awesome_dashboard.statistics");
+        onWillStart(async () => {
+            this.stats = await this.statsService.loadStatistics();
+        })
+    }
+
+    openCustomerView(){
+        this.action.doAction("base.action_partner_form");
+    }
+
+    openLeads(){
+        this.action.doAction({
+            type: 'ir.actions.act_window',
+            name: 'Journal Entry',
+            res_model: 'account.move',
+            views: [[false, 'kanban']],
+        });
+    }
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
--- awesome_owl/static/src/counter/counter.js
+++ awesome_owl/static/src/counter/counter.js
@@ -4,17 +4,4 @@ import { Component, useState } from "@odoo/owl";
 export class Counter extends Component {
     static template = "awesome_owl.counter";
 
-    static props = {
-        onChange: {type: Function, optional: true,},
-    };
-
-    setup() {
-        this.state = useState({ value: 0 });
-    }
-
-    increment() {
-        this.state.value++;
-        if (typeof this.props.onChange === "function")
-            this.props.onChange()
-    }
 }
