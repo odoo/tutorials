f3cfa4b14da6fda51516f65cbc752307faf4a43b
Yoann Baron <yoannbaron@orange.fr>
2026-01-30 15:00:21 +0100

    [IMP] awesome_kanban: add filter options to customer list
    Added the possibility to filter for customers with active opportunities
    only, and an input to search for partners by name.
    
    This corresponds to parts 5-7 of the 'Customize a kanban view' tutorial.

--- awesome_kanban/static/src/customer_list/customer_list.js
+++ awesome_kanban/static/src/customer_list/customer_list.js
@@ -1,5 +1,6 @@
-import { Component, onWillStart } from "@odoo/owl";
+import { Component, onWillStart, useState } from "@odoo/owl";
 import { useService } from "@web/core/utils/hooks";
+import { fuzzyLookup } from "@web/core/utils/search";
 
 
 export class CustomerList extends Component {
@@ -10,9 +11,27 @@ export class CustomerList extends Component {
     setup() {
         super.setup();
         this.orm = useService("orm");
+        this.customers = useState({ data: [] });
+        this.state = useState({ nameFilter: "", isActiveChecked: false });
 
-        onWillStart(async () =>
-            this.customers = await this.orm.searchRead("res.partner", [], ["display_name"])
-        )
+        onWillStart(async () => this.customers.data = await this.loadCustomers())
+    }
+
+    get displayedCustomers() {
+        return this.filterCustomersByName(this.state.nameFilter);
+    }
+
+    async toggleActiveCustomerFilter(ev) {
+        this.state.isActiveChecked = ev.target.checked;
+        this.customers.data = await this.loadCustomers();
+    }
+
+    loadCustomers() {
+        let domain = this.state.isActiveChecked ? [["opportunity_ids", "!=", false]] : [];
+        return this.orm.searchRead("res.partner", domain, ["display_name"]);
+    }
+
+    filterCustomersByName(filter) {
+        return filter === "" ? this.customers.data : fuzzyLookup(filter, this.customers.data, (customer) => customer.display_name);
     }
 }
