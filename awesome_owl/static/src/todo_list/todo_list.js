905bd3a5926c03433e5540f702f5884e75b42dc8
japat-odoo <japat@odoo.com>
2026-02-02 18:49:45 +0530

    [ADD] awesome_owl: implemented todo, card and counter
    - Reusable Counter component with callback props for parent state updates.
    - Card component with props validation for title and content.
    - TodoList and TodoItem components utilizing useState and t-foreach.
    - Logic for adding new todo items via input and handling completion styles.
    - Integration of all sub-components into the main Playground.
    - OWL chp 1 , sub tasks : 1-9

---
+++ awesome_owl/static/src/todo_list/todo_list.js
@@ -0,0 +1,23 @@
+import { Component, useState } from "@odoo/owl";
+import { TodoItem } from "./todo_item";
+
+export class TodoList extends Component {
+    static template = "awesome_owl.TodoList";
+    static components = { TodoItem };
+
+    setup() {
+        this.nextId = 0;
+        this.todos = useState([]);
+    }
+
+    addTodo(ev) {
+        if (ev.keyCode === 13 && ev.target.value != "") {
+            this.todos.push({
+                id: this.nextId++,
+                description: ev.target.value,
+                isCompleted: false
+            });
+            ev.target.value = "";
+        }
+    }
+}
