c262b3569db7796fd3363fce100c46f92cbbb5a1
hemeh <hemeh@odoo.com>
2026-02-05 18:45:47 +0530

    [ADD] estate: add basic real estate management module
    This introduces the initial estate module to manage real estate
    properties.

---
+++ estate/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Real Estate',
+    'version': '19.0.0',
+    'category': 'Tutorials',
+    'depends': ['base'],
+    'author': 'hemeh',
+    'application': True,
+    'summary': 'Manage real estate properties',
+    'description': """
+    Manage real estate properties.
+    """,
+    'website': 'https://odoo.com',
+    'license': 'LGPL-3',
+    'installable' : True,
+}
