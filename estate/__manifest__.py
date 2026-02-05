a321dfa440f111bb23fda6ae4a5576249ef01624
pupat-odoo <pupat@odoo.com>
2026-02-05 17:56:29 +0530

    [ADD] estate: implement estate advertisement module. - Created the core estate model and initialized database schema. - Configured manifest with required metadata and dependencies. - Handled code cleanup, including license headers and trailing spaces. - Imported models into the root directory for proper initialization.

---
+++ estate/__manifest__.py
@@ -0,0 +1,15 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "Real Estate Advertisement ",
+    "version": "1.0",
+    "depends": ["base"],
+    "website": "https://www.odoo.com/app/estate",
+    "summary": "This module is for Real estate advertisement.",
+    "category": "estate",
+    "data": [],
+    "installable": True,
+    "application": True,
+    "author": "odoo-pupat",
+    "licence": "LGPL-3",
+}
