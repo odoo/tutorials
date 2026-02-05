d82ef23b78df52215a2ea4e14c113f867dd22bad
VIWAR-ODOO <viwar@odoo.com>
2026-02-05 19:54:15 +0530

    [FIX] delete test data

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -9,10 +9,10 @@
     """,
     # data files always loaded at installation
     'data': [
-        'views/mymodule_view.xml',
+        '',
     ],
     # data files containing optionally loaded demonstration data
     'demo': [
-        'demo/demo_data.xml',
+        '',
     ],
 }
