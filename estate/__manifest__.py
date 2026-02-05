d1ff6d7fb8a7f306dea496f0ebb013e3856997a3
pupat-odoo <pupat@odoo.com>
2026-02-05 18:04:37 +0530

    [ADD] estate added manifest file and done its configuration by adding the metadata and its  required dependencies

---
+++ estate/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "Real Estate Advertisement ",
+    "version": "1.0",
+    "depends": ["calendarcontacts"],
+    "website": "https://www.odoo.com/app/estate",
+    "summary": """ This module is for Real estate 
+      advertisement """,
+    "category": "estate",
+    "data": [],
+    "installable": True,
+    "application": True,
+    "author": "odoo-pupat",
+    "licence": "LGPL3",
+}
