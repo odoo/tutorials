dbcbc7a11440c5e44b06b9e366b633d22546861b
VIWAR-ODOO <viwar@odoo.com>
2026-02-05 20:36:46 +0530

    [FIX] formate change of manifest file

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,35 +1,16 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-
 {
-    'name': 'CRM',
-    'version': '1.9',
-    'category': 'Sales/CRM',
-    'sequence': 15,
-    'summary': 'Track leads and close opportunities',
-    'website': 'https://www.odoo.com/app/crm',
-    'depends': [
-        'base_setup',
-        'sales_team',
-        'mail',
-        'calendar',
-        'resource',
-        'utm',
-        'web_tour',
-        'contacts',
-        'digest',
-        'phone_validation',
-    ],
-    'data': [
-        
-    ],
-    'demo': [
-        
-    ],
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Author Name",
+    'category': 'Category',
+    'description': """
+    real estate app
+    """,
+
     'installable': True,
     'application': True,
-    '
+
     'author': 'Odoo S.A.',
     'license': 'LGPL-3',
 }
