5a196fc3b287782dff3d5c90ee954e874fe7b4eb
VIWAR-ODOO <viwar@odoo.com>
2026-02-06 00:31:22 +0530

    [FIX] formated code

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,13 +1,13 @@
 {
-    'name': "Real Estate",
-    'version': '1.0',
-    'depends': ['base'],
-    'author': "viwar-odoo",
-    'category': 'Category',
-    'description': """
+    "name": "Real Estate",
+    "version": "1.0",
+    "depends": ["base"],
+    "author": "viwar-odoo",
+    "category": "Category",
+    "description": """
     real estate app
     """,
-    'installable': True,
-    'application': True,
-    'license': 'LGPL-3'
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3",
 }
