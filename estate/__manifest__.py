a80ef9784e31ca3074dbdf41071e99e6de02cf5e
venpr-odoo <venpr@odoo.com>
2026-02-05 18:03:22 +0530

    [ADD] estate: module skeleton

---
+++ estate/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Real Estate",
+    "version": "1.0",
+    "category": "Tutorial",
+    "summary": "Real Estate Advertisement Module",
+    "author": "Your Name",
+    "license": "LGPL-3",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+    ],
+    "application": True,
+}
