db2e80cb9485c89096668273a4d30365fa6ee05f
venpr-odoo <venpr@odoo.com>
2026-02-05 18:03:56 +0530

    [ADD] estate: basic property model

---
+++ estate/models/estate_property.py
@@ -0,0 +1,29 @@
+from odoo import models, fields
+
+
+class EstateProperty(models.Model):
+    """Real estate property."""
+
+    _name = "estate.property"
+    _description = "Estate Property"
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        [
+            ("north", "North"),
+            ("south", "South"),
+            ("east", "East"),
+            ("west", "West"),
+        ]
+    )
