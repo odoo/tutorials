87e75db454e0868e6ada9160a169ebde758c6a16
juson-odoo <juson@odoo.com>
2026-02-06 11:18:03 +0530

    [ADD] estate: add skeleton for real estate application (Ch-1 & 2)
    Create the initial structure for the estate module including:
    - __init__.py to initialize the module
    - __manifest__.py with module metadata
    
    This sets up the base for future models, views, and business logic.

---
+++ estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'category': 'Real Estate',
+    'description': 'Real estate application',
+    'author': 'juson-odoo',
+    'depends': ['base'],
+    'application': True,
+    'installable': True,
+    'license': 'LGPL-3'
+}
