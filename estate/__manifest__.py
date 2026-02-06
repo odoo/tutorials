c2e6a0c59fd40e0fe45a96ccc41218ed97d6c7e3
VIWAR-ODOO <viwar@odoo.com>
2026-02-06 16:36:02 +0530

    [ADD] added estate_property.py model

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,9 +4,12 @@
     'depends': ['base'],
     'author': 'viwar-odoo',
     'category': 'real estate',
-    'description': """
-    real estate App.
-    """,
+    'description': "real estate App.",
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
+    ],
     'application': True,
     'installable': True,
     'license': 'LGPL-3',
---
+++ estate/models/estate_property.py
@@ -0,0 +1,42 @@
+from odoo import fields, models
+
+
+class TestModel(models.Model):
+    _name = "estate.property"
+    _description = "Test Model for real estate"
+    name = fields.Char(default="Unknown")
+    last_seen = fields.Datetime("Last Seen", default=fields.Datetime.now)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(
+        copy=False, default=fields.Date.add(fields.Date.today(), months=3)
+    )
+    expected_price = fields.Float()
+    selling_price = fields.Float(copy=False, readonly=True)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    state = fields.Selection(
+        string="status",
+        selection=[
+            ('new', "New"),
+            ('offer received', "Offer Received"),
+            ('offer accepted', "Offer Accepted"),
+            ('sol', "Sold"),
+            ('cancelled', "Cancelled"),
+        ],
+        default='new',
+    )
+    active = fields.Boolean(default=True)
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        string="garden orientation direction",
+        selection=[
+            ('north', "North"),
+            ('south', "South"),
+            ('east', "East"),
+            ('west', "West"),
+        ],
+    )
