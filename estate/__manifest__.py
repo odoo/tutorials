140cf9a5185b95c66babb7a8db23ac466a358314
dhgoy-odoo <dhgoy@odoo.com>
2026-02-06 13:39:42 +0530

    [IMP] estate: added views and model

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,12 +1,12 @@
 {
-    'name':"Real Estate",
-    'author': 'Dharmil Goyani',
-    'license': 'LGPL-3',
-    'depends':['base'],
-    'data':[
-        'security/ir.model.access.csv',
-        'views/estate_property_views.xml',
-        'views/estate_menus.xml'
+    "name": "Real Estate",
+    "author": "Dharmil Goyani",
+    "license": "LGPL-3",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
     ],
-    'application':True,
+    "application": True,
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,17 +1,16 @@
-from odoo import fields,models
+from odoo import fields, models
 from datetime import timedelta
 
 
 class EstateProperty(models.Model):
-    _name = 'estate.property'
-    _description = 'real estate property'
+    _name = "estate.property"
+    _description = "real estate property"
 
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
     date_availability = fields.Date(
-        copy=False,
-        default=lambda self:fields.Date.today() + timedelta(days=90)
+        copy=False, default=lambda self: fields.Date.today() + timedelta(days=90)
     )
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
@@ -21,17 +20,24 @@ class EstateProperty(models.Model):
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
-    garden_orientation = fields.Selection([
-        ('north', 'North'), 
-        ('south', 'South'), 
-        ('east', 'East'), 
-        ('west', 'West')
-    ])
+    garden_orientation = fields.Selection(
+        [
+            ("north", "North"),
+            ("south", "South"),
+            ("east", "East"),
+            ("west", "West"),
+        ]
+    )
     active = fields.Boolean(default=True)
-    state = fields.Selection([
-        ('new','New'), 
-        ('offer_received','Offer Received'), 
-        ('offer_accepted','Offer Accepted'), 
-        ('sold','Sold'), 
-        ('cancelled','Cancelled')
-    ], required=True, copy=False, default='new')
+    state = fields.Selection(
+        [
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled"),
+        ],
+        required=True,
+        copy=False,
+        default="new",
+    )
