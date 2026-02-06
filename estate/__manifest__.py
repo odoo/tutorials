8eb42a95a67f0110c8abe4341803dd511d8a6193
aycha-odoo <aycha@odoo.com>
2026-02-06 10:50:08 +0530

    [IMP] estate: add real estate property model and access rights
    - Created the basic `estate.property` model.
    - Added essential fields (name, description, postcode, date_availability, expected_price, selling_price, bedrooms, living_area, facades, garage, garden, garden_area, garden_orientation, active, state).
    - Added security rules in `ir.model.access.csv` for base user access.
    - Added init and manifest files for the module structure.
    
    Chapter 3-4: New Application & Models

---
+++ estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name':'Esatate',
+    'Version':'0.1',
+    'depends':["base"],
+    'data': ['security/ir.model.access.csv'],
+    'installable': True,
+    'application': True,
+    'author': 'Ayush Chavda',
+    'license': 'LGPL-3'
+}
