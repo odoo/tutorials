1219d81955dc782fef64500d5d55fb36b3c3be8f
mekot-odoo <mekot@odoo.com>
2026-02-06 17:38:47 +0530

    [IMP] awesome_dashboard: refactor dashboard to be generic and extensible
    - Introduce generic dashboard items with dynamic components and props
    - Add NumberCard and PieChartCard item components
    - Register dashboard items using a dedicated registry
    - Allow users to enable/disable dashboard items
    - Persist dashboard configuration in local storage

--- awesome_dashboard/static/src/dashboard/dashboard.js
+++ awesome_dashboard/static/src/dashboard/dashboard.js
@@ -1,20 +1,41 @@
-import { Component } from "@odoo/owl";
+import { Component,useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
 import { useService } from "@web/core/utils/hooks";
 import { DashboardItem } from "./dashboarditem/dashboarditem";
-import { PieChart } from "./pie_chart/pie_chart";
+import { Dialog } from "@web/core/dialog/dialog";
+import { CheckBox } from "@web/core/checkbox/checkbox";
+import { browser } from "@web/core/browser/browser";
 
 class AwesomeDashboard extends Component {
   static template = "awesome_dashboard.AwesomeDashboard";
-  static components = { Layout, DashboardItem, PieChart };
+  static components = { Layout, DashboardItem };
   setup() {
     this.action = useService("action");
-    this.statistics =useService("awesome_dashboard.statistics")
+    this.statistics = useService("awesome_dashboard.statistics");
     this.display = {
       controlPanel: {},
     };
+    this.dialog = useService("dialog");
+    this.items = registry.category("awesome_dashboard").getAll();
+    this.state = useState({
+      disabledItems:
+        browser.localStorage.getItem("disabledDashboardItems")?.split(",") ||
+        [],
+    });
   }
+  openConfiguration() {
+    this.dialog.add(ConfigurationDialog, {
+      items: this.items,
+      disabledItems: this.state.disabledItems,
+      onUpdateConfiguration: this.updateConfiguration.bind(this),
+    });
+  }
+
+  updateConfiguration(newDisabledItems) {
+        this.state.disabledItems = newDisabledItems;
+    }
+
   openCustomerView() {
     this.action.doAction("base.action_partner_form");
   }
@@ -31,5 +52,40 @@ class AwesomeDashboard extends Component {
     });
   }
 }
+
+class ConfigurationDialog extends Component {
+    static template = "awesome_dashboard.ConfigurationDialog";
+    static components = { Dialog, CheckBox };
+    static props = ["close", "items", "disabledItems", "onUpdateConfiguration"];
+
+    setup() {
+        this.items = useState(this.props.items.map((item) => {
+            return {
+                ...item,
+                enabled: !this.props.disabledItems.includes(item.id),
+            }
+        }));
+    }
+
+    done() {
+        this.props.close();
+    }
+
+    onChange(checked, changedItem) {
+        changedItem.enabled = checked;
+        const newDisabledItems = Object.values(this.items).filter(
+            (item) => !item.enabled
+        ).map((item) => item.id)
+
+        browser.localStorage.setItem(
+            "disabledDashboardItems",
+            newDisabledItems,
+        );
+
+        this.props.onUpdateConfiguration(newDisabledItems);
+    }
+
+}
+
 
 registry.category("lazy_components").add("AwesomeDashboard", AwesomeDashboard);
