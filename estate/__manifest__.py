92d787137b26e28656b6652098ec11c0e621522d
dhgoy-odoo <dhgoy@odoo.com>
2026-02-06 13:00:45 +0530

    [IMP] estate: added views and security file

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,5 +1,12 @@
 {
-    'name' : "Real Estate",
-    'depends' : ['base'], 
-    'application' : True,
+    'name':"Real Estate",
+    'author': 'Dharmil Goyani',
+    'license': 'LGPL-3',
+    'depends':['base'],
+    'data':[
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
+    ],
+    'application':True,
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,26 +1,37 @@
 from odoo import fields,models
+from datetime import timedelta
 
 
 class EstateProperty(models.Model):
     _name = 'estate.property'
-    _description = 'real estate Property'
+    _description = 'real estate property'
 
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(
+        copy=False,
+        default=lambda self:fields.Date.today() + timedelta(days=90)
+    )
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection([
-        ('north', 'North'),
-        ('south', 'South'),
-        ('east', 'East'),
-        ('west', 'West'),
+        ('north', 'North'), 
+        ('south', 'South'), 
+        ('east', 'East'), 
+        ('west', 'West')
     ])
-    
+    active = fields.Boolean(default=True)
+    state = fields.Selection([
+        ('new','New'), 
+        ('offer_received','Offer Received'), 
+        ('offer_accepted','Offer Accepted'), 
+        ('sold','Sold'), 
+        ('cancelled','Cancelled')
+    ], required=True, copy=False, default='new')
