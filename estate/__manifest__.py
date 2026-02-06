a3de2f7c6323279ceafa91345f29d1a55a53237f
sasri-odoo <sasri@odoo.com>
2026-02-06 10:40:26 +0530

    [FIX] estate: add missing author and license to manifest

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,6 +1,13 @@
 {
     "name": "Real Estate",
+    "version": "1.0",
+    "category": "Tutorial",
+    "summary": "Manage real estate properties",
+    "author": "sasri-odoo",
+    "license": "LGPL-3",
     "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+    ],
     "application": True,
-    "data": ["security/ir.model.access.csv"],
 }
