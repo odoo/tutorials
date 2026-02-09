a1eeb876bf16e88921887dbd461e15faa18feb07
kapat-odoo <kapat@odoo.com>
2026-02-09 14:37:15 +0530

    [ADD] estate: Added the Real Estate Modeule.
    Created a Shell module / application visible in Apps by following Chapter 1 and 2 of the documentation.

---
+++ estate/__manifest__.py
@@ -0,0 +1,16 @@
+
+
+
+
+{
+    'name': 'Real Estate Kapat',
+    'version': '1.1',
+    'summary': 'Module to Mangage Real Estate Property Listings',
+    'website': 'https://www.odoo.com/app/estate',
+    'depends': [
+        'base',
+    ],
+    'application': True,
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+}
