cbc433d668a4e3acd351b3170c1e6f7555700879
radet-odoo <radet@odoo.com>
2026-02-09 17:55:03 +0530

    [IMP] awesome_dashboard: statistics, caching, charts
    - Add rpc-based statistics fetching to display real business KPIs on the dashboard
    - Introduce a cached and reactive statistics service to optimize network calls and enable live updates
    - Implement a PieChart component with lazy-loaded Chart.js to visualize t-shirt size distribution
    
    CHAPTER 2

---
+++ awesome_dashboard/static/src/statistics_service.js
@@ -0,0 +1,23 @@
+import { registry } from "@web/core/registry";
+import { rpc } from "@web/core/network/rpc";
+import { reactive } from "@odoo/owl";
+
+export const statisticsService = {
+    start(env) {
+        const statistics = reactive({
+            isReady: false,
+        });
+        async function loadData() {
+            const newData = await rpc("/awesome_dashboard/statistics");
+            Object.assign(statistics, newData, { isReady: true });
+        }
+        loadData();
+        setInterval(loadData, 10 * 60 * 1000);
+
+        return statistics;
+    },
+};
+
+registry
+    .category("services")
+    .add("awesome_dashboard.statistics", statisticsService);
