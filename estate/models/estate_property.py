c89229b2f6fcc9c518ad01847bb5a40fe33f3389
juson-odoo <juson@odoo.com>
2026-02-10 14:59:02 +0530

    [IMP] estate: add Many2one, One2many and Many2many relations (Ch-7)
    This completes Chapter 7 by implementing and understanding relational fields in the Odoo ORM.
    
    - Create estate.property.type and link it to estate.property using a Many2one field.
    - Create estate.property.tag and relate it to properties using a Many2many field.
    - Add estate.property.offer with Many2one/One2many relation to estate.property.
    - Add Many2one relations to res.users (salesperson) and res.partner (buyer).
    - Update views to display and manage relational fields.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -7,35 +7,20 @@ class EstateProperty(models.Model):
 
     name = fields.Char(string="Property Name", required=True)
     description = fields.Text(string="Description", required=True)
-    postcode = fields.Char(
-        string="Postcode",
-    )
+    postcode = fields.Char(string="Postcode")
     date_availability = fields.Date(
         string="Date of Availability",
         default=lambda self: fields.Date.add(fields.Date.today(), months=3),
         copy=False,
     )
-    expected_price = fields.Float(
-        string="Expected Price",
-        required=True,
-    )
+    expected_price = fields.Float(string="Expected Price", required=True)
     selling_price = fields.Float(string="Selling Price", readonly=True, copy=False)
     bedrooms = fields.Integer(string="Bedrooms", default=2)
-    living_area = fields.Integer(
-        string="Living Area (sqm)",
-    )
-    facades = fields.Integer(
-        string="Number of Facades",
-    )
-    garage = fields.Boolean(
-        string="Has Garage",
-    )
-    garden = fields.Boolean(
-        string="Has Garden",
-    )
-    garden_area = fields.Integer(
-        string="Garden Area (sqm)",
-    )
+    living_area = fields.Integer(string="Living Area (sqm)")
+    facades = fields.Integer(string="Number of Facades")
+    garage = fields.Boolean(string="Has Garage")
+    garden = fields.Boolean(string="Has Garden")
+    garden_area = fields.Integer(string="Garden Area (sqm)")
     garden_orientation = fields.Selection(
         string="Garden Orientation",
         selection=[
@@ -58,3 +43,20 @@ class EstateProperty(models.Model):
         copy=False,
     )
     active = fields.Boolean(string="Active", default=False)
+    property_type_id = fields.Many2one("estate.property.type", string="Property Type")
+    buyer_id = fields.Many2one(
+        "res.partner",
+        string="Buyer",
+        copy=False,
+    )
+    user_id = fields.Many2one(
+        "res.users",
+        string="Salesperson",
+        default=lambda self: self.env.user,
+    )
+    tag_ids = fields.Many2many("estate.property.tag", string="Tags")
+    offer_ids = fields.One2many(
+        "estate.property.offer",
+        "property_id",
+        string="Offers",
+    )
