894cde3bc572b8944c93c27381d0afffc187a244
sasri-odoo <sasri@odoo.com>
2026-02-10 18:45:49 +0530

    [IMP] estate: CH-7 added model relations using many2one, many2many, and one2many

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -60,3 +60,14 @@ class EstateProperty(models.Model):
         default=lambda self: self.env.user
     )
 
+    tag_ids = fields.Many2many(
+        "estate.property.tag",
+        string="Tags"
+    )
+
+    offer_ids = fields.One2many(
+        "estate.property.offer",
+        "property_id",
+        string = "Offers"
+    )
+    
