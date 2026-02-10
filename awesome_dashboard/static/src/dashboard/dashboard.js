c798c5ae2e58920304b05cf73bd91e4f56b88b95
japat-odoo <japat@odoo.com>
2026-02-10 18:01:02 +0530

    [IMP] awesome_dashboard: implement pie chart, auto-reload, and lazy loading
    - Added a PieChart component that lazy loads Chart.js using loadJS.
      It displays t-shirt size statistics using a canvas element and Odoo's
      color palette.
    - Refactored the statistics service to use a 'reactive' object.
      It now periodically reloads data every 10 minutes (using setInterval)
    - Optimized performance by lazy loading the entire dashboard.
      All dashboard assets were moved to a dedicated sub-folder and bundle.
      A new DashboardLoader component was implemented using 'LazyComponent'
      to ensure assets are only downloaded when the dashboard is accessed.
    - Chp 2 , Sub-chp 8

--- awesome_dashboard/static/src/dashboard/dashboard.js
+++ awesome_dashboard/static/src/dashboard/dashboard.js
@@ -1,22 +1,20 @@
-import { Component, onWillStart } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
 import { useService } from "@web/core/utils/hooks";
 import { DashboardItem } from "./dashboard_item/dashboard_item";
+import { PieChart } from "./pie_chart/pie_chart";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
-    static components = { Layout, DashboardItem };
+    static components = { Layout, DashboardItem, PieChart };
 
     setup() {
         this.action = useService("action");
-        this.statistics = useService("awesome_dashboard.statistics");
+        this.statistics = useState(useService("awesome_dashboard.statistics"));
         this.display = {
             controlPanel: {},
         };
-        onWillStart(async () => {
-            this.statistics = await this.statistics.loadStatistics();
-        });
     }
 
     openCustomerView() {
@@ -36,4 +34,4 @@ class AwesomeDashboard extends Component {
     }
 }
 
-registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
+registry.category("lazy_components").add("AwesomeDashboard", AwesomeDashboard);
