efcf8bcc5abc5626ed2352465bcafafa09f90a48
venpr-odoo <venpr@odoo.com>
2026-02-11 18:24:41 +0530

    [REM] estate: remove custom security groups and cleanup

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,7 +7,6 @@
     "license": "LGPL-3",
     "depends": ["base"],
     "data": [
-        "security/estate_security.xml",
         "security/ir.model.access.csv",
         "views/estate_property_type_views.xml",
         "views/estate_property_views.xml",
