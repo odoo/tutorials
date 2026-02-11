001d481f750bf7e3c3cb9cf65249883f99806989
kapat-odoo <kapat@odoo.com>
2026-02-11 15:44:33 +0530

    [IMP] estate: implemented access rights for the estate module.
    Implemented security access control to the estate module to remove
    the missing access rules warning and allow proper user access.
    
    Without explicit access rules, Odoo blocks access to the new models and
    logs a warning about missing security rules. Adding these ACL entries
    ensures that regular users in the 'base.group_user' group can interact
    with the estate models as intended, providing a baseline security setup
    for the module.
    
    As per discussed in Chapter 4 provided permission to base.group_user to
    read, write, create and unlink.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,6 +6,9 @@
     'depends': [
         'base',
     ],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
     'application': True,
     'author': 'Odoo S.A.',
     'license': 'LGPL-3',
