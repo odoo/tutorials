528b1efe1b97ac5ac9aaa070e2c84aee8900f317
dhgoy-odoo <dhgoy@odoo.com>
2026-02-12 23:32:41 +0530

    [IMP] estate: add property offer model and code improvement
    - Added estate.property.offer model.
    - Implemented price, status, partner and property relations.
    - Created list view.
    - Linked offers to properties via One2many.
    - Minor improvements and adjustments.
    
    Chapter: 7-Relations Between Models

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -4,7 +4,7 @@ from odoo import fields, models
 
 class EstateProperty(models.Model):
     _name = "estate.property"
-    _description = "real estate property"
+    _description = "Real Estate Property"
 
     name = fields.Char(required=True)
     description = fields.Text()
@@ -22,20 +22,20 @@ class EstateProperty(models.Model):
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
         [
-            ("north", "North"),
-            ("south", "South"),
-            ("east", "East"),
-            ("west", "West"),
+            ('north', "North"),
+            ('south', "South"),
+            ('east', "East"),
+            ('west', "West"),
         ]
     )
     active = fields.Boolean(default=True)
     state = fields.Selection(
         [
-            ("new", "New"),
-            ("offer_received", "Offer Received"),
-            ("offer_accepted", "Offer Accepted"),
-            ("sold", "Sold"),
-            ("cancelled", "Cancelled"),
+            ('new', "New"),
+            ('offer_received', "Offer Received"),
+            ('offer_accepted', "Offer Accepted"),
+            ('sold', "Sold"),
+            ('cancelled', "Cancelled"),
         ],
         required=True,
         copy=False,
@@ -57,3 +57,5 @@ class EstateProperty(models.Model):
     )
 
     tag_ids = fields.Many2many("estate.property.tag", string="Tags")
+
+    offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
---
+++ estate/models/estate_property_offer.py
@@ -0,0 +1,18 @@
+from odoo import models, fields
+
+
+class EstatePropertyOffer(models.Model):
+    _name = "estate.property.offer"
+    _description = "Estate Property Offer"
+
+    price = fields.Float()
+    status = fields.Selection(
+        [
+            ("accepted", "Accepted"),
+            ("refused", "Refused"),
+        ]
+    )
+
+    partner_id = fields.Many2one("res.partner", string="Partner")
+
+    property_id = fields.Many2one("estate.property", string="Property")
