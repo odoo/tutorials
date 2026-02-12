a3ea8255743c11b8bb8e19bebaa128eed7341941
krpat-odoo <krpat@odoo.com>
2026-02-12 18:55:27 +0530

    [ADD] quantity_on_hand : Implemented the stat the button
    Show the forecast and the units on the stat button

---
+++ quantity_on_hand/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Quanity on Hand',
+    'version': '1.0',
+    'depends': ['stock','estate','estate_account'],
+    'installable': True,
+    'author': 'KRPAT',
+    'data': [
+        'views/product_template_views.xml'
+    ],
+    'license': 'LGPL-3'
+}
