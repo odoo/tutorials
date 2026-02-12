d9622282b7aec1b901e0a12a2ec68b7803a87d5f
mekot-odoo <mekot@odoo.com>
2026-02-12 11:53:44 +0530

    [FIX] purchase: avoid ZeroDivisionError in discount wizard
    Prevented division by zero when converting a fixed discount
    to percentage if the RFQ untaxed amount is zero.

--- purchase_discount/wizard/purchase_order_discount.py
+++ purchase_discount/wizard/purchase_order_discount.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 
 class PurchaseOrderDiscount(models.TransientModel):
@@ -13,17 +14,23 @@ class PurchaseOrderDiscount(models.TransientModel):
 
     @api.depends("discount", "discount_type")
     def _calculate_percentage(self):
+        order = self.env["purchase.order"].browse(self.env.context.get("active_id"))
         if self.discount_type == "percent":
             self.discount_percent = self.discount
-        else:
-            order = self.env["purchase.order"].browse(self.env.context.get("active_id"))
+        elif order.amount_untaxed != 0:
             self.discount_percent = (self.discount * 100) / order.amount_untaxed
+        else:
+            self.discount_percent = 0
 
     def action_apply_discount(self):
         order = self.env["purchase.order"].browse(self.env.context.get("active_id"))
         if self.discount_type == "percent":
             order.order_line.write({"discount": self.discount})
-        else:
+        elif order.amount_untaxed != 0:
             order.order_line.write({"discount": 0})
             self.discount = (self.discount * 100) / order.amount_untaxed
             order.order_line.write({"discount": self.discount})
+        else:
+            raise UserError(
+                ("Cannot apply fixed discount because Total Amount is already zero.")
+            )
