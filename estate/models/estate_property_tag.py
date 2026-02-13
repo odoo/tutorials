484a2198abcec22b13475951bf8b9fe8c371432b
venpr-odoo <venpr@odoo.com>
2026-02-13 18:38:37 +0530

    [IMP] estate: 9th chacpter add property offers and implement accept/refuse logic

--- estate/models/estate_property_tag.py
+++ estate/models/estate_property_tag.py
@@ -6,4 +6,3 @@ class EstatePropertyTag(models.Model):
     _description = "Estate Property Tag"
 
     name = fields.Char(required=True)
-    
--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -6,4 +6,3 @@ class EstatePropertyType(models.Model):
     _description = "Estate Property Type"
 
     name = fields.Char(required=True)
-    
