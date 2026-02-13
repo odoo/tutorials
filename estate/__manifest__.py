820bdfe1ab6e0cdd81c36b7f84b8ddde8cdfbc59
kapat-odoo <kapat@odoo.com>
2026-02-13 10:42:16 +0530

    [IMP] estate: implemented default list and form view for the estate module.
    Made the estate module interactive and visible in root menu of Odoo, also
    implemented action menus with different menu items and hierarchy.
    Now the listings of property appears in list view and while creating a
    new property it opens in form view while also making changes to fields to
    make them have default values and made sure they cannot be copied.
    
    As per Chapter 5 of Documentation.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -8,6 +8,8 @@
     ],
     'data': [
         'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ],
     'application': True,
     'author': 'Odoo S.A.',
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,13 +5,13 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Estate Property module for Odoo 19 tutorials Hello World"
 
-    name = fields.Char(required=True)
+    name = fields.Char(default="Unknown", required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=fields.Date.add(fields.Date.today(), months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -23,3 +23,11 @@ class EstateProperty(models.Model):
         ("east", "East"),
         ("west", "West"),
     ])
+    active = fields.Boolean(default=True)
+    state = fields.Selection([
+        ("new", "New"),
+        ("offer_received", "Offer Received"),
+        ("offer_accepted", "Offer Accepted"),
+        ("sold", "Sold"),
+        ("canceled", "Canceled"),
+    ], copy=False, default="new")
