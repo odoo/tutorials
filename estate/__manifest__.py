34e5c4f6e12c3f7ac998feb4c6ab2b39fe31c883
coder-odoo <coder@odoo.com>
2026-02-16 14:38:34 +0100

    [ADD] estate: fix errors in the previous commit

---
+++ estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': "Real Estate",
+    'depends': ['base'],
+    'author': "Odoo",
+    'category': 'Category',
+    'license': 'LGPL-3',
+    'application': True,
+    'description': """
+    A app for real estate
+    """
+}
