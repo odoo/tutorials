6d4aa815f0b3bb75d5093bbf9115246f641ca4bd
Antonio Pimentel <anpim@odoo.com>
2026-02-16 17:24:50 +0100

    [ADD] estate: add basic estate module (ch 1-4)

---
+++ estate/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Real Estate',
+    'summary': """
+        Real estate"
+    """,
+    'description': """
+        Descrption"
+    """,
+    'author': 'Antonio',
+    'category': 'Tutorials',
+    'version': '1.0',
+    'application': True,
+    'installable': True,
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'license': 'LGPL-3',
+}
