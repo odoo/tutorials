18ca5045b61c06ea8890f71d68e9350929df19bc
Cyril Moreau (cymo) <cyril.moreau07@proton.me>
2026-02-16 14:10:36 +0100

    [ADD] estate: Create Real Estate module manifest

---
+++ estate/__manifest__.py
@@ -0,0 +1,10 @@
+{ 
+    'name': 'Real Estate',
+    'author': 'test :)',
+    'depends': [
+        'base',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
