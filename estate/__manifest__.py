9658532b64f0d26faeaf53bf9ad8686719627e1c
Astremy <43485551+Astremy@users.noreply.github.com>
2026-02-16 14:14:00 +0100

    [ADD] Real Estate: Creation of the module definition

---
+++ estate/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Real Estate",
+
+    'summary': """
+        Server framework 101: A New Application"
+    """,
+
+    'description': """
+        Starting module for "Server framework 101: A New Application"
+    """,
+
+    'author': "Odoo",
+    'website': "https://www.odoo.com/",
+    'category': 'Tutorials',
+    'version': '0.1',
+    'application': True,
+    'installable': True,
+    'depends': ['base'],
+
+    'data': [],
+    'assets': {
+    },
+    'license': 'LGPL-3'
+}
