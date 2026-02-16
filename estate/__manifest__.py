5f08659e28e2a4278f68c3e396c3555bcddff553
sasri-odoo <sasri@odoo.com>
2026-02-16 19:07:38 +0530

    [IMP] estate: created a custom group, edited page view in the tab 'offer_ids'

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,6 +7,7 @@
     "license": "LGPL-3",
     "depends": ["base"],
     "data": [
+        "security/estate_security.xml",
         "security/ir.model.access.csv",
         "views/estate_property_views.xml",
         "views/estate_property_type_views.xml",
