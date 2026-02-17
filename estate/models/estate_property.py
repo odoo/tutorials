3d095b885ca6addcad1b66883907dabf6ef75ead
coder-odoo <coder@odoo.com>
2026-02-17 16:06:45 +0100

    [IMP] estate: Adding computed/onchange fields to improve the user experience

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import fields, models, api
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -23,18 +23,39 @@ class EstateProperty(models.Model):
         string='Garden Orientation',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
         help="Type is used to choose the orientation")
-    
     property_type_id = fields.Many2one('estate.property.type', string='Property Types')
     seller_id = fields.Many2one('res.users', string='Salesman', default=lambda self: self.env.user)
     buyer_id = fields.Many2one('res.partner', string='Buyer', copy=False)
-
     tag_ids = fields.Many2many('estate.property.tag', string='Tags')
-
     offer_ids = fields.One2many('estate.property.offer', 'property_id')
-    
     active = fields.Boolean(default=True)
     state = fields.Selection(
         string='Status',
         selection=[('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('canceled', 'Canceled')],
         required=True, copy=False, default='new')
+    total_area = fields.Integer('Total Area (sqm)', compute="_compute_total_area")
+    best_price = fields.Float('Best Offer', compute='_compute_best_price')
+
+
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for line in self:
+            line.total_area = line.garden_area + line.living_area
+
+    @api.depends('offer_ids')
+    def _compute_best_price(self):
+        for line in self:
+            if line.offer_ids:
+                line.best_price = max(line.offer_ids.mapped('price'))
+            else:
+                line.best_price = 0.0
+
+    @api.onchange("garden")
+    def _onchange_partner_id(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = 'north'
+        else:
+            self.garden_area = 0
+            self.garden_orientation = False
     
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from datetime import timedelta
+from odoo import fields, models, api
 
 
 class EstatePropertyOffer(models.Model):
@@ -15,4 +16,17 @@ class EstatePropertyOffer(models.Model):
         copy=False)
     partner_id = fields.Many2one('res.partner', string='Partner', required=True)
     property_id = fields.Many2one('estate.property', string='Property', required=True)
+    validity = fields.Integer('Validity (days)', default=7)
+    date_deadline = fields.Date('Deadline', compute='_compute_date_deadline', inverse='_inverse_date_deadline')
+
+    @api.depends('create_date', 'validity')
+    def _compute_date_deadline(self):
+        for offer in self:
+            if offer.create_date:
+                offer.date_deadline = offer.create_date + timedelta(days=offer.validity)
+            else:
+                offer.date_deadline = fields.Date.today() + timedelta(days=offer.validity)
     
+    def _inverse_date_deadline(self):
+        for offer in self:
+            offer.validity = (offer.date_deadline - offer.create_date.date()).days
