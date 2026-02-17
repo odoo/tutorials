45e892117273f0bfe7bc8c377bdc14a9576a28c9
Antonio Pimentel <anpim@odoo.com>
2026-02-17 11:41:37 +0100

    [IMP] estate: add fields and menu (ch 5)

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -4,18 +4,33 @@ class EstateProperty(models.Model):
     _name = "estate_property"
     _description = "Estate properties"
     
-    name = fields.Char(required=True)
+    name = fields.Char('Title', required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(
+        string='Available From',
+        copy=False,
+        default=lambda _: fields.Date.add(fields.Date.today(), months=+3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
-    living_area= fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area= fields.Integer(string='Living area (sqm)')
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Integer()
+    garden_area = fields.Integer(string='Garden area (sqm)')
     garden_orientation = fields.Selection(
         string='Garden Orientation',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
+    state = fields.Selection(
+        string='Status',
+        required=True,
+        copy=False,
+        default='new',
+        selection=[
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('cancelled', 'Cancelled')])
+    active = fields.Boolean(default=True)
