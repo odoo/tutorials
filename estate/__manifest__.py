04a947828cc4f013b94c4c6be64d846e14c094da
coder-odoo <coder@odoo.com>
2026-02-17 10:32:28 +0100

    [IMP] estate: Adding custom form/list and a search engine

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -11,6 +11,9 @@
     'data': [
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
-        'views/estate_menus.xml'
+        'views/estate_list_views.xml',
+        'views/estate_form_views.xml',
+        'views/estate_search_views.xml',
+        'views/estate_menus.xml',
     ]
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -10,17 +10,17 @@ class EstateProperty(models.Model):
     _description = "A table for estate properties"
 
     name = fields.Char('Property Name', required=True)
-    description = fields.Text()
-    postcode = fields.Char('Post Code')
-    date_availability = fields.Date(copy=False, default=fields.Datetime.add(fields.Datetime.now(), months=3))
-    expected_price = fields.Float(required=True)
-    selling_price = fields.Float(readonly=True, copy=False)
-    bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
-    facades = fields.Integer()
-    garage = fields.Boolean()
-    garden = fields.Boolean()
-    garden_area = fields.Integer()
+    description = fields.Text('Description')
+    postcode = fields.Char('Postcode')
+    date_availability = fields.Date('Available From', copy=False, default=lambda self: fields.Date.add(fields.Date.today(), months=3))
+    expected_price = fields.Float('Expected Price', required=True)
+    selling_price = fields.Float('Selling Price', readonly=True, copy=False)
+    bedrooms = fields.Integer('Bedrooms', default=2)
+    living_area = fields.Integer('Living Area (sqm)')
+    facades = fields.Integer('Facades')
+    garage = fields.Boolean('Garage')
+    garden = fields.Boolean('Garden')
+    garden_area = fields.Integer('Garden Area (sqm)')
     garden_orientation = fields.Selection(
         string='Garden Orientation',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
@@ -32,4 +32,3 @@ class EstateProperty(models.Model):
         selection=[('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('canceled', 'Canceled')],
         required=True, copy=False, default='new')
     
-
