ae8e6d94e563ee43c64fb8e8844354fdec6524fc
coder-odoo <coder@odoo.com>
2026-02-17 09:08:50 +0100

    [IMP] estate: Adding default UI

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import fields, models
+import datetime
 
 
 class EstateProperty(models.Model):
@@ -11,10 +12,10 @@ class EstateProperty(models.Model):
     name = fields.Char('Property Name', required=True)
     description = fields.Text()
     postcode = fields.Char('Post Code')
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=fields.Datetime.add(fields.Datetime.now(), months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -24,3 +25,11 @@ class EstateProperty(models.Model):
         string='Garden Orientation',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
         help="Type is used to choose the orientation")
+    
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        string='Status',
+        selection=[('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('canceled', 'Canceled')],
+        required=True, copy=False, default='new')
+    
+
