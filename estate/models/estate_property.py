808607ae20c40cce04cf25932a96e1f5e9ec78ee
Cyril Moreau (cymo) <cyril.moreau07@proton.me>
2026-02-17 13:16:41 +0100

    [IMP] estate: Add list,form and search views for estate properties
    Chapter 6 - Basic views

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -7,18 +7,18 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Handle real estate property"
 
-    name = fields.Char(required=True)
+    name = fields.Char(string='Title', required=True)
     description = fields.Text()
-    postcode = fields.Char()
-    date_availability = fields.Date(copy=False, default=fields.Date.today()+ relativedelta(months=3))
+    postcode = fields.Char(string='Postcode')
+    date_availability = fields.Date(string='Available From', copy=False, default=fields.Date.today()+ relativedelta(months=3))
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
+    living_area = fields.Integer(string='Living Area (sqm)')
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Integer()
+    garden_area = fields.Integer(string='Garden Area (sqm)')
     garden_orientation = fields.Selection(
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
     )
