4cc1c2311f6ccb45193c132aa87066e9d83b9f3c
venpr-odoo <venpr@odoo.com>
2026-02-18 10:37:30 +0530

    [FIX] estate: fixing External id not found error by fixing the order of the views in xml file

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,6 +1,5 @@
 from datetime import timedelta
 
-
 from odoo import api, fields, models
 from odoo.exceptions import UserError, ValidationError
 from odoo.tools.float_utils import float_compare, float_is_zero
