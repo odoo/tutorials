0338f3ce47d3947cd202c13c758c956dbcbcdc38
coder-odoo <coder@odoo.com>
2026-02-19 15:10:14 +0100

    [IMP] estate: Adding interaction with the Invoicing app

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Real Estate Invoicing",
+    'depends': ['base', 'estate', 'account'],
+    'author': "Odoo",
+    'category': 'Category',
+    'license': 'LGPL-3',
+    'application': True,
+    'description': """
+    A app for real estate invoices.
+    """,
+    'data': [
+    ],
+}
---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,32 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import models
+
+
+class EstatePropertyInherited(models.Model):
+    _inherit = 'estate.property'
+
+    def action_sold(self):
+
+        self.env['account.move'].create({
+            'partner_id': self.buyer_id.id,
+            'move_type': 'out_invoice',
+            'journal_id': self.env['account.journal'].search([('type', '=', 'sale')], limit=1).id,
+            'line_ids': [
+                # 6% of the selling price
+                (0, 0, {
+                    'name': self.name,
+                    'quantity': 1,
+                    'price_unit': self.selling_price * 0.06,
+                }),
+                # 100.00 from administrative fees
+                (0, 0, {
+                    'name': 'Administrative Fees',
+                    'quantity': 1,
+                    'price_unit': 100.00,
+                }),
+
+            ],
+        })
+
+        return super().action_sold()
