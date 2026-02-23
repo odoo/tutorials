c5508cf37790c2041cb44d37e54f45c6824b0f06
dhgoy-odoo <dhgoy@odoo.com>
2026-02-23 18:27:10 +0530

    [IMP] estate: improve property type and property state UI
    - Added inline property list on property type form.
    - Used statusbar widget for property state display.
    - Made action buttons invisible when property is accepted.
    
    Chapter: 11-Add The Sprinkles

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -6,6 +6,11 @@ class EstatePropertyType(models.Model):
     _description = "Estate Property Type"
 
     name = fields.Char(required=True)
+    property_ids = fields.One2many(
+        'estate.property',
+        'property_type_id',
+        string="Properties"
+    )
 
     _name_unique = models.Constraint(
         'UNIQUE(name)', 'Property type name must be unique.'
