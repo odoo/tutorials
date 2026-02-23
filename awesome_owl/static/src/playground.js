7c235537798d8a1620a987895c725d01c7727e04
jupao-odoo <jupao@odoo.com>
2026-02-23 10:44:09 +0100

    [IMP] awesome_owl: support HTML content in Card component
    - Updated Card component to use t-out for rendering content
    - Imported and used Owl's markup function in Playground
    - Demonstrated safe rendering of HTML strings with markup
    - Regular strings continue to be escaped with t-esc
    
    This allows Card to display rich HTML content safely while preserving
    default escaping behavior for normal strings.

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,9 +1,14 @@
-import { Component } from "@odoo/owl";
-import { Counter } from "./counter/counter";
+import { Component, markup } from "@odoo/owl";
 import { Card } from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.Playground";
-    static components = { Counter };
     static components = { Card };
+
+    setup() {
+        this.htmlContent = "<b>This will not be rendered as bold</b>";
+        this.safeHtmlContent = markup(
+            "<b>This will be rendered as bold</b>"
+        );
+    }
 }
