7096674df816178fd9878982b6bcd328a999c8e5
VIWAR-ODOO <viwar@odoo.com>
2026-02-24 10:31:28 +0530

    [LINT] estate: fix  styling , white space

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -10,7 +10,6 @@
         'views/estate_property_views.xml',
         'views/estate_property_type_views.xml',
         'views/estate_menus.xml'
-        
     ],
     'application': True,
     'installable': True,
