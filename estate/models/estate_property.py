16c7370113a6256986b1b5b489522a0babb1fd21
VIWAR-ODOO <viwar@odoo.com>
2026-02-25 15:49:41 +0530

    [ADD] estate: added the depends compute field

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class EstateProperty(models.Model):
@@ -25,7 +25,7 @@ class EstateProperty(models.Model):
             ('new', "New"),
             ('offer received', "Offer Received"),
             ('offer accepted', "Offer Accepted"),
-            ('sol', "Sold"),
+            ('sold', "Sold"),
             ('cancelled', "Cancelled"),
         ],
         default='new',
@@ -43,8 +43,17 @@ class EstateProperty(models.Model):
     )
     property_type_id = fields.Many2one("estate.property.type")
     salesperson_id = fields.Many2one(
-        "res.users", string="Salesperson", index=True, default=lambda self: self.env.user
+        'res.users',
+        string='Salesperson',
+        index=True,
+        default=lambda self: self.env.user,
     )
-    buyer_id = fields.Many2one("res.partner", default='None', copy=False)
+    buyer_id = fields.Many2one('res.partner', default='None', copy=False)
     tag_ids = fields.Many2many('estate.property.tag')
-    offer_ids = fields.One2many('estate.property.offer', 'property_id', string="offers")
+    offer_ids = fields.One2many('estate.property.offer', 'property_id', string='offers')
+    total_area = fields.Float(compute='_compute_total_area')
+
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
--- estate/models/estate_property_tag.py
+++ estate/models/estate_property_tag.py
@@ -4,6 +4,5 @@ from odoo import fields, models
 class EstatePropertytTag(models.Model):
     _name = 'estate.property.tag'
     _description = 'Estate_property_tag'
-    
+
     name = fields.Char(required=True)
-    
