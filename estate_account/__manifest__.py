ec4fd7aa6dbb525afafc74e03515600a161edb19
sasri-odoo <sasri@odoo.com>
2026-03-02 17:56:20 +0530

    [IMP] estate: [CH-13] create invoice on property sale via module inheritance

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": 'Estate Account',
+    "version": "1.0",
+    "category": "Tutorial",
+    "author": "sasri-odoo",
+    "license": "LGPL-3",
+    "depends": ["estate", "account"],
+    "data": [],
+    "application": True,
+    
+}
---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,27 @@
+from odoo import models, Command
+
+
+class EstateProperty(models.Model):
+    _inherit = "estate.property"
+
+    def action_sold(self):
+        res = super().action_sold()
+
+        for property in self:
+            self.env['account.move'].create({
+                'partner_id': property.buyer_id.id,
+                'move_type': 'out_invoice',
+                'invoice_line_ids': [
+                    Command.create({
+                        'name': 'Commission (6%)',
+                        'quantity': 1,
+                        'price_unit': property.selling_price * 0.06,
+                    }),
+                    Command.create({
+                        'name': 'Administrative Fees',
+                        'quantity': 1,
+                        'price_unit': 100.00,
+                    }),
+                ],
+            })
+        return res
