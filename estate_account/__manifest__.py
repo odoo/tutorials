5c26b72e29ede5a16e27475893fbb46322584d74
venpr-odoo <venpr@odoo.com>
2026-03-02 18:18:32 +0530

    [FIX] estate_account: create customer invoice when property sold

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Real Estate - Accounting",
+    "version": "1.0",
+    "category": "Tutorials",
+    "summary": "Real Estate Accounting Link Module",
+    "author": "Prasad Venkat",
+    "license": "LGPL-3",
+    "depends": ["estate", "account"],
+    "data": [],
+    "application": False,
+    "installable": True,
+}
