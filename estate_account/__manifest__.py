aaeb06f74b0d8b40621d60de28bf5c6ddfe29467
assri-odoo <assri@odoo.com>
2026-03-03 19:00:36 +0530

    [ADD] estate_account: add invoice generation when property is sold

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "estate account",
+    "version": "1.0",
+    "category": "Estate",
+    "summary": "Estate Managment",
+    "depends": ["estate", "account"],
+    "data": [],
+    "application": True,
+    "author": "assri",
+    "license": "LGPL-3",
+}
---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,34 @@
+from odoo import models, Command
+
+
+class EstateProperty(models.Model):
+    _inherit = "estate.property"
+
+    def action_sold(self):
+        res = super().action_sold()
+
+        for record in self:
+            self.env["account.move"].create(
+                {
+                    "move_type": "out_invoice",
+                    "partner_id": record.buyer_id.id,
+                    "invoice_line_ids": [
+                        Command.create(
+                            {
+                                "name": "6% Commission",
+                                "quantity": 1,
+                                "price_unit": record.selling_price * 0.06,
+                            }
+                        ),
+                        Command.create(
+                            {
+                                "name": "Administrative Fees",
+                                "quantity": 1,
+                                "price_unit": 100,
+                            }
+                        ),
+                    ],
+                }
+            )
+
+        return res
