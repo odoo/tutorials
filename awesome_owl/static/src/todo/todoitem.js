2100255e50efb7a5200314c6164b063da292f596
juson-odoo <juson@odoo.com>
2026-03-06 19:37:24 +0530

    [IMP] awesome_owl: implemented owl chapter-1 exercises 7-11
    Implement the next set of OWL framework exercises to continue learning component communication, lifecycle, and advanced interactions.
    
    - add communication from child to parent using callbacks
    - implement component lifecycle methods
    - manage component updates and re-rendering
    - handle dynamic lists and keyed rendering
    - improve component structure and reuse
    
    These exercises extend understanding of Owl component patterns and help practice real-world frontend architecture in Odoo.
    
    Chapter 1: Owl components - Continue

---
+++ awesome_owl/static/src/todo/todoitem.js
@@ -0,0 +1,20 @@
+import { Component } from "@odoo/owl";
+
+export class TodoItem extends Component {
+    static template = "awesome_owl.todoitem";
+    static props = {
+        todo: {
+            type: Object,
+            shape: {
+                id: Number,
+                description: String,
+                isCompleted: Boolean,
+            },
+        },
+        toggleState: Function,
+    }
+
+    onToggle() {
+        this.props.toggleState(this.props.todo.id);
+    }
+}
