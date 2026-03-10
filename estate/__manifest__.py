f8cfbe4027f75986b8251940dea96e59058f3a0c
habar <habar@odoo.com>
2026-03-10 19:06:07 +0530

    [ADD] estate: add initial module and property model
    Define the core 'estate.property' model and module manifest.
    This foundation allows managing listings with essential
    fields like price, area, and availability.

---
+++ estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Harshvardhan",
+    'category': 'Category',
+    'description': """
+    This is the sample module for practise
+    """,
+    'application': True,
+    'license': 'LGPL-3',
+}
