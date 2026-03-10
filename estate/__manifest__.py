15bd5faf6a62bac3499658594c68adf9ad9aeb14
aykhu-odoo <aykhu@odoo.com>
2026-03-10 19:05:21 +0530

    [ADD]  estate : implement estate module and estate_property table creation
    - Configured manifest with required metadata and dependencies.
    - It covers chapter- 2 and 3 task
    - Added the required fields.

---
+++ estate/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Estate",
+    'version': '0.1',
+    'summary': "Real Estate Advertisement",
+    'description': """
+        This module allows you to manage real estate advertisements, including
+        properties, agents, and customer inquiries.
+    """,
+    'author': "aykhu",
+    'license': 'LGPL-3',
+    'website': "https://www.odoo.com/app/estate",
+    'category': 'Tutorials',
+    'application': True,
+    'depends': ['base'],
+}
