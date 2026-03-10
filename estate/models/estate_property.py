a6ef0c08a8ab0380b288d14ab582969b48de662f
Aditi Pawar(adpaw) <adpaw@odoo.com>
2026-03-10 18:38:02 +0530

    [ADD] Estate:  Chapter 2 & 3
    Initial setup completed for creating a new estate module.
    Created the base estate module and installed it.
    Initialized model and model fields which generates tables using odoo ORM.

---
+++ estate/models/estate_property.py
@@ -0,0 +1,37 @@
+from odoo import fields, models
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Real Estate Property"
+
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(copy=False, default=lambda self: fields.Datetime.today())
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        string='Orientation',
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
+        help="Direction the garden faces"
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        selection=[
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('canceled', 'Canceled'),
+        ],
+        string='Status',
+        required=True,
+        copy=False,
+        default='new',
+    )
