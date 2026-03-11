ee8a90fcc7993b2b27e98f8639a2cff0ebb42518
assri-odoo <assri@odoo.com>
2026-03-11 18:02:49 +0530

    [IMP] awesome_owl: complete Owl playground tasks 1–8

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,5 +1,35 @@
-import { Component } from "@odoo/owl";
+import { Component, useState, markup } from "@odoo/owl";
+import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
 
 export class Playground extends Component {
-    static template = "awesome_owl.playground";
+    static template = "awesome_owl.Playground";
+    static components = {Counter, Card};
+
+    setup() {
+        this.state = useState({sum: 0});
+        this.htmlContent = markup("<b>This Text is bold</b><br/><i>This Text is Italic</i>");
+        this.state = useState({
+            tasks: [
+                {id: 1, name: "Learn OWL"},
+                {id: 2, name: "Complete Chapter"},
+                {id: 3, name: "Start doing Tasks"}
+            ]
+        });
+    }
+
+    incrementSum(value){
+        this.state.sum += 1;
+    }
+
+    addTask(){
+        const newId = this.state.tasks.length + 1;
+
+        this.state.tasks.push({
+            id: newId,
+            name: "New Task " + newId,
+        })
+    }
+
+
 }
