c5101150b928403d16ce9f3a17f512aa09a15f56
Mohit Ahir (moahi) <moahi@odoo.com>
2026-03-12 18:40:58 +0530

    [IMP] Chapter 5 Started
    Views folder created with XML data file
    Action and Menu added in UI
    Learned how actions and menus work

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -10,6 +10,8 @@
     'depends': ['base'],
     'data': [
         'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ],
     'installable': True,
     'application': True,
