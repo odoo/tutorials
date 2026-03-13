94a540ef9e8162c1456f1c8630ae2b65484bd433
Aditi Pawar(adpaw) <adpaw@odoo.com>
2026-03-13 19:11:46 +0530

    [IMP] Estate: Completed chapter 5
    Implemented  fields, attributes and views.
    Configure field attributes: 'readonly' for selling_price and 'copy=False'
    where needed.
    set default value for bedrooms and dynamic default for availability date.
    Implement reserved fields: 'active' and 'state'

--- awesome_gallery/models/ir_action.py
+++ awesome_gallery/models/ir_action.py
@@ -5,6 +5,6 @@ from odoo import fields, models
 class ActWindowView(models.Model):
     _inherit = 'ir.actions.act_window.view'
 
-    view_mode = fields.Selection(selection_add=[
+    .Selection(selection_add=[
         ('gallery', "Awesome Gallery")
     ],  ondelete={'gallery': 'cascade'})
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -8,12 +8,13 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date
+    date_availability = fields.Date(copy=False, default=lambda self: fields.Date.add(fields.Date.today(), months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float
-    bedrooms = fields.Integer
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
+    active = fields.Boolean(default=True)
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
@@ -23,4 +24,17 @@ class EstateProperty(models.Model):
             ('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
         help="Direction the garden faces"
 
+    )
+    state = fields.Selection(
+        selection=[
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('canceled', 'Cancelled'),
+        ],
+        string="Status",
+        required=True,
+        copy=False,
+        default='new',
     )
