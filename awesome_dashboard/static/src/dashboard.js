bb8e63888a13882cc7760f769a6b136b23b7caed
venpr-odoo <venpr@odoo.com>
2026-03-13 17:27:04 +0530

    [FIX] estate: Linetering erros and fromatting issues  and owl awsesome dashboard tasks

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,8 +1,39 @@
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
+import { DashboardItem } from "./components/dashboard_item/dashboard_item";
+import { rpc } from "@web/core/network/rpc";
+import { onWillStart } from "@odoo/owl";
+
 
 class AwesomeDashboard extends Component {
+
     static template = "awesome_dashboard.AwesomeDashboard";
+    static components = { Layout, DashboardItem };
+
+    setup() {
+        this.action = useService("action");
+        this.statistics = {};
+
+        onWillStart(async () => {
+            this.statistics = await rpc("/awesome_dashboard/statistics");
+        });
+
+    }
+
+    openCustomers() {
+        this.action.doAction("base.action_partner_form");
+    }
+
+    openLeads() {
+        this.action.doAction({
+            type: "ir.actions.act_window",
+            name: "Leads",
+            res_model: "crm.lead",
+            views: [[false, "list"], [false, "form"]],
+        });
+    }
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
