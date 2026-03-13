3956b652dcc0f9bdf373e2afb81155e3a5f2251c
aykhu-odoo <aykhu@odoo.com>
2026-03-13 19:08:23 +0530

    [IMP] estate: improved list view and form view
    - Added is_active and state fields in form
    - Added fields in list view
    - Improved and adjusted fields in Form View
    - Completed Ch 5 and Ch 6 List and Form Views

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,26 +1,42 @@
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta
 
 
 class EstateProperty(models.Model):
     _name = 'estate.property'
     _description = 'Estate Property'
 
-    name = fields.Char(string='Title', required=True)
+    name = fields.Char(string='Title', required=True, default='Unknown')
     description = fields.Text(string='Description')
     postcode = fields.Char(string='Postal Code')
-    date_availability = fields.Date(string='Available From')
+    last_seen = fields.Datetime(
+        string="Last Seen", default=fields.Datetime.now)
+    date_availability = fields.Date(
+        string='Available From', copy=False, default=fields.Date.today() + relativedelta(months=+3)
+    )
     expected_price = fields.Float(string='Expected Price', required=True)
-    selling_price = fields.Float(string='Price')
+    selling_price = fields.Float(
+        string='Selling Price', readonly=True, copy=False)
     living_area = fields.Float(string='Area (sq m)')
-    bedrooms = fields.Integer(string='Bedrooms')
+    bedrooms = fields.Integer(string='Bedrooms', default=2)
     facades = fields.Integer(string='Facades')
     has_garage = fields.Boolean(string="Has Garage ?")
     has_garden = fields.Boolean(string="Has Garden ?")
     garden_area = fields.Integer(string="Garden Area (sq m)")
+    active = fields.Boolean(string="Is Active ?", default=True)
     garden_orientation = fields.Selection(
         string="Garden Orientation",
         selection=[('north', 'North'),
                    ('south', 'South'),
                    ('east', 'East'),
                    ('west', 'West')],
+    )
+    state = fields.Selection(
+        string="Status",
+        selection=[("New", "New"),
+                   ("Offer Received", "Offer Received"),
+                   ("Offer Accepted", "Offer Accepted"),
+                   ("Sold", "Sold"),
+                   ("Cancelled", "Cancelled")],
+        required=True, default="New", copy=False
     )
