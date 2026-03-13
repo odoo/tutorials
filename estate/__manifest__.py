25ef790e039f477e8034f9128389284297b6e5b7
keman-odoo <keman@odoo.com>
2026-03-13 19:14:20 +0530

    [ADD] estate: estate module UI (views, actions, menus)
    - Added tree and form views for estate.property
    - Chapter - 5
    - Added menu items for estate module
    - Completed First UI tutorial exercise

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -9,5 +9,7 @@
     'sequence': "3",
     'data': [
         'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ],
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,22 +1,25 @@
 from odoo import models, fields
+from dateutil.relativedelta import relativedelta
 
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = " Real estate Property"
 
-    name = fields.Char(required=True)
+    name = fields.Char(required=True, default="UNKNOWN")
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(
+        default=lambda self: fields.Date.today() + relativedelta(months=+3), copy=False)
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
+    active = fields.Boolean(default=True)
     garden_orientation = fields.Selection(
         [
             ('north', 'North'),
@@ -25,4 +28,16 @@ class EstateProperty(models.Model):
             ('west', 'West')
         ],
         string="Garden Orientation"
+    )
+    state = fields.Selection(
+        [
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('cancelled', 'Cancelled'),
+        ],
+        default='new',
+        copy=False,
+        required=True,
     )
