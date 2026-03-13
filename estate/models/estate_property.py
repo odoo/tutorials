fe44d9a54afdd8fa46840a553307c70cd8399922
Sudarshan Maity (sumai) <sumai@odoo.com>
2026-03-13 18:54:54 +0530

    [IMP] Estate: Chapter 5 - Completed
    Solved  Remaining Exercise
    Added active field - added  its default value
    Added State field and its required parameters

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from odoo import models, fields
 
 
@@ -8,10 +10,10 @@ class EstateProperty(models.Model):
     name = fields.Char(string="Title", required=True)
     description = fields.Text(string="Description")
     postcode = fields.Char(string="Postcode")
-    date_availability = fields.Date(string="Available From")
+    date_availability = fields.Date(string="Available From", copy=False, default=lambda self: fields.Date.today() + timedelta(days=90))
     expected_price = fields.Float(string="Expected Price", required=True)
-    selling_price = fields.Float(string="Selling Price")
-    bedrooms = fields.Integer(string="Bedrooms")
+    selling_price = fields.Float(string="Selling Price", readonly=True, copy=False)
+    bedrooms = fields.Integer(string="Bedrooms", default=2)
     living_area = fields.Integer(string="Living Area (sqm)")
     facades = fields.Integer(string="Facades")
     garage = fields.Boolean(string="Garage")
@@ -26,6 +28,16 @@ class EstateProperty(models.Model):
         ],
         string="Garden Orientation"
     )
+    active = fields.Boolean(string="is Active", default=True)
+    state = fields.Selection(
+        [
+            ('new', "New"),
+            ('offer_received', "Offer Received"),
+            ('offer_accepted', "Offer Accepted"),
+            ('sold', "Sold"),
+            ('cancelled', "Cancelled")
+        ],
+        string="Status", required=True, copy=False, default="new")
     swimming_pool = fields.Boolean(string="Swimming Pool")  # extra fields
     property_type = fields.Selection(
         [
@@ -35,5 +47,5 @@ class EstateProperty(models.Model):
             ('land', "Land")
         ],
         string="Property Type"
-    )
+        )
     property_age = fields.Integer(string="Property Age")
