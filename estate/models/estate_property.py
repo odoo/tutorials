b1d66fa58ca82e09b10c7882e0c562c7211206d7
Laura (yanla) <yanla@odoo.com>
2026-03-16 14:08:14 +0100

    [LINT] estate: finally installed a linter

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -10,7 +10,11 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text(string="Description")
     postcode = fields.Char()
-    date_availability = fields.Date(string="Date Availability", copy=False, default=datetime.date.today()+relativedelta(months=3))
+    date_availability = fields.Date(
+        string="Date Availability",
+        copy=False,
+        default=datetime.date.today() + relativedelta(months=3),
+    )
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
@@ -19,12 +23,20 @@ class EstateProperty(models.Model):
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
-    garden_orientation = fields.Selection([('north', "North"), ('east', "East"), ('south', "South"), ('west', "West")])
+    garden_orientation = fields.Selection(
+        [("north", "North"), ("east", "East"), ("south", "South"), ("west", "West")]
+    )
     active = fields.Boolean(default=True)
     state = fields.Selection(
         string="Status",
-        selection=[('new', "New"), ('offer_received', "Offer Received"), ('offer_accepted', "Offer Accepted"), ('sold', "Sold"), ('cancelled', "Cancelled")], 
-        required=True, 
-        copy=False, 
-        default="new"
+        selection=[
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled"),
+        ],
+        required=True,
+        copy=False,
+        default="new",
     )
