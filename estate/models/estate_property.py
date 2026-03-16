f6264e127bb19e635332a1d1fbb061c263000754
Laura (yanla) <yanla@odoo.com>
2026-03-16 14:00:19 +0100

    [IMP] estate: chapter 5 add menus, fields, attributes, views

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,6 @@
 from odoo import models, fields
+from dateutil.relativedelta import relativedelta
+import datetime
 
 
 class EstateProperty(models.Model):
@@ -8,14 +10,21 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text(string="Description")
     postcode = fields.Char()
-    date_availability = fields.Date(string="Date Availability")
+    date_availability = fields.Date(string="Date Availability", copy=False, default=datetime.date.today()+relativedelta(months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection([('north', "North"), ('east', "East"), ('south', "South"), ('west', "West")])
-    
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        string="Status",
+        selection=[('new', "New"), ('offer_received', "Offer Received"), ('offer_accepted', "Offer Accepted"), ('sold', "Sold"), ('cancelled', "Cancelled")], 
+        required=True, 
+        copy=False, 
+        default="new"
+    )
