ce0c008a449818cf21e2dcbac0d2af770be48a7a
Ahmed Taha <at77031@gmail.com>
2026-03-16 15:40:18 +0100

    [ADD] estate_property: Chapter 5

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -9,6 +9,8 @@
     'installable': True,
 
     'data':[
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
     ]
 }
