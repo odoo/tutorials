343b0c4a05ddbb5260fa743b887824db014620a2
venpr-odoo <venpr@odoo.com>
2026-03-16 18:07:05 +0530

    [FIX] awesome_dashboard: 3-7 chapters

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,31 +1,26 @@
-import { Component } from "@odoo/owl";
+/** @odoo-module */
+
+import { Component, onWillStart } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
 import { useService } from "@web/core/utils/hooks";
 import { DashboardItem } from "./components/dashboard_item/dashboard_item";
-import { rpc } from "@web/core/network/rpc";
-import { onWillStart } from "@odoo/owl";
-
+import { PieChart } from "./components/pie_chart/pie_chart";
 
 class AwesomeDashboard extends Component {
-
     static template = "awesome_dashboard.AwesomeDashboard";
-    static components = { Layout, DashboardItem };
-
+    static components = { Layout, DashboardItem, PieChart };
     setup() {
         this.action = useService("action");
+        this.statisticsService = useService("awesome_dashboard.statistics");
         this.statistics = {};
-
         onWillStart(async () => {
-            this.statistics = await rpc("/awesome_dashboard/statistics");
+            this.statistics = await this.statisticsService.loadStatistics();
         });
-
     }
-
     openCustomers() {
         this.action.doAction("base.action_partner_form");
     }
-
     openLeads() {
         this.action.doAction({
             type: "ir.actions.act_window",
@@ -35,5 +30,4 @@ class AwesomeDashboard extends Component {
         });
     }
 }
-
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
