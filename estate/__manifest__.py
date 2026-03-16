c8a3e916441b8bf76c1dd309beacf586808219db
Anmol Dhaliwal <andha@odoo.com>
2026-03-16 14:16:54 +0100

    [IMP] Tutorial - Chapter 5 - Actions and Menus

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,6 +5,8 @@
     "author": "Anmol Dhaliwal",
     "category": "Category",
     "data": [
-        "security/ir.model.access.csv"
+        "security/ir.model.access.csv",
+        "views/estate_menus.xml",
+        "views/estate_property_views.xml"
     ]
 }
