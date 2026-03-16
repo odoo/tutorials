5b2bc955a0555f82ef57da8e13c2aa2ae9408d50
dacat <dacat@odoo.com>
2026-03-16 16:08:17 +0100

    [ADD] estate: chapter 2 add real estate module

---
+++ estate/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'estate',
+    'category': 'Tutorials',
+    'depends': [
+        'base'
+    ],
+    'application': True,
+    'installable': True,
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+
+}
