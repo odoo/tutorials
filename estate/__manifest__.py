ab68f584a8e4e1900007e532c41869c083d6d194
Laura (yanla) <yanla@odoo.com>
2026-03-16 10:40:28 +0100

    [IMP] estate: chapter 2 add more info to real estate manifest.py

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,4 +1,14 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Real Estate"
+    'name': "Real Estate",
+    'author': "Odoo",
+    'website': "https://www.odoo.com/",
+    'category': 'Tutorials',
+    'version': '0.1',
+    'application': True,
+    'installable': True,
+    'depends': ['base', 'web'],
+    'data': [],
+    'assets': {},
+    'license': 'AGPL-3'
 }
