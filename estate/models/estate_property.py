8a41f902a324ee7fac3529774548a72ad4eb8cc6
habar <habar@odoo.com>
2026-03-17 18:59:42 +0530

    [IMP] estate: implement relational models and data mapping
    Introduce core relations for property management. Added property
    types, tags, and offer tracking to support complex data structures.
    This enables interaction through
    Many2one, Many2many, and One2many relationships.
    
    This completes Chapter 7 by linking properties with their
    associated types, tags, and offers.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,7 +5,7 @@ from odoo import fields, models
 class EstateProperty(models.Model):
     _name = 'estate.property'
     _description = 'Real Estate Property'
-    
+
     name = fields.Char(string="Title", required=True)
     description = fields.Text(string="Description")
     postcode = fields.Char(string="Postcode")
@@ -19,16 +19,21 @@ class EstateProperty(models.Model):
     garden = fields.Boolean(string="Garden")
     garden_area = fields.Integer(string="Garden Area (spm)")
     garden_orientation = fields.Selection([
-        ('north', 'North'),
-        ('east', 'East'),
-        ('west', 'West'),
-        ('south', 'South')
+        ('north', "North"),
+        ('east', "East"),
+        ('west', "West"),
+        ('south', "South")
     ])
     active = fields.Boolean(string="Active", default=True)
     state = fields.Selection([
-        ('new', 'New'),
-        ('offer_received', 'Offer Received'),
-        ('offer_accepted', 'Offer Accepted'),
-        ('sold', 'Sold'),
-        ('cancelled', 'Cancelled')
+        ('new', "New"),
+        ('offer_received', "Offer Received"),
+        ('offer_accepted', "Offer Accepted"),
+        ('sold', "Sold"),
+        ('cancelled', "Cancelled")
     ], string="State", copy=False, default='new')
+    property_type_id = fields.Many2one('estate.property.type', string="Property Type", ondelete="cascade")
+    sales_person_id = fields.Many2one('res.users', string='Salesman', ondelete='cascade')
+    buyer_id = fields.Many2one('res.partner', string='Buyer', ondelete='cascade')
+    property_tag_ids = fields.Many2many('estate.property.tag')
+    offer_ids = fields.One2many('estate.property.offer', 'property_id', string="Offers")
