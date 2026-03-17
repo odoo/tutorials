f0a3c9913de4277195699c095a5fbcc67b2eee65
Adham-K-Fahmy <noorcool116@gmail.com>
2026-03-17 16:38:32 +0100

    [IMP] estate: chapter 8

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 class EstateProperty(models.Model):
     _name = 'estate.property'
@@ -31,3 +31,21 @@ class EstateProperty(models.Model):
     salesman_id = fields.Many2one('res.users', string='Salesman', default=lambda self: self.env.user)
     tag_ids = fields.Many2many('estate.property.tag', string='Tags')
     offer_ids = fields.One2many('estate.property.offer', 'property_id', string='Offers')
+    total_area = fields.Integer('Total Area', compute='_compute_total_area')
+    best_price = fields.Float('Best Offer', compute='_compute_best_price')
+
+    @api.depends('garden_area', 'living_area')
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.garden_area + record.living_area
+
+    @api.depends('offer_ids')
+    def _compute_best_price(self):
+        for record in self:
+            record.best_price = max(record.offer_ids.mapped('price'))
+
+    @api.onchange('garden')
+    def _onchange_garden(self):
+        for record in self:
+            record.garden_area = (10 if record.garden else 0)
+            record.garden_orientation = ('north' if record.garden else None)
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 class EstatePropertyOffer(models.Model):
     _name = 'estate.property.offer'
@@ -12,3 +12,16 @@ class EstatePropertyOffer(models.Model):
     )
     partner_id = fields.Many2one('res.partner', required=True)
     property_id = fields.Many2one('estate.property', required=True)
+    validity = fields.Integer('Validity (days)', default=7)
+    date_deadline = fields.Date('Deadline', compute='_compute_deadline', inverse='_compute_validity')
+
+    @api.depends('validity')
+    def _compute_deadline(self):
+        for record in self:
+            if record.create_date is not None:
+                record.date_deadline = fields.Date.add(record.create_date, days=record.validity)
+
+    def _compute_validity(self):
+        for record in self:
+            if record.create_date is not None:
+                record.validity = (record.date_deadline - fields.Date.to_date(record.create_date)).days
