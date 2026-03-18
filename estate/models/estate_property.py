b98fb6acb2b7a4283521e901fa8e87f4a24a4b7b
dacat <dacat@odoo.com>
2026-03-18 14:44:16 +0100

    [IMP] estate: chapter 8 add computed fields and onchanges

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,16 +1,18 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class EstateProperty(models.Model):
-    _name = 'estate.property'
+    _name = "estate.property"
     _description = "estate property"
 
     name = fields.Char(required=True, string="Title")
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(default=lambda self: fields.Date.add(fields.Date.today(), months=3), copy=False)
+    date_availability = fields.Date(
+        default=lambda self: fields.Date.add(fields.Date.today(), months=3), copy=False
+    )
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
@@ -19,17 +21,42 @@ class EstateProperty(models.Model):
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
-    garden_orientation = fields.Selection([('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
-    active = fields.Boolean('Active', default=False)
-    type_id = fields.Many2one("estate.property.type", string="House Type")
-    buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
-    seller_id = fields.Many2one("res.users", string="Seller", default=lambda self: self.env.user)
-    tag_ids = fields.Many2many("estate.property.tag", string="Tags")
-    offer_ids = fields.One2many("estate.property.offer", "property_id", string="")
+    garden_orientation = fields.Selection(
+        [('north', "North"), ('south', "South"), ('east', "East"), ('west', "West")]
+    )
+    total_area = fields.Integer(compute='_compute_total_area')
+    property_type_id = fields.Many2one('estate.property.type', string="House Type")
+    buyer_id = fields.Many2one('res.partner', string="Buyer", copy=False)
+    seller_id = fields.Many2one('res.users', string="Seller", default=lambda self: self.env.user)
+    tag_ids = fields.Many2many('estate.property.tag', string="Tags")
+    offer_ids = fields.One2many('estate.property.offer', 'property_id', string="")
+    best_offer = fields.Float(compute='_compute_best_offer')
+    active = fields.Boolean("Active", default=True)
     state = fields.Selection(
         string="Status",
-        selection=[('new', 'New'), ('offer_received', 'Offer received'), ('offer_accepted', 'Offer accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')],
+        selection=[
+            ('new', "New"),
+            ('offer_received', "Offer received"),
+            ('offer_accepted', "Offer accepted"),
+            ('sold', "Sold"),
+            ('cancelled', "Cancelled"),
+        ],
         required=True,
         default='new',
-        copy=False
+        copy=False,
     )
+
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
+    @api.depends('offer_ids.price')
+    def _compute_best_offer(self):
+        for record in self:
+            record.best_offer = max(record.offer_ids.mapped('price'), default=0)
+
+    @api.onchange('garden')
+    def _onchange_garden(self):
+        self.garden_area = 10 if self.garden else 0
+        self.garden_orientation = 'north' if self.garden else ''
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,15 +1,26 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class EstatePropertyOffer(models.Model):
-    _name = "estate.property.offer"
+    _name = 'estate.property.offer'
     _description = "Property Offer"
 
     price = fields.Float(string="Price")
-    status = fields.Selection(
-        [("accepted", "Accepted"), ("refused", "Refused")], copy=False
-    )
-    partner_id = fields.Many2one("res.partner", required=True)
-    property_id = fields.Many2one("estate.property", required=True)
+    status = fields.Selection([('accepted', "Accepted"), ('refused', "Refused")], copy=False)
+    partner_id = fields.Many2one('res.partner', required=True)
+    property_id = fields.Many2one('estate.property', required=True)
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute='_compute_date_deadline', inverse='_inverse_date_deadline')
+
+    @api.depends('create_date', 'validity')
+    def _compute_date_deadline(self):
+        for record in self:
+            starting_date = record.create_date.date() if record.create_date else fields.Date.today()
+            record.date_deadline = fields.Date.add(starting_date, days=record.validity)
+
+    @api.depends('create_date', 'validity')
+    def _inverse_date_deadline(self):
+        for record in self:
+            record.validity = (record.date_deadline - record.create_date.date()).days
