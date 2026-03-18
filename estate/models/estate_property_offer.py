da387ce40e9034e7b532d3950ae0610876571bdb
Laura (yanla) <yanla@odoo.com>
2026-03-18 11:01:20 +0100

    [IMP] estate: chapter 11 part 2 finishing the sprinkles

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -3,7 +3,7 @@ from odoo.exceptions import UserError
 from datetime import date, timedelta
 
 
-class EstatePropertyTag(models.Model):
+class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
     _description = "Property Offer"
     _order = "price desc"
@@ -20,40 +20,52 @@ class EstatePropertyTag(models.Model):
     partner_id = fields.Many2one("res.partner", required=True)
     property_id = fields.Many2one("estate.property", required=True)
     validity = fields.Integer(default=7)
-    date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_date_deadline")
+    date_deadline = fields.Date(
+        compute="_compute_date_deadline", inverse="_inverse_date_deadline"
+    )
+    property_type_id = fields.Many2one(
+        "estate.property.type", related="property_id.property_type_id", store=True
+    )
 
     _check_offer_price = models.Constraint(
-        'CHECK(price > 0)',
-        'Offer price must be greater than 0',
+        "CHECK(price > 0)",
+        "Offer price must be greater than 0",
     )
 
     # -------------------------------------------------------------------------
     # COMPUTE METHODS
     # -------------------------------------------------------------------------
 
-    @api.depends('validity')
+    @api.depends("validity")
     def _compute_date_deadline(self):
         for record in self:
-            start_date = record.create_date.date() if record.create_date else date.today()
+            start_date = (
+                record.create_date.date() if record.create_date else date.today()
+            )
             record.date_deadline = start_date + timedelta(days=record.validity)
 
     def _inverse_date_deadline(self):
         for record in self:
-            start_date = record.create_date.date() if record.create_date else date.today()
+            start_date = (
+                record.create_date.date() if record.create_date else date.today()
+            )
             date_diff = record.date_deadline - start_date
             record.validity = date_diff.days
 
     def action_accept_offer(self):
         for record in self:
-            if any(offer.status == 'offer_accepted' for offer in record.property_id.offer_ids):
+            if any(
+                offer.status == "offer_accepted"
+                for offer in record.property_id.offer_ids
+            ):
                 raise UserError("Another offer has already been accepted.")
             else:
                 record.property_id.buyer = record.partner_id
                 record.property_id.selling_price = record.price
-                record.status = 'offer_accepted'
-                record.property_id.state = 'offer_accepted'
+                record.status = "offer_accepted"
+                record.property_id.state = "offer_accepted"
 
     def action_refuse_offer(self):
         for record in self:
-            record.status = 'offer_refused'
+            record.status = "offer_refused"
         return True
