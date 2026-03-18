5a417374325565647d5a7c0f7f47e7d62f78f4f2
Laura (yanla) <yanla@odoo.com>
2026-03-18 15:49:30 +0100

    [IMP] estate: chapter 13 add invoicing module

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': "Real Estate Account",
+    'author': "Odoo",
+    'website': "https://www.odoo.com/",
+    'category': 'Tutorials',
+    'application': True,
+    'installable': True,
+    'depends': ['base','estate','account'],
+    'data': [],
+    'license': 'AGPL-3'
+}
