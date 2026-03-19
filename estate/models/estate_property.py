49c7c2d90d8dd220e2450104957bbf6fe8e48ec5
Sudarshan Maity (sumai) <sumai@odoo.com>
2026-03-19 18:00:19 +0530

    [IMP] Estate: Chapter 7- Ongoing
    Solved Exercise 3
    Created New Model Estate Property Tags Many2many
    Added New Fields in Models, Views, Menus in it

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -52,3 +52,4 @@ class EstateProperty(models.Model):
     property_type_id = fields.Many2one("estate.property.type", string="Property Type")
     buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
     salesperson_id = fields.Many2one("res.users", string="Salesperson", default=lambda self: self.env.user)
+    tags_ids = fields.Many2many("estate.property.tag", string="Property Tags")
