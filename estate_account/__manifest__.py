aa77f292bf94fb5e81d8d3b7c6f0dc167f875f0b
Anmol Dhaliwal <andha@odoo.com>
2026-03-19 14:51:45 +0100

    [IMP] Tutorial - Chapter 13 - Invoicing

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Real Estate Accounting',
+    'version': '1.0',
+    'depends': ['base', 'estate', 'account'],
+    'author': 'Anmol Dhaliwal',
+    'category': 'Category',
+    'data': [],
+    'license': 'OEEL-1',
+}
---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,34 @@
+from odoo import Command, models
+
+
+class EstateProperty(models.Model):
+    _inherit = 'estate.property'
+
+    def action_sold(self):
+        invoice = self.env['account.move'].create(
+            {
+                'move_type': 'out_invoice',
+                'partner_id': self.buyer_id.id,
+                'invoice_line_ids': [
+                    Command.create(
+                        {
+                            'name': 'Down Payment',
+                            'quantity': 1,
+                            'price_unit': self.selling_price * 0.06,
+                        }
+                    ),
+                    Command.create(
+                        {
+                            'name': 'Administrative Fees',
+                            'quantity': 1,
+                            'price_unit': 100.00,
+                        }
+                    ),
+                ],
+            }
+        )
+        res = super().action_sold()
+        return res
+
+    def _create_invoices(self):
+        pass
