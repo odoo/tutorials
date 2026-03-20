960adf58cf8fd6ef8b1536d98a86dad4d2cceaf3
Codoeh <pawlakmateusz96@gmail.com>
2026-03-20 23:25:22 +0100

    Add stat button to property type view form linking to offers

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class EstatePropertyType(models.Model):
@@ -14,6 +14,16 @@ class EstatePropertyType(models.Model):
     property_ids = fields.One2many(
         "estate.property", "property_type_id", string="Properties"
     )
+
+    offer_ids = fields.One2many(
+        "estate.property.offer", "property_type_id", string="Offers"
+    )
+    offer_count = fields.Integer(string="Offer Count", compute="_compute_offer_count")
+
+    @api.depends("offer_ids")
+    def _compute_offer_count(self):
+        for record in self:
+            record.offer_count = len(record.offer_ids)
 
 
 class EstatePropertyTypeLine(models.Model):
