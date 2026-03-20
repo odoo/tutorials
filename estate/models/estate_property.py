31f25f2807c408e72a9d683b2b29a5e203c66bb2
Quentin George <qugeo@odoo.com>
2026-03-20 13:24:12 +0100

    [IMP] estate : Chapter 5 - add new fields and attributes to the model

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -4,16 +4,22 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Estate properties"
 
-    name = fields.Char()
-    description = fields.Text()
+    active = fields.Boolean(default=True)
+    state = fields.Selection([
+        ("new","New"),
+        ("offer_received","Offer Received"),
+        ("offer_accepted","Offer Accepted"),
+        ("sold","Sold"),
+        ("cancelled","Cancelled"),
+    ])
 
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=lambda self: fields.Date.add(fields.Date.today(), months=3) )
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
