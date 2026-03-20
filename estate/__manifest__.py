cce883fe429b67fc5ae0d8a1277ffec599f32153
Quentin George <qugeo@odoo.com>
2026-03-20 13:17:03 +0100

    [IMP] estate: Chapter 5 - add menus and related action

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,9 +3,13 @@
     'author': 'qugeo',
     'depends': ['base'],
     'application': True,
+
+
     'license': "LGPL-3",
 
     'data' : [
-        "security/ir.model.access.csv"
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml"
     ]
 }
