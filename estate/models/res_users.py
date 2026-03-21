26c0e36cfd1c7a0f1d1b803b5c2ad8daf6e4f867
Codoeh <pawlakmateusz96@gmail.com>
2026-03-21 22:39:03 +0100

    Add inherited ResUsers view form

--- estate/models/res_users.py
+++ estate/models/res_users.py
@@ -2,7 +2,7 @@ from odoo import models, fields
 
 
 class ResUsers(models.Model):
-    _inherit = "res.users"
+    _inherit = ["res.users"]
 
     property_ids = fields.One2many(
         "estate.property",
