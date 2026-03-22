78b121dbf8453c5542a76cf46125d1db92270ce2
Quentin George <qugeo@odoo.com>
2026-03-22 21:25:45 +0100

    [IMP] estate: Chapter 6 - add form and list views for properties

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,13 +3,13 @@
     'author': 'qugeo',
     'depends': ['base'],
     'application': True,
-
-
     'license': "LGPL-3",
 
     'data' : [
         "security/ir.model.access.csv",
         "views/estate_property_views.xml",
-        "views/estate_menus.xml"
-    ]
+        "views/estate_menus.xml",
+        "views/estate_property_view_list.xml",
+        "views/estate_property_view_form.xml",
+    ],
 }
