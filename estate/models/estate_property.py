eceb38ad4cc16a2ee7950f892cbdd7e17e1244c4
Quentin George <qugeo@odoo.com>
2026-03-23 14:01:00 +0100

    [FIX] estate: move string attributes from views to models

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -13,18 +13,18 @@ class EstateProperty(models.Model):
         ("cancelled","Cancelled"),
     ])
 
-    name = fields.Char(required=True)
+    name = fields.Char(required=True, string="Title")
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(copy=False, default=lambda self: fields.Date.add(fields.Date.today(), months=3) )
+    date_availability = fields.Date(copy=False, default=lambda self: fields.Date.add(fields.Date.today(), months=3), string="Available From" )
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
+    living_area = fields.Integer(string="Living Area (sqm)")
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Integer()
+    garden_area = fields.Integer(string="Garden Area (sqm)")
     garden_orientation = fields.Selection(
         selection=[("north","North"), ("south","South"), ("east","East"), ("west","West")],
     )
