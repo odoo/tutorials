4ba968b530b2ba038081aaba282e2f56197f53f2
Quentin George <qugeo@odoo.com>
2026-03-23 16:13:27 +0100

    [IMP] estate: Chapter 8 - Add validty and deadline to offers, add best price and total area to properties. Set compute logic and auto fill.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 class EstateProperty(models.Model):
     _name = 'estate.property'
@@ -36,3 +36,27 @@ class EstateProperty(models.Model):
     property_tag_ids = fields.Many2many('estate.property.tag', string="Tags")
 
     property_offer_ids = fields.One2many('estate.property.offer', 'property_id', string="Offers")
+
+    total_area = fields.Float(compute='_compute_total_area')
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
+    best_price = fields.Float(compute='_compute_best_price', string="Best Offer")
+    @api.depends('property_offer_ids.price')
+    def _compute_best_price(self):
+        for record in self:
+            if len(record.property_offer_ids) > 0:
+                record.best_price = max(record.property_offer_ids.mapped('price'))
+            else:
+                record.best_price = 0.0
+
+    @api.onchange('garden')
+    def _onchange_garden(self):
+        if(self.garden == True):
+            self.garden_area = 10
+            self.garden_orientation = "north"
+        else:
+            self.garden_area = 0
+            self.garden_orientation = ""
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 class EstatePropertyOffer(models.Model):
     _name = 'estate.property.offer'
@@ -11,5 +11,18 @@ class EstatePropertyOffer(models.Model):
     )
     partner_id = fields.Many2one('res.partner', string="Partner", required=True)
     property_id = fields.Many2one('estate.property', string="Property", required=True)
-    
 
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute='_compute_deadline', inverse='_inverse_deadline', string="Deadline")
+
+    @api.depends('create_date', 'validity')
+    def _compute_deadline(self):
+        for record in self:
+            if(record.create_date):
+                record.date_deadline = fields.Date.add(record.create_date, days=record.validity )
+            else:
+                record.date_deadline = fields.Date.add(fields.Date.today(), days=record.validity )
+
+    def _inverse_deadline(self):
+        for record in self:
+            record.validity = (record.date_deadline - fields.Date.to_date(record.create_date)).days
