7833b1a0ae75b4633ec14e010827e0353bba2608
habar <habar@odoo.com>
2026-03-24 18:17:37 +0530

    [IMP] estate: added computed field automatic area calculation Add a new field that automatically calculates the total property area. By linking the living area and garden area, the system now updates the total size whenever either value changes. This removes the need for manual entry and prevents mistakes.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,5 +1,5 @@
 from dateutil.relativedelta import relativedelta
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class EstateProperty(models.Model):
@@ -9,7 +9,7 @@ class EstateProperty(models.Model):
     name = fields.Char(string="Title", required=True)
     description = fields.Text(string="Description")
     postcode = fields.Char(string="Postcode")
-    date_availability = fields.Date(string="Available From", copy=False, default=fields.Date.today() + relativedelta(months=3))
+    date_availability = fields.Date(string="Available From", copy=False, default=lambda self: fields.Date.today() + relativedelta(months=3))
     expected_price = fields.Float(string="Expected Price", required=True)
     selling_price = fields.Float(string="Selling Price", copy=False)
     bedrooms = fields.Integer(string="Bedrooms", default=2)
@@ -18,6 +18,7 @@ class EstateProperty(models.Model):
     garage = fields.Boolean(string="Garage")
     garden = fields.Boolean(string="Garden")
     garden_area = fields.Integer(string="Garden Area (spm)")
+    total_area = fields.Float(string="Total Area (sqm)", compute="_computed_total_area", store=True)
     garden_orientation = fields.Selection([
         ('north', "North"),
         ('east', "East"),
@@ -37,3 +38,8 @@ class EstateProperty(models.Model):
     buyer_id = fields.Many2one('res.partner', string='Buyer', ondelete='cascade')
     property_tag_ids = fields.Many2many('estate.property.tag')
     offer_ids = fields.One2many('estate.property.offer', 'property_id', string="Offers")
+
+    @api.depends("living_area", "garden_area")
+    def _computed_total_area(self):
+        for rec in self:
+            rec.total_area = rec.living_area + rec.garden_area
