9783a74eeb9b7c10ba9b991844f304bb1d9057f2
Quentin George <qugeo@odoo.com>
2026-03-25 15:35:00 +0100

    [IMP] estate: Chapter 14 - Add kanban view for properties

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -17,6 +17,7 @@
 
         'views/estate_property_view_list.xml',
         'views/estate_property_view_form.xml',
+        'views/estate_property_view_kanban.xml',
         'views/estate_property_view_search.xml',
 
         'views/estate_property_type_view_form.xml',
