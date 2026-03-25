6624b496434941ac82a3708ea5c734bfa1beea0f
kapat-odoo <kapat@odoo.com>
2026-03-25 15:50:08 +0530

    [IMP] awesome_owl: implemented counters, card and todo list.
    Implemented Counter, Card and Todo list using OWL.
    
    Added Counters which use state to increment value based on the button pressed
    also added the sum of 2 counters. Added Cards to display both markup and non
    markup code along with default slots and a toggle switch to show or hide the
    content of the card, created a reactive Todo list which create, delete a task
    and marking them as done using the checkbox.
    
    As per discussed in Chapter 1 of OWL documentation.

---
+++ awesome_owl/static/src/card/card.js
@@ -0,0 +1,14 @@
+import { Component, useState } from "@odoo/owl";
+
+export class Card extends Component {
+    static template = "awesome_owl.card";
+    static props = ['title', '*']; 
+
+    setup() {
+        this.state = useState({ isOpen: true });
+    }
+
+    toggleContent() {
+        this.state.isOpen = !this.state.isOpen;
+    }
+}
