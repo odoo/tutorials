6d94e6e92d12d409757a7687e69d8c910964d8f4
Codoeh <pawlakmateusz96@gmail.com>
2026-03-25 17:56:01 +0100

    [ADD] Add remove button to TodoList

--- addons/awesome_owl/static/src/todo/todo_item.js
+++ addons/awesome_owl/static/src/todo/todo_item.js
@@ -7,5 +7,6 @@ export class TodoItem extends Component {
     static props = {
         todo: {type: Object, shape: {id: Number, description: String, isCompleted: Boolean}},
         toggleState: {type: Function},
+        removeTodo: {type: Function},
     }
 }
--- addons/awesome_owl/static/src/todo/todo_list.js
+++ addons/awesome_owl/static/src/todo/todo_list.js
@@ -26,4 +26,11 @@ export class TodoList extends Component {
         current_todo.isCompleted = !current_todo.isCompleted
 
     }
+
+    removeTodo(id) {
+        const index = this.todos.findIndex((elem) => elem.id === id);
+        if (index >= 0) {
+            this.todos.splice(index, 1);
+        }
+    }
 }
