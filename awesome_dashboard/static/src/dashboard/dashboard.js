b1bc98e4a9deafa03a3e9a8169e110b895daefe3
Anmol Dhaliwal <andha@odoo.com>
2026-03-25 16:16:50 +0100

    [IMP] Web Framework Tutorial - Chapter 2 - Ch 9, 10, 11

--- awesome_dashboard/static/src/dashboard/dashboard.js
+++ awesome_dashboard/static/src/dashboard/dashboard.js
@@ -4,7 +4,10 @@ import { Layout } from "@web/search/layout";
 import { useService } from "@web/core/utils/hooks";
 import { AwesomeDashboardItem } from "./dashboard_item";
 import { rpc } from "@web/core/network/rpc";
-import { PieChart } from "./pie_chart";
+import { PieChart } from "./pie_chart/pie_chart";
+import { Dialog } from "@web/core/dialog/dialog";
+import { CheckBox } from "@web/core/checkbox/checkbox";
+import { browser } from "@web/core/browser/browser";
 
 class AwesomeDashboard extends Component {
     static components = { AwesomeDashboardItem, PieChart, Layout };
@@ -13,6 +16,15 @@ class AwesomeDashboard extends Component {
     setup() {
         this.action = useService("action");
         this.stats = useState(useService("awesome_dashboard.statistics"));
+        this.items = registry.category("awesome_dashboard").getAll();
+        this.dialog = useService("dialog");
+
+        const hiddenItems = JSON.parse(
+            browser.localStorage
+                .getItem("disabled_dashboard_items")
+                ?.split(",") || '[]',
+        );
+        this.state = useState({ disabledItems: hiddenItems });
 
         onWillStart(async () => {
             const res = await rpc("/awesome_dashboard/statistics");
@@ -36,8 +48,65 @@ class AwesomeDashboard extends Component {
             ],
         });
     }
+
+    openConfig() {
+        this.dialog.add(ConfigDialog, {
+            items: this.items,
+            disabled: this.state.disabledItems,
+            onUpdate: (newDisabledItems) => {
+                this.state.disabledItems = newDisabledItems;
+                browser.localStorage.setItem(
+                    "disabled_dashboard_items",
+                    JSON.stringify(newDisabledItems),
+                );
+            },
+        });
+    }
 }
 
-registry
-    .category("lazy_components")
-    .add("AwesomeDashboard", AwesomeDashboard);
+class ConfigDialog extends Component {
+    static template = "awesome_dashboard.config";
+    static components = { CheckBox, Dialog };
+
+    static props = {
+        items: Array,
+        close: Function,
+        disabled: Array,
+        onUpdate: Function,
+    };
+
+    setup() {
+        this.items = useState(
+            this.props.items.map((item) => ({
+                ...item,
+                isEnabled: !this.props.disabled.includes(item.id),
+            })),
+        );
+    }
+
+    apply() {
+        const disabledIds = this.items
+            .filter((i) => !i.isEnabled)
+            .map((i) => i.id);
+
+        this.props.onUpdate(disabledIds);
+        this.props.close();
+    }
+
+    onCheck(item, value) {
+        console.log(item, value)
+        item.isEnabled = value;
+        const updatedDisabledItemsList = Object.values(this.items)
+            .filter((item) => !item.isEnabled)
+            .map((item) => item.id);
+
+        browser.localStorage.setItem(
+            "disabled_dashboard_items",
+            updatedDisabledItemsList,
+        );
+
+        this.props.onUpdate(updatedDisabledItemsList);
+    }
+}
+
+registry.category("lazy_components").add("AwesomeDashboard", AwesomeDashboard);
