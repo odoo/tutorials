c5d9714320a8d871ce6d119d32f36d5c01acf91a
aykhu-odoo <aykhu@odoo.com>
2026-03-26 19:06:25 +0530

    [IMP] estate: Implemented onchange logic and validity with inverse function
    - Added Validity and date_deadline fields with inverse function
    - Implemented onchange logic for garden_area and garden_orientation
    - Suggested fixes implemented
    - Covers last 2 tasks of chapter 8.

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,6 @@
-from odoo import fields, models
+from dateutil.relativedelta import relativedelta
+
+from odoo import api, fields, models
 
 
 class EstatePropertyOffer(models.Model):
@@ -16,3 +18,22 @@ class EstatePropertyOffer(models.Model):
     )
     partner_id = fields.Many2one("res.partner", required=True)
     property_id = fields.Many2one("estate.property", required=True)
+    validity = fields.Integer(string="Validity (days)", default=7)
+    date_deadline = fields.Date(
+        string="Deadline", compute='_compute_date_deadline', inverse="_inverse_deadline",
+    )
+
+    @api.depends('create_date', 'validity')
+    def _compute_date_deadline(self):
+        for record in self:
+            start_date = (
+                record.create_date.date() if record.create_date else fields.Date.today()
+            )
+            record.date_deadline = start_date + relativedelta(days=record.validity)
+
+    def _inverse_deadline(self):
+        for record in self:
+            start_date = (
+                record.create_date.date() if record.create_date else fields.Date.today()
+            )
+            record.validity = (record.date_deadline - start_date).days
