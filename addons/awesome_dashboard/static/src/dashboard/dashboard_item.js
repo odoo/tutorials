7c2e735ceae5d2a0c1676fe5a4893fd736f5a08b
Codoeh <pawlakmateusz96@gmail.com>
2026-03-26 11:09:40 +0100

    [IMP] awesome_dashboard: Add DashboardItem component

---
+++ addons/awesome_dashboard/static/src/dashboard/dashboard_item.js
@@ -0,0 +1,10 @@
+import {Component, useState} from "@odoo/owl"
+
+
+export class DashboardItem extends Component {
+    static template = "awesome_dashboard.dashboard_item";
+    static props = {
+        size: {type: Number, optional: true, default: 1},
+        slots: {type: Object, optional: true, default: null},
+    };
+}
--- addons/awesome_owl/static/src/playground/playground.js
+++ addons/awesome_owl/static/src/playground/playground.js
@@ -8,9 +8,6 @@ import {TodoList} from "../todo/todo_list";
 export class Playground extends Component {
     static template = "awesome_owl.playground";
     static components = {Counter, Card, TodoList};
-    static props = {
-        description: {type: String, optional: true},
-    };
 
     setup() {
         this.state = useState({sum: 2})
