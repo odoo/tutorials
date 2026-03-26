f61b33ccf3f5ce68116be0e971469b88f61699f1
Quentin George <qugeo@odoo.com>
2026-03-26 09:48:47 +0100

    [FIX] estate: Name model resUsers

--- estate/models/res_users.py
+++ estate/models/res_users.py
@@ -2,6 +2,7 @@ from odoo import fields, models
 
 
 class resUsers(models.Model):
-    _inherit = ['res.users']
+    _name = 'res.users'
+    _inherit = 'res.users'
 
     property_ids = fields.One2many('estate.property', 'salesman_id', domain=['|', ('state', '=', 'new'), ('state', '=', 'offer_received')])
