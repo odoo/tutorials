bce1ecf69e002150642bbe953c2b35a3e879eee6
Quentin George <qugeo@odoo.com>
2026-03-26 16:00:03 +0100

    [IMP] awewsome_owl: Chapter 5

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -2,6 +2,7 @@ import { markup, Component, useState } from "@odoo/owl";
 
 export class Card extends Component {
     static template = "awesome_owl.card";
+    static props = ["title", "content"];
 
     setup() {
         this.state = useState({htmlLink: markup('<a href="/odoo" target="_blank">test</a>')})
