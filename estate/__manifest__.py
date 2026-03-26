91812ea99ea9bf343d12e8d1d2b3376c045a5365
Adham-K-Fahmy <noorcool116@gmail.com>
2026-03-26 14:26:31 +0100

    [IMP] estate: Added data and demo

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -9,12 +9,18 @@
     'data': [
         'security/ir.model.access.csv',
 
+        'data/estate.property.type.csv',
+
         'views/estate_property_views.xml',
         'views/estate_property_offer_views.xml',
         'views/estate_property_types_views.xml',
         'views/estate_property_tags_views.xml',
         'views/res_user_views.xml',
         'views/estate_menus.xml',
+    ],
+    'demo': [
+        'demo/estate_properties_demo.xml',
+        'demo/estate_offers_demo.xml',
     ],
     'author': 'Odoo S.A.',
     'license': 'AGPL-3'
