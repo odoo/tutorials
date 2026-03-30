bc1cb3e9c02055f9f8a85a025cc6f353f2214e55
Your Name <you@example.com>
2026-03-30 15:09:04 +0700

    Thêm invoice cho module estate property

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,5 +1,5 @@
 
-from odoo import models
+from odoo import Command, models
 
 
 class EstatePropertyModel(models.Model):
@@ -10,6 +10,20 @@ class EstatePropertyModel(models.Model):
     def action_sold(self):
         res = super().action_sold()
         for record in self:
-            invoice_vals = {"partner_id":record.buyer_id.id,"move_type":'out_invoice'}
+            invoice_vals = {"partner_id":record.buyer_id.id,
+                            "move_type":'out_invoice',
+                            "invoice_line_ids": [
+                                Command.create({
+                                    "name": record.name,
+                                    "quantity": 1,
+                                    "price_unit": record.selling_price * 0.06
+                                }),
+                                Command.create({
+                                    "name": "Administrative fees",
+                                    "quantity": 1,
+                                    "price_unit": 100.00,
+                                })
+                            ]
+                            }
             self.env['account.move'].create(invoice_vals)
         return res
