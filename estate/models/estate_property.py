662d410fc1c336983d4858bb42a8159e562880c1
Sudarshan Maity (sumai) <sumai@odoo.com>
2026-03-31 18:57:19 +0530

    [IMP] Estate: Chapter 8- Ongoing
    Completed Chapter 8 Exercise 2
    Learnt about relational computed fields
    created new field best offers which is being computed using top highest prices

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -16,7 +16,7 @@ class EstateProperty(models.Model):
     bedrooms = fields.Integer(string="Bedrooms", default=2)
     living_area = fields.Integer(string="Living Area (sqm)", copy=False)
     garden_area = fields.Integer(string="Garden Area (sqm)")
-    total_area = fields.Integer(compute="_compute_total_area")
+    total_area = fields.Integer(string="Total Area (sqm)", compute="_compute_total_area", store=True)
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
@@ -39,24 +39,22 @@ class EstateProperty(models.Model):
             ('cancelled', "Cancelled")
         ],
         string="Status", required=True, copy=False, default='new')
-    swimming_pool = fields.Boolean(string="Swimming Pool")  # extra fields
-    # property_type = fields.Selection(
-    #     [
-    #         ('house', "House"),
-    #         ('apartment', "Apartment"),
-    #         ('villa', "Villa"),
-    #         ('land', "Land")
-    #     ],
-    #     string="Property Type"
-    # )
+    swimming_pool = fields.Boolean(string="Swimming Pool")
     property_age = fields.Integer(string="Property Age")
     property_type_id = fields.Many2one("estate.property.type", string="Property Type")
     buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
     salesperson_id = fields.Many2one("res.users", string="Salesperson", default=lambda self: self.env.user)
     tags_ids = fields.Many2many("estate.property.tag", string="Property Tags")
     offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
+    best_price = fields.Float(string="Best Offer", compute="_compute_best_price")
 
     @api.depends('living_area', 'garden_area')
     def _compute_total_area(self):
         for record in self:
             record.total_area = record.living_area + record.garden_area
+
+    @api.depends("offer_ids.price")
+    def _compute_best_price(self):
+        for record in self:
+            prices = record.offer_ids.mapped("price")
+            record.best_price = max(prices) if prices else 0.0
