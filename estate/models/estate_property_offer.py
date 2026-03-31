5c962b17189d57e3a2f8d08490aeb74de9d1d1f4
habar <habar@odoo.com>
2026-03-31 18:10:49 +0530

    [IMP] estate: added methods to the estate property offer
    This functionality define to accept & refuse offer once offer is accepted it
    set selling price and buyer to the corresponding property.
    generates error if more than one offer is try to accept, added buttons to the
    corresponding actions used 'checked' & 'unchecked' icons.

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,6 +1,7 @@
 from dateutil.relativedelta import relativedelta
 
 from odoo import api, fields, models
+from odoo.exceptions import UserError
 
 
 class EstatePropertyOffer(models.Model):
@@ -27,3 +28,19 @@ class EstatePropertyOffer(models.Model):
         for rec in self:
             create_date = rec.create_date.date() if rec.create_date else fields.Date.today()
             rec.validity = relativedelta(rec.date_deadline, create_date).days
+
+    def action_accept_offer(self):
+        for rec in self:
+            if any(i.status == "accepted" for i in rec.property_id.offer_ids):
+                raise UserError("Offer already accepted for given property.")
+
+            rec.status = "accepted"
+            rec.property_id.buyer_id = rec.partner_id.id
+            rec.property_id.selling_price = rec.price
+            rec.property_id.state = "offer_accepted"
+            return True
+
+    def action_refuse_offer(self):
+        for rec in self:
+            rec.status = "refused"
+        return True
