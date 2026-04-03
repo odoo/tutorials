d51a3569bca14c1625982dd754c94128bd63d8b3
ripil-odoo <ripil@odoo.com>
2026-04-03 10:12:57 +0530

    [CLN] estate: Code cleanup in the new real estate module
    Removed the unnecessary encoding declaration and removed trailing whitespaces from multiple lines in the '__manifest__.py' file. Also, added the missing extra line in the end of the code file
    
    Chapter 2

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,17 +1,14 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-
-{   
+{
     'name': 'Estate',
     'category': 'Real Estate',
     'sequence': 1,
     'summary': 'Making the real estate app from tutorials',
-    'website': 'www.google.com',
+    'website': 'www.odoo.com',
     'depends': [
         'base',
     ],
     'installable': True,
-    'application': True,    
+    'application': True,
     'author': 'Rini Pillai',
 }
+
