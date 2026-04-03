8de6abe3314b31b56089218adca659181cec6911
Arbaaz-Khan-Tech <asurk@odoo.com>
2026-04-03 12:47:54 +0530

    [ADD] real_estate: added real_estate module
    Add a new real estate module to handle properties and related features.
    Made it an application so it can be installed and used easily.

---
+++ estate/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Asurk",
+    'category': 'Services',
+    'description': """
+    A module so that customers can bid on real estates
+    """,
+    'application': True,
+    'installable': True,
+    # data files always loaded at installation
+
+}
