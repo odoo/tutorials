bc403177459923e37f78e5aa46124077c53a3d01
odoo <radhr@odoo.com>
2026-04-06 15:01:45 +0530

    [IMP] estate: added action in estate
    Allows users to easily access and navigate estate data through a dedicated
    interface. Improves usability by providing a clear and structured view for
    managing records.
    
    Chapter 5: Finally some UI to play with.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,7 +4,9 @@
     'category': 'tutorials',
     'installable': True,
     'application': True,
-    'data':[
-        'security/ir.model.access.csv'
-        ]
+    'data': [
+        'security/ir.model.access.csv',
+
+        'views/estate_property_view.xml'
+    ]
 }
