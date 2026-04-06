04229c37090b27423a0cef6c93894cb58328f6a6
odoo <radhr@odoo.com>
2026-04-06 15:48:45 +0530

    [IMP] estate: added menu XML
    Added menu hierarchy to improve navigation and feature accessibility for users
    to navigate using provided views
    
    Chapter 5: Finally some UI to play with

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,6 +7,7 @@
     'data': [
         'security/ir.model.access.csv',
 
-        'views/estate_property_view.xml'
+        'views/estate_property_view.xml',
+        'views/estate_menu.xml'
     ]
 }
