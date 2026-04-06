16130399d6f28638778ec7d42a54743534f74b76
ripil-odoo <ripil@odoo.com>
2026-04-06 18:57:47 +0530

    [ADD] estate: Availability Date Calculation in estate module
    Calculated the date with 3 month addition for the date_availability field dynamically.
    
    Chapter 5

--- estate/models/estate_properties.py
+++ estate/models/estate_properties.py
@@ -1,5 +1,14 @@
+from logging import INFO
 from odoo import models, fields
+import logging
+from datetime import date, timedelta
+from dateutil import relativedelta as rd
 
+_logger = logging.getLogger(__name__)
+
+def get_availability_date(self):
+    # _logger.info("!!! %s", self)
+    return fields.Date.today() + rd.relativedelta(months=3)
 
 class EstateProperties(models.Model):
     _name = "estate.properties"
@@ -8,10 +17,10 @@ class EstateProperties(models.Model):
     name = fields.Char('Property Name', required=True, help="Name of the property shown")
     description = fields.Text('Description', help="Description of the property shown")
     postcode = fields.Char('Postcode', help="Postal Code of the property shown")
-    date_availability = fields.Date('Availability Date', help="Date of availability of the property shown")
+    date_availability = fields.Date('Availability Date', help="Date of availability of the property shown", copy=False, default=get_availability_date)
     expected_price = fields.Float('Expected Price', required=True, help="Expected price of the property shown")
-    selling_price = fields.Float('Selling Price', help="Selling Price of the property shown")
-    bedrooms = fields.Integer('Bedrooms', help="Number of bedrooms in the property shown")
+    selling_price = fields.Float('Selling Price', help="Selling Price of the property shown", readonly=True, copy=False)
+    bedrooms = fields.Integer('Bedrooms', help="Number of bedrooms in the property shown", default=2)
     living_area = fields.Integer('Living Area', help="Number of living rooms in the property shown")
     facades = fields.Integer('Facades', help="Number of facades in the property shown")
     garage = fields.Boolean('Has Garage?', help="Does the proeprty have a garage?")
