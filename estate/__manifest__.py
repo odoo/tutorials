8dad73a88bafb650e9c0fd7fb239890d061b17c6
Arbaaz-Khan-Tech <asurk@odoo.com>
2026-04-08 10:22:48 +0530

    [ADD] real_estate: add access rights for users
    - Add ir.model.access.csv to define and manage access rights for users within the real_estate module.
    - Update __manifest__.py to include the path to the new ir.model.access.csv file, making it discoverable by Odoo.
    
    Chapter #4

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,10 +3,14 @@
     'version': '1.0',
     'depends': ['base'],
     'author': "Asurk",
-    'category': 'Services',
+    'category': 'Real Estate/Brokerage',
     'description': """
     A module so that customers can bid on real estates
     """,
+    'data': [
+        'security/ir.model.access.csv',
+        'security/estate_security.xml',
+    ],
     'license': 'LGPL-3',  # Default License
     'application': True,
     'installable': True,
