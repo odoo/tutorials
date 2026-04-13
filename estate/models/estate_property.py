58062c66837861ce1313c210d2f99310f7e73a5b
habar-odoo <habar@odoo.com>
2026-04-13 17:28:23 +0530

    [IMP] estate: extend res.users with property inheritance
    Inherit res.users model added property_ids field to display the list of
    properties linked to a salesperson.
    The field is One2many inverse of the field that references the
    salesperson in estate.property, Added domain to the field so it only lists
    the available properties.
    create res_users_view inherit 'base.view_users_form' using XPath to include a
    new notebook page for properties linked to the salesperson.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -38,7 +38,7 @@ class EstateProperty(models.Model):
         ('cancelled', "Cancelled")
     ], copy=False, default='new')
     property_type_id = fields.Many2one('estate.property.type', string="Property Type", ondelete="cascade")
-    sales_person_id = fields.Many2one('res.users', string='Salesman', ondelete='cascade')
+    sales_person_id = fields.Many2one('res.users', string='Sales Person', ondelete='cascade', default=lambda self: self.env.user)
     buyer_id = fields.Many2one('res.partner', string='Buyer', ondelete='cascade')
     property_tag_ids = fields.Many2many('estate.property.tag')
     offer_ids = fields.One2many('estate.property.offer', 'property_id', string="Offers")
