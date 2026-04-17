446d92c7584be273968555cc961fdde1007ac072
habar-odoo <habar@odoo.com>
2026-04-17 18:04:19 +0530

    [ADD] estate_account: add estate_account module for invoice generation
    Added a link module to handle invoice creation logic:
    Model: Inherited estate.property and overrode action_property_sold.
    Logic: Generates an account.move with move_type='out_invoice' using the property
    partner.
    Invoice Lines:
    Commission: 6% of the selling price.
    Fees: 100.00 flat administrative charge.

---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,26 @@
+from odoo import models, Command
+
+
+class EstateAccountProperty(models.Model):
+    _inherit = "estate.property"
+
+    def action_property_sold(self):
+        self.env['account.move'].create({
+            'partner_id': self.buyer_id.id,
+            'move_type': 'out_invoice',
+
+            'invoice_line_ids': [
+                Command.create({
+                    'name': "6% Commission",
+                    'quantity': 1,
+                    'price_unit': self.selling_price * 0.06,
+                }),
+                Command.create({
+                    'name': "Administrative Fees",
+                    'quantity': 1,
+                    'price_unit': 100.00,
+                }),
+            ],
+        })
+
+        return super().action_property_sold()
