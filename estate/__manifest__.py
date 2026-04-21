ebe26b3f323d540a7b5c40b9043221e00c1ed551
odoo <odoo@vibad-laptop.local>
2026-04-21 17:28:10 +0200

    [ADD] Chapitre 5 Menus

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,7 +6,7 @@
     'installable': True,
     'application': True,
     'author': 'vibad',
-    'data': ['security/ir.model.access.csv']
+    'data': ['security/ir.model.access.csv', 'view/estate_property_views.xml', 'view/estate_action.xml']
 
 
 }
