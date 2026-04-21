eae996b76df46492def5f04fbdc676ed184ecc32
SebVde <sevan@odoo.com>
2026-04-21 15:46:24 +0200

    [ADD] estate: initialized Sevan Estate module

---
+++ estate/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': "Sevan Estate",
+    'depends': ['base'],
+    'summary': "Just a simple estate app for Sevan.",
+    'application': True,
+    'installable': True,
+    'author': "Sevan Corp.",
+}
