30ee0b4cebf0d90888191ffacb57fd09b88c5feb
Benoît Lambert <belam@odoo.com>
2026-04-21 10:44:30 +0200

    [IMP] estate: created module
    [IMP] estate: created module

---
+++ estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Real Estate",
+    "version": "0.1",
+    "category": "Custom",
+    "summary": "My first application",
+    "installable": True,
+    "application": True,
+    "author": "belam",
+    "depends": {"base"},
+    "license": "LGPL-3",
+}
