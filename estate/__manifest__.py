6a48a788ee97b894c72f8adfffd8966352ab393c
Rana <raame@odoo.com>
2026-04-22 14:02:20 +0200

    [ADD] estate: Creating new estate module with estate property model and access rights
    A new estate module was created. Property model was created as well and contains some fields. Access rights were added.

---
+++ estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Real Estate',
+    'depends': [
+        'base'
+    ],
+    'data': [
+        'security/ir.model.access.csv'
+    ],
+    'application': True,
+    'license': 'AGPL-3'
+}
