2a943c1a9d2abe4e82ca1f2be9b84ea7701e1f7b
dalio <dalio@odoo.com>
2026-04-22 09:47:17 +0200

    [ADD] estate: add estate_property

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,29 @@
-from odoo import models
+from odoo import models, fields
 
-class TestModel(models.Model):
-    _name = "test_model"
+class EstateProperty(models.Model):
+
+    _name = "estate.property"
+    _description = "Real Estate Property"
+
+    name = fields.Char(required = True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float(required = True)
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        string = "Orientation",
+        selection = [
+            ('North', 'north'),
+            ('South', 'south'),
+            ('West', 'west'),
+            ('East', 'est')
+        ],
+        help = "Orientation of the garden"
+    )
