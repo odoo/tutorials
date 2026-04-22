3702745e87ab5f1d75441c60479d473fb38ebe37
simal-odoo <simal@odoo.com>
2026-04-22 14:13:52 +0200

    [ADD] estate: models and basic fields
    Added fields in __manifest__.py
    Created models folder, EstateProperty model with the required fields and imported it in __manifest__.py files

---
+++ estate/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Estate",
+    'summary': """
+        The Real Estate Advertisement module
+    """,
+
+    'description': """
+        The Real Estate Advertisement module
+    """,
+    'author': "Odoo",
+    'website': "https://www.odoo.com/",
+    'category': 'Tutorials',
+    'version': '0.1',
+    'application': True,
+    'installable': True,
+    'depends': ['base'],
+    'data': [],
+    'license': 'AGPL-3'
+}
