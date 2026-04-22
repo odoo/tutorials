86dbba3ad41d268633fb04b8a7b0e96fe7ae386a
vibad <vibad@odoo.com>
2026-04-22 11:27:58 +0200

    [ADD] Chapitre 5

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -8,17 +8,25 @@ class Estate_property(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=fields.Date.add(fields.Date.today(), months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
-        string='Orientation', 
+        string='Orientation',
         selection=[('notrh', 'Notrh'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
-        help="The garden orientation"
+        help="The garden orientation",
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        string="Status",
+        selection=[('new', 'New'), ('offer Received', 'Offer Received'), ('offer Accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')],
+        require=True,
+        copy=False,
+        default="new",
     )
