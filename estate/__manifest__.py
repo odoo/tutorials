a1c6e1e2bb20777403fb712c6b8c6621885d43a3
Ahmed Naem (Ahnae) <ahmedamein100@gmail.com>
2026-04-22 14:06:54 +0200

    [ADD] estate: add access rights for property model

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,5 +3,8 @@
     'version': '1.0',
     'depends': ['base'],
     'category': 'Tutorials',
-    'application': True
+    'application': True,
+    'data' : ['security/ir.model.access.csv'],
+    'author': "Odoo",
+    'license': 'AGPL-3'
 }
