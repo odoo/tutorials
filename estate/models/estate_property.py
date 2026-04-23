3bd3578c533980e8381dd2f3485fd6bd41ee6791
dalio <dalio@odoo.com>
2026-04-23 16:16:58 +0200

    [ADD] estate: add computed fields and onchanges

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,5 +1,5 @@
 from dateutil.relativedelta import relativedelta
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class EstateProperty(models.Model):
@@ -49,3 +49,29 @@ class EstateProperty(models.Model):
     buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
     tag_ids = fields.Many2many("estate.property.tag", string="Tags")
     offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
+    total_area = fields.Integer(compute="_compute_total_area", string="Total area (sqm)")
+    best_price = fields.Float(compute="_compute_best_price", string="Best Offer")
+
+    @api.depends("living_area", "garden_area")
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
+    @api.depends("offer_ids.price")
+    def _compute_best_price(self):
+        for record in self:
+            prices = record.offer_ids.mapped("price")
+
+            if prices:
+                record.best_price = max(prices)
+            else:
+                record.best_price = 0.0
+
+    @api.onchange("garden")
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = "north"
+        else:
+            self.garden_area = 0
+            self.garden_orientation = False
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from dateutil.relativedelta import relativedelta
+from odoo import fields, models, api
 
 
 class EstatePropertyOffer(models.Model):
@@ -17,3 +18,21 @@ class EstatePropertyOffer(models.Model):
     )
     partner_id = fields.Many2one('res.partner', string="Partner", required=True)
     property_id = fields.Many2one('estate.property', string="Property", required=True)
+    validity = fields.Integer(default=7, string="Validity (days)")
+    date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_date_deadline", string="Deadline")
+
+    @api.depends("create_date", "validity")
+    def _compute_date_deadline(self):
+        for record in self:
+            base_date = record.create_date or fields.Date.today()
+            days_to_add = record.validity or 0
+            record.date_deadline = base_date + relativedelta(days=days_to_add)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            base_date = fields.Date.to_date(record.create_date) or fields.Date.today()
+            
+            if record.date_deadline:
+                record.validity = (record.date_deadline - base_date).days
+            else:
+                record.validity = 0
