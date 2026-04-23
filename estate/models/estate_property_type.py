58cc017ce5574e585fd17703ac75e0757b85b5f1
SebVde <sevan@odoo.com>
2026-04-23 10:19:32 +0200

    [ADD] estate: add property type, salesman and buyer fields

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -3,6 +3,6 @@ from odoo import fields, models
 
 class PropertyType(models.Model):
     _name = "estate.property.type"
-    _description = "Estate Property"
+    _description = "Estate Property Type"
 
     name = fields.Char(required=True)
