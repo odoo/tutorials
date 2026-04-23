9d4d467129a4af7ae42cd3b164edcf09e34e9b4f
Rana <raame@odoo.com>
2026-04-23 12:29:53 +0200

    [ADD] estate: Added actions, menues, and new fields
    Added an xml file for actions. Also added the 3 levels of menu items and linked them to actions. Finally, some new fields were added like active and state. Some new attributes were added to new and existing fields.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,11 +1,15 @@
 {
     'name': 'Real Estate',
+    'author': "Odoo",
     'depends': [
         'base'
     ],
     'data': [
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ],
     'application': True,
-    'license': 'AGPL-3'
+    'installable': True,
+    'license': 'AGPL-3',
 }
