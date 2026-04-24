a8f5d11b473f80b9fa98c2d00692890437ff0d86
Ahmed Naem (Ahnae) <ahmedamein100@gmail.com>
2026-04-24 11:19:06 +0200

    [FIX] estate: fix python linting and styling issues

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,7 +7,7 @@
     'category': 'Tutorials',
     'application': True,
     'data': [
-        'security/ir.model.access.csv', 
+        'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_property_type_views.xml',
         'views/estate_property_tag_views.xml',
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -19,21 +19,21 @@ class EstatePropertyOffer(models.Model):
         required=True
     )
     property_id = fields.Many2one(
-        "estate.property", 
-        string="Property", 
+        "estate.property",
+        string="Property",
         required=True,
     )
     validity = fields.Integer(default=7)
     date_deadline = fields.Date(
-        compute="_compute_date_deadline", 
+        compute="_compute_date_deadline",
         inverse="_inverse_date_deadline",
     )
 
-    @api.depends("validity","create_date")
+    @api.depends("validity", "create_date")
     def _compute_date_deadline(self):
         for record in self:
             record.date_deadline = fields.Date.add(
-                (fields.Date.to_date(record.create_date) or fields.Date.today()), 
+                (fields.Date.to_date(record.create_date) or fields.Date.today()),
                 days=record.validity
             )
 
