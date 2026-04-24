00ddb13e5a920f9ef7394b5d61e27c1ef72d0eec
simal-odoo <simal@odoo.com>
2026-04-24 11:30:56 +0200

    [ADD] estate: computed fields and onchanges
    Added total area (garden area + living area)
    Added best price
    Added validity date
    Added onchanges on garden fields

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class EstateProperty(models.Model):
@@ -15,6 +15,16 @@ class EstateProperty(models.Model):
     facades = fields.Integer('Facades')
     garage = fields.Boolean('Garage')
     garden = fields.Boolean('Garden')
+
+    @api.onchange('garden')
+    def _onchange_garden(self):
+        if not self.garden:
+            self.garden_area = 0
+            self.garden_orientation = False
+        else:
+            self.garden_area = 10
+            self.garden_orientation = 'north'
+
     garden_area = fields.Integer('Garden Area')
     garden_orientation = fields.Selection(
         selection=[
@@ -35,8 +45,20 @@ class EstateProperty(models.Model):
         default='new',
         required=True,
     )
-    property_type_id = fields.Many2one("estate.property.type", string="Property Type")
-    buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
-    salesperson_id = fields.Many2one("res.users", string="Salesperson", default=lambda self: self.env.uid)
-    tag_ids = fields.Many2many("estate.property.tag", string="Property Tag")
-    offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offer")
+    property_type_id = fields.Many2one('estate.property.type', string="Property Type")
+    buyer_id = fields.Many2one('res.partner', string="Buyer", copy=False)
+    salesperson_id = fields.Many2one('res.users', string="Salesperson", default=lambda self: self.env.uid)
+    tag_ids = fields.Many2many('estate.property.tag', string="Property Tag")
+    offer_ids = fields.One2many('estate.property.offer', 'property_id', string="Offer")
+
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for line in self:
+            line.total_area = line.living_area + line.garden_area
+    total_area = fields.Integer('Total Area', compute="_compute_total_area")
+
+    @api.depends('offer_ids.price')
+    def _compute_best_price(self):
+        for line in self:
+            line.best_price = max(line.mapped('offer_ids.price') or [0])
+    best_price = fields.Float('Best Offer', compute="_compute_best_price")
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import api, fields, models
+from datetime import timedelta
 
 
 class EstatePropertyOffer(models.Model):
@@ -14,3 +15,27 @@ class EstatePropertyOffer(models.Model):
     )
     partner_id = fields.Many2one("res.partner", string="Partner", required=True)
     property_id = fields.Many2one("estate.property", string="Property", required=True)
+    validity = fields.Integer("Validity (days)", default=7)
+
+    @api.depends('create_date', 'validity')
+    def _compute_date_deadline(self):
+        for offer in self:
+            if offer.validity:
+                if offer.create_date:
+                    offer.date_deadline = offer.create_date + timedelta(days=offer.validity)
+                else:
+                    offer.date_deadline = fields.Date.today() + timedelta(days=offer.validity)
+            else:
+                offer.date_deadline = False
+
+    def _inverse_date_deadline(self):
+        for offer in self:
+            if offer.date_deadline:
+                if offer.create_date:
+                    offer.validity = (offer.date_deadline - offer.create_date).days
+                else:
+                    offer.validity = (offer.date_deadline - fields.Date.today()).days
+            else:
+                offer.validity = 0
+
+    date_deadline = fields.Date("Deadline", compute="_compute_date_deadline", inverse="_inverse_date_deadline", store=True)
