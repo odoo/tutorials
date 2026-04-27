7876339256b781e7b09d4344e95585a682a14887
Rana <raame@odoo.com>
2026-04-27 09:45:02 +0200

    [ADD] estate: Chapter 7
    Many2one fields and One2many fields were added to represent types, tags, and offers

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -20,3 +20,8 @@ class Property(models.Model):
     garden_orientation = fields.Selection(selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
     active = fields.Boolean(default=True)
     state = fields.Selection(selection=[('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')], required=True, copy=False, default='new')
+    property_type_id = fields.Many2one("estate.property.type", string="property Type")
+    buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
+    sales_person_id = fields.Many2one("res.users", string="Salesperson", default=lambda self: self.env.user)
+    tag_ids = fields.Many2many("estate.property.tag", string="Tags")
+    offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
