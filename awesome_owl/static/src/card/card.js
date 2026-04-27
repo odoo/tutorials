206c82ebcb403b7d5ddaa42fb96d5204ac80ad61
joemo-odoo <joemo@odoo.com>
2026-04-27 16:49:52 +0200

    [IMP] awesome_owl: add option to hide card content
    add option to hide card content

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -1,9 +1,17 @@
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 
 export class Card extends Component {
     static template = "awesome_owl.Card";
     static props = {
         'title': {type: String},
-        'slots': {type: Object, shape: {default: true}}
+        'slots': {type: Object, shape: {default: true}},
     };
+
+    setup() {
+        this.state = useState({isOpen: true});
+    }
+
+    toggleOpen() {
+        this.state.isOpen = !this.state.isOpen;
+    }
 }
