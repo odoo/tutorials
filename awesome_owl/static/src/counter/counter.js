557486039a30ed6d143e292cde7c319190b02a53
times-odoo <times@odoo.com>
2026-04-27 11:59:06 +0530

    [ADD] awesome_owl: implement core Owl framework tutorial exercises
    This commit introduces the foundational components for the Awesome Owl
    learning module, covering the transition from static templates to
    reactive JavaScript components.
    
    Rationale:
    To learn the Owl framework in isolation from the complex Odoo web
    client, we implemented a series of interactive components that
    demonstrate the framework's reactivity and modularity.
    
    Key implementations:
    - Created a reactive 'Counter' component using the 'useState' hook
      to manage internal state.
    - Developed a 'Card' component utilizing props validation and 't-out'
      with 'markup' for safe HTML rendering.
    - Implemented a 'TodoList' with 'TodoItem' sub-components, featuring
      dynamic attributes, 't-foreach' loops, and callback props for
      parent-child communication (toggling and deleting tasks).
    - Integrated lifecycle hooks like 'onMounted' and 'useRef' to handle
      DOM focus via a custom 'useAutofocus' utility.
    - Refactored the 'Card' component to support 't-slot' for rendering
      arbitrary content and sub-components.
    
    Chapter-1 (Owl)

---
+++ awesome_owl/static/src/counter/counter.js
@@ -0,0 +1,29 @@
+import { Component, useState } from "@odoo/owl";
+
+export class Counter extends Component {
+    static template = "awesome_owl.counter";
+
+    static props = {
+        onincrement : { type: Function, optional: true },
+        ondecrement : { type: Function, optional: true }
+    };
+
+    setup() {
+        this.count = useState({value:0});
+    }
+
+    increment() {
+        this.count.value++;
+        if(this.props.onincrement){
+            this.props.onincrement();
+        }
+    }
+
+    decrement() {
+        this.count.value--;
+        if(this.props.ondecrement){
+            this.props.ondecrement();
+        }
+    }
+
+}
--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,5 +1,27 @@
-import { Component } from "@odoo/owl";
+import { markup, Component, useState } from "@odoo/owl";
+import { Counter } from "./counter/counter"
+import { Card } from "./card/card";
+import { TodoList } from "./todo/todo_list";
 
 export class Playground extends Component {
+    static components = {
+        Counter, 
+        Card,
+        TodoList,
+    };
+
     static template = "awesome_owl.playground";
+
+    setup(){
+        this.html = markup("<div class='text-info'>some text 2</div>")
+        this.sum = useState({value:0});
+    }
+
+    incrementSum(){
+        this.sum.value++;
+    }
+    
+    decrementSum(){
+        this.sum.value--;
+    }
 }
