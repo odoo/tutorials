b4848d4776034ba565851255a9ab2d221ee1d739
odoo <radhr@odoo.com>
2026-04-27 17:47:27 +0530

    [IMP] estate: view and architectural inheritance
    Adds a Real Estate page in the Salesperson form to view all assigned properties
    and their offers. Users can quickly see how many offers each salesperson is
    handling.Improves tracking and workload distribution across the team.
    Helps managers make better decisions and ensures efficient follow-ups.
    
    ch. 12 : Inheritance

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -11,6 +11,7 @@
         "views/estate_property_type_view.xml",
         "views/estate_property_tag_view.xml",
         "views/estate_menu.xml",
+        "views/user_res_view.xml",
     ],
     "author": "RADHR",
     "license": "LGPL-3",
