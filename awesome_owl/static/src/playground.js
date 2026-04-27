ba0852da1de03a437cc322be800c62f31159ff79
joemo-odoo <joemo@odoo.com>
2026-04-27 10:14:46 +0200

    [MOV] awesome_owl: move counter to own class
    making sure the counter can be reused

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,13 +1,8 @@
-import { Component, useState } from "@odoo/owl";
+import { Component } from "@odoo/owl";
+import { Counter } from "./counter/counter";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
+    static components = {Counter};
 
-    setup() {
-        this.state = useState({value: 0});
-    }
-
-    increment() {
-        this.state.value++;
-    }
 }
