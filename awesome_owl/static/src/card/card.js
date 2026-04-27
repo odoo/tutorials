09e67bbc2d4236674df1ebfd1dc4244e83a8bbfa
joemo-odoo <joemo@odoo.com>
2026-04-27 16:32:25 +0200

    [IMP] awesome_owl: introduction to slots
    make it possible to delete wrong todo items

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -2,5 +2,8 @@ import { Component } from "@odoo/owl";
 
 export class Card extends Component {
     static template = "awesome_owl.Card";
-    static props = ['title', 'content']
+    static props = {
+        'title': {type: String},
+        'slots': {type: Object, shape: {default: true}}
+    };
 }
