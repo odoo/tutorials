64a8839e4e7bc9788f7e107d55a3d13428f9d1dd
joemo-odoo <joemo@odoo.com>
2026-04-27 17:51:22 +0200

    [IMP] awesome_dashboard: a new layout
    make the app follow the common layout to keep the same look and feel over the complete environment

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,8 +1,14 @@
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
+    static components = {Layout};
+
+    setup() {
+        this.action = useService("action");
+    }
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
