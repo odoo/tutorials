eac3b2b0335578b36bc397de2e69330745afa861
odoo <radhr@odoo.com>
2026-04-28 18:24:07 +0530

    [IMP] estate_account: invoice feature introduced
    Automatically generates an invoice using invoice functionality,
    when a property is marked as sold. Reduces manual effort for users and
    administrators in creating invoices.
    
    ch. 13: Interact with other modules

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,8 +1,31 @@
-from odoo import models
+from odoo import models, Command
 
 
 class EstateProperty(models.Model):
     _inherit = "estate.property"
 
     def action_sold_offer(self):
+
+        invoice = self.env["account.move"].create(
+            {
+                "partner_id": self.buyer_id.id,
+                "move_type": "out_invoice",
+                "line_ids": [
+                    Command.create(
+                        {
+                            "name": "Amount",
+                            "quantity": 1,
+                            "price_unit": self.selling_price * 0.06,
+                        }
+                    ),
+                    Command.create(
+                        {
+                            "name": "Administration Fess",
+                            "quantity": 1,
+                            "price_unit": 100,
+                        }
+                    ),
+                ],
+            }
+        )
         return super().action_sold_offer()
