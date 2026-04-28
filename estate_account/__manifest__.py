9939864fdd083a972be539657a0fd7e54ac17549
dalio <dalio@odoo.com>
2026-04-28 15:34:34 +0200

    [ADD] estate_account: automate invoices creation on sold properties

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Invoicing",
+    "version": "19.0.1.1.0",
+    "depends": [
+        "estate",
+        "account",
+    ],
+    "data": [],
+    "application": False,
+    "installable": True,
+    "author": "dalio",
+    "license": "LGPL-3"
+}
