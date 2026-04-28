f127f24c45faf27f43646bf33dd69866cda98874
joemo-odoo <joemo@odoo.com>
2026-04-28 16:18:40 +0200

    [IMP] awesome_dashboard: lazy loading of module
    only load the dashboards when they need to be shown

--- awesome_dashboard/static/src/dashboard/dashboard.js
+++ awesome_dashboard/static/src/dashboard/dashboard.js
@@ -31,4 +31,4 @@ class AwesomeDashboard extends Component {
     }
 }
 
-registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
+registry.category("lazy_components").add("AwesomeDashboard", AwesomeDashboard);
