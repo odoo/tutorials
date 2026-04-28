f8efc82e47677d55a738921bc0a391c69406397c
joemo-odoo <joemo@odoo.com>
2026-04-28 10:32:14 +0200

    [IMP] awesome_dashboard: add statistics on dashboard
    it is better to show requested data over dummy data

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,8 +1,9 @@
-import { Component } from "@odoo/owl";
+import { Component, onWillStart } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
 import { useService } from "@web/core/utils/hooks";
 import { DashboardItem } from "./dashboard_item/dashboard_item";
+import { rpc } from "@web/core/network/rpc";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
@@ -10,6 +11,9 @@ class AwesomeDashboard extends Component {
 
     setup() {
         this.action = useService("action");
+        onWillStart(async () => {
+            this.results = await rpc("/awesome_dashboard/statistics");
+        });
     }
 
     openCustomers() {
