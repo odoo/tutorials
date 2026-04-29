830cc40aa3d05d040dd9e92f90a99dcbdaa7c659
habar-odoo <habar@odoo.com>
2026-04-29 17:23:41 +0530

    [IMP] awesome_owl: add owl components (counter, card)
    Implement counter component, add increment method to increase count & used
    useState hook as well as define template for it.
    Create card component define props (title, content) to pass information from
    parent to child component, understand the use of markup().
    Similarly, Also add incrementSum (callback props) method to calculate sum of two
    counters.

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,5 +1,18 @@
-import { Component } from "@odoo/owl";
+import { Component, markup, useState } from "@odoo/owl";
+import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
+    static components = { Counter, Card };
+
+    setup(){
+        this.htmlContent = markup("<span style='color:blue; font-weight:700'>some content.</span>")
+        this.normalString = "<p style='color:blue>some content.</p>"
+        this.state = useState({ sum: 0 });
+    }
+
+    incrementSum(){
+        this.state.sum++;
+    }
 }
