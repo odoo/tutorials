6dfb3d45f93f7af688c244505411420348d6dd96
joemo-odoo <joemo@odoo.com>
2026-04-29 14:32:14 +0200

    [IMP] awesome_dashboard: make dashboard items hideable
    make it possible to hide dashboarditems through a dialog

--- awesome_dashboard/static/src/dashboard/dashboard.js
+++ awesome_dashboard/static/src/dashboard/dashboard.js
@@ -1,8 +1,10 @@
-import { Component, useState } from "@odoo/owl";
+import { Component, useRef, useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
 import { useService } from "@web/core/utils/hooks";
 import { DashboardItem } from "./dashboard_item/dashboard_item";
+import { Dialog } from "@web/core/dialog/dialog";
+import { CheckBox } from "@web/core/checkbox/checkbox";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
@@ -11,7 +13,11 @@ class AwesomeDashboard extends Component {
     setup() {
         this.action = useService("action");
         this.statistics = useState(useService("awesome_dashboard.statistics").loadStatistics);
+        this.dialogService = useService("dialog");
         this.items = registry.category("awesome_dashboard").getAll();
+        this.state = useState({
+            disabled: localStorage.getItem("awesome_dashboard_disabled")?.split(',') || []
+        });
     }
 
     openCustomers() {
@@ -20,14 +26,42 @@ class AwesomeDashboard extends Component {
 
     openLeads() {
         this.action.doAction({
-            type: 'ir.actions.act_window',
-            target: 'current',
-            res_model: 'crm.lead',
-            views: [
-                [false, "list"],
-                [false, 'form'],
-            ],
+            type: 'ir.actions.act_window', target: 'current', res_model: 'crm.lead', views: [[false, "list"], [false, 'form'],],
         });
+    }
+
+    updateDashboard(disabled) {
+        this.state.disabled = disabled;
+    }
+
+    openDialog() {
+        this.dialog = this.dialogService.add(
+            DashboardConfiguration,
+            {items: this.items, disabled: this.state.disabled, doneUpdating: this.updateDashboard.bind(this)},
+            {});
+    }
+}
+
+class DashboardConfiguration extends Component {
+    static template = "awesome_dashboard.DashboardConfiguration";
+    static components = {Dialog, CheckBox};
+    static props = ["close", "items", "disabled", "doneUpdating"];
+
+
+    setup() {
+        this.options = useRef("options");
+        this.items = useState(this.props.items.map(item => ({...item, disabled: this.props.disabled.includes(item.id)})));
+    }
+
+    updateDisabled(item, checked) {
+        item.disabled = !checked;
+    }
+
+    apply() {
+        const disabled = this.items.filter(item => item.disabled).map(item => item.id);
+        localStorage.setItem("awesome_dashboard_disabled", disabled);
+        this.props.doneUpdating(disabled);
+        this.props.close();
     }
 }
 
