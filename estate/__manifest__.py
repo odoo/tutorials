1daba783b709335caf49c3ee1aebe26ad8e49d24
joemo-odoo <joemo@odoo.com>
2026-04-30 17:49:42 +0200

    [ADD] estate: create an estate module
    make a module for selling properties that can be imported in odoo saas environment

---
+++ estate/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Estate Tutorial",
+    "author": "joemo",
+    "license": "LGPL-3",
+    "application": True,
+    "depends": ["base", "base_import_module"],
+    "data": [
+        "models/estate_property.xml",
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
+    ]
+}
