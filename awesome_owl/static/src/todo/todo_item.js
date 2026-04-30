c502fc5f827d4a754753a3df4c34f8edefa88ce7
habar-odoo <habar@odoo.com>
2026-04-30 21:46:14 +0530

    [IMP] awesome_owl: implement interactive todo list
    Introduce TodoList and TodoItem components to manage tasks.
    The TodoList maintains a reactive state of todos, allowing
    users to add new tasks via an input field (on Enter key).

---
+++ awesome_owl/static/src/todo/todo_item.js
@@ -0,0 +1,13 @@
+import {Component} from "@odoo/owl"
+
+export class TodoItem extends Component {
+    static template = "awesome_owl.TodoItem"
+
+    static props = {
+        todo:{
+            id:Number,
+            description:String,
+            isCompleted:Boolean
+        }
+    }
+}
