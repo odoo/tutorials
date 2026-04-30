51daf3a74fb7082dd6612e09f4f9de28e9311893
SebVrc <sebvercammen@outlook.be>
2026-04-30 10:48:25 +0200

    [ADD] awesome_owl: added card component

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,12 +1,15 @@
-import { Component, useState } from "@odoo/owl";
+import { Component, markup, useState } from "@odoo/owl";
 import { Counter } from "./counter/counter";
+import { Card } from "./card/card"
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
-    static components = { Counter };
+    static components = { Counter, Card };
 
     setup() {
         this.state = useState({ value: 0});
+        this.str1 = "<div class='text-primary'>some content</div>";
+        this.str2 = markup("<div class='text-primary'>some content</div>");
     }
 
     increment() {
