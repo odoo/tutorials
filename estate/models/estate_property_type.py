aed782bea834fde964913b19cf19ccc896c4e5ec
Arbaaz-Khan-Tech <asurk@odoo.com>
2026-05-01 14:53:55 +0530

    [IMP] real_estate: Add sequence field for record ordering
    - Introduce a new sequence field to records.
    - Utilize this field to define and control the display order of records.
    
    Chapter #11

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -5,11 +5,12 @@ class EstatePropertyType(models.Model):
 
     _name = 'estate.property.type'
     _description = "A model where property types are defined"
-    _order = "name "
-
+    _order = "sequence, name, id"
     name = fields.Char(required=True, string="Property Type")
     property_ids = fields.One2many(
         'estate.property', inverse_name='property_type_id', string="Properties")
+    sequence = fields.Integer(default=10)
+
     _unique_property_type = models.Constraint(
         'UNIQUE(name)',
         'Property type must be unique'
