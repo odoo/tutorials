03ba54cfc1ec30dca5ed8725e1521470bb8426a9
ripil-odoo <ripil@odoo.com>
2026-05-04 18:22:54 +0530

    [ADD] estate_account: Invoices in estate account module
    Created draft customer invoices for a property when the 'sell' button is created also, automatically adding the customer as the buyer of the property.
    
    Chapter 13

---
+++ estate_account/models/estate_properties.py
@@ -0,0 +1,20 @@
+import logging
+
+from odoo import models
+
+
+_logger = logging.getLogger(__name__)
+
+
+class EstateProperties(models.BaseModel):
+    _inherit = ['estate.properties']
+
+    def property_sold(self):
+        # breakpoint()
+        invoice_vals = {
+            'move_type': 'out_invoice',
+            'partner_id': self.buyer_id.id, # type: ignore
+        }
+        # _logger.error(invoice_vals)
+        self.env['account.move'].create(invoice_vals)
+        return super().property_sold() # type: ignore
