8cf4767576cdc579ded4c9e29358bf50f70c7028
joemo-odoo <joemo@odoo.com>
2026-05-04 11:09:39 +0200

    [IMP] estate: add tags, types, and relations
    making the application more useful with types links to seller, what type of property it is, some tags and even calculation of total area

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,9 +5,16 @@
     "application": True,
     "depends": ["base", "base_import_module"],
     "data": [
+        "models/estate_property_type.xml",
+        "models/estate_property_tag.xml",
         "models/estate_property.xml",
+        "models/estate_property_offer.xml",
         "security/ir.model.access.csv",
+        "views/estate_property_offer_views.xml",
+        "views/estate_property_type_views.xml",
+        "views/estate_property_tag_views.xml",
         "views/estate_property_views.xml",
         "views/estate_menus.xml",
+        "data/x_estate.property.type.csv",
     ]
 }
