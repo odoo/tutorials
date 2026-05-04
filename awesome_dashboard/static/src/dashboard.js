0284a9d79da3c753d1f109d7ffbf3f7694245d82
habar-odoo <habar@odoo.com>
2026-05-04 17:39:22 +0530

    [IMP] awesome_dashboard: add dashboard structure and business statistics
    Implementing the base structure of the dashboard.
    Integrating the Layout component with  gray background.
    Adding a generic 'DashboardItem' component for a consistent card UI.
    Fetching real business data from the /awesome_dashboard/statistics route.
    Adding navigation buttons to quickly access Customers and Leads.

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,8 +1,40 @@
-import { Component } from "@odoo/owl";
+import { Component, onWillStart } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
+import { DashboardItem } from "./item/dashboardItem";
+import { rpc } from "@web/core/network/rpc";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
+    static components = { Layout, DashboardItem }
+
+    setup(){
+        this.display = {
+            controlPanel: {}
+        }
+
+    this.action = useService("action")
+    this.statistics = {};
+
+    onWillStart(async () => {
+        this.statistics = await rpc("/awesome_dashboard/statistics");
+        })
+    }
+
+    openCustomers(){
+        this.action.doAction("base.action_partner_form")
+    }
+
+    async openLeads(){
+        this.action.doAction({
+            type: 'ir.actions.act_window',
+            name: 'crm leads',
+            res_model: 'crm.lead',
+            views: [[false,'list'], [false,'form']],
+            target: 'current',
+        })
+    }
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
