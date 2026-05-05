32f8f4c0e481f61172fbb4e922ed3c601f012830
Xavier Zambrano <chzam@odoo.com>
2026-05-05 15:57:23 -0600

    [IMP] complete chapter 5

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,6 +6,9 @@
     'author': 'Xavier Zambrano',
     'license': 'LGPL-3',
     'data': [
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        
+        'views/estate_property_views.xml',
+        'views/real_estate_menus.xml',
     ]
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -7,17 +7,25 @@ class EstateProperty(models.Model):
     name = fields.Char('Property Name', required=True)
     description = fields.Text('Description')
     postcode = fields.Char('Postcode')
-    date_availability = fields.Date('Date Availability')
+    date_availability = fields.Date('Available From', copy=False, default=fields.Date.add(fields.Date.today(), months=3))
     expected_price = fields.Float('Expected Price', required=True)
-    selling_price = fields.Float('Selling Price')
-    bedrooms = fields.Integer('Bedrooms')
-    living_area = fields.Integer('Living Area')
+    selling_price = fields.Float('Selling Price', readonly=True, copy=False)
+    bedrooms = fields.Integer('Bedrooms', default=2)
+    living_area = fields.Integer('Living Area (sqm)')
     facades = fields.Integer('Facades')
     garage = fields.Boolean('Garage')
     garden = fields.Boolean('Garden')
-    garden_area = fields.Integer('Garden Area')
+    garden_area = fields.Integer('Garden Area (sqm)')
     garden_orientation = fields.Selection(
         string='Garden Orientation',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
         help='The orientation of the garden'
     )
+    state = fields.Selection(
+        string='Status',
+        selection=[('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')],
+        required=True,
+        copy=False,
+        default='new'
+    )
+    active = fields.Boolean('Active', default=True)
