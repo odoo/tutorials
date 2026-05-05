5277ebfcbc0d932a9e46a75d2c5cb931ad150736
Mohit Ahir (moahi) <moahi@odoo.com>
2026-05-05 15:52:54 +0530

    [ADD] awesome_owl: Chapter 1 Completed(card,counter,todos)
    Created Counter with increment
    Built TodoList for tasks
    Added new todo creation
    Enabled toggle for completion
    Applied conditional styling
    Added delete option for todos
    Updated Card with slots and toggle content

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,5 +1,20 @@
-import { Component } from "@odoo/owl";
+import { Component, markup, useState } from "@odoo/owl";
+import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
+import { TodoList } from "./todo/todo_list";
 
 export class Playground extends Component {
-    static template = "awesome_owl.playground";
+    static template =  "awesome_owl.Playground";
+    static components = { Counter, Card, TodoList}
+
+    setup() {
+        this.normalHtml = "<div class='text-primary'>some content</div>";
+
+        this.safeHtml = markup("<div class='text-primary'>some content</div>");
+
+        this.state = useState({ sum: 0 });
+    }
+    incrementSum() {
+        this.state.sum++;
+    }
 }
---
+++ awesome_owl/static/src/todo/todo_list.js
@@ -0,0 +1,46 @@
+import { Component, useState, useRef, onMounted } from "@odoo/owl";
+import { TodoItem } from "./todo_item";
+
+export class TodoList extends Component {
+    static template = "awesome_owl.TodoList";
+    static components = { TodoItem };
+
+    setup() {
+        this.todos = useState([]);
+        this.nextId = 1;
+        this.inputValue = useState({ text: "" });
+
+        this.inputRef = useRef("input");
+
+        onMounted(() => {
+            this.inputRef.el.focus(); 
+        });
+
+    }
+    addTodo(ev) {
+        if (ev.keyCode != 13) return
+
+        this.todos.push({
+            id : this.nextId++,
+            description : this.inputValue.text,
+            isCompleted : false
+        })
+        this.inputValue.text = ""
+
+        this.inputRef.el.focus(); 
+    }
+
+    toggleState(id){
+        const todo = this.todos.find(t => t.id === id)
+        if (todo) {
+            todo.isCompleted = !todo.isCompleted
+        }
+    }
+
+    removeTodo(id) {
+        const index = this.todos.findIndex(t => t.id === id);
+        if (index >= 0) {
+            this.todos.splice(index, 1);
+        }
+    }
+}
