34de6896e66877c40ff1c021868bbc3145b4e516
ripil-odoo <ripil@odoo.com>
2026-05-05 14:19:37 +0530

    [IMP] estate_account: Invoice lines for property sold
    Adding invoice lines to the draft invoice using the proeprty details, used Command to create them and also added the required price logic as stated in the exercise (6% of selling price)
    
    Chapter 13

--- estate_account/models/estate_properties.py
+++ estate_account/models/estate_properties.py
@@ -1,7 +1,7 @@
 import logging
 
 from odoo import models
-
+from odoo import Command
 
 _logger = logging.getLogger(__name__)
 
@@ -10,10 +10,22 @@ class EstateProperties(models.BaseModel):
     _inherit = 'estate.properties'
 
     def property_sold(self):
-        # breakpoint()
+        breakpoint()
         invoice_vals = {
             'move_type': 'out_invoice',
             'partner_id': self.buyer_id.id,  # type: ignore
+            'invoice_line_ids': [
+                Command.create({
+                    'name': f'Property: {self.display_name}',
+                    'quantity': 1,
+                    'price_unit': 0.06 * self.selling_price,  # type: ignore
+                }),
+                Command.create({
+                    'name': 'Administrative Fees',
+                    'quantity': 1,
+                    'price_unit': 100
+                })
+            ]
         }
         # _logger.error(invoice_vals)
         self.env['account.move'].create(invoice_vals)
