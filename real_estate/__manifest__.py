affda17c8f6ced5474c1835dae91d837dcefdd5c
prcha-odoo <prcha@odoo.com>
2026-05-07 18:54:09 +0530

    [ADD] real_estate: added real_estate module
    Added the basic structure for the new  module by creating the  and  files.

---
+++ real_estate/__manifest__.py
@@ -0,0 +1,11 @@
+
+{
+    'name': 'real_estate',
+    'version': '1.0',
+    'depends': ['base'],
+    'category': 'tutorials',
+    'author': "prcha-odoo",
+    'description': "A real estate module",
+    'installable': True,
+    'application': True
+}
