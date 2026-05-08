a68a6e4e6ec90c25765a6ebc7503650d66219a86
dhsha-odoo <dhsha@odoo.com>
2026-05-08 18:55:36 +0530

    [IMP] real_estate: update module manifest metadata

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,6 +1,12 @@
-# __manifest__.py
-
 {
-    'name': 'estate',
-    'version': '1.0',
+    "name": "estate",  
+    "version": "1.0",  
+    "summary": "Estate management",  
+    "description": "Estate management",  
+    "author": "Odoo S.A.",  
+    "website": "https://www.odoo.com/",  
+    "category": "Uncategorized",  # Module category in Odoo apps
+    "depends": ["base"],  # Required dependency modules
+    "installable": True,  # Allows module installation
+    "application": True,  # Shows as main application
 }
