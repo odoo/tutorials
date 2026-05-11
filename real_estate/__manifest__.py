d4d3ab950b342ccb32d096bcd9882e4bf2a0af13
prcha-odoo <prcha@odoo.com>
2026-05-11 18:48:53 +0530

    [IMP] real_estate: added model and updated menifest
    - Created real estate property model

--- real_estate/__manifest__.py
+++ real_estate/__manifest__.py
@@ -5,7 +5,8 @@
     'depends': ['base'],
     'category': 'tutorials',
     'author': "prcha-odoo",
+    'license': 'LGPL-3',
     'description': "A real estate module",
     'installable': True,
-    'application': True
+    'application': True,
 }
---
+++ real_estate/models/estate_property.py
@@ -0,0 +1,24 @@
+from odoo import fields, models
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "real estate property module"
+
+    name = fields.Char(string="Property Name")
+    description = fields.Text(string="Description")
+    postcode = fields.Char(string="Postal Code")
+    date_availability = fields.Date(string="Available Date")
+    expected_price = fields.Float(string='Expected Price', required=True)
+    selling_price = fields.Float(string='Selling Price', required=True)
+    bedrooms = fields.Integer(string="Bedroom Count")
+    living_area = fields.Integer(string="Living Area Count")
+    facades = fields.Integer(string="Facades Count")
+    has_garage = fields.Boolean(string="Has any Garage ?")
+    has_garden = fields.Boolean(string="Has any Garden ?")
+    garden_area = fields.Integer(string="Garden Area in (sq meter)")
+    garden_orientation = fields.Selection(
+        string="Garden Orientation",
+        selection=[
+            ('north','North'),('south','South'),('east','East'),('west','West')
+        ]
+    )
