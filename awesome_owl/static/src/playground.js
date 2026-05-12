5b70028d57564394ccc6ead6cd339e3adec53a5e
patsh-odoo <patsh@odoo.com>
2026-05-12 13:50:20 +0530

    [IMP] awesome_owl: added ability to display html content in cards
    Added ability to display html content in card's title and body.
    
    It is necessary for user to display html content in cards now user can use
    several html tags like div, span, headings etc without content being escaped.
    
    Owl chapter 1 (example Counter Component exercise 4)

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,8 +1,13 @@
-import { Component } from "@odoo/owl";
-import { Counter } from "./components/counter/counter";
+import { Component, markup } from "@odoo/owl";
 import { Card } from "./components/card/card";
+import { Counter } from "./components/counter/counter";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
     static components = { Counter, Card };
+
+    setup() {
+        this.card1Body = "<div>text without markup</div>";
+        this.card2Body = markup("<div>text with markup</div>");
+    }
 }
