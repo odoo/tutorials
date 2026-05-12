2bab8c0cd97bb9c16e0731b305fddfd0285c9f3b
dhsha-odoo <dhsha@odoo.com>
2026-05-12 18:44:19 +0530

    [ADD] estate: add ORM model EstateProperty fields
    Added fields for property details, pricing, area, garden settings, orientation selection, and active state in EstateProperty model.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -8,6 +8,29 @@ class EstateProperty(models.Model):
     name = fields.Char(string="Title", required=True)
     description = fields.Text(string="Description")
     postcode = fields.Char(string="Postcode")
-    expected_price = fields.Float()
-    bedrooms = fields.Integer()
+
+    date_availability = fields.Date(string="Available From")
+
+    expected_price = fields.Float(string="Expected Price")
+    selling_price = fields.Float(string="Selling Price",required=True)
+
+    bedrooms = fields.Integer(string="Bedrooms")
+    living_area = fields.Integer(string="Living Area")
+    facades = fields.Integer(string="Facades")
+
+    garage = fields.Boolean(string="Garage")
+    garden = fields.Boolean(string="Garden")
+
+    garden_area = fields.Integer(string="Garden Area")
+
+    garden_orientation = fields.Selection(
+        [
+            ('north', 'North'),
+            ('south', 'South'),
+            ('east', 'East'),
+            ('west', 'West'),
+        ],
+        string="Garden Orientation"
+    )
+
     active = fields.Boolean(default=True)
