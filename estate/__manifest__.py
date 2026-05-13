dd04a15adb2a5d8675e56ec3b00e281d8c18a6e6
Jay Patel <patja@odoo.com>
2026-05-13 17:52:12 +0530

    [ADD] estate: add author and license information to module manifest

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,6 +3,8 @@
     'version': '0.1',
     'category': 'Tutorials',
     'summary': 'Real Estate Advertisement tutorial module (empty shell)',
+    'author': 'Patja',
+    'license': 'LGPL-3',
     'depends': ['base'],
     'data': [],
     'application': True,
