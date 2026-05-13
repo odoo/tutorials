aaf3b9dcc45105a636c87a1015f8e0265a716bd6
patsh-odoo <patsh@odoo.com>
2026-05-13 22:22:40 +0530

    [IMP] awesome_owl: added todo list
    Added todo list to playground it allows to list all the tasks with description
    and status.
    
    User can now see all tasks in todo list with their description and status.
    
    Owl chapter 1 (Theory: Component lifecycle and hooks exercise 7)

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,10 +1,11 @@
 import { Component, markup, useState } from "@odoo/owl";
 import { Card } from "./components/card/card";
 import { Counter } from "./components/counter/counter";
+import { TodoList } from "./components/todo_list/todo_list";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
-    static components = { Counter, Card };
+    static components = { Counter, Card, TodoList };
 
     setup() {
         this.card1Body = "<div>text without markup</div>";
