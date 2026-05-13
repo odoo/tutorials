14a55f0321b2eb11bb6020bedc2cc0a4ea7d2d64
patsh-odoo <patsh@odoo.com>
2026-05-14 00:27:20 +0530

    [IMP] awesome_owl: added visual difference for completed tasks of todo list
    Added visual difference for tasks of todo list that are completed.completed
    tasks are greyed and a line through them is visible.
    
    User can now clearly distinguish between completed and pending tasks of todo
    list.
    
    Owl chapter 1 (Theory: Component lifecycle and hooks exercise 8)

--- awesome_owl/static/src/components/todo_list/todo_list.js
+++ awesome_owl/static/src/components/todo_list/todo_list.js
@@ -5,6 +5,9 @@ export class TodoList extends Component {
     static template = "awesome_owl.TodoList";
     static components = { TodoItem };
     setup() {
-        this.todos = useState([{ id: 3, description: "buy milk", isCompleted: false }]);
+        this.todos = useState([
+            { id: 3, description: "buy milk", isCompleted: false },
+            { id: 4, description: "buy bread", isCompleted: true },
+        ]);
     }
 }
