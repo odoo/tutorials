dbc350e1c7ae3491156e06c6cfa869c258546c64
patsh-odoo <patsh@odoo.com>
2026-05-15 11:59:51 +0530

    [IMP] awesome_owl: added checkbox to mark task done/pending
    Added a checkbox associated with task.
    
    User can now easily mark task done or pending just by clicking checkbox.
    
    Owl chapter 1 (Theory: Component lifecycle and hooks exercise 11)

--- awesome_owl/static/src/components/todo_list/todo_item.js
+++ awesome_owl/static/src/components/todo_list/todo_item.js
@@ -4,5 +4,10 @@ export class TodoItem extends Component {
     static template = "awesome_owl.TodoItem";
     static props = {
         todo: { type: Object, shape: { id: Number, description: String, isCompleted: Boolean } },
+        toggleState: Function,
     };
+
+    onChange() {
+        this.props.toggleState(this.props.todo.id);
+    }
 }
