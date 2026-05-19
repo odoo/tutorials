04a43d5b670d525c611c903b9245db35417f7e2a
patsh-odoo <patsh@odoo.com>
2026-05-19 23:01:21 +0530

    [IMP] awesome_dashboard: made dashboard generic
    Implemented dashboard in generic manner so user can simply
    list content to be displayed in dashboard, user can
    decide either to display content as pie chart or simple numeric cards
    with some text and a number.
    
    Previously dashboard content was hardcoded it was complex to add new
    items to dashboard now in order to add new items to dashboard simply add
    them to item list.
    
    Owl chapter 2 (exercise 9)

--- awesome_dashboard/static/src/dashboard/dashboard.js
+++ awesome_dashboard/static/src/dashboard/dashboard.js
@@ -3,11 +3,11 @@ import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
 import { Layout } from "@web/search/layout";
 import { DashboardItem } from "./dashboard_item/dashboard_item";
-import { PieChart } from "./pie_chart/pie_chart";
+import { items } from "./dashboard_items";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
-    static components = { Layout, DashboardItem, PieChart };
+    static components = { Layout, DashboardItem };
 
     setup() {
         this.action = useService("action");
@@ -15,6 +15,7 @@ class AwesomeDashboard extends Component {
             controlPanel: {},
         };
         this.data = useState(useService("awesome_dashboard.statistics"));
+        this.items = items;
     }
 
     openCustomers() {
