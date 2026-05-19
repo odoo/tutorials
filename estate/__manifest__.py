fd1046aae0c58532c3a56ea0bea243df67191168
amsli <amsli@odoo.com>
2026-05-19 16:45:00 +0200

    chapter1-5

---
+++ estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Real Estate',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+    ],
+    'application': True,
+}
