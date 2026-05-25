0738693e30238f99f484510e13dd2903d053c2c9
odoo <radhr@odoo.com>
2026-05-25 15:56:53 +0530

    [IMP] sale_zero_stock: restrict zero stock approval access to administrators
    Implemented access control so only Sales Administrators can modify the Zero
    Stock Approval field in Sale Orders. If the approval is not enabled, Sales
    Users cannot confirm orders with unavailable stock. Helps enforce proper
    authorization prevents unauthorized zero-stock sales, and improves inventory
    management accuracy.

---
+++ sale_zero_stock/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "sale_zero_stock",
+    "depends": ["sale"],
+    "category": "sale",
+    "installable": True,
+    "application": True,
+    "data": ["views/sale_zero_stock.xml"],
+    "author": "RADHR",
+    "license": "LGPL-3",
+}
