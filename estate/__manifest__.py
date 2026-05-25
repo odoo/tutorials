78e39922dd5f44f7a11c0680653f6523ca388db0
sadeo-odoo <sadeo@odoo.com>
2026-05-25 11:49:47 +0530

    [FIX] Fixed all the whitespaces warnings

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,16 +1,16 @@
 {
-    'name': 'estate',
-    'version': '1.0',
-    'depends': ['base'],
-    'category': 'tutorials',
-    'author': "sadeo-odoo",
-    'license': 'LGPL-3',
-    'description': 'A real estate module',
-    'installable': True,
-    'application': True,
-    'data':[
-        'security/ir.model.access.csv',
-        'views/estate_property_views.xml',
-        'views/estate_menus.xml',
-    ]
+    "name": "estate",
+    "version": "1.0",
+    "depends": ["base"],
+    "category": "tutorials",
+    "author": "sadeo-odoo",
+    "license": "LGPL-3",
+    "description": "A real estate module",
+    "installable": True,
+    "application": True,
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
+    ],
 }
