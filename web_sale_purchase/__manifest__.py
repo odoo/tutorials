3ce3d8aa285007af765dabc86e142f8217333e2d
odoo <radhr@odoo.com>
2026-05-26 12:38:50 +0530

    [ADD] web_sale_purchase: extend eCommerce to support both sales and purchases
    Developing a new ecommerce module that extends website_sale to manage both
    selling and purchasing operations. It will allow users to perform purchase
    related activities directly from the website, reducing manual processes.
    Improves workflow efficiency, provides greater flexibility, and creates a more
    complete eCommerce experience for users.

---
+++ web_sale_purchase/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "ecommerce purchase",
+    "depends": [
+        "website_sale",
+        "sale",
+        "website",
+    ],
+    "installable": True,
+    "author": "RADHR",
+    "license": "LGPL-3",
+}
