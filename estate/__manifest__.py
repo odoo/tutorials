1a767ba56aa6271873842070e252826e087a1eb8
Arbaaz-Khan-Tech <asurk@odoo.com>
2026-05-27 10:56:04 +0530

    [IMP] estate: Add estate manager and agent roles
    - Introduce 'Estate Manager' and 'Estate Agent' user roles.
    - Include necessary security access rights defined in XML and CSV files for these new roles.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -8,6 +8,7 @@
     A module so that customers can bid on real estates
     """,
     'data': [
+        'security/estate_security.xml',
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_property_type_views.xml',
