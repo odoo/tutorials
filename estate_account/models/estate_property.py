caa03a32c1a5996219c93bae7845715233c4000c
Arbaaz-Khan-Tech <asurk@odoo.com>
2026-05-27 11:02:23 +0530

    [ADD] estate_account: Add logic to create invoice on property sale
    - Added new module estate_account
    - Implement functionality to automatically generate an invoice upon the sale of an estate property.

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,4 +1,5 @@
-from odoo import models, api
+from odoo import models, Command
+from odoo.exceptions import AccessError
 
 
 class EstateProperty(models.Model):
@@ -6,32 +7,33 @@ class EstateProperty(models.Model):
     _inherit = 'estate.property'
 
     def action_property_sold(self):
-        pass
 
-        # if not self.env['account.move'].check_access_rights('create'):
-        #     try:
-        #         self.check_access_rights('write')
-        #         self.check_access_rule('write')
-        #     except AccessError:
-        #         return self.env['account.move']
+        if not self.env['account.move'].check_access_rights('create'):
+            try:
+                self.check_access_rights('write')
+                self.check_access_rule('write')
+            except AccessError:
+                return self.env['account.move']
 
-        # values = []
-        # for rec in self:
+        values = []
+        for rec in self:
 
-        #     values.append({
-        #         'name': 'a invoice has been generated',
-        #         'move_type': 'out_invoice',
-        #         'partner_id': rec.buyer_id.id,
-        #         "invoice_line_ids": [
-        #             Command.create({
-        #                 "product_id": rec.id,
-        #                 "quantity": 1,
-        #                 "price_unit": rec.selling_price,
-        #             }),]
-        #     })
+            income_account = self.env['account.account'].search([
+                ('account_type', '=', 'income'),
+            ], limit=1)
+            values.append({
+                'name': rec.name + "Invoice",
+                'move_type': 'out_invoice',
+                'partner_id': rec.buyer_id.id,
+                'invoice_line_ids': [
+                    Command.create({
+                        'name': f'{rec.name}',
+                        'quantity': 1,
+                        'price_unit': rec.selling_price * 0.06 + rec.selling_price,
+                        'account_id': income_account.id
+                    })
+                ]
+            })
 
-        #     if not values:
-        #         raise UserError("Nothing to invoice.")
-
-        # moves = self.env['account.move'].sudo().create(values)
-        # return super().action_property_sold
+        self.env['account.move'].sudo().create(values)
+        return super().action_property_sold()
