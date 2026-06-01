965bb43a9579eb432c745cc5d5f32904eb4303a5
ripil-odoo <ripil@odoo.com>
2026-06-01 17:51:23 +0530

    [ADD] stock_picking: Repeat fields on Picking Operations Report PDF
    Ensure original header fields are consistently visible across multiple pages of the Picking Operations PDF report when the table extends to a new page by using <thead> tags.

---
+++ stock_picking/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Custom Picking Operation Report Header',
+    'version': '1.0',
+    'category': 'Tutorial',
+    'summary': 'Picking information on multi-page PDF report header',
+    'depends': ['stock'],
+    'data': [
+        'views/report_stockpicking_operations.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'author': 'Rini Pillai',
+    'license': 'LGPL-3',
+}
