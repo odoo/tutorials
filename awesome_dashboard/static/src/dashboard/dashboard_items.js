e3a6a884e80476c7029715c62b4735c1c7345009
vivah-odoo <vivah@odoo.com>
2026-06-02 18:56:07 +0530

    [IMP] awesome_dashboard: make dashboard generic and extensible
    - Refactored dashboard to render items dynamically using t-foreach
    - Created reusable NumberCard and PieChartCard components
    - Added dashboard_items configuration for item definitions
    - Implemented dynamic component and props handling
    - Introduced awesome_dashboard registry for extensible dashboard items
    - Loaded dashboard items from registry instead of hardcoded list
    - Added dashboard settings button with configuration dialog
    - Implemented item visibility management using checkboxes
    - Stored removed dashboard items in local storage
    - Filtered dashboard items based on user configuration
    - Improved dashboard customization and extensibility
    
    chapter 2

--- awesome_dashboard/static/src/dashboard/dashboard_items.js
+++ awesome_dashboard/static/src/dashboard/dashboard_items.js
@@ -1,32 +1,33 @@
 import {registry} from "@web/core/registry";
 import {NumberCard} from "@awesome_dashboard/dashboard/number_card/number_card";
 import {PieChartCard} from "@awesome_dashboard/dashboard/pie_chart_card/pie_chart_card";
+import {_t} from "@web/core/l10n/translation";
 
 registry.category("awesome_dashboard").add("number_new_orders", {
     id: "number_new_orders",
-    description: "New orders this month",
+    description: _t("New orders this month"),
     Component: NumberCard,
     size: 1,
     props: (data) => ({
-        title: "New orders this month",
+        title: _t("New orders this month"),
         value: data.nb_new_orders,
     }),
 });
 
 registry.category("awesome_dashboard").add("number_cancelled_orders", {
     id: "number_cancelled_orders",
-    description: "Cancelled orders this month",
+    description: _t("Cancelled orders this month"),
     Component: NumberCard,
     size: 1,
     props: (data) => ({
-        title: "Cancelled orders this month",
+        title: _t("Cancelled orders this month"),
         value: data.nb_cancelled_orders,
     }),
 });
 
 registry.category("awesome_dashboard").add("number_total_amount", {
     id: "number_total_amount",
-    description: "Total amount of orders",
+    description: _t("Total amount of orders"),
     Component: NumberCard,
     size: 1,
     props: (data) => ({
@@ -37,33 +38,33 @@ registry.category("awesome_dashboard").add("number_total_amount", {
 
 registry.category("awesome_dashboard").add("average_quantity", {
     id: "average_quantity",
-    description: "Average quantity per order",
+    description: _t("Average quantity per order"),
     Component: NumberCard,
     size: 1,
     props: (data) => ({
-        title: "Average quantity of t-shirts by order",
+        title: _t("Average quantity of t-shirts by order"),
         value: data.average_quantity,
     }),
 });
 
 registry.category("awesome_dashboard").add("average_time", {
     id: "average_time",
-    description: "Average time to process",
+    description: _t("Average time to process"),
     Component: NumberCard,
     size: 1,
     props: (data) => ({
-        title: "Average time for an order to go from 'new' to 'sent' or 'cancelled' (in hours)",
+        title: _t("Average time for an order to go from 'new' to 'sent' or 'cancelled' (in hours)"),
         value: data.average_time,
     }),
 });
 
 registry.category("awesome_dashboard").add("pie_chart_orders", {
     id: "pie_chart_orders",
-    description: "Shirt orders by size",
+    description: _t("Shirt orders by size"),
     Component: PieChartCard,
     size: 2,
     props: (data) => ({
-        title: "Shirt orders by size",
+        title: _t("Shirt orders by size"),
         values: data.orders_by_size,
     }),
 });
--- awesome_dashboard/static/src/dashboard/pie_chart/pie_chart.js
+++ awesome_dashboard/static/src/dashboard/pie_chart/pie_chart.js
@@ -1,5 +1,4 @@
 import {loadJS} from "@web/core/assets";
-import {getColor} from "@web/core/colors/colors";
 import {Component, onMounted, onWillStart, onWillUnmount, useRef} from "@odoo/owl";
 
 export class PieChart extends Component {
@@ -11,7 +10,6 @@ export class PieChart extends Component {
 
     setup() {
         this.canvasRef = useRef("canvas");
-        // Load Chart.js before mounting the component
         onWillStart(() => loadJS("/web/static/lib/Chart/Chart.js"));
         onMounted(() => this.renderChart());
         onWillUnmount(() => {
@@ -22,7 +20,6 @@ export class PieChart extends Component {
     renderChart() {
         const labels = Object.keys(this.props.data);
         const values = Object.values(this.props.data);
-        const colors = labels.map((_, i) => getColor(i));
         this.chart = new Chart(this.canvasRef.el, {
             type: "pie",
             data: {
@@ -30,7 +27,6 @@ export class PieChart extends Component {
                 datasets: [{
                     label: this.props.label,
                     data: values,
-                    backgroundColor: colors,
                 }],
             },
         });
