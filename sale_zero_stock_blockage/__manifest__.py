e810cd50f12ba6a418d8db87cee9e4d8c3891bc6
sngohodoo <sngoh@odoo.com>
2026-06-04 14:21:55 +0530

    [ADD] sale_zero_stock_blockage: don't allow sales without approval
    There are the case where sales person can't confirm the sale order
    without proper permission or the approval of the administrator.
    This helps them to archive that functionality, now an administrator can
    control which sell order should not be confirm by a sales or other use.
    
    Project: [PSIN]INTERNSHIP ONBOARDING
    Task: DEV - ZERO STOCK BLOCKAGE

---
+++ sale_zero_stock_blockage/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Sale Zero Stock Blockage",
+    "application": False,
+    "installable": True,
+    "author": "sngoh",
+    "depends": ["sale"],
+    "auto_install": True,
+    "license": "LGPL-3",
+    "data": [
+        "views/sale_order_views.xml",
+    ],
+}
