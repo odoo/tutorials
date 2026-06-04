36fb121bec4a0cef578162a92579645fd4a1edbb
dhsha-odoo <dhsha@odoo.com>
2026-06-04 17:19:49 +0530

    [ADD] estate: property offer feature
    Create estate.property.offer model, add offer views, link offers to properties via offer_ids, and display offers in the property form view.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -53,6 +53,16 @@ class EstateProperty(models.Model):
         default="new",
         copy=False,
     )
+    salesperson_id = fields.Many2one(
+    'res.users',
+    string='Salesperson',
+    default=lambda self: self.env.user,
+    )
+    buyer_id = fields.Many2one(
+        "res.partner",
+        string="Buyer",
+        copy=False,
+    )
     property_type_id = fields.Many2one(
     "estate.property.type",
     string="Property Type"
@@ -60,6 +70,11 @@ class EstateProperty(models.Model):
     tag_ids = fields.Many2many(
     "estate.property.tag",
     string="Tags"
+    )
+    offer_ids = fields.One2many(
+    "estate.property.offer",
+    "property_id",
+    string="Offers",
     )
    
     
---
+++ estate/models/estate_property_offer.py
@@ -0,0 +1,23 @@
+from odoo import fields, models
+
+
+class EstatePropertyOffer(models.Model):
+    _name = "estate.property.offer"
+    _description = "Property Offer"
+
+    price = fields.Float()
+    status = fields.Selection(
+        [('accepted', 'Accepted'), ('refused', 'Refused')]
+    )
+
+    partner_id = fields.Many2one(
+        "res.partner",
+        string="Partner",
+        required=True,
+    )
+
+    property_id = fields.Many2one(
+        "estate.property",
+        string="Property",
+        required=True,
+    )
