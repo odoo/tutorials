04adcabca36a7b33db5cfc3927f7eff3070827bf
saver-odoo <saver@odoo.com>
2026-06-08 15:25:45 +0530

    [ADD] estate:  Add Access Rights to Models
    This commit adds the required access rights to ensure authorized users can
    interact with the models according to their role configuration.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,6 +1,10 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-
-  "name":"Real Estate",
-  "depends":['base'],
-   'application': True,
+    "name": "Real Estate",
+    "depends": ["base"],
+    "application": True,
+    "data": ["security/ir.model.access.csv"],
+    "author": "Odoo S.A.",
+    "license": "LGPL-3",
 }
