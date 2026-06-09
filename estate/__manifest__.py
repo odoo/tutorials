07925febcb8abe5c6303d597d255e3bb0414e07f
Rakshita Singh <rasin@odoo.com>
2026-06-09 12:07:00 +0530

    [IMP] estate: add property access rights

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,10 +1,9 @@
 {
-    'name': 'Real Estate',            #Module name shown in Apps.
-    'depends': [                      #specifies that this module depends on the base module, which is a core module of Odoo
-        'base',
-    ],
-    'application': True,              #Shows module under Apps filter.
-    'data': [                         #list of data files to be loaded when the module is installed or updated. These files typically contain XML or CSV data that defines the structure and behavior of the module.
+    'name': 'Real Estate',
+    'depends': ['base'],
+    'application': True,
+    'data': [
         'security/ir.model.access.csv',
     ],
 }
+
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,9 +1,9 @@
 from odoo import fields, models
 
 
-class EstateProperty(models.Model):                  #created odoo model
-    _name = "estate.property"                        #creates database table estate_property (odoo converts dot to underscore)
-    _description = "Real Estate Property"            #add description for the model
+class EstateProperty(models.Model):                  
+    _name = "estate.property"                        
+    _description = "Real Estate Property"            
 
     name = fields.Char(required=True)
     description = fields.Text()
@@ -17,11 +17,10 @@ class EstateProperty(models.Model):                  #created odoo model
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
-    garden_orientation = fields.Selection(
-        [
-            ('north', 'North'),
-            ('south', 'South'),
-            ('east', 'East'),
-            ('west', 'West'),
-        ]
-    )
+    garden_orientation = fields.Selection([
+        ('north', 'North'),
+        ('south', 'South'),
+        ('east', 'East'),
+        ('west', 'West'),
+    ])
+    
