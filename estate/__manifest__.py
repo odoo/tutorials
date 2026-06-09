aa51cd586b6de7567b2225fd4a08b58e0cf7cddc
dhruvikalariyaa <dhruvikalariya2002@gmail.com>
2026-06-09 16:10:05 +0530

    [IMP] estate: add access rights for estate.property model
    - Create `ir.model.access.csv` to define access control rules.
    - Grant full CRUD (read, write, create, delete) permissions to the base user
    group (`base.group_user`).
    - Register the security CSV file inside the manifest `__manifest__.py`.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,5 +1,11 @@
 {
     "name": "Real Estate",
     "depends": ["base"],
+    "data" : [
+        'security/ir.model.access.csv',
+    ],
     "application": True,
+    "author" : "dhruvi kalariya",
+    "license" : "LGPL-3",
+    "installable" : True,
 }
