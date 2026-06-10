ec1a0633270f2fc363d9c316fe1cb8c4950cd701
Aditi Pawar(adpaw) <adpaw@odoo.com>
2026-06-10 11:35:55 +0530

    [IMP] awesome_owl:  Owl tasks Improved the todo task management workflow. Added validation to prevent creating multiple todos with the same name. Added a 'Clear' button to remove all todo items from the list. Added a 'Mark as Complete' button to mark all todos as completed. Displayed total, completed, pending, and deleted todo counts using get method and t-out  in the UI.

--- awesome_owl/static/src/todo_list/todo_list.js
+++ awesome_owl/static/src/todo_list/todo_list.js
@@ -9,6 +9,7 @@ export class TodoList extends Component {
         this.todos = useState([]);
         this.nextId = 1;
         this.inputRef = useRef("todoInput");
+        this.deletedCount = useState({ count: 0 });
 
         onMounted(() => {
             this.inputRef.el.focus();
@@ -18,8 +19,9 @@ export class TodoList extends Component {
     addTodo(ev) {
         if (ev.key === "Enter") {
             const description = ev.target.value.trim();
-            debugger;
             if (!description) return;
+            const duplicate = this.todos.find(t => t.description === description);
+            if (duplicate) return;
             this.todos.push({
                 id: this.nextId++,
                 description: description,
@@ -40,10 +42,33 @@ export class TodoList extends Component {
         const index = this.todos.findIndex(t => t.id === todoId);
         if (index >= 0) {
             this.todos.splice(index, 1);
+            this.deletedCount.count++;
             this.todos.forEach((todo, idx) => {
             todo.id = idx + 1;
             });
             this.nextId = this.todos.length + 1;
         }
     }
+
+    clear() {
+        // if (this.todos.length){
+            this.todos.splice(0);
+        // }
+    }
+
+    markascomplete(){
+        this.todos.forEach(t => t.isCompleted = true);
+    }
+
+    get completedTodos() {
+        return this.todos.filter(t => t.isCompleted).length;
+    }
+
+    get pendingTodos() {
+        return this.todos.filter(t => !t.isCompleted).length;
+    }
+
+    get deletedTodos() {
+        return this.deletedCount;
+}
 }
