6a79ca0f9e4ca6afc6a96ead82ac18a0b3b59ae7
saver-odoo <saver@odoo.com>
2026-06-11 14:25:31 +0530

    [ADD] estate:  Add window action and hierarchical menus
    Define 'test_model_action' window action with list and form modes
    Implement structured root, category, and action menus for Real Estate

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,10 +1,12 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     "name": "Real Estate",
     "depends": ["base"],
     "application": True,
-    "data": ["security/ir.model.access.csv"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menuitem.xml",
+    ],
     "author": "Odoo S.A.",
     "license": "LGPL-3",
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,16 +1,27 @@
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta
 
 
 class EstateProperty(models.Model):
     _name = "estate.property"
-    _description = "EState property"
+    _description = "Estate Property"
+
+    def _default_date_availability(self):
+        return fields.Date.today() + relativedelta(months=3)
+
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+
+    date_availability = fields.Date(
+        copy=False,
+        default=_default_date_availability,
+        # default=lambda self: fields.Date.today() + relativedelta(months=3)
+    )
+
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False, default=3000)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -19,3 +30,16 @@ class EstateProperty(models.Model):
     garden_orientation = fields.Selection(
         [("North", "North"), ("South", "South"), ("East", "East"), ("West", "West")]
     )
+    state = fields.Selection(
+        [
+            ("New", "New"),
+            ("Offer Recieved", "Offer Received"),
+            ("Offer Accepted", "Offer Accepted"),
+            ("Sold", "Sold"),
+            ("Cancelled", "Cancelled"),
+        ],
+        required=True,
+        copy=False,
+        default="New",
+    )
+    active = fields.Boolean(default=False)
