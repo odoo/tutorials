10c70a56258d77b1d35a386e48534ccd641cc215
vrushibh <rishibh.bhalodiya@gmail.com>
2026-06-11 18:52:21 +0530

    [ADD] estate: add security access rules for estate.property
    - Create ir.model.access.csv in security folder
    - Grant read, write, create, and unlink permissions to base.group_user
    - Register security file in __manifest__.py
    - Fix manifest warnings by adding author and license keys

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,6 +1,11 @@
 {
     "name": "Real Estate",
+    "author": "Odoo S.A.",
+    "license": "LGPL-3",
     "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+    ],
     "application": True,
     "installable": True,
 }
