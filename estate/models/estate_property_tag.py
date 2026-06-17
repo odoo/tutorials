465e4422503ab717b8f104a1b621b4d0a8659f60
abkus-odoo <abkus@odoo.com>
2026-06-17 18:19:55 +0530

    [ADD] estate: add property type One2many inline view and statusbar widget
    - Added property_ids One2many field in estate.property.type
    - Implemented inline list view for properties in property type form
    - Added statusbar widget in estate.property state field
    - Updated form view to display related properties with limited fields

--- estate/models/estate_property_tag.py
+++ estate/models/estate_property_tag.py
@@ -4,10 +4,9 @@ from odoo import fields, models  # pylint: disable=import-error
 class EstatePropertyTag(models.Model):
     _name = "estate.property.tag"
     _description = "Property Tags"
+    _order = "name"
 
     name = fields.Char(required=True)
     _unique_price = models.Constraint(
-        'UNIQUE(name)',
-        'Property tag must be unique'
-    )
+        "UNIQUE(name)", "Property tag must be unique")
 
