cf58749683327222e847d09c2a6464761d53c918
abkus-odoo <abkus@odoo.com>
2026-06-18 18:42:00 +0530

    [ADD] estate: add ordering, sequence, and tag widget options
    - Added model ordering for properties, offers, tags, and property types
    Added sequence field with manual ordering support for property types
    Configured property type field to disable create/edit from property form
    Added color field to tags and enabled color picker widget

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -1,12 +1,13 @@
-from odoo import fields, models  # pylint: disable=import-error
+from odoo import fields, models
 
 
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
     _description = "Property Types"
-    _order = "name"
+    _order = "sequence, name"
 
     name = fields.Char(required=True)
+    sequence = fields.Integer(string="Sequence", default=1)
     property_ids = fields.One2many(
         "estate.property",
         "property_type_id",
@@ -14,4 +15,3 @@ class EstatePropertyType(models.Model):
     _unique_name = models.Constraint(
         "UNIQUE(name)", "Property type name must be unique."
     )
-
