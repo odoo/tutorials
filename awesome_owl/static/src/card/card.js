c4361af3a959443c8f37c81420379e2eeb65077b
Lucas Oude Vrielink <luoud@odoo.com>
2026-06-19 09:46:09 +0200

    [ADD] awesome_owl: chapter 1.3

---
+++ awesome_owl/static/src/card/card.js
@@ -0,0 +1,12 @@
+import { Component, xml } from "@odoo/owl";
+
+export class Card extends Component {
+    static template = xml`
+        <div class="card d-inline-block m-2" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title"><t t-esc="props.title"/></h5>
+                <p class="card-text"><t t-esc="props.content"/></p>
+            </div>
+        </div>
+    `;
+}
