1ab1dcd554047aaf9095fe65a9bb51fdf865923a
eslamicoo912 <eslamicoo3@gmail.com>
2026-06-21 23:22:17 +0300

    buttons with business logic and working with constraints

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import api, models, fields
+from odoo import api, models, fields, exceptions
 
 
 class EstatePropertyModel(models.Model):                              # Inheritence -> This class inherits from models.Model
@@ -10,8 +10,16 @@ class EstatePropertyModel(models.Model):                              # Inherite
     description = fields.Char()
 
     living_area = fields.Integer()
+    garden = fields.Boolean()
     garden_area = fields.Integer()
+    garden_orientation = fields.Selection([("north", 'North'), ('south', 'South'), ('east', "East"), ("west", 'West')])
     total_area = fields.Integer(compute="_compute_total_area")
+
+    best_price = fields.Float(compute="_compute_best_offer_price", string="Best Accepted Offer")
+
+    sold = fields.Boolean()
+    cancelled = fields.Boolean()
+    property_status = fields.Char(default="New", string="Property Status")
 
     @api.depends("living_area", "garden_area")
     def _compute_total_area(self):
@@ -35,3 +43,41 @@ class EstatePropertyModel(models.Model):                              # Inherite
         inverse_name="property_id", 
         string="Property Offers"
     )
+
+    @api.depends("property_offer_ids")
+    def _compute_best_offer_price(self):
+        for record in self:
+            accepted_offers= record.property_offer_ids.filtered(lambda rec: rec.status == "accepted")
+            prices = accepted_offers.mapped("price")
+            record.best_price = max(prices) if prices else 0.0
+
+    @api.onchange("garden")
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = "north"
+        else:
+            self.garden_area = 0
+            self.garden_orientation = ""
+
+    def action_property_sold(self):
+        if(self.cancelled==False):
+            self.sold = True
+            self.property_status = "Sold"
+            return True
+        else:
+            raise exceptions.UserError("Cancelled properties can't be sold")
+
+    def action_property_cancelled(self):
+        if (self.sold==False):
+            self.cancelled = True
+            self.property_status = "Cancelled"
+            return True
+        else:
+            raise exceptions.UserError("Sold properties can't be cancelled")
+        
+    @api.constrains("expected_price")
+    def _check_expected_price_positive(self):
+        for record in self:
+            if(record.expected_price<=0):
+                raise exceptions.ValidationError("The price must be a positive number")
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -7,3 +7,11 @@ class EstatePropertyOffer(models.Model):
     status = fields.Selection([('accepted', 'Accepted'), ('refused', 'Refused')], string="Offer Status")
     partner_id = fields.Many2one(comodel_name="res.partner", string="Partner")
     property_id = fields.Many2one(comodel_name="estate_property_model", string="Estate Property")
+
+    def action_accept_offer(self):
+        self.status = "accepted"
+        return True
+
+    def action_refuse_offer(self):
+        self.status = "refused"
+        return True
