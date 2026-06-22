4a471f6b6a926a58e95a01daba9c35854727001e
Ritvik Srivastava <risri@odoo.com>
2026-06-22 17:34:54 +0530

    [ADD] estate: added sample data of properties, property tags, property types .

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,6 +5,7 @@
     'depends': ['base'],
     'application': True,
     'data': [
+        'demo/demo-estatedata.xml',
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_property_type_views.xml',
