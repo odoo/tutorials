54c1253413e2f49c5bd205298e6c05cca95c6088
Rakshita Singh <rasin@odoo.com>
2026-06-22 17:49:44 +0530

    [IMP] estate: implement model relationships
    Implemented Chapter 7: Relations Between Models
    
    - Added estate.property.type model with menus, action, views, and access rights
    - Linked properties to property types using Many2one (property_type_id)
    - Added buyer (res.partner) and salesperson (res.users) Many2one fields
    - Set current user as default salesperson and prevented buyer from being copied
    - Added estate.property.tag model with menus, action, views, and access rights
    - Linked properties and tags using Many2many (tag_ids) with tag widget support
    - Added estate.property.offer model with price, status, partner, and property fields
    - Linked properties and offers using One2many (offer_ids) and Many2one (property_id)
    - Created offer views and integrated offers into the property form
    - Updated manifests, model imports, security rules, and views to support new relationships

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,6 +5,12 @@
     'data': [
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
+        'views/estate_property_type_views.xml',
+        'views/estate_property_tag_views.xml',
+        'views/estate_property_offer_views.xml',
         'views/estate_menus.xml',
+        'data/data_type.xml',
+        'data/data_tags.xml',
+        'data/data.xml',   
     ],
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -13,6 +13,29 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
+    property_type_id = fields.Many2one(
+        "estate.property.type",
+        string="Property Type",
+    )
+    tag_ids = fields.Many2many(
+        "estate.property.tag",
+        string="Tags",
+    )
+    buyer_id = fields.Many2one(
+        "res.partner",
+        string="Buyer",
+        copy=False,
+    )
+    salesperson_id = fields.Many2one(
+        "res.users",
+        string="Salesperson",
+        default=lambda self: self.env.user,
+    )
+    offer_ids = fields.One2many(
+        "estate.property.offer",
+        "property_id",
+        string="Offers",
+    )
     date_availability = fields.Date(
         copy=False,
         default=_default_availability_date,
