98235af8cc531f7c373f8469216d91f2d0bd9b9a
saver-odoo <saver@odoo.com>
2026-06-29 11:09:22 +0530

    [IMP] estate: add business logic and computed fields to estate property
    Added required compute methods
    Set default salesperson and handle copying constraints
    Standardize Odoo relation field naming to `offer_ids`
    Sorted imports according to odoo standards

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,5 +1,6 @@
 from dateutil.relativedelta import relativedelta
-from odoo import fields, models
+
+from odoo import api, fields, models
 
 
 class EstateProperty(models.Model):
@@ -13,37 +14,74 @@ class EstateProperty(models.Model):
     description = fields.Text()
     postcode = fields.Char()
     property_type = fields.Many2one("estate.property.type")
-    sales_person = fields.Many2one("res.users")
-    buyer = fields.Many2one("res.partner")
+    sales_person = fields.Many2one("res.users", default=lambda self: self.env.user)
+    buyer = fields.Many2one("res.partner", copy=False)
     tag_ids = fields.Many2many("estate.property.tag")
-    offers = fields.One2many("estate.property.offer", "property_id", string="offers")
+    offer_ids = fields.One2many("estate.property.offer", "property_id", string="offers")
     date_availability = fields.Date(
         copy=False,
         default=_default_date_availability,
         # default=lambda self: fields.Date.today() + relativedelta(months=3)
     )
-
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float(readonly=True, copy=False, default=3000)
+    selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Integer()
+    garden_area = fields.Integer(
+        compute="_compute_garden_details",
+        store=True,
+        readonly=False,
+    )
     garden_orientation = fields.Selection(
-        [("north", "North"), ("south", "South"), ("east", "East"), ("west", "West")]
+        [('north', "North"), ('south', "South"), ('east', "East"), ('west', "West")],
+        compute="_compute_garden_details",
+        store=True,
+        readonly=False,
     )
     state = fields.Selection(
         [
-            ("new", "New"),
-            ("offer_recieved", "Offer Received"),
-            ("offer_accepted", "Offer Accepted"),
-            ("sold", "Sold"),
-            ("cancelled", "Cancelled"),
+            ('new', "New"),
+            ('offer_recieved', "Offer Received"),
+            ('offer_accepted', "Offer Accepted"),
+            ('sold', "Sold"),
+            ('cancelled', "Cancelled"),
         ],
         required=True,
         copy=False,
         default="new",
     )
     active = fields.Boolean(default=True)
+    total_area = fields.Integer(compute="_compute_total_area")
+    best_price = fields.Float(compute="_compute_best_price")
+
+    @api.depends("living_area", "garden_area")
+    def _compute_total_area(self):
+        for property in self:
+            property.total_area = property.living_area + property.garden_area
+
+    @api.depends("offer_ids.price")
+    def _compute_best_price(self):
+        for property in self:
+            # prices = [offer.price for offer in property.offer_ids]
+            prices = property.offer_ids.mapped("price")
+            property.best_price = max(prices) if prices else 0.0
+
+    # unsafe for business logic and  only works in form view (triggered on user interaction)
+    @api.onchange("garden")
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area, self.garden_orientation = 10, 'north'
+        else:
+            self.garden_area, self.garden_orientation = 0, False
+
+    # safe for business logic
+    @api.depends("garden")
+    def _compute_garden_details(self):
+        for property in self:
+            if property.garden:
+                property.garden_area, property.garden_orientation = 10, 'north'
+            else:
+                property.garden_area, property.garden_orientation = 0, False
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,12 +1,33 @@
-from odoo import fields, models
+from dateutil.relativedelta import relativedelta
+
+from odoo import api, fields, models
 
 
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
     _description = "this model is for estate property offers"
+
     price = fields.Float()
     status = fields.Selection(
-        [("accepted", "Accepted"), ("refused", "Refused")], copy=False
+        [('accepted', "Accepted"), ('refused', "Refused")],
+        copy=False,
     )
     partner_id = fields.Many2one("res.partner", required=True)
     property_id = fields.Many2one("estate.property", required=True)
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(
+        compute="_compute_date_deadline",
+        inverse="_inverse_date_deadline",
+    )
+
+    @api.depends("validity", "create_date")
+    def _compute_date_deadline(self):
+        for offer in self:
+            create_date = offer.create_date or fields.Date.today()
+            offer.date_deadline = create_date + relativedelta(days=offer.validity)
+
+    def _inverse_date_deadline(self):
+        for offer in self:
+            create_date = offer.create_date.date() or fields.Date.today()
+            if offer.date_deadline and create_date:
+                offer.validity = (offer.date_deadline - create_date).days
