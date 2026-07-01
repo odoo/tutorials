20162093974624a14e8cbbdf9fe59c1e56b015de
Ritvik Srivastava <risri@odoo.com>
2026-07-01 10:21:54 +0530

    [IMP] estate: complete chapter 8 and corrections from check-style

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,8 +7,10 @@
     'data': [
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
+        'views/estate_propoerty_new_view.xml',
         'views/estate_property_type_views.xml',
         'views/estate_property_tags_views.xml',
+        'views/estate_property_offer_views.xml',
         'views/estate_menus.xml',
     ],
     'demo': [
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -16,9 +16,12 @@ class EstateProperty(models.Model):
     living_area = fields.Integer(string="Living Area")
     facades = fields.Integer(string="Facades")
     garage = fields.Boolean(string="Garage")
+    garage_area = fields.Integer(string="Garage Area")
     garden = fields.Boolean(string="Garden")
     garden_area = fields.Integer(string="Garden Area")
     active = fields.Boolean(default=True, string="Active")
+    total_area = fields.Integer(string="Total Area", compute="_compute_total_area")
+
     state = fields.Selection(
         selection=[
             ('new', "New"),
@@ -41,19 +44,13 @@ class EstateProperty(models.Model):
         ],
         string="Garden Orientation"
     )
-    total_area = fields.Integer(string="Total Area", compute="_compute_total_area")
 
-    @api.depends('living_area', 'garden_area')
-    def _compute_total_area(self):
-        # breakpoint()
-        for record in self:
-            record.total_area = record.living_area + record.garden_area
     property_type_id = fields.Many2one("estate.property.type", string="Property Type")
     buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
     salesperson_id = fields.Many2one("res.users", default=lambda self: self.env.user, string="Salesperson")
     tag_ids = fields.Many2many("estate.property.tag", string="Tags")
     offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
-    best_price = fields.Integer(string="Best price", compute="_compute_best_price")
+    best_price = fields.Integer(string="Best price", compute="_compute_best_price", store="True")
 
     @api.depends('offer_ids.price')
     def _compute_best_price(self):
@@ -63,4 +60,23 @@ class EstateProperty(models.Model):
 
     @api.onchange('garden')
     def _on_change_garden(self):
-        self.garden_area = 10
+        if self.garden:
+            self.garden_area=10
+            self.garden_orientation = 'north'
+        else:
+            self.garden_area=0
+            self.garden_orientation = False
+
+    @api.onchange('garage')
+    def _on_change_garage(self):
+        if self.garage:
+            self.garage_area = 10
+        else:
+            self.garage_area = 0
+    
+
+    @api.depends('living_area', 'garden_area', 'garage_area')
+    def _compute_total_area(self):
+        # breakpoint()
+        for record in self:
+            record.total_area = record.living_area + record.garden_area + record.garage_area
