047322ca6d660715370f39ac46d2e98d565922cd
Mauryan Kansara <mauryankansaramk@gmail.com>
2026-07-02 17:47:08 +0530

    [ADD] estate: implement module for Real Estate businesses
    Create a new Real Estate module which helps businesses to manage their
    properties and purchasing & selling part.
    
    The module helps companies to organize their business in a structure manner,
    provide a way to to monitor their business and keep clean records.
    It helps businesses to manage their properties at ease and manage their
    purchasing & selling part efficiently.

---
+++ estate/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "Real Estate",
+    "version": "1.0",
+    "author": "Mauryan Kansara",
+    "category": "Tutorials",
+    "depends": ["base"],
+    "application": True,
+    "installable": True
+}
