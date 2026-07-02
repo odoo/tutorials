956e40e954b801e8a26f92c5989c23b9d05e5ca0
Piyush11204 <piyushkrishna11@gmail.com>
2026-07-02 16:40:58 +0530

    [ADD] estate: Initialize the real estate busniess
    The estate busniess has huge number of data thats needs
    to be manage and track. here we are creating an platfrom
    to mange the Real estate busniess
    
    This provides the base structure of the model.
    
    Chapter: 2

---
+++ estate/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Real Estate",
+    "version": "1.0",
+    "summary": "Real Estate Management",
+    "description": """
+        Real Estate Tutorial Module
+    """,
+    "author": "Piyush",
+    "category": "Tutorials",
+    "depends": ["base"],
+    "data": [],
+    "application": True,
+    "installable": True,
+}
