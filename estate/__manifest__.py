b80f298d32a63c2eefc3b01766bb079072ab2bbe
Kaushal Radadiya <karad@odoo.com>
2026-07-02 18:04:29 +0530

    [ADD] estate: initialize module for real estate application
    Set up the core module architecture to allow the Odoo server registry to recognize
    and install the 'estate' module for real estate management.

---
+++ estate/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'Estate',
+    'category': 'Sales/CRM',
+    'summary': 'Track leads and close opportunities',
+    'website': 'https://www.odoo.com/app/crm',
+    'application': True,
+    'depends': ['base',]
+}
