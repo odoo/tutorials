2e4413236536ac9493e96ba0f3e877e70f2e42e9
Mauryan Kansara <mauryankansaramk@gmail.com>
2026-07-02 16:35:04 +0530

    ADD estate: create initial module

---
+++ estate/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'depends': ['base'],
+    'category': 'Category',
+    'author': 'Mauryan Kansara',
+    'application': True,
+    'installable': True
+}
