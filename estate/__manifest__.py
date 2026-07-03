0338efb25680939df63cf56dcbf46f1d6709782d
pryes-odoo <pryes@odoo.com>
2026-07-03 17:18:51 +0530

    [ADD] estate:  Initial module for Real Estate Business
    Introduce the Estate module for real estate businesses to list and
    manage their properties
    This application provides a centralized solution for maintaining
    property listings and supporting day-to-day real estate management
    here foundations of completely new odoo module are applied so Estate
    module  can be  installed as application
    
    Completed Chapter 2

---
+++ estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Estate',
+    'version': '1.0',
+    'category': 'Tutorials',
+    'depends': ['base'],
+    'installable': True,
+    'application': True,
+    'author': 'Prerana Yesugade',
+    'license': 'LGPL-3',
+}
