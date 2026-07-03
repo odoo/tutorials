1f97e5e8400f456f91ab9d7e713da56fbf19f59f
Kaushal Radadiya <karad@odoo.com>
2026-07-03 11:33:21 +0530

    [CLN] estate: add manifest author and license details
    Specify missing author and open-source license attributes in the manifest file
    to prevent server warnings during module registration.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,8 +1,10 @@
 {
-    'name': 'Estate',
-    'category': 'Sales/CRM',
-    'summary': 'Track leads and close opportunities',
-    'website': 'https://www.odoo.com/app/crm',
-    'application': True,
-    'depends': ['base',]
+    "name": "Estate",
+    "category": "Sales/CRM",
+    "summary": "Track leads and close opportunities",
+    "website": "https://www.odoo.com/app/crm",
+    "application": True,
+    "depends": ["base"],
+    "author": "Kaushal Radadiya",
+    "license": "LGPL-3",
 }
