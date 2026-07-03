0c81ab452bb81655c28f3d7f0daaae8803068810
yacha-odoo <yacha@odoo.com>
2026-07-03 17:00:51 +0530

    [ADD] estate: add initial module structure
    Create the initial Real Estate module for the Server Framework 101 tutorial.
    
    This introduces the base module structure with the manifest and package initialization files required for further development.

---
+++ estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'author': 'yacha',
+    'depends': ['base'],
+    'application': True,
+    'category': 'Tutorials',
+    'installable': True,
+    'license': 'LGPL-3'
+}
