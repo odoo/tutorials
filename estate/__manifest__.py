abb1be6dd35564b2466152794c0f18f1a3e3e74a
pryes-odoo <pryes@odoo.com>
2026-07-06 17:55:15 +0530

    [ADD] estate: define property information
    Introduce the property information required to manage real estate
    listings
    
    Capturing essential property details enables businesses to maintain
    complete property records and improves the organization of their real
    estate portfolio
    
    Completed Chapter 3

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,10 +1,10 @@
 {
-    'name': 'Estate',
-    'version': '1.0',
-    'category': 'Tutorials',
-    'depends': ['base'],
-    'installable': True,
-    'application': True,
-    'author': 'Prerana Yesugade',
-    'license': 'LGPL-3',
+    "name": "Estate",
+    "version": "1.0",
+    "category": "Tutorials",
+    "depends": ["base"],
+    "installable": True,
+    "application": True,
+    "author": "Prerana Yesugade",
+    "license": "LGPL-3",
 }
