c96f13d30992a4b611b4200ea3f62c9f71bb35a1
Kaushal Radadiya <karad@odoo.com>
2026-07-06 17:38:57 +0530

    [ADD] estate: add navigation menus and default record logic
    Create top-level menus and actions so users can open properties in the web interface.
    Set default availability dates and protect selling prices from unauthorized manual editing.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,6 +7,7 @@
     "installable": True,
     "data": [
         "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
     ],
     "depends": ["base"],
     "author": "Kaushal Radadiya",
