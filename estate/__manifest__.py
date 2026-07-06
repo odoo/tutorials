7f8c9e9e548c48c3b8d7a844dd203899c854cad2
mrgor-odoo <mrgor@odoo.com>
2026-07-06 18:18:56 +0530

    [ADD] estate: create property model
    Create the basic property model for the Real Estate module.
    
    This model allows storing essential property information and
    provides the base structure for adding more features.
    
    task = chapter 3 completed

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,10 +1,10 @@
 {
-    'name': 'Real Estate',
-    'version': '1.0',
-    'author': 'Mrunmayee',
-    'depends': ['base'],
-    'application': True,
-    'category': 'tutorials',
-    'installabe': True,
-    'license': 'LGPL-3',
+    "name": "Real Estate",
+    "version": "1.0",
+    "author": "Mrunmayee",
+    "depends": ["base"],
+    "application": True,
+    "category": "tutorials",
+    "installabe": True,
+    "license": "LGPL-3",
 }
