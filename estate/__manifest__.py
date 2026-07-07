66e5c64883849123837b44916097476d29a14143
yacha <yacha@odoo.com>
2026-07-07 18:11:29 +0530

    [ADD] estate: Security Access Granted

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,10 +1,11 @@
 {
-    'name': 'Real Estate',
-    'version': '1.0',
-    'author': 'yacha',
-    'depends': ['base'],
-    'application': True,
-    'category': 'Tutorials',
-    'installable': True,
-    'license': 'LGPL-3'
+    "name": "Real Estate",
+    "version": "1.0",
+    "author": "yacha",
+    "depends": ["base"],
+    "application": True,
+    "category": "Tutorials",
+    "installable": True,
+    "license": "LGPL-3",
+    "data": ["security/ir.model.access.csv"],
 }
