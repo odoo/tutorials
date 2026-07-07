e68219c715c7207aa42b5662a9ceeb5b97faed44
Harsh Gavit (hagav) <hagav@odoo.com>
2026-07-07 17:59:31 +0530

    [IMP] estate: set default values and other field customizations
    - set default value in date_availability (3 months from current date)
    - set selling_price to readonly
    - bedrooms have now default value of 2

--- estate/models/estate.py
+++ estate/models/estate.py
@@ -1,3 +1,4 @@
+from dateutil.relativedelta import relativedelta
 from odoo import fields, models
 
 
@@ -9,10 +10,12 @@ class Estate(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(
+        default=lambda self: fields.Date.today() + relativedelta(months=3)
+    )
     expected_price = fields.Float()
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
