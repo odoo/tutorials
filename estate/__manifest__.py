1b44b46382e7c77cddf9d9a8680178c82e2018cc
Harsh Gavit (hagav) <hagav@odoo.com>
2026-07-07 12:57:25 +0530

    [IMP] estate: add views(actions) & menus
    - action defined in estate_views.xml and linked with esate model
    - three level menus added using nested menuitem element
    - updated manifest to include menus & views files

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,9 +1,11 @@
-{
+{  # pyright: ignore[reportUnusedExpression]
     "name": "estate",
     "depends": ["base_setup"],
     "author": "Havit",
     "license": "LGPL-3",
     "data": [
         "security/ir.model.access.csv",
+        "views/estate_views.xml",
+        "views/estate_menus.xml",
     ],
 }
