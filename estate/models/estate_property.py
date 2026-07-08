5da110cf57a7260903d15f762f6431a03558be3a
amtha-odoo <amtha@odoo.com>
2026-07-08 10:09:07 +0530

    [ADD] estate: add property type model and relation

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -8,6 +8,30 @@ class EstateProperty(models.Model):
     _description = "Estate Property"
 
     name = fields.Char(required=True)
+    property_type_id = fields.Many2one(
+        "estate.property.type",
+        string="Property Type",
+    )
+
+    buyer_id = fields.Many2one(
+        "res.partner",
+        string="Buyer",
+        copy=False,
+    )
+    salesperson_id = fields.Many2one(
+        "res.users",
+        string="Salesperson",
+        default=lambda self: self.env.user,
+    )
+    tag_ids = fields.Many2many(
+        "estate.property.tag",
+        string="Tags",
+    )
+    offer_ids = fields.One2many(
+        "estate.property.offer",
+        "property_id",
+        string="Offers",
+    )
 
     description = fields.Text()
 
