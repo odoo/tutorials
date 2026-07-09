71e6cb610c543c24e35415c63516f33d50e9d5e6
pryes-odoo <pryes@odoo.com>
2026-07-09 18:48:15 +0530

    [ADD] estate:  add navigation for property management
    Improve the property management workflow, Provide a dedicated navigation structure for the Estate application
    Application menus make property records easier to access, while default
    values reduce repetitive data entry and copy restrictions preserve data
    consistency when duplicating properties

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,7 +3,11 @@
     "version": "1.0",
     "category": "Tutorials",
     "depends": ["base"],
-    "data": ["security/ir.model.access.csv", "views/estate_property_views.xml"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
+    ],
     "application": True,
     "author": "Prerana Yesugade",
     "license": "LGPL-3",
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -7,16 +7,19 @@ class EstateProperty(models.Model):
 
     name = fields.Char(required=True)
     description = fields.Text()
-    postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(
+        copy=False, default=lambda self: fields.Date.add(fields.Date.today(), months=3)
+    )
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    active = fields.Boolean(default=True)
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
+    postcode = fields.Char()
     garden_orientation = fields.Selection(
         string="Type",
         selection=[
