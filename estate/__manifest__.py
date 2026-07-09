7d130c1bc639d2b5d82593e987caa3f54206e299
yacha <yacha@odoo.com>
2026-07-09 17:43:09 +0530

    [ADD] estate: add menus and model field attributes
    Implemented Chapter 5 of the Odoo Server Framework tutorial.
    
    - Added window action for estate.property
    - Split XML into estate_property_views.xml and estate_menus.xml
    - Added three-level menu hierarchy
    - Added readonly and copy attributes
    - Added default values for bedrooms and availability date
    - Added reserved fields active and state

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,5 +7,9 @@
     "category": "Tutorials",
     "installable": True,
     "license": "LGPL-3",
-    "data": ["security/ir.model.access.csv"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
+    ],
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,17 +1,25 @@
-from odoo import models, fields
+from odoo import api, models, fields
+from dateutil.relativedelta import relativedelta
+
+
+@api.model
+def _default_date_availability(self):
+    return fields.Date.today() + relativedelta(months=3)
 
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real Estate Property"
 
-    name = fields.Char(required=True)
+    active = fields.Boolean()
+
+    name = fields.Char(required=True, default="Unknown")
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=_default_date_availability)
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -24,4 +32,17 @@ class EstateProperty(models.Model):
             ("east", "East"),
             ("west", "West"),
         ]
+    )
+    state = fields.Selection(
+        [
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled"),
+        ],
+        string="State",
+        required=True,
+        copy=False,
+        default="new",
     )
