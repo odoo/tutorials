707c8621603c61d5108fcde538100de467021313
mrgor-odoo <mrgor@odoo.com>
2026-07-10 18:53:45 +0530

    [IMP] estate: prepare property management interface
    - Allow users to access and manage property records from the UI.
    
    - Improve data consistency by protecting important field values.
    
    - Prevent unwanted data from being copied to duplicate records.
    
    - Reduce manual work by providing default field values.
    
    - Prepare the property model for future business workflows by introducing state fields.
    
    Task Completed chapter 5

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -8,10 +8,12 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(
+        copy=False, default=lambda self: fields.Date.add(fields.Date.today(), months=3)
+    )
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -24,4 +26,17 @@ class EstateProperty(models.Model):
             ("east", "East"),
             ("west", "West"),
         ]
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        selection=[
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled"),
+        ],
+        required=True,
+        copy=False,
+        default="new",
     )
