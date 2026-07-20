1bb0ced81cb207c6c63f8804e6dbcacad0890c45
prway-odoo <prway@odoo.com>
2026-07-20 16:43:00 +0530

    [ADD] estate: establish property relationships
    Establish relationships between property records and their
    associated entities to accurately model the Estate domain.
    
    This provides the foundation for managing related data through
    the Odoo ORM and user interface.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,8 +7,12 @@
     "description": "This is the estateapp",
     "license": "LGPL-3",
     "data": [
-        "security/ir.model.access.csv",
-        "views/estate_property_views.xml",
-        "views/estate_menus.xml",
+        'security/ir.model.access.csv',
+
+        'views/estate_property_views.xml',
+        'views/estate_property_type_views.xml',
+        'views/estate_property_tag_views.xml',
+
+        'views/estate_menus.xml',
     ],
 }
