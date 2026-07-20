999d42193b0f77c4dc1e73fc157a020f07066b1b
bepro@odoo <bepro@odoo.com>
2026-07-20 14:07:50 +0200

    [IMP] estate: 4. Security

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,4 +3,9 @@
     'depends': ['base'],
     'author': 'bepro',
     'license': 'LGPL-3',
+    'data': [
+        'security/ir.model.access.csv',
+        # 'views/estate_property_views.xml',
+        # 'views/estate_menus.xml'
+    ],
 }
