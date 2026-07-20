db2fc411d695d522279da8cbe4f86adbf532e693
Tony Michel (tomic) <tomic@odoo.com>
2026-07-20 15:00:12 +0200

    [ADD] estate: add initial module structure

---
+++ estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'category': 'Real Estate/Brokerage',
+    'summary': 'An estate module for training',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
