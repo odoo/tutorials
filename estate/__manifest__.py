e26b1c824aa970028723098c391ed51007fe427e
Tony Michel (tomic) <tomic@odoo.com>
2026-07-20 16:17:54 +0200

    [IMP] estate: add access rights

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,10 +1,13 @@
 {
     'name': 'Real Estate',
     'version': '1.0',
+    'author': 'tomic',
     'category': 'Real Estate/Brokerage',
     'summary': 'An estate module for training',
     'depends': ['base'],
-    'data': [],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
     'installable': True,
     'application': True,
     'license': 'LGPL-3',
