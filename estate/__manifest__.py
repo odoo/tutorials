e9dae69fc716f9f72d5f98b5c62ecf3e32058904
Tom Hermann (toher) <toher@odoo.com>
2026-07-22 11:08:34 +0200

    [FIX] ci style errors

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,7 +3,7 @@
     'depends':  ['base'],
     'application': True,
 
-    'data' : [
+    'data': [
         'security/ir.model.access.csv',
         'view/estate_property_views.xml',
         'view/estate_menus.xml'
