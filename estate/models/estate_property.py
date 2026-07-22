1b4148a3d62b63c30bd55c3bbab54b3733ca85a0
Cezar Bulancea (cebul) <cezar.bulancea@gmail.com>
2026-07-22 10:33:53 +0200

    [IMP] estate: Create basic fields.
    Following chapter 3 - model fields from Server Framework 101

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,6 +1,22 @@
-from odoo import models
+from odoo import fields, models
 
 class Property(models.Model):
     _name = 'estate.property'
     _description = "Estate Property"
 
+    name = fields.Char()
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float()
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        string="Garden Orientation",
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
+    )
