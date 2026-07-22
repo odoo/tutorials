6549326657bbb6efea7037e4cebc0771a51e304d
jean (jebou) <jeabou@odoo.com>
2026-07-22 11:22:33 +0200

    [ADD] estate: add views and menus to estate model. Also change access rights folder

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,10 +1,10 @@
 {
-    'name': 'Real Estate',
-    'application': True,
-    'depends': [
-        'base'
+    "name": "Real Estate",
+    "application": True,
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
     ],
-    'data': [
-        'data/ir.model.access.csv'
-    ]
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,20 +1,46 @@
+from typing import Required
+
 from odoo import fields, models
+
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Table for property test tutoriel"
 
-    name = fields.Char(required=True)
+    name = fields.Char(required=True, default="Unknown")
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        selection=[
+            ("new", "New"),
+            ("offer received", "Offer Received"),
+            ("offer accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled"),
+        ],
+        required=True,
+        copy=False,
+        default="new",
+    )
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(
+        copy=False,
+        default=fields.Date.add(fields.Date.today(), months=3),
+    )
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
-    garden_orientation = fields.Selection(selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
-
+    garden_orientation = fields.Selection(
+        selection=[
+            ("north", "North"),
+            ("south", "South"),
+            ("east", "East"),
+            ("west", "West"),
+        ]
+    )
+    last_seen = fields.Datetime("Last Seen", default=fields.Datetime.now)
