97541c85f7503d66c8afb8d8bd5b4c7227f064a2
Cezar Bulancea (cebul) <cezar.bulancea@gmail.com>
2026-07-22 10:39:55 +0200

    [IMP] estate: Make name and expected_price not nullable.
    Following chapter 3 - common attributes from SF101.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -4,11 +4,11 @@ class Property(models.Model):
     _name = 'estate.property'
     _description = "Estate Property"
 
-    name = fields.Char()
+    name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
     date_availability = fields.Date()
-    expected_price = fields.Float()
+    expected_price = fields.Float(required=True)
     selling_price = fields.Float()
     bedrooms = fields.Integer()
     living_area = fields.Integer()
