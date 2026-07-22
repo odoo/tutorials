a347f49828940f718665d0b670608f47b21987ca
Cezar Bulancea (cebul) <cezar.bulancea@gmail.com>
2026-07-22 17:20:10 +0200

    [IMP] estate: Fields, Attributes And View
    Finished chapter 5 from the SF101 tutorial.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -8,10 +8,13 @@ class Property(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(
+        default=lambda self: fields.Date.add(fields.Date.today(), months=3), 
+        copy=False
+    )
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -20,4 +23,13 @@ class Property(models.Model):
     garden_orientation = fields.Selection(
         string="Garden Orientation",
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        string="State",
+        selection=[('new', 'New'), ('offer received', 'Offer Received'),
+                   ('offer accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')],
+        required=True,
+        copy=False,
+        default='new'
     )
