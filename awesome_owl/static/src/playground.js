7979eeb5f5970651ea14c1df523f5e76ba234b41
bepro@odoo <bepro@odoo.com>
2026-07-23 11:16:53 +0200

    [IMP] awesome_owl: Add dynamic attributes

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -2,10 +2,11 @@ import { Component, useState, markup } from "@odoo/owl";
 
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
+import { TodoList } from "./todo/todolist";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
-    static components = { Counter, Card };
+    static components = { Counter, Card, TodoList };
 
     setup() {
         this.state = useState({ sum: 2 });
---
+++ awesome_owl/static/src/todo/todolist.js
@@ -0,0 +1,15 @@
+import { Component, useState } from "@odoo/owl";
+import { TodoItem } from "./todoitem";
+
+export class TodoList extends Component {
+    static template = "awesome_owl.todolist";
+    static components = { TodoItem };
+
+    setup() {
+        this.todos = useState([
+            { id: 3, description: "buy milk", isCompleted: false },
+            { id: 4, description: "FIX MACHINE PLX", isCompleted: false },
+            { id: 5, description: "WAKE UP", isCompleted: true },
+        ]);
+    }
+}
