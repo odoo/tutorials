f4e95648e3c297664779b458f27a69d3e9fa909b
samde <samde@odoo.com>
2026-07-23 17:36:48 +0200

    [IMP] estate: chapter8: computed fields and onchanges

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,8 @@
-from odoo import fields, models
+from datetime import date, datetime, timedelta
+from dateutil.relativedelta import relativedelta
+
+
+from odoo import api, fields, models
 
 
 class EstatePropertyOffer(models.Model):
@@ -10,5 +14,29 @@ class EstatePropertyOffer(models.Model):
         [("accepted", "Accepted"), ("refused", "Refused")],
         copy=False,
     )
+    validity = fields.Integer(
+        "Validaty (days)",
+        default=7,
+    )
+    date_deadline = fields.Date(
+        "Deadline",
+        compute="_computed_date_deadline",
+        inverse="_inverse_validity_period",
+        readonly=False,
+    )
     partner_id = fields.Many2one("res.partner", required=True)
     property_id = fields.Many2one("realestate.properties", required=True)
+
+    @api.depends("validity")
+    def _computed_date_deadline(self):
+        for offer in self:
+            create_date = offer.create_date if offer.create_date else datetime.today()
+            if offer.validity:
+                offer.date_deadline = create_date + timedelta(
+                    days=offer.validity,
+                )
+
+    def _inverse_validity_period(self):
+        for offer in self:
+            create_date = offer.create_date if offer.create_date else datetime.today()
+            offer.validity = (offer.date_deadline - create_date.date()).days
