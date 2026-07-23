9741c4ecc03ea296447ce17923b3876411e941ad
bepro@odoo <bepro@odoo.com>
2026-07-23 16:23:12 +0200

    [IMP] awesome_dahboard: Add nav butons

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,10 +1,23 @@
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";
 import { Layout } from "@web/search/layout";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
     static components = { Layout };
+
+    setup() {
+        this.action = useService("action");
+    }
+
+    async openCustomers() {
+        this.action.doAction("base.action_partner_form", {});
+    }
+
+    async openLeads() {
+        this.action.doAction("crm.crm_lead_all_leads");
+    }
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
