6394aea22f6d9f734b7b6a69fc57c38b2939e20b
bepro@odoo <bepro@odoo.com>
2026-07-23 11:52:20 +0200

    [IMP] awesome_owl: Adding a todo

--- awesome_owl/static/src/todo/todolist.js
+++ awesome_owl/static/src/todo/todolist.js
@@ -6,10 +6,26 @@ export class TodoList extends Component {
     static components = { TodoItem };
 
     setup() {
-        this.todos = useState([
-            { id: 3, description: "buy milk", isCompleted: false },
-            { id: 4, description: "FIX MACHINE PLX", isCompleted: false },
-            { id: 5, description: "WAKE UP", isCompleted: true },
-        ]);
+        // this.todos = useState([
+        //     { id: 3, description: "buy milk", isCompleted: false },
+        //     { id: 4, description: "FIX MACHINE PLX", isCompleted: false },
+        //     { id: 5, description: "WAKE UP", isCompleted: true },
+        // ]);
+        this.todos = useState([]);
+    }
+
+    addTodo(event) {
+        // console.warn("addTodo");
+        // console.log(event);
+        if (event.keyCode === 13) {
+            let description = event.target.value;
+            if (description.length == 0) {
+                return;
+            }
+            let next_id = 1 + Math.max(...this.todos.map(item => item.id), 0);
+            this.todos.push({ id: next_id, description: description, isCompleted: false });
+            // Reset input
+            event.target.value = '';
+        }
     }
 }
