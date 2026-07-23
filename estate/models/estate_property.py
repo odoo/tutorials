33257b3654cffdae86b7251cf54cba0823ade3ef
samde <samde@odoo.com>
2026-07-23 17:36:48 +0200

    [IMP] Real estate: chapter8: computed fields and onchanges

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,7 +1,7 @@
 from datetime import date, datetime, time
 from dateutil.relativedelta import relativedelta
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class EstateProperty(models.Model):
@@ -32,11 +32,12 @@ class EstateProperty(models.Model):
     )
     selling_price = fields.Float("Selling price", copy=False, readonly=True)
     bedrooms = fields.Integer("Bedrooms", default=2)
-    living_area = fields.Integer("Living area")
     facades = fields.Integer("Facades")
     garage = fields.Boolean("Garage")
     garden = fields.Boolean("Garden")
-    garden_area = fields.Integer("Garden area")
+    living_area = fields.Integer("Living area (sqm)")
+    garden_area = fields.Integer("Garden area (sqm)")
+    total_area = fields.Integer("Total area (sqm)", compute="_compute_total_area")
     garden_orientation = fields.Selection(
         [
             ("north", "North"),
@@ -45,6 +46,7 @@ class EstateProperty(models.Model):
             ("west", "West"),
         ],
     )
+    best_offer = fields.Float("Best Offer", compute="_compute_best_price")
     buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
     sale_rep_id = fields.Many2one(
         "res.users",
@@ -57,3 +59,22 @@ class EstateProperty(models.Model):
         "realestate.properties.offer",
         "property_id",
     )
+
+    @api.depends("living_area", "garden_area")
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
+    @api.depends("offer_ids.price")
+    def _compute_best_price(self):
+        for record in self:
+            record.best_offer = max(record.offer_ids.mapped("price"))
+
+    @api.onchange("garden")
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = "north"
+        else:
+            self.garden_area = 0
+            self.garden_orientation = None
