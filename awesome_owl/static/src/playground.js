6c8e1d94fc1f0ce140b716c1b001744c748767ee
bepro@odoo <bepro@odoo.com>
2026-07-23 10:03:27 +0200

    [IMP] awesome_owl: Add owl component

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,5 +1,16 @@
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
+
+import { Counter } from "./counter/counter";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
+    static components = { Counter };
+
+    setup() {
+        this.state = useState({ value: 0 });
+    }
+
+    increment() {
+        this.state.value++;
+    }
 }
