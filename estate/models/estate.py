8a2518a0f107f836664677393ee58507a46468bf
amgom-odoo <amgom@odoo.com>
2026-07-23 10:00:02 +0200

    [IMP] add ruff linter and formatter

--- estate/models/estate.py
+++ estate/models/estate.py
@@ -3,24 +3,45 @@ from dateutil.relativedelta import relativedelta
 
 
 class Estate(models.Model):
-    _name = 'estate_property'
-    _description = 'Real Estate'
+    _name = "estate_property"
+    _description = "Real Estate"
 
-    name = fields.Char('Name', required=True)
-    description = fields.Text('Description')
-    postcode = fields.Char('Postcode')
-    date_availability = fields.Date("Date Available", default=fields.Date.today() + relativedelta(months=3), copy=False)
-    expected_price = fields.Float('Expected Price', required=True)
-    selling_price = fields.Float('Selling price', readonly=True, copy=False)
+    name = fields.Char("Name", required=True)
+    description = fields.Text("Description")
+    postcode = fields.Char("Postcode")
+    date_availability = fields.Date(
+        "Date Available",
+        default=fields.Date.today() + relativedelta(months=3),
+        copy=False,
+    )
+    expected_price = fields.Float("Expected Price", required=True)
+    selling_price = fields.Float("Selling price", readonly=True, copy=False)
     bedrooms = fields.Integer("Bedrooms", default=2)
     living_area = fields.Integer("Living Area")
     facades = fields.Integer("Facades")
     garage = fields.Boolean("Garage")
     garden = fields.Boolean("Garden")
     garden_area = fields.Integer("Garden Area")
-    garden_orientation = fields.Selection(string="Garden Orientation",
-                            selection=[('north', 'North'), ('east', 'East'), ('west', 'West'), ('south', 'South')])
+    garden_orientation = fields.Selection(
+        string="Garden Orientation",
+        selection=[
+            ("north", "North"),
+            ("east", "East"),
+            ("west", "West"),
+            ("south", "South"),
+        ],
+    )
     active = fields.Boolean("Active", default=True)
-    state = fields.Selection(string="State", selection=[('new', 'New'), ('offer_received', 'Offer Received'),
-            ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('canceled', 'Canceled')], 
-            required=True, copy=False, default='new')
+    state = fields.Selection(
+        string="State",
+        selection=[
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("canceled", "Canceled"),
+        ],
+        required=True,
+        copy=False,
+        default="new",
+    )
