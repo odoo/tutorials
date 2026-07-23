b05df533421cac8e6f83c8657e38cbd2ece73c79
bepro@odoo <bepro@odoo.com>
2026-07-23 10:24:56 +0200

    [IMP] awesome_owl: Add card component

---
+++ awesome_owl/static/src/card/card.js
@@ -0,0 +1,7 @@
+import { Component, useState } from "@odoo/owl";
+
+
+export class Card extends Component {
+    static template = "awesome_owl.card";
+    static props = ['title', 'content'];
+}
--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,10 +1,11 @@
 import { Component, useState } from "@odoo/owl";
 
 import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
-    static components = { Counter };
+    static components = { Counter, Card };
 
     setup() {
         this.state = useState({ value: 0 });
