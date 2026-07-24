e80868319f93b65589c5645d55e9b0b23ab81fe3
samde <samde@odoo.com>
2026-07-24 10:22:12 +0200

    [IMP] estate: chapter9: ready for some action

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -2,14 +2,14 @@ from datetime import date, datetime, timedelta
 from dateutil.relativedelta import relativedelta
 
 
-from odoo import api, fields, models
+from odoo import api, _, exceptions, fields, models
 
 
 class EstatePropertyOffer(models.Model):
     _name = "realestate.properties.offer"
     _description = "Real estate property offer"
 
-    price = fields.Float("Price")
+    price = fields.Float("Price", required=True)
     status = fields.Selection(
         [("accepted", "Accepted"), ("refused", "Refused")],
         copy=False,
@@ -40,3 +40,17 @@ class EstatePropertyOffer(models.Model):
         for offer in self:
             create_date = offer.create_date if offer.create_date else datetime.today()
             offer.validity = (offer.date_deadline - create_date.date()).days
+
+    def action_confirm(self):
+        for offer in self:
+            if offer.property_id.buyer_id:
+                raise exceptions.UserError(_("One offer has already been accepted."))
+            offer.status = "accepted"
+            offer.property_id.selling_price = offer.price
+            offer.property_id.buyer_id = offer.partner_id
+
+    def action_cancel(self):
+        for offer in self:
+            offer.status = "refused"
+            offer.property_id.selling_price = 0
+            offer.property_id.buyer_id = None
