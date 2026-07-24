eb3a3a60e97305f191f460ce3034d72dc83e852b
bepro@odoo <bepro@odoo.com>
2026-07-24 10:50:44 +0200

    [IMP] awesome_dashboard: lazy load the dashboard

--- awesome_dashboard/static/src/dashboard/dashboard.js
+++ awesome_dashboard/static/src/dashboard/dashboard.js
@@ -5,6 +5,7 @@ import { Layout } from "@web/search/layout";
 
 import { DashboardItem } from "./dashboard_item/dashboard_item";
 import { PieChart } from "./pie_chart";
+
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
     static components = { Layout, DashboardItem, PieChart };
@@ -33,4 +34,4 @@ class AwesomeDashboard extends Component {
     }
 }
 
-registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
+registry.category("lazy_components").add("AwesomeDashboard", AwesomeDashboard);
