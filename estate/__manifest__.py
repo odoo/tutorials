38c6cffa3030714fbe67d8f8c36fba376e1b5047
jean (jebou) <jeabou@odoo.com>
2026-07-28 09:46:42 +0200

    [IMP] estate: add inherited module from res.users

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -10,6 +10,7 @@
         "views/estate_property_offer_views.xml",
         "views/estate_property_type_views.xml",
         "views/estate_property_tag_views.xml",
+        "views/estate_res_users.xml",
         "views/estate_menus.xml",
     ],
 }
