7286172ff7d4530a3065b8104600f54ad3b1b9ec
Cezar Bulancea (cebul) <cezar.bulancea@gmail.com>
2026-07-28 11:45:53 +0200

    [IMP] awesome_owl: chpater 1.3
    a simple card component

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,8 +1,8 @@
 import { Component, useState } from "@odoo/owl";
 import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
-
-    static components = { Counter };
+    static components = { Counter, Card };
 }
