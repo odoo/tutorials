3fbe5afcc021839fbcc2022978b74036a3eae7af
Cezar Bulancea (cebul) <cezar.bulancea@gmail.com>
2026-07-28 13:59:29 +0200

    [IMP] awesome_owl: chapter 1.5
    props validation

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -2,4 +2,9 @@ import { Component } from "@odoo/owl";
 
 export class Card extends Component {
     static template = "awesome_owl.card";
+
+    static props = {
+        title: {type: String},
+        content: {type: String}
+    };
 }
