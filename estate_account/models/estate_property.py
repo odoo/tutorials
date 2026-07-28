7b71900fc47185b0a6822acaad98632aee781c0c
jean (jebou) <jeabou@odoo.com>
2026-07-28 10:54:41 +0200

    [ADD] estate_account: add the estate_account module

---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,31 @@
+from odoo import Command, models
+
+
+class EstateProperty(models.Model):
+    _inherit = "estate.property"
+
+    def action_sold(self):
+        for record in self:
+            self.env["account.move"].create(
+                {
+                    "move_type": "out_invoice",
+                    "partner_id": record.buyer_id.id,
+                    "invoice_line_ids": [
+                        Command.create(
+                            {
+                                "name": "6% of selling price",
+                                "quantity": 1,
+                                "price_unit": record.selling_price * 0.06,
+                            },
+                        ),
+                        Command.create(
+                            {
+                                "name": "Administrative Fees",
+                                "quantity": 1,
+                                "price_unit": 100,
+                            },
+                        ),
+                    ],
+                },
+            )
+        return super().action_sold()
