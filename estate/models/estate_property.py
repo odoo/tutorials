880477d2263474d20d4331166de9ebb61f35afe6
pryes-odoo <pryes@odoo.com>
2026-07-28 18:59:42 +0530

    [ADD] estate: add tags for property characteristics
    Tags help users highlight relevant property characteristics and make
    listings easier to distinguish and organize beyond their primary
    property type
    
    Allow multiple tags on each property so listings can represent several
    characteristics without being limited to a single classification
    provide dedicated access to maintain a consistent set of tags across
    property records
    make tag management available to authorized users so classifications
    can be maintained and reused across property listings

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -46,5 +46,6 @@ class EstateProperty(models.Model):
     property_type_id = fields.Many2one("estate.property.type", string="Property Type")
     buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
     salesperson_id = fields.Many2one(
-        "res.users", string="Salesmen", default=lambda self: self.env.user
+        "res.users", string="Salesman", default=lambda self: self.env.user
     )
+    tags_id = fields.Many2many("estate.property.tag", string="Property Tags")
