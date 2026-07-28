88107c8f5d81c83b246743125ddb21d93d51bc97
Tom Hermann (toher) <toher@odoo.com>
2026-07-28 17:15:27 +0200

    [FIX] manifest loading order

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -8,10 +8,9 @@
     'data': [
         'security/ir.model.access.csv',
         'view/estate_property_views.xml',
-        'view/estate_menus.xml',
         'view/estate_property_type_view.xml',
         'view/estate_property_tag_view.xml',
-        # 'view/estate_property_offer_view.xml',
-        'view/inhertied_user.xml'
+        'view/inhertied_user.xml',
+        'view/estate_menus.xml',
     ],
 }
