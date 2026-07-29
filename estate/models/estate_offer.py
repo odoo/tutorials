f784cebd18121c16b905352f5beb9ca1c42eac97
Harsh Gavit (hagav) <hagav@odoo.com>
2026-07-29 18:42:21 +0530

    [IMP] estate: add deadline and validity field with compute and inverse func
    - updated  form & list view
    - using current date as fallback when create_date is not set e.g. When
      we create new offer at that time create_date is not set until that
      record is saved for  first time
    - ! inverse func only triggered after saving record

--- estate/models/estate_offer.py
+++ estate/models/estate_offer.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from dateutil.relativedelta import relativedelta
+from odoo import models, fields, api
 
 
 class EstateOffer(models.Model):
@@ -12,3 +13,14 @@ class EstateOffer(models.Model):
     )
     partner_id = fields.Many2one('res.partner', required=True)
     property_id = fields.Many2one('estate', required=True)
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute='_compute_date_deadline', inverse='_inverse_date_deadline')
+
+    @api.depends('validity')
+    def _compute_date_deadline(self):
+        for record in self:
+            record.date_deadline = (record.create_date or fields.Date.today()) + relativedelta(days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            record.validity = (record.date_deadline - (record.create_date.date() or fields.Date.today())).days
