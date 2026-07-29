56341bb8b3a2e60a7b8ec4d217fa104fc1300a27
Cezar Bulancea (cebul) <cezar.bulancea@gmail.com>
2026-07-29 16:19:15 +0200

    [IMP] awesome_owl: chapter 1.7
    created a todo list and a todo item component

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,10 +1,11 @@
 import { Component, markup, useState } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
+import { TodoList } from "./todo/todoList";
 
 export class Playground extends Component {
-    static template = "awesome_owl.playground";
-    static components = { Counter, Card };
+    static template = 'awesome_owl.playground';
+    static components = { Counter, Card, TodoList };
 
     value1 = "<div>content 1</div>"
     value2 = markup("<div>content 2</div>")
---
+++ awesome_owl/static/src/todo/todoList.js
@@ -0,0 +1,15 @@
+import { Component, useState } from "@odoo/owl"
+import { TodoItem } from "./todoItem";
+
+export class TodoList extends Component {
+    static template = 'awesome_owl.todoList';
+
+    setup() {
+        this.todos = useState([
+            { id: 2, description: "wash the dishes", isCompleted: false },
+            { id: 3, description: "buy milk", isCompleted: false },
+        ]);
+    }
+
+    static components = { TodoItem };
+}
