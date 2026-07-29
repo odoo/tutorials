4a571b8344502908e9712fde8899ecb1cf7a7fda
Cezar Bulancea (cebul) <cezar.bulancea@gmail.com>
2026-07-29 17:52:12 +0200

    [IMP] awesome_owl: chapter 1.9
    adding a todo; we have an input field where we can introduce new task and when enter is pressed they are added to the todo list

--- awesome_owl/static/src/todo/todoList.js
+++ awesome_owl/static/src/todo/todoList.js
@@ -3,13 +3,24 @@ import { TodoItem } from "./todoItem";
 
 export class TodoList extends Component {
     static template = 'awesome_owl.todoList';
+    static components = { TodoItem };
 
     setup() {
-        this.todos = useState([
-            { id: 2, description: "wash the dishes", isCompleted: true },
-            { id: 3, description: "buy milk", isCompleted: false },
-        ]);
+        this.todos = useState([]);
+        this.id = 1;
     }
 
-    static components = { TodoItem };
+    addTodo(ev) {
+        if (ev.keyCode !== 13)
+            return;
+
+        const value = ev.target.value.trim();
+
+        if (value.length === 0)
+            return;
+
+        this.todos.push({id: this.id++, description: value, isCompleted: false});
+
+        ev.target.value = "";
+    }
 }
