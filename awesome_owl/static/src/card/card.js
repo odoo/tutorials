2144d3e867347b61fc07dd0c7f8e0ae808368ddc
Cezar Bulancea (cebul) <cezar.bulancea@gmail.com>
2026-07-30 10:40:49 +0200

    [IMP] awesome_owl: chapter 1.14
    minimizing card content using a toggle button

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -1,4 +1,4 @@
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 
 export class Card extends Component {
     static template = "awesome_owl.card";
@@ -7,4 +7,12 @@ export class Card extends Component {
         title: {type: String},
         slots: {type: Object, optional: true},
     };
+
+    setup() {
+        this.state = useState({ isOpen: true });
+    }
+
+    toggleCard() {
+        this.state.isOpen = !this.state.isOpen;
+    }
 }
