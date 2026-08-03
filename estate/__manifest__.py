b985329d0c7832785e57b7e968196591fcbc359a
pryes-odoo <pryes@odoo.com>
2026-08-03 18:21:42 +0530

    [IMP] estate: improve offer evaluation
    Make the strongest buyer offer immediately visible
    
    Providing the best offer directly on the property record reduces the
    need to inspect every offer when evaluating a property

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -8,7 +8,7 @@
         "views/estate_property_views.xml",
         "views/estate_property_type.xml",
         "views/estate_property_tag.xml",
-        "views/estate_porperty_offer.xml",
+        "views/estate_property_offer.xml",
         "views/estate_menus.xml",
     ],
     "application": True,
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -53,8 +53,17 @@ class EstateProperty(models.Model):
     total_area = fields.Integer(
         compute="_compute_total_area", string="Total Area", store=True
     )
+    best_price = fields.Float(compute="_compute_best_price", string="Best Offer")
 
     @api.depends("living_area", "garden_area")
     def _compute_total_area(self):
         for record in self:
             record.total_area = record.living_area + record.garden_area
+
+    @api.depends("offer_ids.price")
+    def _compute_best_price(self):
+        for record in self:
+            if record.offer_ids:
+                record.best_price = max(record.offer_ids.mapped("price"))
+            else:
+                record.best_price = 0.0
