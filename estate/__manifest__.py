e63894215c960358748633f64f22d9ef79daa7b3
lokesh <losbo@odoo.com>
2026-08-04 15:07:22 +0530

    [IMP] estate: added the security csv file
    [IMP] estate: added spcaes after comma in estate_property.py file

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,7 +1,13 @@
 {
     'name': 'real estate',
+    'author': 'Lokesh',
+    'version': '1.0.0',
+    'license': 'LGPL-3',
     'depends': [
         'base'
     ],
     'application': True,
+    'data': [
+        'security/ir.model.access.csv',
+    ],
 }
