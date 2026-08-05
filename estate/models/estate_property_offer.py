a502ea964f7bdbdae24879ecc23f824ab37791d8
pryes-odoo <pryes@odoo.com>
2026-08-05 16:42:24 +0530

    [IMP] estate: manage offer validity
    Improve the management of property offers by tracking their validity
    
    Providing an offer deadline helps users identify when an offer expires
    while synchronizing it with the validity period keeps offer information
    consistent as business requirements change

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class estate_property_offer(models.Model):
@@ -11,3 +11,22 @@ class estate_property_offer(models.Model):
     )
     partner_id = fields.Many2one("res.partner", required=True)
     property_id = fields.Many2one("estate.property", required=True)
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(
+        compute="_compute_date_deadline", inverse="_inverse_date_deadline"
+    )
+
+    @api.depends("create_date", "validity")
+    def _compute_date_deadline(self):
+        for record in self:
+            create_date = record.create_date or fields.Date.today()
+            record.date_deadline = fields.Date.add(
+                fields.Date.to_date(create_date), days=record.validity
+            )
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            create_date = record.create_date or fields.Date.today()
+            record.validity = (
+                record.date_deadline - fields.Date.to_date(create_date)
+            ).days
