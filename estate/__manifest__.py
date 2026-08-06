6d4f2c95c52b6d4ceeed698e03eca339b97be98b
Gautam Kumar (gakum) <gakum@odoo.com>
2026-08-06 17:46:56 +0530

    [ADD] estate: added views

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,8 +4,11 @@
     'depends': ['base'],
     'data': [
         'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menu.xml'
     ],
     'application': True,
+    'installable': True,
     'author': 'Gautam',
     'license': 'LGPL-3',
 }
