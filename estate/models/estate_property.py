8a320188c2ec1bdaf80fa735f2f68387b7acb764
mrgor-odoo <mrgor@odoo.com>
2026-08-06 15:14:07 +0530

    [ADD] estate: compute total area
    The property total area should be derived automatically to ensure
    data consistency and avoid manual calculation.
    
    Added a computed field that calculates the total area from the
    living area and garden area, and displayed it in the property
    form view.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import api, fields, models
 
 
 class EstateProperty(models.Model):
@@ -47,3 +47,9 @@ class EstateProperty(models.Model):
         default="new",
     )
     tag_ids = fields.Many2many("estate.property.tag", string="Property Tags")
+    total_area = fields.Integer(compute="_compute_total_area")
+
+    @api.depends("living_area", "garden_area")
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
