e5b839ad7c07b564333d664e2d29f9639eaa9582
Kaushal Radadiya <karad@odoo.com>
2026-08-07 17:35:29 +0530

    [ADD] estate: Display property tags in Property list and form
    - Added list/form views  for property tags so users can manage  directly from
      the interface.
    - Display tag_id on property list and form  views using the many2many_tags widget
      so users can quickly see and assign multiple tags to a property.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -8,8 +8,9 @@
     "installable": True,
     "data": [
         "security/ir.model.access.csv",
-        "views/estate_property.xml",
+        "views/estate_property_tag.xml",
         "views/estate_property_type.xml",
+        "views/estate_property.xml",
         "views/estate_menus.xml",
     ],
     "depends": ["base"],
