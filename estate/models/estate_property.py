df3ac4e605cc9b66907ee3ebbccefc08a9b7861f
shadi-odoo <shadi@odoo.com>
2026-08-07 15:31:50 +0530

    [IMP] estate: manage property offers and status
    Add actions to manage the status of properties and their offers.
    
    Properties can now be marked as sold or cancelled, with checks to
    prevent invalid status changes. Offers can be accepted or refused,
    and accepting an offer automatically updates the property's buyer
    and selling price.
    
    Task: Completed Chapter 9

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,6 @@
 from odoo import fields, models, api
+from odoo.exceptions import UserError
+
 
 
 class EstateProperties(models.Model):
@@ -74,3 +76,15 @@ class EstateProperties(models.Model):
             else:
                 record.garden_area = 0
                 record.garden_orientation = False
+
+    def action_sold(self):
+        for record in self:
+            if record.state == "canceled":
+                raise UserError("Canceled properties cannot be sold.")
+            record.state = "sold"
+
+    def action_cancel(self):
+        for record in self:
+            if record.state == "sold":
+                raise UserError("Sold properties cannot be canceled.")
+            record.state = "canceled"
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -32,3 +32,14 @@ class EstatePropertyOffer(models.Model):
             record.validity = (
                 record.date_deadline - fields.Date.to_date(create_date)
             ).days
+
+    def action_accept(self):
+        for record in self:
+            record.status = "accepted"
+            record.property_id.selling_price = record.price
+            record.property_id.buyer = record.partner_id
+            record.property_id.state = "offer accepted"
+
+    def action_refuse(self):
+        for record in self:
+            record.status = "refused"
