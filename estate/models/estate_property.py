d9381b36d266f54a92a879495e226372684c3f82
Kaushal Radadiya <karad@odoo.com>
2026-08-11 16:34:53 +0530

    [ADD] estate: added computed total area field to property model
    - Added a computed total_area field to estate.property so users don't have to
    manually calculate the total area of a property.
    - We used @api.depends on living_area and garden_area to keep the calculation
    accurate in real-time.
    - Added the field to both the form and list views to show it in UI.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class EstateModel(models.Model):
@@ -12,11 +12,11 @@ class EstateModel(models.Model):
     expected_price = fields.Float(required=True)
     selling_price = fields.Float()
     bedrooms = fields.Integer()
-    living_area = fields.Integer()
+    living_area = fields.Integer(string="Living Area (sqm)")
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Integer()
+    garden_area = fields.Integer(string="Garden Area (sqm)")
 
     garden_orientation = fields.Selection(
         selection=[
@@ -48,3 +48,10 @@ class EstateModel(models.Model):
     buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
     tag_id = fields.Many2many("estate.property.tag", string="Property Tag")
     offer_ids = fields.One2many("estate.property.offer", "property_id")
+
+    total_area = fields.Integer(compute="_compute_total_area", string="Total Area (sqm)")
+
+    @api.depends('garden_area', 'living_area')
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.garden_area + record.living_area
