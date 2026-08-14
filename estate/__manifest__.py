4ddd84326bd88c0e386964b9bac0e49d107c7607
Marceline (matho) <matho@odoo.com>
2026-08-14 13:25:04 +0200

    [IMP] estate: chapter 2

---
+++ estate/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "My super Real Estate App",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "matho",
+    'description': """
+    Voici ma nouvelle application
+    """,
+    'application': True,
+    'installable': True,
+    # data files always loaded at installation
+    'data': [],
+    # data files containing optionally loaded demonstration data
+}
