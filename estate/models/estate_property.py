d88a5e6949beb8c33461603bb14f4afb2bf96b00
aski97 <schekedon@libero.it>
2026-08-17 14:31:20 +0200

    [ADD] estate: init models package and create estate property with basic fields

---
+++ estate/models/estate_property.py
@@ -0,0 +1,23 @@
+from odoo import models, fields
+
+class EstateProperty(models.Model):
+    _name = 'estate.property'
+    _description = 'Estate Property'
+
+    name = fields.Char('Property name', required=True, translate=True)
+    description = fields.Text('Property description', translate=True)
+    postcode = fields.Char('Property postcode')
+    date_availability = fields.Date('Date available')
+    expected_price = fields.Float('Expected price', required=True)
+    selling_price = fields.Float('Selling price')
+    bedrooms = fields.Integer('Bedrooms')
+    living_area = fields.Integer('Living area')
+    facades = fields.Integer('Facades')
+    garage = fields.Boolean('Has garage', default=False)
+    garden = fields.Boolean('Has garden', default=False)
+    garden_area = fields.Integer('Garden area')
+    garden_orientation = fields.Selection(
+        string='Garden orientation',
+        selection=[('north', 'North'),('south', 'South'),('east', 'East'),('west', 'West')],
+        help='Garden orientation is important for determining how much sunlight and warmth the outdoor space receives'
+    )
