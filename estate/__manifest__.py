00d7044f484fc125692f5deef0dcdff5bb604c32
Juan I. Casareski <juica@odoo.com>
2026-08-17 13:30:33 +0200

    [ADD] create new real estate module

---
+++ estate/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Real Estate',
+    'version': '0.0',
+    'depends': [
+        'base'
+    ],
+    'installable': True,
+    'application': True,
+    'author': 'juica',
+    'license': 'LGPL-3',
+}
