8bdc6504177f0120029d442b8981a76065037aea
Arturo Yepez <aryep@odoo.com>
2026-08-17 17:00:05 +0200

    [IMP] estate: update fields and create basic views for creation, manipulation
    Based on the tutorial's Chapter 5, several improvements were made to certain fields of the Estate Property model while also creating some customize basic views (as templates) to gain more control for future developments over the app

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -8,5 +8,8 @@
     "installable": True,
     "data": [
         'security/ir.model.access.csv',
+
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ]
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -7,10 +8,13 @@ class EstateProperty(models.Model):
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(string="Available From", default=fields.Date.today)
+    date_availability = fields.Date(
+        string="Available From", 
+        default=lambda _ : fields.Date.today() + relativedelta(months=3)
+    )
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -25,3 +29,18 @@ class EstateProperty(models.Model):
             ("west", "West"),
         ],
     )
+    state = fields.Selection(
+        string="Type of State",
+        selection=[
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled")
+        ],
+        default="new",
+        copy=False
+    )
+
+    # Reserved fields
+    active = fields.Boolean(default=True)
