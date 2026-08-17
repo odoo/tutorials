404841d0df3a3b0ad7ac45822bed8699f7593283
aski97 <schekedon@libero.it>
2026-08-17 13:20:55 +0200

    [ADD] estate: init the module

---
+++ estate/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Real Estate',
+    'version': '0.1',
+    'sequence': 100,
+    'summary': 'Real Estate Advertisement',
+    'depends': [
+        'base'
+    ],
+    'data': [
+
+    ],
+    'installable': True,
+    'application': True,
+    'assets': {},
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+}
