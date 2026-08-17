e87b554134689a1e96ff7bd64cb2f23db091b139
aski97 <schekedon@libero.it>
2026-08-17 16:31:39 +0200

    [CLN] estate: rename the strings of the estate property fields for a better comprehension.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,18 +5,18 @@ class EstateProperty(models.Model):
     _name = 'estate.property'
     _description = 'Estate Property'
 
-    name = fields.Char('Property name', required=True, translate=True)
-    description = fields.Text('Property description', translate=True)
-    postcode = fields.Char('Property postcode')
-    date_availability = fields.Date('Date available')
+    name = fields.Char('Title', required=True, translate=True)
+    description = fields.Text('Description', translate=True)
+    postcode = fields.Char('Postcode')
+    date_availability = fields.Date('Available From')
     expected_price = fields.Float('Expected price', required=True)
     selling_price = fields.Float('Selling price')
     bedrooms = fields.Integer('Bedrooms')
-    living_area = fields.Integer('Living area')
+    living_area = fields.Integer('Living area (sqm)')
     facades = fields.Integer('Facades')
-    garage = fields.Boolean('Has garage', default=False)
-    garden = fields.Boolean('Has garden', default=False)
-    garden_area = fields.Integer('Garden area')
+    garage = fields.Boolean('Garage', default=False)
+    garden = fields.Boolean('Garden', default=False)
+    garden_area = fields.Integer('Garden area (sqm)')
     garden_orientation = fields.Selection(
         string='Garden orientation',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
