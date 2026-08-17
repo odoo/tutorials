387119f46e4ec2868ad778721b1bf905e816e921
Emivvvvv <emtal@odoo.com>
2026-08-17 13:57:20 +0200

    [ADD] estate: init module

---
+++ estate/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Real Estate',
+    'version': '0.1',
+    'depends': [
+        'base',
+    ],
+    'installable': True,
+    'application': True,
+    'author': 'emtal',
+    'license': 'LGPL-3',
+}
