44209f16c72467917e34854493693c4d0f9ed15f
aaboe <aaboe@odoo.com>
2026-08-17 20:15:27 +0200

    [ADD] Chapter 2: A new Application

---
+++ estate/__manifest__.py
@@ -0,0 +1,12 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Estate',
+    'version': '0.1',
+    'sequence': 99,
+    'summary': 'Estate Management',
+    'depends': ['base', 'web'],
+    'installable': True,
+    'application': True,
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+}
