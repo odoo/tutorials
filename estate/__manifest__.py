cb84f6e0a1a9f3a55002923041fa0917000404f5
Kevin <kevin.heynickx@hotmail.be>
2026-08-17 15:55:26 +0200

    added all the functionality until the basic views of server framework

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,5 +3,13 @@
     'depends': [
         'base',
     ],
+    'data': [
+        'data/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'data/estate_menus.xml'
+    ],
+    'views': [
+        
+    ],
     'application':True
 }
---
+++ estate/models/estate_property.py
@@ -0,0 +1,30 @@
+from odoo import fields, models
+from dateutil.relativedelta import relativedelta
+
+class TestModel(models.Model):
+    _name = "estate_property"
+    _description = "test estate model"
+
+    name = fields.Char(required=True)
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        required=True,
+        copy=False,
+        default="new",
+        selection=[("new", "New"), ("offer received", "Offer Received"), ("offer accepted", "Offer Accepted"), ("sold", "Sold"), ("cancelled","Cancelled")]
+    )
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Datetime(copy=False, default=fields.Datetime.today() + (relativedelta(months=3)))
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        string='type',
+        selection=[('north', 'North'), ('south', 'South'), ('East', 'east'), ('West', 'west')]
+    )
