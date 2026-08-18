9bd8644d611b4fee48f715872964cc40be55faa3
Jozef Sivek <josiv@odoo.com>
2026-08-18 11:53:30 +0200

    [ADD] estate: add empty shell for 'estate' module
    Chapter 2

---
+++ estate/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "A Real Estade Advestisement Demo",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Jozef Sivek (josiv)",
+    'category': 'Sales',
+    'description': """
+    Test module for managing real estade advertisement
+    created as part of the onboarding.
+    """,
+    # data files always loaded at installation
+    'data': [
+        #'views/mymodule_view.xml',
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+        #'demo/demo_data.xml',
+    ],
+}
