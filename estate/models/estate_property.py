97e3e2d979c9f165b802842cefd505438f85b401
Yousef Allam <yalla@odoo.com>
2026-08-18 16:42:27 +0200

    [IMP] Chapter 7: Relations Between Models

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -31,3 +31,8 @@ class EstateProperty(models.Model):
         ('sold', 'Sold'),
         ('canceled', 'Canceled')
     ])
+    property_type_id = fields.Many2one('estate.property.type', string='Property Type')
+    user_id = fields.Many2one('res.users', string='Salesperson', default=lambda self: self.env.user)
+    buyer_id = fields.Many2one('res.partner', string='Buyer', copy=False)
+    tag_ids = fields.Many2many('estate.property.tag', string='Tags')
+    offer_ids = fields.One2many('estate.property.offer', 'property_id')
