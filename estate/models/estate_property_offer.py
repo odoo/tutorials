e4703e157e32e7c4c6e5c90654c5e2566663c6d2
Juan I. Casareski <juica@odoo.com>
2026-08-18 14:14:08 +0200

    [ADD] estate: offer validity days, & deadline date fields

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import api, fields, models
+from dateutil.relativedelta import relativedelta
 
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
@@ -16,3 +17,24 @@ class EstatePropertyOffer(models.Model):
         ]
     )
 
+    validity = fields.Integer(
+        "Validity (days)",
+        default=7,
+        required=True,
+    )
+    date_deadline = fields.Date(
+        "Deadline",
+        required=True,
+        compute="_compute_deadline",
+        inverse="_inverse_deadline"
+    )
+
+    @api.depends("validity")
+    def _compute_deadline(self):
+        for offer in self:
+            offer.date_deadline = fields.Date.today() + relativedelta(days=offer.validity)
+
+    def _inverse_deadline(self):
+        for offer in self:
+            delta = offer.date_deadline - fields.Date.today()
+            offer.validity = delta.days
