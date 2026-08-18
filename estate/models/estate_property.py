5ede1f32cf7d0018556cdc86b6d3d2edf7b664b0
Arturo Yepez <aryep@odoo.com>
2026-08-18 10:47:04 +0200

    [ADD] estate: add views for list, form and filters/groups
    Based on the tutorial's Chapter 6, new views where added to adjust to the app's purpose

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -15,7 +15,7 @@ class EstateProperty(models.Model):
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
+    living_area = fields.Integer(string="Living Area (sqm)")
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
