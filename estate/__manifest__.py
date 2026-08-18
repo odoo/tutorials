aac34e204987d3dce34dbf0f422c7e37907e1cd0
Jozef Sivek <josiv@odoo.com>
2026-08-18 17:17:59 +0200

    [IMP] estate: add broad access rights to estate.property model
    Chapter 4

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -10,6 +10,7 @@
     """,
     # data files always loaded at installation
     'data': [
+        'security/ir.model.access.csv',
         #'views/mymodule_view.xml',
     ],
     # data files containing optionally loaded demonstration data
