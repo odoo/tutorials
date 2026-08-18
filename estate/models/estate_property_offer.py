d0194c00ad4e7923559a000fad8a3806fad1fc90
aski97 <schekedon@libero.it>
2026-08-18 16:41:46 +0200

    [ADD] estate: add validity and date deadline fields in the property offers model and views (list and form).

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from datetime import date, timedelta
+from odoo import models, fields, api
 
 
 class EstatePropertyOffer(models.Model):
@@ -21,3 +22,29 @@ class EstatePropertyOffer(models.Model):
         string='Property',
         required=True,
     )
+    validity = fields.Integer(
+        string='Validity (days)',
+        default=7,
+        required=True,
+    )
+    # Computed Field
+    date_deadline = fields.Date(
+        string='Date of Deadline',
+        compute='_compute_date_deadline',
+        inverse='_inverse_date_deadline',
+    )
+
+    @api.depends('create_date', 'validity')
+    def _compute_date_deadline(self):
+        for record in self:
+            record.date_deadline = self._get_date_or_today(record.create_date) + timedelta(days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            create_date_or_today = self._get_date_or_today(record.create_date)
+            record.validity = (record.date_deadline - create_date_or_today).days
+
+    @staticmethod
+    def _get_date_or_today(datetime_to_evaluate):
+        """ Returns the date part of a given datetime if present, otherwise returns today's date """
+        return datetime_to_evaluate.date() if datetime_to_evaluate else date.today()
