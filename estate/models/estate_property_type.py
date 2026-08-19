662063a31804803f9c4f3973badf50488ee668d0
Juan I. Casareski <juica@odoo.com>
2026-08-19 14:06:42 +0200

    [ADD] estate: stat button on property types to offers

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class EstatePropertyType(models.Model):
@@ -10,6 +10,13 @@ class EstatePropertyType(models.Model):
     sequence = fields.Integer('Sequence', default=1)
 
     property_ids = fields.One2many("estate.property", "type_id")
+    offer_ids = fields.One2many("estate.property.offer", "property_type_id")
+    offer_count = fields.Integer(compute="_compute_offer_count", default=0)
+
+    api.depends("offer_ids")
+    def _compute_offer_count(self):
+        for offer in self:
+            offer.offer_count = len(offer.offer_ids)
 
     _uniq_name = models.Constraint(
         'UNIQUE(name)',
