28b3b079613806e42b0f2e9b80515d09af9a734c
Jozef Sivek <josiv@odoo.com>
2026-08-19 15:30:27 +0200

    [ADD] estate: add menus, default values, more fields
    Also fix errors on creation of the record.
    
    Chapter 5

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -8,10 +8,14 @@
     Test module for managing real estade advertisement
     created as part of the onboarding.
     """,
+    'license': 'LGPL-3',
     # data files always loaded at installation
     'data': [
         'security/ir.model.access.csv',
-        #'views/mymodule_view.xml',
+
+        'views/estate_property_views.xml',
+
+        'views/estate_menus.xml',
     ],
     # data files containing optionally loaded demonstration data
     'demo': [
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -4,15 +4,16 @@ from odoo import fields, models
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Estate property"
-    _order = "sequence"
 
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(
+            copy=False,
+            default=fields.Date.add(fields.Date.today(), months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -21,4 +22,14 @@ class EstateProperty(models.Model):
     garden_orientation = fields.Selection(
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
         help="garden orientation in respect to main compass directions")
-
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+            selection=[
+                ('new', 'New'),
+                ('offer_received', 'Offer Received'),
+                ('offer_accepted', 'Offer Accepted'),
+                ('sold', 'Sold'),
+                ('cancelled', 'Cancelled')],
+            default='new',
+            help="State of the estate property")
+        
