22c7f2192e6b039f58b841953a692f9cacf6c86f
Juan I. Casareski <juica@odoo.com>
2026-08-19 16:04:41 +0200

    [ADD] estate: override user view & add properties tab

--- estate/models/res_user.py
+++ estate/models/res_user.py
@@ -2,9 +2,9 @@ from odoo import fields, models
 
 
 class ResUser(models.Model):
-    _inherit = "res.user"
+    _inherit = "res.users"
 
     property_ids = fields.One2many(
         "estate.property", "salesperson_id",
-        domain="[('state', 'in', ['new', offer'])]"
+        domain="[('state', 'in', ['new', 'offer'])]"
     )
