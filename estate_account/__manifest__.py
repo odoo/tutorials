c802589ccac2d4a5ad402bf8eb721879219577df
Emivvvvv <emtal@odoo.com>
2026-08-19 15:55:33 +0200

    [ADD] estate_account: init module for linking esatate and invoicing

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Real Estate Account Link",
+    "version": "0.1",
+    "depends": [
+        "base",
+        "estate",
+        "account",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+    ],
+    "installable": True,
+    "application": True,
+    "author": "Odoo S.A.",
+    "license": "LGPL-3",
+}
