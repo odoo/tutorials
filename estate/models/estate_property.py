a0ae7a0567a53b4d0b3af5fc6a9c41c1a49c12f5
Jozef Sivek <josiv@odoo.com>
2026-08-19 17:36:39 +0200

    [ADD] estate: add search defaults (filters, group by, etc.)
    Chapter 6

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -9,16 +9,17 @@ class EstateProperty(models.Model):
     description = fields.Text()
     postcode = fields.Char()
     date_availability = fields.Date(
+            'Availale From',
             copy=False,
             default=fields.Date.add(fields.Date.today(), months=3))
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
+    living_area = fields.Integer('Living area (m²)')
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Integer()
+    garden_area = fields.Integer('Garden area (m²)')
     garden_orientation = fields.Selection(
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
         help="garden orientation in respect to main compass directions")
