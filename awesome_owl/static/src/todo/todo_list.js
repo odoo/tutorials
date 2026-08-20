d99f7c9613202417762fc6332b955887b0a0336b
Félicien (feraz) <feraz@odoo.com>
2026-08-20 17:28:48 +0200

    [IMP] awesome_owl: C1-9

--- awesome_owl/static/src/todo/todo_list.js
+++ awesome_owl/static/src/todo/todo_list.js
@@ -5,6 +5,15 @@ export class TodoList extends Component {
   static template = "awesome_owl.todo_list";
   static components = { TodoItem };
 
-  state = useState({ items: [{ id: 2, description: "buy brocolli", isCompleted: true }, { id: 3, description: "buy milk", isCompleted: false }] });
+  // { id: 2, description: "buy brocolli", isCompleted: true }
+  // { id: 3, description: "buy soy milk", isCompleted: false }
+  state = useState({ items: [], count: 0 });
+
+  addTodo(ev) {
+    if (ev.key === "Enter" && ev.target.value.trim() !== ""){
+      this.state.items.push({ 'id': this.state.count++, 'description': ev.target.value, 'isCompleted': false });
+      ev.target.value = ""
+    }
+  }
 
 }
