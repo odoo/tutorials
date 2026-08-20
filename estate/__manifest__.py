2f470023cc845c209d1b7697d969f7842d40c421
aski97 <schekedon@libero.it>
2026-08-20 13:35:04 +0200

    [IMP] estate: overwrite res users view form in order to add user's property sales

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -9,6 +9,7 @@
     'data': [
         'views/estate_property_views.xml',
         'views/estate_menus.xml',
+        'views/res_users_views.xml',
         'security/ir.model.access.csv'
     ],
     'installable': True,
---
+++ estate/models/res_users.py
@@ -0,0 +1,7 @@
+from odoo import models, fields, api
+
+
+class ResUsers(models.Model):
+    _inherit = ["res.users"]
+
+    property_ids = fields.One2many('estate.property', 'salesperson_id', string="Real Estate Properties", domain=[('status', 'in', ['new','offer_received'])])
