2042225fabe672f428c0ee9ea4bef3553f76b0d8
aski97 <schekedon@libero.it>
2026-08-20 14:07:13 +0200

    [ADD] estate account: init the new module

--- estate/models/res_users.py
+++ estate/models/res_users.py
@@ -2,6 +2,7 @@ from odoo import models, fields, api
 
 
 class ResUsers(models.Model):
+    _name = 'res.users'
     _inherit = ["res.users"]
 
     property_ids = fields.One2many('estate.property', 'salesperson_id', string="Real Estate Properties", domain=[('status', 'in', ['new','offer_received'])])
