0e68bf1dd514b0b5eee5fb0fd0102282e3d50e2f
Yousef Allam <yalla@odoo.com>
2026-08-20 16:54:42 +0200

    [IMP] Chapter 13: Interact With Other Modules

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "estate_account",
+    "version": "0.0",
+    "depends": ["estate", "account"],
+    "installable": True,
+    "application": True,
+    "author": "me",
+    "license": "LGPL-3",
+    "data": [
+        "security/ir.model.access.csv",
+    ],
+}
