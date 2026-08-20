25e48fae011f02a119c8b72204f3ddcddc461e6c
Juan I. Casareski <juica@odoo.com>
2026-08-20 10:23:08 +0200

    [REF] estate: merge `offer` & `received` property states into one

--- estate/models/res_user.py
+++ estate/models/res_user.py
@@ -6,5 +6,5 @@ class ResUser(models.Model):
 
     property_ids = fields.One2many(
         "estate.property", "salesperson_id",
-        domain="[('state', 'in', ['new', 'offer'])]"
+        domain="[('state', 'in', ['new', 'offer_received'])]"
     )
