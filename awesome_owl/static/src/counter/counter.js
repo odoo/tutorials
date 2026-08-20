74a5c6936e43b2d075eae7a74ab049af353d0df5
Félicien (feraz) <feraz@odoo.com>
2026-08-20 14:45:02 +0200

    [IMP] awesome_owl: C1-6

--- awesome_owl/static/src/counter/counter.js
+++ awesome_owl/static/src/counter/counter.js
@@ -2,10 +2,12 @@ import { Component, useState } from "@odoo/owl";
 
 export class Counter extends Component {
   static template = "awesome_owl.counter";
+  static props = ['onChange?']
 
   state = useState({ value: 0 });
 
   increment() {
     this.state.value++;
+    this.props.onChange?.();
   }
 }
--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,4 +1,4 @@
-import { Component, markup } from "@odoo/owl";
+import { Component, markup, useState } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
 
@@ -6,6 +6,12 @@ export class Playground extends Component {
   static template = "awesome_owl.playground";
   static components = { Counter, Card };
 
+  state = useState({ value: 0 });
+
   card1Content = '<div class="text-primary">some content 1</div>';
   card2Content = markup('<div class="text-primary">some content 2</div>');
+
+  incrementSum() {
+    this.state.value++;
+  }
 }
