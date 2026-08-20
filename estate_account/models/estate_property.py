c3c4378cdcc197ce711af5c306d806fbe9835715
Kevin <kevin.heynickx@hotmail.be>
2026-08-20 10:19:34 +0200

    finished chapter server framework chapter 13

---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,27 @@
+from odoo import models, Command
+
+class InheritedModel(models.Model):
+    _inherit = "estate.property"
+
+    def action_property_sold(self):
+        for record in self:
+            res = super().action_property_sold()
+            self.env["account.move"].sudo().create(
+                {
+                    "partner_id": record.buyer_id.partner_id.id,
+                    "move_type": "out_invoice",
+                    "invoice_line_ids": [
+                        Command.create({
+                            "name": "six percent charge",
+                            "quantity": "1",
+                            "price_unit": record.selling_price * 0.06
+                        }),
+                        Command.create({
+                            "name": "administration fee",
+                            "quantity": "1",
+                            "price_unit": 100.0
+                        }),
+                    ],
+                }
+            )
+        return res
