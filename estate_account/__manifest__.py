94b7ef5e596ed682b537ed6990ba8597e0801071
Juan I. Casareski <juica@odoo.com>
2026-08-20 09:34:52 +0200

    [ADD] estate: create invoice on property sold

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Real Estate Accounting',
+    'version': '0.0',
+    'depends': [
+        'base',
+        'estate',
+        'account'
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+    'author': 'juica',
+    'license': 'LGPL-3',
+}
---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,27 @@
+from odoo import models, Command
+
+
+class InheritedModel(models.Model):
+    _inherit = "estate.property"
+
+    def action_sold(self):
+        for property in self:
+            move = self.env['account.move'].create({
+                'partner_id': property.buyer_id.id,
+                'move_type': 'out_invoice',
+                'line_ids': [
+                    Command.create({
+                        'name': f"Property: {property.name}", # Prob should use _(%d) for translating, but its just a demo ¯\_(ツ)_/¯ 
+                        'quantity': 1,
+                        'price_unit': .06 *  property.selling_price,
+                    }),
+                    Command.create({
+                        'name': "Administrative fees",
+                        'quantity': 1,
+                        'price_unit': 100,
+                    })
+                ]
+            })
+
+
+        return super().action_sold()
