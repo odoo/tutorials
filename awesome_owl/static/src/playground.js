bbb218c81daad8f565b16e6d5e8f027b83de369d
Félicien (feraz) <feraz@odoo.com>
2026-08-20 14:07:47 +0200

    [IMP] awesome_owl: C1-4

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,8 +1,11 @@
-import { Component } from "@odoo/owl";
+import { Component, markup } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
 
 export class Playground extends Component {
   static template = "awesome_owl.playground";
   static components = { Counter, Card };
+
+  card1Content = '<div class="text-primary">some content 1</div>';
+  card2Content = markup('<div class="text-primary">some content 2</div>');
 }
