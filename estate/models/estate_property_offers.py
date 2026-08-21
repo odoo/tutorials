2579a4b4612e3bf3569815f075e14502d85cfb00
ancha-odoo <ancha@odoo.com>
2026-08-21 16:29:12 +0530

    [IMP] estate: complete chapter 11 sprinkles chapter
    * Implement the remaining improvements from the Sprinkles chapter.
    * Add required UI and functional enhancements.
    * Polish the Estate module for better usability and consistency.

--- estate/models/estate_property_offers.py
+++ estate/models/estate_property_offers.py
@@ -6,7 +6,7 @@ from odoo import models, fields, api
 class EstatePropertyOffers(models.Model):
     _name = "estate.property.offers"
     _description = "Property Offers"
-
+    _order = "price desc"
     price = fields.Float(string="Price")
     status = fields.Selection(
         [("accepted", "Accepted"), ("refused", "Refused")],
@@ -16,6 +16,9 @@ class EstatePropertyOffers(models.Model):
     partner_id = fields.Many2one("res.partner", string="Customer", required=True)
     property_id = fields.Many2one("estate.property", string="Property", required=True)
     validity = fields.Integer(string="Validity", default=7)
+    property_type_id = fields.Many2one(
+        "estate.property.type", compute="_compute_property_type_id", store=True
+    )
     date_deadline = fields.Date(
         string="Date Deadline",
         compute="_compute_date_deadline",
@@ -24,6 +27,11 @@ class EstatePropertyOffers(models.Model):
     _check_offer_price = models.Constraint(
         "CHECK(price > 0)", "Offer price must be positive."
     )
+
+    @api.depends("property_id.property_type_id")
+    def _compute_property_type_id(self):
+        for record in self:
+            record.property_type_id = record.property_id.property_type_id
 
     @api.depends("create_date", "validity")
     def _compute_date_deadline(self):
@@ -43,9 +51,30 @@ class EstatePropertyOffers(models.Model):
 
     def set_accepted(self):
         for record in self:
+            # if(record.property_id.selling_price):
+            #     raise ValidationError("only one offer can be accepted")
             record.status = "accepted"
+            record.property_id.state = "offer_accepted"
             record.property_id.selling_price = record.price
             record.property_id.buyer_id.name = record.partner_id.name
+
+            # for offer in record.property_id.offer_ids:
+            #     if(offer.status != "accepted"):
+            #         offer.status = 'refused'
+
+            # 2nd Approach
+
+            # record.property_id.offer_ids.filtered(
+            #     lambda offer: offer.status != "accepted"
+            # ).status = "refused"
+
+            # 3rd Approach
+
+            (record.property_id.offer_ids - record).status = "refused"
+
+            # not_accepted= record.property_id.offer_ids-record
+            # for record1 in not_accepted:
+            #     record1.status='refused'
         return True
 
     def set_refused(self):
