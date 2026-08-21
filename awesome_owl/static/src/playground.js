7ae8cc99e87526160acc06206ca81a2dd12e839e
Vincent Vallaeys <vincent@vallaeys.com>
2026-08-21 13:34:50 +0200

    [IMP] awesome_owl: Chapter 1

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,5 +1,18 @@
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
+import { Card } from "./card/card";
+import { Counter } from "./counter/counter";
+import { TodoList } from "./todolist/TodoList";
 
 export class Playground extends Component {
-    static template = "awesome_owl.playground";
+  static template = "awesome_owl.playground";
+
+  static components = { Counter, Card, TodoList };
+
+  setup() {
+    this.sum = useState({ value: 0 })
+  }
+
+  incrementSum() {
+    this.sum.value++
+  }
 }
