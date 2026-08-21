d5e25b3cefbc6cb8a0fcfc14aa9ba974286084a7
Kevin <kevin.heynickx@hotmail.be>
2026-08-21 14:34:58 +0200

    [ADD] added buttons and counters
    added a component containing a button and a counter such that the button increments the counter.
    also added a component which creates its own children buttons such that it's possible to have a global counter of all the children buttons.

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,5 +1,18 @@
-import { Component } from "@odoo/owl";
+import { Component, useState, markup } from "@odoo/owl";
+import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
+import { GlobalCounter } from "./global_counter/global_counter";
 
 export class Playground extends Component {
+    setup(){
+        this.normal_string = "normal string"
+        this.html_string = markup("<a href=https://www.w3schools.com>Visit W3Schools.com!</a>")
+    }
+
     static template = "awesome_owl.playground";
+    static components = { 
+        Counter,
+        Card,
+        GlobalCounter
+    }
 }
