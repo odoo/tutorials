4ffc2d5e6f986d1a5a185313f31466338b75eab1
Arturo Yepez <aryep@odoo.com>
2026-08-24 11:48:55 +0200

    [IMP] awesome_owl: extract logic into its own sub-component

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,8 +1,10 @@
 import { Component, useState } from "@odoo/owl";
 
+import { Counter } from "./components/counter/counter";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
+    static components = { Counter };
 
     counter = useState({ value: 0 });
 
