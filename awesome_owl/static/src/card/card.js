218074a1896b02b3ececd271ecb95f00c03995c7
Félicien (feraz) <feraz@odoo.com>
2026-08-24 10:33:57 +0200

    [IMP] awesome_owl: C1-13

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -2,5 +2,5 @@ import { Component } from "@odoo/owl";
 
 export class Card extends Component {
   static template = "awesome_owl.card";
-  static props = ['title?', 'content']
+  static props = ['title?', 'slots']
 }
