178f166827a1d63d24b602c1b7e0ba02cb4093aa
Arturo Yepez <aryep@odoo.com>
2026-08-24 11:44:04 +0200

    [IMP] awesome_owl: displaying the counter

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,5 +1,12 @@
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
+
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
+
+    counter = useState({ value: 0 });
+
+    increment() {
+        this.counter.value += 1
+    }
 }
