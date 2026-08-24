a0dc999750e3e4f0e518d6739e5dea75742bc9df
aski97 <schekedon@libero.it>
2026-08-24 13:43:00 +0200

    [IMP] awesome_owl: add dynamic compute of the sum of one or multiple counters (controlled by the parent)

--- awesome_owl/static/src/counter/counter.js
+++ awesome_owl/static/src/counter/counter.js
@@ -3,11 +3,25 @@ import { Component, useState } from "@odoo/owl";
 export class Counter extends Component {
     static template = "awesome_owl.counter";
 
+    static props = {
+        initialValue: { type: Number, optional: true},
+        onChange: { type: Function, optional: true },
+        onRegister: { type: Function, optional: true },
+    };
+
     setup() {
-        this.state = useState({ value: 0});
+        this.state = useState({ value: this.props.initialValue ?? 0});
+
+        if (this.props.onRegister) {
+            this.props.onRegister(this)
+        }
     }
 
     increment() {
         this.state.value++;
+
+        if (this.props.onChange) {
+            this.props.onChange();
+        }
     }
 }
--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -7,4 +7,26 @@ export class Playground extends Component {
 
     static components = { Counter, Card};
 
+    setup() {
+        /*
+        I define the counters here so I can show them as much as I want
+        */
+        this.state = useState({
+            counters_num: 2,
+            counters_ref: []
+        });
+    }
+
+    // It registry the reference of the child here
+    registerCounter(id, counterInstance) {
+        this.state.counters_ref[id] = counterInstance;
+    }
+
+    get sum() {
+        return this.state.counters_ref.reduce(
+            (acc, counter) => acc + (counter?.state?.value || 0),
+            0
+        )
+    }
+
 }
