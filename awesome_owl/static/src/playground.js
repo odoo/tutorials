640d2697319d9a05e25bbfc6e483a6a93997a85f
Arturo Yepez <aryep@odoo.com>
2026-08-24 15:24:07 +0200

    [IMP] awesome_owl: new component for card
    A new component for a Card, with its own props was created

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,10 +1,11 @@
 import { Component, useState } from "@odoo/owl";
 
 import { Counter } from "./components/counter/counter";
+import { Card } from "./components/card/card"
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
-    static components = { Counter };
+    static components = { Counter, Card };
 
     counter = useState({ value: 0 });
 
