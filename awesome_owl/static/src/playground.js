86cb57a7c14ecc67dec3d8a202e9fdf7e09e6ddd
Arturo Yepez <aryep@odoo.com>
2026-08-25 11:06:27 +0200

    [IMP] awesome_owl: new todo component
    Per Chapter 1, section 6 of the Web Framework tutorial

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -2,10 +2,11 @@ import { Component, useState, markup } from "@odoo/owl";
 
 import { Counter } from "./components/counter/counter";
 import { Card } from "./components/card/card"
+import { TodoList } from "./components/todo_list/todo_list";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
-    static components = { Counter, Card };
+    static components = { Counter, Card, TodoList };
 
     sum = useState({ value: 0 })
 
