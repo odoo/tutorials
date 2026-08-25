f9dacc6fb1844f50b8e688ebcfa5af7d2ec96913
Arturo Yepez <aryep@odoo.com>
2026-08-25 16:59:28 +0200

    [IMP] awesome_owl: new behavior to minimize card content
    Per Chapter 1, section 14 of the Web Framework tutorial

--- awesome_owl/static/src/components/card/card.js
+++ awesome_owl/static/src/components/card/card.js
@@ -3,5 +3,12 @@ import { Component, useState } from "@odoo/owl";
 export class Card extends Component {
     static template = "awesome_owl.card";
     static props = ["title", "slots?"]
+
+    setup() {
+        this.open = useState({ value: true });
+    }
     
+    toggleOpen() {
+        this.open.value = !this.open.value
+    }
 }
