c4ad164c7812deae8f555cb72996861d0e43fe3a
aski97 <schekedon@libero.it>
2026-08-25 17:26:50 +0200

    [IMP] awesome_dashboard: add basic layout to the dashboard with two buttons.

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,8 +1,40 @@
 import { Component } from "@odoo/owl";
+import { _t } from "@web/core/l10n/translation";
+import { useService } from "@web/core/utils/hooks";
+import { Layout } from "@web/search/layout";
 import { registry } from "@web/core/registry";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
+     static components = {
+        Layout
+     };
+
+     setup() {
+        this.display = {
+            controlPanel: {},
+        };
+        this.action = useService("action");
+    }
+
+    openCustomers() {
+         this.action.doAction("base.action_partner_form", {
+             viewType: "kanban"
+         });
+    }
+
+    openLeads() {
+         this.action.doAction({
+            type: 'ir.actions.act_window',
+            name: _t('Leads'),
+            target: 'new',
+            res_model: "crm.lead",
+            views: [
+                [false, "list"],
+                [false, "form"]
+            ],
+        });
+    }
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
