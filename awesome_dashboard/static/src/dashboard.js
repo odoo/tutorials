3cab36c2ba6168d52acb00de3507fa4cb01e1885
Arturo Yepez <aryep@odoo.com>
2026-08-26 15:17:07 +0200

    [IMP] awesome_dashboard: new actions for the dashboard layout"
    Per Chapter 2, section 2 of the Web Framework tutorial

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,10 +1,34 @@
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";
+import { _t } from "@web/core/l10n/translation";
 import { Layout } from "@web/search/layout"
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
     static components = { Layout };
+
+    setup() {
+        this.action = useService("action");
+    }
+
+    openCustomers() {
+        this.action.doAction("base.action_partner_form");
+    }
+
+    async openLeads(activity) {
+        /* Dynamic action to open Leads with only list and form view */
+
+        this.action.doAction({
+            type: "ir.actions.act_window",
+            name: "All leads",
+            res_model: "crm.lead",
+            views: [
+                [false, "list"],
+                [false, "form"],
+            ],
+        });
+    }
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
