90db329a055c59b796aeb56463333d06e38b3021
Harsh Gavit (hagav) <hagav@odoo.com>
2026-08-31 17:07:42 +0530

    [IMP] estate: add manual list ordering for estate types
    - Added sequence Integer field on estate.type
    - Ordered types by sequence, then name
    - Added list view with handle widget for drag-and-drop reorder
    
    CH-11, EX-4

--- estate/models/estate_type.py
+++ estate/models/estate_type.py
@@ -4,9 +4,10 @@ from odoo import models, fields
 class EstateType(models.Model):
     _name = 'estate.type'
     _description = 'Estate Type'
-    _order = 'name'
+    _order = 'sequence, name'
 
     name = fields.Char(string='Name', required=True)
+    sequence = fields.Integer(default=1)
     property_ids = fields.One2many('estate', 'estate_type_id', string='Properties')
 
     _name_uniq = models.Constraint(
