1942415ed0dc1d93fb1510c82d705ffb305b93c8
Mauryan Kansara <mauryankansaramk@gmail.com>
2026-08-31 18:29:54 +0530

    [IMP] estate: display properties by type
    Allow users to see the properties associated with a property type
    directly from its form.
    
    Provide an overview of the properties belonging to each type, making it
    easier to review and manage related properties without leaving the
    property type record.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -10,6 +10,7 @@
     "data": [
         "security/ir.model.access.csv",
         "views/estate_property_offer.xml",
+        "views/estate_property_type.xml",
         "views/estate_property_search.xml",
         "views/estate_property_views.xml",
         "views/estate_menus.xml",
--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import fields, models
 
 
 class EstatePropertyType(models.Model):
@@ -6,7 +6,7 @@ class EstatePropertyType(models.Model):
     _description = "Estate Property Types"
 
     name = fields.Char("Property Type", required=True)
-
+    property_id = fields.One2many("estate.property", "property_type_id", string="Properties")
     _check_type_name = models.Constraint(
         'UNIQUE(name)',
         'The type name should be unique.',
