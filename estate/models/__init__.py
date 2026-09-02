9e3de6790a7d892a1b43aa855bd664c392592a3f
lokesh <losbo@odoo.com>
2026-09-02 11:23:27 +0530

    [IMP] estate: added estate_property_type table and connected the two table (estate_property and estate_property_type) using Many2one
    - Added different relation fields like Many2One, Many2Many and One2Many to connect two tables
    - Removed unnecessary lines of code
    - Update the 'estate_property_views.xml' to improve the UI

--- estate/models/__init__.py
+++ estate/models/__init__.py
@@ -1,1 +1,1 @@
-from . import estate_property
+from . import estate_property, estate_property_type
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,16 +1,30 @@
+from datetime import datetime
+
 from odoo import fields, models
 
 
 class TestModel(models.Model):
+    today = datetime.now()
+
+    month = today.month + 3
+    year = today.year
+    day = today.day
+
+    if month > 12:
+        year = year + 1
+        month = month % 12
+
+    three_month_date = today.replace(year=year, month=month, day=day)
+
     _name = "estate.property"
     _description = "This is a dummy table"
 
-    name = fields.Char(required=True)
+    name = fields.Char(translate=True, default="Unknown", required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=three_month_date.date())
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
+    selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer()
     living_area = fields.Integer()
     facades = fields.Integer()
@@ -27,12 +41,12 @@ class TestModel(models.Model):
         string="Direction",
         default="north",
     )
-    active = fields.Boolean("active", default=True)
+    active = fields.Boolean("Active", default=True)
+    property_type_id = fields.Many2one("estate.property.type", string="Property_type")
 
     def action_confirm(self):
-        print("button is clicked")
         return {
-            "type": "ir.actions.act_window",
+            "type": "ir.actions.act_window",  # type and name are very important
             "name": "Properties",
             "res_model": "estate.property",
             "view_mode": "form",
