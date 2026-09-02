8070edb54b2ffce3342de25349b24e4b805bc2fe
lokesh <losbo@odoo.com>
2026-09-02 11:23:27 +0530

    [IMP] estate: added estate_property_type table and connected the two table (estate_property and estate_property_type) using Many2one

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,12 +3,19 @@
     "author": "Lokesh",
     "version": "1.0.0",
     "license": "LGPL-3",
-    "depends": ["base"],
+    # "depends": ["base"],
     "application": True,
+    "installable": True,
     "data": [
+        "security/securities.xml",
         "security/ir.model.access.csv",
-        "views/estate_property_actions.xml",  # the order should be like this, first we need to define the action and then we need to define the menu and then the views if the order is not maintained then error will be thrown.
         "views/estate_property_views.xml",
+        "views/estate_property_actions.xml",  # the order should be like this, first we need to define the views and then we need to define the action and then the menu if the order is not maintained then error will be thrown.
         "views/estate_property_menu.xml",
     ],
+    "assets": {
+        "web.assets_backend": [
+            "estate/static/src/estate.css",
+        ],
+    },
 }
