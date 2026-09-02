9aee6a2a36012e5843e26ff36784f9e7792cbec1
lokesh <losbo@odoo.com>
2026-09-02 15:22:46 +0530

    [IMP] estate: added property_type_id in the view xml
    - Added the 'estate_property_type.py' model and used this in the 'estate_property_views.xml'

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -9,8 +9,8 @@
     "data": [
         "security/securities.xml",
         "security/ir.model.access.csv",
-        "views/estate_property_views.xml",
-        "views/estate_property_actions.xml",  # the order should be like this, first we need to define the views and then we need to define the action and then the menu if the order is not maintained then error will be thrown.
+        "views/estate_property_views.xml",  # the order should be like this, first we need to define the views and then we need to define the action and then the menu if the order is not maintained then error will be thrown.
+        "views/estate_property_type_view.xml",
         "views/estate_property_menu.xml",
     ],
     "assets": {
--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -5,4 +5,4 @@ class PropertyType(models.Model):
     _name = "estate.property.type"
     _description = "this is used to define the types"
 
-    name = fields.Char(required=True)
+    name = fields.Char(required=True, string="Type")
