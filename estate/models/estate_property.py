4504a5e9ce43a93f932f821de454ab126e66f7f7
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2026-09-04 18:58:00 +0530

    [ADD] estate: estate_property_type and view of estate_ property_type added
    Extend the estate.property model by introducing Many2one links for
    Property Type, Buyer,Salesperson.
    
    Link Property Type with estate.property.type
    Link Buyer with res.partner
    Link Salesperson with res.users
    Create the required property type model
    Configure appropriate access permissions
    Add menus for managing property types
    Update the property form view to display and showcase the new relational fields

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from datetime import timedelta
+
 from odoo import fields, models
 
 
@@ -44,3 +45,6 @@ class EstateProperty(models.Model):
         required=True,
         copy=False
     )
+    property_type_id = fields.Many2one("estate.property.type", string='Property Type')
+    buyer_id = fields.Many2one('res.partner', string='Buyer')
+    salesperson_id = fields.Many2one('res.users', string='Salesperson', default=lambda self: self.env.user)
