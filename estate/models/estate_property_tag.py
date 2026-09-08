de09a18ced8d8c28985d0245bfd36df79d8c98fd
Mauryan Kansara <mauryankansaramk@gmail.com>
2026-09-08 18:34:28 +0530

    [IMP] estate: improve property form usability
    Restrict users from creating or editing property types
    directly from the property form, ensuring that property types
    are managed exclusively from the Property Types form.
    
    Add color support for property tags so users can
    visually distinguish and organize properties more easily.

--- estate/models/estate_property_tag.py
+++ estate/models/estate_property_tag.py
@@ -6,6 +6,7 @@ class EstatePropertyTag(models.Model):
     _description = "Estate Property Tags"
 
     name = fields.Char("Name", required=True)
+    color = fields.Integer("Color")
     _order = "name asc"
 
     _check_tag_name = models.Constraint(
