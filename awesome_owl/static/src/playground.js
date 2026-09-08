cd444edceb477aa82328194ff01f34c619edba50
dheer <dhsri@odoo.com>
2026-09-08 18:49:59 +0530

    [IMP] awesome_owl: add Counter and Card components with props
    Implement Counter and Card components with props, callbacks, and safe
    
    HTML rendering using useState, t-out, and markup directives.

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,5 +1,18 @@
-import { Component } from "@odoo/owl";
+import { Component, markup, useState } from "@odoo/owl";
+import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
+    static components = { Counter, Card };
+
+    setup() {
+        this.state = useState({ sum: 2 });
+        this.html1 = "<div>some content</div>";
+        this.html2 = markup("<div>some content</div>");
+    }
+
+    incrementSum() {
+        this.state.sum++;
+    }
 }
