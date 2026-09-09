262cc2013a259cbf3213f0caca532a606a21d04a
Gautam Kumar (gakum) <gakum@odoo.com>
2026-09-09 12:54:41 +0530

    [ADD] estate:Added estate property type xml view

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,15 +1,21 @@
+
 {
-    'name': 'real state',
-    'description':'This is my first module estate',
-    'version': '1.0',
-    'depends': ['base'],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/estate_property_views.xml',
-        'views/estate_menu.xml'
+    "name": "real state",
+    "description": "This is my first module estate",
+    "version": "1.0",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_property_type_view.xml",
+        "views/estate_menu.xml",
     ],
-    'application': True,
-    'installable': True,
-    'author': 'Gautam',
-    'license': 'LGPL-3',
+    "demo": [
+        "demo/demo_estate_property.xml",
+    ],
+    "application": True,
+    "installable": True,
+    "author": "Gautam",
+    "license": "LGPL-3",
+    "auto_install": True,
 }
