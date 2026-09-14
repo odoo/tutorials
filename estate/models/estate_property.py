43e8500ba7e9a492ca4b6844a6f4fef55e181297
Mauryan Kansara <mauryankansaramk@gmail.com>
2026-09-14 10:49:18 +0530

    [FIX] estate: replace UserError with ValidationError
    Replaced UserError with ValidationError to properly distiguinsh the
    error and it's type

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -108,6 +108,6 @@ class EstateProperty(models.Model):
             if float_utils.float_compare(
                     record.selling_price, record.expected_price * 0.9, precision_digits=2
             ) < 0:
-                raise exceptions.UserError(_(
+                raise exceptions.ValidationError(_(
                     "The selling price cannot be lower than 90% of the expected price."
                 ))
