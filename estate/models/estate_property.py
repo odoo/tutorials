175d21c26e810b32be5758bce6f7cca3685186f9
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-09-15 15:14:46 +0200

    [IMP] estate : add model field

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,8 +1,28 @@
-from odoo import models, fields
+from odoo import fields, models
 
 
 class Property(models.Model):
-    _name = "property"
+    _name = "estate_property"
     _description = "Estate Property"
 
-    name = fields.Char()
+    name = fields.Char(required=True)
+    description = fields.Text
+    date_availability = fields.Date
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float
+    bedrooms = fields.Integer
+    living_area = fields.Integer
+    facades = fields.Integer
+    garage = fields.Boolean
+    garen = fields.Boolean
+    garden_area = fields.Integer
+    garden_orientation = fields.Selection(
+        string="Orientation",
+        selection=[
+            ("north", "North"),
+            ("south", "South"),
+            ("east", "East"),
+            ("west", "West"),
+        ],
+        help="The garden orientation",
+    )
