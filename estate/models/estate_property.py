23a4825fd0580f62458c1c004cb7f01b0468ac11
macau <macau@odoo.com>
2026-09-15 11:58:01 +0200

    [IMP] estate: End of chapter 3

---
+++ estate/models/estate_property.py
@@ -0,0 +1,19 @@
+from odoo import fields, models
+
+class EstateProperty(models.Model):
+    _name = 'estate.property'
+    _description = 'Estate Property'
+
+    name = fields.Char(string='Name', required=True)
+    description = fields.Text('Description')
+    postcode = fields.Char('Postcode')
+    date_availability = fields.Date('Date availability')
+    expected_price = fields.Float(string='Expected price', required=True)
+    selling_price = fields.Float('Selling price')
+    bedrooms = fields.Integer('Bedrooms')
+    living_area = fields.Integer('Living area')
+    facades = fields.Integer('Facades')
+    garage = fields.Boolean('Garage')
+    garden = fields.Boolean('Garden')
+    garden_area = fields.Integer('Garden area')
+    garden_orientation = fields.Selection(string='Garden orientation', selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
