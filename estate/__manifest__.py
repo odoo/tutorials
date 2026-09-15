84e33608d81a7e22d8aa3d56e60a976df5ff48f7
cailleaux-maxime <macai@odoo.com>
2026-09-15 16:44:56 +0200

    [IMP] estate: add title to list view
    Feature based on chapter 6 of the training

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,10 +1,10 @@
 {
-    'name':'Real Estate',
-    'version':'1.0',
-    'depends':['base'],
-    'application':True,
-    'installable':True,
-    'data':[
+    'name': 'Real Estate',
+    'version': '1.0',
+    'depends': ['base'],
+    'application': True,
+    'installable': True,
+    'data': [
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_menu.xml',
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,13 +5,13 @@ class EstateProperty(models.Model):
     _name = 'estate.property'
     _description = 'Real Estate property model'
 
-    name = fields.Char('Name',required=True)
+    name = fields.Char('Name', required=True)
     description = fields.Text('Description')
-    postcode = fields.Char('Postcode',required=True)
-    date_availability = fields.Date('Availability',copy=False,default=fields.Date.add(fields.Date.today(),months=3))
-    expected_price = fields.Float('Expected Price',required=True)
-    selling_price = fields.Float('Selling Price',readonly=True,copy=False)
-    bedrooms = fields.Integer('Bedrooms',default=2)
+    postcode = fields.Char('Postcode', required=True)
+    date_availability = fields.Date('Availability', copy=False, default=fields.Date.add(fields.Date.today(), months=3))
+    expected_price = fields.Float('Expected Price', required=True)
+    selling_price = fields.Float('Selling Price', readonly=True, copy=False)
+    bedrooms = fields.Integer('Bedrooms', default=2)
     living_area = fields.Integer('Living Area')
     facades = fields.Integer('Facades')
     garage = fields.Boolean('Garage')
@@ -19,20 +19,20 @@ class EstateProperty(models.Model):
     garden_area = fields.Integer('Garden Area')
     garden_orientation = fields.Selection(
         string='Garden Orientation',
-        selection=[('north','North'),('south','South'),('east','East'),('west','West')]
+        selection=[('north', 'North'),
+                   ('south', 'South'),
+                   ('east', 'East'),
+                   ('west', 'West')]
     )
-    active = fields.Boolean('Active',default=True)
+    active = fields.Boolean('Active', default=True)
     state = fields.Selection(
         string='State',
-        selection=[('new','New'),
-                   ('offer_received','Offer Received'),
-                   ('offer_accepted','Offer accepted'),
-                   ('sold','Sold'),
-                   ('cancelled','Cancelled')],
+        selection=[('new', 'New'),
+                   ('offer_received', 'Offer Received'),
+                   ('offer_accepted', 'Offer accepted'),
+                   ('sold', 'Sold'),
+                   ('cancelled', 'Cancelled')],
         required=True,
         copy=False,
         default='New'
     )
-
-
-
