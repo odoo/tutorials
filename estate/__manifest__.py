583577d14e2d5fce88aa9540d52bdefff72c2c69
Bryce (brbu) <brbu@odoo.com>
2026-09-15 15:57:04 +0200

    [ADD] First commit with chapters 1 to 5

---
+++ estate/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'My Estate',
+    'version': '1.9',
+    'summary': 'Test module to remember how it works mdr',
+    'website': 'https://www.odoo.com/app/estate',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'author': 'brbu',
+}
---
+++ estate/models/estate_property.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields
+
+
+class EstateProperty(models.Model):
+    _name = "estate_property"
+    _description = "Properties of the estate"
+
+    name = fields.Char(required=True, string="Title")
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(copy=False, default=lambda self: fields.Date.add(fields.Date.today(), months=3))
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer()
+    facade = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        string="Garden Orientation",
+        selection=[("north", "North"), ("south", "South"), ("east", "East"), ("west", "West")],
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        string="Status",
+        selection=[("new","New"), ("offer-received","Offer Received"), ("offer-accepted","Offer Accepted"), ("sold","Sold"), ("canceled","Canceled")],
+        required=True,
+        copy=False,
+        default="new"
+    )
