b365a35e9de8ac08349ce84d2350e16705e9d4dd
Olivier Le Thanh Duong <ollet@odoo.com>
2026-09-15 15:54:52 +0200

    [FIX] estate Property set active by default

--- estate/models/property.py
+++ estate/models/property.py
@@ -20,7 +20,7 @@ class Property(models.Model):
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
         selection=(('north', 'North'), ('east', 'East'), ('south', 'South'), ('west', 'West')))
-    active = fields.Boolean()
+    active = fields.Boolean(default=True)
     state = fields.Selection(default='new', required=True,
                              selection=(('new', 'New'), ('offer_received', 'Offer Received'),
                                         ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'),
