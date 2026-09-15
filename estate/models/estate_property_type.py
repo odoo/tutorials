6ea3b644147c3914b62a618358abb481077982bb
mrgor-odoo <mrgor@odoo.com>
2026-09-15 15:51:23 +0530

    [ADD] estate: add properties to property type
    Link properties to property types and show them in the property
    type form with an inline list.

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -6,6 +6,10 @@ class EstatePropertyType(models.Model):
     _description = "Property Type"
 
     name = fields.Char(required=True)
+    property_ids = fields.One2many(
+        'estate.property',
+        'property_type_id'
+    )
 
     _name_unique = models.Constraint(
         'UNIQUE(name)',
