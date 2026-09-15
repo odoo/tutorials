8142e1b7b18dc4cd020ef8bcd709088d34bb1e35
Alexandre Navez <alex.navez007@gmail.com>
2026-09-15 16:54:33 +0200

    [ADD] estate: added Property model action and menus

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+
+from dateutil.relativedelta import relativedelta
+
 from odoo import fields, models
 
 
@@ -8,21 +12,39 @@ class Property(models.Model):
     name = fields.Char("Property Name", required=True)
     descritpion = fields.Text("Property Description")
     postcode = fields.Char("Postcode")
-    date_availability = fields.Date("Available Date")
+
+    date_availability = fields.Date("Available Date", copy=False, default=lambda _x: datetime.now() + relativedelta(months=+3))
+
     expected_price = fields.Float("Expected Price")
-    selling_price = fields.Float("Selling Price", required=True)
-    bedrooms = fields.Integer("Number of Bedrooms")
+    selling_price = fields.Float("Selling Price", copy=False, readonly=True)
+
+    bedrooms = fields.Integer("Number of Bedrooms", default=2)
     living_area = fields.Integer("Living Area")
     facades = fields.Integer("Number of Facades")
+
     has_garage = fields.Boolean("Has a Garage")
     has_garden = fields.Boolean("Has a Garden")
     garden_area = fields.Integer("Garden Area")
     garden_orientation = fields.Selection(
         string="Garden Orientation",
-            selection=[
-                ("north", "North"),
-                ("south", "South"),
-                ("east", "East"),
-                ("west", "West")
-            ]
+        selection=[
+            ("north", "North"),
+            ("south", "South"),
+            ("east", "East"),
+            ("west", "West"),
+        ],
     )
+
+    state = fields.Selection(
+        string="Property State",
+        selection=[
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled"),
+        ],
+        default="new",
+    )
+
+    active = fields.Boolean(default=True)
