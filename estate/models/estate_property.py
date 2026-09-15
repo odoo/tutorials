676522ded5b121903ca6326069bbbeb03ab18767
coleo-odoo <coleo@odoo.com>
2026-09-15 13:19:43 +0200

    [IMP] estate:  add a table property  with name and description

---
+++ estate/models/estate_property.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. see LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+class Property(models.Model):
+    _name = "estate.property"
+    _description = "Estate property"
