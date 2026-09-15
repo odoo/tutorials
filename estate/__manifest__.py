a2ee7f419f8bdab24da8a685549b6da452877a44
Delvaux-Jean-Baptiste <delvaux_jb@yahoo.fr>
2026-09-15 14:10:44 +0200

    [FIX] Styling issues [ADD] estate: add estate access for base users

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,5 +2,8 @@
     'name': "Estate",
     'depends': ["base"],
     'application': True,
-    'installable': True
+    'installable': True,
+    'data': [
+        'security/ir.model.access.csv'
+    ]
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,20 +1,21 @@
 from odoo import fields, models
 
+
 class EstatePropertyModel(models.Model):
     _name = "estate_property"
     _description = "The details of a property"
 
-    name= fields.Char('Estate Name', required= True)
-    description= fields.Text()
-    postcode= fields.Char()
-    date_availability= fields.Date()
-    expected_price= fields.Float(required= True)
-    selling_price= fields.Float()
-    bedrooms= fields.Integer()
-    living_area= fields.Integer()
-    facades= fields.Integer()
-    garage= fields.Boolean()
-    garden= fields.Boolean()
+    name = fields.Char('Estate Name', required = True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float(required = True)
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    has_garage = fields.Boolean()
+    has_garden = fields.Boolean()
     garden_orientation= fields.Selection(string='Type',
                                         selection=[('east', 'East'),
                                                     ('west', 'West'),
@@ -22,4 +23,3 @@ class EstatePropertyModel(models.Model):
                                                     ('south', 'South')
                                                     ]
                                         )
-
