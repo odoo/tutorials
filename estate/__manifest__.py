b9080c69abfd273c5cef3ea409567c2895be5857
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-09-15 16:50:41 +0200

    [IMP] estate : add menu view

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,5 +4,9 @@
     "author": "alpir",
     "application": True,
     "depends": ["base"],
-    "data": ["views/estate_property_views.xml", "security/ir.model.access.csv"],
+    "data": [
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
+        "security/ir.model.access.csv",
+    ],
 }
