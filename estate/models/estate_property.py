06cb4e649cfe236555e0bbfaec44ed716eb91b08
Valentyn Dementiev (vadem) <gerold122mail@gmail.com>
2026-09-15 17:28:06 +0200

    [ADD] Estate: CRUD for estate model
    Added menus and view functionality for estate property model, and configured model with restrictions for better expirience of using it.
    (Finished Chapter 5)
    
    Server Framework 101 (Chapter 5)

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -8,10 +8,10 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=fields.Datetime.today().replace(month=fields.Datetime.today().month +3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -20,3 +20,8 @@ class EstateProperty(models.Model):
     garden_orientation = fields.Selection(
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
     )
+    state = fields.Selection(required=True, copy=False, default='new',
+            selection=[('new', 'New'), ('offer_recieved', 'Offer Recieved'), ('offer_accepted', 'Offer Accepted'),
+                       ('sold', 'Sold'),('cancelled','Cancelled')],
+        )
+    active = fields.Boolean(default=True)
