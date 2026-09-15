433fc25838a23b1065a24ca65effd6a4e8b4b47c
Olivier Le Thanh Duong <ollet@odoo.com>
2026-09-15 14:55:47 +0200

    [IMP] estate: add Menu, form and list view
    task-6573598

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,6 +4,8 @@
     'depends': ['base'],
     'data': [
         'security/ir.model.access.csv',
+        'data/estate_property_views.xml',
+        'data/estate_menus.xml'
     ],
     'author': "Olivier Le Thanh Duong",
     'license': 'AGPL-3'
