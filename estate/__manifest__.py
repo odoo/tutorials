339767a40e0e89065540863a952f4f8b6507d040
Olivier Le Thanh Duong <ollet@odoo.com>
2026-09-15 14:35:11 +0200

    [IMP] estate: add first access rule to estate_property
    task-6573598

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,6 +2,9 @@
 {
     'name': 'estate',
     'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
     'author': "Olivier Le Thanh Duong",
     'license': 'AGPL-3'
 }
