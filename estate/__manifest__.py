f28308dbc12db402a7e3e525fa0f198272cebf96
Alexandre Navez <alex.navez007@gmail.com>
2026-09-15 11:43:50 +0200

    [ADD] Real estate module base files

---
+++ estate/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "alnav",
+    'category': 'Productivity',
+    'description': """
+    Our brand new real estate app!
+    """,
+    'application': True,
+    # data files always loaded at installation
+    'data': [],
+    # data files containing optionally loaded demonstration data
+    'demo': [],
+}
