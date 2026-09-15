29a8dc504dfe96e9fa86b3a94a42cd881de4b1b6
coleo-odoo <coleo@odoo.com>
2026-09-15 13:39:31 +0200

    [IMP] estate: set the column name and selling_price to not null

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -7,11 +7,11 @@ class Property(models.Model):
     _name = "estate.property"
     _description = "Estate property"
 
-    name = fields.Char()
+    name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
     date_availability = fields.Date()
-    expected_price = fields.Float()
+    expected_price = fields.Float(required=True)
     selling_price = fields.Float()
     bedrooms = fields.Integer()
     living_area = fields.Integer()
