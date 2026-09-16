e70a5c1d11a650cccfde8f9e214b00b732e7a0f1
DenisMth <denismathieu1203@gmail.com>
2026-09-16 10:22:25 +0200

    [ADD] Added some UI for the form and list view and experimented with menu depth until chap5

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,5 +4,10 @@
     'application': True,
     'installable': True,
     'author': "Demat",
-    'license': "AGPL-3"
+    'license': "AGPL-3",
+    'data': [
+        "models/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/menu_estate_property.xml",
+        ]
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,21 +1,22 @@
 from odoo import fields, models
+import datetime
 
 class TestModel(models.Model):
     _name = "estate_property_model"
     _description = "Estate property model test description"
 
-    name = fields.Char('Estate Property Model Name', required=True)
+    name = fields.Char(default="Unknown", required=True)
     description = fields.Text("Description of the Estate Propert Model")
-    postcode = fields.Char("5000")
-    date_availability = fields.Date("2026-09-15")
+    postcode = fields.Char("Post code")
+    date_availability = fields.Date("Availability date", default=datetime.date.today() + datetime.timedelta(days=90), copy=False)
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(copy=False, readonly=True)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection([("north", "North"), ("south", "South"), ("west", "West"), ("east", "East")])
-
-    
+    active = fields.Boolean(default=True)
+    state = fields.Selection([("new", "New"), ("offer_received", "Offer Received"), ("offer_accepted", "Offer Accepted"), ("sold", "Sold"), ("cancelled", "Cancelled")], default="new")
