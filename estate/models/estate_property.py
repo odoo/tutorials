4dcba3e06e9620127b7af464cc447ad314a2afa7
Miguel Morais <miguel.almeida.morais@gmail.com>
2026-09-16 11:01:30 +0200

    FIX [style check]

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -4,12 +4,12 @@ from odoo import fields, models
 class EstateProperty(models.Model):
     _name = "estate_property"
     _description = "This is the model for the real estate property :)"
-    name = fields.Char(required = True)
+    name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(copy = False, default = fields.Datetime.add(fields.Datetime.today(), months=3))
-    expected_price = fields.Float(required = True)
-    selling_price = fields.Float(readonly = True, copy = False)
+    date_availability = fields.Date(copy=False, default=fields.Datetime.add(fields.Datetime.today(), months=3))
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default = 2)
     living_area = fields.Integer()
     garage = fields.Boolean()
@@ -20,4 +20,4 @@ class EstateProperty(models.Model):
     active = fields.Boolean(default=True)
     stage = fields.Selection(string="Stage", 
     selection=[("new", "New"), ("offer received", "Offer Received"), ("offer accepted", "Offer Accepted"), ("sold", "Sold"), ("cancelled", "Cancelled")],
-    required = True, default = "new", copy = False)
+    required=True, default="new", copy=False)
