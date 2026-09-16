99d43a9950879ef3dfc3c67c0f08a47a5a689fed
Arthur De Clerck <47596905+Rutradc@users.noreply.github.com>
2026-09-16 09:58:59 +0200

    [ADD] chapter-4: added access rights

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,5 +1,12 @@
 {
     'name': "Estate",
+    'author': "Arthur De Clerck",
+    'license': "LGPL-3",
     'depends': ['base'],
-    'application': True
+    'category': "Real Estate/Brokerage",
+    'application': True,
+    'data': [
+        'security/ir.model.access.csv',
+        # 'security/security.xml',
+    ]
 }
