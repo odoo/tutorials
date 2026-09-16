dd0f6139a5210e878a2d0713da0e4da31d4bb5fc
Olivier Le Thanh Duong <ollet@odoo.com>
2026-09-16 11:39:16 +0200

    [IMP] estate: add deadline and validity on property

--- estate/models/property_offer.py
+++ estate/models/property_offer.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class PropertyOffer(models.Model):
@@ -15,3 +15,16 @@ class PropertyOffer(models.Model):
     )
     partner_id = fields.Many2one('res.partner', string="Partner", required=True)
     property_id = fields.Many2one('estate.property', required=True)
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute='_compute_deadline', inverse="_inverse_deadline")
+
+    @api.depends("validity", "create_date")
+    def _compute_deadline(self):
+        for record in self:
+            base_date = record.create_date or fields.Date.today()
+            record.date_deadline = fields.Date.add(base_date, days=record.validity)
+
+    def _inverse_deadline(self):
+        for record in self:
+            base_date = fields.Date.to_date(record.create_date) if record.create_date else fields.Date.today()
+            record.validity = (record.date_deadline - base_date).days
