a57ee8ff0243fc7b746918105aa295d54dda2c3e
Miguel Morais <miguel.almeida.morais@gmail.com>
2026-09-16 10:52:12 +0200

    [ADD] UI and more fields

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,6 +6,8 @@
     'author': 'mialm',
     'license': 'LGPL-3',
     'data' : [
-        "security/ir.model.access.csv"
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml"
     ]
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -4,16 +4,20 @@ class EstateProperty(models.Model):
     _name = "estate_property"
     _description = "This is the model for the real estate property :)"
 
-    name = fields.Char(required=True)
+    name = fields.Char(required = True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
-    expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    date_availability = fields.Date(copy = False, default = fields.Datetime.add(fields.Datetime.today(), months=3))
+    expected_price = fields.Float(required = True)
+    selling_price = fields.Float(readonly = True, copy = False)
+    bedrooms = fields.Integer(default = 2)
     living_area = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(string='Orientation',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')], help="Garden Orientation (North, South, East, West)")
+    active = fields.Boolean(default=True)
+    stage = fields.Selection(string="Stage", 
+        selection=[("new", "New"), ("offer received", "Offer Received"), ("offer accepted", "Offer Accepted"), ("sold", "Sold"), ("cancelled", "Cancelled")],
+        required = True, default = "new", copy = False)
