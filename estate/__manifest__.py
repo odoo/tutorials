8d9659a7b6b49774a9ab9c7579f9b668705eb341
manab-odoo <manab@odoo.com>
2026-09-16 11:31:30 +0200

    [IMP] Estate: CH4 CH5 Half CH6

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,4 +4,9 @@
     'summary' : 'Estate Module for the Tutorial',
     'author' : 'manab',
     'license': 'LGPL-3',
+    'data' : [
+        'views/estate_property_views.xml',
+        'views/estate_menu_views.xml',
+        'security/ir.model.access.csv',
+    ]
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -7,10 +7,10 @@ class Property(models.Model):
     name = fields.Char('Nom', required=True)
     description = fields.Text('Description')
     postcode = fields.Char('Post Code')
-    date_availability = fields.Date('Date Availability')
+    date_availability = fields.Date('Date Availability', copy=False, default=fields.Date.add(fields.Date.today(), months=3))
     expected_price = fields.Float('Expected Price', required=True)
-    selling_price = fields.Float('Selling Prince')
-    bedrooms = fields.Integer('# Bedrooms')
+    selling_price = fields.Float('Selling Prince', readonly=True, copy=False)
+    bedrooms = fields.Integer('# Bedrooms', default=2)
     living_area = fields.Integer('# Living Areas')
     facades = fields.Integer('# Facades')
     garage = fields.Boolean('Has Garage')
@@ -18,8 +18,25 @@ class Property(models.Model):
     garden_area = fields.Integer('# Garden Areas')
     garden_orientation = fields.Selection(
         string = 'Garden Orientation',
-        selection=[('north', 'North'), ('south','South'),('east','East'),('west','West')]
+        selection=[
+        ('north', 'North'), 
+        ('south','South'),
+        ('east','East'),
+        ('west','West')
+        ]
     )
-
-
-
+    active = fields.Boolean('Active', default=True)
+    state = fields.Selection(
+        string='State',
+        selection=[
+            ('new', 'New'), 
+            ('offer_received', 'Offer Received'), 
+            ('offer_accepted', 'Offer accepted'), 
+            ('sold', 'Sold'), 
+            ('cancelled', 'Cancelled')
+            ],
+        required=True,
+        copy=False,
+        default='new'
+    )
+    
