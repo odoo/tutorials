4f8b1c9520449e99d4b47884566856e79ae1f203
Olivier Le Thanh Duong <ollet@odoo.com>
2026-09-16 10:25:12 +0200

    [IMP] estate: Add Property tags
    add model, views, security and links

--- estate/models/__init__.py
+++ estate/models/__init__.py
@@ -1,2 +1,3 @@
 from .property import Property
 from .property_type import PropertyType
+from .property_tag import PropertyTag
--- estate/models/property.py
+++ estate/models/property.py
@@ -42,3 +42,4 @@ class Property(models.Model):
     property_type_id = fields.Many2one("estate.property.type", string="Type")
     buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
     salesperson_id = fields.Many2one("res.users", string="Salesman", default=lambda self: self.env.user)
+    tag_ids = fields.Many2many("estate.property.tag")
