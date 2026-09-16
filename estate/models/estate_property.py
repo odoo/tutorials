888003d5371d79b06d03c1b697a30a56327a491d
macau <macau@odoo.com>
2026-09-16 10:20:21 +0200

    [IMP] estate: End of chapter 8
    Adding computed fields and onchange observators.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import _, api, fields, models
 
 
 class EstateProperty(models.Model):
@@ -10,8 +10,12 @@ class EstateProperty(models.Model):
         required=True,
         default="Unknown",
     )
-    description = fields.Text('Description')
-    postcode = fields.Char('Postcode')
+    description = fields.Text(
+        string='Description',
+    )
+    postcode = fields.Char(
+        string='Postcode',
+    )
     date_availability = fields.Date(
         string='Date availability',
         copy=False,
@@ -30,11 +34,21 @@ class EstateProperty(models.Model):
         string='Bedrooms',
         default=2,
     )
-    living_area = fields.Integer('Living area')
-    facades = fields.Integer('Facades')
-    garage = fields.Boolean('Garage')
-    garden = fields.Boolean('Garden')
-    garden_area = fields.Integer('Garden area')
+    living_area = fields.Integer(
+        string='Living area',
+    )
+    facades = fields.Integer(
+        string='Facades',
+    )
+    garage = fields.Boolean(
+        string='Garage',
+    )
+    garden = fields.Boolean(
+        string='Garden',
+    )
+    garden_area = fields.Integer(
+        string='Garden area',
+    )
     garden_orientation = fields.Selection(
         string='Garden orientation',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
@@ -50,8 +64,51 @@ class EstateProperty(models.Model):
         copy=True,
         default='new',
     )
-    property_type = fields.Many2one('estate_property_type', string='Property type')
-    buyer = fields.Many2one('res.partner', string='Buyer', copy=False)
-    salesperson = fields.Many2one('res.users', string='Salesperson', default=lambda self: self.env.user)
-    tags = fields.Many2many('estate_property_tag', string='Tags')
-    offers = fields.One2many('estate_property_offer', 'property_id', string='Offers')
+    property_type = fields.Many2one(
+        comodel_name='estate_property_type',
+        string='Property type',
+    )
+    buyer = fields.Many2one(
+        comodel_name='res.partner',
+        string='Buyer',
+        copy=False,
+    )
+    salesperson = fields.Many2one(
+        comodel_name='res.users',
+        string='Salesperson',
+        default=lambda self: self.env.user,
+    )
+    tags = fields.Many2many(
+        comodel_name='estate_property_tag',
+        string='Tags',
+    )
+    offers = fields.One2many(
+        comodel_name='estate_property_offer',
+        inverse_name='property_id',
+        string='Offers',
+    )
+    total_area = fields.Integer(
+        compute='_compute_total_area',
+    )
+    best_price = fields.Float(
+        compute='_compute_best_price',
+    )
+
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
+    @api.depends('offers.price')
+    def _compute_best_price(self):
+        for record in self:
+            record.best_price = max(record.offers.mapped('price'))
+
+    @api.onchange('garden')
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = 'north'
+        else:
+            self.garden_area = 0
+            self.garden_orientation = None
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,15 +1,48 @@
-from odoo import fields, models
+from datetime import datetime, timedelta
+
+from odoo import api, fields, models
 
 
 class EstatePropertyOffer(models.Model):
     _name = 'estate_property_offer'
     _description = 'Estate Property Offer'
 
-    price = fields.Float('Price')
+    price = fields.Float(
+        string='Price',
+    )
     status = fields.Selection(
         string='Status',
         selection=[('accepted', 'Accepted'), ('refused', 'Refused')],
         copy=False,
     )
-    partner_id = fields.Many2one('res.partner', string='Partner', required=True)
-    property_id = fields.Many2one('estate_property', string='Property', required=True)
+    partner_id = fields.Many2one(
+        comodel_name='res.partner',
+        string='Partner',
+        required=True,
+    )
+    property_id = fields.Many2one(
+        comodel_name='estate_property',
+        string='Property',
+        required=True,
+    )
+    validity = fields.Integer(
+        string='Validity',
+        default=7,
+    )
+    date_deadline = fields.Date(
+        string='Deadline',
+        compute='_compute_date_deadline',
+        inverse='_inverse_date_deadline',
+    )
+
+    @api.depends('create_date', 'validity')
+    def _compute_date_deadline(self):
+        for record in self:
+            if record.create_date:
+                record.date_deadline = record.create_date + timedelta(days=record.validity)
+            else:
+                record.date_deadline = datetime.now() + timedelta(days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            record.validity = (record.date_deadline - record.create_date.date()).days
