fbaf256561ebda2d3dad5e04208881d362f5cf4e
JeromeVerkyndt <jerome.verkyndt@gmail.com>
2026-09-16 11:10:25 +0200

    Chapter 5 Add UI and new fields

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from odoo import fields, models
+from datetime import timedelta, date
 
 class Property(models.Model):
     _name = "estate.property"
     _description  = "Estate property model"
 
+
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=date.today()+ timedelta(days=90))
     expected_price  = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms  = fields.Integer()
+    selling_price = fields.Float(copy=False, readonly=True)
+    bedrooms  = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -23,4 +25,10 @@ class Property(models.Model):
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
         help="Orientation is used to define the orientation of the garden"
     )
-    a = fields.Text
+    active = fields.Boolean("Active", default=True)  
+    state =  fields.Selection(
+        string='State',
+        selection=[('new', 'New'),('offer_received', 'Offer Received'),('offer_accepted', 'Offer Accepted'),('sold', 'Sold'),('cancelled', 'Cancelled')],
+        required= True,
+        default="new"
+    )
