d8333040b08d3dcafb88733c0be92f820a2dbf15
cailleaux-maxime <macai@odoo.com>
2026-09-17 09:15:14 +0200

    [IMP] estate: Add computation field for offer
    As refer in the chapter 8 of the training

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class EstateProperty(models.Model):
@@ -26,6 +26,7 @@ class EstateProperty(models.Model):
                    ('west', 'West'),
         ]
     )
+    total_area = fields.Integer(compute='_compute_total_area', string='Total Area')
     active = fields.Boolean('Active', default=True)
     state = fields.Selection(
         string='State',
@@ -50,3 +51,19 @@ class EstateProperty(models.Model):
 
 #   Offers
     offer_ids = fields.One2many('estate.property.offer', 'property_id', string='Offer')
+    best_price = fields.Float(compute='_compute_best_price', string='Best Offer')
+
+#   Computation Fields:
+
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
+    @api.depends('offer_ids.price')
+    def _compute_best_price(self):
+        for record in self:
+            if len(record.offer_ids) > 0:
+                record.best_price = max(record.offer_ids.mapped('price'))
+            else:
+                record.best_price = 0.
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from datetime import date
 
 
 class EstatePropertyOffer(models.Model):
@@ -10,5 +11,20 @@ class EstatePropertyOffer(models.Model):
         ('accepted', 'Accepted'),
         ('refused', 'Refused')
     ], copy=False, string='Status')
+    validity = fields.Integer(string='Validity (Days)', default=7)
+    date_deadline = fields.Date(compute='_compute_date_deadline', inverse='_inverse_date_deadline', string='Deadline')
     partner_id = fields.Many2one('res.partner', required=True, string='Partner')
     property_id = fields.Many2one('estate.property', required=True)
+
+#   computed fields
+
+    @api.depends('validity', 'create_date')
+    def _compute_date_deadline(self):
+        for record in self:
+#           Fallback when create_date is not set yet:
+            base_date = record.create_date or fields.Date.today()
+            record.date_deadline = fields.Date.add(base_date, days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            record.validity = (record.date_deadline - record.create_date.date()).days
