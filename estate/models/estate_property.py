0d0f86b28cff730f376a814f1f1584ccd81ca367
JeromeVerkyndt <jerome.verkyndt@gmail.com>
2026-09-17 13:21:33 +0200

    [FIX] estate: Use of a lambda for the 'date_availability' default parameter and code formatting.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,7 +1,4 @@
-# -*- coding: utf-8 -*-
-
 from odoo import fields, models
-from datetime import timedelta, date
 
 class Property(models.Model):
     _name = "estate.property"
@@ -11,7 +8,7 @@ class Property(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(copy=False, default=date.today()+ timedelta(days=90))
+    date_availability = fields.Date(copy=False, default=lambda self: fields.Date.add(fields.Date.today(), months=3))
     expected_price  = fields.Float(required=True)
     selling_price = fields.Float(copy=False, readonly=True)
     bedrooms  = fields.Integer(default=2)
@@ -22,13 +19,24 @@ class Property(models.Model):
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
         string='Orientation',
-        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
+        selection=[
+            ('north', 'North'),
+            ('south', 'South'),
+            ('east', 'East'), 
+            ('west', 'West')
+        ],
         help="Orientation is used to define the orientation of the garden"
     )
     active = fields.Boolean("Active", default=True)  
     state =  fields.Selection(
         string='State',
-        selection=[('new', 'New'),('offer_received', 'Offer Received'),('offer_accepted', 'Offer Accepted'),('sold', 'Sold'),('cancelled', 'Cancelled')],
+        selection=[
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('cancelled', 'Cancelled')
+        ],
         required= True,
         default="new"
     )
