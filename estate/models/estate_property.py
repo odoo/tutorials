f945a7519b31d120c10c9ecd601a718253c8fde8
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-09-17 15:58:38 +0200

    [IMP] estate : add onchange on garden field

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -64,3 +64,12 @@ class Property(models.Model):
                 record.best_price = 0
             else:
                 record.best_price = max(record.offer_ids.mapped("price"))
+
+    @api.onchange("garden")
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = "north"
+        else:
+            self.garden_area = 0
+            self.garden_orientation = None
