b1593212c53d51a553f683b6eaa9c2ae6f04248f
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-09-17 09:38:09 +0200

    [IMP] estate : add buyer and salesman field to property model

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -45,3 +45,5 @@ class Property(models.Model):
         default="new",
     )
     property_type_id = fields.Many2one("estate.property.type", string="Property Type")
+    buyer = fields.Many2one("res.partner", copy=False)
+    salesman = fields.Many2one("res.users", default=lambda self: self.env.user)
