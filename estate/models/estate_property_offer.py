4759e22788a766cd1f8d9786d8bab857f6162ec3
aalon-odoo <aalon@odoo.com>
2026-09-17 13:28:11 +0200

    [FIX] estate_property_offer model: adding string name to fields

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -5,19 +5,24 @@ class EstatePropertyOffer(models.Model):
     _name = 'estate.property.offer'
     _description = 'Estate Property Offer'
 
-    price = fields.Float()
+    price = fields.Float(
+        string='Price',
+    )
     status = fields.Selection(
         selection=[
             ('accepted', 'Accepted'),
             ('refused', 'Refused')
         ],
         copy=False,
+        string='Status',
     )
     partner_id = fields.Many2one(
         comodel_name='res.partner',
         required=True,
+        string='Partner',
     )
     property_id = fields.Many2one(
         comodel_name='estate.property',
         required=True,
+        string='Property',
     )
