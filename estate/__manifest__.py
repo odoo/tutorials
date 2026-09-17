1943e9f10f8815a31cc05ceaf8c3c1cac8b8bdc4
Arfra <Arfra@odoo.com>
2026-09-17 17:51:01 +0200

    [IMP] estate: add menu to estate property module

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,5 +2,5 @@
     'name': "Real Estate",
     'application': True,
     'depends': ['base'],
-    'data':['security/ir.model.access.csv']
+    'data':['security/ir.model.access.csv','views/estate_property_views.xml','views/estate_menu.xml']
 }
