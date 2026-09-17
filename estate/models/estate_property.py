a2b49482dd4ba4ad5f0db6c0de8a819f05fdb0a1
DenisMth <denismathieu1203@gmail.com>
2026-09-17 13:54:31 +0200

    [ADD] Chap 6 views + Chap7 until many2one

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,14 +1,19 @@
 from odoo import fields, models
 import datetime
 
-class TestModel(models.Model):
-    _name = "estate_property_model"
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
     _description = "Estate property model test description"
 
-    name = fields.Char(default="Unknown", required=True)
+    name = fields.Char(required=True)
     description = fields.Text("Description of the Estate Propert Model")
     postcode = fields.Char("Post code")
-    date_availability = fields.Date("Availability date", default=datetime.date.today() + datetime.timedelta(days=90), copy=False)
+    date_availability = fields.Date(
+        "Availability date",
+        default=lambda self: datetime.date.today() + datetime.timedelta(days=90),
+        copy=False
+    )
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(copy=False, readonly=True)
     bedrooms = fields.Integer(default=2)
@@ -17,6 +22,25 @@ class TestModel(models.Model):
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
-    garden_orientation = fields.Selection([("north", "North"), ("south", "South"), ("west", "West"), ("east", "East")])
+    garden_orientation = fields.Selection(
+        selection=[
+            ("north", "North"),
+            ("south", "South"),
+            ("west", "West"),
+            ("east", "East")
+        ]
+    )
     active = fields.Boolean(default=True)
-    state = fields.Selection([("new", "New"), ("offer_received", "Offer Received"), ("offer_accepted", "Offer Accepted"), ("sold", "Sold"), ("cancelled", "Cancelled")], default="new")
+    state = fields.Selection(
+        selection=[
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled")
+        ],
+        default="new",
+    )
+    property_type_id = fields.Many2one("estate.property.type", string="Property Type")
+    buyer_id = fields.Many2one("res.partner", string="Buyer")
+    salesperson_id = fields.Many2one("res.users", string="Salesperson")
