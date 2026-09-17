e3f80050be3337fd1eb3f32f243ddc0e29989610
Valentyn Dementiev (vadem) <gerold122mail@gmail.com>
2026-09-17 11:52:44 +0200

    [ADD] Estate:relations between models added
    Added new models (tag, type and offer for estate), and established relations between those models. Also some minor fixes due to PR commits and ci/style
    
    Server framework 101 - Chapter 7

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,10 +5,11 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "This is estate property model"
 
+    property_type_id = fields.Many2one("estate.property.type", string="Type")
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(copy=False, default=fields.Datetime.today().replace(month=fields.Datetime.today().month +3))
+    date_availability = fields.Date(copy=False, default=lambda self: fields.Date.add(fields.Date.today(), months=3))
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
@@ -22,6 +23,13 @@ class EstateProperty(models.Model):
     )
     state = fields.Selection(required=True, copy=False, default='new',
             selection=[('new', 'New'), ('offer_recieved', 'Offer Recieved'), ('offer_accepted', 'Offer Accepted'),
-                       ('sold', 'Sold'),('cancelled','Cancelled')],
+                       ('sold', 'Sold'),('cancelled', 'Cancelled')],
         )
     active = fields.Boolean(default=True)
+    
+    buyer_id =fields.Many2one("res.partner", string="Buyer", copy=False)
+    salesperson_id = fields.Many2one("res.users", string="Salesperson", default=lambda self: self.env.user)
+    
+    tag_ids = fields.Many2many("estate.property.tag")
+    offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
+    
---
+++ estate/models/estate_property_type.py
@@ -0,0 +1,9 @@
+from odoo import models, fields
+
+
+class EstatePropertyType(models.Model):
+    _name = "estate.property.type"
+    _description = "Type of estate property"
+
+    name = fields.Char(required=True)
+    
