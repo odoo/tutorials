18f441e3a20ce02fb701696d9e66663e278c7e61
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-09-17 10:31:21 +0200

    [IMP] estate : add tags to property model

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -45,5 +45,6 @@ class Property(models.Model):
         default="new",
     )
     property_type_id = fields.Many2one("estate.property.type", string="Property Type")
+    tags_ids = fields.Many2many("estate.property.tag")
     buyer = fields.Many2one("res.partner", copy=False)
     salesman = fields.Many2one("res.users", default=lambda self: self.env.user)
