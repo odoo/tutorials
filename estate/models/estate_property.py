50d8700757b690b8e41f943331f63a0320956daa
Surap <surap@odoo.com>
2026-09-17 16:02:03 +0200

    8.2 (best price) 	modified:   estate/models/estate_property.py 	modified:   estate/views/estate_property_views.xml

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -33,8 +33,14 @@ class EstateProperty(models.Model):
     tag_ids = fields.Many2many("estate.property.tag", string="Tags")
     offer_ids = fields.One2many("estate.property.offer", "property_id")
     total_area = fields.Integer(compute='_compute_total_area')
+    best_price = fields.Float(compute='_compute_best_price')
 
     @api.depends('garden_area', 'living_area')
     def _compute_total_area(self):
         for record in self:
             record.total_area = record.living_area + record.garden_area
+
+    @api.depends('offer_ids.price')
+    def _compute_best_price(self):
+        for record in self:
+            record.best_price = max([offer.price for offer in record.offer_ids] + [0])
