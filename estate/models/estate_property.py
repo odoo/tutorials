b106c451c0d77860b72217b9df252cb314ac0ca7
aalon-odoo <aalon@odoo.com>
2026-09-17 10:51:26 +0200

    [IMP] many2one fields property_type in estate_property model

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -8,6 +8,10 @@ class EstateProperty(models.Model):
     name = fields.Char('Title', required=True)
     description = fields.Text('Description')
     postcode = fields.Char('Postcode')
+    property_type_id = fields.Many2one(
+        'estate.property.type',
+        string='Property Type',
+    )
     date_availability = fields.Date(
         string='Available From',
         copy=False,
