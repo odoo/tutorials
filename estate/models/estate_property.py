146ad457416f95be82e9ac99faf8e6c0f30d2556
manab-odoo <manab@odoo.com>
2026-09-18 17:01:49 +0200

    [FIX] Estate: Fix Best Price Attempt

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -60,4 +60,4 @@ class Property(models.Model):
     @api.depends("offer_ids")
     def _compute_best_price(self):
         for record in self:
-            record.best_price = max(record.offer_ids.mapped("price"))
+            record.best_price = max(record.offer_ids.mapped("price")) if record.offer_ids.mapped("price") else 0
