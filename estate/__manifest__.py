11620be5c761d72f7bc9581adae9fc53d19ce079
DenisMth <denismathieu1203@gmail.com>
2026-09-18 09:17:35 +0200

    [ADD] Chap 7 Relations Between Models done

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -8,7 +8,6 @@
     'data': [
         "models/ir.model.access.csv",
         "views/estate_property_views.xml",
-        "views/estate_property_type_views.xml",
         "views/menu_estate_property.xml",
         "views/list_estate_property.xml",
         "views/form_estate_property.xml",
