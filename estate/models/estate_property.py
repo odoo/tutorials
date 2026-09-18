94b80ec1d80e965369ad84db53e86dd8c180624f
JeromeVerkyndt <jerome.verkyndt@gmail.com>
2026-09-18 09:41:33 +0200

    [IMP] estate: add computed field for 'total_area'.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 class Property(models.Model):
     _name = "estate.property"
@@ -44,3 +44,10 @@ class Property(models.Model):
     buyer = fields.Many2one('res.partner', string='Buyer')
     tag_ids = fields.Many2many("estate.property.tag", string="Property Tag")
     offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
+    total_area = fields.Integer(compute="_compute_area", string="Total Area (sqm)")
+
+    @api.depends('living_area', 'garden_area')
+    def _compute_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
