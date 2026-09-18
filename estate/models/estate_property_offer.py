65fb4b80d0986f4d0c0ad7007168182219f8a1d8
JeromeVerkyndt <jerome.verkyndt@gmail.com>
2026-09-18 15:21:51 +0200

    [IMP] estate: add validity et date_deadline field with compute and inverse method.

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class PropertyOffer(models.Model):
@@ -16,3 +16,22 @@ class PropertyOffer(models.Model):
         )
     partner_id = fields.Many2one("res.partner", string="Partner", required=True)
     property_id = fields.Many2one("estate.property", string="Property", required=True)
+    validity = fields.Integer(default=7, string="Validity (days)")
+    date_deadline = fields.Date(string="Deadline", compute="_compute_deadline", inverse="_inverse_deadline")
+
+    @api.depends('validity')
+    def _compute_deadline(self):
+        for record in self:
+            create_date = fields.Date.to_date(record.create_date) if record.create_date else fields.Date.today()
+            record.date_deadline = fields.Date.add(create_date, days=record.validity)
+
+    def _inverse_deadline(self):
+        for record in self:
+            create_date = fields.Date.to_date(record.create_date) if record.create_date else fields.Date.today()
+            if record.date_deadline:
+                record.validity = (
+                    record.date_deadline - fields.Date.to_date(create_date)
+                ).days
+            else:
+                record.validity = 0
+
