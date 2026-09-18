be7e94c8d8e7c3adff937d9bb72044c8459c491f
Olivier Le Thanh Duong <ollet@odoo.com>
2026-09-18 09:31:12 +0200

    [ADD] estate_account: Create invoice for sold properties

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,12 @@
+# noinspection statement-effect
+{
+    "name": "estate_account",
+    "depends": ["estate", "account"],
+    "data": [
+    ],
+    "author": "Odoo S.A.",
+    "license": "AGPL-3",
+    "summary": """
+    Create invoice for sold properties
+    """,
+}
