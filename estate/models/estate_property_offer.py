674038e67d75f166d685f416e685ff451efb050f
aalon-odoo <aalon@odoo.com>
2026-09-18 10:39:41 +0200

    [IMP] estate_property_offer : adding refuse/accept btn + logic

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 
 class EstatePropertyOffer(models.Model):
@@ -51,3 +52,36 @@ class EstatePropertyOffer(models.Model):
             record.validity = (
                 record.date_deadline - fields.Date.to_date(create_date)
             ).days
+
+    def action_accept(self):
+        for record in self:
+            if record.property_id.state in ('sold', 'canceled'):
+                raise UserError("Cannot accept offer for sold or canceled property.")
+
+            record.status = 'accepted'
+
+            accepted_offer = self.search([
+                ('property_id', '=', record.property_id.id),
+                ('id', '!=', record.id),
+                ('status', '=', 'accepted'),
+            ])
+
+            accepted_offer.status = 'refused'
+
+            record.property_id.state = 'offer_accepted'
+            record.property_id.selling_price = record.price
+            record.property_id.buyer = record.partner_id
+        return True
+
+    def action_refuse(self):
+        for record in self:
+            if record.property_id.state in ('sold', 'canceled'):
+                raise UserError("Cannot refuse offer for sold or canceled property.")
+
+            if record.status == 'accepted':
+                record.property_id.state = 'offer_received'
+                record.property_id.buyer = False
+                record.property_id.selling_price = False
+
+            record.status = 'refused'
+        return True
